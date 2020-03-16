61de9045688fbe91c7b030e3d0af491763b6e245
zac-williamson <blorktronics@gmail.com>
2020-03-16 17:51:27 +0000

    added native support for ECDSA signatures
    ecdsa verification passes against NIST test data

---
+++ circuits/cpp/barretenberg/src/aztec/crypto/hashers/hashers.hpp
@@ -0,0 +1,26 @@
+#include "../blake2s/blake2s.hpp"
+#include "../sha256/sha256.hpp"
+#include "../keccak/keccak.hpp"
+
+#include <vector>
+#include "memory.h"
+struct KeccakHasher {
+    static std::vector<uint8_t> hash(const std::vector<uint8_t>& message)
+    {
+        keccak256 hash_result = ethash_keccak256(&message[0], message.size());
+
+        std::vector<uint8_t> output;
+        output.resize(32);
+
+        memcpy((void*)&output[0], (void*)&hash_result.word64s[0], 32);
+        return output;
+    }
+};
+
+struct Sha256Hasher {
+    static std::vector<uint8_t> hash(const std::vector<uint8_t>& message) { return sha256::sha256(message); }
+};
+
+struct Blake2sHasher {
+    static std::vector<uint8_t> hash(const std::vector<uint8_t>& message) { return blake2::blake2s(message); }
+};
