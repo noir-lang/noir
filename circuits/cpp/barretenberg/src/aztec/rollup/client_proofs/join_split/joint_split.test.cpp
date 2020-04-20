5c0f510172e169f8d4e85b65b7419a161e119919
Charlie Lye <karl.lye@gmail.com>
2020-04-20 20:17:19 +0100

    Additional tests.

--- circuits/cpp/barretenberg/src/aztec/rollup/client_proofs/join_split/joint_split.test.cpp
+++ circuits/cpp/barretenberg/src/aztec/rollup/client_proofs/join_split/joint_split.test.cpp
@@ -102,6 +102,27 @@ TEST_F(client_proofs_join_split, test_2_input_notes)
     EXPECT_TRUE(sign_and_verify(tx));
 }
 
+TEST_F(client_proofs_join_split, test_0_output_notes)
+{
+    tx_note input_note1 = { user.public_key, 100, user.note_secret };
+    tx_note input_note2 = { user.public_key, 50, user.note_secret };
+    tx_note output_note1 = { user.public_key, 0, user.note_secret };
+    tx_note output_note2 = { user.public_key, 0, user.note_secret };
+
+    join_split_tx tx;
+    tx.owner_pub_key = user.public_key;
+    tx.public_input = 0;
+    tx.public_output = 150;
+    tx.num_input_notes = 2;
+    tx.input_index = { 0, 1 };
+    tx.merkle_root = tree->root();
+    tx.input_path = { tree->get_hash_path(0), tree->get_hash_path(1) };
+    tx.input_note = { input_note1, input_note2 };
+    tx.output_note = { output_note1, output_note2 };
+
+    EXPECT_TRUE(sign_and_verify(tx));
+}
+
 TEST_F(client_proofs_join_split, test_unbalanced_notes_fails)
 {
     tx_note input_note1 = { user.public_key, 100, user.note_secret };
@@ -121,4 +142,117 @@ TEST_F(client_proofs_join_split, test_unbalanced_notes_fails)
     tx.output_note = { output_note1, output_note2 };
 
     EXPECT_FALSE(sign_and_verify(tx));
+}
+
+TEST_F(client_proofs_join_split, test_wrong_input_note_owner_fails)
+{
+    tx_note input_note1 = { user.public_key, 100, user.note_secret };
+    tx_note input_note2 = { grumpkin::g1::element::random_element(), 50, user.note_secret };
+    tx_note output_note1 = { user.public_key, 70, user.note_secret };
+    tx_note output_note2 = { user.public_key, 80, user.note_secret };
+
+    join_split_tx tx;
+    tx.owner_pub_key = user.public_key;
+    tx.public_input = 0;
+    tx.public_output = 0;
+    tx.num_input_notes = 2;
+    tx.input_index = { 0, 1 };
+    tx.merkle_root = tree->root();
+    tx.input_path = { tree->get_hash_path(0), tree->get_hash_path(1) };
+    tx.input_note = { input_note1, input_note2 };
+    tx.output_note = { output_note1, output_note2 };
+
+    EXPECT_FALSE(sign_and_verify(tx));
+}
+
+TEST_F(client_proofs_join_split, test_random_output_owners_succeeds)
+{
+    tx_note input_note1 = { user.public_key, 100, user.note_secret };
+    tx_note input_note2 = { user.public_key, 50, user.note_secret };
+    tx_note output_note1 = { grumpkin::g1::element::random_element(), 70, user.note_secret };
+    tx_note output_note2 = { grumpkin::g1::element::random_element(), 80, user.note_secret };
+
+    join_split_tx tx;
+    tx.owner_pub_key = user.public_key;
+    tx.public_input = 0;
+    tx.public_output = 0;
+    tx.num_input_notes = 2;
+    tx.input_index = { 0, 1 };
+    tx.merkle_root = tree->root();
+    tx.input_path = { tree->get_hash_path(0), tree->get_hash_path(1) };
+    tx.input_note = { input_note1, input_note2 };
+    tx.output_note = { output_note1, output_note2 };
+
+    EXPECT_TRUE(sign_and_verify(tx));
+}
+
+TEST_F(client_proofs_join_split, test_wrong_hash_path_fails)
+{
+    tx_note input_note1 = { user.public_key, 100, user.note_secret };
+    tx_note input_note2 = { user.public_key, 50, user.note_secret };
+    tx_note output_note1 = { user.public_key, 70, user.note_secret };
+    tx_note output_note2 = { user.public_key, 80, user.note_secret };
+
+    join_split_tx tx;
+    tx.owner_pub_key = user.public_key;
+    tx.public_input = 0;
+    tx.public_output = 0;
+    tx.num_input_notes = 2;
+    tx.input_index = { 0, 2 };
+    tx.merkle_root = tree->root();
+    tx.input_path = { tree->get_hash_path(0), tree->get_hash_path(1) };
+    tx.input_note = { input_note1, input_note2 };
+    tx.output_note = { output_note1, output_note2 };
+
+    EXPECT_FALSE(sign_and_verify(tx));
+}
+
+TEST_F(client_proofs_join_split, test_wrong_merkle_root_fails)
+{
+    tx_note input_note1 = { user.public_key, 100, user.note_secret };
+    tx_note input_note2 = { user.public_key, 50, user.note_secret };
+    tx_note output_note1 = { user.public_key, 70, user.note_secret };
+    tx_note output_note2 = { user.public_key, 80, user.note_secret };
+
+    join_split_tx tx;
+    tx.owner_pub_key = user.public_key;
+    tx.public_input = 0;
+    tx.public_output = 0;
+    tx.num_input_notes = 2;
+    tx.input_index = { 0, 1 };
+    tx.merkle_root = fr::random_element();
+    tx.input_path = { tree->get_hash_path(0), tree->get_hash_path(1) };
+    tx.input_note = { input_note1, input_note2 };
+    tx.output_note = { output_note1, output_note2 };
+
+    EXPECT_FALSE(sign_and_verify(tx));
+}
+
+TEST_F(client_proofs_join_split, test_wrong_signature_fails)
+{
+    tx_note input_note1 = { user.public_key, 100, user.note_secret };
+    tx_note input_note2 = { user.public_key, 50, user.note_secret };
+    tx_note output_note1 = { user.public_key, 70, user.note_secret };
+    tx_note output_note2 = { user.public_key, 80, user.note_secret };
+
+    // Going to sign with this incorrect key.
+    auto pk = grumpkin::fr::random_element();
+
+    join_split_tx tx;
+    tx.owner_pub_key = user.public_key;
+    tx.public_input = 0;
+    tx.public_output = 0;
+    tx.num_input_notes = 2;
+    tx.input_index = { 0, 2 };
+    tx.merkle_root = tree->root();
+    tx.input_path = { tree->get_hash_path(0), tree->get_hash_path(1) };
+    tx.input_note = { input_note1, input_note2 };
+    tx.output_note = { output_note1, output_note2 };
+    tx.signature = sign_notes({ tx.input_note[0], tx.input_note[1], tx.output_note[0], tx.output_note[1] },
+                                { pk, grumpkin::g1::one * pk });
+
+    auto prover = new_join_split_prover(tx);
+    auto proof = prover.construct_proof();
+
+    EXPECT_FALSE(verify_proof(proof));
 }
