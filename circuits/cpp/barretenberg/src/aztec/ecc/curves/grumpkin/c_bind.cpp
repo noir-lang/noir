6ec808b7967903af6db149ba66faaa7c8e9516e1
Charlie Lye <karl.lye@gmail.com>
2020-05-02 16:48:20 +0100

    Initial WIP.

---
+++ circuits/cpp/barretenberg/src/aztec/ecc/curves/grumpkin/c_bind.cpp
@@ -0,0 +1,15 @@
+#include "grumpkin.hpp"
+
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+extern "C" {
+
+WASM_EXPORT void ecc_grumpkin__mul(uint8_t const* point_buf, uint8_t const* scalar_buf, uint8_t* result)
+{
+    auto point = from_buffer<grumpkin::g1::affine_element>(point_buf);
+    auto scalar = from_buffer<grumpkin::fr>(scalar_buf);
+    grumpkin::g1::affine_element r = point * scalar;
+    write(result, r);
+}
+
+}
