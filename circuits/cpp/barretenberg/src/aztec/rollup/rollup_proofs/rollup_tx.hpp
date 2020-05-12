a52dc1dedff8e0c3d638a077e579dfe2d3956d5d
Charlie Lye <karl.lye@gmail.com>
2020-05-12 12:30:22 +0100

    Fix serialization of rollup_tx.

--- circuits/cpp/barretenberg/src/aztec/rollup/rollup_proofs/rollup_tx.hpp
+++ circuits/cpp/barretenberg/src/aztec/rollup/rollup_proofs/rollup_tx.hpp
@@ -30,84 +30,87 @@ struct rollup_tx {
     std::vector<fr> new_null_roots;
     std::vector<fr_hash_path> old_null_paths;
     std::vector<fr_hash_path> new_null_paths;
+
+// VSCode doesn't recognise this C++20 feature.
+#ifndef __INTELLISENSE__
+    bool operator==(rollup_tx const&) const = default;
+#endif
 };
 
-template <typename B>
-inline void read(B& buf, rollup_tx& tx)
+template <typename B> inline void read(B& buf, rollup_tx& tx)
 {
     ::read(buf, tx.rollup_id);
     ::read(buf, tx.num_txs);
     ::read(buf, tx.proof_lengths);
     ::read(buf, tx.data_start_index);
     read(buf, tx.txs);
+
+    read(buf, tx.rollup_root);
     read(buf, tx.old_data_root);
-    // read(buf, tx.old_null_root);
-    // read(buf, tx.old_data_paths);
-    // read(buf, tx.old_null_paths);
-    // read(buf, tx.new_data_root);
-    // read(buf, tx.new_null_root);
-    // read(buf, tx.new_data_paths);
-    // read(buf, tx.new_null_paths);
+    read(buf, tx.new_data_root);
+    read(buf, tx.old_data_path);
+    read(buf, tx.new_data_path);
+
+    read(buf, tx.old_null_root);
+    read(buf, tx.new_null_roots);
+    read(buf, tx.old_null_paths);
+    read(buf, tx.new_null_paths);
 }
 
-template <typename B>
-inline void write(B& buf, rollup_tx const& tx)
+template <typename B> inline void write(B& buf, rollup_tx const& tx)
 {
     ::write(buf, tx.rollup_id);
     ::write(buf, tx.num_txs);
     ::write(buf, tx.proof_lengths);
     ::write(buf, tx.data_start_index);
+
     write(buf, tx.txs);
+    write(buf, tx.rollup_root);
     write(buf, tx.old_data_root);
-    // write(buf, tx.old_null_root);
-    // write(buf, tx.old_data_paths);
-    // write(buf, tx.old_null_paths);
-    // write(buf, tx.new_data_root);
-    // write(buf, tx.new_null_root);
-    // write(buf, tx.new_data_paths);
-    // write(buf, tx.new_null_paths);
-}
-/*
-inline bool operator==(rollup_tx const& lhs, rollup_tx const& rhs){
-    return lhs.rollup_id == rhs.rollup_id && lhs.num_txs == rhs.num_txs && lhs.proof_lengths == rhs.proof_lengths &&
-           lhs.txs == rhs.txs && lhs.old_data_root == rhs.old_data_root && lhs.old_null_root == rhs.old_null_root &&
-           lhs.old_data_paths == rhs.old_data_paths && lhs.old_null_paths == rhs.old_null_paths &&
-           lhs.new_data_root == rhs.new_data_root && lhs.new_null_root == rhs.new_null_root &&
-           lhs.new_data_paths == rhs.new_data_paths && lhs.new_null_paths == rhs.new_null_paths;
+    write(buf, tx.new_data_root);
+    write(buf, tx.old_data_path);
+    write(buf, tx.new_data_path);
+
+    write(buf, tx.old_null_root);
+    write(buf, tx.new_null_roots);
+    write(buf, tx.old_null_paths);
+    write(buf, tx.new_null_paths);
 }
 
-inline std::ostream&
-operator<<(std::ostream& os, rollup_tx const& tx)
+inline std::ostream& operator<<(std::ostream& os, rollup_tx const& tx)
 {
     os << "rollup_id: " << tx.rollup_id << "\n";
     os << "num_txs: " << tx.num_txs << "\n";
     os << "proof_lengths: " << tx.proof_lengths << "\n";
+    os << "data_start_index: " << tx.data_start_index << "\n";
     os << "proof_data:\n";
     for (auto p : tx.txs) {
         os << p << "\n";
     }
+
+    os << "\nDATA TREE UPDATE CONTEXT:\n";
+    os << "rollup_root: " << tx.rollup_root << "\n";
     os << "old_data_root: " << tx.old_data_root << "\n";
+    os << "new_data_root: " << tx.new_data_root << "\n";
+    os << "old_data_path: " << tx.old_data_path << "\n";
+    os << "new_data_path: " << tx.new_data_path << "\n";
+
+    os << "\nNULL TREE UPDATE CONTEXT:\n";
     os << "old_null_root: " << tx.old_null_root << "\n";
-    os << "old_data_paths:\n";
-    for (auto e : tx.old_data_paths) {
-        os << e.first << ": " << e.second;
+    os << "new_null_roots:\n";
+    for (auto e : tx.new_null_roots) {
+        os << e << "\n";
     }
     os << "old_null_paths:\n";
     for (auto e : tx.old_null_paths) {
-        os << e.first << ": " << e.second;
-    }
-    os << "new_data_root: " << tx.new_data_root << "\n";
-    os << "new_null_root: " << tx.new_null_root << "\n";
-    os << "new_data_paths:\n";
-    for (auto e : tx.new_data_paths) {
-        os << e.first << ": " << e.second;
+        os << e << "\n";
     }
     os << "new_null_paths:\n";
     for (auto e : tx.new_null_paths) {
-        os << e.first << ": " << e.second;
+        os << e << "\n";
     }
     return os;
 }
-*/
+
 } // namespace rollup_proofs
 } // namespace rollup
--- circuits/cpp/barretenberg/src/aztec/rollup/rollup_proofs/rollup_tx.test.cpp
+++ circuits/cpp/barretenberg/src/aztec/rollup/rollup_proofs/rollup_tx.test.cpp
@@ -6,30 +6,28 @@ using namespace barretenberg;
 
 TEST(rollup_tx, test_serialization)
 {
-    /*
+    auto random_pair = std::make_pair(fr::random_element(), fr::random_element());
+
     rollup_tx rollup;
     rollup.rollup_id = 5;
     rollup.num_txs = 3;
     rollup.proof_lengths = 123;
     rollup.data_start_index = 0;
     rollup.txs = std::vector(rollup.num_txs, std::vector<uint8_t>(rollup.proof_lengths, 0x80));
+
+    rollup.rollup_root = fr::random_element();
     rollup.old_data_root = fr::random_element();
-    rollup.old_null_root = fr::random_element();
-    auto random_pair = std::make_pair(fr::random_element(), fr::random_element());
-    rollup.old_data_paths =
-        std::vector(rollup.num_txs * 2, std::make_pair(uint128_t(0), fr_hash_path(32, random_pair)));
-    rollup.old_null_paths =
-        std::vector(rollup.num_txs * 2, std::make_pair(uint128_t(0), fr_hash_path(128, random_pair)));
     rollup.new_data_root = fr::random_element();
-    rollup.new_null_root = fr::random_element();
-    rollup.new_data_paths =
-        std::vector(rollup.num_txs * 2, std::make_pair(uint128_t(0), fr_hash_path(32, random_pair)));
-    rollup.new_null_paths =
-        std::vector(rollup.num_txs * 2, std::make_pair(uint128_t(0), fr_hash_path(128, random_pair)));
+    rollup.old_data_path = fr_hash_path(32, random_pair);
+    rollup.new_data_path = fr_hash_path(32, random_pair);
+
+    rollup.old_null_root = fr::random_element();
+    rollup.new_null_roots = std::vector(rollup.num_txs * 2, fr::random_element());
+    rollup.old_null_paths = std::vector(rollup.num_txs * 2, fr_hash_path(128, random_pair));
+    rollup.new_null_paths = std::vector(rollup.num_txs * 2, fr_hash_path(128, random_pair));
 
     auto buf = to_buffer(rollup);
     auto result = from_buffer<rollup_tx>(buf);
 
     EXPECT_EQ(rollup, result);
-    */
 }
