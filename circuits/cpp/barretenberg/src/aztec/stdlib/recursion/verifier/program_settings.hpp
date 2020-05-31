3c21f2e92c4517516c7b53a0bd378e170f6043e7
zac-williamson <blorktronics@gmail.com>
2020-05-31 15:14:51 +0100

    fixed another typedef clash

--- circuits/cpp/barretenberg/src/aztec/stdlib/recursion/verifier/program_settings.hpp
+++ circuits/cpp/barretenberg/src/aztec/stdlib/recursion/verifier/program_settings.hpp
@@ -14,14 +14,14 @@ template <typename Curve> class recursive_turbo_verifier_settings : public waffl
     typedef typename Curve::fr_ct fr_ct;
     typedef typename Curve::g1_base_t::affine_element g1_base_t;
     typedef typename Curve::Composer Composer;
-    typedef Transcript<Composer> Transcript;
-    typedef waffle::VerifierPermutationWidget<fr_ct, g1_base_t, Transcript> PermutationWidget;
+    typedef Transcript<Composer> Transcript_pt;
+    typedef waffle::VerifierPermutationWidget<fr_ct, g1_base_t, Transcript_pt> PermutationWidget;
     typedef waffle::unrolled_turbo_settings base_settings;
 
-    typedef waffle::VerifierTurboFixedBaseWidget<fr_ct, g1_base_t, Transcript, base_settings> TurboFixedBaseWidget;
-    typedef waffle::VerifierTurboArithmeticWidget<fr_ct, g1_base_t, Transcript, base_settings> TurboArithmeticWidget;
-    typedef waffle::VerifierTurboRangeWidget<fr_ct, g1_base_t, Transcript, base_settings> TurboRangeWidget;
-    typedef waffle::VerifierTurboLogicWidget<fr_ct, g1_base_t, Transcript, base_settings> TurboLogicWidget;
+    typedef waffle::VerifierTurboFixedBaseWidget<fr_ct, g1_base_t, Transcript_pt, base_settings> TurboFixedBaseWidget;
+    typedef waffle::VerifierTurboArithmeticWidget<fr_ct, g1_base_t, Transcript_pt, base_settings> TurboArithmeticWidget;
+    typedef waffle::VerifierTurboRangeWidget<fr_ct, g1_base_t, Transcript_pt, base_settings> TurboRangeWidget;
+    typedef waffle::VerifierTurboLogicWidget<fr_ct, g1_base_t, Transcript_pt, base_settings> TurboLogicWidget;
 
     static constexpr size_t num_challenge_bytes = 16;
     static constexpr transcript::HashType hash_type = transcript::HashType::PedersenBlake2s;
@@ -29,7 +29,7 @@ template <typename Curve> class recursive_turbo_verifier_settings : public waffl
 
     static fr_ct append_scalar_multiplication_inputs(waffle::verification_key* key,
                                                      const fr_ct& alpha_base,
-                                                     const Transcript& transcript,
+                                                     const Transcript_pt& transcript,
                                                      std::map<std::string, fr_ct>& scalars)
     {
         auto updated_alpha = PermutationWidget::append_scalar_multiplication_inputs(
@@ -51,7 +51,7 @@ template <typename Curve> class recursive_turbo_verifier_settings : public waffl
 
     static fr_ct compute_quotient_evaluation_contribution(waffle::verification_key* key,
                                                           const fr_ct& alpha_base,
-                                                          const Transcript& transcript,
+                                                          const Transcript_pt& transcript,
                                                           fr_ct& t_eval)
     {
         auto updated_alpha_base = PermutationWidget::compute_quotient_evaluation_contribution(
