9fb95020b91a541c77836f88b229f622e25c9d28
zac-williamson <blorktronics@gmail.com>
2020-05-31 14:47:34 +0100

    removed typdef clash in packed_byte_array

--- circuits/cpp/barretenberg/src/aztec/stdlib/primitives/packed_byte_array/packed_byte_array.cpp
+++ circuits/cpp/barretenberg/src/aztec/stdlib/primitives/packed_byte_array/packed_byte_array.cpp
@@ -25,11 +25,11 @@ packed_byte_array<Composer>::packed_byte_array(Composer* parent_context, const s
     , num_bytes(n)
 {
     const size_t num_elements = num_bytes / BYTES_PER_ELEMENT + (num_bytes % BYTES_PER_ELEMENT != 0);
-    limbs = std::vector<field_t>(num_elements);
+    limbs = std::vector<field_pt>(num_elements);
 }
 
 template <typename Composer>
-packed_byte_array<Composer>::packed_byte_array(const std::vector<field_t>& input, const size_t bytes_per_input)
+packed_byte_array<Composer>::packed_byte_array(const std::vector<field_pt>& input, const size_t bytes_per_input)
     : context(get_context_from_fields(input))
     , num_bytes(bytes_per_input * input.size())
 {
@@ -38,12 +38,12 @@ packed_byte_array<Composer>::packed_byte_array(const std::vector<field_t>& input
 
     const size_t num_elements = num_bytes / BYTES_PER_ELEMENT + (num_bytes % BYTES_PER_ELEMENT != 0);
     for (size_t i = 0; i < num_elements; ++i) {
-        field_t limb(context, 0);
+        field_pt limb(context, 0);
 
         const size_t num_inputs = (i == num_elements - 1) ? (input.size() - (i * inputs_per_limb)) : inputs_per_limb;
         for (size_t j = 0; j < num_inputs; ++j) {
             const uint64_t limb_shift = (BYTES_PER_ELEMENT - ((j + 1) * bytes_per_input)) << 3;
-            limb += input[i * inputs_per_limb + j] * field_t(context, uint256_t(1) << limb_shift);
+            limb += input[i * inputs_per_limb + j] * field_pt(context, uint256_t(1) << limb_shift);
         }
         limbs.push_back(limb);
     }
@@ -85,10 +85,10 @@ packed_byte_array<Composer>::packed_byte_array(const byte_array& input)
     for (size_t i = 0; i < num_elements; ++i) {
         const size_t bytes_in_element = (i == num_elements - 1) ? num_bytes - i * BYTES_PER_ELEMENT : BYTES_PER_ELEMENT;
         const size_t bits_in_element = bytes_in_element * 8;
-        field_t limb(context, 0);
+        field_pt limb(context, 0);
         for (size_t j = 0; j < bits_in_element; ++j) {
             const uint64_t shift = static_cast<uint64_t>(bits_per_element - 1 - j);
-            limb += field_t(bits[i * bits_per_element + j]) * field_t(context, uint256_t(1) << shift);
+            limb += field_pt(bits[i * bits_per_element + j]) * field_pt(context, uint256_t(1) << shift);
         }
         limbs.push_back(limb);
     }
@@ -113,37 +113,39 @@ packed_byte_array<Composer>::packed_byte_array(packed_byte_array&& other)
     , limbs(other.limbs.begin(), other.limbs.end())
 {}
 
-template <typename Composer> packed_byte_array<Composer>& packed_byte_array<Composer>::operator=(const packed_byte_array& other)
+template <typename Composer>
+packed_byte_array<Composer>& packed_byte_array<Composer>::operator=(const packed_byte_array& other)
 {
     context = other.context;
     num_bytes = other.num_bytes;
-    limbs = std::vector<field_t>(other.limbs.begin(), other.limbs.end());
+    limbs = std::vector<field_pt>(other.limbs.begin(), other.limbs.end());
     return *this;
 }
 
-template <typename Composer> packed_byte_array<Composer>& packed_byte_array<Composer>::operator=(packed_byte_array&& other)
+template <typename Composer>
+packed_byte_array<Composer>& packed_byte_array<Composer>::operator=(packed_byte_array&& other)
 {
     context = other.context;
     num_bytes = other.num_bytes;
-    limbs = std::vector<field_t>(other.limbs.begin(), other.limbs.end());
+    limbs = std::vector<field_pt>(other.limbs.begin(), other.limbs.end());
     return *this;
 }
 
 template <typename Composer> packed_byte_array<Composer>::operator byte_array() const
 {
-    std::vector<bool_t> bits;
+    std::vector<bool_pt> bits;
     const size_t num_bits = num_bytes * 8;
 
     constexpr size_t bits_per_element = BYTES_PER_ELEMENT * 8;
     for (size_t i = 0; i < limbs.size(); ++i) {
         const size_t bits_in_limb = (i == limbs.size() - 1) ? num_bits - (i * bits_per_element) : bits_per_element;
-        field_t accumulator(context, 0);
+        field_pt accumulator(context, 0);
         uint256_t limb_value(limbs[i].get_value());
         for (size_t j = 0; j < bits_in_limb; ++j) {
             const uint64_t bit_shift = (bits_per_element - 1 - j);
             uint64_t bit_val = (limb_value >> bit_shift).data[0] & (uint64_t)(1);
-            bool_t bit(witness_t(context, fr(bit_val)));
-            accumulator += (field_t(bit) * field_t(context, uint256_t(1) << bit_shift));
+            bool_pt bit(witness_t(context, fr(bit_val)));
+            accumulator += (field_pt(bit) * field_pt(context, uint256_t(1) << bit_shift));
             bits.emplace_back(bit);
         }
         accumulator.assert_equal(limbs[i]);
@@ -151,7 +153,8 @@ template <typename Composer> packed_byte_array<Composer>::operator byte_array() 
     return byte_array(context, bits);
 }
 
-template <typename Composer> void packed_byte_array<Composer>::append(const field_t& to_append, const size_t bytes_to_append)
+template <typename Composer>
+void packed_byte_array<Composer>::append(const field_pt& to_append, const size_t bytes_to_append)
 {
     const size_t current_capacity = limbs.size() * BYTES_PER_ELEMENT;
     const size_t current_size = size();
@@ -174,17 +177,17 @@ template <typename Composer> void packed_byte_array<Composer>::append(const fiel
     const uint64_t next_padding = (BYTES_PER_ELEMENT - num_bytes_for_new_limb) << 3;
     bool is_constant = to_append.witness_index == UINT32_MAX;
 
-    field_t to_current;
-    to_current = is_constant ? field_t(context, append_current) : witness_t(context, append_current);
-    limbs[limbs.size() - 1] += (to_current * field_t(context, uint256_t(1) << current_padding));
+    field_pt to_current;
+    to_current = is_constant ? field_pt(context, append_current) : witness_t(context, append_current);
+    limbs[limbs.size() - 1] += (to_current * field_pt(context, uint256_t(1) << current_padding));
 
-    field_t reconstructed = to_current;
+    field_pt reconstructed = to_current;
     if (num_bytes_for_new_limb > 0) {
-        field_t to_add;
-        to_add = is_constant ? field_t(context, append_next) : witness_t(context, append_next);
-        limbs.emplace_back(to_add * field_t(context, next_padding));
+        field_pt to_add;
+        to_add = is_constant ? field_pt(context, append_next) : witness_t(context, append_next);
+        limbs.emplace_back(to_add * field_pt(context, next_padding));
 
-        reconstructed += to_add * field_t(context, uint256_t(1) << uint256_t(num_bytes_for_current_limb * 8));
+        reconstructed += to_add * field_pt(context, uint256_t(1) << uint256_t(num_bytes_for_current_limb * 8));
     }
 
     if (!is_constant) {
@@ -195,15 +198,16 @@ template <typename Composer> void packed_byte_array<Composer>::append(const fiel
 }
 
 template <typename Composer>
-std::vector<field_t<Composer>> packed_byte_array<Composer>::to_unverified_byte_slices(const size_t bytes_per_slice) const
+std::vector<field_t<Composer>> packed_byte_array<Composer>::to_unverified_byte_slices(
+    const size_t bytes_per_slice) const
 {
-    std::vector<field_t> slices;
+    std::vector<field_pt> slices;
     for (size_t i = 0; i < limbs.size(); ++i) {
         uint256_t limb_value(limbs[i].get_value());
         const size_t bytes_in_limb = (i == limbs.size() - 1) ? num_bytes - (i * BYTES_PER_ELEMENT) : BYTES_PER_ELEMENT;
         const size_t num_slices = (bytes_in_limb / bytes_per_slice) + (bytes_in_limb % bytes_per_slice != 0);
 
-        field_t accumulator(context, 0);
+        field_pt accumulator(context, 0);
         for (size_t j = 0; j < num_slices; ++j) {
             const size_t bytes_in_slice =
                 (j == num_slices - 1) ? bytes_in_limb - (j * bytes_per_slice) : bytes_per_slice;
@@ -215,9 +219,9 @@ std::vector<field_t<Composer>> packed_byte_array<Composer>::to_unverified_byte_s
             if (limbs[i].witness_index != UINT32_MAX) {
                 slices.push_back(witness_t(context, fr(slice)));
             } else {
-                slices.push_back(field_t(context, fr(slice)));
+                slices.push_back(field_pt(context, fr(slice)));
             }
-            accumulator += (slices.back() * field_t(context, uint256_t(1) << start));
+            accumulator += (slices.back() * field_pt(context, uint256_t(1) << start));
         }
 
         limbs[i].assert_equal(accumulator);
