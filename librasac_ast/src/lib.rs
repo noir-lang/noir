69b5772f3e8b1ef5d2a92194ab9219071abd5a64
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-08-17 14:26:38 +0100

    Initial commit for the AST

---
+++ librasac_ast/src/lib.rs
@@ -0,0 +1,25 @@
+/// This module contains two Ident structures, due to the fact that an identifier may or may not return a value
+/// statement::Ident does not return a value, while Expression::Ident does.
+mod expression;
+mod statement;
+
+pub use expression::*;
+pub use statement::*;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum Type {
+    FieldElement,
+    Constant,
+    Public,
+    Witness,
+    I8,
+    U8,
+    I16,
+    U16,
+    I32,
+    U32,
+    Bool,
+    Concrete(Ident, Vec<Type>),
+    Error,
+    Unknown, // XXX: Currently have not implemented structs, so this type is a stub
+}
