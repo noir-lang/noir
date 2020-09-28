153cc6f81e931d4abfdd754a4ae527ebcd1f84b7
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-09-28 22:26:00 +0100

    [parser]
    update mod file of prefix parser

--- libnoirc_parser/src/prefix_parser/mod.rs
+++ libnoirc_parser/src/prefix_parser/mod.rs
@@ -1,3 +1,4 @@
+mod array;
 mod declaration;
 mod function;
 mod group;
@@ -5,22 +6,27 @@ mod r#if;
 mod literal;
 mod name;
 mod unary;
+mod r#use;
 
+pub use array::ArrayParser;
 pub use declaration::DeclarationParser;
 pub use function::FuncParser;
 pub use group::GroupParser;
 pub use literal::LiteralParser;
 pub use name::NameParser;
 pub use r#if::IfParser;
+pub use r#use::UseParser;
 pub use unary::UnaryParser;
 
 /// This file defines all Prefix parser ie it defines how we parser statements which begin with a specific token or token type
 use libnoirc_ast::{
-    BlockStatement, Expression, FunctionDefinition, FunctionLiteral, Ident, IfExpression, Literal,
-    PrefixExpression, Type,
+    ArrayLiteral, BlockStatement, Expression, FunctionDefinition, FunctionLiteral, Ident,
+    IfExpression, Literal, PrefixExpression, Type,
 };
 use libnoirc_lexer::token::{Keyword, Token, TokenKind};
 
 use crate::{Parser, Precedence, PrefixParser};
 
-use libnoirc_ast::{ConstStatement, LetStatement, PrivateStatement, PublicStatement, Statement};
+use libnoirc_ast::{
+    ConstStatement, ImportStatement, LetStatement, PrivateStatement, PublicStatement, Statement,
+};
