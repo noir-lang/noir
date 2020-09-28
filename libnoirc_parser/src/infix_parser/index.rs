b6e8c2e3ddc3a2a2bbe1c7cf9fb2223cea8ff44c
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-09-28 22:28:29 +0100

    [parser]
    - Add Path and Index Parsers

---
+++ libnoirc_parser/src/infix_parser/index.rs
@@ -0,0 +1,31 @@
+use super::*;
+
+pub struct IndexParser;
+
+impl InfixParser for IndexParser {
+    fn parse(parser: &mut Parser, collection_name: Expression) -> Expression {
+        let collection_name_string = match collection_name {
+            Expression::Ident(x) => x,
+            _ => unimplemented!("collection name expression should only be an identifier"),
+        };
+
+        // Current token is now the left bracket that sits between the name of the collection
+        // and the index. Since we can have constants be indices, we will just parse an expression
+        // and not look for an integer
+        let curr_precedence = Precedence::from(&parser.curr_token);
+        parser.advance_tokens();
+        let index = parser.parse_expression(curr_precedence).unwrap();
+
+        // Skip the ']'
+        if !parser.peek_check_variant_advance(Token::RightBracket) {
+            panic!("Expected a Right bracket to end the index operator")
+        }
+
+        let index_expr = IndexExpression {
+            collection_name: collection_name_string.into(),
+            index: index,
+        };
+
+        Expression::Index(Box::new(index_expr))
+    }
+}
---
+++ libnoirc_parser/src/infix_parser/path.rs
@@ -0,0 +1,47 @@
+use super::*;
+use crate::prefix_parser::NameParser;
+use crate::PrefixParser;
+use libnoirc_ast::Ident;
+
+pub struct PathParser;
+
+impl InfixParser for PathParser {
+    // XXX: Currently we only support importing for functions
+    // We will introduce importing for constants later.
+    // Ultimately, paths will be used to call static methods on a struct, external funcs and external global constants
+    fn parse(parser: &mut Parser, root_module: Expression) -> Expression {
+        // Current token is now the `::`. We need to advance and peek to see if there is another `::`
+        parser.advance_tokens();
+
+        // Current token should now be an identifier. Lets peek at the next token to check if the path is finished
+        let mut parsed_path = vec![root_module];
+        while parser.peek_token == Token::DoubleColon {
+            let name = NameParser::parse(parser);
+            parsed_path.push(name);
+            parser.advance_tokens();
+        }
+
+        // Current token will be a `::` at this point. Lets advance the parser and parse the method
+        // XXX: We parse a general expression, and have the analyser restrict it to be a method. In the future, we will accept global constants along with methods.
+        parser.advance_tokens();
+        let method = parser.parse_expression(Precedence::Lowest).unwrap();
+
+        // Convert path into strings
+        let mut path_idents = Vec::new();
+        for path in parsed_path.into_iter() {
+            let path_ident = match path {
+                Expression::Ident(x) => Ident(x),
+                _ => unimplemented!("name space should only be an identifier"),
+            };
+            path_idents.push(path_ident);
+        }
+
+        // By default CallParser with set the path as Current
+        // We extract the Call expression and set the path correctly here
+        let call_expr = match method {
+            Expression::Call(_, call_expr) => call_expr,
+            _ => unimplemented!("Currently you can only access external functions"),
+        };
+        Expression::Call(path_idents.into(), call_expr)
+    }
+}
