bd60c48e8d631cb1ce96d4e298f92152f4fdae81
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-10-13 18:28:37 +0100

    [ACIR]
    Create a new module named native types and put the types that are native to ACIR in here. These are types that we can directly optimise with no help

---
+++ acir/src/native_types/mod.rs
@@ -0,0 +1,7 @@
+mod linear;
+mod arithmetic;
+mod witness;
+
+pub use witness::Witness;
+pub use linear::Linear;
+pub use arithmetic::Arithmetic;
---
+++ acir/src/native_types/witness.rs
@@ -0,0 +1,34 @@
+
+// Each witness can be identified with an String
+// The string will be derived from the identifier name in the variable
+
+// Lets make this only usize, and have a map in the compiler to map Witness string to usize
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd)]
+pub struct Witness(pub String, pub usize);
+
+use std::cmp::Ordering;
+impl Ord for Witness {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.1.cmp(&other.1)
+    }
+}
+
+impl Default for Witness {
+    // Place holder value
+    fn default() -> Witness {
+        Witness("zero".to_string(), 0)
+    }
+}
+
+impl Witness {
+    pub fn new(variable_name: String, witness_index: usize) -> Witness {
+        Witness(variable_name, witness_index)
+    }
+    pub fn witness_index(&self) -> usize {
+        self.1
+    }
+    pub fn variable_name(&self) -> &str {
+        &self.0
+    }
+}
