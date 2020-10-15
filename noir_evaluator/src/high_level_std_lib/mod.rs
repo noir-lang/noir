f7a8047926249b05831456143afc9705e24fce5d
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-10-15 13:22:48 +0100

    [Evaluator]
    - Remove high level std module. This is no longer necessary as the low level library can be implemented in the language itself and so we do not need separate logic to handle it

--- noir_evaluator/src/high_level_std_lib/mod.rs
+++
@@ -1,9 +0,0 @@
-// This will contain rasa modules.
-
-// The std library is made up of low level and high level.
-
-// Low level refers to them being available in the underlying plonk proving system, like sha256
-
-// High level means that they have been made in the rasa language as modules
-
-// XXX: Will create another module, which pulls in both low level and high level and we expose that
