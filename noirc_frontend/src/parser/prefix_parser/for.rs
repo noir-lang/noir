745c6485f253eed47db4efbd23d15dbd2436110b
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-10-23 18:14:54 +0100

    [Parser]
    - Add parser for for expressions

---
+++ noirc_frontend/src/parser/prefix_parser/for.rs
@@ -0,0 +1,44 @@
+use super::*;
+
+pub struct ForParser;
+
+impl PrefixParser for ForParser{
+    fn parse(parser: &mut Parser) -> Expression { 
+        if !parser.peek_check_kind_advance(TokenKind::Ident) {
+            panic!("Expected an identifier after the For keyword")
+        }   
+
+        let identifier = parser.curr_token.token().to_string();
+
+        if !parser.peek_check_variant_advance(&Token::Keyword(Keyword::In)) {
+            panic!("Expected the keyword `In` after the Identifier {}", &identifier)
+        }
+        parser.advance_tokens(); // in
+        
+        // Parse range
+        let start_range = parser.parse_expression(Precedence::Lowest).unwrap();
+        if !parser.peek_check_variant_advance(&Token::DoubleDot) {
+            panic!("Expected a `..` after the start range in for loop ")
+        }
+        parser.advance_tokens(); // ..
+        let end_range = parser.parse_expression(Precedence::Lowest).unwrap();
+        
+        if !parser.peek_check_variant_advance(&Token::LeftBrace) {
+            panic!("Expected a Left Brace after the range statement")
+        };
+
+        // Parse body
+        let block = parser.parse_block_statement();
+
+        let for_expr = ForExpression {
+            identifier: identifier.into(),
+            start_range,
+            end_range,
+            block,
+        };
+
+        Expression::For(Box::new(for_expr))
+
+    }
+}
+
--- noirc_frontend/src/parser/prefix_parser/mod.rs
+++ noirc_frontend/src/parser/prefix_parser/mod.rs
@@ -8,6 +8,7 @@ mod name;
 mod unary;
 mod r#use;
 mod module;
+mod r#for;
 
 pub use array::ArrayParser;
 pub use declaration::DeclarationParser;
@@ -17,13 +18,14 @@ pub use literal::LiteralParser;
 pub use name::NameParser;
 pub use r#if::IfParser;
 pub use r#use::UseParser;
+pub use r#for::ForParser;
 pub use unary::UnaryParser;
 pub use module::ModuleParser;
 
 /// This file defines all Prefix parser ie it defines how we parser statements which begin with a specific token or token type
 use crate::ast::{
     ArrayLiteral, BlockStatement, Expression, FunctionDefinition, Ident,
-    IfExpression, Literal, PrefixExpression, Type,
+    IfStatement, ForExpression, Literal, PrefixExpression, Type,
 };
 use crate::token::{Keyword, Token, TokenKind};
 
