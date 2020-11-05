8941a63d4a0cfbe864621dcbe9a27481a686f095
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-11-05 19:10:15 +0000

    [Evaluator]
    - Modify builtin methods to use Evaluator error

--- noir_evaluator/src/builtin/arrayprod.rs
+++ noir_evaluator/src/builtin/arrayprod.rs
@@ -1,9 +1,9 @@
-use super::BuiltInCaller;
+use super::{BuiltInCaller, EvaluatorError};
 use crate::object::{Array, Object};
 use crate::binary_op;
 use crate::{CallExpression, Environment, Evaluator};
 
-/// Sums all of the elements in an array
+/// Takes the direct product of the elements in an array
 pub struct ArrayProd;
 
 impl BuiltInCaller for ArrayProd {
@@ -11,20 +11,21 @@ impl BuiltInCaller for ArrayProd {
         evaluator: &mut Evaluator,
         env: &mut Environment,
         mut call_expr: CallExpression,
-    ) -> Object {
+    ) -> Result<Object, EvaluatorError> {
 
-        assert_eq!(call_expr.arguments.len(),1);
-        let arr_expr = call_expr.arguments.pop().unwrap();
-        let arr = match Array::from_expression(evaluator, env, arr_expr) {
-            Some(arr) => arr,
-            None => panic!("ArrayProd should only take a single parameter, which is an array. This should have been caught by the compiler in the analysis phase")
+        let arr_expr = {
+            assert_eq!(call_expr.arguments.len(),1);
+            call_expr.arguments.pop().unwrap()
         };
 
-        let mut result = arr.get(0);
+        // ArrayProd should only take a single parameter, which is an array. This should have been caught by the compiler in the analysis phase
+        let arr = Array::from_expression(evaluator, env, arr_expr)?;
+
+        let mut result = arr.get(0)?;
         for i in 1..arr.contents.len(){
-            result = binary_op::handle_mul_op(result, arr.get(i as u128),env, evaluator);
+            result = binary_op::handle_mul_op(result, arr.get(i as u128)?,env, evaluator)?;
         }
 
-        result
+        Ok(result)
     }
 }
--- noir_evaluator/src/builtin/arraysum.rs
+++ noir_evaluator/src/builtin/arraysum.rs
@@ -1,7 +1,7 @@
 use super::BuiltInCaller;
 use crate::object::{Array, Object};
 use crate::binary_op;
-use crate::{CallExpression, Environment, Evaluator};
+use crate::{CallExpression, Environment, Evaluator, EvaluatorError};
 
 /// Sums all of the elements in an array
 pub struct ArraySum;
@@ -12,20 +12,21 @@ impl BuiltInCaller for ArraySum {
         evaluator: &mut Evaluator,
         env: &mut Environment,
         mut call_expr: CallExpression,
-    ) -> Object {
+    ) -> Result<Object, EvaluatorError> {
 
-        assert_eq!(call_expr.arguments.len(),1);
-        let arr_expr = call_expr.arguments.pop().unwrap();
-        let arr = match Array::from_expression(evaluator, env, arr_expr) {
-            Some(arr) => arr,
-            None => panic!("ArraySum should only take a single parameter, which is an array. This should have been caught by the compiler in the analysis phase")
+        let arr_expr = {
+            assert_eq!(call_expr.arguments.len(),1);
+            call_expr.arguments.pop().unwrap()
         };
 
-        let mut result = arr.get(0);
+        // ArraySum should only take a single parameter, which is an array. This should have been caught by the compiler in the analysis phase
+        let arr = Array::from_expression(evaluator, env, arr_expr)?;
+
+        let mut result = arr.get(0)?;
         for i in 1..arr.contents.len(){
-            result = binary_op::handle_add_op(result, arr.get(i as u128),env, evaluator);
+            result = binary_op::handle_add_op(result, arr.get(i as u128)?,env, evaluator)?;
         }
 
-        result
+        Ok(result)
     }
 }
