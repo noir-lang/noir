dfdec42008ff717e8a45c2a0abbfd635a1bb26a1
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-12-14 17:14:07 +0000

    [Parser]
    `mod` keyword is now used to declare modules and no longer define them inline

--- noirc_frontend/src/parser/prefix_parser/module.rs
+++ noirc_frontend/src/parser/prefix_parser/module.rs
@@ -3,20 +3,18 @@ use super::*;
 pub struct ModuleParser;
 
 impl ModuleParser {
-    pub(crate) fn parse_module_definition(parser: &mut Parser) -> Result<(String, Program), ParserError> {
+    pub(crate) fn parse_module_decl(parser: &mut Parser) -> Result<String, ParserError> {
+        // Currently on the mod keyword
+        //
         parser.peek_check_kind_advance(TokenKind::Ident)?;
-
+        
         let module_identifier = match parser.curr_token.token() {
             Token::Ident(x) => x.to_string(),
             _=> unreachable!()
         };
+        
+        parser.peek_check_variant_advance(&Token::Semicolon)?;
 
-        parser.peek_check_variant_advance(&Token::LeftBrace)?;
-        // Advance past the Left brace
-        parser.advance_tokens();
-
-        let module = parser.parse_module();
-
-        Ok((module_identifier, module))
+        Ok(module_identifier)
     }
 }
