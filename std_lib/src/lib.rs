e848d6f1826929aed154fc23468e63a2d833f3dd
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-01-15 21:25:03 +0000

    stdlib
    - remove constant statement. instead if importing the `Rust` constant. We now copy the std_lib into a known directory on the file system and the compiler gets the files from there. We do this by using `build.rs`

--- std_lib/src/lib.rs
+++ std_lib/src/lib.rs
@@ -1,4 +1,3 @@
-pub const LIB_NOIR : &'static str = include_str!("lib.nr");
 
 // This lib.rs file is here so that we can leverage the Rust build system
 // The contents of src except for Rust files will be copied to a config directory.
