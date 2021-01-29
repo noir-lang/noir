be4a60f1bcd5c01e4958727f8501104100d72ac6
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-01-29 13:35:39 +0000

    pwg
    Add tests for merkle to ensure consistency with aztec

--- pwg/src/merkle.rs
+++ pwg/src/merkle.rs
@@ -2,6 +2,7 @@ use noir_field::FieldElement;
 
 
 pub type HashPath = Vec<(FieldElement, FieldElement)>;
+
 struct MerkleTree{
     depth : u32,
     total_size : u32,
@@ -67,7 +68,7 @@ impl MerkleTree {
 
         let mut offset = 0usize;
         let mut layer_size = self.total_size;
-        for i in 0..self.depth {
+        for _ in 0..self.depth {
             self.hashes[offset + index] = current;
             index &= (!0) - 1;
             current = compress_native(self.hashes[offset + index], self.hashes[offset + index + 1]);
@@ -80,15 +81,86 @@ impl MerkleTree {
 
         self.root
     }
+    /// Gets a message at `index`. This is not the leaf
     pub fn get_message_at_index(&self,index :usize ) -> Vec<u8> {
         self.pre_images[index].clone()
     }
 }
 
 fn hash(message : &[u8]) -> FieldElement {
-    todo!()
+    aztec_backend::barretenberg_rs::blake2s::hash_to_field(message)
 }
 
+// This is what the blake2s hash to function should look like
+// if it was all in  Rust. This function panics because `from_bytes` 
+// requires that the input is canonical and reduced.
+// blake2s will produce output that is 256 bits while 
+// bn254 is 254 bits
+// fn hash(message : &[u8]) -> FieldElement {
+    // let mut hasher = Blake2s::new();
+    // hasher.update(message);
+    // let res = hasher.finalize();
+    // FieldElement::from_bytes(&res[..])
+// }
+
+// XXX(FIXME) : Currently, this is very aztec specific, because this PWG does not have
+// a way to deal with generic ECC operations
 fn compress_native(left : FieldElement, right : FieldElement) -> FieldElement {
-    todo!()
+    aztec_backend::barretenberg_rs::pedersen::compress_native(left, right)
+}
+
+#[test]
+fn basic_interop_initial_root() {
+    // Test that the initial root is computed correctly
+    let tree = MerkleTree::new(3);
+    // Copied from barretenberg by copying the stdout from MemoryTree
+    let expected_hex = "0620374242254671503abf57d13969d41bbae97e59fa97cd7777cd683beb9eb8";
+    assert_eq!(tree.root.to_hex(), expected_hex)
+}
+#[test]
+fn basic_interop_hashpath() {
+    // Test that the hashpath is correct
+    let tree = MerkleTree::new(3);
+
+    let path = tree.get_hash_path(0);
+
+    let expected_hash_path = vec![
+        ("1cdcf02431ba623767fe389337d011df1048dcc24b98ed81cec97627bab454a0","1cdcf02431ba623767fe389337d011df1048dcc24b98ed81cec97627bab454a0" ),
+        ("262e1ae3710241581182198b69a10601148ee1dd20ae638f99cde7b3ede59754","262e1ae3710241581182198b69a10601148ee1dd20ae638f99cde7b3ede59754" ),
+        ("0f88aa985f23258a12a78b35eab5d3d8d41d091a71113f2d4b2731d96ab78cfd","0f88aa985f23258a12a78b35eab5d3d8d41d091a71113f2d4b2731d96ab78cfd" ),
+    ];
+
+    for (got, expected_segment) in path.into_iter().zip(expected_hash_path) {
+        assert_eq!(got.0.to_hex().as_str(), expected_segment.0);
+        assert_eq!(got.1.to_hex().as_str(), expected_segment.1)
+    }
+}
+#[test]
+fn basic_interop_update() {
+    // Test that the hashpath is correct
+    let mut tree = MerkleTree::new(3);
+
+    tree.update_element(0, vec![0;64]);
+    tree.update_element(1, vec![1;64]);
+    tree.update_element(2, vec![2;64]);
+    tree.update_element(3, vec![3;64]);
+    tree.update_element(4, vec![4;64]);
+    tree.update_element(5, vec![5;64]);
+    tree.update_element(6, vec![6;64]);
+    let root = tree.update_element(7, vec![7;64]);
+
+    assert_eq!("241fc8d893854e78dd2d427e534357fe02279f209193f0f82e13a3fd4e15375e", root.to_hex());
+
+    let path = tree.get_hash_path(2);
+
+    let expected_hash_path = vec![
+        ("06c2335d6f7acb84bbc7d0892cefebb7ca31169a89024f24814d5785e0d05324","12dc36b01cbd8a6248b04e08f0ec91aa6d11a91f030b4a7b1460281859942185"),
+        ("2a57882283ba48c2e523fbf8142c0867e82cfaba2410793d3331a9c685f40790","145beb7dcd00d8eee922af3fee2b002c6e56b716630752b787acfbe685769040"),
+        ("20a7f69fa7eada3e900b803301074386ef2ea8be29f3aa943eefc3654c0a94e6","073b84f35922842dcf4c10596c3fe3eab7e61939120d2aca0a531e4e6fdce22b"),
+    ];
+
+    for (got, expected_segment) in path.into_iter().zip(expected_hash_path) {
+        assert_eq!(got.0.to_hex().as_str(), expected_segment.0);
+        assert_eq!(got.1.to_hex().as_str(), expected_segment.1)
+    }
 }
