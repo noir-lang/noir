a27e644667b0dbc9840eb7653f3646e61bb68793
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-02-04 20:29:50 +0000

    aztec backend
    - Add tests for Schnorr and Pedersen
    - Make Assignment pub(crate) visibility, and derive Debug

--- aztec_backend/src/barretenberg_rs/composer.rs
+++ aztec_backend/src/barretenberg_rs/composer.rs
@@ -26,8 +26,8 @@ impl StandardComposer {
     }
 }
 
-#[derive(Clone)]
-pub struct Assignments(Vec<Scalar>);
+#[derive(Debug, Clone)]
+pub struct Assignments(pub(crate) Vec<Scalar>);
 pub type WitnessAssignments = Assignments;
 
 impl Assignments {
@@ -105,8 +105,7 @@ impl RangeConstraint {
 #[derive(Clone, Hash, Debug)]
 pub struct SchnorrConstraint {
     pub message: Vec<i32>,
-    pub signature_s: [u8; 32],
-    pub signature_e: [u8; 32],
+    pub signature: [i32; 64],
     pub public_key_x: i32,
     pub public_key_y: i32,
     pub result: i32,
@@ -122,15 +121,9 @@ impl SchnorrConstraint {
             buffer.extend_from_slice(&constraint.to_be_bytes());
         }
 
-        let sig_s_len = (self.signature_s.len()) as u32;
-        buffer.extend_from_slice(&sig_s_len.to_be_bytes());
-        for sig_byte in self.signature_s.iter() {
-            buffer.extend_from_slice(&sig_byte.to_be_bytes());
-        }
-
-        let sig_e_len = (self.signature_e.len()) as u32;
-        buffer.extend_from_slice(&sig_e_len.to_be_bytes());
-        for sig_byte in self.signature_e.iter() {
+        let sig_len = (self.signature.len()) as u32;
+        buffer.extend_from_slice(&sig_len.to_be_bytes());
+        for sig_byte in self.signature.iter() {
             buffer.extend_from_slice(&sig_byte.to_be_bytes());
         }
 
@@ -217,6 +210,51 @@ impl Sha256Constraint {
         buffer
     }
 }
+#[derive(Clone, Hash, Debug)]
+pub struct Blake2sConstraint {
+    pub inputs: Vec<(i32, i32)>,
+    pub result_low_128: i32,
+    pub result_high_128: i32,
+}
+
+impl Blake2sConstraint {
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut buffer = Vec::new();
+
+        let inputs_len = self.inputs.len() as u32;
+        buffer.extend_from_slice(&inputs_len.to_be_bytes());
+        for constraint in self.inputs.iter() {
+            buffer.extend_from_slice(&constraint.0.to_be_bytes());
+            buffer.extend_from_slice(&constraint.1.to_be_bytes());
+        }
+
+        buffer.extend_from_slice(&self.result_low_128.to_be_bytes());
+        buffer.extend_from_slice(&self.result_high_128.to_be_bytes());
+
+        buffer
+    }
+}
+#[derive(Clone, Hash, Debug)]
+pub struct PedersenConstraint {
+    pub inputs: Vec<i32>,
+    pub result: i32,
+}
+
+impl PedersenConstraint {
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut buffer = Vec::new();
+
+        let inputs_len = self.inputs.len() as u32;
+        buffer.extend_from_slice(&inputs_len.to_be_bytes());
+        for constraint in self.inputs.iter() {
+            buffer.extend_from_slice(&constraint.to_be_bytes());
+        }
+
+        buffer.extend_from_slice(&self.result.to_be_bytes());
+
+        buffer
+    }
+}
 
 #[derive(Clone, Hash, Debug)]
 pub struct LogicConstraint {
@@ -271,6 +309,8 @@ pub struct ConstraintSystem {
     pub merkle_membership_constraints: Vec<MerkleMembershipConstraint>,
     pub merkle_root_constraints: Vec<MerkleRootConstraint>,
     pub schnorr_constraints: Vec<SchnorrConstraint>,
+    pub blake2s_constraints: Vec<Blake2sConstraint>,
+    pub pedersen_constraints: Vec<PedersenConstraint>,
     pub constraints: Vec<Constraint>,
 }
 
@@ -321,6 +361,20 @@ impl ConstraintSystem {
         let schnorr_len = self.schnorr_constraints.len() as u32;
         buffer.extend_from_slice(&schnorr_len.to_be_bytes());
         for constraint in self.schnorr_constraints.iter() {
+            buffer.extend(&constraint.to_bytes());
+        }
+
+        // Serialise each Blake2s constraint
+        let blake2s_len = self.blake2s_constraints.len() as u32;
+        buffer.extend_from_slice(&blake2s_len.to_be_bytes());
+        for constraint in self.blake2s_constraints.iter() {
+            buffer.extend(&constraint.to_bytes());
+        }
+
+        // Serialise each Pedersen constraint
+        let pedersen_len = self.pedersen_constraints.len() as u32;
+        buffer.extend_from_slice(&pedersen_len.to_be_bytes());
+        for constraint in self.pedersen_constraints.iter() {
             buffer.extend(&constraint.to_bytes());
         }
         
@@ -395,7 +449,6 @@ impl StandardComposer {
         witness: WitnessAssignments,
     ) -> Vec<u8> {    
         use core::convert::TryInto;
-
 
         let cs_buf = constraint_system.to_bytes();
         let cs_ptr = self.barretenberg.allocate(&cs_buf);
@@ -516,6 +569,8 @@ mod test {
             merkle_membership_constraints: vec![],
             merkle_root_constraints : vec![],
             schnorr_constraints : vec![],
+            blake2s_constraints : vec![],
+            pedersen_constraints : vec![],
             constraints: vec![constraint.clone()],
         };
 
@@ -583,6 +638,8 @@ mod test {
             merkle_membership_constraints: vec![],
             merkle_root_constraints : vec![],
             schnorr_constraints : vec![],
+            blake2s_constraints : vec![],
+            pedersen_constraints : vec![],
             constraints: vec![constraint],
         };
 
@@ -635,6 +692,8 @@ mod test {
             merkle_membership_constraints: vec![],
             merkle_root_constraints : vec![],
             schnorr_constraints : vec![],
+            blake2s_constraints : vec![],
+            pedersen_constraints : vec![],
             constraints: vec![constraint, constraint2],
         };
 
@@ -650,6 +709,104 @@ mod test {
         };
 
         test_circuit(&constraint_system, vec![case_1, case_2]);
+    }
+    #[test]
+    fn test_schnorr_constraints() {
+
+        let mut signature_indices = [0i32;64];
+        for i in 13..(13+64) {
+            signature_indices[i-13] = i as i32;
+        }
+        let result_indice = signature_indices.last().unwrap() + 1;
+
+        let constraint = SchnorrConstraint{
+            message: vec![1,2,3,4,5,6,7,8,9,10],
+            public_key_x: 11,
+            public_key_y: 12,
+            signature: signature_indices,
+            result: result_indice,
+        };
+  
+        let constraint_system = ConstraintSystem {
+            var_num: 80,
+            pub_var_num: 0,
+            logic_constraints: vec![],
+            range_constraints: vec![],
+            sha256_constraints: vec![],
+            merkle_membership_constraints: vec![],
+            merkle_root_constraints : vec![],
+            schnorr_constraints : vec![constraint],
+            blake2s_constraints : vec![],
+            pedersen_constraints : vec![],
+            constraints: vec![],
+        };
+
+        let pub_x = Scalar::from_hex("0x17cbd3ed3151ccfd170efe1d54280a6a4822640bf5c369908ad74ea21518a9c5").unwrap();
+        let pub_y = Scalar::from_hex("0x0e0456e3795c1a31f20035b741cd6158929eeccd320d299cfcac962865a6bc74").unwrap();
+        
+        let sig : [i128; 64]= [
+            7, 131, 147, 205, 145, 77, 60, 169, 159, 86, 91, 209, 140, 210, 4, 21, 186, 39, 221, 195,
+            62, 35, 220, 144, 135, 28, 201, 97, 145, 125, 146, 211, 92, 16, 67, 59, 162, 133, 144, 52,
+            184, 137, 241, 102, 176, 152, 138, 220, 21, 40, 211, 178, 191, 67, 71, 11, 209, 191, 86,
+            91, 196, 68, 98, 214,
+        ];
+        let mut sig_as_scalars = [Scalar::zero(); 64];
+        for i in 0..64 {
+            sig_as_scalars[i] = sig[i].into()
+        }
+        let message : Vec<Scalar> = vec![0.into(),1.into(),2.into(),3.into(),4.into(),5.into(),6.into(),7.into(), 8.into(), 9.into()];
+        let mut witness_values = Vec::new();
+        witness_values.extend(message);
+        witness_values.push(pub_x);
+        witness_values.push(pub_y);
+        witness_values.extend(&sig_as_scalars);
+        witness_values.push(Scalar::zero());
+
+        let case_1 = WitnessResult {
+            witness: Assignments(witness_values),
+            public_inputs: None,
+            result: true,
+        };
+
+        test_circuit(&constraint_system, vec![case_1]);
+    }
+    #[test]
+    fn test_ped_constraints() {
+
+        let constraint = PedersenConstraint {
+            inputs: vec![1,2],
+            result: 3,
+        };
+
+        let constraint_system = ConstraintSystem {
+            var_num: 80,
+            pub_var_num: 0,
+            logic_constraints: vec![],
+            range_constraints: vec![],
+            sha256_constraints: vec![],
+            merkle_membership_constraints: vec![],
+            merkle_root_constraints : vec![],
+            schnorr_constraints : vec![],
+            blake2s_constraints : vec![],
+            pedersen_constraints : vec![constraint],
+            constraints: vec![],
+        };
+
+        let scalar_0 = Scalar::from_hex("0x00").unwrap();
+        let scalar_1 = Scalar::from_hex("0x01").unwrap();
+    
+        let mut witness_values = Vec::new();
+        witness_values.push(scalar_0);
+        witness_values.push(scalar_1);
+        witness_values.push(Scalar::zero());
+    
+        let case_1 = WitnessResult {
+            witness: Assignments(witness_values),
+            public_inputs: None,
+            result: true,
+        };
+
+        test_circuit(&constraint_system, vec![case_1]);
     }
 
     #[derive(Clone)]
