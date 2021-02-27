4ea8c032946e823c72cbc9e167dc6dabb32766cf
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-02-27 20:10:59 +0000

    noirc_driver
    - Add ability to easily test for regressions and integrations.

---
+++ noirc_driver/tests/tests.rs
@@ -0,0 +1,27 @@
+use noirc_driver::Driver;
+use std::path::PathBuf;
+
+#[test]
+fn fail() {
+    let mut fail_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    fail_dir.push("tests/fail/");
+
+    let paths = std::fs::read_dir(fail_dir).unwrap();
+
+    for path in paths {
+        let path = path.unwrap().path();
+        assert!(!Driver::file_compiles(&path), "path: {}", path.display())
+    }
+}
+#[test]
+fn pass() {
+    let mut pass_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    pass_dir.push("tests/pass/");
+
+    let paths = std::fs::read_dir(pass_dir).unwrap();
+
+    for path in paths {
+        let path = path.unwrap().path();
+        assert!(Driver::file_compiles(&path), "path: {}", path.display())
+    }
+}
