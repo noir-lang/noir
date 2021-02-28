882288c32d9c932bbdf31e5aca24f002d0439d70
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-02-28 20:05:32 +0000

    Evaluator/Environment
    Add method to lookup the variable name via the witness index

--- noir_evaluator/src/environment.rs
+++ noir_evaluator/src/environment.rs
@@ -1,5 +1,6 @@
 use super::errors::RuntimeErrorKind;
 use super::object::{Array, Object};
+use acir::native_types::Witness;
 use noirc_frontend::hir::scope::{
     ScopeForest as GenericScopeForest, ScopeTree as GenericScopeTree,
 };
@@ -36,8 +37,31 @@ impl Environment {
 
     pub fn get(&mut self, name: &String) -> Object {
         let scope = self.0.current_scope_tree();
-        scope.find_key(name).unwrap().clone()
+        scope.find(name).unwrap().clone()
     }
+
+    // This method is somewhat of a hack, due to the fact that we do not map
+    // Witness indices to variable names.
+    pub fn find_with_value(&mut self, val: &Witness) -> Option<String> {
+        let mut found = None;
+        for scope in self.0.current_scope_tree().0.iter().rev() {
+            found = scope.0.iter().find_map(|(k, v)| match v {
+                Object::Null | Object::Array(_) | Object::Constants(_) | Object::Arithmetic(_) => {
+                    None
+                }
+                Object::Integer(x) => {
+                    // Integers are assumed to always be unit
+                    (&x.witness == val).then(|| k)
+                }
+                Object::Linear(x) => (x.is_unit() && &x.witness == val).then(|| k),
+            });
+            if found.is_some() {
+                break;
+            }
+        }
+        found.cloned()
+    }
+
     pub fn get_array(&mut self, name: &String) -> Result<Array, RuntimeErrorKind> {
         let poly = self.get(name);
 
