ff26531f183ceb59b6fa90be73cd6384b1376f42
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-02-28 14:49:43 +0000

    Parser - Errors
    - Remove unused error variants; (InfixFunc, InternalError)
    
    - Add error variant for when the path consists of just a keyword
    
    - Remove the need for the parser to track the file_id. The caller should have this on hand.

--- noirc_frontend/src/parser/errors.rs
+++ noirc_frontend/src/parser/errors.rs
@@ -1,5 +1,5 @@
-use crate::lexer::errors::LexerError;
 use crate::lexer::token::{SpannedToken, Token, TokenKind};
+use crate::{lexer::errors::LexerError, PathKind};
 
 use noirc_errors::CustomDiagnostic as Diagnostic;
 use noirc_errors::DiagnosableError;
@@ -12,8 +12,6 @@ pub enum ParserErrorKind {
     LexerError(LexerError),
     #[error(" expected expression, found `{}`", lexeme)]
     ExpectedExpression { span: Span, lexeme: String },
-    #[error(" `{:?}` cannot be used as a binary operator.", lexeme)]
-    NoInfixFunction { span: Span, lexeme: String },
     #[error("Unexpected token found")]
     UnexpectedToken {
         span: Span,
@@ -26,60 +24,75 @@ pub enum ParserErrorKind {
         expected: TokenKind,
         found: TokenKind,
     },
+    #[error("Paths with a single segment, cannot have the single segment be a keyword")]
+    SingleKeywordSegmentNotAllowed { span: Span, path_kind: PathKind },
     #[error("Unstructured Error")]
     UnstructuredError { span: Span, message: String },
     #[error("Token is not a unary operation")]
     TokenNotUnaryOp { spanned_token: SpannedToken },
     #[error("Token is not a binary operation")]
     TokenNotBinaryOp { spanned_token: SpannedToken },
-    #[error("Internal Compiler Error, unrecoverable")]
-    // Actually lets separate these two types of errors
-    InternalError { message: String, span: Span },
 }
 
-impl ParserErrorKind {
-    pub fn into_err(self, file_id: usize) -> ParserError {
-        ParserError {
-            kind: self,
-            file_id,
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct ParserError {
-    pub(crate) kind: ParserErrorKind,
-    file_id: usize,
-}
-
-impl DiagnosableError for ParserError {
+impl DiagnosableError for ParserErrorKind {
     fn to_diagnostic(&self) -> Diagnostic {
-        match &self.kind {
+        match self {
             ParserErrorKind::LexerError(lex_err) => lex_err.to_diagnostic(),
-            ParserErrorKind::InternalError{message, span} => unreachable!("Internal Error. This is a bug in the compiler. Please report the following message :\n {} \n with the following span {:?}", message,span),
-            ParserErrorKind::ExpectedExpression{span, lexeme} => {
-                let mut diag = Diagnostic::simple_error(format!("Unexpected start of an expression {}", lexeme), format!("did not expect this token"), *span);
+            ParserErrorKind::ExpectedExpression { span, lexeme } => {
+                let mut diag = Diagnostic::simple_error(
+                    format!("Unexpected start of an expression {}", lexeme),
+                    format!("did not expect this token"),
+                    *span,
+                );
                 diag.add_note(format!("This error is commonly caused by either a previous error cascading or an unclosed delimiter."));
                 diag
-            },
-            ParserErrorKind::NoInfixFunction{span, lexeme} => {
-                Diagnostic::simple_error(format!("Token {} cannot be used as an Infix operator", lexeme), format!("cannot be used as a infix operator."), *span)
-            },
-            ParserErrorKind::TokenNotUnaryOp{spanned_token} => {
-                Diagnostic::simple_error(format!("Unsupported unary operation {}", spanned_token.token()), format!("cannot use as a unary operation."), spanned_token.into_span())
-            },
-            ParserErrorKind::TokenNotBinaryOp{spanned_token} => {
-                Diagnostic::simple_error(format!("Unsupported binary operation {}", spanned_token.token()), format!("cannot use as a binary operation."), spanned_token.into_span())
-            },
-            ParserErrorKind::UnexpectedToken{span , expected, found} => {
-                Diagnostic::simple_error(format!("Expected a {} but found {}", expected, found), format!("Expected {}", expected), *span)
             }
-            ParserErrorKind::UnexpectedTokenKind{span , expected, found} => {
-                Diagnostic::simple_error(format!("Expected a {} but found {}", expected, found), format!("Expected {}", expected), *span)
-            },
-            ParserErrorKind::UnstructuredError{span, message} => {
+            ParserErrorKind::TokenNotUnaryOp { spanned_token } => Diagnostic::simple_error(
+                format!("Unsupported unary operation {}", spanned_token.token()),
+                format!("cannot use as a unary operation."),
+                spanned_token.into_span(),
+            ),
+            ParserErrorKind::TokenNotBinaryOp { spanned_token } => Diagnostic::simple_error(
+                format!("Unsupported binary operation {}", spanned_token.token()),
+                format!("cannot use as a binary operation."),
+                spanned_token.into_span(),
+            ),
+            ParserErrorKind::UnexpectedToken {
+                span,
+                expected,
+                found,
+            } => Diagnostic::simple_error(
+                format!("Expected a {} but found {}", expected, found),
+                format!("Expected {}", expected),
+                *span,
+            ),
+            ParserErrorKind::UnexpectedTokenKind {
+                span,
+                expected,
+                found,
+            } => Diagnostic::simple_error(
+                format!("Expected a {} but found {}", expected, found),
+                format!("Expected {}", expected),
+                *span,
+            ),
+            ParserErrorKind::UnstructuredError { span, message } => {
                 Diagnostic::simple_error("".to_owned(), message.to_string(), *span)
-            },
+            }
+            ParserErrorKind::SingleKeywordSegmentNotAllowed { span, path_kind } => {
+                let note = match path_kind{
+                    PathKind::Dep => "You have specified `dep`. However, it is not possible to determine which dependency you want to import.\n Try `use dep::{name of dependency}`",
+                    PathKind::Crate => "You have specified `crate`. However, it is not possible to determine which module you want to import.\n Try `use crate::{name of module}`",
+                    _=> unreachable!("ice: this error is caused by single segment paths which contain a keyword")
+                };
+
+                let mut diag = Diagnostic::simple_error(
+                    format!("path is ambiguous"),
+                    format!("path contains a single keyword"),
+                    *span,
+                );
+                diag.add_note(note.to_owned());
+                diag
+            }
         }
     }
 }
