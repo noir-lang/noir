22399b3f2460ede39b25a5f0661e8530798f0659
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-03-01 19:32:56 +0000

    Nargo - prove_cmd
    Refactor the method to process the abi. This method takes an ABI and InputValues, then adds the witness values in the witness vector in the correct order, according to the ABI.

--- nargo/src/cli/prove_cmd.rs
+++ nargo/src/cli/prove_cmd.rs
@@ -2,11 +2,13 @@ use std::{collections::BTreeMap, path::PathBuf};
 
 use acvm::acir::native_types::Witness;
 use clap::ArgMatches;
+use noir_field::FieldElement;
+use noirc_abi::{input_parser::InputValue, Abi};
 use pwg::Solver;
 
 use crate::resolver::Resolver;
 
-use super::{create_dir, write_to_file, PROOFS_DIR};
+use super::{create_dir, write_to_file, PROOFS_DIR, PROOF_EXT};
 
 pub(crate) fn run(args: ArgMatches) {
     let proof_name = args
@@ -30,21 +32,49 @@ fn prove(proof_name: &str) {
     // Parse the initial witness values
     let mut path_to_src_dir = std::env::current_dir().unwrap();
     path_to_src_dir.push(std::path::PathBuf::from("src"));
-    let (witness_map, collection_names) =
-        noirc_abi::input_parser::Format::Toml.parse(path_to_src_dir);
+    let witness_map = noirc_abi::input_parser::Format::Toml.parse(path_to_src_dir);
 
     // Check that enough witness values were supplied
-    if compiled_program.abi.as_ref().unwrap().len() != witness_map.len() {
+    let num_params = compiled_program.abi.as_ref().unwrap().num_parameters();
+    if num_params != witness_map.len() {
         panic!(
             "Expected {} number of values, but got {} number of values",
-            compiled_program.abi.as_ref().unwrap().len(),
+            num_params,
             witness_map.len()
         )
     }
 
+    let abi = compiled_program.abi.unwrap();
+    let mut solved_witness = process_abi_with_input(abi, witness_map);
+
+    Solver::solve(&mut solved_witness, compiled_program.circuit.gates.clone());
+
+    let proof = backend_ptr
+        .backend()
+        .prove_with_meta(compiled_program.circuit, solved_witness);
+
+    let mut proof_path = create_proof_dir();
+    proof_path.push(proof_name);
+    proof_path.set_extension(PROOF_EXT);
+
+    println!("proof : {}", hex::encode(&proof));
+
+    let path = write_to_file(hex::encode(&proof).as_bytes(), &proof_path);
+    println!("Proof successfully created and located at {}", path)
+}
+
+fn create_proof_dir() -> PathBuf {
+    create_dir(PROOFS_DIR).expect("could not create the `contract` directory")
+}
+
+/// Ordering is important here, which is why we need the ABI to tell us what order to add the elements in
+/// We then need the witness map to get the elements field values.
+fn process_abi_with_input(
+    abi: Abi,
+    witness_map: BTreeMap<String, InputValue>,
+) -> BTreeMap<Witness, FieldElement> {
     let mut solved_witness = BTreeMap::new();
 
-    let abi = compiled_program.abi.unwrap();
     let param_names = abi.parameter_names();
     let mut index = 0;
 
@@ -55,47 +85,23 @@ fn prove(proof_name: &str) {
             param
         );
 
-        // Note: the collection name will not be in the witness_map
-        // only mangled_names for it's elements
-        if let Some(collection) = collection_names.iter().find(|(name, _)| name == param) {
-            for i in 0..collection.1 {
-                let mangled_element_name =
-                    noirc_abi::input_parser::mangle_array_element_name(&collection.0, i);
-                let value = witness_map.get(&mangled_element_name).expect(err_msg);
-
+        let value = witness_map.get(param).expect(err_msg).clone();
+        match value {
+            InputValue::Field(element) => {
                 let old_value =
-                    solved_witness.insert(Witness::new(index + WITNESS_OFFSET), value.clone());
+                    solved_witness.insert(Witness::new(index + WITNESS_OFFSET), element);
                 assert!(old_value.is_none());
-
-                index += 1
+                index += 1;
             }
-        } else {
-            let value = witness_map.get(param).expect(err_msg);
-
-            let old_value =
-                solved_witness.insert(Witness::new(index + WITNESS_OFFSET), value.clone());
-            assert!(old_value.is_none());
-
-            index += 1;
+            InputValue::Vec(arr) => {
+                for element in arr {
+                    let old_value =
+                        solved_witness.insert(Witness::new(index + WITNESS_OFFSET), element);
+                    assert!(old_value.is_none());
+                    index += 1;
+                }
+            }
         }
     }
-
-    Solver::solve(&mut solved_witness, compiled_program.circuit.gates.clone());
-
-    let proof = backend_ptr
-        .backend()
-        .prove_with_meta(compiled_program.circuit, solved_witness);
-
-    let mut proof_path = create_proof_dir();
-    proof_path.push(proof_name);
-    proof_path.set_extension("proof");
-
-    println!("proof : {}", hex::encode(&proof));
-
-    let path = write_to_file(hex::encode(&proof).as_bytes(), &proof_path);
-    println!("Proof successfully created and located at {}", path)
-}
-
-fn create_proof_dir() -> PathBuf {
-    create_dir(PROOFS_DIR).expect("could not create the `contract` directory")
+    solved_witness
 }
