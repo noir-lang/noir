e5af4f4e71e5ef61ac04631271f3382d9b464251
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-03-05 18:44:19 +0000

    DirCollector - Errors
    - Errors that can occur while collecting definitions. There is a bit of code repeat here, but we leave it as is for now, as we might refactor out the component specific errors like ParserErrorKind and only ever expose Diagnostic

---
+++ noirc_frontend/src/hir/def_collector/errors.rs
@@ -0,0 +1,83 @@
+use crate::Ident;
+
+use noirc_errors::CustomDiagnostic as Diagnostic;
+use noirc_errors::DiagnosableError;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum DefCollectorErrorKind {
+    #[error("duplicate function found in namespace")]
+    DuplicateFunction { first_def: Ident, second_def: Ident },
+    #[error("duplicate function found in namespace")]
+    DuplicateModuleDecl { first_def: Ident, second_def: Ident },
+    #[error("duplicate import")]
+    DuplicateImport { first_def: Ident, second_def: Ident },
+    #[error("duplicate import")]
+    UnresolvedModuleDecl { mod_name: Ident },
+}
+
+impl DiagnosableError for DefCollectorErrorKind {
+    fn to_diagnostic(&self) -> Diagnostic {
+        match self {
+            DefCollectorErrorKind::DuplicateFunction {
+                first_def,
+                second_def,
+            } => {
+                let first_span = first_def.0.span();
+                let second_span = second_def.0.span();
+                let func_name = &first_def.0.contents;
+
+                let mut diag = Diagnostic::simple_error(
+                    format!("duplicate definitions of {} function found", func_name),
+                    format!("first definition found here"),
+                    first_span,
+                );
+                diag.add_secondary(format!("second definition found here"), second_span);
+                diag
+            }
+            DefCollectorErrorKind::DuplicateModuleDecl {
+                first_def,
+                second_def,
+            } => {
+                let first_span = first_def.0.span();
+                let second_span = second_def.0.span();
+                let mod_name = &first_def.0.contents;
+
+                let mut diag = Diagnostic::simple_error(
+                    format!("module {} has been declared twice", mod_name),
+                    format!("first declaration found here"),
+                    first_span,
+                );
+                diag.add_secondary(format!("second declaration found here"), second_span);
+                diag
+            }
+            DefCollectorErrorKind::DuplicateImport {
+                first_def,
+                second_def,
+            } => {
+                let first_span = first_def.0.span();
+                let second_span = second_def.0.span();
+                let import_name = &first_def.0.contents;
+
+                let mut diag = Diagnostic::simple_error(
+                    format!("the name `{}` is defined multiple times", import_name),
+                    format!("first import found here"),
+                    first_span,
+                );
+                diag.add_secondary(format!("second import found here"), second_span);
+                diag
+            }
+            DefCollectorErrorKind::UnresolvedModuleDecl { mod_name } => {
+                let span = mod_name.0.span();
+                let mod_name = &mod_name.0.contents;
+
+                let diag = Diagnostic::simple_error(
+                    format!("could not resolve module `{}` ", mod_name),
+                    format!(""),
+                    span,
+                );
+                diag
+            }
+        }
+    }
+}
