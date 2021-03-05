5f7c24dff92db8b2c20681d11efd9edae9095480
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-03-05 17:50:11 +0000

    FM
    - Add VirtualPaths which are paths which link to the filesystem, however you cannot use them to find a file. An example of a virtual path transformation is "src/lib.rs" -> "src/lib". There is a connection with the file system, however you cannot use "src/lib" to find the lib.rs file directly, you need to add the file extension.
    
    - If you read the PathBuf in a VirtualPath, it will always point to a directory and not a file(directly).
    
    - remove .parent method; before we were storing the actual file paths to files, so we used .parent to get the directory. This is no longer needed with VirtualPaths.

--- fm/src/lib.rs
+++ fm/src/lib.rs
@@ -2,21 +2,29 @@ mod file_map;
 pub use file_map::{File, FileId, FileMap};
 
 pub mod util;
+use std::path::{Path, PathBuf};
 pub use util::*;
 
-use std::path::{Path, PathBuf};
-
-pub const LIB_FILE: &'static str = "lib";
-pub const BIN_FILE: &'static str = "main"; // XXX: This will be changed to be the entry point and not a static name.
-pub const FILE_EXTENSION: &'static str = "nr";
-pub const MOD_FILE: &'static str = "mod";
+const FILE_EXTENSION: &'static str = "nr";
+const MOD_FILE: &'static str = "mod";
 
 // XXX: Create a trait for file io
+/// An enum to differentiate between the root file
+/// which the compiler starts at, and the others.
+/// This is so that submodules of the root, can live alongside the
+/// root file as files.
+pub enum FileType {
+    Root,
+    Normal,
+}
+
+#[derive(Debug)]
+struct VirtualPath(PathBuf);
 
 #[derive(Debug)]
 pub struct FileManager {
     file_map: file_map::FileMap,
-    paths: Vec<PathBuf>,
+    paths: Vec<VirtualPath>,
 }
 
 impl FileManager {
@@ -30,7 +38,7 @@ impl FileManager {
     // XXX: Maybe use a AsRef<Path> here, for API ergonomics
     // XXX: Would it break any assumptions, if we returned the same FileId,
     // given the same file_path?
-    pub fn add_file(&mut self, path_to_file: &PathBuf) -> Option<FileId> {
+    pub fn add_file(&mut self, path_to_file: &PathBuf, file_type: FileType) -> Option<FileId> {
         // We expect the caller to ensure that the file is a valid noir file
         let ext = path_to_file
             .extension()
@@ -42,7 +50,8 @@ impl FileManager {
         let source = std::fs::read_to_string(&path_to_file).ok()?;
 
         let file_id = self.file_map.add_file(path_to_file.clone().into(), source);
-        self.paths.push(path_to_file.clone());
+        let path_to_file = virtualise_path(path_to_file, file_type);
+        self.paths.push(path_to_file);
 
         Some(file_id)
     }
@@ -51,27 +60,28 @@ impl FileManager {
         // Unwrap as we ensure that all file_id's map to a corresponding file in the file map
         self.file_map.get_file(file_id).unwrap()
     }
-    pub fn path(&mut self, file_id: FileId) -> &Path {
+    fn path(&mut self, file_id: FileId) -> &Path {
         // Unchecked as we ensure that all file_ids are created by the file manager
         // So all file_ids will points to a corresponding path
-        self.paths[file_id.as_usize()].as_path()
-    }
-    pub fn parent(&mut self, file_id: FileId) -> &Path {
-        // Unwrap as we ensure that all file_ids's point to files
-        // whom logically live in some directory
-        self.path(file_id).parent().unwrap()
+        self.paths[file_id.as_usize()].0.as_path()
     }
 
     pub fn resolve_path(&mut self, anchor: FileId, mod_name: &str) -> Result<FileId, String> {
         let mut candidate_files = Vec::new();
 
-        let dir = self.parent(anchor).to_path_buf();
+        let dir = self.path(anchor).to_path_buf();
 
-        candidate_files.push(dir.to_path_buf().join(&format!("{}.nr", mod_name)));
-        candidate_files.push(dir.to_path_buf().join(&format!("{}/mod.nr", mod_name)));
+        candidate_files.push(
+            dir.to_path_buf()
+                .join(&format!("{}.{}", mod_name, FILE_EXTENSION)),
+        );
+        candidate_files.push(
+            dir.to_path_buf()
+                .join(&format!("{}/{}.{}", mod_name, MOD_FILE, FILE_EXTENSION)),
+        );
 
         for candidate in candidate_files.iter() {
-            if let Some(file_id) = self.add_file(candidate) {
+            if let Some(file_id) = self.add_file(candidate, FileType::Normal) {
                 return Ok(file_id);
             }
         }
@@ -84,7 +94,28 @@ impl FileManager {
             .to_owned())
     }
 }
-
+/// Takes a path to a noir file. This will panic on paths to directories
+/// Returns
+/// For Normal filetypes, given "src/mod.nr" this method returns "src/mod"
+/// For Root filetypes, given "src/mod.nr" this method returns "src"
+fn virtualise_path(path: &PathBuf, file_type: FileType) -> VirtualPath {
+    let mut path = path.clone();
+    let path = match file_type {
+        FileType::Root => {
+            path.pop();
+            path
+        }
+        FileType::Normal => {
+            let base = path.parent().unwrap();
+            let path_no_ext: PathBuf = path
+                .file_stem()
+                .expect("ice: this should have been the path to a file")
+                .into();
+            base.join(path_no_ext)
+        }
+    };
+    VirtualPath(path)
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -104,7 +135,7 @@ mod tests {
 
         let mut fm = FileManager::new();
 
-        let file_id = fm.add_file(&file_path).unwrap();
+        let file_id = fm.add_file(&file_path, FileType::Normal).unwrap();
 
         let _foo_file_path = dummy_file_path(&dir, "foo.nr");
         fm.resolve_path(file_id, "foo").unwrap();
@@ -116,7 +147,7 @@ mod tests {
         let dir = tempdir().unwrap();
         let file_path = dummy_file_path(&dir, "my_dummy_file.nr");
 
-        let file_id = fm.add_file(&file_path).unwrap();
+        let file_id = fm.add_file(&file_path, FileType::Normal).unwrap();
 
         let sub_dir = TempDir::new_in(dir).unwrap();
         std::fs::create_dir_all(sub_dir.path()).unwrap();
