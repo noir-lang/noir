3b005ffab454240e66bed19939e997bad4653d84
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-04-01 21:20:21 +0100

    arkworks_backend
    - remove unused dependencies
    - add comment on why we are using Bn254 in the bridge
    - move the ConstraintSynthesizer trait impl further up, as it is the most important. The other functions here are to convert from acir to these bridge structs

--- crates/arkworks_backend/src/bridge.rs
+++ crates/arkworks_backend/src/bridge.rs
@@ -38,55 +38,10 @@ pub struct AcirArithGate<F: Field> {
     constant_term: F,
 }
 
+// XXX: This is in place, until we make noir_field generic.
 use ark_bn254::Fr;
 type Bn254Acir = AcirCircuit<Fr>;
 type Bn254AcirArithGate = AcirArithGate<Fr>;
-
-impl From<(Circuit, Vec<Fr>)> for Bn254Acir {
-    fn from(circ_val: (Circuit, Vec<Fr>)) -> Bn254Acir {
-        // Currently non-arithmetic gates are not supported
-        // so we extract all of the arithmetic gates only
-        let circ = circ_val.0;
-        let arith_gates: Vec<_> = circ
-            .gates
-            .into_iter()
-            .filter(|gate| gate.is_arithmetic())
-            .map(|gate| Bn254AcirArithGate::from(gate.arithmetic()))
-            .collect();
-
-        let values = circ_val.1;
-
-        let num_vars = (circ.current_witness_index + 1) as usize;
-        Bn254Acir {
-            gates: arith_gates,
-            values,
-            num_variables: num_vars,
-            public_inputs: circ.public_inputs,
-        }
-    }
-}
-
-impl From<Arithmetic> for Bn254AcirArithGate {
-    fn from(arith_gate: Arithmetic) -> Bn254AcirArithGate {
-        let converted_mul_terms: Vec<_> = arith_gate
-            .mul_terms
-            .into_iter()
-            .map(|(coeff, l_var, r_var)| (coeff.into_repr(), l_var, r_var))
-            .collect();
-
-        let converted_linear_combinations: Vec<_> = arith_gate
-            .linear_combinations
-            .into_iter()
-            .map(|(coeff, var)| (coeff.into_repr(), var))
-            .collect();
-
-        Bn254AcirArithGate {
-            mul_terms: converted_mul_terms,
-            add_terms: converted_linear_combinations,
-            constant_term: arith_gate.q_c.into_repr(),
-        }
-    }
-}
 
 impl<ConstraintF: Field> ConstraintSynthesizer<ConstraintF> for AcirCircuit<ConstraintF> {
     fn generate_constraints(
@@ -145,5 +100,51 @@ impl<ConstraintF: Field> ConstraintSynthesizer<ConstraintF> for AcirCircuit<Cons
         }
 
         Ok(())
+    }
+}
+
+impl From<(Circuit, Vec<Fr>)> for Bn254Acir {
+    fn from(circ_val: (Circuit, Vec<Fr>)) -> Bn254Acir {
+        // Currently non-arithmetic gates are not supported
+        // so we extract all of the arithmetic gates only
+        let circ = circ_val.0;
+        let arith_gates: Vec<_> = circ
+            .gates
+            .into_iter()
+            .filter(|gate| gate.is_arithmetic())
+            .map(|gate| Bn254AcirArithGate::from(gate.arithmetic()))
+            .collect();
+
+        let values = circ_val.1;
+
+        let num_vars = (circ.current_witness_index + 1) as usize;
+        Bn254Acir {
+            gates: arith_gates,
+            values,
+            num_variables: num_vars,
+            public_inputs: circ.public_inputs,
+        }
+    }
+}
+
+impl From<Arithmetic> for Bn254AcirArithGate {
+    fn from(arith_gate: Arithmetic) -> Bn254AcirArithGate {
+        let converted_mul_terms: Vec<_> = arith_gate
+            .mul_terms
+            .into_iter()
+            .map(|(coeff, l_var, r_var)| (coeff.into_repr(), l_var, r_var))
+            .collect();
+
+        let converted_linear_combinations: Vec<_> = arith_gate
+            .linear_combinations
+            .into_iter()
+            .map(|(coeff, var)| (coeff.into_repr(), var))
+            .collect();
+
+        Bn254AcirArithGate {
+            mul_terms: converted_mul_terms,
+            add_terms: converted_linear_combinations,
+            constant_term: arith_gate.q_c.into_repr(),
+        }
     }
 }
