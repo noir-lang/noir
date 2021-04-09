f5825ab601ac357146620cd87428f96f325db8b0
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-04-09 19:13:33 +0100

    arkworks backend
    - Enforce all arithmetic gates to be equal to zero

--- crates/arkworks_backend/src/bridge.rs
+++ crates/arkworks_backend/src/bridge.rs
@@ -1,5 +1,5 @@
-use acir::{circuit::Circuit, native_types::Witness};
-use acir::{circuit::PublicInputs, native_types::Arithmetic};
+use acir::circuit::PublicInputs;
+use acir::native_types::Witness;
 use ark_ff::Field;
 use ark_relations::{
     lc,
@@ -7,7 +7,6 @@ use ark_relations::{
         ConstraintSynthesizer, ConstraintSystemRef, LinearCombination, SynthesisError, Variable,
     },
 };
-
 // AcirCircuit and AcirArithGate are structs that arkworks can synthesise.
 //
 // The difference between these structures and the ACIR structure that the compiler uses is the following:
@@ -25,17 +24,17 @@ use ark_relations::{
 // - verify(index_vk, verifier, rng)
 #[derive(Clone)]
 pub struct AcirCircuit<F: Field> {
-    gates: Vec<AcirArithGate<F>>,
-    public_inputs: PublicInputs,
-    values: Vec<F>,
-    num_variables: usize,
+    pub(crate) gates: Vec<AcirArithGate<F>>,
+    pub(crate) public_inputs: PublicInputs,
+    pub(crate) values: Vec<F>,
+    pub(crate) num_variables: usize,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct AcirArithGate<F: Field> {
-    mul_terms: Vec<(F, Witness, Witness)>,
-    add_terms: Vec<(F, Witness)>,
-    constant_term: F,
+    pub(crate) mul_terms: Vec<(F, Witness, Witness)>,
+    pub(crate) add_terms: Vec<(F, Witness)>,
+    pub(crate) constant_term: F,
 }
 
 // XXX: This is in place, until we make noir_field generic.
@@ -48,23 +47,17 @@ impl<ConstraintF: Field> ConstraintSynthesizer<ConstraintF> for AcirCircuit<Cons
         self,
         cs: ConstraintSystemRef<ConstraintF>,
     ) -> Result<(), SynthesisError> {
-        // The first variable is zero in Noir.
-        // In PLONK there is no Variable::zero
-        // so we offset the witnesses in Noir by one
-        let zero = cs.new_witness_variable(|| Ok(ConstraintF::zero()))?;
-
-        let mut variables = vec![zero];
+        let mut variables = Vec::with_capacity(self.values.len());
 
         // First create all of the witness indices by adding the values into the constraint system
-        for i in 0..self.num_variables {
-            let val = self.values[i];
+        for (i, val) in self.values.iter().enumerate() {
             let var = if self.public_inputs.contains(i) {
-                cs.new_input_variable(|| Ok(val))?
+                cs.new_input_variable(|| Ok(*val))?
             } else {
-                cs.new_witness_variable(|| Ok(val))?
+                cs.new_witness_variable(|| Ok(*val))?
             };
 
-            variables.push(var)
+            variables.push(var);
         }
 
         // Now iterate each gate and add it to the constraint system
@@ -74,11 +67,9 @@ impl<ConstraintF: Field> ConstraintSynthesizer<ConstraintF> for AcirCircuit<Cons
             // Process Mul terms
             for mul_term in gate.mul_terms {
                 let coeff = mul_term.0;
-                let left_var = variables[mul_term.1.as_usize()];
-                let right_var = variables[mul_term.2.as_usize()];
+                let left_val = self.values[mul_term.1.as_usize()];
+                let right_val = self.values[mul_term.2.as_usize()];
 
-                let left_val = cs.assigned_value(left_var).unwrap();
-                let right_val = cs.assigned_value(right_var).unwrap();
                 let out_val = left_val * &right_val;
 
                 let out_var = cs.new_witness_variable(|| Ok(out_val))?;
@@ -95,56 +86,9 @@ impl<ConstraintF: Field> ConstraintSynthesizer<ConstraintF> for AcirCircuit<Cons
             // Process constant term
             arith_gate += (gate.constant_term, Variable::One);
 
-            let result = cs.new_lc(arith_gate)?;
-            cs.enforce_constraint(lc!() + Variable::One, lc!() + result, lc!() + result)?;
+            cs.enforce_constraint(lc!() + Variable::One, arith_gate, lc!())?;
         }
 
         Ok(())
-    }
-}
-
-impl From<(Circuit, Vec<Fr>)> for Bn254Acir {
-    fn from(circ_val: (Circuit, Vec<Fr>)) -> Bn254Acir {
-        // Currently non-arithmetic gates are not supported
-        // so we extract all of the arithmetic gates only
-        let circ = circ_val.0;
-        let arith_gates: Vec<_> = circ
-            .gates
-            .into_iter()
-            .filter(|gate| gate.is_arithmetic())
-            .map(|gate| Bn254AcirArithGate::from(gate.arithmetic()))
-            .collect();
-
-        let values = circ_val.1;
-
-        let num_vars = (circ.current_witness_index + 1) as usize;
-        Bn254Acir {
-            gates: arith_gates,
-            values,
-            num_variables: num_vars,
-            public_inputs: circ.public_inputs,
-        }
-    }
-}
-
-impl From<Arithmetic> for Bn254AcirArithGate {
-    fn from(arith_gate: Arithmetic) -> Bn254AcirArithGate {
-        let converted_mul_terms: Vec<_> = arith_gate
-            .mul_terms
-            .into_iter()
-            .map(|(coeff, l_var, r_var)| (coeff.into_repr(), l_var, r_var))
-            .collect();
-
-        let converted_linear_combinations: Vec<_> = arith_gate
-            .linear_combinations
-            .into_iter()
-            .map(|(coeff, var)| (coeff.into_repr(), var))
-            .collect();
-
-        Bn254AcirArithGate {
-            mul_terms: converted_mul_terms,
-            add_terms: converted_linear_combinations,
-            constant_term: arith_gate.q_c.into_repr(),
-        }
     }
 }
