2e6205dffbdab38b855afeb3478f7703d113bd6e
Charlie Lye <karl.lye@gmail.com>
2021-04-16 21:58:53 +0100

    Cl/build tags (https://github.com/AztecProtocol/barretenberg/pull/341)
    Build system updates.

--- circuits/cpp/barretenberg/src/aztec/numeric/bitop/count_leading_zeros.test.cpp
+++ circuits/cpp/barretenberg/src/aztec/numeric/bitop/count_leading_zeros.test.cpp
@@ -1,29 +1,33 @@
 #include "count_leading_zeros.hpp"
 #include <gtest/gtest.h>
 
-TEST(bitop, clz_uint32_0)
+TEST(bitop, clz_uint32_31)
 {
     uint32_t a = 0b00000000000000000000000000000001;
     EXPECT_EQ(numeric::count_leading_zeros(a), 31U);
 }
 
-TEST(bitop, clz_uint32_31)
+TEST(bitop, clz_uint32_0)
 {
     uint32_t a = 0b10000000000000000000000000000001;
     EXPECT_EQ(numeric::count_leading_zeros(a), 0U);
 }
 
-TEST(bitop, clz_uint64_63)
+TEST(bitop, clz_uint64_0)
 {
     uint64_t a = 0b1000000000000000000000000000000100000000000000000000000000000000;
     EXPECT_EQ(numeric::count_leading_zeros(a), 0U);
 }
 
-TEST(bitop, clz_size_t_7)
+TEST(bitop, clz_size_t)
 {
     size_t a = 0x80;
     auto r = numeric::count_leading_zeros(a);
-    EXPECT_EQ(r, 56U);
+    if (sizeof(a) == 4) {
+        EXPECT_EQ(r, 24U);
+    } else {
+        EXPECT_EQ(r, 56U);
+    }
 }
 
 TEST(bitop, clz_uint256_255)
@@ -33,7 +37,7 @@ TEST(bitop, clz_uint256_255)
     EXPECT_EQ(r, 255U);
 }
 
-TEST(bitop, clz_uint256_7)
+TEST(bitop, clz_uint256_248)
 {
     uint256_t a = 0x80;
     auto r = numeric::count_leading_zeros(a);
