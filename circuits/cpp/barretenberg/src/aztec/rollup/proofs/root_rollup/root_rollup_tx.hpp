85c315a2aa7e9dd3cc960b4c4bea6cdfde84c8ad
Charlie Lye <karl.lye@gmail.com>
2021-05-19 22:54:00 +0000

    WIP

--- circuits/cpp/barretenberg/src/aztec/rollup/proofs/root_rollup/root_rollup_tx.hpp
+++ circuits/cpp/barretenberg/src/aztec/rollup/proofs/root_rollup/root_rollup_tx.hpp
@@ -42,13 +42,13 @@ struct root_rollup_tx {
     fr_hash_path old_defi_interaction_path;
     fr_hash_path new_defi_interaction_path;
 
+    // The interaction nonce is added to the partial claim notes in the defi deposits.
+    uint32_t interaction_nonce;
+
     // num_defi_interactions bridge ids.
     // Will be padded to NUM_BRIDGE_CALLS_PER_BLOCK.
     // All defi deposits must match one of these.
     std::vector<uint256_t> bridge_ids;
-
-    // The interaction nonce is added to the partial claim notes in the defi deposits.
-    uint32_t interaction_nonce;
 
     // num_defi_interactions defi_interaction_notes;
     // Will be padded to NUM_BRIDGE_CALLS_PER_BLOCK.
@@ -72,9 +72,9 @@ template <typename B> inline void read(B& buf, root_rollup_tx& tx)
     read(buf, tx.new_defi_interaction_root);
     read(buf, tx.old_defi_interaction_path);
     read(buf, tx.new_defi_interaction_path);
+    read(buf, tx.interaction_nonce);
     read(buf, tx.bridge_ids);
     read(buf, tx.defi_interaction_notes);
-    read(buf, tx.interaction_nonce);
 }
 
 template <typename B> inline void write(B& buf, root_rollup_tx const& tx)
@@ -92,9 +92,9 @@ template <typename B> inline void write(B& buf, root_rollup_tx const& tx)
     write(buf, tx.new_defi_interaction_root);
     write(buf, tx.old_defi_interaction_path);
     write(buf, tx.new_defi_interaction_path);
+    write(buf, tx.interaction_nonce);
     write(buf, tx.bridge_ids);
     write(buf, tx.defi_interaction_notes);
-    write(buf, tx.interaction_nonce);
 }
 
 inline std::ostream& operator<<(std::ostream& os, root_rollup_tx const& tx)
