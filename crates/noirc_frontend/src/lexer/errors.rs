01d0ff08d548e47d8c98160cc2fc8f3709114fcd
kevaundray <kevtheappdev@gmail.com>
2021-05-24 18:35:27 +0100

    Remove explicit Lexer aborts (#73)

--- crates/noirc_frontend/src/lexer/errors.rs
+++ crates/noirc_frontend/src/lexer/errors.rs
@@ -6,19 +6,33 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum LexerErrorKind {
     #[error("An unexpected character {:?} was found.", found)]
-    UnexpectedCharacter { span: Span, found: char },
+    UnexpectedCharacter {
+        span: Span,
+        expected: String,
+        found: char,
+    },
     #[error("The character {:?} is not in the language.", found)]
     CharacterNotInLanguage { span: Span, found: char },
-    #[error("Internal Error : {:?} is not a double char token", found)]
+    #[error("NotADoubleChar : {:?} is not a double char token", found)]
     NotADoubleChar { span: Span, found: Token },
+    #[error("InvalidIntegerLiteral : {:?} is not a integer", found)]
+    InvalidIntegerLiteral { span: Span, found: String },
+    #[error("MalformedFuncAttribute : {:?} is not a valid attribute", found)]
+    MalformedFuncAttribute { span: Span, found: String },
+    #[error("TooManyBits")]
+    TooManyBits { span: Span, max: u32, got: u32 },
 }
 
 impl DiagnosableError for LexerErrorKind {
     fn to_diagnostic(&self) -> Diagnostic {
         match self {
-            LexerErrorKind::UnexpectedCharacter { span, found } => Diagnostic::simple_error(
+            LexerErrorKind::UnexpectedCharacter {
+                span,
+                expected,
+                found,
+            } => Diagnostic::simple_error(
                 "an unexpected character was found".to_string(),
-                format!(" {:?} is unexpected", found),
+                format!(" expected {} , but got {}", expected, found),
                 *span,
             ),
             LexerErrorKind::CharacterNotInLanguage { span, found } => Diagnostic::simple_error(
@@ -31,6 +45,24 @@ impl DiagnosableError for LexerErrorKind {
                 format!(
                     " {:?} is not a double char, this is an internal error",
                     found
+                ),
+                *span,
+            ),
+            LexerErrorKind::InvalidIntegerLiteral { span, found } => Diagnostic::simple_error(
+                "invalid integer literal".to_string(),
+                format!(" {} is not an integer", found),
+                *span,
+            ),
+            LexerErrorKind::MalformedFuncAttribute { span, found } => Diagnostic::simple_error(
+                "malformed function attribute".to_string(),
+                format!(" {} is not a valid attribute", found),
+                *span,
+            ),
+            LexerErrorKind::TooManyBits { span, max, got } => Diagnostic::simple_error(
+                "integer literal too large".to_string(),
+                format!(
+                    "The maximum number of bits needed to represent a field is {}, This integer type needs {} bits",
+                    max, got
                 ),
                 *span,
             ),
