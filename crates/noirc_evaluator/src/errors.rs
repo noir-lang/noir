77b61f143073d56df59a9d284c3f79e6e2f601ab
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-06-15 13:07:58 +0100

    evaluator
    - Add RunTimeError and impl DiagnosableError for it

--- crates/noirc_evaluator/src/errors.rs
+++ crates/noirc_evaluator/src/errors.rs
@@ -3,6 +3,30 @@ use noirc_errors::DiagnosableError;
 use noirc_errors::Span;
 use thiserror::Error;
 
+#[derive(Debug)]
+pub struct RuntimeError {
+    pub span: Span,
+    pub kind: RuntimeErrorKind,
+}
+
+impl RuntimeError {
+    // XXX: In some places, we strip the span because we do not want span to
+    // be introduced into the binary op or low level function code, for simplicity.
+    //
+    // It's possible to have it there, but it means we will need to proliferate the code with span
+    //
+    // This does make error reporting, less specific!
+    pub fn remove_span(self) -> RuntimeErrorKind {
+        self.kind
+    }
+}
+
+impl RuntimeErrorKind {
+    pub fn add_span(self, span: Span) -> RuntimeError {
+        RuntimeError { span, kind: self }
+    }
+}
+
 #[derive(Error, Debug)]
 pub enum RuntimeErrorKind {
     // Array errors
@@ -42,24 +66,25 @@ impl RuntimeErrorKind {
     }
 }
 
-impl DiagnosableError for RuntimeErrorKind {
+impl DiagnosableError for RuntimeError {
     fn to_diagnostic(&self) -> Diagnostic {
-        match self {
+        let span = self.span;
+        match &self.kind {
             RuntimeErrorKind::ArrayOutOfBounds { index, bound } => Diagnostic::simple_error(
                 "index out of bounds".to_string(),
                 format!(
                     "out of bounds error, index is {} but length is {}",
                     index, bound
                 ),
-                Span::default(),
+                span,
             ),
             RuntimeErrorKind::ArrayNotFound { found_type, name } => Diagnostic::simple_error(
                 format!("cannot find an array with name {}", name),
                 format!("{} has type", found_type),
-                Span::default(),
+                span,
             ),
             RuntimeErrorKind::UnstructuredError { message } => {
-                Diagnostic::simple_error("".to_owned(), message.to_string(), Span::default())
+                Diagnostic::simple_error("".to_owned(), message.to_string(), span)
             }
             RuntimeErrorKind::UnsupportedOp {
                 op,
@@ -71,14 +96,14 @@ impl DiagnosableError for RuntimeErrorKind {
                     "no support for {} with types {} and {}",
                     op, first_type, second_type
                 ),
-                Span::default(),
+                span,
             ),
             RuntimeErrorKind::Spanless(message) => Diagnostic::from_message(message),
             RuntimeErrorKind::Unimplemented(message) => Diagnostic::from_message(message),
             RuntimeErrorKind::FunctionNonMainContext { func_name } => Diagnostic::simple_error(
                 "cannot call function outside of main".to_owned(),
                 format!("function {} can only be called in main", func_name),
-                Span::default(),
+                span,
             ),
         }
     }
