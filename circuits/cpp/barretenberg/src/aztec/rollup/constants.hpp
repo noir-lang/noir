cdf3016f77410dc62053c4045a18f4390ed10d87
Suyash Bagad <suyash@aztecprotocol.com>
2021-07-30 11:29:41 +0530

    Change NUM_ASSETS from 4 to 16 in One Rollup (https://github.com/AztecProtocol/barretenberg/pull/440)
    * initial commit for num_assets=16.
    
    * Regen.
    
    * Regen.
    
    * Remove maxNumAsset=100 bound.

--- circuits/cpp/barretenberg/src/aztec/rollup/constants.hpp
+++ circuits/cpp/barretenberg/src/aztec/rollup/constants.hpp
@@ -14,7 +14,7 @@ constexpr size_t MAX_NO_WRAP_INTEGER_BIT_LENGTH = 252;
 constexpr size_t MAX_TXS_BIT_LENGTH = 10;
 constexpr size_t TX_FEE_BIT_LENGTH = MAX_NO_WRAP_INTEGER_BIT_LENGTH - MAX_TXS_BIT_LENGTH;
 
-constexpr size_t NUM_ASSETS_BIT_LENGTH = 2;
+constexpr size_t NUM_ASSETS_BIT_LENGTH = 4;
 constexpr size_t NUM_ASSETS = 1 << NUM_ASSETS_BIT_LENGTH;
 constexpr size_t MAX_NUM_ASSETS_BIT_LENGTH = 30;
 constexpr size_t MAX_NUM_ASSETS = 1 << MAX_NUM_ASSETS_BIT_LENGTH;
