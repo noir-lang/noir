cf2f238ec3ed291fbf339621361fa06ffd679832
kevaundray <kevtheappdev@gmail.com>
2021-11-16 19:12:45 +0000

    Extends Merkle API to include Insert (#132)

--- crates/acvm/src/backends/csat_3_plonk_aztec/pwg/gadget_call.rs
+++ crates/acvm/src/backends/csat_3_plonk_aztec/pwg/gadget_call.rs
@@ -25,63 +25,34 @@ impl GadgetCaller {
             }
             OPCODE::AES => return Err(gadget_call.name),
             OPCODE::MerkleMembership => {
-                let mut inputs_iter = gadget_call.inputs.iter();
+                const SHOULD_INSERT: bool = false;
 
-                let _root = inputs_iter.next().expect("expected a root");
-                let root = *input_to_value(initial_witness, _root);
+                let merkle_data =
+                    process_merkle_gadget(initial_witness, gadget_call, SHOULD_INSERT);
+                assert!(merkle_data.new_root.is_none());
 
-                let _leaf = inputs_iter.next().expect("expected a leaf");
-                let leaf = *input_to_value(initial_witness, _leaf);
-
-                let _index = inputs_iter.next().expect("expected the depth parameter");
-                // The value of index should not be set yet; it was created in the evaluator
-
-                let hashpath_indices: Vec<_> = inputs_iter.collect();
-                let arity = 2;
-                let depth = hashpath_indices.len() / arity;
-
-                // TODO: We either need to hardcode this on a known program path
-                // TODO or allow the user to input it somehow in a settings file.
-                let merkle_tree = MerkleTree::from_path("../data/merkle_db");
-                let expected_merkle_root = merkle_tree.root();
-                let expected_depth = merkle_tree.depth();
-
-                assert_eq!(root, expected_merkle_root, "the merkle root provided does not match the merkle root in your db, got {} expected {}", root.to_hex(), expected_merkle_root.to_hex());
-                assert_eq!(depth as u32, expected_depth, "the depth provided does not match the merkle depth in your db, got {} expected {}", depth, expected_depth);
-
-                // Update the index
-
-                let index = merkle_tree.find_index_from_leaf(&leaf).unwrap_or_else(|| {
-                    panic!(
-                        "could not find leaf in the merkle tree. {} not found",
-                        leaf.to_hex()
-                    )
-                });
-
-                let index_fr = FieldElement::from(index as i128);
-                // Set the index here
-                initial_witness.insert(_index.witness, index_fr);
-
-                // Update hashpath
-                let path = merkle_tree.get_hash_path(index);
-                let mut hashpath_indices = hashpath_indices.iter();
-                for (left_hash, right_hash) in path.iter().copied() {
-                    let left = hashpath_indices.next().unwrap().witness;
-                    let right = hashpath_indices.next().unwrap().witness;
-
-                    initial_witness.insert(left, left_hash);
-                    initial_witness.insert(right, right_hash);
-                }
-                let hash_path = flatten_path(path);
+                let hash_path = flatten_path(merkle_data.hashpath);
 
                 let result = MerkleTree::check_membership(
                     hash_path.iter().collect(),
-                    &root,
-                    &index_fr,
-                    &leaf,
+                    &merkle_data.old_root,
+                    &merkle_data.index,
+                    &merkle_data.leaf,
                 );
 
                 initial_witness.insert(gadget_call.outputs[0], result);
+            }
+            OPCODE::InsertRegularMerkle => {
+                const SHOULD_INSERT: bool = true;
+
+                let merkle_data =
+                    process_merkle_gadget(initial_witness, gadget_call, SHOULD_INSERT);
+
+                let new_root = merkle_data
+                    .new_root
+                    .expect("new root should be computed for insertions");
+
+                initial_witness.insert(gadget_call.outputs[0], new_root);
             }
             OPCODE::SchnorrVerify => {
                 // In barretenberg, if the signature fails, then the whole thing fails.
@@ -186,5 +157,94 @@ impl GadgetCaller {
             }
         }
         Ok(())
+    }
+}
+
+struct MerkleData {
+    hashpath: super::merkle::HashPath,
+    old_root: FieldElement,
+    new_root: Option<FieldElement>,
+    leaf: FieldElement,
+    index: FieldElement,
+}
+fn process_merkle_gadget(
+    initial_witness: &mut BTreeMap<Witness, FieldElement>,
+    gadget_call: &GadgetCall,
+    should_insert: bool,
+) -> MerkleData {
+    let mut inputs_iter = gadget_call.inputs.iter();
+
+    let _root = inputs_iter.next().expect("expected a root");
+    let root = *input_to_value(initial_witness, _root);
+
+    let _leaf = inputs_iter.next().expect("expected a leaf");
+    let leaf = *input_to_value(initial_witness, _leaf);
+
+    let _index = inputs_iter.next().expect("expected the depth parameter");
+    // The value of index should not be set yet; it was created in the evaluator
+
+    let hashpath_indices: Vec<_> = inputs_iter.collect();
+    let arity = 2;
+    let depth = hashpath_indices.len() / arity;
+
+    // TODO: We either need to hardcode this on a known program path
+    // TODO or allow the user to input it somehow in a settings file.
+    let mut merkle_tree = MerkleTree::from_path("../data/merkle_db");
+    let expected_merkle_root = merkle_tree.root();
+    let expected_depth = merkle_tree.depth();
+
+    assert_eq!(
+        root,
+        expected_merkle_root,
+        "the merkle root provided does not match the merkle root in your db, got {} expected {}",
+        root.to_hex(),
+        expected_merkle_root.to_hex()
+    );
+    assert_eq!(
+        depth as u32, expected_depth,
+        "the depth provided does not match the merkle depth in your db, got {} expected {}",
+        depth, expected_depth
+    );
+
+    let (index, new_root) = if should_insert {
+        // To insert, we first fetch the index of the fist empty leaf
+        // Then we insert into the trie to compute the new root
+        // It should be inserted into that same empty spot
+        let _index = merkle_tree.find_index_for_empty_leaf();
+        let new_root = merkle_tree.update_leaf(_index, leaf);
+
+        (_index, Some(new_root))
+    } else {
+        let _index = merkle_tree.find_index_from_leaf(&leaf).unwrap_or_else(|| {
+            panic!(
+                "could not find leaf in the merkle tree. {} not found",
+                leaf.to_hex()
+            )
+        });
+        (_index, None)
+    };
+
+    let index_fr = FieldElement::from(index as i128);
+    // Set the index here
+    initial_witness.insert(_index.witness, index_fr);
+
+    // Update hashpath
+    let path = merkle_tree.get_hash_path(index);
+
+    let mut hashpath_indices = hashpath_indices.iter();
+    for (left_hash, right_hash) in path.iter().copied() {
+        let left = hashpath_indices.next().unwrap().witness;
+        let right = hashpath_indices.next().unwrap().witness;
+
+        initial_witness.insert(left, left_hash);
+        initial_witness.insert(right, right_hash);
+    }
+
+    MerkleData {
+        hashpath: path,
+        old_root: root,
+        new_root,
+        leaf,
+        index: index_fr,
     }
 }
--- crates/noirc_evaluator/src/low_level_function_impl/mod.rs
+++ crates/noirc_evaluator/src/low_level_function_impl/mod.rs
@@ -8,6 +8,7 @@ mod blake2s;
 mod ecdsa_secp256k1;
 mod fixed_based_scalar_mul;
 mod hash_to_field;
+mod insert_regular_merkle;
 mod merkle_membership;
 mod pedersen;
 mod schnorr;
@@ -20,6 +21,7 @@ use blake2s::Blake2sGadget;
 use ecdsa_secp256k1::EcdsaSecp256k1Gadget;
 use fixed_based_scalar_mul::FixedBaseScalarMulGadget;
 use hash_to_field::HashToFieldGadget;
+use insert_regular_merkle::InsertRegularMerkleGadget;
 use merkle_membership::MerkleMembershipGadget;
 use noirc_errors::Span;
 use noirc_frontend::hir_def::expr::HirCallExpression;
@@ -65,6 +67,7 @@ pub fn call_low_level(
         OPCODE::EcdsaSecp256k1 => EcdsaSecp256k1Gadget::call(evaluator, env, call_expr),
         OPCODE::HashToField => HashToFieldGadget::call(evaluator, env, call_expr),
         OPCODE::FixedBaseScalarMul => FixedBaseScalarMulGadget::call(evaluator, env, call_expr),
+        OPCODE::InsertRegularMerkle => InsertRegularMerkleGadget::call(evaluator, env, call_expr),
         k => {
             let message = format!("The OPCODE {} exists, however, currently the compiler does not have a concrete implementation for it", k);
             Err(RuntimeErrorKind::UnstructuredError { message }.add_span(span))
