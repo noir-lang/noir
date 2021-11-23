55057cdbeeeba153966871bec7004dba671cd0d2
guipublic <guipublic@gmail.com>
2021-11-23 16:06:39 +0000

    try to install barretenberg deps #2

--- barretenberg_dep.sh
+++ barretenberg_dep.sh
@@ -1,6 +1,1 @@
-apt-get update -y && apt-get install -y \
-  xz-utils \
-  build-essential \
-  curl \
-  && curl -SL http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz | tar -xJC . \
-  && mv clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04 /usr/local/clang_9.0.0
+apt-get update -y && apt-get install -y gcc g++ clang cmake zsh python
