327a3a24117a61d595d1d70149d3410974241721
guipublic <guipublic@gmail.com>
2021-11-23 17:51:48 +0000

    try to install barretenberg deps#4

--- barretenberg_dep.sh
+++ barretenberg_dep.sh
@@ -5,5 +5,5 @@ sudo apt-get update -y && sudo apt-get install -y \
   && curl -SL http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz | tar -xJC . \
   && mv clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04 /usr/local/clang_9.0.0
 
-sudo export PATH="/usr/local/clang_9.0.0/bin:$PATH"
-sudo export LD_LIBRARY_PATH="/usr/local/clang_9.0.0/lib:$LD_LIBRARY_PATH"
+export PATH="/usr/local/clang_9.0.0/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/clang_9.0.0/lib:$LD_LIBRARY_PATH"
