6307699cd0f5c8760ed4d5cdb6f336cb14d3d916
kevaundray <kevtheappdev@gmail.com>
2022-02-22 19:08:06 +0000

    add implementation for make_extern_call_wasm

--- crates/noir_nd/src/lib.rs
+++ crates/noir_nd/src/lib.rs
@@ -7,7 +7,7 @@ pub use noir_field::FieldElement;
 use libloading::{Library, Symbol};
 use std::{os::raw::c_char, path::Path};
 
-pub type CExternInput = (*const c_char, *const [u8; 32], *mut [u8; 32]);
+use crate::wasm_loader::CompiledModule;
 
 // The outputs vector will always be empty and will be populated by the C code
 pub fn to_c_extern_func_call(
@@ -41,7 +41,55 @@ pub fn make_extern_call_wasm<P: AsRef<Path>>(
     inputs: &Vec<[u8; 32]>,
     outputs: &mut [[u8; 32]],
 ) {
-    todo!("WASM compilation is not yet implemented")
+    let path = path.as_ref();
+    let f = std::fs::File::open(&path).unwrap();
+    let reader = std::io::BufReader::new(f);
+    let wasm_bytes = reader.buffer().to_vec();
+
+    // We store the cache in a directory one level deeper than where we found the wasm file
+    let mut path_to_cache = path.to_path_buf();
+    path_to_cache.pop(); // This pops off the file from the path
+    path_to_cache.push("/cache");
+
+    // TODO: can we do this once outside of this module
+    let mut compiled_mod = CompiledModule::new(&wasm_bytes, &path_to_cache);
+
+    let name = name.as_bytes();
+    // Send the inputs as a single flat vector to WASM and divide by 32 to get the number of items
+    let inputs: Vec<_> = inputs
+        .into_iter()
+        .map(|input| input.to_vec())
+        .flatten()
+        .collect();
+    let outputs_bytes_len: usize = outputs.iter().map(|output| output.len()).sum();
+
+    let name_ptr = compiled_mod.allocate(name);
+    let inputs_ptr = compiled_mod.allocate(&inputs);
+    let outputs_ptr = compiled_mod.allocate(&vec![0u8; outputs_bytes_len]);
+
+    use wasmer::Value;
+
+    compiled_mod.call_multiple(
+        "call_func",
+        vec![
+            &name_ptr.addr,
+            &Value::I32(name_ptr.size as i32),
+            &inputs_ptr.addr,
+            &Value::I32(inputs_ptr.size as i32),
+            &outputs_ptr.addr,
+            &Value::I32(outputs_ptr.size as i32),
+        ],
+    );
+
+    // The memory referenced by outputs ptr should now be populated
+    let populated_output = compiled_mod.read_memory(&outputs_ptr, outputs_bytes_len);
+    for (i, chunk) in populated_output.chunks(32).enumerate() {
+        outputs[i] = chunk.try_into().unwrap();
+    }
+
+    compiled_mod.free(name_ptr);
+    compiled_mod.free(inputs_ptr);
+    compiled_mod.free(outputs_ptr);
 }
 
 fn make_extern_call_dll<P: AsRef<Path>>(
