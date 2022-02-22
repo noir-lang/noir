28eaa81240fa7f5f00a4107c508ad0d3c705332c
kevaundray <kevtheappdev@gmail.com>
2022-02-22 19:06:04 +0000

    add call_func to wasm examples

--- crates/noir_nd/src/wasm_examples.rs
+++ crates/noir_nd/src/wasm_examples.rs
@@ -23,3 +23,22 @@ pub fn free(ptr: *mut u8, size: u32, alignment: u32) {
         std::alloc::dealloc(ptr, layout);
     }
 }
+
+#[wasm_bindgen]
+pub fn call_func(
+    name_ptr: *const u8,
+    name_len: usize,
+    inputs_ptr: *const u8,
+    inputs_len: usize,
+    outputs_ptr: *mut u8,
+    outputs_len: usize,
+) {
+    let name = unsafe { std::slice::from_raw_parts(name_ptr, name_len) };
+    let inputs = unsafe { std::slice::from_raw_parts(inputs_ptr, inputs_len) };
+    let outputs = unsafe { std::slice::from_raw_parts_mut(outputs_ptr, outputs_len) };
+
+    if name == String::from("modify_output").as_bytes() {
+        outputs[0..64].copy_from_slice(&vec![3u8; 32]);
+        outputs[64..128].copy_from_slice(&vec![9u8; 32]);
+    }
+}
