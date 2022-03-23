085f20c040c6608c9ed6a7edeb3b6accc4ce3a8d
Jake Fecher <jake@aztecprotocol.com>
2022-03-23 12:18:05 -0400

    Change remaining files

--- crates/noirc_frontend/src/hir/type_check/mod.rs
+++ crates/noirc_frontend/src/hir/type_check/mod.rs
@@ -9,10 +9,7 @@ mod stmt;
 use errors::TypeCheckError;
 use expr::type_check_expression;
 
-use crate::{
-    hir_def::types::Type,
-    node_interner::{FuncId, NodeInterner},
-};
+use crate::node_interner::{FuncId, NodeInterner};
 
 use self::stmt::bind_pattern;
 
@@ -38,10 +35,7 @@ pub fn type_check_func(interner: &mut NodeInterner, func_id: FuncId) -> Vec<Type
     let function_last_type = type_check_expression(interner, func_as_expr, &mut errors);
 
     // Check declared return type and actual return type
-    if !can_ignore_ret
-        && (&function_last_type != declared_return_type)
-        && function_last_type != Type::Error
-    {
+    if !can_ignore_ret && !function_last_type.matches(declared_return_type) {
         let func_span = interner.id_span(func_as_expr); // XXX: We could be more specific and return the span of the last stmt, however stmts do not have spans yet
         errors.push(TypeCheckError::TypeMismatch {
             expected_typ: declared_return_type.to_string(),
