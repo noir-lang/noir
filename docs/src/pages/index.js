15b1ab16ea8da15b29ec7748b82f9f5dc70a210f
Josh Crites <critesjosh@gmail.com>
2022-05-24 17:28:03 -0400

    update broken links

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/how-aztec-works/intro">
+            Aztec Intro
           </Link>
         </div>
       </div>
