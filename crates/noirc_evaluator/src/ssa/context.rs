30547550d60a728b9a001211be1137edc7ee342c
Jake Fecher <jake@aztecprotocol.com>
2022-07-07 09:33:52 -0400

    Fix evaluator clippy warnings

--- crates/noirc_evaluator/src/ssa/context.rs
+++ crates/noirc_evaluator/src/ssa/context.rs
@@ -183,8 +183,9 @@ impl<'a> SsaContext<'a> {
             Operation::Phi { root, block_args } => {
                 let mut s = format!("phi {}", self.node_to_string(*root));
                 for (value, block) in block_args {
-                    s += &format!(
-                        ", {} from block {}",
+                    s = format!(
+                        "{}, {} from block {}",
+                        s,
                         self.node_to_string(*value),
                         block.0.into_raw_parts().0
                     );
