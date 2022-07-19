2fd062d3c41a392e492c56558f70436e31bae178
vezenovm <mvezenov@gmail.com>
2022-07-19 14:26:19 -0400

    initial error reporting changes for nargo prove/verify

--- crates/noirc_abi/src/input_parser/toml.rs
+++ crates/noirc_abi/src/input_parser/toml.rs
@@ -4,9 +4,11 @@ use std::{collections::BTreeMap, path::Path};
 
 use super::InputValue;
 
-pub(crate) fn parse<P: AsRef<Path>>(path_to_toml: P) -> BTreeMap<String, InputValue> {
+pub(crate) fn parse<P: AsRef<Path>>(path_to_toml: P) -> Result<BTreeMap<String, InputValue>, String> {
     let path_to_toml = path_to_toml.as_ref();
-    assert!(path_to_toml.exists(), "cannot find input file at located {}", path_to_toml.display());
+    if !path_to_toml.exists() {
+        return Err(format!("cannot find input file at located {}", path_to_toml.display()));
+    }
 
     // Get input.toml file as a string
     let input_as_string = std::fs::read_to_string(path_to_toml).unwrap();
@@ -20,41 +22,49 @@ pub(crate) fn parse<P: AsRef<Path>>(path_to_toml: P) -> BTreeMap<String, InputVa
 
 /// Converts the Toml mapping to the native representation that the compiler
 /// understands for Inputs
-fn toml_map_to_field(toml_map: BTreeMap<String, TomlTypes>) -> BTreeMap<String, InputValue> {
+fn toml_map_to_field(toml_map: BTreeMap<String, TomlTypes>) -> Result<BTreeMap<String, InputValue>, String> {
     let mut field_map = BTreeMap::new();
 
     for (parameter, value) in toml_map {
         match value {
             TomlTypes::String(string) => {
+                let new_value = parse_str(&string)?;       
                 let old_value =
-                    field_map.insert(parameter.clone(), InputValue::Field(parse_str(&string)));
-                assert!(old_value.is_none(), "duplicate variable name {}", parameter);
+                    field_map.insert(parameter.clone(), InputValue::Field(new_value));
+                if !old_value.is_none() {
+                    return Err(format!("duplicate variable name {}", parameter))
+                }
             }
             TomlTypes::Integer(integer) => {
+                let new_value = parse_str(&integer.to_string())?;
                 let old_value = field_map
-                    .insert(parameter.clone(), InputValue::Field(parse_str(&integer.to_string())));
-                assert!(old_value.is_none(), "duplicate variable name {}", parameter);
+                    .insert(parameter.clone(), InputValue::Field(new_value));
+                if !old_value.is_none() {
+                    return Err(format!("duplicate variable name {}", parameter))
+                }
             }
             TomlTypes::ArrayNum(arr_num) => {
-                let array_elements: Vec<_> =
+                let array_elements: Result<Vec<_>, _> =
                     arr_num.into_iter().map(|elem_num| parse_str(&elem_num.to_string())).collect();
-
                 let old_value =
-                    field_map.insert(parameter.clone(), InputValue::Vec(array_elements));
-                assert!(old_value.is_none(), "duplicate variable name {}", parameter);
+                    field_map.insert(parameter.clone(), InputValue::Vec(array_elements?));
+                if !old_value.is_none() {
+                    return Err(format!("duplicate variable name {}", parameter))
+                }           
             }
             TomlTypes::ArrayString(arr_str) => {
-                let array_elements: Vec<_> =
+                let array_elements: Result<Vec<_>, _> =
                     arr_str.into_iter().map(|elem_str| parse_str(&elem_str)).collect();
-
                 let old_value =
-                    field_map.insert(parameter.clone(), InputValue::Vec(array_elements));
-                assert!(old_value.is_none(), "duplicate variable name {}", parameter);
+                    field_map.insert(parameter.clone(), InputValue::Vec(array_elements?));
+                if !old_value.is_none() {
+                    return Err(format!("duplicate variable name {}", parameter))
+                }            
             }
         }
     }
 
-    field_map
+    Ok(field_map)
 }
 
 #[derive(Debug, Deserialize)]
@@ -71,12 +81,17 @@ enum TomlTypes {
     ArrayString(Vec<String>),
 }
 
-fn parse_str(value: &str) -> FieldElement {
+fn parse_str(value: &str) -> Result<FieldElement, String> {
     if value.starts_with("0x") {
-        FieldElement::from_hex(value)
-            .unwrap_or_else(|| panic!("Could not parse hex value {}", value))
+        match FieldElement::from_hex(value) {
+            None => Err(format!("Could not parse hex value {}", value)),
+            Some(val) => Ok(val)
+        }
     } else {
-        let val: i128 = value.parse().expect("Expected witness values to be integers");
-        FieldElement::from(val)
+        let val: i128 = match value.parse() {
+            Err(msg) => return Err(format!("Expected witness values to be integers, provided value `{}` causes `{}` error", value, msg)),
+            Ok(parsed_val) => parsed_val
+        };
+        Ok(FieldElement::from(val))
     }
 }
