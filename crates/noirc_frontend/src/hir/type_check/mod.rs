29246a3bdd8b8f92664019ad0c6f8b2aacda3689
jfecher <jake@aztecprotocol.com>
2022-07-20 09:55:46 -0400

    Implement 'main' return (#232)
    * Implement 'main' return
    
    * Formatting
    
    * Enforce public return for main
    
    * fix clippy issues

--- crates/noirc_frontend/src/hir/type_check/mod.rs
+++ crates/noirc_frontend/src/hir/type_check/mod.rs
@@ -15,7 +15,11 @@ use self::stmt::bind_pattern;
 
 /// Type checks a function and assigns the
 /// appropriate types to expressions in a side table
-pub fn type_check_func(interner: &mut NodeInterner, func_id: FuncId) -> Vec<TypeCheckError> {
+pub fn type_check_func(
+    interner: &mut NodeInterner,
+    func_id: FuncId,
+    main_id: Option<FuncId>,
+) -> Vec<TypeCheckError> {
     // First fetch the metadata and add the types for parameters
     // Note that we do not look for the defining Identifier for a parameter,
     // since we know that it is the parameter itself
@@ -46,8 +50,16 @@ pub fn type_check_func(interner: &mut NodeInterner, func_id: FuncId) -> Vec<Type
         });
     }
 
-    // Return type cannot be public
-    if declared_return_type.is_public() {
+    // Return type cannot be public unless it is the main function in which case it must be
+    if main_id == Some(func_id) {
+        if !declared_return_type.is_public() && declared_return_type != &crate::Type::Unit {
+            errors.push(TypeCheckError::Unstructured {
+                msg: "Return type of main must be a 'pub' type since it is exposed to the verifier"
+                    .into(),
+                span: interner.expr_location(func_as_expr).span,
+            });
+        }
+    } else if declared_return_type.is_public() {
         errors.push(TypeCheckError::PublicReturnType {
             typ: declared_return_type.clone(),
             span: interner.expr_span(func_as_expr),
@@ -145,7 +157,7 @@ mod test {
             name,
             kind: FunctionKind::Normal,
             attributes: None,
-            file,
+            location,
             parameters: vec![
                 Param(Identifier(x), Type::witness(None)),
                 Param(Identifier(y), Type::witness(None)),
@@ -156,7 +168,7 @@ mod test {
         };
         interner.push_fn_meta(func_meta, func_id);
 
-        let errors = super::type_check_func(&mut interner, func_id);
+        let errors = super::type_check_func(&mut interner, func_id, None);
         assert!(errors.is_empty());
     }
 
@@ -275,7 +287,8 @@ mod test {
         }
 
         // Type check section
-        let errors = super::type_check_func(&mut interner, func_ids.first().cloned().unwrap());
+        let errors =
+            super::type_check_func(&mut interner, func_ids.first().cloned().unwrap(), None);
         assert_eq!(errors, vec![]);
     }
 }
