6e2005ef067e78bc7d838c38ee9c0622a5e9aa46
guipublic <guipublic@gmail.com>
2022-07-22 13:11:25 +0000

    Fix for circuits having unused witness

--- crates/aztec_backend/src/acvm_interop/proof_system.rs
+++ crates/aztec_backend/src/acvm_interop/proof_system.rs
@@ -17,9 +17,20 @@ impl ProofSystemCompiler for Plonk {
 
         // Add witnesses in the correct order
         // Note: The witnesses are sorted via their witness index, since we implement Ord on Witness and use a BTreeMap
+        // witness_values may not have all the witness indexes, e.g for unused witness which are not solved by the solver
+        // in that case we add a dummy value.
         let mut sorted_witness = Assignments::new();
-        for (_, value) in witness_values.iter() {
-            sorted_witness.push(*value);
+        let mut c = 0;
+        let mut i = 1;
+        while c < witness_values.len() {
+            if let Some(value) = witness_values.get(&Witness(i)) {
+                sorted_witness.push(*value);
+                c += 1;
+            } else {
+                //unused witness
+                sorted_witness.push(FieldElement::zero());
+            }
+            i += 1;
         }
 
         composer.create_proof(sorted_witness)
