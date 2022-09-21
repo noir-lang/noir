1c2217666e41414db672f76743276e0ef3e6ff71
Adrian Hamelink <adrian.hamelink@gmail.com>
2022-09-21 15:15:05 +0200

    ah/multisig-fix (https://github.com/AztecProtocol/barretenberg/pull/1316)
    * schnorr:
    - use optional instead of possibly invalid signatures
      - c_bind functions return bool
      - need to change bb.js to handle this case
    - implement proof of possession in a separate module
    - unify call to generate schnorr challenge
    - domain separation for H_reg and H_non
    - check number of outputs during "combine_sigs"
    
    * compare number of round 1 nonces with num signers
    
    * fix verification of FS challenge in both pop & sig
    
    * added comments and parametrize test over hash
    
    * - slightly clean up headers
    - use two hash functions for proper domain separation
    - more generic serialization functions
    - hash message when generating a to prevent colisions
    - reinstate hmac for PoP
    - additional verification of round1 messages
    - update documentation
    
    * fix `valid_round1_nonces` usage
    
    * revert changes to tests used for testing
    
    * - hash the group generator for Hnon and Hreg
    - update documentation
    - use prefix and suffix instead of hashing m for 'a'
    - change order of
    
    Co-authored-by: Arijit Dutta <arijitdutta67@users.noreply.github.com>
    
    * Return zero buffer if failed to create combined key.
    
    * Add more error handlings.
    
    * Change return value type.
    
    * added comments to multisig ts wrapper
    
    * fix formatting
    
    * fix domain separation
    using string_view led to the domain separator not being included in hash
    
    * addresses comments by @arijitdutta67
    
    Co-authored-by: Arijit Dutta <arijitdutta67@users.noreply.github.com>
    Co-authored-by: Leila Wang <leizciw@gmail.com>

--- circuits/cpp/barretenberg/src/aztec/crypto/schnorr/multisig.test.cpp
+++ circuits/cpp/barretenberg/src/aztec/crypto/schnorr/multisig.test.cpp
@@ -5,115 +5,129 @@
 
 using namespace barretenberg;
 
-using key_pair = crypto::schnorr::key_pair<grumpkin::fr, grumpkin::g1>;
+template <typename Hash> struct MultisigTest : public ::testing::Test {
+    using G = grumpkin::g1;
+    using Fr = grumpkin::fr;
+    using KeyPair = crypto::schnorr::key_pair<Fr, G>;
+    using multisig = crypto::schnorr::multisig<G, Hash, Blake2sHasher>;
+    using multisig_public_key = typename multisig::MultiSigPublicKey;
 
-using multisig = crypto::schnorr::multisig<grumpkin::g1, Blake2sHasher>;
-
-inline key_pair generate_account()
-{
-    key_pair account;
-    account.private_key = grumpkin::fr::random_element();
-    account.public_key = grumpkin::g1::one * account.private_key;
-    return account;
-}
-
-std::vector<multisig::MultiSigPublicKey> create_signer_pubkeys(const std::vector<key_pair>& accounts)
-{
-    // setup multisig signers
-    std::vector<multisig::MultiSigPublicKey> signer_pubkeys;
-    for (size_t i = 0; i < accounts.size(); ++i) {
-        auto& signer = accounts[i];
-        signer_pubkeys.push_back(multisig::create_multi_sig_public_key(signer));
-    }
-    return signer_pubkeys;
-}
-
-crypto::schnorr::signature create_multisig(const std::string& message,
-                                           const std::vector<key_pair>& accounts,
-                                           const bool tamper_proof_of_possession = false)
-{
-    std::vector<multisig::RoundOnePublicOutput> round1_pub;
-    std::vector<multisig::RoundOnePrivateOutput> round1_priv;
-    std::vector<multisig::RoundTwoPublicOutput> round2;
-    std::vector<multisig::MultiSigPublicKey> signer_pubkeys = create_signer_pubkeys(accounts);
-
-    if (tamper_proof_of_possession) {
-        signer_pubkeys[0].proof_of_possession =
-            crypto::schnorr::construct_signature<Blake2sHasher, grumpkin::fq, grumpkin::fr, grumpkin::g1>("test",
-                                                                                                          accounts[0]);
-    }
-    const size_t num_signers = accounts.size();
-
-    for (size_t i = 0; i < num_signers; ++i) {
-        auto [_round1_pub, _round1_priv] = multisig::construct_signature_round_1();
-        round1_pub.push_back(_round1_pub);
-        round1_priv.push_back(_round1_priv);
+    static KeyPair generate_account()
+    {
+        KeyPair account;
+        account.private_key = Fr::random_element();
+        account.public_key = G::one * account.private_key;
+        return account;
     }
 
-    for (size_t i = 0; i < num_signers; ++i) {
-        auto& signer = accounts[i];
-        round2.push_back(
-            multisig::construct_signature_round_2(message, signer, round1_priv[i], signer_pubkeys, round1_pub));
+    static std::vector<multisig_public_key> create_signer_pubkeys(const std::vector<KeyPair>& accounts)
+    {
+        // setup multisig signers
+        std::vector<multisig_public_key> signer_pubkeys;
+        for (size_t i = 0; i < accounts.size(); ++i) {
+            auto& signer = accounts[i];
+            signer_pubkeys.push_back(multisig_public_key(signer));
+        }
+        return signer_pubkeys;
     }
 
-    crypto::schnorr::signature signature = multisig::combine_signatures(message, signer_pubkeys, round1_pub, round2);
-    return signature;
-}
+    static std::optional<crypto::schnorr::signature> create_multisig(const std::string& message,
+                                                                     const std::vector<KeyPair>& accounts,
+                                                                     const bool tamper_proof_of_possession = false)
+    {
+        std::vector<typename multisig::RoundOnePublicOutput> round1_pub;
+        std::vector<typename multisig::RoundOnePrivateOutput> round1_priv;
+        std::vector<typename multisig::RoundTwoPublicOutput> round2;
+        std::vector<typename multisig::MultiSigPublicKey> signer_pubkeys = create_signer_pubkeys(accounts);
 
-TEST(multisig, verify_multi_signature_blake2s)
+        if (tamper_proof_of_possession) {
+            signer_pubkeys[0].proof_of_possession.response += 1;
+        }
+        const size_t num_signers = accounts.size();
+
+        for (size_t i = 0; i < num_signers; ++i) {
+            auto [_round1_pub, _round1_priv] = multisig::construct_signature_round_1();
+            round1_pub.push_back(_round1_pub);
+            round1_priv.push_back(_round1_priv);
+        }
+
+        for (size_t i = 0; i < num_signers; ++i) {
+            auto& signer = accounts[i];
+            if (auto round2_output = multisig::construct_signature_round_2(
+                    message, signer, round1_priv[i], signer_pubkeys, round1_pub)) {
+                round2.push_back(*round2_output);
+            }
+        }
+        return multisig::combine_signatures(message, signer_pubkeys, round1_pub, round2);
+    }
+};
+
+using HashTypes = ::testing::Types<KeccakHasher, Sha256Hasher>;
+TYPED_TEST_SUITE(MultisigTest, HashTypes);
+
+TYPED_TEST(MultisigTest, verify_multi_signature_blake2s)
 {
+    using G = grumpkin::g1;
+    using Fr = grumpkin::fr;
+    using Fq = grumpkin::fq;
+    using KeyPair = crypto::schnorr::key_pair<Fr, G>;
+    using multisig = crypto::schnorr::multisig<G, TypeParam>;
+
     std::string message = "The quick brown dog jumped over the lazy fox.";
 
     const size_t num_signers = 5;
 
-    std::vector<key_pair> accounts(num_signers);
+    std::vector<KeyPair> accounts(num_signers);
     for (auto& acct : accounts) {
-        acct = generate_account();
+        acct = this->generate_account();
     }
 
-    auto signature = create_multisig(message, accounts);
+    auto signature = this->create_multisig(message, accounts);
+    ASSERT_TRUE(signature.has_value());
 
-    bool result = crypto::schnorr::verify_signature<Blake2sHasher, grumpkin::fq, grumpkin::fr, grumpkin::g1>(
-        message, multisig::validate_and_combine_signer_pubkeys(create_signer_pubkeys(accounts)), signature);
+    auto pub_key = multisig::validate_and_combine_signer_pubkeys(this->create_signer_pubkeys(accounts));
+    ASSERT_TRUE(pub_key.has_value());
+
+    bool result = crypto::schnorr::verify_signature<Blake2sHasher, Fq, Fr, G>(message, *pub_key, *signature);
 
     EXPECT_EQ(result, true);
 }
 
-TEST(multisig, multi_signature_fails_if_proof_of_possession_invalid)
+TYPED_TEST(MultisigTest, multi_signature_fails_if_proof_of_possession_invalid)
 {
+    using G = grumpkin::g1;
+    using Fr = grumpkin::fr;
+    using KeyPair = crypto::schnorr::key_pair<Fr, G>;
+
     std::string message = "The quick brown dog jumped over the lazy fox.";
 
     const size_t num_signers = 5;
 
-    std::vector<key_pair> accounts(num_signers);
+    std::vector<KeyPair> accounts(num_signers);
     for (auto& acct : accounts) {
-        acct = generate_account();
+        acct = this->generate_account();
     }
 
-    auto signature = create_multisig(message, accounts, true);
-
-    bool result = crypto::schnorr::verify_signature<Blake2sHasher, grumpkin::fq, grumpkin::fr, grumpkin::g1>(
-        message, multisig::validate_and_combine_signer_pubkeys(create_signer_pubkeys(accounts)), signature);
-
-    EXPECT_EQ(result, false);
+    auto signature = this->create_multisig(message, accounts, true);
+    ASSERT_FALSE(signature.has_value());
 }
 
-TEST(multisig, multi_signature_fails_if_duplicates)
+TYPED_TEST(MultisigTest, multi_signature_fails_if_duplicates)
 {
+    using G = grumpkin::g1;
+    using Fr = grumpkin::fr;
+    using KeyPair = crypto::schnorr::key_pair<Fr, G>;
+
     std::string message = "The quick brown dog jumped over the lazy fox.";
 
     const size_t num_signers = 5;
 
-    std::vector<key_pair> accounts(num_signers);
+    std::vector<KeyPair> accounts(num_signers);
     for (auto& acct : accounts) {
-        acct = generate_account();
+        acct = this->generate_account();
     }
 
     accounts[2] = accounts[4]; // :o
-    auto signature = create_multisig(message, accounts);
-
-    bool result = crypto::schnorr::verify_signature<Blake2sHasher, grumpkin::fq, grumpkin::fr, grumpkin::g1>(
-        message, multisig::validate_and_combine_signer_pubkeys(create_signer_pubkeys(accounts)), signature);
-
-    EXPECT_EQ(result, false);
+    auto signature = this->create_multisig(message, accounts);
+    ASSERT_FALSE(signature.has_value());
 }
