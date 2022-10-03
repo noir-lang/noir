a2e1a36c6bda01fa14a0fe10b6d83b1e691d76c9
Josh Crites <critesjosh@gmail.com>
2022-10-03 19:39:26 -0400

    aggregate info

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -65,7 +65,7 @@ const sidebars = {
         'developers/cli',
         'developers/bridges',
         'developers/noir',
-        'developers/addresses'
+        'developers/mainnet-info'
       ]
     },
     // {
