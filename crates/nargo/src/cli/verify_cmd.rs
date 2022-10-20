11af66accc677859d6e6c2c9657c92e223450541
Tom French <15848336+TomAFrench@users.noreply.github.com>
2022-10-20 11:34:08 +0100

    Remove `setpub` (#372)
    * refactor: remove setpub
    
    * chore: remove setpub field from Verifier.toml files
    
    * refactor: remove some leftover setpubs
    
    * feat: remove setpub keyword

--- crates/nargo/src/cli/verify_cmd.rs
+++ crates/nargo/src/cli/verify_cmd.rs
@@ -6,12 +6,6 @@ use clap::ArgMatches;
 use noirc_abi::{input_parser::InputValue, Abi};
 use std::{collections::BTreeMap, path::Path, path::PathBuf};
 
-// The verifier.toml file will by default have an
-// array that will be used to add public inputs
-// which were not in the main function field.
-// This constant stores the name of that array.
-pub const RESERVED_PUBLIC_ARR: &str = "setpub";
-
 pub(crate) fn run(args: ArgMatches) -> Result<(), CliError> {
     let proof_name = args.subcommand_matches("verify").unwrap().value_of("proof").unwrap();
     let mut proof_path = std::path::PathBuf::new();
@@ -48,7 +42,7 @@ fn process_abi_with_verifier_input(
             })
             .clone();
 
-        if !value.matches_abi(param_type) && param_name != RESERVED_PUBLIC_ARR {
+        if !value.matches_abi(param_type) {
             return Err(CliError::Generic(format!("The parameters in the main do not match the parameters in the {}.toml file. \n Please check `{}` parameter. ", VERIFIER_INPUT_FILE,param_name)));
         }
 
@@ -67,22 +61,6 @@ fn process_abi_with_verifier_input(
     Ok(public_inputs)
 }
 
-use noirc_abi::{AbiFEType, AbiType};
-pub fn add_dummy_setpub_arr(abi: &mut Abi) {
-    // Add a dummy setpub array
-    // This method is used so that an empty "setpub" array is created in the verifier.toml
-    // and also so that when the verifier searches for public inputs,
-    // they also search for this "setpub"
-    let dummy_arr = AbiType::Array {
-        visibility: AbiFEType::Private,
-        length: 0,
-        typ: Box::new(AbiType::Field(AbiFEType::Private)),
-    };
-    // setpub is a keyword in Noir, so it cannot be used as a field name
-    // ensuring there will be no conflicts
-    abi.parameters.push((RESERVED_PUBLIC_ARR.into(), dummy_arr));
-}
-
 pub fn verify_with_path<P: AsRef<Path>>(
     program_dir: P,
     proof_path: P,
@@ -93,8 +71,7 @@ pub fn verify_with_path<P: AsRef<Path>>(
 
     let compiled_program = driver.into_compiled_program(backend.np_language(), show_ssa);
 
-    let mut public_abi = compiled_program.abi.clone().unwrap().public_abi();
-    add_dummy_setpub_arr(&mut public_abi);
+    let public_abi = compiled_program.abi.clone().unwrap().public_abi();
     let num_pub_params = public_abi.num_parameters();
     let mut public_inputs = BTreeMap::new();
     if num_pub_params != 0 {
