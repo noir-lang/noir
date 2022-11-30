f0a27df9fecc745061be084dc1da4593ca88dd2a
Tom French <tom@tomfren.ch>
2022-11-30 18:55:58 +0000

    refactor: rename `AbiType.size` to `AbiType.field_count`

--- crates/noirc_abi/src/lib.rs
+++ crates/noirc_abi/src/lib.rs
@@ -67,12 +67,12 @@ impl AbiType {
     }
 
     /// Returns the number of field elements required to represent the type once encoded.
-    pub fn size(&self) -> usize {
+    pub fn field_count(&self) -> usize {
         match self {
             AbiType::Field(_) | AbiType::Integer { .. } => 1,
-            AbiType::Array { visibility: _, length, typ } => typ.size() * (*length as usize),
+            AbiType::Array { visibility: _, length, typ } => typ.field_count() * (*length as usize),
             AbiType::Struct { fields, .. } => {
-                fields.iter().fold(0, |acc, (_, field_type)| acc + field_type.size())
+                fields.iter().fold(0, |acc, (_, field_type)| acc + field_type.field_count())
             }
         }
     }
