eda354660126be31e7e5a83d11714c73b4b04828
guipublic <47281315+guipublic@users.noreply.github.com>
2022-11-30 18:55:36 +0100

    Assign returned results (#540)
    * Create intermediate variable when returning a function call
    
    * Add regression test case

--- crates/noirc_evaluator/src/ssa/context.rs
+++ crates/noirc_evaluator/src/ssa/context.rs
@@ -139,11 +139,14 @@ impl SsaContext {
 
     //Display an object for debugging puposes
     fn node_to_string(&self, id: NodeId) -> String {
+        let mut result = String::new();
         if let Some(var) = self.try_get_node(id) {
-            format!("{}", var)
-        } else {
-            format!("unknown {:?}", id.0.into_raw_parts().0)
+            result = format!("{}", var);
         }
+        if result.is_empty() {
+            result = format!("unknown {:?}", id.0.into_raw_parts().0)
+        }
+        result
     }
 
     fn binary_to_string(&self, binary: &node::Binary) -> String {
