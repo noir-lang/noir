17bd75e153dd44b1ef822cefcf6c01f5e461e9a8
guipublic <guipublic@gmail.com>
2022-12-19 14:08:51 +0000

    implement to_radix

--- crates/acvm/src/lib.rs
+++ crates/acvm/src/lib.rs
@@ -133,61 +133,43 @@ pub trait PartialWitnessGenerator {
                             _ => true,
                         }
                     }
-                    Directive::Truncate { a, b, c, bit_size } => match initial_witness.get(a) {
-                        Some(val_a) => {
-                            let pow: BigUint = BigUint::one() << bit_size;
-
-                            let int_a = BigUint::from_bytes_be(&val_a.to_bytes());
-                            let int_b: BigUint = &int_a % &pow;
-                            let int_c: BigUint = (&int_a - &int_b) / &pow;
-
-                            initial_witness.insert(
-                                *b,
-                                FieldElement::from_be_bytes_reduce(&int_b.to_bytes_be()),
-                            );
-                            initial_witness.insert(
-                                *c,
-                                FieldElement::from_be_bytes_reduce(&int_c.to_bytes_be()),
-                            );
-                            false
-                        }
-                        _ => true,
-                    },
-                    Directive::Split { a, b, bit_size } => {
+                    Directive::Truncate { a, b, c, bit_size } => {
                         match Self::get_value(a, initial_witness) {
                             Some(val_a) => {
-                                let a_big = BigUint::from_bytes_be(&val_a.to_bytes());
-                                for i in 0..*bit_size {
-                                    let j = i as usize;
-                                    let v = if a_big.bit(j as u64) {
-                                        FieldElement::one()
-                                    } else {
-                                        FieldElement::zero()
-                                    };
-                                    match initial_witness.entry(b[j]) {
-                                        std::collections::btree_map::Entry::Vacant(e) => {
-                                            e.insert(v);
-                                        }
-                                        std::collections::btree_map::Entry::Occupied(e) => {
-                                            if e.get() != &v {
-                                                return GateResolution::UnsatisfiedConstrain;
-                                            }
-                                        }
-                                    }
-                                }
+                                let pow: BigUint = BigUint::one() << bit_size;
+
+                                let int_a = BigUint::from_bytes_be(&val_a.to_bytes());
+                                let int_b: BigUint = &int_a % &pow;
+                                let int_c: BigUint = (&int_a - &int_b) / &pow;
+
+                                initial_witness.insert(
+                                    *b,
+                                    FieldElement::from_be_bytes_reduce(&int_b.to_bytes_be()),
+                                );
+                                initial_witness.insert(
+                                    *c,
+                                    FieldElement::from_be_bytes_reduce(&int_c.to_bytes_be()),
+                                );
                                 false
                             }
                             _ => true,
                         }
                     }
-                    Directive::ToBytes { a, b, byte_size } => {
+                    Directive::ToRadix { a, b, radix } => {
                         match Self::get_value(a, initial_witness) {
                             Some(val_a) => {
-                                let mut a_bytes = val_a.to_bytes();
-                                a_bytes.reverse();
-                                for i in 0..*byte_size {
+                                let a_big = BigUint::from_bytes_be(&val_a.to_bytes());
+                                let a_dec = a_big.to_radix_le(*radix);
+                                if b.len() < a_dec.len() {
+                                    return GateResolution::UnsatisfiedConstrain;
+                                }
+                                for i in 0..b.len() {
                                     let i_usize = i as usize;
-                                    let v = FieldElement::from_be_bytes_reduce(&[a_bytes[i_usize]]);
+                                    let v = if i_usize < a_dec.len() {
+                                        FieldElement::from_be_bytes_reduce(&[a_dec[i_usize]])
+                                    } else {
+                                        FieldElement::zero()
+                                    };
                                     match initial_witness.entry(b[i_usize]) {
                                         std::collections::btree_map::Entry::Vacant(e) => {
                                             e.insert(v);
---
+++ crates/noirc_evaluator/src/ssa/builtin.rs
@@ -0,0 +1,75 @@
+use acvm::{acir::OPCODE, FieldElement};
+use num_bigint::BigUint;
+use num_traits::{One, Zero};
+
+use super::node::ObjectType;
+
+#[derive(Clone, Debug, Hash, Copy, PartialEq, Eq)]
+pub enum Opcode {
+    LowLevel(OPCODE),
+    ToBits,
+    ToRadix,
+}
+
+impl std::fmt::Display for Opcode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name())
+    }
+}
+
+impl Opcode {
+    pub fn lookup(op_name: &str) -> Option<Opcode> {
+        match op_name {
+            "to_bits" => Some(Opcode::ToBits),
+            "to_radix" => Some(Opcode::ToRadix),
+            _ => OPCODE::lookup(op_name).map(Opcode::LowLevel),
+        }
+    }
+
+    pub fn name(&self) -> &str {
+        match self {
+            Opcode::LowLevel(op) => op.name(),
+            Opcode::ToBits => "to_bits",
+            Opcode::ToRadix => "to_radix",
+        }
+    }
+
+    pub fn get_max_value(&self) -> BigUint {
+        match self {
+            Opcode::LowLevel(op) => {
+                match op {
+                    OPCODE::SHA256
+                    | OPCODE::Blake2s
+                    | OPCODE::Pedersen
+                    | OPCODE::FixedBaseScalarMul => BigUint::zero(), //pointers do not overflow
+                    OPCODE::SchnorrVerify | OPCODE::EcdsaSecp256k1 | OPCODE::MerkleMembership => {
+                        BigUint::one()
+                    } //verify returns 0 or 1
+                    _ => todo!("max value must be implemented for opcode {} ", op),
+                }
+            }
+            Opcode::ToBits | Opcode::ToRadix => BigUint::zero(), //pointers do not overflow
+        }
+    }
+
+    //Returns the number of elements and their type, of the output result corresponding to the OPCODE function.
+    pub fn get_result_type(&self) -> (u32, ObjectType) {
+        match self {
+            Opcode::LowLevel(op) => {
+                match op {
+                    OPCODE::AES => (0, ObjectType::NotAnObject), //Not implemented
+                    OPCODE::SHA256 => (32, ObjectType::Unsigned(8)),
+                    OPCODE::Blake2s => (32, ObjectType::Unsigned(8)),
+                    OPCODE::HashToField => (1, ObjectType::NativeField),
+                    OPCODE::MerkleMembership => (1, ObjectType::NativeField), //or bool?
+                    OPCODE::SchnorrVerify => (1, ObjectType::NativeField),    //or bool?
+                    OPCODE::Pedersen => (2, ObjectType::NativeField),
+                    OPCODE::EcdsaSecp256k1 => (1, ObjectType::NativeField), //field?
+                    OPCODE::FixedBaseScalarMul => (2, ObjectType::NativeField),
+                }
+            }
+            Opcode::ToBits => (FieldElement::max_num_bits(), ObjectType::Boolean),
+            Opcode::ToRadix => (FieldElement::max_num_bits(), ObjectType::NativeField),
+        }
+    }
+}
