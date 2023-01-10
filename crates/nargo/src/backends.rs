83d70ee55e630761707d2e6f084c3ead64da20e3
Blaine Bublitz <blaine.bublitz@gmail.com>
2023-01-10 14:24:38 -0700

    Add feature flag for wasm backend when building nargo

--- crates/nargo/src/backends.rs
+++ crates/nargo/src/backends.rs
@@ -1,8 +1,8 @@
 cfg_if::cfg_if! {
     if #[cfg(feature = "plonk_bn254")] {
-
         pub use aztec_backend::Plonk as ConcreteBackend;
-
+    } else if #[cfg(feature = "plonk_bn254_wasm")] {
+        pub use aztec_wasm_backend::Plonk as ConcreteBackend;
     } else if #[cfg(feature = "marlin")] {
         // R1CS_MARLIN_ARKWORKS
         pub use marlin_arkworks_backend::Marlin as ConcreteBackend;
