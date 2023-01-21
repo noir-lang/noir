d07934ea113fc39d1f08a1dc73eaf5c3b5348ba7
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-01-21 16:23:42 +0000

    fix clippy

--- crates/noirc_frontend/src/monomorphisation/printer.rs
+++ crates/noirc_frontend/src/monomorphisation/printer.rs
@@ -272,8 +272,8 @@ impl Display for Definition {
         match self {
             Definition::Local(id) => write!(f, "l{}", id.0),
             Definition::Function(id) => write!(f, "f{}", id.0),
-            Definition::Builtin(name) => write!(f, "{}", name),
-            Definition::LowLevel(name) => write!(f, "{}", name),
+            Definition::Builtin(name) => write!(f, "{name}"),
+            Definition::LowLevel(name) => write!(f, "{name}"),
         }
     }
 }
