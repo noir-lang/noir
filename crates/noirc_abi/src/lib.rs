edc4728a5b91498b30dd8eedf06ccf0b1242b8ab
TomAFrench <tom@tomfren.ch>
2023-01-30 15:39:28 +0000

    refactor: remove unnecessary collection

--- crates/noirc_abi/src/lib.rs
+++ crates/noirc_abi/src/lib.rs
@@ -268,18 +268,15 @@ impl Abi {
                 InputValue::Vec(field_elements)
             }
             AbiType::String { length } => {
-                let field_elements: Vec<FieldElement> =
-                    field_iterator.take(*length as usize).collect();
-
-                let string_as_slice = field_elements
-                    .iter()
+                let string_as_slice: Vec<u8> = field_iterator
+                    .take(*length as usize)
                     .map(|e| {
                         let mut field_as_bytes = e.to_be_bytes();
                         let char_byte = field_as_bytes.pop().unwrap(); // A character in a string is represented by a u8, thus we just want the last byte of the element
                         assert!(field_as_bytes.into_iter().all(|b| b == 0)); // Assert that the rest of the field element's bytes are empty
                         char_byte
                     })
-                    .collect::<Vec<_>>();
+                    .collect();
 
                 let final_string = str::from_utf8(&string_as_slice).unwrap();
 
