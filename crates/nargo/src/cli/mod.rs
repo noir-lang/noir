a80cfa16662b8a1758171a63bf6f49812d24b139
Globallager <72797635+Globallager@users.noreply.github.com>
2023-01-30 21:18:13 +0800

    cargo fmt

--- crates/nargo/src/cli/mod.rs
+++ crates/nargo/src/cli/mod.rs
@@ -54,7 +54,10 @@ pub fn start_cli() {
                 .about("Checks the constraint system for errors")
                 .arg(allow_warnings.clone()),
         )
-        .subcommand(App::new("contract").about("Generate a Solidity verifier smart contract for the program"))
+        .subcommand(
+            App::new("contract")
+                .about("Generates a Solidity verifier smart contract for the program")
+        )
         .subcommand(
             App::new("new")
                 .about("Create a new binary project")
