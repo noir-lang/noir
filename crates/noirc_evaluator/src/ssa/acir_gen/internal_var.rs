8c28ca33716db4b29c08e9c4d55fb2dae6f96041
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-02-04 19:58:17 +0000

    move helper methods into internal_var

--- crates/noirc_evaluator/src/ssa/acir_gen/internal_var.rs
+++ crates/noirc_evaluator/src/ssa/acir_gen/internal_var.rs
@@ -1,4 +1,4 @@
-use super::{expression_from_witness, witness_from_expression};
+use super::expression_from_witness;
 use crate::{ssa::node::NodeId, Evaluator};
 use acvm::{
     acir::native_types::{Expression, Witness},
@@ -198,6 +198,77 @@ impl From<FieldElement> for InternalVar {
     fn from(constant: FieldElement) -> InternalVar {
         InternalVar::from_constant(constant)
     }
+}
+
+// Returns a `Witness` if the `Expression` can be represented as a degree-1
+// univariate polynomial. Otherwise, Return None.
+//
+// Note that `Witness` is only capable of expressing polynomials of the form
+// f(x) = x and not polynomials of the form f(x) = mx+c , so this method has
+// extra checks to ensure that m=1 and c=0
+//
+// TODO: move to ACVM repo
+fn witness_from_expression(arith: &Expression) -> Option<Witness> {
+    let is_deg_one_univariate = expression_is_deg_one_univariate(arith);
+
+    if is_deg_one_univariate {
+        // If we get here, we know that our expression is of the form `f(x) = mx+c`
+        // We want to now restrict ourselves to expressions of the form f(x) = x
+        // ie where the constant term is 0 and the coefficient in front of the variable is
+        // one.
+        let coefficient = arith.linear_combinations[0].0;
+        let variable = arith.linear_combinations[0].1;
+        let constant = arith.q_c;
+
+        let coefficient_is_one = coefficient.is_one();
+        let constant_term_is_zero = constant.is_zero();
+
+        if coefficient_is_one && constant_term_is_zero {
+            return Some(variable);
+        }
+    }
+
+    None
+}
+// Returns true if highest degree term in the expression is one.
+//
+// - `mul_term` in an expression contains degree-2 terms
+// - `linear_combinations` contains degree-1 terms
+// Hence, it is sufficient to check that there are no `mul_terms`
+//
+// Examples:
+// -  f(x, y) = x + y would return true
+// -  f(x, y) = xy would return false, the degree here is 2
+// -  f(x,y) = 0 would return true, the degree is 0
+//
+// TODO: move to ACVM repo
+fn expression_is_degree_1(expression: &Expression) -> bool {
+    expression.mul_terms.is_empty()
+}
+// Returns true if the expression can be seen as a degree-1 univariate polynomial
+//
+// - `mul_terms` in an expression can be univariate, however unless the coefficient
+// is zero, it is always degree-2.
+// - `linear_combinations` contains the sum of degree-1 terms, these terms do not
+// need to contain the same variable and so it can be multivariate. However, we
+// have thus far only checked if `linear_combinations` contains one term, so this
+// method will return false, if the `Expression` has not been simplified.
+//
+// Hence, we check in the simplest case if an expression is a degree-1 univariate,
+// by checking if it contains no `mul_terms` and it contains one `linear_combination` term.
+//
+// Examples:
+// - f(x,y) = x would return true
+// - f(x,y) = x + 6 would return true
+// - f(x,y) = 2*y + 6 would return true
+// - f(x,y) = x + y would return false
+// - f(x, y) = x + x should return true, but we return false *** (we do not simplify)
+//
+// TODO move to ACVM repo
+// TODO: ACVM has a method called is_linear, we should change this to `max_degree_one`
+fn expression_is_deg_one_univariate(expression: &Expression) -> bool {
+    let has_one_univariate_term = expression.linear_combinations.len() == 1;
+    expression_is_degree_1(expression) && has_one_univariate_term
 }
 
 #[test]
