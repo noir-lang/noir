200e7ab10500d3c40e92ad249592d67ffa876464
guipublic <guipublic@gmail.com>
2023-02-09 13:46:06 +0000

    keep instructions with false predicate

--- crates/noirc_evaluator/src/ssa/node.rs
+++ crates/noirc_evaluator/src/ssa/node.rs
@@ -704,7 +704,11 @@ impl Binary {
     }
 
     fn zero_div_error(&self) -> Result<(), RuntimeError> {
-        Err(RuntimeErrorKind::Spanless("Panic - division by zero".to_string()).into())
+        if self.predicate.is_none() {
+            Err(RuntimeErrorKind::Spanless("Panic - division by zero".to_string()).into())
+        } else {
+            Ok(())
+        }
     }
 
     fn evaluate<F>(
