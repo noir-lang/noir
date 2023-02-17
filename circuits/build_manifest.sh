576c1d53b9463493424f786bb38b302880a305d2
David Banks <47112877+dbanks12@users.noreply.github.com>
2023-02-16 21:29:51 -0500

    add build-system and all relevant CI (#9)

---
+++ circuits/build_manifest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Source this file to define the PROJECTS variable.
+# PROJECT elements have structure PROJECT:WORKING_DIR:DOCKERFILE:REPO.
+#
+# TODO: Generate this from build_manifest.json
+
+# Commenting out a few projects, as the main use case is now to build the images needed to run end-to-end tests.
+# If wanting to just see if docker images actually build, you can temporarily uncomment required projects.
+PROJECTS=(
+  aztec3-circuits-wasm:./:./dockerfiles/Dockerfile.wasm-linux-clang:aztec3-circuits-wasm-linux-clang
+  aztec3-circuits-x86_64-clang:./:./dockerfiles/Dockerfile.x86_64-linux-clang:aztec3-circuits-x86_64-linux-clang
+  aztec3-circuits-x86_64-clang-assert:./:./dockerfiles/Dockerfile.x86_64-linux-clang-assert:aztec3-circuits-x86_64-linux-clang-assert
+  aztec3-circuits-x86_64-gcc:./:./dockerfiles/Dockerfile.x86_64-linux-gcc:aztec3-circuits-x86_64-linux-gcc
+)
