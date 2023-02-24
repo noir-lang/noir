423fb6db64c1e21d284222a8345994b85d191652
Adrian Hamelink <adrian@aztecprotocol.com>
2023-02-24 16:32:29 +0100

    Port timer.hpp fixes from db46ae9 (https://github.com/AztecProtocol/barretenberg/pull/147)
    * Port timer.hpp fixes from db46ae9
    
    * Comments

--- circuits/cpp/barretenberg/cpp/src/aztec/common/timer.hpp
+++ circuits/cpp/barretenberg/cpp/src/aztec/common/timer.hpp
@@ -1,43 +1,76 @@
 #pragma once
-#include <stdio.h>
+#include <cstdio>
 #include <string>
 #include <sys/resource.h>
 #include <sys/time.h>
-#include <time.h>
+#include <ctime>
 
+/**
+ * @brief Get the execution between a block of code.
+ *
+ */
 class Timer {
   private:
     struct timespec _startTime;
     struct timespec _endTime;
 
+    static constexpr int64_t NanosecondsPerSecond = 1000LL * 1000 * 1000;
+
+    /**
+     * @brief Manually sets the start time.
+     */
+    void start() { clock_gettime(CLOCK_REALTIME, &_startTime); }
+
+    /**
+     * @brief Manually sets the end time.
+     */
+    void end() { clock_gettime(CLOCK_REALTIME, &_endTime); }
+
   public:
+    /**
+     * @brief Initialize a Timer with the current time.
+     *
+     */
     Timer()
         : _endTime({})
     {
         start();
     }
 
-    void start() { clock_gettime(CLOCK_REALTIME, &_startTime); }
-
-    void end() { clock_gettime(CLOCK_REALTIME, &_endTime); }
-
-    std::string toString() const
+    /**
+     * @brief Return the number of nanoseconds elapsed since the start of the timer.
+     */
+    [[nodiscard]] int64_t nanoseconds() const
     {
-        struct timespec endTime;
+        struct timespec end;
         if (_endTime.tv_nsec == 0 && _endTime.tv_sec == 0) {
-            clock_gettime(CLOCK_REALTIME, &endTime);
+            clock_gettime(CLOCK_REALTIME, &end);
         } else {
-            endTime = _endTime;
+            end = _endTime;
         }
 
-        auto seconds = endTime.tv_sec - _startTime.tv_sec;
-        auto ns = endTime.tv_nsec - _startTime.tv_nsec;
+        int64_t nanos = (end.tv_sec - _startTime.tv_sec) * NanosecondsPerSecond;
+        nanos += (end.tv_nsec - _startTime.tv_nsec);
 
-        if (_startTime.tv_nsec > endTime.tv_nsec) { // clock underflow
-            --seconds;
-            ns += 1000000000;
-        }
+        return nanos;
+    }
 
-        return std::to_string((double)seconds + (double)ns / (double)1000000000);
+    /**
+     * @brief Return the number of seconds elapsed since the start of the timer.
+     */
+    [[nodiscard]] double seconds() const
+    {
+        int64_t nanos = nanoseconds();
+        double secs = static_cast<double>(nanos) / NanosecondsPerSecond;
+        return secs;
+    }
+
+    /**
+     * @brief Return the number of seconds elapsed since the start of the timer as a string.
+     */
+    [[nodiscard]] std::string toString() const
+    {
+        double secs = seconds();
+        return std::to_string(secs);
     }
 };
