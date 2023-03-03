71e393e6cd6232c53cd8b59c3cfbf8bb359b8778
Leila Wang <leizciw@gmail.com>
2023-03-03 17:57:42 +0000

    Move jest to dependencies.

--- yarn-project/end-to-end/scripts/start_e2e.sh
+++ yarn-project/end-to-end/scripts/start_e2e.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 set -eu
 
-echo ">> 0"
 export NODE_NO_WARNINGS=1
-echo ">> 1"
 node ${NODE_ARGS-} --openssl-legacy-provider --experimental-vm-modules $(yarn bin jest) --no-cache --runInBand $1
-echo ">> 2"
