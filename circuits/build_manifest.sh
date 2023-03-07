2699bb5d821974c8407c7b178107423ddd5d8347
David Banks <47112877+dbanks12@users.noreply.github.com>
2023-03-07 11:05:14 -0700

    move c++ into cpp/ (#36)

--- circuits/build_manifest.sh
+++ circuits/build_manifest.sh
@@ -7,8 +7,8 @@
 # Commenting out a few projects, as the main use case is now to build the images needed to run end-to-end tests.
 # If wanting to just see if docker images actually build, you can temporarily uncomment required projects.
 PROJECTS=(
-  aztec3-circuits-wasm:./:./dockerfiles/Dockerfile.wasm-linux-clang:aztec3-circuits-wasm-linux-clang
-  aztec3-circuits-wasm-assert:./:./dockerfiles/Dockerfile.wasm-linux-clang-assert:aztec3-circuits-wasm-linux-clang-assert
-  aztec3-circuits-x86_64-clang:./:./dockerfiles/Dockerfile.x86_64-linux-clang:aztec3-circuits-x86_64-linux-clang
-  aztec3-circuits-x86_64-clang-assert:./:./dockerfiles/Dockerfile.x86_64-linux-clang-assert:aztec3-circuits-x86_64-linux-clang-assert
+  aztec3-circuits-wasm:cpp:./dockerfiles/Dockerfile.wasm-linux-clang:aztec3-circuits-wasm-linux-clang
+  aztec3-circuits-wasm-assert:cpp:./dockerfiles/Dockerfile.wasm-linux-clang-assert:aztec3-circuits-wasm-linux-clang-assert
+  aztec3-circuits-x86_64-clang:cpp:./dockerfiles/Dockerfile.x86_64-linux-clang:aztec3-circuits-x86_64-linux-clang
+  aztec3-circuits-x86_64-clang-assert:cpp:./dockerfiles/Dockerfile.x86_64-linux-clang-assert:aztec3-circuits-x86_64-linux-clang-assert
 )
