1c8d355b1328f8b0242de3a8bd70f65b3433f82c
Koby <koby@aztecprotocol.com>
2023-03-17 14:29:22 +0100

    feat(nix): Rev read from file or taken from package meta

--- flake.nix
+++ flake.nix
@@ -50,35 +50,40 @@
 
         craneLib = craneLibScope.overrideToolchain rustToolchain;
 
-        commonArgs = {
-          src = ./.;
-
-          doCheck = false;
-
-          cargoCheckCommand = "true";
-          cargoBuildCommand = "cargo build --release";
-
-        };
-
-        src = pkgs.copyPathToStore ./.;
-
-        # This is a problem for now
-        GIT_COMMIT = if (self ? rev) then self.rev else "unknown";
-        GIT_DIRTY = "false";
-
-        nargo = craneLib.buildPackage ({
-          pname = "nargo";
-
-          doCheck = false;
-
-          inherit GIT_COMMIT;
-          inherit GIT_DIRTY;
-          
-          # Bindegn needs these
+        environment = {
+          # rust-bindgen needs to know the location of libclang
           LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
 
+          # We set the environment variable because requiring 2 versions of bb collide when pkg-config searches for it
+          BARRETENBERG_BIN_DIR = "${pkgs.pkgsCross.wasi32.barretenberg}/bin";
+
+          # We fetch the transcript as a dependency and provide it to the build.
+          # This is necessary because the Nix sandbox is read-only and downloading during tests would fail
+          BARRETENBERG_TRANSCRIPT = pkgs.fetchurl {
+            url = "http://aztec-ignition.s3.amazonaws.com/MAIN%20IGNITION/sealed/transcript00.dat";
+            sha256 = "sha256-ryR/d+vpOCxa3gM0lze2UVUKNUinj0nN3ScCfysN84k=";
+          };
+        };
+
+        # if file exists in git tree, commit hash wil bea read from it
+        # or unknown value will be assigned
+        COMMIT_HASH = if builtins.pathExists ./.commit
+        then builtins.readFile ./.commit
+        else "unknown";
+
+        # rev attribute meta is only available when nix build https://github.com/noir-lang/noir 
+        # is issued therefore reading this info from file is a hack for CI
+        GIT_COMMIT = if (self ? rev) then self.rev else COMMIT_HASH;
+        GIT_DIRTY = "false";
+
+        commonArgs = {
+          pname = "nargo";
+          src = ./.;
+
           nativeBuildInputs = [
+            # This provides the pkg-config tool to find barretenberg & other native libraries
             pkgs.pkg-config
+            # This provides the `lld` linker to cargo
             pkgs.llvmPackages.bintools
           ];
 
@@ -86,12 +91,54 @@
             pkgs.llvmPackages.openmp
             pkgs.barretenberg
           ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            # Need libiconv and apple Security on Darwin. See https://github.com/ipetkov/crane/issues/156
             pkgs.libiconv
+            pkgs.darwin.apple_sdk.frameworks.Security
           ];
+
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+
+          inherit GIT_COMMIT;
+          inherit GIT_DIRTY;
+
+        } // environment;
+
+        src = pkgs.copyPathToStore ./.;
+
+
+        nargo = craneLib.buildPackage ({
+
+          doCheck = true;
+          
+          cargoBuildCommand = "cargo build --release";
+
         } // commonArgs);
 
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
       in rec {
-        checks = { inherit nargo; };
+        checks = { 
+          cargo-check = craneLib.buildPackage (commonArgs // {
+            inherit cargoArtifacts;
+
+            doCheck = true;
+          });
+
+          cargo-clippy = craneLib.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+
+            cargoClippyExtraArgs = "--all-targets --workspace -- -D warnings";
+
+            doCheck = true;
+          });
+
+          cargo-test = craneLib.cargoTest (commonArgs // {
+            inherit cargoArtifacts;
+
+            cargoTestArgs = "--workspace -- --test-threads=1";
+
+            doCheck = true;
+          });
+        };
 
         packages.default = nargo;
 
@@ -102,17 +149,20 @@
 
           buildInputs = packages.default.buildInputs ;
 
+          inherit COMMIT_HASH;
+
           LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
           # Uncertain if below line is needed. dev Shell is not yet fully working
           # BINDGEN_EXTRA_CLANG_ARGS = "-I${pkgs.barretenberg}/include -isystem ${pkgs.llvmPackages.libcxx.dev}/include";
 
-          nativeBuildInputs = with pkgs; [
+          TERM = "xterm-256color";
+
+          nativeBuildInputs = with pkgs; packages.default.buildInputs ++ [
             which
             starship
             git
             cargo
             rustc
-            pkg-config            
           ];
 
           shellHook = ''
