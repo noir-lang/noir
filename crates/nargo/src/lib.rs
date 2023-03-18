6094673cdee8e268c5d1c543ad72ba6fb182aa93
Tom French <tom@tomfren.ch>
2023-03-18 19:52:42 +0000

    feat: add error type for dependency resolution

--- crates/nargo/src/lib.rs
+++ crates/nargo/src/lib.rs
@@ -2,6 +2,10 @@
 #![warn(unused_crate_dependencies, unused_extern_crates)]
 #![warn(unreachable_pub)]
 
+//! Nargo is the package manager for Noir
+//! This name was used because it sounds like `cargo` and
+//! Noir Package Manager abbreviated is npm, which is already taken.
+
 // Necessary as we use `color_eyre` in `main.rs`.
 use color_eyre as _;
 
@@ -11,15 +15,6 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use crate::errors::CliError;
-// Nargo is the package manager for Noir
-// This name was used because it sounds like `cargo` and
-// Noir Package Manager abbreviated is npm, which is already taken.
-
-fn nargo_crates() -> PathBuf {
-    dirs::home_dir().unwrap().join("nargo")
-}
-
 mod backends;
 pub mod cli;
 mod constants;
@@ -28,45 +23,36 @@ mod git;
 mod manifest;
 mod resolver;
 
+use manifest::InvalidPackageError;
+
+fn nargo_crates() -> PathBuf {
+    dirs::home_dir().unwrap().join("nargo")
+}
+
 /// Searches for the Nargo.toml file
 ///
 /// XXX: In the end, this should find the root of the project and check
 /// for the Nargo.toml file there
 /// However, it should only do this after checking the current path
 /// This allows the use of workspace settings in the future.
-fn find_package_manifest(current_path: &Path) -> Result<PathBuf, CliError> {
-    match find_file(current_path, "Nargo", "toml") {
-        Some(p) => Ok(p),
-        None => Err(CliError::Generic(format!(
-            "cannot find a Nargo.toml in {}",
-            current_path.display()
-        ))),
-    }
+fn find_package_manifest(current_path: &Path) -> Result<PathBuf, InvalidPackageError> {
+    find_file(current_path, "Nargo", "toml")
+        .ok_or_else(|| InvalidPackageError::MissingManifestFile(current_path.to_path_buf()))
 }
 
-fn lib_or_bin(current_path: &Path) -> Result<(PathBuf, CrateType), CliError> {
+fn lib_or_bin(current_path: &Path) -> Result<(PathBuf, CrateType), InvalidPackageError> {
     // A library has a lib.nr and a binary has a main.nr
     // You cannot have both.
-    let src_path = match find_dir(current_path, "src") {
-        Some(path) => path,
-        None => {
-            return Err(CliError::Generic(format!(
-                "cannot find src file in path {}",
-                current_path.display()
-            )))
-        }
-    };
+    let src_path = find_dir(current_path, "src")
+        .ok_or_else(|| InvalidPackageError::NoSourceDir(current_path.to_path_buf()))?;
+
     let lib_nr_path = find_file(&src_path, "lib", "nr");
     let bin_nr_path = find_file(&src_path, "main", "nr");
     match (lib_nr_path, bin_nr_path) {
-        (Some(_), Some(_)) => Err(CliError::Generic(
-            "package cannot contain both a `lib.nr` and a `main.nr`".to_owned(),
-        )),
+        (Some(_), Some(_)) => Err(InvalidPackageError::ContainsMultipleCrates),
         (None, Some(path)) => Ok((path, CrateType::Binary)),
         (Some(path), None) => Ok((path, CrateType::Library)),
-        (None, None) => Err(CliError::Generic(
-            "package must contain either a `lib.nr`(Library) or a `main.nr`(Binary).".to_owned(),
-        )),
+        (None, None) => Err(InvalidPackageError::ContainsZeroCrates),
     }
 }
 
