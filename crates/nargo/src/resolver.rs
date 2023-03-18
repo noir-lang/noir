0d0fb6c5f180171205f1c1a9c48af87f70363af4
Tom French <tom@tomfren.ch>
2023-03-18 23:22:29 +0000

    chore: rename `has_local_path` to `has_local_dependency`

--- crates/nargo/src/resolver.rs
+++ crates/nargo/src/resolver.rs
@@ -123,7 +123,7 @@ impl<'a> Resolver<'a> {
 
         // Resolve all transitive dependencies
         for (dependency_path, (crate_id, dep_meta)) in cached_packages {
-            if dep_meta.remote && manifest.has_local_path() {
+            if dep_meta.remote && manifest.has_local_dependency() {
                 return Err(DependencyResolutionError::RemoteDepWithLocalDep { dependency_path });
             }
             let mut new_res = Resolver::with_driver(self.driver);
