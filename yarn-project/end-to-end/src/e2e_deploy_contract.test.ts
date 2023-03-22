ea6da1aba787857cf9c362185660452b510e9dbf
Leila Wang <leizciw@gmail.com>
2023-03-22 11:14:13 +0000

    Revert "Milestone 1.1 - Deploying a contract." (#62)

--- yarn-project/end-to-end/src/e2e_deploy_contract.test.ts
+++ yarn-project/end-to-end/src/e2e_deploy_contract.test.ts
@@ -1,31 +1,7 @@
-import { AztecAddress, AztecRPCClient, ContractDeployer } from '@aztec/aztec.js';
-import { createAztecNode } from '@aztec/aztec-node';
-import { createAztecRPCServer } from '@aztec/aztec-rpc';
-import { abi } from './fixtures/test_contract.json';
+import { Aztec } from '@aztec/aztec.js';
 
 describe('e2e_deploy_contract', () => {
-  let arc: AztecRPCClient;
-  let accounts: AztecAddress[];
-
-  beforeAll(async () => {
-    const node = await createAztecNode();
-    arc = await createAztecRPCServer({ node });
-    await arc.addAccount();
-    accounts = await arc.getAccounts();
-  });
-
-  it('should deploy a contract', async () => {
-    const deployer = new ContractDeployer(abi, arc);
-    const receipt = await deployer.deploy().send().getReceipt();
-    expect(receipt).toEqual(
-      expect.objectContaining({
-        from: accounts[0],
-        status: 1,
-      }),
-    );
-
-    const { contractAddress } = receipt;
-    const bytecode = await arc.getCode(contractAddress);
-    expect(bytecode).toEqual(abi.bytecode);
+  it('should deploy a contract', () => {
+    expect(() => new Aztec()).not.toThrow();
   });
 });
