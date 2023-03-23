778634e1a2f25fb0a5228e9a5c4ba1b0fcdfee3c
Koby Hall <102518238+kobyhallx@users.noreply.github.com>
2023-03-23 13:00:15 +0100

    Update crates/wasm/src/lib.rs
    Co-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -68,11 +68,11 @@ impl Default for WASMCompileOptions {
 pub fn init_log_level(level: String) {
     // Set the static variable from Rust
     use std::sync::Once;
+    
+    let log_level = Level::from_str(&level).unwrap_or(Level::Error)
     static SET_HOOK: Once = Once::new();
     SET_HOOK.call_once(|| {
-        wasm_logger::init(wasm_logger::Config::new(
-            Level::from_str(&level).unwrap_or(Level::Error),
-        ));
+        wasm_logger::init(wasm_logger::Config::new(log_level));
     });
 }
 const BUILD_INFO: BuildInfo = BuildInfo {
