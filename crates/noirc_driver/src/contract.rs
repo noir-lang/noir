473428cfc3109f4c03e6cff7b76f995daa6ef4fa
kevaundray <kevtheappdev@gmail.com>
2023-03-27 12:19:47 +0100

    feat: Merge all contracts into one ABI (#1033)
    * add contract function type and contract method
    
    * when compiling contracts, we explicitly wrap the program as a ContractMethod along with a default FunctionType
    
    * add a `save_contract_to_file` method
    
    * add method to save and preprocess contracts
    
    * Update crates/nargo/src/cli/compile_cmd.rs
    
    Co-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>
    
    * use `function` instead of `method` to describe a function in a contract.
    
    * fix clippy
    
    * Update crates/nargo/src/cli/fs/program.rs
    
    Co-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>
    
    * Update crates/noirc_driver/src/contract.rs
    
    Co-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>
    
    * fix: update remaining instance of variable name
    
    * Format code
    
    ---------
    
    Co-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>
    Co-authored-by: jfecher <jake@aztecprotocol.com>
    Co-authored-by: Tom French <tom@tomfren.ch>

--- crates/noirc_driver/src/contract.rs
+++ crates/noirc_driver/src/contract.rs
@@ -2,10 +2,45 @@ use std::collections::BTreeMap;
 
 use crate::CompiledProgram;
 
+/// ContractFunctionType describes the types
+/// smart contract functions that are allowed.
+///
+/// Note:
+/// - All Noir programs in the non-contract context
+///   can be seen as `Secret`.
+/// - It may be possible to have `unconstrained`
+/// functions in regular Noir programs. For now
+/// we leave it as a property of only contract functions.
+#[derive(serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ContractFunctionType {
+    /// This function will be executed in a private
+    /// context.
+    Secret,
+    /// This function will be executed in a public
+    /// context.
+    Public,
+    /// A function which is non-deterministic
+    /// and does not require any constraints.
+    Unconstrained,
+}
+/// Each function in the contract will be compiled
+/// as a separate noir program.
+///
+/// A contract function unlike a regular Noir program
+/// however can have addition properties.
+/// One of these being a function type.
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct ContractFunction {
+    pub func_type: ContractFunctionType,
+    pub function: CompiledProgram,
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
 pub struct CompiledContract {
     /// The name of the contract.
     pub name: String,
     /// Each of the contract's functions are compiled into a separate `CompiledProgram`
     /// stored in this `BTreeMap`.
-    pub functions: BTreeMap<String, CompiledProgram>,
+    pub functions: BTreeMap<String, ContractFunction>,
 }
