d1bd22a087f39219198081e9f9238aa5f5b3310f
Leila Wang <leizciw@gmail.com>
2023-03-29 17:41:00 +0000

    Fix AztecAddress.

--- yarn-project/foundation/src/aztec-address/index.ts
+++ yarn-project/foundation/src/aztec-address/index.ts
@@ -1,3 +1,8 @@
 import { Fr } from '../fields/index.js';
+import { toBigIntBE } from '../index.js';
 
-export class AztecAddress extends Fr {}
+export class AztecAddress extends Fr {
+  constructor(public readonly buffer: Buffer) {
+    super(toBigIntBE(buffer));
+  }
+}
