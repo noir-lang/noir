d5ccaf1757f2c8527d3bcbc18eb6296abfd8f085
ludamad <adam.domurad@gmail.com>
2023-03-29 17:01:46 +0100

    fix: Work around localhost=>::1 (#77)

--- yarn-project/archiver/src/index.ts
+++ yarn-project/archiver/src/index.ts
@@ -8,7 +8,7 @@ export * from './archiver/index.js';
 export * from '@aztec/l2-block';
 
 const {
-  ETHEREUM_HOST = 'http://localhost:8545/',
+  ETHEREUM_HOST = 'http://127.0.0.1:8545/',
   ROLLUP_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3',
   YEETER_ADDRESS = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
 } = process.env;
