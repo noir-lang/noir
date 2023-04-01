75f1a130e8ff63cecb235af0c71b8b72199bb881
Charlie Lye <karl.lye@gmail.com>
2023-04-01 07:47:46 +0100

    Fix need to yarn install in e2e. noir/wasm deps were dev instead of prod. Much cleanup, comments. (#146)

--- bootstrap_docker.sh
+++ bootstrap_docker.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # This script builds the projects listed in build_mainifest.sh, terminating when it reaches TARGET_PROJECT.
-# If run from within a project, it will build only that project, unless ONLY_TARGET=false.
+# If run from within a project, it will build only that project, unless env var ONLY_TARGET=false.
+#
+# To build everything in build_manifest.sh:
+#   bootstrap_docker.sh
+#
+# To build all projects leading up to and including yarn-project-base:
+#   bootstrap_docker.sh yarn-project-base
+#
+# To build just end-to-end:
+#   cd yarn-project/end-to-end
+#   ../../bootstrap_docker.sh
+#
+# To build all projects leading up to and including end-to-end, from within end-to-end:
+#   cd yarn-project/end-to-end
+#   ONLY_TARGET=false ../../bootstrap_docker.sh
 
 set -e
 
@@ -9,14 +23,20 @@ COMMIT_HASH=$(git rev-parse HEAD)
 
 # If we're calling this script from within a project directory, that's the target project.
 if [ -z "$TARGET_PROJECT" ]; then
-  TARGET_PROJECT=$(git rev-parse --show-prefix)
-  if [ -n "$TARGET_PROJECT" ]; then
+  PATH_PREFIX=$(git rev-parse --show-prefix)
+  if [ -n "$PATH_PREFIX" ]; then
     # We are in a project folder.
     ONLY_TARGET=${ONLY_TARGET:-true}
-    TARGET_PROJECT=$(basename $TARGET_PROJECT)
+    TARGET_PROJECT=$(basename $PATH_PREFIX)
     cd $(git rev-parse --show-cdup)
   fi
 fi
 
 source ./build-system/scripts/setup_env $COMMIT_HASH '' mainframe_$USER $(git rev-parse --show-toplevel)
 build_local $TARGET_PROJECT $ONLY_TARGET
+
+if [ -z "$TARGET_PROJECT" ]; then
+  echo
+  echo "Success! You could now run e.g.:"
+  echo "  docker run -ti --rm aztecprotocol/end-to-end:latest e2e_zk_token_contract.test"
+fi
