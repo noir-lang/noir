d3981625687c24644239f10a425ea13e36bbb8e0
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-04-06 22:40:07 +0100

    review

--- crates/noirc_evaluator/src/ssa_refactor.rs
+++ crates/noirc_evaluator/src/ssa_refactor.rs
@@ -1,7 +1,8 @@
 //! SSA stands for Single Static Assignment
-//! This module will convert the monomorphized AST
-//! into an SSA IR. This IR will consist of basic blocks and
-//! a control flow graph.
+//! The IR presented in this module will already
+//! be in SSA form and will be used to apply
+//! conventional optimizations like Common Subexpression
+//! elimination and constant folding.
 
 mod basic_block;
 mod ir;
