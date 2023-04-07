3e1794151f4c8841be97e687bb3b69ce41fcc5c8
crites <critesjosh@gmail.com>
2023-04-07 09:20:40 -0400

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -49,10 +49,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
-        gtag: {
-          trackingID: "G-WSBTSFJCSF",
-          anonymizeIP: true,
-        }
+//         gtag: {
+//           trackingID: "G-WSBTSFJCSF",
+//           anonymizeIP: true,
+//         }
       }),
     ],
   ],
