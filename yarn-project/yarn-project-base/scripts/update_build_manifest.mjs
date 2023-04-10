9cae9f5e4707c19c59242bbedd915d50687b82fc
Santiago Palladino <santiago@aztecprotocol.com>
2023-04-10 22:08:51 +0100

    Add prepare:check step to CI (#228)
    * Add prepare:check step to CI
    
    Verifies that yarn prepare has been run in the project root.
    
    * Break deps on purpose
    
    * Copy scripts folder
    
    * Fix path to scripts
    
    * Fix path again
    
    * Relocate deps scripts
    
    * Copy full project
    
    * Hack around build system
    
    * Copy tsconfigs
    
    * Remove outdated and unrelated comment
    
    * Add missing copy
    
    * Remove extra copy
    
    * Add anoter missing copy
    
    * Revert "Break deps on purpose"
    
    This reverts commit 1f714451abf8d565b01c4c36a61ff0bc6716cd50.
    
    * Fix typo

--- yarn-project/yarn-project-base/scripts/update_build_manifest.mjs
+++ yarn-project/yarn-project-base/scripts/update_build_manifest.mjs
@@ -4,37 +4,49 @@
 
 import { existsSync, readFileSync, writeFileSync } from "fs";
 import { basename, dirname, resolve, join } from "path";
+import { cwd } from 'process';
 
 // Update build_manifest.json with new dependencies
-function updateBuildManifest(buildManifestFile, allDependencies, projectKey) {
+function updateBuildManifest(buildManifestFile, allDependencies, projectKey, options) {
   // Check if build_manifest.json exists
   if (!existsSync(buildManifestFile)) {
-    console.error(`Error: ${buildManifestFile} not found.`);
+    console.error(`Error: ${buildManifestFile} not found (cwd ${cwd()}).`);
     process.exit(2);
   }
 
   // Read build_manifest.json
-  const buildManifestData = JSON.parse(
-    readFileSync(buildManifestFile, "utf-8")
-  );
+  const buildManifestData = JSON.parse(readFileSync(buildManifestFile, 'utf-8'));
 
   if (projectKey in buildManifestData) {
     // Filter package names from dependencies that start with "@aztec/"
-    const aztecDependencies = Object.keys(allDependencies).filter(
-      (packageName) => packageName.startsWith("@aztec/")
-    );
+    const aztecDependencies = Object.keys(allDependencies).filter(packageName => packageName.startsWith('@aztec/'));
 
     // Update the "dependencies" key in the corresponding section of the buildManifestData
     // Take just the folder name component
-    buildManifestData[projectKey]["dependencies"] = aztecDependencies.map(
-      (packageName) => packageName.split("/")[1]
-    );
+    const updatedDependencies = aztecDependencies.map(packageName => packageName.split('/')[1]);
+
+    // If we are just checking, throw if dependencies don't match
+    if (options.checkOnly) {
+      const currentDependencies = buildManifestData[projectKey]['dependencies'];
+      if (
+        updatedDependencies.length !== currentDependencies.length ||
+        !updatedDependencies.reduce((ret, val, idx) => ret && val === currentDependencies[idx], true)
+      ) {
+        console.error(
+          `Dependencies for project ${projectKey} have changed and the build_manifest needs to be updated. Run yarn prepare on the yarn-project root.`,
+          `\n Current: ${JSON.stringify(currentDependencies)}`,
+          `\n Updated: ${JSON.stringify(updatedDependencies)}`,
+        );
+        process.exit(10);
+      }
+    }
+    // Otherwise, update them
+    else {
+      buildManifestData[projectKey]['dependencies'] = updatedDependencies;
+    }
 
     // Write the updated data back to build_manifest.json
-    writeFileSync(
-      buildManifestFile,
-      JSON.stringify(buildManifestData, null, 2)
-    );
+    writeFileSync(buildManifestFile, JSON.stringify(buildManifestData, null, 2));
   } else {
     console.error(`Error: '${projectKey}' not found in build_manifest.json`);
     process.exit(3);
@@ -58,6 +70,17 @@ function main() {
       process.exit(2);
     }
 
+    // Process options if any
+    const options = { checkOnly: false };
+    for (const arg of process.argv.slice(3)) {
+      if (arg === "--check") {
+        options.checkOnly = true;
+      } else {
+        console.error(`Unknown option ${arg}`);
+        process.exit(3);
+      }
+    }
+
     // Read package.json
     const packageData = JSON.parse(readFileSync(packageJsonFile, "utf-8"));
 
@@ -76,7 +99,8 @@ function main() {
     updateBuildManifest(
       buildManifestFile,
       packageData.dependencies,
-      projectKey
+      projectKey,
+      options
     );
   } catch (err) {
     console.error(`Failed updating ${resolve(process.argv[2])}`);
