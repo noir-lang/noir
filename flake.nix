8ada5fe67f2c62bd48521028e9302fedff0390cc
Blaine Bublitz <blaine.bublitz@gmail.com>
2023-04-19 15:28:11 -0700

    Update nix and direnv stuff

--- flake.nix
+++ flake.nix
@@ -2,25 +2,45 @@
   description = "Build the Noir programming language";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-22.11";
     };
 
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
+      # All of these inputs (a.k.a. dependencies) need to align with inputs we
+      # use so they use the `inputs.*.follows` syntax to reference our inputs
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
       };
     };
 
+    crane = {
+      url = "github:ipetkov/crane";
+      # All of these inputs (a.k.a. dependencies) need to align with inputs we
+      # use so they use the `inputs.*.follows` syntax to reference our inputs
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        flake-compat.follows = "flake-compat";
+        rust-overlay.follows = "rust-overlay";
+      };
+    };
+
     barretenberg = {
       url = "github:AztecProtocol/barretenberg";
+      # All of these inputs (a.k.a. dependencies) need to align with inputs we
+      # use so they use the `inputs.*.follows` syntax to reference our inputs
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
@@ -41,6 +61,8 @@
       };
 
       rustToolchain = pkgs.rust-bin.stable."1.66.0".default.override {
+        # We include rust-src to ensure rust-analyzer works.
+        # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/4
         extensions = [ "rust-src" ];
       };
 
@@ -49,6 +71,13 @@
       environment = {
         # rust-bindgen needs to know the location of libclang
         LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+
+        # Barretenberg fails if tests are run on multiple threads, so we set the test thread
+        # count to 1 throughout the entire project
+        #
+        # Note: Setting this allows for consistent behavior across build and shells, but is mostly
+        # hidden from the developer - i.e. when they see the command being run via `nix flake check`
+        RUST_TEST_THREADS = "1";
 
         # We set the environment variable because barretenberg must be compiled in a special way for wasm
         BARRETENBERG_BIN_DIR = "${pkgs.barretenberg-wasm}/bin";
@@ -62,22 +91,28 @@
       GIT_COMMIT = if (self ? rev) then self.rev else "unknown";
       GIT_DIRTY = if (self ? rev) then "false" else "true";
 
-      commonArgs = {
+      # As per https://discourse.nixos.org/t/gcc11stdenv-and-clang/17734/7 since it seems that aarch64-linux uses
+      # gcc9 instead of gcc11 for the C++ stdlib, while all other targets we support provide the correct libstdc++
+      stdenv =
+        if (pkgs.stdenv.targetPlatform.isGnu && pkgs.stdenv.targetPlatform.isAarch64) then
+          pkgs.overrideCC pkgs.llvmPackages.stdenv (pkgs.llvmPackages.clang.override { gccForLibs = pkgs.gcc11.cc; })
+        else
+          pkgs.llvmPackages.stdenv;
+
+      # Combine the environment and other configuration needed for crane to build our Rust packages
+      commonArgs = environment // {
         pname = "noir";
         # x-release-please-start-version
         version = "0.3.2";
         # x-release-please-end
 
-        # As per https://discourse.nixos.org/t/gcc11stdenv-and-clang/17734/7 since it seems that aarch64-linux uses
-        # gcc9 instead of gcc11 for the C++ stdlib, while all other targets we support provide the correct libstdc++
-        stdenv = with pkgs;
-          if (stdenv.targetPlatform.isGnu && stdenv.targetPlatform.isAarch64) then
-            overrideCC llvmPackages.stdenv (llvmPackages.clang.override { gccForLibs = gcc11.cc; })
-          else
-            llvmPackages.stdenv;
+        # Use our custom stdenv to build and test our Rust project
+        inherit stdenv;
 
         src = ./.;
 
+        # Running checks don't do much more than compiling itself and increase
+        # the build time by a lot, so we disable them throughout all our flakes
         doCheck = false;
 
         nativeBuildInputs = [
@@ -96,12 +131,9 @@
           pkgs.darwin.apple_sdk.frameworks.Security
         ];
 
-        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-
         inherit GIT_COMMIT;
         inherit GIT_DIRTY;
-
-      } // environment;
+      };
 
       # Build *just* the cargo dependencies, so we can reuse all of that work between runs
       cargoArtifacts = craneLib.buildDepsOnly commonArgs;
@@ -115,7 +147,8 @@
         cargo-clippy = craneLib.cargoClippy (commonArgs // {
           inherit cargoArtifacts;
 
-          cargoClippyExtraArgs = "--all-targets --workspace -- -D warnings";
+          # TODO(blaine): It'd be nice to include these flags when running `cargo clippy` in a devShell.
+          cargoClippyExtraArgs = "--all-targets -- -D warnings";
 
           doCheck = true;
         });
@@ -123,7 +156,8 @@
         cargo-test = craneLib.cargoTest (commonArgs // {
           inherit cargoArtifacts;
 
-          cargoTestExtraArgs = "--workspace -- --test-threads=1";
+          # TODO(blaine): It'd be nice to include this flag when running `cargo test` in a devShell.
+          cargoTestExtraArgs = "--workspace";
 
           doCheck = true;
         });
@@ -134,22 +168,24 @@
       # TODO: Look into installable apps with Nix flakes
       # apps.default = flake-utils.lib.mkApp { drv = nargo; };
 
-      devShells.default = pkgs.mkShell.override { stdenv = pkgs.llvmPackages.stdenv; } {
+      # Setup the environment to match the stdenv from `nix build` & `nix flake check`, and
+      # combine it with the environment settings, the inputs from our checks derivations,
+      # and extra tooling via `nativeBuildInputs`
+      devShells.default = pkgs.mkShell.override { inherit stdenv; } (environment // {
         inputsFrom = builtins.attrValues checks;
-
-        # buildInputs = packages.default.buildInputs;
-
-        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
 
         nativeBuildInputs = with pkgs; [
           which
           starship
           git
+          nil
+          nixpkgs-fmt
+          llvmPackages.lldb # This ensures the right lldb is in the environment for running rust-lldb
         ];
 
         shellHook = ''
           eval "$(starship init bash)"
         '';
-      };
+      });
     });
 }
