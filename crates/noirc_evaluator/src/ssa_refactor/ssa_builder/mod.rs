00eb01c597c24b853aa514bc77b12a40254401d4
Jake Fecher <jake@aztecprotocol.com>
2023-04-28 11:15:02 -0500

    Grammar

--- crates/noirc_evaluator/src/ssa_refactor/ssa_builder/mod.rs
+++ crates/noirc_evaluator/src/ssa_refactor/ssa_builder/mod.rs
@@ -221,7 +221,7 @@ impl FunctionBuilder {
             .map(|intrinsic| self.current_function.dfg.import_intrinsic(intrinsic))
     }
 
-    /// Removes the given instruction from the current block or panic otherwise.
+    /// Removes the given instruction from the current block or panics otherwise.
     pub(crate) fn remove_instruction_from_current_block(&mut self, instruction: InstructionId) {
         self.current_function.dfg[self.current_block].remove_instruction(instruction);
     }
