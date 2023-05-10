98ac822bda7208070499bbc161881612d45dd84c
sirasistant <sirasistant@gmail.com>
2023-05-10 12:01:39 +0000

    feat: adapted to heterogeneous bb calls

--- crates/noirc_evaluator/src/lib.rs
+++ crates/noirc_evaluator/src/lib.rs
@@ -13,7 +13,7 @@ pub mod ssa_refactor;
 use acvm::{
     acir::circuit::{opcodes::Opcode as AcirOpcode, Circuit, PublicInputs},
     acir::native_types::{Expression, Witness},
-    compiler::transformers::IsOpcodeSupported,
+    compiler::{optimizers::simplify::CircuitSimplifier, transformers::IsOpcodeSupported},
     Language,
 };
 use errors::{RuntimeError, RuntimeErrorKind};
@@ -83,6 +83,7 @@ pub fn create_circuit(
         opcodes,
         ..
     } = evaluator;
+    let simplifier = CircuitSimplifier::new(current_witness_index);
     let optimized_circuit = acvm::compiler::compile(
         Circuit {
             current_witness_index,
@@ -92,6 +93,7 @@ pub fn create_circuit(
         },
         np_language,
         is_opcode_supported,
+        &simplifier,
     )
     .map_err(|_| RuntimeErrorKind::Spanless(String::from("produced an acvm compile error")))?;
 
