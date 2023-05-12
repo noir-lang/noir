da473685524fc6e5e17f9c3eb95116378ac41fb8
Blaine Bublitz <blaine.bublitz@gmail.com>
2023-05-12 06:23:19 +0100

    chore!: Update to acvm 0.11.0 (#1322)
    * chore!: Update to acvm 0.11.0
    
    * chore: Update nargo core to return backend errors
    
    * chore: Make CliError generic over a Backend
    
    chore!: Split filesystem errors off from CliError
    
    chore!: Make all run functions take a backend and pass it from the CLI entry
    
    * update to latest commit
    
    * chore: replace long `Backend` type parameters with `B`
    
    * fix grep problems
    
    * remove unneeded import
    
    * latest master
    
    * chore: update `acvm-backend-barretenberg` to 0.1.0 commit
    
    * chore: use `try_vecmap` in old `vecmap` locations
    
    * chore: add missing `?`
    
    * official release of backend
    
    ---------
    
    Co-authored-by: Tom French <tom@tomfren.ch>

--- crates/nargo/src/ops/codegen_verifier.rs
+++ crates/nargo/src/ops/codegen_verifier.rs
@@ -1,10 +1,8 @@
 use acvm::SmartContract;
 
-use crate::NargoError;
-
-pub fn codegen_verifier(
-    backend: &impl SmartContract,
+pub fn codegen_verifier<B: SmartContract>(
+    backend: &B,
     verification_key: &[u8],
-) -> Result<String, NargoError> {
-    Ok(backend.eth_contract_from_vk(verification_key))
+) -> Result<String, B::Error> {
+    backend.eth_contract_from_vk(verification_key)
 }
