a44592065f923fb00dccbf7b336a3e7429f674f4
Joss <joss@aztecprotocol.com>
2023-05-25 17:52:55 +0100

    chore(ssa refactor): mv GeneratedAcir into file

---
+++ crates/noirc_evaluator/src/ssa_refactor/acir_gen/generated_acir.rs
@@ -0,0 +1,20 @@
+//! `GeneratedAcir` is constructed as part of the `acir_gen` pass to accumulate all of the ACIR
+//! program as it is being converted from SSA form.
+use acvm::acir::{circuit::Opcode, native_types::Witness};
+
+/// The output of the Acir-gen pass
+pub(crate) struct GeneratedAcir {
+    /// The next witness index that may be declared.
+    ///
+    /// Equivalent to acvm::acir::circuit::Circuit's field of the same name.
+    pub(crate) current_witness_index: u32,
+
+    /// The opcodes of which the compiled ACIR will comprise.
+    pub(crate) opcodes: Vec<Opcode>,
+
+    /// All witness indices that comprise the final return value of the program
+    ///
+    /// Note: This may contain repeated indices, which is necessary for later mapping into the
+    /// abi's return type.
+    pub(crate) return_witnesses: Vec<Witness>,
+}
--- crates/noirc_evaluator/src/ssa_refactor/acir_gen/mod.rs
+++ crates/noirc_evaluator/src/ssa_refactor/acir_gen/mod.rs
@@ -1,28 +1,14 @@
 //! This file holds the pass to convert from Noir's SSA IR to ACIR.
-use acvm::acir::{circuit::Opcode, native_types::Witness};
+
+pub(crate) use self::generated_acir::GeneratedAcir;
 
 use super::ssa_gen::Ssa;
+
+mod generated_acir;
 
 /// Context struct for the acir generation pass.
 /// May be similar to the Evaluator struct in the current SSA IR.
 struct Context {}
-
-/// The output of the Acir-gen pass
-pub(crate) struct GeneratedAcir {
-    /// The next witness index that may be declared.
-    ///
-    /// Equivalent to acvm::acir::circuit::Circuit's field of the same name.
-    pub(crate) current_witness_index: u32,
-
-    /// The opcodes of which the compiled ACIR will comprise.
-    pub(crate) opcodes: Vec<Opcode>,
-
-    /// All witness indices that comprise the final return value of the program
-    ///
-    /// Note: This may contain repeated indices, which is necessary for later mapping into the
-    /// abi's return type.
-    pub(crate) return_witnesses: Vec<Witness>,
-}
 
 impl Ssa {
     pub(crate) fn into_acir(self, _main_param_array_lengths: &[usize]) -> GeneratedAcir {
