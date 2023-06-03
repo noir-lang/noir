a7268281a88b2e98f71938f41aacd0d047b458a5
vicyyn <nader_ouerdiane@hotmail.com>
2023-06-03 14:53:37 +0100

    chore: Implement checksum for cached CRS

--- crates/nargo_cli/src/cli/fs/common_reference_string.rs
+++ crates/nargo_cli/src/cli/fs/common_reference_string.rs
@@ -1,12 +1,17 @@
-use std::{env, path::PathBuf};
+use std::{env, path::PathBuf, sync::atomic::AtomicU32};
 
 use acvm::{acir::circuit::Circuit, CommonReferenceString};
 
 use super::{create_named_dir, write_to_file};
 
+use crc::{Crc, CRC_32_ISCSI};
+
 // TODO(#1388): pull this from backend.
 const BACKEND_IDENTIFIER: &str = "acvm-backend-barretenberg";
 const TRANSCRIPT_NAME: &str = "common-reference-string.bin";
+const CASTAGNOLI: Crc<u32> = Crc::<u32>::new(&CRC_32_ISCSI);
+
+static CRS_CHECKSUM: AtomicU32 = AtomicU32::new(0);
 
 fn common_reference_string_location() -> PathBuf {
     let cache_dir = match env::var("NARGO_BACKEND_CACHE_DIR") {
@@ -19,9 +24,24 @@ fn common_reference_string_location() -> PathBuf {
 pub(crate) fn read_cached_common_reference_string() -> Vec<u8> {
     let crs_path = common_reference_string_location();
 
-    // TODO(#1390): Implement checksum
     match std::fs::read(crs_path) {
-        Ok(common_reference_string) => common_reference_string,
+        Ok(common_reference_string) => {
+            let loaded_checksum = CRS_CHECKSUM.load(std::sync::atomic::Ordering::Acquire);
+            let checksum = CASTAGNOLI.checksum(&common_reference_string);
+            if loaded_checksum == 0 {
+                CRS_CHECKSUM
+                    .compare_exchange_weak(
+                        loaded_checksum,
+                        checksum,
+                        std::sync::atomic::Ordering::Acquire,
+                        std::sync::atomic::Ordering::Relaxed,
+                    )
+                    .unwrap();
+            } else if checksum != loaded_checksum {
+                panic!("Common reference string checksum mismatch");
+            }
+            return common_reference_string;
+        }
         Err(_) => vec![],
     }
 }
@@ -43,7 +63,14 @@ pub(crate) fn update_common_reference_string<B: CommonReferenceString>(
         backend.update_common_reference_string(common_reference_string.to_vec(), circuit)
     };
 
-    runtime.block_on(fut)
+    match runtime.block_on(fut) {
+        Ok(common_reference_string) => {
+            let checksum = CASTAGNOLI.checksum(&common_reference_string);
+            CRS_CHECKSUM.store(checksum, std::sync::atomic::Ordering::Release);
+            return Ok(common_reference_string);
+        }
+        Err(e) => return Err(e),
+    };
 }
 
 pub(crate) fn write_cached_common_reference_string(common_reference_string: &[u8]) {
@@ -52,4 +79,6 @@ pub(crate) fn write_cached_common_reference_string(common_reference_string: &[u8
     create_named_dir(crs_path.parent().unwrap(), "crs");
 
     write_to_file(common_reference_string, &crs_path);
+    let checksum = CASTAGNOLI.checksum(common_reference_string);
+    CRS_CHECKSUM.store(checksum, std::sync::atomic::Ordering::Release);
 }
