fecdfeebb3246b3663e7d47b687a80a5d4daa534
kevaundray <kevtheappdev@gmail.com>
2023-06-11 19:28:49 +0000

    memory now returns an allocation struct

--- crates/noirc_evaluator/src/brillig/brillig_ir/memory.rs
+++ crates/noirc_evaluator/src/brillig/brillig_ir/memory.rs
@@ -1,3 +1,4 @@
+use acvm::acir::brillig_vm::Value;
 /// Simple memory allocator for brillig
 /// For now it just tracks a free memory pointer
 /// Will probably get smarter in the future
@@ -6,10 +7,34 @@ pub(crate) struct BrilligMemory {
     free_mem_pointer: usize,
 }
 
+/// A number representing a memory address
+#[derive(Default, Clone, Copy)]
+pub struct MemoryAddress(usize);
+
+impl From<MemoryAddress> for Value {
+    fn from(value: MemoryAddress) -> Self {
+        Value::from(value.0)
+    }
+}
+
+/// Allocation details metadata about an array allocation
+pub(crate) struct Allocation {
+    /// This is the starting address for an allocation
+    /// and can also be seen as the array ptr
+    pub(crate) start_address: MemoryAddress,
+    /// This is the address of the last item allocated for
+    /// this array.
+    pub(crate) end_address: MemoryAddress,
+}
+
 impl BrilligMemory {
-    pub(crate) fn allocate(&mut self, size: usize) -> usize {
+    /// Allocate an array of size `size`
+    pub(crate) fn allocate(&mut self, size: usize) -> Allocation {
         let start = self.free_mem_pointer;
         self.free_mem_pointer += size;
-        start
+        Allocation {
+            start_address: MemoryAddress(start),
+            end_address: MemoryAddress(self.free_mem_pointer),
+        }
     }
 }
