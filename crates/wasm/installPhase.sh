d15311f16af1c4c6477ee486fc1767c87885a36d
jonybur <jobur93@gmail.com>
2023-06-14 08:49:28 +0000

    Remove package version extraction from cargo.toml

--- crates/wasm/installPhase.sh
+++ crates/wasm/installPhase.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# Extract version from Cargo.toml using toml2json
-PACKAGE_VERSION=$(toml2json < Cargo.toml | jq -r .workspace.package.version)
-if [ -z "$PACKAGE_VERSION" ]; then
-    echo "Could not extract version from Cargo.toml"
-    exit 1
-fi
-PACKAGE_VERSION+=$VERSION_APPENDIX
-
-echo PACKAGE_VERSION=$PACKAGE_VERSION
-
 mkdir -p $out
 cp README.md $out/
 cp -r ./pkg/* $out/
