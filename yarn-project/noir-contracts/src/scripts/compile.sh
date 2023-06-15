8ac2eb54db049139b9f354f63b323bbee4614894
Maddiaa <47148561+Maddiaa0@users.noreply.github.com>
2023-06-15 07:25:33 -0700

    fix(noir): remove jq command, as aztec3-hacky no longer outputs pk-vk (#860)

--- yarn-project/noir-contracts/src/scripts/compile.sh
+++ yarn-project/noir-contracts/src/scripts/compile.sh
@@ -13,13 +13,6 @@ for CONTRACT_NAME in "$@"; do
     nargo compile main --contracts
   fi
 
-  # Trim the output of the noir json, removing unneeded fields to make it small enough to be processed by copy_output.ts
-  # This is a workaround until noir output files get smaller
-  FILENAME=$(ls target/*.json)
-  echo "Trimming output for $FILENAME"
-  jq -c '.functions[] |= del(.proving_key, .verification_key)' $FILENAME > temp.json 
-  mv temp.json $FILENAME
-
   cd $ROOT
   echo "Copying output for $CONTRACT_NAME"
   NODE_OPTIONS=--no-warnings yarn ts-node --esm src/scripts/copy_output.ts $CONTRACT_NAME
