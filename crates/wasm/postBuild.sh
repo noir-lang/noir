6b628b66dade49c647120b91d1fdb41a8515d8f5
jonybur <jobur93@gmail.com>
2023-06-16 17:34:39 +0000

    Change browser for web on path

--- crates/wasm/postBuild.sh
+++ crates/wasm/postBuild.sh
@@ -3,7 +3,7 @@
 # TODO: Handle the wasm target being built in release mode
 WASM_BINARY=./target/wasm32-unknown-unknown/release/${pname}.wasm
 NODE_WASM=./pkg/nodejs/${pname}_bg.wasm
-BROWSER_WASM=./pkg/browser/${pname}_bg.wasm
+BROWSER_WASM=./pkg/web/${pname}_bg.wasm
 
 wasm-bindgen $WASM_BINARY --out-dir ./pkg/nodejs --typescript --target nodejs
 wasm-bindgen $WASM_BINARY --out-dir ./pkg/web --typescript --target web
