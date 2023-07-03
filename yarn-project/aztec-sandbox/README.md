c1ff3a90fd7bd40cfe64dc3866ba636be512363d
spypsy <spypsy@users.noreply.github.com>
2023-07-03 17:14:05 +0300

    add Sandbox Readme + improve token example (#918)
    * add Sandbox Readme + improve token example
    
    * Update yarn-project/aztec-sandbox/README.md
    
    Co-authored-by: Maddiaa <47148561+Maddiaa0@users.noreply.github.com>
    
    * Add system requirements to readme
    
    * update readme for wasm bundle
    
    ---------
    
    Co-authored-by: Maddiaa <47148561+Maddiaa0@users.noreply.github.com>

--- yarn-project/aztec-sandbox/README.md
+++ yarn-project/aztec-sandbox/README.md
@@ -1,1 +1,54 @@
 # aztec-sandbox
+
+Aztec Sandbox is a package that allows for a simple development environment on Aztec stack. It creates an Aztec RPC server that listens for HTTP requests on `localhost:8080` by default. When started, it deploys all necessary L1 Aztec contracts and then starts listening for RPC requests.
+
+## How to run:
+
+### Docker Compose
+
+The easiesty way to run is by using `docker compose up`. This will create two containers:
+
+1. The sandbox listening on port `8080`
+2. An anvil instance listening on port `8545`
+
+### Node Server
+
+You can also run it as a standalone node server with:
+
+```sh
+yarn start
+```
+
+It will look for a local Ethereum RPC to talk to but you can change this with the `ETHEREUM_HOST` environment variable.
+
+You'll also need to run `./bootstrap.sh` in the `circuits/cpp` directory in order to build the WASM binaries.
+
+## Examples
+
+The package also includes 2 examples. There are some system prerequisites that you will need to run these locally:
+
+- [nvm](https://github.com/nvm-sh/nvm)
+  - Node version > 18
+- [yarn](https://yarnpkg.com/)
+- [jq](https://jqlang.github.io/jq/download/)
+
+Before running locally you'll need to:
+
+- Head to `l1-contracts` directory and run `./bootstrap.sh`
+- Then go to `yarn-project and run:
+  - `yarn install`
+  - `yarn build`
+    And you should be good to go!
+
+From the `aztec-sandbox` directory, you can run the two existing examples:
+
+- Deployment, mint and transfer on an Aztec ZK Token
+  - `yarn run:example:token`
+- An L1 / L2 uniswap token trade.
+  - `yarn run:example:uniswap`
+  - To run this example, you need to use the `docker-compose-fork.yml` configuration.
+
+## Publishing
+
+This package is set-up to be published on dockerhub by CI whenever there's a merge on `master` branch.
+It's published under the tag `aztecprotocol/aztec-sandbox:master`.
---
+++ yarn-project/aztec-sandbox/src/examples/zk_contract.ts
@@ -0,0 +1,99 @@
+import {
+  AztecRPC,
+  Contract,
+  ContractDeployer,
+  createAccounts,
+  createAztecRpcClient,
+  pointToPublicKey,
+} from '@aztec/aztec.js';
+import { AztecAddress, Point } from '@aztec/circuits.js';
+import { createDebugLogger } from '@aztec/foundation/log';
+import { ZkTokenContractAbi } from '@aztec/noir-contracts/examples';
+import { L2BlockL2Logs } from '@aztec/types';
+
+const logger = createDebugLogger('aztec:http-rpc-client');
+
+export const privateKey = Buffer.from('ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', 'hex');
+
+const url = 'http://localhost:8080';
+
+const aztecRpcClient = createAztecRpcClient(url);
+
+const INITIAL_BALANCE = 333n;
+const SECONDARY_AMOUNT = 33n;
+
+/**
+ * Deploys the ZK Token contract.
+ * @param pubKeyPoint - The public key Point that the initial balance will belong to.
+ * @returns An Aztec Contract object with the zk token's ABI.
+ */
+async function deployZKContract(pubKeyPoint: Point) {
+  logger('Deploying L2 contract...');
+  const deployer = new ContractDeployer(ZkTokenContractAbi, aztecRpcClient);
+  const tx = deployer.deploy(INITIAL_BALANCE, pointToPublicKey(pubKeyPoint)).send();
+  const receipt = await tx.getReceipt();
+  const contract = new Contract(receipt.contractAddress!, ZkTokenContractAbi, aztecRpcClient);
+  await tx.isMined();
+  await tx.getReceipt();
+  logger('L2 contract deployed');
+  return contract;
+}
+
+/**
+ * Gets a user's balance from a ZK Token contract.
+ * @param contract - The ZK Token contract.
+ * @param ownerKey - Balance owner's public key.
+ * @param ownerAddress - Balance owner's Aztec Address.
+ * @returns The owner's current balance of the token.
+ */
+async function getBalance(contract: Contract, ownerKey: Point, ownerAddress: AztecAddress) {
+  const [balance] = await contract.methods.getBalance(pointToPublicKey(ownerKey)).view({ from: ownerAddress });
+  return balance;
+}
+
+/**
+ * Main function.
+ */
+async function main() {
+  logger('Running ZK contract test on HTTP interface.');
+
+  const [[ownerAddress, ownerPubKeyPoint], [address2, address2PubKeyPoint]] = await createAccounts(
+    aztecRpcClient,
+    privateKey,
+    2,
+  );
+  logger(`Created account ${ownerAddress.toString()} with public key ${ownerPubKeyPoint.toString()}`);
+  const zkContract = await deployZKContract(ownerPubKeyPoint);
+  const accounts = await aztecRpcClient.getAccounts();
+  logger(`Created ${accounts.length} accounts`);
+  const balance1 = await getBalance(zkContract, ownerPubKeyPoint, ownerAddress);
+  logger(`Initial owner balance: ${balance1}`);
+
+  // Mint more tokens
+  logger(`Minting ${SECONDARY_AMOUNT} more coins`);
+  const mintTx = zkContract.methods.mint(SECONDARY_AMOUNT, ownerPubKeyPoint).send({ from: ownerAddress });
+  await mintTx.isMined(0, 0.5);
+  const balanceAfterMint = await getBalance(zkContract, ownerPubKeyPoint, ownerAddress);
+  logger(`Owner's balance is now: ${balanceAfterMint}`);
+
+  // Perform a transfer
+  logger(`Transferring ${SECONDARY_AMOUNT} tokens from owner to another account.`);
+  const transferTx = zkContract.methods
+    .transfer(SECONDARY_AMOUNT, ownerPubKeyPoint, address2PubKeyPoint)
+    .send({ from: ownerAddress });
+  await transferTx.isMined(0, 0.5);
+  const balanceAfterTransfer = await getBalance(zkContract, ownerPubKeyPoint, ownerAddress);
+  const receiverBalance = await getBalance(zkContract, address2PubKeyPoint, address2);
+  logger(`Owner's balance is now ${balanceAfterTransfer}`);
+  logger(`The transfer receiver's balance is ${receiverBalance}`);
+}
+
+main()
+  .then(() => {
+    logger('Finished running successfuly.');
+    process.exit(0);
+  })
+  .catch(err => {
+    logger('Error in main fn: ', err);
+    process.exit(1);
+  });
