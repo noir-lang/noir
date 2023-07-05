5ac384e3f3382b177a6b4bfe75f1a59a9e187e0f
Blaine Bublitz <blaine.bublitz@gmail.com>
2023-07-05 10:19:57 -0700

    append the contents

--- crates/wasm/installPhase.sh
+++ crates/wasm/installPhase.sh
@@ -5,5 +5,5 @@ cp ./crates/wasm/README.md $out/
 cp ./crates/wasm/package.json $out/
 cp -r ./pkg/* $out/
 
-echo "## Tracking" > $out/README.md
-echo "Built from [noir-lang/noir@$GIT_COMMIT](https://github.com/noir-lang/noir/tree/$GIT_COMMIT)" > $out/README.md
+echo "## Tracking" >> $out/README.md
+echo "Built from [noir-lang/noir@$GIT_COMMIT](https://github.com/noir-lang/noir/tree/$GIT_COMMIT)" >> $out/README.md
