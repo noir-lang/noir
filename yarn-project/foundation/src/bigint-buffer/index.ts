6e0d658599bc2b8ddb3fa893ae81e4508688e4d0
David Banks <47112877+dbanks12@users.noreply.github.com>
2023-07-06 15:16:18 -0400

    fix(foundation): should error on attempt to convert negative bigint to buffer (#981)

--- yarn-project/foundation/src/bigint-buffer/index.ts
+++ yarn-project/foundation/src/bigint-buffer/index.ts
@@ -33,6 +33,7 @@ export function toBigIntBE(buf: Buffer): bigint {
  * @returns A little-endian buffer representation of num.
  */
 export function toBufferLE(num: bigint, width: number): Buffer {
+  if (num < BigInt(0)) throw new Error(`Cannot convert negative bigint ${num.toString()} to buffer with toBufferLE.`);
   const hex = num.toString(16);
   const buffer = Buffer.from(hex.padStart(width * 2, '0').slice(0, width * 2), 'hex');
   buffer.reverse();
@@ -46,6 +47,7 @@ export function toBufferLE(num: bigint, width: number): Buffer {
  * @returns A big-endian buffer representation of num.
  */
 export function toBufferBE(num: bigint, width: number): Buffer {
+  if (num < BigInt(0)) throw new Error(`Cannot convert negative bigint ${num.toString()} to buffer with toBufferBE.`);
   const hex = num.toString(16);
   const buffer = Buffer.from(hex.padStart(width * 2, '0').slice(0, width * 2), 'hex');
   if (buffer.length > width) throw new Error(`Number ${num.toString(16)} does not fit in ${width}`);
