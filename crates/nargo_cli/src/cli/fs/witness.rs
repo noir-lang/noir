a23f7d7bb3b954f1e4296d1eeca40c14cdf37454
vezenovm <mvezenov@gmail.com>
2023-07-06 14:45:26 +0000

    save witness in correct format that bb.js expects and updates acvm-backend-barretenberg dep to show all old CI passing

--- crates/nargo_cli/src/cli/fs/witness.rs
+++ crates/nargo_cli/src/cli/fs/witness.rs
@@ -6,14 +6,17 @@ use super::{create_named_dir, write_to_file};
 use crate::{constants::WITNESS_EXT, errors::FilesystemError};
 
 pub(crate) fn save_witness_to_dir<P: AsRef<Path>>(
-    witness: WitnessMap,
+    witnesses: WitnessMap,
     witness_name: &str,
     witness_dir: P,
 ) -> Result<PathBuf, FilesystemError> {
     create_named_dir(witness_dir.as_ref(), "witness");
     let witness_path = witness_dir.as_ref().join(witness_name).with_extension(WITNESS_EXT);
 
-    let buf: Vec<u8> = witness.try_into()?;
+    let mut buf = Vec::new();
+    for (_, value) in witnesses {
+        buf.extend_from_slice(&value.to_be_bytes());
+    }
 
     write_to_file(buf.as_slice(), &witness_path);
 
