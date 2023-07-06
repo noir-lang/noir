b1595d79dcb1862ec0adbf7a5955bb50c7c6188e
jfecher <jake@aztecprotocol.com>
2023-07-06 17:13:23 +0200

    Update crates/noirc_frontend/src/hir/type_check/expr.rs

--- crates/noirc_frontend/src/hir/type_check/expr.rs
+++ crates/noirc_frontend/src/hir/type_check/expr.rs
@@ -309,7 +309,7 @@ impl<'interner> TypeChecker<'interner> {
 
         index_type.unify(&Type::polymorphic_integer(self.interner), span, &mut self.errors, || {
             TypeCheckError::TypeMismatch {
-                expected_typ: "Integer".to_owned(),
+                expected_typ: "an integer".to_owned(),
                 expr_typ: index_type.to_string(),
                 expr_span: span,
             }
