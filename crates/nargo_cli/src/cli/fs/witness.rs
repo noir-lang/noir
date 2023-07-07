ee5986e1039d91f3c688c4a736b9469fea8c81cc
Charlie Lye <karl.lye@gmail.com>
2023-07-07 08:45:26 +0000

    Fix witness. Exclude vk/pk.

--- crates/nargo_cli/src/cli/fs/witness.rs
+++ crates/nargo_cli/src/cli/fs/witness.rs
@@ -13,7 +13,23 @@ pub(crate) fn save_witness_to_dir<P: AsRef<Path>>(
     create_named_dir(witness_dir.as_ref(), "witness");
     let witness_path = witness_dir.as_ref().join(witness_name).with_extension(WITNESS_EXT);
 
-    let buf: Vec<u8> = serialize_witness_map(witnesses)?;
+    let mut buf: Vec<u8> = Vec::new();
+    #[cfg(not(feature = "flat_witness"))]
+    {
+        buf = serialize_witness_map(witnesses)?;
+    }
+    #[cfg(feature = "flat_witness")]
+    {
+        let mut counter = 1;
+        for (index, value) in witnesses {
+            while counter < index.witness_index() {
+                buf.extend(vec![0; 32]);
+                counter += 1;
+            }
+            buf.extend_from_slice(&value.to_be_bytes());
+            counter += 1;
+        }
+    }
 
     write_to_file(buf.as_slice(), &witness_path);
 
