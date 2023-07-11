7245d7ccee8606dad89d29cb02a7eced75d99b7c
Maxim Vezenov <mvezenov@gmail.com>
2023-07-11 16:35:44 +0100

    Update crates/nargo/src/ops/execute.rs
    Co-authored-by: jfecher <jake@aztecprotocol.com>

--- crates/nargo/src/ops/execute.rs
+++ crates/nargo/src/ops/execute.rs
@@ -73,11 +73,7 @@ fn execute_foreign_call(foreign_call: &ForeignCallWaitInfo) -> ForeignCallResult
         "get_reverse_number_sequence" => {
             let sequence_length: u128 = foreign_call.inputs[0][0].to_field().to_u128();
 
-            let mut sequence = Vec::new();
-            for i in (0..sequence_length).rev() {
-                sequence.push(Value::from(i));
-            }
-            sequence.into()
+            vecmap(0..sequence_length).rev(), |i| Value::from(i)).into()
         }
         _ => panic!("unexpected foreign call type"),
     }
