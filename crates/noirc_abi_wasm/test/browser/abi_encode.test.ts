8d615e4e34da9893c154a6a56f7de8739a7c07d1
TomAFrench <tom@tomfren.ch>
2023-07-18 12:22:58 +0000

    chore: add test framework for new abi package

---
+++ crates/noirc_abi_wasm/test/browser/abi_encode.test.ts
@@ -0,0 +1,24 @@
+import { expect } from "@esm-bundle/chai";
+import initACVM, { abiEncode, abiDecode, WitnessMap } from "../../../../result/";
+import { DecodedInputs } from "../types";
+
+beforeEach(async () => {
+  await initACVM();
+});
+
+it("recovers original inputs when abi encoding and decoding", async () => {
+  const { abi, inputs } = await import("../shared/abi_encode");
+
+  const initial_witness: WitnessMap = abiEncode(abi, inputs, null);
+  const decoded_inputs: DecodedInputs = abiDecode(abi, initial_witness);
+
+
+  expect(BigInt(decoded_inputs.inputs.foo)).to.be.equal(BigInt(inputs.foo));
+  expect(BigInt(decoded_inputs.inputs.bar[0])).to.be.equal(
+    BigInt(inputs.bar[0])
+  );
+  expect(BigInt(decoded_inputs.inputs.bar[1])).to.be.equal(
+    BigInt(inputs.bar[1])
+  );
+  expect(decoded_inputs.return_value).to.be.null;
+});
