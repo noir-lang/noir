c468e176ea07d95b377e59a7fa6c2d658edc4e0b
TomAFrench <tom@tomfren.ch>
2023-07-18 14:52:15 +0000

    chore: refactor scripts

--- crates/wasm/postBuild.sh
+++ crates/wasm/postBuild.sh
@@ -2,10 +2,12 @@
 
 # TODO: Handle the wasm target being built in release mode
 WASM_BINARY=./target/wasm32-unknown-unknown/release/${pname}.wasm
-NODE_WASM=./pkg/nodejs/${pname}_bg.wasm
-BROWSER_WASM=./pkg/web/${pname}_bg.wasm
+NODE_DIR=./pkg/nodejs
+BROWSER_DIR=./pkg/web
+NODE_WASM=${NODE_DIR}/${pname}_bg.wasm
+BROWSER_WASM=${BROWSER_DIR}/${pname}_bg.wasm
 
-wasm-bindgen $WASM_BINARY --out-dir ./pkg/nodejs --typescript --target nodejs
-wasm-bindgen $WASM_BINARY --out-dir ./pkg/web --typescript --target web
+wasm-bindgen $WASM_BINARY --out-dir ${NODE_DIR} --typescript --target nodejs
+wasm-bindgen $WASM_BINARY --out-dir ${BROWSER_DIR} --typescript --target web
 wasm-opt $NODE_WASM -o $NODE_WASM -O
 wasm-opt $BROWSER_WASM -o $BROWSER_WASM -O
