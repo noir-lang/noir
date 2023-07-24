a4a4a65f1d00fc30f75cf0821cb3ce32025b6bfc
kevaundray <kevtheappdev@gmail.com>
2023-07-24 11:45:36 +0000

    small cleanup

--- crates/noirc_evaluator/src/ssa_refactor/acir_gen/acir_ir/acir_variable.rs
+++ crates/noirc_evaluator/src/ssa_refactor/acir_gen/acir_ir/acir_variable.rs
@@ -942,18 +942,39 @@ impl AcirContext {
         self.maybe_eq_predicate(lhs_less_than_rhs, predicate)
     }
 
+    /// Helper function that returns Expression::zero() if its a read operation
+    /// or Expression::one() to signify a memory write operation.
+    ///
+    /// This is needed because the memory operation for dynamic arrays is a single opcode
+    fn memory_op(is_read: bool) -> Expression {
+        if is_read {
+            Expression::zero()
+        } else {
+            Expression::one()
+        }
+    }
+
+    /// Returns a Variable that is constrained to be the result of reading
+    /// from the memory `block_id` at the given `index`.
     pub(crate) fn read_from_memory(&mut self, block_id: BlockId, index: &AcirVar) -> AcirVar {
-        let acir_var_data = self.vars.get(index).expect("ICE: undeclared AcirVar");
-        let index_witness = self.acir_ir.get_or_create_witness(&acir_var_data.to_expression());
-        let result = self.add_variable();
-        let result_var_data = self.vars.get(&result).expect("ICE: undeclared AcirVar");
-        let op = MemOp {
-            operation: Expression::zero(),
-            index: index_witness.into(),
-            value: result_var_data.to_expression().into_owned(),
-        };
+        // Fetch the witness corresponding to the index
+        //
+        // TODO: We can have a helper method which converts AcirVar to Witness
+        let index_var_data = self.vars.get(index).expect("ICE: undeclared AcirVar");
+        let index_witness = self.acir_ir.get_or_create_witness(&index_var_data.to_expression());
+
+        // Create a Variable to hold the result of the read and extract the corresponding Witness
+        let value_read_var = self.add_variable();
+        let value_read_var_data = self.vars.get(&value_read_var).expect("ICE: undeclared AcirVar");
+        let value_read_witness = value_read_var_data.to_expression().into_owned();
+
+        // Add the memory read operation to the list of opcodes
+        let read_op = Self::memory_op(true);
+        let op =
+            MemOp { operation: read_op, index: index_witness.into(), value: value_read_witness };
         self.acir_ir.opcodes.push(Opcode::MemoryOp { block_id, op });
-        result
+
+        value_read_var
     }
 
     pub(crate) fn write_to_memory(&mut self, block_id: BlockId, index: &AcirVar, value: &AcirVar) {
