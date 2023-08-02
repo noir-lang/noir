7d7d274abe060d91a0c9bed5d4bdf1b023910538
Alexander Ivanov <alehander42@gmail.com>
2023-08-02 13:49:46 +0300

    refactor: cleanup
    Co-authored-by: jfecher <jfecher11@gmail.com>

--- crates/noirc_evaluator/src/ssa/ssa_gen/mod.rs
+++ crates/noirc_evaluator/src/ssa/ssa_gen/mod.rs
@@ -86,8 +86,7 @@ impl<'a> FunctionContext<'a> {
     /// Codegen any non-tuple expression so that we can unwrap the Values
     /// tree to return a single value for use with most SSA instructions.
     fn codegen_non_tuple_expression(&mut self, expr: &Expression) -> ValueId {
-        let e = self.codegen_expression(expr);
-        e.into_leaf().eval(self)
+        self.codegen_expression(expr).into_leaf().eval(self)
     }
 
     /// Codegen a reference to an ident.
