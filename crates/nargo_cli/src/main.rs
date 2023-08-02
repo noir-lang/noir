b1e94bdf2e043b75497fee9c0d7110294556028d
f01dab1e <andriy.n@obox.systems>
2023-08-02 10:32:38 +0000

    disable display location section

--- crates/nargo_cli/src/main.rs
+++ crates/nargo_cli/src/main.rs
@@ -5,11 +5,11 @@ use nargo_cli::cli::start_cli;
 
 fn main() -> eyre::Result<()> {
     // Register a panic hook to display more readable panic messages to end-users
-    let (panic_hook, _) = HookBuilder::default()
-        .display_env_section(false)
-        .panic_section("This is a bug. We may have already fixed this in newer versions of Nargo so try searching for similar issues at https://github.com/noir-lang/noir/issues/.\nIf there isn't an open issue for this bug, consider opening one at https://github.com/noir-lang/noir/issues/new?labels=bug&template=bug_report.yml")
-        .into_hooks();
-    panic_hook.install();
+    HookBuilder::default()
+    .display_env_section(false)
+    .display_location_section(cfg!(debug_assertions))
+    .panic_section("This is a bug. We may have already fixed this in newer versions of Nargo so try searching for similar issues at https://github.com/noir-lang/noir/issues/.\nIf there isn't an open issue for this bug, consider opening one at https://github.com/noir-lang/noir/issues/new?labels=bug&template=bug_report.yml")
+    .install()?;
 
     start_cli()
 }
