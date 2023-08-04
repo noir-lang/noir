778b095a9517bcd60b4e7dc3e24d5d0fdb98381f
Tom French <tom@tomfren.ch>
2023-08-04 18:34:23 +0100

    chore: bring noir-wasm-testing repo contents into wasm crate
    ---------
    
    Co-authored-by: jonybur <jobur93@gmail.com>

---
+++ crates/wasm/test/shared.ts
@@ -0,0 +1,31 @@
+import { initialiseResolver } from "@noir-lang/noir-source-resolver";
+import { compile } from "@noir-lang/noir_wasm";
+
+export const noirSourcePath = "../../noir-script/src/main.nr";
+export const nargoArtifactPath = "../../noir-script/target/noir_wasm_testing.json";
+
+export async function compileNoirSource(noir_source: string): Promise<any> {
+  console.log("Compiling Noir source...");
+
+  initialiseResolver((id: string) => {
+    console.log(`Resolving source ${id}`);
+
+    const source = noir_source;
+
+    if (typeof source === "undefined") {
+      throw Error(`Could not resolve source for '${id}'`);
+    } else {
+      return source;
+    }
+  });
+
+  try {
+    const compiled_noir = compile({});
+
+    console.log("Noir source compilation done.");
+
+    return compiled_noir.circuit;
+  } catch (e) {
+    console.log("Error while compiling:", e);
+  }
+}
