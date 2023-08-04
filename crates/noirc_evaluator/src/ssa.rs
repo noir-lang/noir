186375b1c492ca4d84690860f0fa6a8d1a25bea2
Tom French <15848336+TomAFrench@users.noreply.github.com>
2023-08-04 14:20:43 +0100

    chore: update stale comment on `create_circuit` (#2173)

--- crates/noirc_evaluator/src/ssa.rs
+++ crates/noirc_evaluator/src/ssa.rs
@@ -65,10 +65,9 @@ pub(crate) fn optimize_into_acir(
     ssa.into_acir(brillig, abi_distinctness)
 }
 
-/// Compiles the Program into ACIR and applies optimizations to the arithmetic gates
-/// This is analogous to `ssa:create_circuit` and this method is called when one wants
-/// to use the new ssa module to process Noir code.
-// TODO: This no longer needs to return a result, but it is kept to match the signature of `create_circuit`
+/// Compiles the [`Program`] into [`ACIR`][acvm::acir::circuit::Circuit].
+///
+/// The output ACIR is is backend-agnostic and so must go through a transformation pass before usage in proof generation.
 pub fn create_circuit(
     program: Program,
     enable_ssa_logging: bool,
