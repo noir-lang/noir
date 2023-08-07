b402315292518d64e09638b529cce91c8df8a481
Tom French <tom@tomfren.ch>
2023-08-07 11:26:22 +0100

    chore: backwards compatibility for CI

--- crates/nargo_cli/tests/execution_success/rebuild.sh
+++ crates/nargo_cli/tests/execution_success/rebuild.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 set -e
 
-# Parse exclude and fail directories from cargo.toml
-exclude_dirs=$(grep "^exclude" config.toml | sed 's/exclude = \[//;s/\]//;s/\"//g;s/ //g' | tr ',' '\n')
-fail_dirs=$(grep "^fail" config.toml | sed 's/fail = \[//;s/\]//;s/\"//g;s/ //g' | tr ',' '\n')
-
-# Convert them to array
-exclude_array=($exclude_dirs)
-fail_array=($fail_dirs)
-
-# Merge exclude and fail arrays
-exclude_fail_dirs=("${exclude_array[@]}" "${fail_array[@]}" "workspace")
+excluded_dirs=("workspace")
 
 # Loop over every directory
 for dir in ./*; do
@@ -19,7 +10,7 @@ for dir in ./*; do
   fi
 
   dir_name=$(basename "$dir")
-  if [[ ! " ${exclude_fail_dirs[@]} " =~ " ${dir_name} " ]]; then
+  if [[ ! " ${excluded_dirs[@]} " =~ " ${dir_name} " ]]; then
       cd $dir
       if [ -d ./target/ ]; then
         rm -r ./target/
