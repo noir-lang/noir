3c5d08b24957f74af67b168220dda8fb5042e0d0
ethan-000 <s2026080@ed.ac.uk>
2023-08-08 16:44:51 +0100

    .

--- crates/nargo_cli/benches/utils.rs
+++ crates/nargo_cli/benches/utils.rs
@@ -10,5 +10,5 @@ fn get_selected_tests() -> Vec<PathBuf> {
 
     let selected_tests =
         vec!["8_integration", "sha256_blocks", "struct", "eddsa", "regression", "regression_2099"];
-    selected_tests.into_iter().map(|t| PathBuf::from(test_dir.join(t))).collect()
+    selected_tests.into_iter().map(|t| test_dir.join(t)).collect()
 }
