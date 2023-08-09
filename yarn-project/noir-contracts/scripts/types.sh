8e722c3a4deaab2794506092dae7dff4f977db04
Maddiaa <47148561+Maddiaa0@users.noreply.github.com>
2023-08-09 18:25:54 +0100

    fix: add noir clean command & clean noir artifacts when building (#1482)
    title

--- yarn-project/noir-contracts/scripts/types.sh
+++ yarn-project/noir-contracts/scripts/types.sh
@@ -76,7 +76,7 @@ wait
 
 # Write the index ts stuff
 # Remove the output file
-rm $artifacts_dir/index.ts || true
+rm -f $artifacts_dir/index.ts || true
 
 # Generate artifacts package index.ts
 echo "// Auto generated module\n" > "$artifacts_dir/index.ts";
