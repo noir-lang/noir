72890a75b63b7fa2e2b947cb23f28baea203489e
Tom French <tom@tomfren.ch>
2023-08-10 15:16:57 +0100

    chore: add missing newline to `README.md` output

--- crates/wasm/installPhase.sh
+++ crates/wasm/installPhase.sh
@@ -7,6 +7,7 @@ cp ${self_path}/README.md ${self_path}/pkg/
 cp ${self_path}/package.json ${self_path}/pkg/
 cp -r ${self_path}/pkg/* $out/
 
+echo "" >> $out/README.md
 echo "## Tracking" >> $out/README.md
 echo "Built from [noir-lang/noir@$GIT_COMMIT](https://github.com/noir-lang/noir/tree/$GIT_COMMIT)" >> $out/README.md
 
