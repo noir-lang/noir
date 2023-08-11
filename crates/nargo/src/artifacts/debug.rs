4e89750332eb3813e0427a9bfe662c905e65bdda
sirasistant <sirasistant@gmail.com>
2023-08-11 09:48:51 +0000

    fix: use pathbuf instead of string for paths

--- crates/nargo/src/artifacts/debug.rs
+++ crates/nargo/src/artifacts/debug.rs
@@ -1,16 +1,23 @@
 use serde::{Deserialize, Serialize};
-use std::collections::{BTreeMap, BTreeSet};
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    path::PathBuf,
+};
 
 use fm::FileId;
 use noirc_errors::debug_info::DebugInfo;
 use noirc_frontend::hir::Context;
 
+/// For a given file, we store the source code and the path to the file
+/// so consumers of the debug artifact can reconstruct the original source code structure.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DebugFile {
     pub source: String,
-    pub path: String,
+    pub path: PathBuf,
 }
 
+/// A Debug Artifact stores, for a given program, the debug info for every function
+/// along with a map of file Id to the source code so locations in debug info can be mapped to source code they point to.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DebugArtifact {
     pub debug_symbols: Vec<DebugInfo>,
@@ -33,11 +40,7 @@ impl DebugArtifact {
                 file_id,
                 DebugFile {
                     source: file_source.to_string(),
-                    path: compilation_context
-                        .file_manager
-                        .path(file_id)
-                        .to_string_lossy()
-                        .to_string(),
+                    path: compilation_context.file_manager.path(file_id).to_path_buf(),
                 },
             );
         }
