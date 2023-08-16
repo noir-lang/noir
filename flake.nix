5f7bdf3ab52cb576f4c73c99e9279b3f2397c365
kevaundray <kevtheappdev@gmail.com>
2023-08-16 20:06:52 +0100

    chore(noir): Release 0.10.3

--- flake.nix
+++ flake.nix
@@ -117,7 +117,7 @@
 
       sharedArgs = {
         # x-release-please-start-version
-        version = "0.10.2";
+        version = "0.10.3";
         # x-release-please-end
 
         src = pkgs.lib.cleanSourceWith {
