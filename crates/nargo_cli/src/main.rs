427d8f5efcdaf2e6f0a3be6f2ae050edc917830e
f01dab1e <andriy.n@obox.systems>
2023-08-17 18:31:45 +0000

    use color-eyre crate for panic handling

--- crates/nargo_cli/src/main.rs
+++ crates/nargo_cli/src/main.rs
@@ -7,13 +7,18 @@
 //! This name was used because it sounds like `cargo` and
 //! Noir Package Manager abbreviated is npm, which is already taken.
 
+use color_eyre::config::HookBuilder;
+
 mod backends;
 mod cli;
 mod errors;
-mod panic;
+
+const PANIC_MESSAGE: &str = "This is a bug. We may have already fixed this in newer versions of Nargo so try searching for similar issues at https://github.com/noir-lang/noir/issues/.\nIf there isn't an open issue for this bug, consider opening one at https://github.com/noir-lang/noir/issues/new?labels=bug&template=bug_report.yml";
 
 fn main() {
-    panic::set_hook();
+    let (panic_hook, _) =
+        HookBuilder::default().display_env_section(false).panic_section(PANIC_MESSAGE).into_hooks();
+    panic_hook.install();
 
     if let Err(report) = cli::start_cli() {
         eprintln!("{report}");
