3d9946623a1ccc4645a87e6fa6940f19faf4ced0
Tom French <tom@tomfren.ch>
2023-08-20 01:57:26 +0100

    feat: simplify `AcirVar`s using equivalence knowledge from constraints

--- crates/noirc_evaluator/src/ssa/acir_gen/acir_ir/acir_variable.rs
+++ crates/noirc_evaluator/src/ssa/acir_gen/acir_ir/acir_variable.rs
@@ -153,6 +153,65 @@ impl AcirContext {
         self.add_data(var_data)
     }
 
+    fn mark_variables_equivalent(
+        &mut self,
+        lhs: AcirVar,
+        rhs: AcirVar,
+    ) -> Result<(), InternalError> {
+        if lhs == rhs {
+            return Ok(());
+        }
+
+        let lhs_data = self.vars.remove(&lhs).ok_or_else(|| InternalError::UndeclaredAcirVar {
+            call_stack: self.get_call_stack(),
+        })?;
+        let rhs_data = self.vars.remove(&rhs).ok_or_else(|| InternalError::UndeclaredAcirVar {
+            call_stack: self.get_call_stack(),
+        })?;
+
+        let (new_lhs_data, new_rhs_data) = match (lhs_data, rhs_data) {
+            // Always prefer a constant variable.
+            (constant @ AcirVarData::Const(_), _) | (_, constant @ AcirVarData::Const(_)) => {
+                (constant.clone(), constant)
+            }
+
+            // Replace any expressions with witnesses.
+            (witness @ AcirVarData::Witness(_), AcirVarData::Expr(_))
+            | (AcirVarData::Expr(_), witness @ AcirVarData::Witness(_)) => {
+                (witness.clone(), witness)
+            }
+
+            // If both variables are witnesses then use the smaller of the two in future.
+            (AcirVarData::Witness(lhs_witness), AcirVarData::Witness(rhs_witness)) => {
+                let witness = AcirVarData::Witness(std::cmp::min(lhs_witness, rhs_witness));
+                (witness.clone(), witness)
+            }
+
+            (AcirVarData::Expr(lhs_expr), AcirVarData::Expr(rhs_expr)) => {
+                if lhs_expr.is_linear() && rhs_expr.is_linear() {
+                    // If both expressions are linear, choose the one with the fewest terms
+                    let expr = if lhs_expr.linear_combinations.len()
+                        <= rhs_expr.linear_combinations.len()
+                    {
+                        lhs_expr
+                    } else {
+                        rhs_expr
+                    };
+
+                    let expr = AcirVarData::Expr(expr);
+                    (expr.clone(), expr)
+                } else {
+                    (AcirVarData::Expr(lhs_expr), AcirVarData::Expr(rhs_expr))
+                }
+            }
+        };
+
+        self.vars.insert(lhs, new_lhs_data);
+        self.vars.insert(rhs, new_rhs_data);
+
+        Ok(())
+    }
+
     pub(crate) fn get_call_stack(&self) -> CallStack {
         self.acir_ir.call_stack.clone()
     }
@@ -345,6 +404,7 @@ impl AcirContext {
         if diff_expr.is_const() {
             if diff_expr.is_zero() {
                 // Constraint is always true - assertion is unnecessary.
+                self.mark_variables_equivalent(lhs, rhs)?;
                 return Ok(());
             } else {
                 // Constraint is always false - this program is unprovable.
@@ -357,6 +417,8 @@ impl AcirContext {
         }
 
         self.acir_ir.assert_is_zero(diff_expr);
+        self.mark_variables_equivalent(lhs, rhs)?;
+
         Ok(())
     }
 
