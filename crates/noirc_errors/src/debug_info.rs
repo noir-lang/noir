54c46bf29e77374ecf9597fc596177c74a6d7efb
sirasistant <sirasistant@gmail.com>
2023-08-21 10:27:28 +0000

    docs: add comments

--- crates/noirc_errors/src/debug_info.rs
+++ crates/noirc_errors/src/debug_info.rs
@@ -12,6 +12,8 @@ use serde::{Deserialize, Serialize};
 #[derive(Default, Debug, Clone, Deserialize, Serialize)]
 pub struct DebugInfo {
     /// Map opcode index of an ACIR circuit into the source code location
+    /// Serde does not support mapping keys being enums for json, so we indicate
+    /// that they should be serialized to/from strings.
     #[serde_as(as = "BTreeMap<DisplayFromStr, _>")]
     pub locations: BTreeMap<OpcodeLocation, Vec<Location>>,
 }
@@ -21,7 +23,10 @@ impl DebugInfo {
         DebugInfo { locations }
     }
 
-    /// Updates the locations map when the circuit is modified
+    /// Updates the locations map when the circuit is modified.
+    /// The OpcodeLocations are generated with the ACIR, but passing the ACIR through a transformation step
+    /// renders the old OpcodeLocations invalid. The AcirTransformationMap is able to map the old OpcodeLocation to the new ones.
+    /// Note: One old OpcodeLocation might have transformed into more than one new OpcodeLocation.
     pub fn update_acir(&mut self, update_map: AcirTransformationMap) {
         let mut new_locations_map = BTreeMap::new();
 
