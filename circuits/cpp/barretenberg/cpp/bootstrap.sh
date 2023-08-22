87815d7e0c6182973d98155c23d2f60b7c66314c
Alex Orlow <alexorlow17@gmail.com>
2023-08-22 10:34:57 -0400

    chore: update bootstrap.sh in Barretenberg to check for clang 16 (#1717)
    As per #1542 clang 16 is needed so a proposed solution is to check
    version in bootstrap.sh
    
    ---------
    
    Co-authored-by: ludamad <adam.domurad@gmail.com>

--- circuits/cpp/barretenberg/cpp/bootstrap.sh
+++ circuits/cpp/barretenberg/cpp/bootstrap.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-set -eu
+set -u
+
+# Get the clang version string
+clang_version_string=$(clang --version 2>/dev/null)
+
+# Check if clang is installed
+if [ $? -ne 0 ]; then
+  echo "Error: clang is not installed."
+  exit 1
+fi
+
+# Extract the major version number
+major_version=$(echo $clang_version_string | awk -F' ' '/clang version/{print $3}' | awk -F'.' '{print $1}')
+
+if [ "$major_version" -ge 16 ]; then
+  echo "clang version $major_version is good."
+else
+  echo "Error: clang version 16 or greater is required."
+  exit 1
+fi
+
+set -e
 
 # Clean.
 rm -rf ./build
