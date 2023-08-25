c2d19557a447ae25eac673bfa6c8dbbec83133bf
Tom French <tom@tomfren.ch>
2023-08-25 12:11:10 +0100

    chore: remove unnecessary pedersen-related functions

--- crates/barretenberg_black_box_solver/src/pedersen.rs
+++ crates/barretenberg_black_box_solver/src/pedersen.rs
@@ -3,12 +3,6 @@ use super::{Barretenberg, Error};
 use acvm::FieldElement;
 
 pub(crate) trait Pedersen {
-    fn compress_native(
-        &self,
-        left: &FieldElement,
-        right: &FieldElement,
-    ) -> Result<FieldElement, Error>;
-    fn compress_many(&self, inputs: Vec<FieldElement>) -> Result<FieldElement, Error>;
     fn encrypt(
         &self,
         inputs: Vec<FieldElement>,
@@ -18,41 +12,6 @@ pub(crate) trait Pedersen {
 
 #[cfg(feature = "native")]
 impl Pedersen for Barretenberg {
-    fn compress_native(
-        &self,
-        left: &FieldElement,
-        right: &FieldElement,
-    ) -> Result<FieldElement, Error> {
-        use super::FeatureError;
-
-        let result_bytes = barretenberg_sys::pedersen::compress_native(
-            left.to_be_bytes()
-                .as_slice()
-                .try_into()
-                .map_err(|source| FeatureError::FieldElementSlice { source })?,
-            right
-                .to_be_bytes()
-                .as_slice()
-                .try_into()
-                .map_err(|source| FeatureError::FieldElementSlice { source })?,
-        );
-
-        Ok(FieldElement::from_be_bytes_reduce(&result_bytes))
-    }
-
-    #[allow(dead_code)]
-    fn compress_many(&self, inputs: Vec<FieldElement>) -> Result<FieldElement, Error> {
-        use super::native::field_to_array;
-
-        let mut inputs_buf = Vec::new();
-        for f in inputs {
-            inputs_buf.push(field_to_array(&f)?);
-        }
-        let result_bytes = barretenberg_sys::pedersen::compress_many(&inputs_buf);
-
-        Ok(FieldElement::from_be_bytes_reduce(&result_bytes))
-    }
-
     fn encrypt(
         &self,
         inputs: Vec<FieldElement>,
@@ -76,47 +35,6 @@ impl Pedersen for Barretenberg {
 
 #[cfg(not(feature = "native"))]
 impl Pedersen for Barretenberg {
-    fn compress_native(
-        &self,
-        left: &FieldElement,
-        right: &FieldElement,
-    ) -> Result<FieldElement, Error> {
-        use super::FIELD_BYTES;
-
-        let lhs_ptr: usize = 0;
-        let rhs_ptr: usize = lhs_ptr + FIELD_BYTES;
-        let result_ptr: usize = rhs_ptr + FIELD_BYTES;
-
-        self.transfer_to_heap(&left.to_be_bytes(), lhs_ptr);
-        self.transfer_to_heap(&right.to_be_bytes(), rhs_ptr);
-
-        self.call_multiple(
-            "pedersen_plookup_compress_fields",
-            vec![&lhs_ptr.into(), &rhs_ptr.into(), &result_ptr.into()],
-        )?;
-
-        let result_bytes: [u8; FIELD_BYTES] = self.read_memory(result_ptr);
-        Ok(FieldElement::from_be_bytes_reduce(&result_bytes))
-    }
-
-    #[allow(dead_code)]
-    fn compress_many(&self, inputs: Vec<FieldElement>) -> Result<FieldElement, Error> {
-        use super::barretenberg_structures::Assignments;
-        use super::FIELD_BYTES;
-
-        let input_buf = Assignments::from(inputs).to_bytes();
-        let input_ptr = self.allocate(&input_buf)?;
-        let result_ptr: usize = 0;
-
-        self.call_multiple(
-            "pedersen_plookup_compress",
-            vec![&input_ptr, &result_ptr.into()],
-        )?;
-
-        let result_bytes: [u8; FIELD_BYTES] = self.read_memory(result_ptr);
-        Ok(FieldElement::from_be_bytes_reduce(&result_bytes))
-    }
-
     fn encrypt(
         &self,
         inputs: Vec<FieldElement>,
@@ -142,46 +60,6 @@ impl Pedersen for Barretenberg {
 
         Ok((point_x, point_y))
     }
-}
-
-#[test]
-fn basic_interop() -> Result<(), Error> {
-    // Expected values were taken from Barretenberg by running `crypto::pedersen::compress_native`
-    // printing the result in hex to `std::cout` and copying
-    struct Test<'a> {
-        input_left: FieldElement,
-        input_right: FieldElement,
-        expected_hex: &'a str,
-    }
-
-    let tests = vec![
-        Test {
-            input_left: FieldElement::zero(),
-            input_right: FieldElement::one(),
-            expected_hex: "0x0c5e1ddecd49de44ed5e5798d3f6fb7c71fe3d37f5bee8664cf88a445b5ba0af",
-        },
-        Test {
-            input_left: FieldElement::one(),
-            input_right: FieldElement::one(),
-            expected_hex: "0x0e1793a0c122887bcb53c84776f4704c26bc093b25eaa9c7847a672c65e314ae",
-        },
-        Test {
-            input_left: FieldElement::one(),
-            input_right: FieldElement::zero(),
-            expected_hex: "0x0c93b3f27730b2e331e634af15bc9d5a769688921f30b36ca926b35a96b3306c",
-        },
-    ];
-
-    let barretenberg = Barretenberg::new();
-    for test in tests {
-        let expected = FieldElement::from_hex(test.expected_hex).unwrap();
-
-        let got = barretenberg.compress_native(&test.input_left, &test.input_right)?;
-        let got_many = barretenberg.compress_many(vec![test.input_left, test.input_right])?;
-        assert_eq!(got, expected);
-        assert_eq!(got, got_many);
-    }
-    Ok(())
 }
 
 #[test]
