cbf7c2336105b4bca462076c49fe7dae6490aadd
Nikolay Nikolov <nickysn@gmail.com>
2023-08-26 14:20:05 +0300

    refactor(parser): use !generics.is_empty() instead of generics.len()>0

--- crates/noirc_frontend/src/ast/traits.rs
+++ crates/noirc_frontend/src/ast/traits.rs
@@ -177,7 +177,7 @@ impl Display for TraitConstraint {
 impl Display for TraitBound {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let generics = vecmap(&self.trait_generics, |generic| generic.to_string());
-        if generics.len() > 0 {
+        if !generics.is_empty() {
             write!(f, "{}<{}>", self.trait_name, generics.join(", "))
         } else {
             write!(f, "{}", self.trait_name)
