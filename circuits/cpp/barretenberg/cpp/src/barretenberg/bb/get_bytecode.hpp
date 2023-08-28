be9718505c7e387bb46183299c9db855e6d7f91c
kevaundray <kevtheappdev@gmail.com>
2023-08-29 00:43:36 +0100

    fix: Conditionally compile base64 command for bb binary (#1851)
    resolves #1850 
    
    # Checklist:
    Remove the checklist to signal you've completed it. Enable auto-merge if
    the PR is ready to merge.
    - [ ] If the pull request requires a cryptography review (e.g.
    cryptographic algorithm implementations) I have added the 'crypto' tag.
    - [ ] I have reviewed my diff in github, line by line and removed
    unexpected formatting changes, testing logs, or commented-out code.
    - [ ] Every change is related to the PR description.
    - [ ] I have
    [linked](https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue)
    this pull request to relevant issues (if any exist).

--- circuits/cpp/barretenberg/cpp/src/barretenberg/bb/get_bytecode.hpp
+++ circuits/cpp/barretenberg/cpp/src/barretenberg/bb/get_bytecode.hpp
@@ -6,6 +6,12 @@
  */
 inline std::vector<uint8_t> get_bytecode(const std::string& bytecodePath)
 {
+// base64 on mac is different from linux
+#ifdef __APPLE__
+    std::string command = "base64 -D -i " + bytecodePath + " | gunzip";
+#else
     std::string command = "base64 -d " + bytecodePath + " | gunzip";
+#endif
+
     return exec_pipe(command);
 }
