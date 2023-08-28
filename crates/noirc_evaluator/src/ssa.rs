0d5dcec5a16a927023b8d5fbbdf0d7e9b562c245
vezenovm <mvezenov@gmail.com>
2023-08-28 20:27:06 +0000

    don't optimize allocates in constant folding

--- crates/noirc_evaluator/src/ssa.rs
+++ crates/noirc_evaluator/src/ssa.rs
@@ -63,6 +63,8 @@ pub(crate) fn optimize_into_acir(
             // and this pass is missed, slice merging will fail inside of flattening.
             .mem2reg()
             .print(print_ssa_passes, "After Mem2Reg:")
+            .fold_constants()
+            .print(print_ssa_passes, "After Constant Folding:")
             .flatten_cfg()
             .print(print_ssa_passes, "After Flattening:")
             // Run mem2reg once more with the flattened CFG to catch any remaining loads/stores
