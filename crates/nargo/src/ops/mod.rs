7fdf185b7d5cb13e3e68cb255e3d76818c60d49f
Tom French <15848336+TomAFrench@users.noreply.github.com>
2023-08-29 16:42:10 +0100

    Update crates/nargo/src/ops/mod.rs

--- crates/nargo/src/ops/mod.rs
+++ crates/nargo/src/ops/mod.rs
@@ -2,8 +2,7 @@ pub use self::codegen_verifier::codegen_verifier;
 pub use self::execute::execute_circuit;
 pub use self::preprocess::{preprocess_contract_function, preprocess_program};
 pub use self::prove::prove_execution;
-pub use self::test::run_test;
-pub use self::test::TestStatus;
+pub use self::test::{run_test, TestStatus};
 pub use self::verify::verify_proof;
 
 mod codegen_verifier;
