5fe5fbe0ea4d40964e71bddb548df723d562bb17
ethan-000 <s2026080@ed.ac.uk>
2023-08-29 15:54:28 +0100

    optmize update acir

--- crates/noirc_errors/src/debug_info.rs
+++ crates/noirc_errors/src/debug_info.rs
@@ -29,16 +29,14 @@ impl DebugInfo {
     /// renders the old `OpcodeLocation`s invalid. The AcirTransformationMap is able to map the old `OpcodeLocation` to the new ones.
     /// Note: One old `OpcodeLocation` might have transformed into more than one new `OpcodeLocation`.
     pub fn update_acir(&mut self, update_map: AcirTransformationMap) {
-        let mut new_locations_map = BTreeMap::new();
+        let old = self.locations.clone();
+        self.locations.clear();
 
-        for (old_opcode_location, source_locations) in &self.locations {
-            let new_opcode_locations = update_map.new_locations(*old_opcode_location);
-            for new_opcode_location in new_opcode_locations {
-                new_locations_map.insert(new_opcode_location, source_locations.clone());
-            }
+        for (old_opcode_location, source_locations) in old {
+            let _ = update_map.new_locations(old_opcode_location).map(|new_opcode_location| {
+                self.locations.insert(new_opcode_location, source_locations.clone());
+            });
         }
-
-        self.locations = new_locations_map;
     }
 
     pub fn opcode_location(&self, loc: &OpcodeLocation) -> Option<Vec<Location>> {
