456925e697bbf75f2d6409941e7f157c28c945bc
kevaundray <kevtheappdev@gmail.com>
2023-08-29 00:40:39 +0000

    don't save target in same folder as execution success

--- crates/nargo_cli/tests/rebuild.sh
+++ crates/nargo_cli/tests/rebuild.sh
@@ -3,30 +3,42 @@ set -e
 
 excluded_dirs=("workspace" "workspace_default_member")
 
-cd ./execution_success
+current_dir=$(pwd)
+base_path="$current_dir/execution_success"
+
+# Ensure the base acir_artifacts directory exists
+mkdir -p $current_dir/acir_artifacts
 
 # Loop over every directory
-for dir in ./*; do
+for dir in $base_path/*; do
   if [[ ! -d $dir ]]; then
     continue
   fi
 
   dir_name=$(basename "$dir")
+
   if [[ ! " ${excluded_dirs[@]} " =~ " ${dir_name} " ]]; then
+      echo "Creating directory $current_dir/acir_artifacts/$dir_name"
+      mkdir -p $current_dir/acir_artifacts/$dir_name
+      echo "Directory created."
+
       cd $dir
       if [ -d ./target/ ]; then
         rm -r ./target/
       fi
       nargo compile && nargo execute witness
 
-      # Extract bytecode field from JSON, base64 decode it, and save it to a target directory
+      # Extract bytecode field from JSON, base64 decode it, and save it to the target directory
       if [ -f ./target/${dir_name}.json ]; then
           jq -r '.bytecode' ./target/${dir_name}.json | base64 -d > ./target/${dir_name}.bytecode
       fi
 
       # Delete the JSON file after extracting bytecode field
       rm ./target/${dir_name}.json
+      
+      # Move the target directory to the corresponding directory in acir_artifacts
+      mv ./target/ $current_dir/acir_artifacts/$dir_name/
 
-      cd ..
+      cd $base_path
   fi
 done
