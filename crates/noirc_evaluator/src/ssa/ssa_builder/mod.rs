cdf0a7450d4e55d24d9f02b3c55745afcb173295
vezenovm <mvezenov@gmail.com>
2023-08-29 18:10:59 +0000

    remove padding from constant to_radix

--- crates/noirc_evaluator/src/ssa/ssa_builder/mod.rs
+++ crates/noirc_evaluator/src/ssa/ssa_builder/mod.rs
@@ -385,7 +385,7 @@ mod tests {
 
     #[test]
     fn insert_constant_call() {
-        // `bits` should be an array of constants [1, 1, 1, 0...]:
+        // `bits` should be an array of constants [1, 1, 1, 0...] of length 8:
         // let x = 7;
         // let bits = x.to_le_bits(8);
         let func_id = Id::test_new(0);
@@ -404,7 +404,7 @@ mod tests {
             Value::NumericConstant { constant, .. } => *constant,
             _ => panic!(),
         };
-        assert_eq!(slice_len, FieldElement::from(256u128));
+        assert_eq!(slice_len, FieldElement::from(8_u128));
 
         let slice = match &builder.current_function.dfg[call_results[1]] {
             Value::Array { array, .. } => array,
