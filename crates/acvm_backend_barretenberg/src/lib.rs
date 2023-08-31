f18f4a41b6c4e036e77a62f254758e8ed7e9539f
Tom French <tom@tomfren.ch>
2023-08-31 17:48:10 +0100

    chore: remove dead code

--- crates/acvm_backend_barretenberg/src/lib.rs
+++ crates/acvm_backend_barretenberg/src/lib.rs
@@ -1,18 +1,6 @@
 #![warn(unused_crate_dependencies, unused_extern_crates)]
 #![warn(unreachable_pub)]
 
-// `acvm-backend-barretenberg` can either interact with the Barretenberg backend through a static library
-// or through an embedded wasm binary. It does not make sense to include both of these backends at the same time.
-// We then throw a compilation error if both flags are set.
-#[cfg(all(feature = "native", feature = "wasm"))]
-compile_error!("feature \"native\" and feature \"wasm\" cannot be enabled at the same time");
-
-#[cfg(all(feature = "native", target_arch = "wasm32"))]
-compile_error!("feature \"native\" cannot be enabled for a \"wasm32\" target");
-
-#[cfg(all(feature = "wasm", target_arch = "wasm32"))]
-compile_error!("feature \"wasm\" cannot be enabled for a \"wasm32\" target");
-
 mod bb;
 mod proof_system;
 mod smart_contract;
