fb538aeeea7842c39e846d3ec7b0c10b481c5b53
sirasistant <sirasistant@gmail.com>
2023-08-31 11:57:42 +0000

    style: remove old todo

--- crates/noirc_evaluator/src/errors.rs
+++ crates/noirc_evaluator/src/errors.rs
@@ -14,19 +14,8 @@ use thiserror::Error;
 
 use crate::ssa::ir::dfg::CallStack;
 
-fn format_failed_constraint(message: &Option<String>) -> String {
-    match message {
-        Some(message) => format!("Failed constraint: '{}'", message),
-        None => "Failed constraint".to_owned(),
-    }
-}
-
 #[derive(Debug, PartialEq, Eq, Clone, Error)]
 pub enum RuntimeError {
-    // We avoid showing the actual lhs and rhs since most of the time they are just 0
-    // and 1 respectively. This would confuse users if a constraint such as
-    // assert(foo < bar) fails with "failed constraint: 0 = 1."
-    // TODO add assertion message here too
     #[error("{}", format_failed_constraint(.assert_message))]
     FailedConstraint {
         lhs: Box<Expression>,
@@ -50,6 +39,16 @@ pub enum RuntimeError {
     UnknownLoopBound { call_stack: CallStack },
     #[error("Argument is not constant")]
     AssertConstantFailed { call_stack: CallStack },
+}
+
+// We avoid showing the actual lhs and rhs since most of the time they are just 0
+// and 1 respectively. This would confuse users if a constraint such as
+// assert(foo < bar) fails with "failed constraint: 0 = 1."
+fn format_failed_constraint(message: &Option<String>) -> String {
+    match message {
+        Some(message) => format!("Failed constraint: '{}'", message),
+        None => "Failed constraint".to_owned(),
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Error)]
