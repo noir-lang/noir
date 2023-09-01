1bb8e50cf7ca93d71b91c4bf15668c67908f3fa0
kevaundray <kevtheappdev@gmail.com>
2023-09-01 23:47:50 +0000

    remove backend specific methods

--- crates/nargo/src/ops/codegen_verifier.rs
+++
@@ -1,6 +0,0 @@
-use acvm::acir::circuit::Circuit;
-use acvm_backend_barretenberg::{Backend, BackendError};
-
-pub fn codegen_verifier(backend: &Backend, circuit: &Circuit) -> Result<String, BackendError> {
-    backend.eth_contract(circuit)
-}
