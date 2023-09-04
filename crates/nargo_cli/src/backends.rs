8e7010189470c91b2f543d78facdc746e86f9d16
Tom French <tom@tomfren.ch>
2023-09-04 15:49:46 +0100

    chore: small refactor

--- crates/nargo_cli/src/backends.rs
+++ crates/nargo_cli/src/backends.rs
@@ -1,16 +1,18 @@
+use std::path::PathBuf;
+
 use acvm_backend_barretenberg::backends_directory;
 pub(crate) use acvm_backend_barretenberg::Backend;
 
-pub(crate) fn set_active_backend(backend_name: &str) {
-    let backends_directory = backends_directory();
-    let active_backend_file = backends_directory.join("./.selected_backend");
+fn active_backend_file_path() -> PathBuf {
+    backends_directory().join(".selected_backend")
+}
 
-    std::fs::write(active_backend_file, backend_name.as_bytes()).unwrap();
+pub(crate) fn set_active_backend(backend_name: &str) {
+    std::fs::write(active_backend_file_path(), backend_name.as_bytes()).unwrap();
 }
 
 pub(crate) fn get_active_backend() -> String {
-    let backends_directory = backends_directory();
-    let active_backend_file = backends_directory.join("./.selected_backend");
+    let active_backend_file = active_backend_file_path();
 
     if !active_backend_file.is_file() {
         let barretenberg = "acvm-backend-barretenberg";
