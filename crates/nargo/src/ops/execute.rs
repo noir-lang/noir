15a16e25d11fa1d9985fe404f2b90edb58d40f71
sirasistant <sirasistant@gmail.com>
2023-09-04 12:42:31 +0000

    style: rename var again

--- crates/nargo/src/ops/execute.rs
+++ crates/nargo/src/ops/execute.rs
@@ -33,7 +33,7 @@ pub fn execute_circuit<B: BlackBoxFunctionSolver>(
                 unreachable!("Execution should not stop while in `InProgress` state.")
             }
             ACVMStatus::Failure(error) => {
-                let opcode_locations = match &error {
+                let call_stack = match &error {
                     OpcodeResolutionError::UnsatisfiedConstrain {
                         opcode_location: ErrorLocation::Resolved(opcode_location),
                     } => Some(vec![*opcode_location]),
@@ -43,7 +43,7 @@ pub fn execute_circuit<B: BlackBoxFunctionSolver>(
                     _ => None,
                 };
 
-                return Err(NargoError::ExecutionError(match opcode_locations {
+                return Err(NargoError::ExecutionError(match call_stack {
                     Some(call_stack) => {
                         if let Some(assert_message) = get_assert_message(
                             call_stack.last().expect("Call stacks should not be empty"),
