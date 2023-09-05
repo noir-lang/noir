16e04fb5aff8f5e0f650214d106892155185e30c
Tom French <tom@tomfren.ch>
2023-09-05 12:24:01 +0100

    chore: add full mocked backend implementation

---
+++ crates/nargo_cli/test-binaries/mock_backend/src/gates_cmd.rs
@@ -0,0 +1,18 @@
+use clap::Args;
+use std::io::Write;
+use std::path::PathBuf;
+
+#[derive(Debug, Clone, Args)]
+pub(crate) struct GatesCommand {
+    #[clap(short = 'c')]
+    pub(crate) crs_path: Option<PathBuf>,
+
+    #[clap(short = 'b')]
+    pub(crate) bytecode_path: PathBuf,
+}
+
+pub(crate) fn run(args: GatesCommand) {
+    assert!(args.bytecode_path.is_file(), "Could not find bytecode file at provided path");
+
+    std::io::stdout().write_all(&0u64.to_be_bytes()).unwrap();
+}
