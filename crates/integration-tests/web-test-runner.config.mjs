7391a7e288f22c4d3d3659e657059799001dd89b
Koby <kobyhall@proton.me>
2023-09-07 18:49:35 +0200

    chore: see native browser

--- crates/integration-tests/web-test-runner.config.mjs
+++ crates/integration-tests/web-test-runner.config.mjs
@@ -1,11 +1,11 @@
 import { fileURLToPath } from 'url';
 import { esbuildPlugin } from "@web/dev-server-esbuild";
-import { playwrightLauncher } from "@web/test-runner-playwright";
+// import { playwrightLauncher } from "@web/test-runner-playwright";
 import { webdriverLauncher } from '@web/test-runner-webdriver';
 
 export default {
   browsers: [
-    playwrightLauncher({ product: "chromium" }),
+    // playwrightLauncher({ product: "chromium" }),
     // playwrightLauncher({ product: "webkit" }),
     // playwrightLauncher({ product: "firefox" }),
     webdriverLauncher({
