cc39daa14aa8dc2854c667b50622ef730a0218e6
Koby <kobyhall@proton.me>
2023-09-07 18:34:40 +0200

    chore: add missing import

--- crates/integration-tests/web-test-runner.config.mjs
+++ crates/integration-tests/web-test-runner.config.mjs
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'url';
 import { esbuildPlugin } from "@web/dev-server-esbuild";
 import { playwrightLauncher } from "@web/test-runner-playwright";
+import { webdriverLauncher } from '@web/test-runner-webdriver';
 
 export default {
   browsers: [
