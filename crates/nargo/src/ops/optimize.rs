b07f10f033f82b489bd0fbe6ea7a7132bcc2580a
Tom French <tom@tomfren.ch>
2023-09-08 23:55:22 +0100

    chore: clippy fix

--- crates/nargo/src/ops/optimize.rs
+++ crates/nargo/src/ops/optimize.rs
@@ -10,7 +10,7 @@ pub fn optimize_program(
     is_opcode_supported: &impl Fn(&Opcode) -> bool,
 ) -> Result<CompiledProgram, NargoError> {
     let (optimized_circuit, location_map) =
-        acvm::compiler::compile(program.circuit, np_language, &is_opcode_supported)?;
+        acvm::compiler::compile(program.circuit, np_language, is_opcode_supported)?;
 
     program.circuit = optimized_circuit;
     program.debug.update_acir(location_map);
@@ -24,7 +24,7 @@ pub fn optimize_contract(
 ) -> Result<CompiledContract, NargoError> {
     let functions = try_vecmap(contract.functions, |mut func| {
         let (optimized_bytecode, location_map) =
-            acvm::compiler::compile(func.bytecode, np_language, &is_opcode_supported)?;
+            acvm::compiler::compile(func.bytecode, np_language, is_opcode_supported)?;
         func.bytecode = optimized_bytecode;
         func.debug.update_acir(location_map);
         Ok::<_, NargoError>(func)
