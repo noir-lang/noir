bb89980e1944229c4d8110cc9852ef59a0749e12
TomAFrench <tom@tomfren.ch>
2023-09-08 16:44:08 +0000

    chore: add errors to backend CLI commands

--- crates/nargo_cli/src/cli/backend_cmd/install_cmd.rs
+++ crates/nargo_cli/src/cli/backend_cmd/install_cmd.rs
@@ -2,24 +2,29 @@ use clap::Args;
 
 use acvm_backend_barretenberg::{backends_directory, download_backend};
 
-use crate::errors::CliError;
+use crate::errors::{BackendError, CliError};
 
 use super::ls_cmd::get_available_backends;
 
-/// Install a new backend
+/// Install a new backend from a URL.
 #[derive(Debug, Clone, Args)]
 pub(crate) struct InstallCommand {
+    /// The name of the backend to install.
     backend: String,
 
+    /// The URL from which to download the backend.
     url: String,
 }
 
 pub(crate) fn run(args: InstallCommand) -> Result<(), CliError> {
     let installed_backends = get_available_backends();
 
-    assert!(!installed_backends.contains(&args.backend), "backend is already installed");
+    if installed_backends.contains(&args.backend) {
+        return Err(BackendError::AlreadyInstalled(args.backend).into());
+    }
 
-    download_backend(&args.url, &backends_directory().join(args.backend).join("backend_binary"));
+    download_backend(&args.url, &backends_directory().join(args.backend).join("backend_binary"))
+        .map_err(BackendError::from)?;
 
     Ok(())
 }
