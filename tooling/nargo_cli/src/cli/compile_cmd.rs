e956844409cf92da5370dc087b806f4544a4e9cc
Tom French <tom@tomfren.ch>
2023-09-12 18:22:13 +0100

    chore: remove type complexity lint silencing

--- tooling/nargo_cli/src/cli/compile_cmd.rs
+++ tooling/nargo_cli/src/cli/compile_cmd.rs
@@ -78,7 +78,6 @@ pub(crate) fn run(
             compile_program(package, &args.compile_options, np_language, &is_opcode_supported)
         })
         .collect();
-    #[allow(clippy::type_complexity)]
     let contract_results: Vec<(FileManager, CompilationResult<Vec<CompiledContract>>)> =
         contract_packages
             .par_iter()
