d59dec816b0cbbdc242264927e0651286517aec8
Charlie Lye <karl.lye@gmail.com>
2023-09-13 14:36:42 +0000

    Fix and update.sh now part of bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 set -eu
 
-export CLEAN=${1:-}
+export CMD=${1:-}
 
 cd "$(dirname "$0")"
 
+# Lightweight bootstrap. Run `./bootstrap.sh clean` to bypass.
+if [ -f .bootstrapped ]; then
+  echo -e '\033[1mRebuild L1 contracts...\033[0m'
+  (cd l1-contracts && forge build)
+
+  echo -e '\n\033[1mUpdate npm deps...\033[0m'
+  (cd yarn-project && yarn install)
+
+  echo -e '\n\033[1mRebuild Noir contracts...\033[0m'
+  (cd yarn-project/noir-contracts && yarn noir:build:all 2> /dev/null)
+
+  echo -e '\n\033[1mRebuild barretenberg wasm...\033[0m'
+  (cd barretenberg/cpp && cmake --build --preset wasm && cmake --build --preset wasm-threads)
+
+  echo -e '\n\033[1mRebuild circuits wasm...\033[0m'
+  (cd circuits/cpp && cmake --build --preset wasm -j --target aztec3-circuits.wasm)
+
+  exit 0
+fi
+
 # Remove all untracked files and directories.
-if [ -n "$CLEAN" ]; then
-  if [ "$CLEAN" = "clean" ]; then
+if [ -n "$CMD" ]; then
+  if [ "$CMD" = "clean" ]; then
     echo "WARNING: This will erase *all* untracked files, including hooks and submodules."
     echo -n "Continue? [y/n] "
     read user_input
     if [ "$user_input" != "y" ] && [ "$user_input" != "Y" ]; then
       exit 1
     fi
+    rm .bootstrapped
     rm -rf .git/hooks/*
     rm -rf .git/modules/*
     git clean -fd
@@ -44,3 +65,5 @@ chmod +x $HOOKS_DIR/pre-commit
 barretenberg/cpp/bootstrap.sh
 circuits/cpp/bootstrap.sh
 yarn-project/bootstrap.sh
+
+touch .bootstrapped
