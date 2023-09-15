166e72d82b65d0944b45d30369a643553aa434ed
Tom French <tom@tomfren.ch>
2023-09-15 15:13:28 +0100

    chore: clippy fixes

--- compiler/noirc_frontend/src/hir/def_collector/dc_mod.rs
+++ compiler/noirc_frontend/src/hir/def_collector/dc_mod.rs
@@ -470,7 +470,7 @@ impl<'a> ModCollector<'a> {
             };
 
         // Parse the AST for the module we just found and then recursively look for it's defs
-        let ast = parse_file(&mut context.file_manager, child_file_id, errors);
+        let ast = parse_file(&context.file_manager, child_file_id, errors);
 
         // Add module into def collector and get a ModuleId
         if let Some(child_mod_id) =
