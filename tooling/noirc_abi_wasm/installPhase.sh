4b23abf6fa990806f60c279f7a4cccad2ad1d6eb
Tom French <15848336+TomAFrench@users.noreply.github.com>
2023-09-15 18:31:07 +0100

    chore(ci): Cache yarn and playwright dependencies in CI (#2697)
    Co-authored-by: kevaundray <kevtheappdev@gmail.com>

--- tooling/noirc_abi_wasm/installPhase.sh
+++ tooling/noirc_abi_wasm/installPhase.sh
@@ -3,10 +3,6 @@ export self_path=$(dirname "$(readlink -f "$0")")
 
 mkdir -p $out
 cp $self_path/README.md $out/
+cp $self_path/package.json $out/
 cp -r $self_path/nodejs $out/
 cp -r $self_path/web $out/
-
-# The main package.json contains several keys which are incorrect/unwanted when distributing.
-cat $self_path/package.json \
-| jq 'del(.private, .devDependencies, .scripts, .packageManager)' \
-> $out/package.json
