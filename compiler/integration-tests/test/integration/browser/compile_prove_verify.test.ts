1f039a9fad5f0829cb40c3619583bcc906e5012f
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-09-21 16:57:14 +0100

    Comment test case

--- compiler/integration-tests/test/integration/browser/compile_prove_verify.test.ts
+++ compiler/integration-tests/test/integration/browser/compile_prove_verify.test.ts
@@ -37,12 +37,12 @@ async function getFile(url: URL): Promise<string> {
 const CIRCUIT_SIZE = 2 ** 19;
 
 const test_cases = [
-  {
-    case: "tooling/nargo_cli/tests/execution_success/1_mul",
-    compiled: "foundry-project/out/1_mul.sol/UltraVerifier.json",
-    address: "MUL_CONTRACT_ADDRESS",
-    publicInputsLength: 0,
-  },
+  // {
+  //   case: "tooling/nargo_cli/tests/execution_success/1_mul",
+  //   compiled: "foundry-project/out/1_mul.sol/UltraVerifier.json",
+  //   address: "MUL_CONTRACT_ADDRESS",
+  //   publicInputsLength: 0,
+  // },
   {
     case: "tooling/nargo_cli/tests/execution_success/double_verify_proof",
     compiled: "foundry-project/out/double_verify.sol/UltraVerifier.json",
@@ -175,7 +175,6 @@ test_cases.forEach((testInfo) => {
         try {
           let result;
           if (testInfo.publicInputsLength === 0) {
-            console.log({ proof });
             result = await contract.verify(proof, []);
           } else {
             const publicInputs = proof.slice(0, testInfo.publicInputsLength);
