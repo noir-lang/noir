c0a915eb09f980e3aecb559b2e1dddab2279aba3
Tom French <tom@tomfren.ch>
2023-09-26 11:15:52 +0100

    chore(noir_js): remove unnecessary input validation in JS

--- tooling/noir_js/src/witness_generation.ts
+++ tooling/noir_js/src/witness_generation.ts
@@ -1,15 +1,10 @@
 import { abiEncode } from '@noir-lang/noirc_abi';
-import { validateInputs } from './input_validation.js';
 import { base64Decode } from './base64_decode.js';
 import { WitnessMap, executeCircuit } from '@noir-lang/acvm_js';
 
 // Generates the witnesses needed to feed into the chosen proving system
 export async function generateWitness(compiledProgram, inputs): Promise<WitnessMap> {
-  // Validate inputs
-  const { isValid, error } = validateInputs(inputs, compiledProgram.abi);
-  if (!isValid) {
-    throw new Error(error?.toString());
-  }
+  // Throws on ABI encoding error
   const witnessMap = abiEncode(compiledProgram.abi, inputs, null);
 
   // Execute the circuit to generate the rest of the witnesses
