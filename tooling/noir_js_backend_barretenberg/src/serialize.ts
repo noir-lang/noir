26eeabff645423c84c98536ead9fc5f5373680c2
kevaundray <kevtheappdev@gmail.com>
2023-09-29 21:20:36 +0100

    Update tooling/noir_js_backend_barretenberg/src/serialize.ts
    Co-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>

--- tooling/noir_js_backend_barretenberg/src/serialize.ts
+++ tooling/noir_js_backend_barretenberg/src/serialize.ts
@@ -1,7 +1,7 @@
 import { decompressSync as gunzip } from 'fflate';
 import { base64Decode } from './base64_decode.js';
 
-// Converts an bytecode to a Uint8Array
+// Converts bytecode from a base64 string to a Uint8Array
 export function acirToUint8Array(base64EncodedBytecode): Uint8Array {
   const compressedByteCode = base64Decode(base64EncodedBytecode);
   return gunzip(compressedByteCode);
