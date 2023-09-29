ebaa551b459eb8cef953a225ab54b9177b596f52
kevaundray <kevtheappdev@gmail.com>
2023-09-29 11:54:43 +0000

    lint fix

--- compiler/source-resolver/types/index.d.ts
+++ compiler/source-resolver/types/index.d.ts
@@ -1,2 +1,4 @@
 export declare const read_file: (source_id: string) => string;
-export declare function initializeResolver(resolver: (source_id: string) => string): void;
+export declare function initializeResolver(
+  resolver: (source_id: string) => string,
+): void;
