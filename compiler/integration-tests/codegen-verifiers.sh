d19b4c2bb441043dc46036d407298838315ba385
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-09-29 03:04:14 -0400

    Fix cds

--- compiler/integration-tests/codegen-verifiers.sh
+++ compiler/integration-tests/codegen-verifiers.sh
@@ -5,10 +5,12 @@ cd ../..
 # Run codegen-verifier for 1_mul
 cd tooling/nargo_cli/tests/execution_success/1_mul
 nargo codegen-verifier
+cd ../../../../..
 
 # Run codegen-verifier for main
 cd compiler/integration-tests/test/circuits/main
 nargo codegen-verifier
+cd ../../../../..
 
 # Copy compiled contracts from the root of compiler/integration-tests
 cp tooling/nargo_cli/tests/execution_success/1_mul/contract/1_mul/plonk_vk.sol foundry-project/src/1_mul.sol
