0ded32c263bf9103cbe5926606eb760dc52e70f3
Koby <kobyhall@proton.me>
2023-09-29 17:47:01 +0200

    fix: smoke test

--- tooling/noir_js/test/node/smoke.test.ts
+++ tooling/noir_js/test/node/smoke.test.ts
@@ -66,7 +66,7 @@ describe('input validation', () => {
       expect.fail('Expected generatedWitness to throw, due to x not being convertible to a uint64');
     } catch (error) {
       const knownError = error as Error;
-      expect(knownError).to.equal(
+      expect(knownError.message).to.equal(
         'Expected witness values to be integers, provided value causes `invalid digit found in string` error',
       );
     }
