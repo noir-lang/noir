4b73b9e75d68eeca28156979062927b12b8c83de
kevaundray <kevtheappdev@gmail.com>
2023-09-29 09:53:08 +0000

    revert import url

--- tooling/noir_js/test/node/e2e.test.ts
+++ tooling/noir_js/test/node/e2e.test.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import assert_lt_json from '../noir_compiled_examples/assert_lt/target/assert_lt.json' assert { type: 'json' };
 import { generateWitness } from '../../src/index.js';
 import { Program } from '../../src/program.js';
-import { BarretenbergBackend as Backend } from '../../src/backend/barretenberg.js';
+import { BarretenbergBackend as Backend } from '../backend/barretenberg.js';
 
 it('end-to-end proof creation and verification (outer)', async () => {
   // Noir.Js part
