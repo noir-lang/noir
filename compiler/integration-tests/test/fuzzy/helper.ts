9e60f23b1157f7075b609377c2349ecd72a8ca26
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-09-29 00:40:35 -0400

    Prettier

--- compiler/integration-tests/test/fuzzy/helper.ts
+++ compiler/integration-tests/test/fuzzy/helper.ts
@@ -1,41 +1,47 @@
 export function getRandomString(length: number): string {
-    const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
-    }
-    return result;
+  const randomChars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += randomChars.charAt(
+      Math.floor(Math.random() * randomChars.length),
+    );
+  }
+  return result;
 }
 
 export function getRandomNumber(length: number): number {
-    const min = Math.pow(10, length - 1);
-    const max = Math.pow(10, length) - 1;
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  const min = Math.pow(10, length - 1);
+  const max = Math.pow(10, length) - 1;
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export function getRandomUint8Array(length: number): Uint8Array {
-    const result = new Uint8Array(length);
-    for (let i = 0; i < length; i++) {
-        result[i] = Math.floor(Math.random() * 256);
-    }
-    return result;
+  const result = new Uint8Array(length);
+  for (let i = 0; i < length; i++) {
+    result[i] = Math.floor(Math.random() * 256);
+  }
+  return result;
 }
 
 export function getRandomMap(length: number): Map<number, string> {
-    const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const resultMap = new Map<number, string>();
+  const randomChars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const resultMap = new Map<number, string>();
 
-    for (let i = 0; i < length; i++) {
-        const key = i;
-        const valueLength = Math.floor(Math.random() * 10) + 1;
-        let value = '';
+  for (let i = 0; i < length; i++) {
+    const key = i;
+    const valueLength = Math.floor(Math.random() * 10) + 1;
+    let value = "";
 
-        for (let j = 0; j < valueLength; j++) {
-            value += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
-        }
-
-        resultMap.set(key, value);
+    for (let j = 0; j < valueLength; j++) {
+      value += randomChars.charAt(
+        Math.floor(Math.random() * randomChars.length),
+      );
     }
 
-    return resultMap;
+    resultMap.set(key, value);
+  }
+
+  return resultMap;
 }
