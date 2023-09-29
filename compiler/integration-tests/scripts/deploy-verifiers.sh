6b8abbc1b86ee079b469d80531372a45c00bedfc
Tom French <tom@tomfren.ch>
2023-09-29 12:26:15 +0100

    chore: handle anvil gracefully locally vs in CI

--- compiler/integration-tests/scripts/deploy-verifiers.sh
+++ compiler/integration-tests/scripts/deploy-verifiers.sh
@@ -5,9 +5,19 @@ cd $self_path/../foundry-project
 
 forge build
 
-# TODO: bring anvil back to foreground when not in CI
-anvil > /dev/null 2>&1 &
+CI=${CI:-false}
+if [[ $CI = true ]]; then
+    anvil > /dev/null 2>&1 &
+else
+    anvil &
+    pid=$!
+fi
+
 sleep 10
 
 forge create --rpc-url http://127.0.0.1:8545 --mnemonic "test test test test test test test test test test test junk" src/1_mul.sol:UltraVerifier --json > mul_output.json
 forge create --rpc-url http://127.0.0.1:8545 --mnemonic "test test test test test test test test test test test junk" src/main.sol:UltraVerifier  --json > main_output.json
+
+if [[ $CI = false ]]; then
+    wait $pid
+fi
