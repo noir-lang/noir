b27bfd1ba0cefc408112d3a0288d390561f61168
kevaundray <kevtheappdev@gmail.com>
2023-09-30 16:58:02 +0000

    modify script to update artifacts

--- publish-dev-branch.sh
+++ publish-dev-branch.sh
@@ -3,10 +3,13 @@
 # Change the scope to @kevaundray so I can publish to my account for now
 bash ./change-scope.sh
 
+# Run yarn and yarn build so that the built artifacts are up to date;
+# They should now reference the changed scope package names 
+yarn && yarn build
+
 # update the versions in the workspace package.json files
 # so that they become version-{branch-name}-{commit-hash}
 bash ./update-version.sh
 
 # publish and patch the workspace dependencies
-# TODO: can move patching the workspace dependencies to the update-version script
 bash ./publish-script.sh
