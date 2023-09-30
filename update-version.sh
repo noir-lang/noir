390d0ab28b48d3f0959a8ef0ca1161baa7416c2f
kevaundray <kevtheappdev@gmail.com>
2023-09-30 16:33:06 +0000

    update dependencies

--- update-version.sh
+++ update-version.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Get current branch name
 branch_name=$(git rev-parse --abbrev-ref HEAD)
@@ -12,38 +13,66 @@ commit_hash=$(git rev-parse --short HEAD)
 # Construct the tag name
 tag_name="${branch_name}-${commit_hash}"
 
-# Set version to tag name
-newVersion=$tag_name
+# Check if working directory is clean
+# if [ -n "$(git status --porcelain)" ]; then
+#   echo "Working directory is not clean, please commit or stash your changes before publishing"
+#   exit 1
+# fi
 
-# Get the locations of all the packages in the yarn workspace
-packages=$(yarn workspaces list --json | grep location | awk -F'"' '{print $4}' | grep -v '^.$')
-
-# Check if any packages are found
-if [ -z "$packages" ]; then
-  echo "No packages found or not a valid Yarn workspace."
+# Check if yarn is installed
+if ! command -v yarn &> /dev/null; then
+  echo "Yarn is not installed, please install it first"
   exit 1
 fi
 
-# Loop over the packages and update versions
+# Get the list of workspace packages
+packages=$(yarn workspaces list --json | grep location | awk -F'"' '{print $4}' | grep -v '^.$')
+
+# Record the package name and version
+declare -A package_versions
+
+# Loop over the packages and update versions, also adjust the package.json for local dependencies
 for package_location in $packages; do
   packagePath="$package_location/package.json"
-
-  # Check if package.json exists
+  
   if [ -f "$packagePath" ]; then
-    # Extract the current version, get only the SemVer compatible part, and append the newVersion to it
+    # Extract the package name and current version from the package.json
+    package_name=$(jq -r '.name' "$packagePath")
     currentVersion=$(jq -r '.version' "$packagePath")
     
     # Extract the SemVer compatible part using a regex match
-    # This is just a safety measure so that the script is idempotent
-    # Without it, it will keep appending the newVersion to the version
     semverPart=$(echo "$currentVersion" | grep -oP '^\d+\.\d+\.\d+')
     
     # Append the newVersion to the SemVer part
-    appendedVersion="$semverPart-$newVersion"
-
+    appendedVersion="$semverPart-$tag_name"
+    
     # Update the version in package.json
     jq --arg appendedVersion "$appendedVersion" '.version = $appendedVersion' "$packagePath" > "$packagePath.tmp" && mv "$packagePath.tmp" "$packagePath"
     echo "Updated version in $packagePath to $appendedVersion"
+    
+    # Record the updated version
+    package_versions["$package_name"]=$appendedVersion
+  else
+    echo "No package.json found in $package_location"
+  fi
+done
+
+# Adjust Dependency Versions
+for package_location in $packages; do
+  packagePath="$package_location/package.json"
+  
+  if [ -f "$packagePath" ]; then
+    # Check if the package is private
+    is_private=$(jq -r '.private' "$packagePath")
+    if [ "$is_private" == "true" ]; then
+      continue
+    fi
+    
+    # Adjust the dependency versions in package.json
+    for dep_name in "${!package_versions[@]}"; do
+      dep_version=${package_versions["$dep_name"]}
+      jq '.dependencies //= {} | .devDependencies //= {} | .dependencies |= with_entries(if .key == "'$dep_name'" then .value="'$dep_version'" else . end) | .devDependencies |= with_entries(if .key == "'$dep_name'" then .value="'$dep_version'" else . end)' "$packagePath" > "$packagePath.tmp" && mv "$packagePath.tmp" "$packagePath"
+    done
   else
     echo "No package.json found in $package_location"
   fi
