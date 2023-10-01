c2d850b468d4d312ca01c4f4c43b6b72cba9e22e
kevaundray <kevtheappdev@gmail.com>
2023-10-01 13:12:18 +0000

    add init method to noir class

--- tooling/noir_js/src/program.ts
+++ tooling/noir_js/src/program.ts
@@ -1,12 +1,24 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 import { Backend, CompiledCircuit } from '@noir-lang/types';
 import { generateWitness } from './witness_generation.js';
+import initAbi from '@noir-lang/noirc_abi';
+import initACVM from '@noir-lang/acvm_js';
 
 export class Noir {
   constructor(
     private circuit: CompiledCircuit,
     private backend: Backend,
   ) {}
+
+  async init(): Promise<void> {
+    // If these are available, then we are in the
+    // web environment. For the node environment, this
+    // is a no-op.
+    if (typeof initAbi === 'function') {
+      initAbi();
+      initACVM();
+    }
+  }
 
   // Initial inputs to your program
   async generateFinalProof(inputs: any): Promise<Uint8Array> {
