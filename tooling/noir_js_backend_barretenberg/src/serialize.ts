7fcf054d7463732f0017d1ef50029a525b4f3c71
kevaundray <kevtheappdev@gmail.com>
2023-10-02 13:16:31 +0100

    chore: add init method to Noir class for web (#2920)
    Co-authored-by: Koby Hall <102518238+kobyhallx@users.noreply.github.com>
    Co-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>
    Co-authored-by: Tom French <tom@tomfren.ch>

--- tooling/noir_js_backend_barretenberg/src/serialize.ts
+++ tooling/noir_js_backend_barretenberg/src/serialize.ts
@@ -2,7 +2,7 @@ import { decompressSync as gunzip } from 'fflate';
 import { base64Decode } from './base64_decode.js';
 
 // Converts bytecode from a base64 string to a Uint8Array
-export function acirToUint8Array(base64EncodedBytecode): Uint8Array {
+export function acirToUint8Array(base64EncodedBytecode: string): Uint8Array {
   const compressedByteCode = base64Decode(base64EncodedBytecode);
   return gunzip(compressedByteCode);
 }
