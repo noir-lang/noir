8c8946dcd73187ab08069b894bf6e9e5c9739b75
guipublic <guipublic@gmail.com>
2023-10-04 15:05:37 +0000

    merge master

--- acvm-repo/acvm/src/compiler/transformers/mod.rs
+++ acvm-repo/acvm/src/compiler/transformers/mod.rs
@@ -195,6 +195,10 @@ pub(super) fn transform_internal(
                 new_acir_opcode_positions.push(acir_opcode_positions[index]);
                 transformed_opcodes.push(opcode.clone());
             }
+            Opcode::AssertEq { .. } => {
+                new_acir_opcode_positions.push(acir_opcode_positions[index]);
+                transformed_opcodes.push(opcode.clone());
+            }
         }
     }
 
