34ed2c2d4a8e12b649f428dd655a840a1282b69a
spypsy <spypsy@users.noreply.github.com>
2023-10-04 18:55:24 +0100

    fix: include resources folder in circuits.js package.json (#2689)

--- yarn-project/yarn-project-base/scripts/update_package_jsons.mjs
+++ yarn-project/yarn-project-base/scripts/update_package_jsons.mjs
@@ -30,6 +30,10 @@ function getUpdatedValue(source, target, key) {
   const value = source[key];
   if (typeof value === 'object' && !Array.isArray(value)) {
     return { ...target[key], ...value };
+    // merge required files if a project requires more than what's in common
+  } else if (key === 'files') {
+    const res = [...target[key], ...value];
+    return Array.from(new Set(res));
   } else {
     return value;
   }
