f96d3c06b4ee68eea6503dadf97d062568913617
kevaundray <kevtheappdev@gmail.com>
2023-10-05 11:01:44 +0000

    split proof into public inputs and proof

--- tooling/noir_js_types/src/types.ts
+++ tooling/noir_js_types/src/types.ts
@@ -1,16 +1,21 @@
 export interface Backend {
   // Generate an outer proof. This is the proof for the circuit which will verify
   // inner proofs and or can be seen as the proof created for regular circuits.
-  generateFinalProof(decompressedWitness: Uint8Array): Promise<Uint8Array>;
+  generateFinalProof(decompressedWitness: Uint8Array): Promise<ProofData>;
 
   // Generates an inner proof. This is the proof that will be verified
   // in another circuit.
-  generateIntermediateProof(decompressedWitness: Uint8Array): Promise<Uint8Array>;
+  generateIntermediateProof(decompressedWitness: Uint8Array): Promise<ProofData>;
 
-  verifyFinalProof(proof: Uint8Array): Promise<boolean>;
+  verifyFinalProof(proofData: ProofData): Promise<boolean>;
 
-  verifyIntermediateProof(proof: Uint8Array): Promise<boolean>;
+  verifyIntermediateProof(proofData: ProofData): Promise<boolean>;
 }
+
+export type ProofData = {
+  publicInputs: Uint8Array[];
+  proof: Uint8Array;
+};
 
 export type CompiledCircuit = {
   bytecode: string;
