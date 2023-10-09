b02ce03a3d68e919ad764ee8bf65914a326bf2fa
Tom French <tom@tomfren.ch>
2023-10-09 10:05:06 +0100

    chore: cargo fmt

--- acvm-repo/acvm/src/compiler/optimizers/mod.rs
+++ acvm-repo/acvm/src/compiler/optimizers/mod.rs
@@ -14,12 +14,11 @@ use super::{transform_assert_messages, AcirTransformationMap};
 /// Applies [`ProofSystemCompiler`][crate::ProofSystemCompiler] independent optimizations to a [`Circuit`].
 pub fn optimize(acir: Circuit) -> (Circuit, AcirTransformationMap) {
     let (mut acir, transformation_map) = optimize_internal(acir);
-    
+
     acir.assert_messages = transform_assert_messages(acir.assert_messages, &transformation_map);
 
     (acir, transformation_map)
 }
-
 
 /// Applies [`ProofSystemCompiler`][crate::ProofSystemCompiler] independent optimizations to a [`Circuit`].
 pub(super) fn optimize_internal(acir: Circuit) -> (Circuit, AcirTransformationMap) {
