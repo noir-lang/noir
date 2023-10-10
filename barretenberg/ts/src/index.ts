d9ac8080a5525b9792b7b3f10c40583536bb256c
José Pedro Sousa <github@zepedro.me>
2023-10-10 21:06:58 +0100

    feat: adding Fr back as a BB export (ts) (#2770)
    Smallest PR ever. closes #2369
    
    `Fr` stopped being exported since `bb.js` 0.0.1, and it's a very useful
    util class for getting field values from strings, buffers, or to strings
    and buffers.
    
    We've been getting requests for this from Noir developers, and devrel
    actually needs this in order to refactor the
    [Stealthdrop](https://github.com/noir-lang/noir-examples/tree/master/stealthdrop)
    example.
    
    # Checklist:
    Remove the checklist to signal you've completed it. Enable auto-merge if
    the PR is ready to merge.
    - [x] If the pull request requires a cryptography review (e.g.
    cryptographic algorithm implementations) I have added the 'crypto' tag.
    - [x] I have reviewed my diff in github, line by line and removed
    unexpected formatting changes, testing logs, or commented-out code.
    - [x] Every change is related to the PR description.
    - [x] I have
    [linked](https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue)
    this pull request to relevant issues (if any exist).

--- barretenberg/ts/src/index.ts
+++ barretenberg/ts/src/index.ts
@@ -1,3 +1,3 @@
 export { Crs } from './crs/index.js';
 export { Barretenberg } from './barretenberg/index.js';
-export { RawBuffer } from './types/index.js';
+export { RawBuffer, Fr } from './types/index.js';
