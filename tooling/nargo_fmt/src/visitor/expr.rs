6ffcad670e50a2ddcc27362431a1fa4d08fe45b8
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-10-11 17:17:58 +0100

    Add member access

--- tooling/nargo_fmt/src/visitor/expr.rs
+++ tooling/nargo_fmt/src/visitor/expr.rs
@@ -39,6 +39,10 @@ impl FmtVisitor<'_> {
                     self.format_expr(infix.rhs)
                 )
             }
+            ExpressionKind::MemberAccess(member_access_expr) => {
+                let lhs_str = self.format_expr(member_access_expr.lhs);
+                format!("{}.{}", lhs_str, member_access_expr.rhs)
+            }
             ExpressionKind::Literal(literal) => match literal {
                 Literal::Integer(_) => slice!(self, span.start(), span.end()).to_string(),
                 Literal::Array(ArrayLiteral::Repeated { repeated_element, length }) => {
