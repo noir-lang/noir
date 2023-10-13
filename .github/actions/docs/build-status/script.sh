dad22d74fb14c5cc295938e5335459b65a219e33
José Pedro Sousa <github@zepedro.me>
2023-10-13 23:25:56 +0100

    testing for failure

--- .github/actions/docs/build-status/script.sh
+++ .github/actions/docs/build-status/script.sh
@@ -6,7 +6,7 @@ DEPLOY_STATUS=$(curl -X GET "https://api.netlify.com/api/v1/sites/1b11824b-a5b7-
 MAX_RETRIES=10
 COUNT=0
 while [[ "$DEPLOY_STATUS" != "ready" && $COUNT -lt $MAX_RETRIES ]]; do
-    sleep 60
+    sleep 20
     DEPLOY_STATUS=$(curl -X GET "https://api.netlify.com/api/v1/sites/1b11824b-a5b7-4872-8c76-aedbe0ac867c/deploys?branch=$BRANCH_NAME"  | jq -r '.[] | select(.created_at != null) | .state' | head -1)
     COUNT=$((COUNT+1))
 
@@ -14,12 +14,12 @@ while [[ "$DEPLOY_STATUS" != "ready" && $COUNT -lt $MAX_RETRIES ]]; do
     if [[ "$DEPLOY_STATUS" == "ready" ]]; then
         echo "::set-output name=deploy_status::success"
         exit 0
-    else if [[ "$DEPLOY_STATUS" == "failure" ]]; then
+    elif [[ "$DEPLOY_STATUS" == "failure" ]]; then
         echo "::set-output name=deploy_status::failure"
         exit 1
-    else
-        echo "Deploy still running. Retrying in 60 seconds..."
     fi
+
+    echo "Deploy still running. Retrying..."
 done
 
 exit 1
