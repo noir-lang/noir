dc88ea08f929d2284d0baec6d87382e7d6eaa582
José Pedro Sousa <github@zepedro.me>
2023-10-13 20:38:12 +0100

    testing conditional comments

--- .github/actions/docs/build-status/script.sh
+++ .github/actions/docs/build-status/script.sh
@@ -6,15 +6,17 @@ DEPLOY_STATUS=$(curl -X GET "https://api.netlify.com/api/v1/sites/1b11824b-a5b7-
 MAX_RETRIES=10
 COUNT=0
 while [[ "$DEPLOY_STATUS" != "ready" && $COUNT -lt $MAX_RETRIES ]]; do
-sleep 60
-DEPLOY_STATUS=$(curl -X GET "https://api.netlify.com/api/v1/sites/1b11824b-a5b7-4872-8c76-aedbe0ac867c/deploys?branch=$BRANCH_NAME"  | jq -r '.[] | select(.created_at != null) | .state' | head -1)
-COUNT=$((COUNT+1))
+    sleep 60
+    DEPLOY_STATUS=$(curl -X GET "https://api.netlify.com/api/v1/sites/1b11824b-a5b7-4872-8c76-aedbe0ac867c/deploys?branch=$BRANCH_NAME"  | jq -r '.[] | select(.created_at != null) | .state' | head -1)
+    COUNT=$((COUNT+1))
 
-echo $DEPLOY_STATUS
+    # If deploy status is ready, set the output and exit successfully
+    if [[ "$DEPLOY_STATUS" == "ready" ]]; then
+        echo "::set-output name=deploy_status::$DEPLOY_STATUS"
+        exit 0
+    fi
 done
 
-# If deploy status isn't ready after all retries, fail the CI
-if [[ "$DEPLOY_STATUS" != "ready" ]]; then
 echo "Deploy failed or took too long."
+echo "::set-output name=deploy_status::$DEPLOY_STATUS"
 exit 1
-fi
