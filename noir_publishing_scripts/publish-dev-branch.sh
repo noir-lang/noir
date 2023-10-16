2ea35d3555fb0e40ad7407f1976563e60501f331
kevaundray <kevtheappdev@gmail.com>
2023-10-16 20:39:32 +0000

    add publishing scripts

---
+++ noir_publishing_scripts/publish-dev-branch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if working directory is clean
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Working directory is not clean, please commit or stash your changes before publishing"
+  exit 1
+fi
+
+# Change the scope to @kevaundray so I can publish to my account for now
+bash ./scripts/change-scope.sh || { echo "Changing scope script failed"; exit 1; }
+
+# Run yarn and yarn build so that the built artifacts are up to date;
+# They should now reference the changed scope package names 
+yarn && yarn build
+
+# update the versions in the workspace package.json files
+# so that they become version-{branch-name}-{commit-hash}
+bash ./scripts/update-version.sh || { echo "Update version script failed"; exit 1; }
+
+# publish and patch the workspace dependencies
+bash ./scripts/publish-script.sh || { echo "Publish script failed"; exit 1; }
+
+# The above processes will modify a lot of files
+# Lets reset the changes that were made
+git restore --source=HEAD --staged --worktree -- .
---
+++ noir_publishing_scripts/publish-script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+# Check if yarn is installed
+if ! command -v yarn &> /dev/null; then
+  echo "Yarn is not installed, please install it first"
+  exit 1
+fi
+
+# Get the list of workspace packages
+packages=$(yarn workspaces list --json | grep location | awk -F'"' '{print $4}' | grep -v '^.$')
+
+# Loop over the packages and publish them
+for package_location in $packages; do
+  cd $package_location
+  
+  # Extract the package name from the package.json
+  package_name=$(jq -r .name package.json)
+
+  # Check if the package is private
+  is_private=$(jq -r .private package.json)
+  if [ "$is_private" == "true" ]; then
+    cd -
+    continue
+  fi
+  
+  # Publish the package with the constructed tag name
+  npm publish --tag dev --access public
+  
+  cd -
+done
