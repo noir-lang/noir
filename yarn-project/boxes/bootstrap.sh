a3b18048479ee3bed3931615e5eabd27efacd404
Dan Lee <142251406+dan-aztec@users.noreply.github.com>
2023-10-18 03:23:32 -0700

    fix: boxes boostrap dont use ts-node directly and add .prettierignore (#2890)
    since ts-node is available in subpackage, but not in yarn-packages in
    general

--- yarn-project/boxes/bootstrap.sh
+++ yarn-project/boxes/bootstrap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # should be run from yarn-project/boxes
 original_path=$(pwd)
 
@@ -9,16 +8,12 @@ for dir in *; do
   # Check if it's a directory
   if [ -d "${dir}" ]; then
     
+    cd "${dir}"
     # Run the compile command
     echo "Running compile command inside ${dir}..."
 
-    # Change directory to ../cli
-    cd ../cli
-    
-    # Run ts-node command to use latest "compile" code
-    ts-node --esm src/bin/index.ts compile "${original_path}/${dir}/src/contracts" --outdir "${original_path}/${dir}/src/artifacts" --typescript "${original_path}/${dir}/src/artifacts"
-
-    yarn formatting:fix
+    # Runs ts-node command from "../cli" to use latest "compile" code.  this uses the yarn command to use the subpackage ts-node dep
+    yarn compile:local
     
     # Change back to the original directory
     cd "${original_path}"
