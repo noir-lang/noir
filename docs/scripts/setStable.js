e447b5cab10e8f38a79cdb681754095d3a7e0d5e
José Pedro Sousa <github@zepedro.me>
2023-10-26 08:23:44 +0100

    cant test the workflow if not on master, wtf

---
+++ docs/scripts/setStable.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+function main() {
+  const configFile = path.join('docusaurus.config.js');
+
+  // Read the Docusaurus config
+  const configContent = fs.readFileSync(configFile, 'utf8');
+
+  // Update the lastVersion property
+  const newVersion = process.env.STABLE; // Get the version from the script argument
+  if (!newVersion) {
+    console.log('No stable version provided');
+    process.exit(1);
+  }
+  const updatedContent = configContent.replace(/lastVersion: '[^']+'/, `lastVersion: '${newVersion}'`);
+
+  // Write the updated content back
+  fs.writeFileSync(configFile, updatedContent, 'utf8');
+}
+
+main();
