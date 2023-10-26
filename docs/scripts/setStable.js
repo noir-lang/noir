72864ea4b9c3a33185bbe5b02281d9fbccdf86bf
José Pedro Sousa <github@zepedro.me>
2023-10-26 17:30:54 +0100

    getting somewhere...

--- docs/scripts/setStable.js
+++ docs/scripts/setStable.js
@@ -6,17 +6,19 @@ function main() {
   const configFile = path.join('docusaurus.config.js');
 
   // Read the Docusaurus config
-  const configContent = fs.readFileSync(configFile, 'utf8');
+  const config = require(configFile);
 
-  // Update the lastVersion property
-  const newVersion = process.env.STABLE.replace('v', ''); // Get the version from the script argument
-  if (!newVersion) {
-    process.exit(1);
-  }
-  const updatedContent = configContent.replace(/lastVersion: '[^']+'/, `lastVersion: '${newVersion}'`);
+  config.presets[0][1].docs.lastVersion = process.env.STABLE.replace('v', ''); // Get the version from the script argument
+
+  // // Update the lastVersion property
+  // const newVersion = process.env.STABLE.replace('v', ''); // Get the version from the script argument
+  // if (!newVersion) {
+  //   process.exit(1);
+  // }
+  // const updatedContent = configContent.replace(/lastVersion: '[^']+'/, `lastVersion: '${newVersion}'`);
 
   // Write the updated content back
-  fs.writeFileSync(configFile, updatedContent, 'utf8');
+  fs.writeFileSync(configFile, config, 'utf8');
 }
 
 main();
