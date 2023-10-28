42188af3bf0a2fe6bd537191a9f1541495b135cb
José Pedro Sousa <github@zepedro.me>
2023-10-28 14:52:35 +0100

    another strategy

--- docs/scripts/setStable.js
+++ docs/scripts/setStable.js
@@ -2,6 +2,10 @@
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
+const { release } = require('os');
+
+const IGNORE_VERSIONS = ['0.16.0'];
+const NUMBER_OF_VERSIONS_TO_SHOW = 2;
 
 async function main() {
   const versionsFile = path.join(__dirname, '../versions.json');
@@ -13,9 +17,9 @@ async function main() {
 
   const stables = data
     .filter((release) => !release.prerelease && !release.tag_name.includes('aztec'))
+    .filter((release) => !IGNORE_VERSIONS.includes(release.tag_name.replace('v', '')))
     .map((release) => release.tag_name.replace('v', ''))
-    .slice(0, 3);
-
+    .slice(0, NUMBER_OF_VERSIONS_TO_SHOW);
   fs.writeFileSync(versionsFile, JSON.stringify(stables, null, 2));
 }
 
