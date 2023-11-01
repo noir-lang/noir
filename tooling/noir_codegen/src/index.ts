384d71f9fe24a56eeade11889d44a54124e84ca3
TomAFrench <tom@tomfren.ch>
2023-11-01 19:49:57 +0000

    feat: add `noir_codegen` package

---
+++ tooling/noir_codegen/src/index.ts
@@ -0,0 +1,25 @@
+import { CompiledCircuit } from "@noir-lang/types";
+
+const codegenImports = `import { InputMap, InputValue } from "@noir-lang/noirc_abi"
+import { Noir } from "@noir-lang/noir_js"`;
+
+
+const codegenFunction = (name: string, compiled_program: CompiledCircuit) => `export async function ${name}(args: InputMap): Promise<InputValue> {
+  const program = new Noir(${JSON.stringify(compiled_program)});
+  const { returnValue } = await program.execute(args);
+  return returnValue
+}`
+
+export const codegen = (programs: [string, CompiledCircuit][]): string => {
+  let results = [codegenImports];
+  for (const [name, program] of programs) {
+    results.push(codegenFunction(name, stripUnwantedFields(program)));
+  }
+
+  return results.join("\n\n");
+}
+
+function stripUnwantedFields(value: any): CompiledCircuit {
+  const { abi, bytecode } = value;
+  return { abi, bytecode };
+}
---
+++ tooling/noir_codegen/test/index.test.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import { assert_lt } from "./codegen/index.js";
+
+it('codegens a callable function', async () => {
+  const result = await assert_lt({
+    x: '2',
+    y: '3',
+  })
+
+  expect(result).to.be.eq("0x05");
+});
+
