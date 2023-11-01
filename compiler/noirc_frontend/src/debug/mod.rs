e610fce4bf0463f9e400db60b4f4d1923685c3d9
synthia <sy@nthia.dev>
2023-10-31 20:50:52 -0700

    tuple and struct destructuring somewhat working for vars

--- compiler/noirc_frontend/src/debug/mod.rs
+++ compiler/noirc_frontend/src/debug/mod.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use crate::parser::ParsedModule;
 use crate::{ast, parser::{Item,ItemKind}, ast::{Path,PathKind,UseTreeKind}};
 use noirc_errors::{Span, Spanned};
+use std::collections::VecDeque;
 
 #[derive(Debug,Clone)]
 pub struct DebugState {
@@ -76,14 +77,12 @@ impl DebugState {
     fn walk_expr(&mut self, _expr: &mut ast::Expression) {
     }
 
-    fn debug_expr(&mut self, var_name: &str, expr: ast::Expression) -> ast::Expression {
+    fn wrap_var_expr(&mut self, var_name: &str, expr: ast::Expression) -> ast::Expression {
         let var_id = self.insert_var(var_name);
         let kind = ast::ExpressionKind::Call(Box::new(ast::CallExpression {
             func: Box::new(ast::Expression {
                 kind: ast::ExpressionKind::Variable(ast::Path {
-                    segments: vec![ast::Ident(
-                        Spanned::from(Span::single_char(0), "__debug_state_set".to_string())
-                    )],
+                    segments: vec![ident("__debug_state_set")],
                     kind: PathKind::Plain
                 }),
                 span: Span::single_char(0),
@@ -101,24 +100,105 @@ impl DebugState {
         ast::Expression { kind, span: Span::single_char(0) }
     }
 
+    fn wrap_let_statement(&mut self, let_stmt: &ast::LetStatement) -> ast::Statement {
+        // rewrites let statements written like this:
+        //   let (((a,b,c),D { d }),e,f) = x;
+        //
+        // into statements like this:
+        //
+        //   let (a,b,c,d,e,f,g) = {
+        //     let (((a,b,c),D { d }),e,f) = x;
+        //     wrap(a);
+        //     wrap(b);
+        //     ...
+        //     wrap(f);
+        //     (a,b,c,d,e,f,g)
+        //   };
+
+        let vars = pattern_vars(&let_stmt.pattern);
+        let vars_pattern: Vec<ast::Pattern> = vars.iter().map(|id| {
+            ast::Pattern::Identifier(id.clone())
+        }).collect();
+        let vars_exprs: Vec<ast::Expression> = vars.iter().map(|id| id_expr(id)).collect();
+
+        let mut block_stmts = vec![
+            ast::Statement {
+                kind: ast::StatementKind::Let(let_stmt.clone()),
+                span: Span::single_char(0),
+            },
+        ];
+        block_stmts.extend(vars.iter().map(|id| {
+            let var_name = &id.0.contents;
+            ast::Statement {
+                kind: ast::StatementKind::Semi(self.wrap_var_expr(var_name, id_expr(id))),
+                span: Span::single_char(0),
+            }
+        }));
+        block_stmts.push(ast::Statement {
+            kind: ast::StatementKind::Expression(ast::Expression {
+                kind: ast::ExpressionKind::Tuple(vars_exprs),
+                span: Span::single_char(0),
+            }),
+            span: Span::single_char(0),
+        });
+
+        ast::Statement {
+            kind: ast::StatementKind::Let(ast::LetStatement {
+                pattern: ast::Pattern::Tuple(vars_pattern, Span::single_char(0)),
+                r#type: ast::UnresolvedType::unspecified(),
+                expression: ast::Expression {
+                    kind: ast::ExpressionKind::Block(ast::BlockExpression(block_stmts)),
+                    span: Span::single_char(0),
+                },
+            }),
+            span: Span::single_char(0),
+        }
+    }
+
     fn walk_statement(&mut self, stmt: &mut ast::Statement) {
         match &mut stmt.kind {
-            ast::StatementKind::Let(ref mut let_stmt) => {
-                match &let_stmt.pattern {
-                    ast::Pattern::Identifier(id) => {
-                        let expr = let_stmt.expression.clone();
-                        let_stmt.expression = self.debug_expr(&id.0.contents, expr);
-                    },
-                    ast::Pattern::Mutable(_, _) => {
-                    },
-                    ast::Pattern::Tuple(_, _) => {
-                    },
-                    ast::Pattern::Struct(_, _, _) => {
-                    },
-                }
-                self.walk_expr(&mut let_stmt.expression);
+            ast::StatementKind::Let(let_stmt) => {
+                *stmt = self.wrap_let_statement(&let_stmt);
             },
             _ => {},
         }
+    }
+}
+
+fn pattern_vars(pattern: &ast::Pattern) -> Vec<ast::Ident> {
+    let mut vars = vec![];
+    let mut stack = VecDeque::from([ pattern ]);
+    while stack.front().is_some() {
+        let pattern = stack.pop_front().unwrap();
+        match pattern {
+            ast::Pattern::Identifier(id) => {
+                vars.push(id.clone());
+            },
+            ast::Pattern::Mutable(pattern, _) => {
+                stack.push_back(pattern);
+            },
+            ast::Pattern::Tuple(patterns, _) => {
+                stack.extend(patterns.iter());
+            },
+            ast::Pattern::Struct(_, pids, _) => {
+                stack.extend(pids.iter().map(|(_, pattern)| pattern));
+                vars.extend(pids.iter().map(|(id, _)| id.clone()));
+            },
+        }
+    }
+    vars
+}
+
+fn ident(s: &str) -> ast::Ident {
+    ast::Ident(Spanned::from(Span::single_char(0), s.to_string()))
+}
+
+fn id_expr(id: &ast::Ident) -> ast::Expression {
+    ast::Expression {
+        kind: ast::ExpressionKind::Variable(Path {
+            segments: vec![id.clone()],
+            kind: PathKind::Plain,
+        }),
+        span: Span::single_char(0),
     }
 }
