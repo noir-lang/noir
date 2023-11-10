422d13fa537b5f2148831a5cd9f43ded585a323c
Gustavo Giráldez <ggiraldez@manas.tech>
2023-11-09 19:14:53 -0500

    WIP

--- tooling/debugger/src/lib.rs
+++ tooling/debugger/src/lib.rs
@@ -1,20 +1,23 @@
 mod context;
 mod repl;
 
-use std::io::{BufReader, BufWriter};
+use std::io::{Read, Write};
 
 use acvm::BlackBoxFunctionSolver;
 use acvm::{acir::circuit::Circuit, acir::native_types::WitnessMap};
 
+use context::DebugContext;
 use dap::errors::ServerError;
+use dap::events::StoppedEventBody;
 use dap::prelude::Event;
 use dap::requests::Command;
-use dap::responses;
+use dap::responses::{ResponseBody, StackTraceResponse, ThreadsResponse};
 use dap::server::Server;
-use dap::types;
+use dap::types::{Source, StackFrame, StoppedEventReason, Thread};
 use nargo::artifacts::debug::DebugArtifact;
 
 use nargo::NargoError;
+use noirc_driver::CompiledProgram;
 
 pub fn debug_circuit<B: BlackBoxFunctionSolver>(
     blackbox_solver: &B,
@@ -25,28 +28,131 @@ pub fn debug_circuit<B: BlackBoxFunctionSolver>(
     repl::run(blackbox_solver, circuit, &debug_artifact, initial_witness)
 }
 
-pub fn start_dap_server() -> Result<(), ServerError> {
-    let output = BufWriter::new(std::io::stdout());
-    let input = BufReader::new(std::io::stdin());
-    let mut server = Server::new(input, output);
+fn send_stopped_event<R: Read, W: Write>(
+    server: &mut Server<R, W>,
+    reason: StoppedEventReason,
+) -> Result<(), ServerError> {
+    let description = format!("{:?}", &reason);
+    server.send_event(Event::Stopped(StoppedEventBody {
+        reason,
+        description: Some(description),
+        thread_id: Some(0),
+        preserve_focus_hint: Some(false),
+        text: None,
+        all_threads_stopped: Some(false),
+        hit_breakpoint_ids: None,
+    }))?;
+    Ok(())
+}
+
+pub fn loop_initialized<R: Read, W: Write, B: BlackBoxFunctionSolver>(
+    mut server: Server<R, W>,
+    solver: &B,
+    program: CompiledProgram,
+    initial_witness: WitnessMap,
+) -> Result<(), ServerError> {
+    let debug_artifact = DebugArtifact {
+        debug_symbols: vec![program.debug.clone()],
+        file_map: program.file_map.clone(),
+        warnings: program.warnings.clone(),
+    };
+    let mut context =
+        DebugContext::new(solver, &program.circuit, &debug_artifact, initial_witness.clone());
+
+    if matches!(context.get_current_source_location(), None) {
+        // FIXME: remove this?
+        _ = context.next();
+    }
+
+    server.send_event(Event::Initialized)?;
+    send_stopped_event(&mut server, StoppedEventReason::Entry)?;
 
     loop {
         let req = match server.poll_request()? {
             Some(req) => req,
             None => break,
         };
-        if let Command::Initialize(_) = req.command {
-            let rsp = req.success(responses::ResponseBody::Initialize(types::Capabilities {
-                ..Default::default()
-            }));
-
-            // When you call respond, send_event etc. the message will be wrapped
-            // in a base message with a appropriate seq number, so you don't have to keep track of that yourself
-            server.respond(rsp)?;
-
-            server.send_event(Event::Initialized)?;
-        } else {
-            eprintln!("ERROR: unhandled command");
+        match req.command {
+            Command::Disconnect(_) => {
+                eprintln!("INFO: ending debugging session");
+                server.respond(req.ack()?)?;
+                break;
+            }
+            Command::SetBreakpoints(ref args) => {
+                eprintln!("INFO: Received SetBreakpoints {:?}", args);
+                // FIXME: return the breakpoints actually set
+            }
+            Command::Threads => {
+                server.respond(req.success(ResponseBody::Threads(ThreadsResponse {
+                    threads: vec![Thread { id: 0, name: "main".to_string() }],
+                })))?;
+            }
+            Command::StackTrace(_) => {
+                let source_location = context.get_current_source_location();
+                eprintln!("{:?}", source_location);
+                let frames = match source_location {
+                    None => vec![],
+                    Some(locations) => locations
+                        .iter()
+                        .enumerate()
+                        .map(|(index, location)| {
+                            let line_number =
+                                debug_artifact.location_line_number(*location).unwrap();
+                            let column_number =
+                                debug_artifact.location_column_number(*location).unwrap();
+                            StackFrame {
+                                id: index as i64,
+                                name: format!("frame #{index}"),
+                                source: Some(Source {
+                                    name: None,
+                                    path: debug_artifact.file_map[&location.file]
+                                        .path
+                                        .to_str()
+                                        .map(|s| String::from(s)),
+                                    source_reference: None,
+                                    presentation_hint: None,
+                                    origin: None,
+                                    sources: None,
+                                    adapter_data: None,
+                                    checksums: None,
+                                }),
+                                line: line_number as i64,
+                                column: column_number as i64,
+                                end_line: None,
+                                end_column: None,
+                                can_restart: None,
+                                instruction_pointer_reference: None,
+                                module_id: None,
+                                presentation_hint: None,
+                            }
+                        })
+                        .collect(),
+                };
+                eprintln!("{:?}", frames);
+                let total_frames = Some(frames.len() as i64);
+                server.respond(req.success(ResponseBody::StackTrace(StackTraceResponse {
+                    stack_frames: frames,
+                    total_frames,
+                })))?;
+            }
+            Command::Next(_) | Command::StepIn(_) | Command::StepOut(_) => {
+                let result = context.next();
+                eprintln!("INFO: stepped with result {result:?}");
+                match result {
+                    context::DebugCommandResult::Done => {
+                        server.respond(req.success(ResponseBody::Terminate))?;
+                        break;
+                    }
+                    _ => {
+                        server.respond(req.ack()?)?;
+                        send_stopped_event(&mut server, StoppedEventReason::Step)?
+                    },
+                }
+            }
+            _ => {
+                eprintln!("{:?}", req.command);
+                eprintln!("ERROR: unhandled command");
+            }
         }
     }
     Ok(())
--- tooling/nargo_cli/src/cli/dap_cmd.rs
+++ tooling/nargo_cli/src/cli/dap_cmd.rs
@@ -1,6 +1,24 @@
 use backend_interface::Backend;
 use clap::Args;
+use nargo::constants::PROVER_INPUT_FILE;
+use nargo::workspace::Workspace;
+use nargo_toml::{get_package_manifest, resolve_workspace_from_toml, PackageSelection};
+use noirc_abi::input_parser::Format;
+use noirc_driver::{CompileOptions, NOIR_ARTIFACT_VERSION_STRING};
+use noirc_frontend::graph::CrateName;
 
+use std::io::{BufReader, BufWriter, Read, Write};
+use std::path::Path;
+
+use dap::errors::ServerError;
+use dap::requests::Command;
+use dap::responses::ResponseBody;
+use dap::server::Server;
+use dap::types::Capabilities;
+use serde_json::Value;
+
+use super::compile_cmd::compile_bin_package;
+use super::fs::inputs::read_inputs_from_file;
 use crate::errors::CliError;
 
 use super::NargoConfig;
@@ -8,10 +26,130 @@ use super::NargoConfig;
 #[derive(Debug, Clone, Args)]
 pub(crate) struct DapCommand;
 
+fn find_workspace(project_folder: &str, package: Option<&str>) -> Option<Workspace> {
+    let Ok(toml_path) = get_package_manifest(Path::new(project_folder)) else {
+        return None;
+    };
+    let package = package.and_then(|p| serde_json::from_str::<CrateName>(p).ok());
+    let selection = package.map_or(PackageSelection::DefaultOrAll, PackageSelection::Selected);
+    let Ok(workspace) = resolve_workspace_from_toml(
+        &toml_path,
+        selection,
+        Some(NOIR_ARTIFACT_VERSION_STRING.to_string()),
+    ) else {
+        return None;
+    };
+    Some(workspace)
+}
+
+fn loop_uninitialized<R: Read, W: Write>(
+    mut server: Server<R, W>,
+    backend: &Backend,
+) -> Result<(), ServerError> {
+    loop {
+        let req = match server.poll_request()? {
+            Some(req) => req,
+            None => break,
+        };
+        match req.command {
+            Command::Initialize(_) => {
+                let rsp =
+                    req.success(ResponseBody::Initialize(Capabilities { ..Default::default() }));
+                server.respond(rsp)?;
+            }
+
+            Command::Launch(ref arguments) => {
+                if let Some(Value::Object(ref data)) = arguments.additional_data {
+                    if let Some(Value::String(ref project_folder)) = data.get("projectFolder") {
+                        let project_folder = project_folder.as_str();
+                        let package = data.get("package").and_then(|v| v.as_str());
+                        let prover_name = data
+                            .get("proverName")
+                            .and_then(|v| v.as_str())
+                            .unwrap_or(PROVER_INPUT_FILE);
+
+                        eprintln!("Project folder: {}", project_folder);
+                        eprintln!("Package: {}", package.unwrap_or("(none)"));
+                        eprintln!("Prover name: {}", prover_name);
+
+                        let Some(workspace) = find_workspace(project_folder, package) else {
+                            server.respond(req.error("Cannot open workspace"))?;
+                            continue;
+                        };
+                        let Ok((np_language, opcode_support)) = backend.get_backend_info() else {
+                            server.respond(req.error("Failed to get backend info"))?;
+                            continue;
+                        };
+
+                        let Some(package) = workspace.into_iter().find(|p| p.is_binary()) else {
+                            server.respond(req.error("No matching binary packages found in workspace"))?;
+                            continue;
+                        };
+                        let Ok(compiled_program) = compile_bin_package(
+                            &workspace,
+                            package,
+                            &CompileOptions::default(),
+                            np_language,
+                            &|opcode| opcode_support.is_opcode_supported(opcode),
+                        ) else {
+                            server.respond(req.error("Failed to compile project"))?;
+                            continue;
+                        };
+                        let Ok((inputs_map, _)) = read_inputs_from_file(
+                            &package.root_dir,
+                            prover_name,
+                            Format::Toml,
+                            &compiled_program.abi,
+                        ) else {
+                            server.respond(req.error("Failed to read program inputs"))?;
+                            continue;
+                        };
+                        let Ok(initial_witness) = compiled_program.abi.encode(&inputs_map, None) else {
+                            server.respond(req.error("Failed to encode inputs"))?;
+                            continue;
+                        };
+                        #[allow(deprecated)]
+                        let blackbox_solver =
+                            barretenberg_blackbox_solver::BarretenbergSolver::new();
+
+                        server.respond(req.ack()?)?;
+                        noir_debugger::loop_initialized(
+                            server,
+                            &blackbox_solver,
+                            compiled_program,
+                            initial_witness,
+                        )?;
+                        break;
+                    } else {
+                        server.respond(req.error("Missing project folder argument"))?;
+                    }
+                } else {
+                    server.respond(req.error("Missing launch arguments"))?;
+                }
+            }
+
+            Command::Disconnect(_) => {
+                server.respond(req.ack()?)?;
+                break;
+            }
+
+            _ => {
+                eprintln!("ERROR: unhandled command");
+            }
+        }
+    }
+    Ok(())
+}
+
 pub(crate) fn run(
-    _backend: &Backend,
+    backend: &Backend,
     _args: DapCommand,
     _config: NargoConfig,
 ) -> Result<(), CliError> {
-    noir_debugger::start_dap_server().map_err(CliError::DapError)
+    // noir_debugger::start_dap_server(backend).map_err(CliError::DapError);
+    let output = BufWriter::new(std::io::stdout());
+    let input = BufReader::new(std::io::stdin());
+    let server = Server::new(input, output);
+
+    loop_uninitialized(server, backend).map_err(CliError::DapError)
 }
