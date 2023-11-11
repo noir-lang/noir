2eec47b94e1e0eaceb92850a66403d213067390e
TomAFrench <tom@tomfren.ch>
2023-11-11 08:32:16 +0000

    feat: add typed interface to `noir_codegen`

--- tooling/noir_codegen/test/index.test.ts
+++ tooling/noir_codegen/test/index.test.ts
@@ -1,11 +1,8 @@
 import { expect } from 'chai';
-import { assert_lt } from './codegen/index.js';
+import { assert_lt, u64 } from './codegen/index.js';
 
 it('codegens a callable function', async () => {
-  const result = await assert_lt({
-    x: '2',
-    y: '3',
-  });
+  const result: u64 = await assert_lt('2', '3', [0, 0, 0, 0, 0], { foo: true, bar: ['12345', '12345', '12345'] });
 
   expect(result).to.be.eq('0x05');
 });
