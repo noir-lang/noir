2dc44819af08d20ede221c798e027acbadcc2d5a
f01dab1e <andriy.n@obox.systems>
2023-11-16 18:02:01 +0000

    :)

--- tooling/nargo_fmt/src/visitor/item.rs
+++ tooling/nargo_fmt/src/visitor/item.rs
@@ -4,7 +4,7 @@ use noirc_frontend::{
     Distinctness, NoirFunction, ParsedModule, Visibility,
 };
 
-use crate::utils::last_line_contains_single_line_comment;
+use crate::{utils::last_line_contains_single_line_comment, visitor::expr::format_expr_seq};
 
 use super::expr::Tactic::LimitedHorizontalVertical;
 
@@ -39,15 +39,7 @@ impl super::FmtVisitor<'_> {
 
             let generics = func.def.generics;
             let span = (start..end).into();
-            let generics = crate::visitor::expr::format_expr_seq(
-                "<",
-                ">",
-                self.fork(),
-                false,
-                generics,
-                span,
-                tactic,
-            );
+            let generics = format_expr_seq("<", ">", self.fork(), false, generics, span, tactic);
 
             result.push_str(&generics);
         }
@@ -55,15 +47,7 @@ impl super::FmtVisitor<'_> {
         let parameters = if parameters.is_empty() {
             self.slice(params_span).into()
         } else {
-            crate::visitor::expr::format_expr_seq(
-                "(",
-                ")",
-                self.fork(),
-                false,
-                parameters,
-                params_span.into(),
-                tactic,
-            )
+            format_expr_seq("(", ")", self.fork(), false, parameters, params_span.into(), tactic)
         };
 
         result.push_str(&parameters);
