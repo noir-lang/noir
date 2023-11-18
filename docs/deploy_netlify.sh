624d7339d6bd9755156487a07c553b00f92c3b4b
ludamad <adam.domurad@gmail.com>
2023-11-18 17:20:59 +0300

    feat: deploy docs from CCI w/ netlify-cli (#3348)
    We're just going to accept that docs reflect the entire monorepo,
    because:
    
    - needs to make snippets from live code, we don't want that code living
    anywhere special, just doc directives where the code lies
    - we want to run tsdoc, that needs ~everything because of codegen,
    typescript running etc
    
    And netlify CI isn't really meant for this level of complexity. We use
    netlify-cli instead.
    
    ---------
    
    Co-authored-by: ludamad <adam@aztecprotocol.com>

---
+++ docs/deploy_netlify.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+[ -n "${BUILD_SYSTEM_DEBUG:-}" ] && set -x # conditionally trace
+set -eu
+
+extract_repo docs /usr/src extracted-repo
+cd extracted-repo/src/docs
+npm install netlify-cli -g
+netlify deploy --site aztec-docs-dev
+netlify deploy --site aztec-docs-dev --prod
