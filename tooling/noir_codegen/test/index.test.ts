8730a88f3a0a96a6c7117f55aac452b2d76152b5
Tom French <tom@tomfren.ch>
2023-11-22 14:27:06 +0000

    chore: add top-level string argument

--- tooling/noir_codegen/test/index.test.ts
+++ tooling/noir_codegen/test/index.test.ts
@@ -2,7 +2,13 @@ import { expect } from 'chai';
 import { assert_lt, u64 } from './codegen/index.js';
 
 it('codegens a callable function', async () => {
-  const result: u64 = await assert_lt('2', '3', [0, 0, 0, 0, 0], { foo: true, bar: ['12345', '12345', '12345'] });
+  const result: u64 = await assert_lt(
+    '2',
+    '3',
+    [0, 0, 0, 0, 0],
+    { foo: true, bar: ['12345', '12345', '12345'] },
+    '12345',
+  );
 
   expect(result).to.be.eq('0x05');
 });
