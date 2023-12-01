7e81e2c53deaf2b5efcc6b0567fc1240540471eb
spypsy <spypsy@users.noreply.github.com>
2023-12-01 15:17:19 +0000

    fix(ci): export FORK_API_KEY from setup_env (#3512)
    Please provide a paragraph or two giving a summary of the change,
    including relevant motivation and context.
    
    # Checklist:
    Remove the checklist to signal you've completed it. Enable auto-merge if
    the PR is ready to merge.
    - [ ] If the pull request requires a cryptography review (e.g.
    cryptographic algorithm implementations) I have added the 'crypto' tag.
    - [ ] I have reviewed my diff in github, line by line and removed
    unexpected formatting changes, testing logs, or commented-out code.
    - [ ] Every change is related to the PR description.
    - [ ] I have
    [linked](https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue)
    this pull request to relevant issues (if any exist).

--- l1-contracts/scripts/ci_deploy_contracts.sh
+++ l1-contracts/scripts/ci_deploy_contracts.sh
@@ -2,7 +2,7 @@
 
 FORCE_DEPLOY=${2:-"false"}
 
-export ETHEREUM_HOST=$DEPLOY_TAG-mainnet-fork.aztec.network:8545/$API_KEY
+export ETHEREUM_HOST=$DEPLOY_TAG-mainnet-fork.aztec.network:8545/$FORK_API_KEY
 
 # If we have previously successful commit, we can early out if nothing relevant has changed since.
 if [[ $FORCE_DEPLOY == 'false' ]] && check_rebuild cache-"$CONTENT_HASH" $REPOSITORY; then
