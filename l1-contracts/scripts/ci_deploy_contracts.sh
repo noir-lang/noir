b246d1ba3a899af5e7566944a9d79be62827cdd5
spypsy <spypsy@users.noreply.github.com>
2023-12-01 16:47:13 +0000

    fix(ci): define REPOSITORY in deploy_l1_contracts (#3514)

--- l1-contracts/scripts/ci_deploy_contracts.sh
+++ l1-contracts/scripts/ci_deploy_contracts.sh
@@ -4,6 +4,8 @@ FORCE_DEPLOY=${2:-"false"}
 
 export ETHEREUM_HOST=$DEPLOY_TAG-mainnet-fork.aztec.network:8545/$FORK_API_KEY
 
+REPOSITORY="l1-contracts"
+
 CONTENT_HASH=$(calculate_content_hash $REPOSITORY)
 
 # If we have previously successful commit, we can early out if nothing relevant has changed since.
