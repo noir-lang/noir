b0fc82d5982319c43387fec791c9e8d9b8874832
sklppy88 <esau@aztecprotocol.com>
2023-12-02 19:14:44 +0000

    test

--- tooling/noir_js/test/node/cjs.test.cjs
+++ tooling/noir_js/test/node/cjs.test.cjs
@@ -1,7 +1,8 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const chai = require('chai');
-const assert_lt_json = require('./assert_lt.json');
+const assert_lt_json = require('../noir_compiled_examples/assert_lt/target/assert_lt.json');
+
 const { Noir } = require('@noir-lang/noir_js');
 
 it('generates witnesses successfully', async () => {
--- tooling/noir_js/test/node/e2e.test.ts
+++ tooling/noir_js/test/node/e2e.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import assert_lt_json from './assert_lt.json' assert { type: 'json' };
+import assert_lt_json from '../noir_compiled_examples/assert_lt/target/assert_lt.json' assert { type: 'json' };
 import { Noir } from '@noir-lang/noir_js';
 import { BarretenbergBackend as Backend } from '@noir-lang/backend_barretenberg';
 import { CompiledCircuit } from '@noir-lang/types';
