a2460e6e2dc3a69379b99f6df954cd5c217cf323
kevaundray <kevtheappdev@gmail.com>
2023-12-10 22:33:31 +0000

    add stdlib file in noirc_driver that returns the stdlib paths alongside their source code

---
+++ compiler/noirc_driver/src/stdlib.rs
@@ -0,0 +1,24 @@
+use rust_embed::RustEmbed;
+
+#[derive(RustEmbed)]
+#[folder = "../../noir_stdlib/src"]
+#[cfg_attr(not(target_os = "windows"), prefix = "std/")]
+#[cfg_attr(target_os = "windows", prefix = r"std\")] // Note reversed slash direction
+struct StdLibAssets;
+
+// Returns a vector of tuples containing the path to a stdlib file in the std lib crate
+// along with the source code of that file.
+//
+// This is needed because when we preload the file manager, it needs to know where
+// the source code for the stdlib is. The stdlib is treated special because it comes with
+// the compiler and is never included as a dependency like other user defined crates.
+pub(crate) fn stdlib_paths_with_source() -> Vec<(String, String)> {
+    StdLibAssets::iter()
+        .map(|path| {
+            let source = std::str::from_utf8(StdLibAssets::get(&path).unwrap().data.as_ref())
+                .unwrap()
+                .to_string();
+            (path.to_string(), source)
+        })
+        .collect()
+}
