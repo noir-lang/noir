18ea4ef18e258bb9a77cd2d26ab58bc4a7bee7be
Esau <esau@aztecprotocol.com>
2023-12-12 14:34:53 +0000

    test

---
+++ scripts/install_wasm-bindgen-new.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+cargo-binstall wasm-bindgen-cli --version 0.2.86
