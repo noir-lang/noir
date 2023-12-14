455c82427fb144f1acc7d8e43c987a4d2dec1be7
kevaundray <kevtheappdev@gmail.com>
2023-12-14 13:26:19 +0000

    fix

--- tooling/nargo/src/ops/optimize.rs
+++ tooling/nargo/src/ops/optimize.rs
@@ -8,7 +8,7 @@ pub fn optimize_program(
     mut program: CompiledProgram,
     np_language: Language,
 ) -> Result<CompiledProgram, NargoError> {
-    let (optimized_circuit, location_map) = acvm::compiler::compile(program.circuit, np_language)?;
+    let (optimized_circuit, location_map) = acvm::compiler::compile(program.circuit, np_language);
 
     program.circuit = optimized_circuit;
     program.debug.update_acir(location_map);
@@ -21,7 +21,7 @@ pub fn optimize_contract(
 ) -> Result<CompiledContract, NargoError> {
     let functions = try_vecmap(contract.functions, |mut func| {
         let (optimized_bytecode, location_map) =
-            acvm::compiler::compile(func.bytecode, np_language)?;
+            acvm::compiler::compile(func.bytecode, np_language);
         func.bytecode = optimized_bytecode;
         func.debug.update_acir(location_map);
         Ok::<_, NargoError>(func)
