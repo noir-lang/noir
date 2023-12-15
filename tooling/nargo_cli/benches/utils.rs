fd0a1e15fc604102a79534b021603cb1245f7fdd
TomAFrench <tom@tomfren.ch>
2023-12-15 13:50:24 +0000

    chore: update benchmark code

--- tooling/nargo_cli/benches/utils.rs
+++ tooling/nargo_cli/benches/utils.rs
@@ -2,13 +2,23 @@ use std::path::PathBuf;
 
 #[allow(unused)]
 fn get_selected_tests() -> Vec<PathBuf> {
-    let manifest_dir = match std::env::var("CARGO_MANIFEST_DIR") {
+    let root_dir = match std::env::var("CARGO_MANIFEST_DIR") {
         Ok(dir) => PathBuf::from(dir),
-        Err(_) => std::env::current_dir().unwrap().join("crates").join("nargo_cli"),
-    };
-    let test_dir = manifest_dir.join("tests").join("execution_success");
+        Err(_) => std::env::current_dir().unwrap(),
+    }
+    .parent()
+    .unwrap()
+    .parent()
+    .unwrap()
+    .join("test_programs");
 
-    let selected_tests =
-        vec!["8_integration", "sha256_blocks", "struct", "eddsa", "regression", "regression_2099"];
-    selected_tests.into_iter().map(|t| test_dir.join(t)).collect()
+    let test_dir = root_dir.join("execution_success");
+    let selected_tests = vec!["sha256_byte", "struct", "eddsa", "regression", "regression_2099"];
+    let selected_tests = selected_tests.into_iter().map(|t| test_dir.join(t));
+
+    let benchmark_dir = root_dir.join("benchmarks");
+    let benchmarks = vec!["large_loop"];
+    let benchmarks = benchmarks.into_iter().map(|t| benchmark_dir.join(t));
+
+    selected_tests.chain(benchmarks).collect()
 }
