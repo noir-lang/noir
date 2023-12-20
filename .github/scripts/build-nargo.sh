8736d7ca9279e1fcf2293d4e70c9371b1320429f
Esau <esau@aztecprotocol.com>
2023-12-20 20:44:01 +0100

    Remove feature flag

--- .github/scripts/build-nargo.sh
+++ .github/scripts/build-nargo.sh
@@ -6,4 +6,4 @@ export SOURCE_DATE_EPOCH=$(date +%s)
 export GIT_DIRTY=false
 export GIT_COMMIT=$(git rev-parse --verify HEAD)
 
-cargo build --features="noirc_driver/aztec" --release
+cargo build --release
