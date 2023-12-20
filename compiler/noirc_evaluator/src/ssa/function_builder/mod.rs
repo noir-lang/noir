6ddd98ab7d3fefde491cf12b785f76bf0585609e
guipublic <47281315+guipublic@users.noreply.github.com>
2023-12-20 19:16:00 +0100

    fix: bit-shifts for signed integers (#3890)
    # Description
    
    ## Problem\*
    
    Resolves #1479
    
    ## Summary\*
    
    Add support for bit shifts with signed integers.
    We use Rust behaviour for overflows.
    
    ## Additional Context
    
    
    
    ## Documentation\*
    
    Check one:
    - [X] No documentation needed.
    - [ ] Documentation included in this PR.
    - [ ] **[Exceptional Case]** Documentation to be submitted in a separate
    PR.
    
    # PR Checklist\*
    
    - [X] I have tested the changes locally.
    - [X] I have formatted the changes with [Prettier](https://prettier.io/)
    and/or `cargo fmt` on default settings.

--- compiler/noirc_evaluator/src/ssa/function_builder/mod.rs
+++ compiler/noirc_evaluator/src/ssa/function_builder/mod.rs
@@ -267,15 +267,6 @@ impl FunctionBuilder {
     }
 
     /// Insert ssa instructions which computes lhs << rhs by doing lhs*2^rhs
-    pub(crate) fn insert_shift_left(&mut self, lhs: ValueId, rhs: ValueId) -> ValueId {
-        let base = self.field_constant(FieldElement::from(2_u128));
-        let pow = self.pow(base, rhs);
-        let typ = self.current_function.dfg.type_of_value(lhs);
-        let pow = self.insert_cast(pow, typ);
-        self.insert_binary(lhs, BinaryOp::Mul, pow)
-    }
-
-    /// Insert ssa instructions which computes lhs << rhs by doing lhs*2^rhs
     /// and truncate the result to bit_size
     pub(crate) fn insert_wrapping_shift_left(
         &mut self,
@@ -308,8 +299,9 @@ impl FunctionBuilder {
                 let one = self.numeric_constant(FieldElement::one(), Type::unsigned(1));
                 let predicate = self.insert_binary(overflow, BinaryOp::Eq, one);
                 let predicate = self.insert_cast(predicate, typ.clone());
-
-                let pow = self.pow(base, rhs);
+                // we can safely cast to unsigned because overflow_checks prevent bit-shift with a negative value
+                let rhs_unsigned = self.insert_cast(rhs, Type::unsigned(bit_size));
+                let pow = self.pow(base, rhs_unsigned);
                 let pow = self.insert_cast(pow, typ);
                 (FieldElement::max_num_bits(), self.insert_binary(predicate, BinaryOp::Mul, pow))
             };
@@ -323,9 +315,16 @@ impl FunctionBuilder {
     }
 
     /// Insert ssa instructions which computes lhs >> rhs by doing lhs/2^rhs
-    pub(crate) fn insert_shift_right(&mut self, lhs: ValueId, rhs: ValueId) -> ValueId {
+    pub(crate) fn insert_shift_right(
+        &mut self,
+        lhs: ValueId,
+        rhs: ValueId,
+        bit_size: u32,
+    ) -> ValueId {
         let base = self.field_constant(FieldElement::from(2_u128));
-        let pow = self.pow(base, rhs);
+        // we can safely cast to unsigned because overflow_checks prevent bit-shift with a negative value
+        let rhs_unsigned = self.insert_cast(rhs, Type::unsigned(bit_size));
+        let pow = self.pow(base, rhs_unsigned);
         self.insert_binary(lhs, BinaryOp::Div, pow)
     }
 
