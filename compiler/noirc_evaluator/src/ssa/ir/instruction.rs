a3e4e8e5cf9117d945d2b93851ba232ead859764
Tom French <tom@tomfren.ch>
2023-12-21 01:51:03 +0000

    chore: avoid needing multiple rounds of constant folding

--- compiler/noirc_evaluator/src/ssa/ir/instruction.rs
+++ compiler/noirc_evaluator/src/ssa/ir/instruction.rs
@@ -434,104 +434,11 @@ impl Instruction {
                 }
             }
             Instruction::Constrain(lhs, rhs, msg) => {
-                if dfg.resolve(*lhs) == dfg.resolve(*rhs) {
-                    // Remove trivial case `assert_eq(x, x)`
-                    SimplifyResult::Remove
+                let constraints = decompose_constrain(*lhs, *rhs, msg.clone(), dfg);
+                if constraints.is_empty() {
+                    Remove
                 } else {
-                    match (&dfg[dfg.resolve(*lhs)], &dfg[dfg.resolve(*rhs)]) {
-                        (
-                            Value::NumericConstant { constant, typ },
-                            Value::Instruction { instruction, .. },
-                        )
-                        | (
-                            Value::Instruction { instruction, .. },
-                            Value::NumericConstant { constant, typ },
-                        ) if *typ == Type::bool() => {
-                            match dfg[*instruction] {
-                                Instruction::Binary(Binary {
-                                    lhs,
-                                    rhs,
-                                    operator: BinaryOp::Eq,
-                                }) if constant.is_one() => {
-                                    // Replace an explicit two step equality assertion
-                                    //
-                                    // v2 = eq v0, u32 v1
-                                    // constrain v2 == u1 1
-                                    //
-                                    // with a direct assertion of equality between the two values
-                                    //
-                                    // v2 = eq v0, u32 v1
-                                    // constrain v0 == v1
-                                    //
-                                    // Note that this doesn't remove the value `v2` as it may be used in other instructions, but it
-                                    // will likely be removed through dead instruction elimination.
-
-                                    SimplifiedToInstruction(Instruction::Constrain(
-                                        lhs,
-                                        rhs,
-                                        msg.clone(),
-                                    ))
-                                }
-
-                                Instruction::Binary(Binary {
-                                    lhs,
-                                    rhs,
-                                    operator: BinaryOp::Mul,
-                                }) if constant.is_one()
-                                    && dfg.type_of_value(lhs) == Type::bool() =>
-                                {
-                                    // Replace an equality assertion on a boolean multiplication
-                                    //
-                                    // v2 = mul v0, v1
-                                    // constrain v2 == u1 1
-                                    //
-                                    // with a direct assertion that each value is equal to 1
-                                    //
-                                    // v2 = mul v0, v1
-                                    // constrain v0 == 1
-                                    // constrain v1 == 1
-                                    //
-                                    // This is due to the fact that for `v2` to be 1 then both `v0` and `v1` are 1.
-                                    //
-                                    // Note that this doesn't remove the value `v2` as it may be used in other instructions, but it
-                                    // will likely be removed through dead instruction elimination.
-                                    let one = FieldElement::one();
-                                    let one = dfg.make_constant(one, Type::bool());
-
-                                    SimplifiedToInstructionMultiple(vec![
-                                        Instruction::Constrain(lhs, one, msg.clone()),
-                                        Instruction::Constrain(rhs, one, msg.clone()),
-                                    ])
-                                }
-                                Instruction::Not(value) => {
-                                    // Replace an assertion that a not instruction is truthy
-                                    //
-                                    // v1 = not v0
-                                    // constrain v1 == u1 1
-                                    //
-                                    // with an assertion that the not instruction input is falsy
-                                    //
-                                    // v1 = not v0
-                                    // constrain v0 == u1 0
-                                    //
-                                    // Note that this doesn't remove the value `v1` as it may be used in other instructions, but it
-                                    // will likely be removed through dead instruction elimination.
-                                    let reversed_constant = FieldElement::from(!constant.is_one());
-                                    let reversed_constant =
-                                        dfg.make_constant(reversed_constant, Type::bool());
-                                    SimplifiedToInstruction(Instruction::Constrain(
-                                        value,
-                                        reversed_constant,
-                                        msg.clone(),
-                                    ))
-                                }
-
-                                _ => None,
-                            }
-                        }
-
-                        _ => None,
-                    }
+                    SimplifiedToInstructionMultiple(constraints)
                 }
             }
             Instruction::ArrayGet { array, index } => {
@@ -649,6 +556,100 @@ fn simplify_cast(value: ValueId, dst_typ: &Type, dfg: &mut DataFlowGraph) -> Sim
         SimplifiedTo(value)
     } else {
         None
+    }
+}
+
+/// Try to decompose this constrain instruction. This constraint will be broken down such that it instead constrains
+/// all the values which are used to compute the values which were being constrained.
+fn decompose_constrain(
+    lhs: ValueId,
+    rhs: ValueId,
+    msg: Option<String>,
+    dfg: &mut DataFlowGraph,
+) -> Vec<Instruction> {
+    let lhs = dfg.resolve(lhs);
+    let rhs = dfg.resolve(rhs);
+
+    if lhs == rhs {
+        // Remove trivial case `assert_eq(x, x)`
+        Vec::new()
+    } else {
+        match (&dfg[lhs], &dfg[rhs]) {
+            (Value::NumericConstant { constant, typ }, Value::Instruction { instruction, .. })
+            | (Value::Instruction { instruction, .. }, Value::NumericConstant { constant, typ })
+                if *typ == Type::bool() =>
+            {
+                match dfg[*instruction] {
+                    Instruction::Binary(Binary { lhs, rhs, operator: BinaryOp::Eq })
+                        if constant.is_one() =>
+                    {
+                        // Replace an explicit two step equality assertion
+                        //
+                        // v2 = eq v0, u32 v1
+                        // constrain v2 == u1 1
+                        //
+                        // with a direct assertion of equality between the two values
+                        //
+                        // v2 = eq v0, u32 v1
+                        // constrain v0 == v1
+                        //
+                        // Note that this doesn't remove the value `v2` as it may be used in other instructions, but it
+                        // will likely be removed through dead instruction elimination.
+
+                        vec![Instruction::Constrain(lhs, rhs, msg)]
+                    }
+
+                    Instruction::Binary(Binary { lhs, rhs, operator: BinaryOp::Mul })
+                        if constant.is_one() && dfg.type_of_value(lhs) == Type::bool() =>
+                    {
+                        // Replace an equality assertion on a boolean multiplication
+                        //
+                        // v2 = mul v0, v1
+                        // constrain v2 == u1 1
+                        //
+                        // with a direct assertion that each value is equal to 1
+                        //
+                        // v2 = mul v0, v1
+                        // constrain v0 == 1
+                        // constrain v1 == 1
+                        //
+                        // This is due to the fact that for `v2` to be 1 then both `v0` and `v1` are 1.
+                        //
+                        // Note that this doesn't remove the value `v2` as it may be used in other instructions, but it
+                        // will likely be removed through dead instruction elimination.
+                        let one = FieldElement::one();
+                        let one = dfg.make_constant(one, Type::bool());
+
+                        [
+                            decompose_constrain(lhs, one, msg.clone(), dfg),
+                            decompose_constrain(rhs, one, msg, dfg),
+                        ]
+                        .concat()
+                    }
+                    Instruction::Not(value) => {
+                        // Replace an assertion that a not instruction is truthy
+                        //
+                        // v1 = not v0
+                        // constrain v1 == u1 1
+                        //
+                        // with an assertion that the not instruction input is falsy
+                        //
+                        // v1 = not v0
+                        // constrain v0 == u1 0
+                        //
+                        // Note that this doesn't remove the value `v1` as it may be used in other instructions, but it
+                        // will likely be removed through dead instruction elimination.
+                        let reversed_constant = FieldElement::from(!constant.is_one());
+                        let reversed_constant = dfg.make_constant(reversed_constant, Type::bool());
+                        decompose_constrain(value, reversed_constant, msg, dfg)
+                    }
+
+                    _ => vec![Instruction::Constrain(lhs, rhs, msg)],
+                }
+            }
+
+            _ => vec![Instruction::Constrain(lhs, rhs, msg)],
+        }
     }
 }
 
