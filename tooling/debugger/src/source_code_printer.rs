05efccd57f28f8f20c8b7ab069aa8bb610ea3fd0
Martin Verzilli <martin.verzilli@gmail.com>
2023-12-22 21:37:31 +0100

    Refactor source code printing

---
+++ tooling/debugger/src/source_code_printer.rs
@@ -0,0 +1,180 @@
+use acvm::acir::circuit::OpcodeLocation;
+use codespan_reporting::files::Files;
+use nargo::artifacts::debug::DebugArtifact;
+use noirc_errors::Location;
+use owo_colors::OwoColorize;
+use std::ops::Range;
+
+#[derive(Debug)]
+struct PrintedLine<'a> {
+    number: usize,
+    cursor: &'a str,
+    content: &'a str,
+    highlight: Option<Range<usize>>,
+}
+
+impl<'a> PrintedLine<'a> {
+    fn ellipsis(line_number: usize) -> Self {
+        Self { number: line_number, cursor: "", content: "...", highlight: None }
+    }
+}
+
+#[derive(Debug)]
+struct PrintedLocation<'a> {
+    location: Location,
+    lines: Vec<PrintedLine<'a>>,
+}
+
+impl<'a> PrintedLocation<'a> {
+    fn new(loc: Location) -> Self {
+        Self { location: loc, lines: [].into() }
+    }
+
+    fn add_line(&mut self, line: PrintedLine<'a>) {
+        self.lines.push(line);
+    }
+}
+
+pub(crate) fn print_source_code_location(
+    debug_artifact: &DebugArtifact,
+    location: &OpcodeLocation,
+) {
+    let rendered_locations = render(debug_artifact, location);
+
+    for loc in rendered_locations {
+        print_location_path(debug_artifact, loc.location);
+
+        for line in loc.lines {
+            match line.highlight {
+                Some(highlight) => {
+                    println!(
+                        "{:>3} {:2} {}{}{}",
+                        line.number,
+                        line.cursor,
+                        line.content[0..highlight.start].to_string().dimmed(),
+                        &line.content[highlight.start..highlight.end],
+                        line.content[highlight.end..].to_string().dimmed(),
+                    );
+                }
+                None => {
+                    println!(
+                        "{:>3} {:2} {}",
+                        line.number.dimmed(),
+                        line.cursor.dimmed(),
+                        line.content.to_string().dimmed(),
+                    );
+                }
+            }
+        }
+    }
+}
+
+fn print_location_path(debug_artifact: &DebugArtifact, loc: Location) {
+    let line_number = debug_artifact.location_line_number(loc).unwrap();
+    let column_number = debug_artifact.location_column_number(loc).unwrap();
+
+    println!("At {}:{line_number}:{column_number}", debug_artifact.name(loc.file).unwrap());
+}
+
+fn render<'a>(
+    debug_artifact: &'a DebugArtifact,
+    location: &OpcodeLocation,
+) -> Vec<PrintedLocation<'a>> {
+    let mut rendered_locations: Vec<PrintedLocation> = [].into();
+
+    let locations = debug_artifact.debug_symbols[0].opcode_location(location);
+
+    //TODO: use map instead?
+    let Some(locations) = locations else { return rendered_locations };
+
+    for loc in locations {
+        let mut rendered_loc = PrintedLocation::new(loc);
+
+        let loc_line_index = debug_artifact.location_line_index(loc).unwrap();
+        let loc_end_line_index = debug_artifact.location_end_line_index(loc).unwrap();
+
+        // How many lines before or after the location's line we
+        // print
+        let context_lines = 5;
+
+        let first_line_to_print =
+            if loc_line_index < context_lines { 0 } else { loc_line_index - context_lines };
+
+        let last_line_index = debug_artifact.last_line_index(loc).unwrap();
+
+        // Print all lines that the current location spans
+        let last_line_to_print = std::cmp::min(loc_end_line_index + context_lines, last_line_index);
+
+        let source = debug_artifact.location_source_code(loc).unwrap();
+
+        for (current_line_index, line) in source.lines().enumerate() {
+            let current_line_number = current_line_index + 1;
+
+            if current_line_index < first_line_to_print {
+                // Ignore lines before range starts
+                continue;
+            } else if current_line_index == first_line_to_print && current_line_index > 0 {
+                // Denote that there's more lines before but we're not showing them
+                rendered_loc.add_line(PrintedLine::ellipsis(current_line_number));
+                break;
+            }
+
+            if current_line_index > last_line_to_print {
+                // Denote that there's more lines after but we're not showing them,
+                // and stop printing
+                rendered_loc.add_line(PrintedLine::ellipsis(current_line_number));
+                break;
+            }
+
+            if current_line_index < loc_line_index {
+                rendered_loc.add_line(PrintedLine {
+                    number: current_line_number,
+                    cursor: "",
+                    content: line,
+                    highlight: None,
+                });
+            } else if current_line_index == loc_line_index {
+                let to_highlight = debug_artifact.location_in_line(loc).unwrap();
+
+                // Highlight current location from where it starts
+                // to the end of the current line
+                rendered_loc.add_line(PrintedLine {
+                    number: current_line_number,
+                    cursor: "->",
+                    content: line,
+                    highlight: Some(to_highlight),
+                });
+            } else if current_line_index < loc_end_line_index {
+                // Highlight current line if it's contained by the current location
+                rendered_loc.add_line(PrintedLine {
+                    number: current_line_number,
+                    cursor: "",
+                    content: line,
+                    highlight: Some(Range { start: 0, end: line.len() - 1 }),
+                });
+            } else if current_line_index == loc_end_line_index {
+                let to_highlight = debug_artifact.location_in_end_line(loc).unwrap();
+
+                // Highlight current location from the beginning
+                // of the line until the location's own end
+                rendered_loc.add_line(PrintedLine {
+                    number: current_line_number,
+                    cursor: "",
+                    content: line,
+                    highlight: Some(to_highlight),
+                });
+            } else {
+                rendered_loc.add_line(PrintedLine {
+                    number: current_line_number,
+                    cursor: "",
+                    content: line,
+                    highlight: None,
+                })
+            }
+        }
+
+        rendered_locations.push(rendered_loc);
+    }
+
+    rendered_locations
+}
