b53bacfcde8217e15c8329f42ff546064f9b44bc
Jan Beneš <janbenes1234@gmail.com>
2024-01-04 15:48:38 +0100

    chore: checking noir formatting in CI (#3828)
    Fixes #3825

--- yarn-project/run_nargo_fmt.sh
+++ yarn-project/run_nargo_fmt.sh
@@ -1,6 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-nargo_executable="$(git rev-parse --show-toplevel)/noir/target/release/nargo"
+# Note: This script formats the files multiple times when the given project is included in a workspace.
+#       Tackling this became a time sink, so I decided to leave it as is for now.
+
+set -eu
+
+# We set the executable path as if we were in CI
+nargo_executable="/usr/src/noir/target/release/nargo"
+
+# Check if nargo_executable exists and is executable
+if [ ! -x "$nargo_executable" ]; then
+    # If not, we try to set a nargo path as if the script was run locally
+    nargo_executable="$(git rev-parse --show-toplevel)/noir/target/release/nargo"
+
+    if [ ! -x "$nargo_executable" ]; then
+        echo "Error: nargo executable not found"
+        exit 1
+    fi
+fi
 
 # Find all Nargo.toml files and run 'nargo fmt'
 find . -name "Nargo.toml" | while read -r file; do
