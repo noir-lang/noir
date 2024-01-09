5b8da623569d160e5ec45a278a0f887ec6e61978
Esau <esau@aztecprotocol.com>
2024-01-10 00:45:47 +0100

    asdf

--- .github/scripts/noir-codegen-test.sh
+++ .github/scripts/noir-codegen-test.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -eu
 
-./scripts/nargo_compile_noir_codegen_assert_lt.sh
-# rm -rf /usr/src/noir/tooling/noir_codegen/test/test_lib/target/debug_assert_lt.json
 yarn workspace @noir-lang/noir_codegen test
--- scripts/nargo_compile_noir_codegen_assert_lt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ./tooling/noir_codegen/test/test_lib
-nargo compile
