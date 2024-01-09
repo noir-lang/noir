e1247174e161ad8484681e574ef475f2f857b18e
thunkar <gregojquiros@gmail.com>
2024-01-09 14:06:42 +0000

    force public functions for trait implementation

--- compiler/noirc_frontend/src/parser/parser.rs
+++ compiler/noirc_frontend/src/parser/parser.rs
@@ -613,7 +613,11 @@ fn trait_implementation() -> impl NoirParser<TopLevelStatement> {
 }
 
 fn trait_implementation_body() -> impl NoirParser<Vec<TraitImplItem>> {
-    let function = function_definition(true).map(TraitImplItem::Function);
+    let function = function_definition(true).map(|mut f| {
+        // Trait impl functions are always public
+        f.def_mut().visibility = FunctionVisibility::Public;
+        TraitImplItem::Function(f)
+    });
 
     let alias = keyword(Keyword::Type)
         .ignore_then(ident())
