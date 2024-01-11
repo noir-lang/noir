9aa0986633391c0db343c6c6ca84b1405ba44d7b
Tom French <15848336+TomAFrench@users.noreply.github.com>
2024-01-11 21:36:28 +0000

    chore: delete the compiler from `noir-compiler` (#3959)

--- yarn-project/noir-compiler/src/cli/add_noir_compiler_commander_actions.ts
+++ yarn-project/noir-compiler/src/cli/add_noir_compiler_commander_actions.ts
@@ -4,43 +4,10 @@ import { Command } from 'commander';
 import { dirname } from 'path';
 
 /**
- * CLI options for configuring behavior
- */
-interface Options {
-  // eslint-disable-next-line jsdoc/require-jsdoc
-  outdir: string;
-  // eslint-disable-next-line jsdoc/require-jsdoc
-  typescript: string | undefined;
-  // eslint-disable-next-line jsdoc/require-jsdoc
-  interface: string | undefined;
-  // eslint-disable-next-line jsdoc/require-jsdoc
-  compiler: string | undefined;
-}
-
-/**
  *
  */
 export function addNoirCompilerCommanderActions(program: Command, log: LogFn = () => {}) {
   addCodegenCommanderAction(program, log);
-}
-
-/**
- *
- */
-export function addCompileCommanderAction(program: Command, log: LogFn = () => {}) {
-  program
-    .command('compile')
-    .argument('<project-path>', 'Path to the bin or Aztec.nr project to compile')
-    .option('-o, --outdir <path>', 'Output folder for the binary artifacts, relative to the project path', 'target')
-    .option('-ts, --typescript <path>', 'Optional output folder for generating typescript wrappers', undefined)
-    .option('-i, --interface <path>', 'Optional output folder for generating an Aztec.nr contract interface', undefined)
-    .option('-c --compiler <string>', 'Which compiler to use. Either nargo or wasm.', 'wasm')
-    .description('Compiles the Noir Source in the target project')
-
-    .action(async (projectPath: string, options: Options) => {
-      const { compileNoir } = await import('./compile_noir.js');
-      await compileNoir(projectPath, options, log);
-    });
 }
 
 /**
--- yarn-project/noir-compiler/src/index.ts
+++ yarn-project/noir-compiler/src/index.ts
@@ -1,49 +1,8 @@
-import { ContractArtifact } from '@aztec/foundation/abi';
-
-import { join, resolve } from 'path';
-
-import { CompileOpts, NargoContractCompiler } from './compile/nargo.js';
-import { createNodejsFileManager } from './compile/noir/file-manager/nodejs-file-manager.js';
-import { NoirWasmCompileOptions, NoirWasmContractCompiler } from './compile/noir/noir-wasm-compiler.js';
-import { generateArtifact, generateContractArtifact } from './contract-interface-gen/abi.js';
-import { ProgramArtifact } from './noir_artifact.js';
-
 export * from './versions.js';
 
 export { generateTypescriptContractInterface } from './contract-interface-gen/contractTypescript.js';
 export { generateNoirContractInterface } from './contract-interface-gen/noir.js';
 export { generateTypescriptProgramInterface } from './contract-interface-gen/programTypescript.js';
-export { generateContractArtifact };
+export { generateContractArtifact } from './contract-interface-gen/abi.js';
 
 export * from './noir_artifact.js';
-
-/**
- * Compile Aztec.nr contracts in project path using a nargo binary available in the shell.
- * @param projectPath - Path to project.
- * @param opts - Compiler options.
- * @returns Compiled artifacts.
- */
-export async function compileUsingNargo(projectPath: string, opts: CompileOpts = {}): Promise<ContractArtifact[]> {
-  return (await new NargoContractCompiler(projectPath, opts).compile()).map(artifact =>
-    generateContractArtifact(artifact),
-  );
-}
-
-/**
- * Compile Aztec.nr contracts in project path using built-in noir_wasm.
- * @param projectPath - Path to project.
- * @param opts - Compiler options.
- * @returns Compiled artifacts.
- */
-export async function compileUsingNoirWasm(
-  projectPath: string,
-  opts: NoirWasmCompileOptions,
-): Promise<(ContractArtifact | ProgramArtifact)[]> {
-  const cacheRoot = process.env.XDG_CACHE_HOME ?? join(process.env.HOME ?? '', '.cache');
-  const fileManager = createNodejsFileManager(join(cacheRoot, 'aztec-noir-compiler'));
-  const compiler = await NoirWasmContractCompiler.new(fileManager, resolve(projectPath), opts);
-  const artifacts = await compiler.compile();
-  return artifacts.map(artifact => {
-    return generateArtifact(artifact);
-  });
-}
