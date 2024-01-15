e3817991330f433951e595f075f45b638507f583
thunkar <gregojquiros@gmail.com>
2024-01-15 15:52:18 +0100

    use cargo binstall

--- compiler/wasm/scripts/install_wasm-pack.sh
+++ compiler/wasm/scripts/install_wasm-pack.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 set -eu
+
+cd $(dirname "$0")/..
+
 # Install wasm-pack 
-cargo install wasm-pack@0.12.1
+CARGO_BINSTALL_CHECK=$(./scripts/command-check.sh cargo-binstall)
+echo $CARGO_BINSTALL_CHECK
+if [ $CARGO_BINSTALL_CHECK != "true" ]; then
+    curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+fi
+
+cargo-binstall wasm-pack@0.12.1 -y
