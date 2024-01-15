0282e26e1fe68eaa3b1c6a362148ecbc4d7b389d
Tom French <15848336+TomAFrench@users.noreply.github.com>
2024-01-15 15:47:31 +0000

    Update compiler/noirc_evaluator/src/ssa/function_builder/mod.rs

--- compiler/noirc_evaluator/src/ssa/function_builder/mod.rs
+++ compiler/noirc_evaluator/src/ssa/function_builder/mod.rs
@@ -363,7 +363,7 @@ impl FunctionBuilder {
                 let r_squared = self.insert_binary(r, BinaryOp::Mul, r);
                 let a = self.insert_binary(r_squared, BinaryOp::Mul, lhs);
                 let idx = self.field_constant(FieldElement::from((bit_size - i) as i128));
-                let b = self.insert_array_get(rhs_bits, idx, Type::unsigned(1));
+                let b = self.insert_array_get(rhs_bits, idx, Type::field());
                 let r1 = self.insert_binary(a, BinaryOp::Mul, b);
                 let c = self.insert_binary(one, BinaryOp::Sub, b);
                 let r2 = self.insert_binary(c, BinaryOp::Mul, r_squared);
