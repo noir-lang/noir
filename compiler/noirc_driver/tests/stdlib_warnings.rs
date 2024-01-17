682b05a9aa48428712ccb4035648295ae75db3c9
sirasistant <sirasistant@gmail.com>
2024-01-17 13:51:26 +0000

    test: fix test

--- compiler/noirc_driver/tests/stdlib_warnings.rs
+++ compiler/noirc_driver/tests/stdlib_warnings.rs
@@ -12,12 +12,16 @@ fn stdlib_does_not_produce_constant_warnings() -> Result<(), ErrorsAndWarnings> 
     let root = Path::new("");
     let file_name = Path::new("main.nr");
     let mut file_manager = file_manager_with_stdlib(root);
-    let file_id = file_manager.add_file_with_source(file_name, source.to_owned()).expect(
+    file_manager.add_file_with_source(file_name, source.to_owned()).expect(
         "Adding source buffer to file manager should never fail when file manager is empty",
     );
-    let parsed_source = parse_file(&file_manager, file_id);
+    let parsed_files = file_manager
+        .as_file_map()
+        .all_file_ids()
+        .map(|&file_id| (file_id, parse_file(&file_manager, file_id)))
+        .collect();
 
-    let mut context = Context::new(file_manager, HashMap::from([(file_id, parsed_source)]));
+    let mut context = Context::new(file_manager, parsed_files);
     let root_crate_id = prepare_crate(&mut context, file_name);
 
     let ((), warnings) = noirc_driver::check_crate(&mut context, root_crate_id, false, false)?;
