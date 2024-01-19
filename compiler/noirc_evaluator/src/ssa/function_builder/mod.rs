0b641af1fe0de51ddb8f3e923a189a9b2de7a671
TomAFrench <tom@tomfren.ch>
2024-01-19 18:35:46 +0000

    fix: fix mismatched type issues

--- compiler/noirc_evaluator/src/ssa/function_builder/mod.rs
+++ compiler/noirc_evaluator/src/ssa/function_builder/mod.rs
@@ -369,8 +369,10 @@ impl FunctionBuilder {
                 let idx = self.field_constant(FieldElement::from((bit_size - i) as i128));
                 let b = self.insert_array_get(rhs_bits, idx, Type::bool());
                 let not_b = self.insert_not(b);
-                let r1 = self.insert_binary(a, BinaryOp::Mul, b);
-                let r2 = self.insert_binary(not_b, BinaryOp::Mul, r_squared);
+                let b_as_field = self.insert_cast(b, Type::field());
+                let not_b_as_field = self.insert_cast(not_b, Type::field());
+                let r1 = self.insert_binary(a, BinaryOp::Mul, b_as_field);
+                let r2 = self.insert_binary(not_b_as_field, BinaryOp::Mul, r_squared);
                 r = self.insert_binary(r1, BinaryOp::Add, r2);
             }
             r
