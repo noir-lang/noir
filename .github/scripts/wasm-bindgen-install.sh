8038fc8069154f96c994c5a45f356370f480fcdb
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-01-23 01:10:23 +0000

    make consistent with other wasm-bindgen install

--- .github/scripts/wasm-bindgen-install.sh
+++ .github/scripts/wasm-bindgen-install.sh
@@ -2,4 +2,5 @@
 set -eu
 
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-cargo-binstall wasm-bindgen-cli --version 0.2.86 -y
+cargo binstall wasm-bindgen-cli@0.2.86 --force --no-confirm
+
