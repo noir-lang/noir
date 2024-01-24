c2723e1bb021f69fa062d928af7b12e74715baaf
Michael J. Klein <lambdamichael@gmail.com>
2024-01-24 16:53:32 -0500

    distinguish parser from lexer unexpected token, fix panic on empty module, replace map(|_| ..) with to(..), remove redundant or_not on zero-or-more attributes() parser

--- compiler/noirc_frontend/src/lexer/errors.rs
+++ compiler/noirc_frontend/src/lexer/errors.rs
@@ -64,7 +64,7 @@ impl LexerErrorKind {
 
                 (
                     "An unexpected character was found".to_string(),
-                    format!("Expected {expected}, but found {found}"),
+                    format!("Lexer: expected {expected}, but found {found}"),
                     *span,
                 )
             },
