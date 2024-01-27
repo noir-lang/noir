e1184ffc2f4ac2d2de0e8a106614bdd266a06cf4
esau <152162806+sklppy88@users.noreply.github.com>
2024-01-27 04:50:07 -0600

    chore: add note getter test to cci (#4236)

--- yarn-project/end-to-end/src/e2e_note_getter.test.ts
+++ yarn-project/end-to-end/src/e2e_note_getter.test.ts
@@ -8,6 +8,9 @@ interface NoirOption<T> {
   _value: T;
 }
 
+const sortFunc = (a: any, b: any) =>
+  a.points > b.points ? 1 : a.points < b.points ? -1 : a.randomness > b.randomness ? 1 : -1;
+
 function unwrapOptions<T>(options: NoirOption<T>[]): T[] {
   return options.filter((option: any) => option._is_some).map((option: any) => option._value);
 }
@@ -28,9 +31,17 @@ describe('e2e_note_getter', () => {
   afterAll(() => teardown());
 
   it('inserts notes from 0-9, then makes multiple queries specifying the total suite of comparators', async () => {
-    const numbers = [...Array(10).keys()];
-    await Promise.all(numbers.map(number => contract.methods.insert_note(number).send().wait()));
-    await contract.methods.insert_note(5).send().wait();
+    // ISSUE #4243
+    // Calling this function does not work like this
+    // const numbers = [...Array(10).keys()];
+    // await Promise.all(numbers.map(number => contract.methods.insert_note(number).send().wait()));
+    // It causes a race condition complaining about root mismatch
+
+    await contract.methods
+      .insert_notes([...Array(10).keys()])
+      .send()
+      .wait();
+    await contract.methods.insert_note(5, Fr.ZERO).send().wait();
 
     const [returnEq, returnNeq, returnLt, returnGt, returnLte, returnGte] = await Promise.all([
       contract.methods.read_note(5, Comparator.EQ).view(),
@@ -41,15 +52,21 @@ describe('e2e_note_getter', () => {
       contract.methods.read_note(5, Comparator.GTE).view(),
     ]);
 
-    expect(unwrapOptions(returnEq).map(({ points, randomness }: any) => ({ points, randomness }))).toStrictEqual([
-      { points: 5n, randomness: 1n },
-      { points: 5n, randomness: 1n },
-    ]);
+    expect(
+      unwrapOptions(returnEq)
+        .map(({ points, randomness }: any) => ({ points, randomness }))
+        .sort(sortFunc),
+    ).toStrictEqual(
+      [
+        { points: 5n, randomness: 1n },
+        { points: 5n, randomness: 0n },
+      ].sort(sortFunc),
+    );
 
     expect(
       unwrapOptions(returnNeq)
         .map(({ points, randomness }: any) => ({ points, randomness }))
-        .sort((a: any, b: any) => (a.points > b.points ? 1 : -1)),
+        .sort(sortFunc),
     ).toStrictEqual(
       [
         { points: 0n, randomness: 1n },
@@ -61,13 +78,13 @@ describe('e2e_note_getter', () => {
         { points: 8n, randomness: 1n },
         { points: 4n, randomness: 1n },
         { points: 3n, randomness: 1n },
-      ].sort((a: any, b: any) => (a.points > b.points ? 1 : -1)),
+      ].sort(sortFunc),
     );
 
     expect(
       unwrapOptions(returnLt)
         .map(({ points, randomness }: any) => ({ points, randomness }))
-        .sort((a: any, b: any) => (a.points > b.points ? 1 : -1)),
+        .sort(sortFunc),
     ).toStrictEqual(
       [
         { points: 0n, randomness: 1n },
@@ -75,51 +92,51 @@ describe('e2e_note_getter', () => {
         { points: 2n, randomness: 1n },
         { points: 4n, randomness: 1n },
         { points: 3n, randomness: 1n },
-      ].sort((a: any, b: any) => (a.points > b.points ? 1 : -1)),
+      ].sort(sortFunc),
     );
 
     expect(
       unwrapOptions(returnGt)
         .map(({ points, randomness }: any) => ({ points, randomness }))
-        .sort((a: any, b: any) => (a.points > b.points ? 1 : -1)),
+        .sort(sortFunc),
     ).toStrictEqual(
       [
         { points: 7n, randomness: 1n },
         { points: 9n, randomness: 1n },
         { points: 6n, randomness: 1n },
         { points: 8n, randomness: 1n },
-      ].sort((a: any, b: any) => (a.points > b.points ? 1 : -1)),
+      ].sort(sortFunc),
     );
 
     expect(
       unwrapOptions(returnLte)
         .map(({ points, randomness }: any) => ({ points, randomness }))
-        .sort((a: any, b: any) => (a.points > b.points ? 1 : -1)),
+        .sort(sortFunc),
     ).toStrictEqual(
       [
         { points: 5n, randomness: 1n },
-        { points: 5n, randomness: 1n },
+        { points: 5n, randomness: 0n },
         { points: 0n, randomness: 1n },
         { points: 1n, randomness: 1n },
         { points: 2n, randomness: 1n },
         { points: 4n, randomness: 1n },
         { points: 3n, randomness: 1n },
-      ].sort((a: any, b: any) => (a.points > b.points ? 1 : -1)),
+      ].sort(sortFunc),
     );
 
     expect(
       unwrapOptions(returnGte)
         .map(({ points, randomness }: any) => ({ points, randomness }))
-        .sort((a: any, b: any) => (a.points > b.points ? 1 : -1)),
+        .sort(sortFunc),
     ).toStrictEqual(
       [
-        { points: 5n, randomness: 1n },
+        { points: 5n, randomness: 0n },
         { points: 5n, randomness: 1n },
         { points: 7n, randomness: 1n },
         { points: 9n, randomness: 1n },
         { points: 6n, randomness: 1n },
         { points: 8n, randomness: 1n },
-      ].sort((a: any, b: any) => (a.points > b.points ? 1 : -1)),
+      ].sort(sortFunc),
     );
   }, 300_000);
 });
