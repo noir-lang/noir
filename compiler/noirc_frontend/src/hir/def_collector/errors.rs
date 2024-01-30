c8d7009a8cc88bf9db26e082ee1530dfa4f14a6a
Tom French <tom@tomfren.ch>
2024-01-30 17:36:55 +0000

    chore: remove unused error

--- compiler/noirc_frontend/src/hir/def_collector/errors.rs
+++ compiler/noirc_frontend/src/hir/def_collector/errors.rs
@@ -73,8 +73,6 @@ pub enum DefCollectorErrorKind {
         "Either the type or the trait must be from the same crate as the trait implementation"
     )]
     TraitImplOrphaned { span: Span },
-    #[error("Usage of the `#[foreign]` or `#[builtin]` function attributes are not allowed outside of the Noir standard library")]
-    LowLevelFunctionOutsideOfStdlib { span: Span },
     #[error("macro error : {0:?}")]
     MacroError(MacroError),
 }
@@ -246,11 +244,6 @@ impl From<DefCollectorErrorKind> for Diagnostic {
             DefCollectorErrorKind::TraitImplOrphaned { span } => Diagnostic::simple_error(
                 "Orphaned trait implementation".into(),
                 "Either the type or the trait must be from the same crate as the trait implementation".into(),
-                span,
-            ),
-            DefCollectorErrorKind::LowLevelFunctionOutsideOfStdlib { span } => Diagnostic::simple_error(
-                "Definition of low-level function outside of standard library".into(),
-                "Usage of the `#[foreign]` or `#[builtin]` function attributes are not allowed outside of the Noir standard library".into(),
                 span,
             ),
             DefCollectorErrorKind::MacroError(macro_error) => {
