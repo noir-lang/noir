1a3ea85f3db87378d7e3a48dd0010304c1324b55
vezenovm <mvezenov@gmail.com>
2024-01-30 19:38:54 +0000

    accept changes

--- tooling/nargo/src/ops/execute.rs
+++ tooling/nargo/src/ops/execute.rs
@@ -16,12 +16,12 @@ pub fn execute_circuit<B: BlackBoxFunctionSolver, F: ForeignCallExecutor>(
     foreign_call_executor: &mut F,
 ) -> Result<WitnessMap, NargoError> {
     let mut acvm = ACVM::new(blackbox_solver, &circuit.opcodes, initial_witness);
-    // TODO: remove only for debugging
-    let mut num_foreign_calls = 0;
+
+    // This message will be resolved by a nargo foreign call only when we have an unsatisfied assertion.
     let mut assert_message: Option<String> = None;
     loop {
         let solver_status = acvm.solve();
-        // dbg!(acvm.get_assert_message());
+
         match solver_status {
             ACVMStatus::Solved => break,
             ACVMStatus::InProgress => {
@@ -42,7 +42,7 @@ pub fn execute_circuit<B: BlackBoxFunctionSolver, F: ForeignCallExecutor>(
                 return Err(NargoError::ExecutionError(match call_stack {
                     Some(call_stack) => {
                         // First check whether we have a runtime assertion message that should be returned on an ACVM failure
-                        // If we do not have a runtime assertion message, we should check whether the circuit has any hardcoded
+                        // If we do not have a runtime assertion message, we should check whether the circuit has any harcoded
                         // messages associated with a specific `OpcodeLocation`.
                         // Otherwise return the provided opcode resolution error.
                         if let Some(assert_message) = assert_message {
