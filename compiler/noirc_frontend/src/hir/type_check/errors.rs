1b2c2d74b15ef3cb8362aa9d9fc520efc20cd39e
vezenovm <mvezenov@gmail.com>
2024-02-06 20:56:51 +0000

    switch err listing order

--- compiler/noirc_frontend/src/hir/type_check/errors.rs
+++ compiler/noirc_frontend/src/hir/type_check/errors.rs
@@ -115,12 +115,12 @@ pub enum TypeCheckError {
     NoMatchingImplFound { constraints: Vec<(Type, String)>, span: Span },
     #[error("Constraint for `{typ}: {trait_name}` is not needed, another matching impl is already in scope")]
     UnneededTraitConstraint { trait_name: String, typ: Type, span: Span },
-    #[error("Slices cannot be returned from an unconstrained runtime to a constrained runtime")]
-    UnconstrainedSliceReturnToConstrained { span: Span },
     #[error(
         "Cannot pass a mutable reference from a constrained runtime to an unconstrained runtime"
     )]
     ConstrainedReferenceToUnconstrained { span: Span },
+    #[error("Slices cannot be returned from an unconstrained runtime to a constrained runtime")]
+    UnconstrainedSliceReturnToConstrained { span: Span },
 }
 
 impl TypeCheckError {
