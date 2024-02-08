a8ffe0f18b6839de6a6eae3c3918b0ae91385708
gvozdvmozgu <131762131+gvozdvmozgu@users.noreply.github.com>
2024-02-08 22:57:59 +0200

    chore: implement import formatting (#4248)
    # Description
    
    chore: implement import formatting
    
    ## Additional Context
    
    https://github.com/noir-lang/noir/issues/3945#issuecomment-1877016455
    
    ## Documentation
    
    Check one:
    - [x] No documentation needed.
    - [ ] Documentation included in this PR.
    - [ ] **[Exceptional Case]** Documentation to be submitted in a separate
    PR.
    
    # PR Checklist
    
    - [x] I have tested the changes locally.
    - [x] I have formatted the changes with [Prettier](https://prettier.io/)
    and/or `cargo fmt` on default settings.
    
    Co-authored-by: kevaundray <kevtheappdev@gmail.com>
    Co-authored-by: jfecher <jake@aztecprotocol.com>

--- tooling/nargo_fmt/src/visitor/expr.rs
+++ tooling/nargo_fmt/src/visitor/expr.rs
@@ -5,8 +5,9 @@ use noirc_frontend::{
 
 use super::{ExpressionType, FmtVisitor, Shape};
 use crate::{
+    items::{HasItem, Item, Items},
     rewrite,
-    utils::{self, first_line_width, Expr, FindToken, Item},
+    utils::{first_line_width, FindToken},
     Config,
 };
 
@@ -81,8 +82,7 @@ impl FmtVisitor<'_> {
 
             let nested_indent = visitor.shape();
             let exprs: Vec<_> =
-                utils::Exprs::new(&visitor, nested_indent, fields_span, constructor.fields)
-                    .collect();
+                Items::new(&visitor, nested_indent, fields_span, constructor.fields).collect();
             let exprs = format_exprs(
                 visitor.config,
                 Tactic::HorizontalVertical,
@@ -189,7 +189,7 @@ impl FmtVisitor<'_> {
 
 // TODO: fixme
 #[allow(clippy::too_many_arguments)]
-pub(crate) fn format_seq<T: Item>(
+pub(crate) fn format_seq<T: HasItem>(
     shape: Shape,
     prefix: &str,
     suffix: &str,
@@ -206,7 +206,7 @@ pub(crate) fn format_seq<T: Item>(
 
     nested_indent.indent.block_indent(visitor.config);
 
-    let exprs: Vec<_> = utils::Exprs::new(&visitor, nested_indent, span, exprs).collect();
+    let exprs: Vec<_> = Items::new(&visitor, nested_indent, span, exprs).collect();
     let exprs = format_exprs(visitor.config, tactic, trailing_comma, exprs, nested_indent, reduce);
 
     wrap_exprs(prefix, suffix, exprs, nested_indent, shape, mode)
@@ -249,11 +249,11 @@ pub(crate) fn format_parens(
     format_seq(shape, "(", ")", visitor, trailing_comma, exprs, span, tactic, mode, reduce)
 }
 
-fn format_exprs(
+pub(crate) fn format_exprs(
     config: &Config,
     tactic: Tactic,
     trailing_comma: bool,
-    exprs: Vec<Expr>,
+    exprs: Vec<Item>,
     shape: Shape,
     reduce: bool,
 ) -> String {
@@ -396,7 +396,7 @@ pub(crate) enum Tactic {
 impl Tactic {
     fn definitive(
         self,
-        exprs: &[Expr],
+        exprs: &[Item],
         short_width_threshold: usize,
         reduce: bool,
     ) -> DefinitiveTactic {
@@ -449,7 +449,7 @@ enum DefinitiveTactic {
 }
 
 impl DefinitiveTactic {
-    fn reduce(self, exprs: &[Expr], short_array_element_width_threshold: usize) -> Self {
+    fn reduce(self, exprs: &[Item], short_array_element_width_threshold: usize) -> Self {
         match self {
             DefinitiveTactic::Vertical
                 if no_long_exprs(exprs, short_array_element_width_threshold) =>
@@ -467,7 +467,7 @@ fn has_single_line_comment(slice: &str) -> bool {
     slice.trim_start().starts_with("//")
 }
 
-fn no_long_exprs(exprs: &[Expr], max_width: usize) -> bool {
+fn no_long_exprs(exprs: &[Item], max_width: usize) -> bool {
     exprs.iter().all(|expr| expr.value.len() <= max_width)
 }
 
