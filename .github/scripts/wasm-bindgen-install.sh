6c08caa99f64c2f1a841ffd17d3fe96f96f87bb2
Tom French <tom@tomfren.ch>
2024-02-11 15:06:15 +0000

    chore(ci): standardise scripts used for CI

--- .github/scripts/wasm-bindgen-install.sh
+++ .github/scripts/wasm-bindgen-install.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eu
 
-# TODO call this script directly
-./scripts/install_wasm-bindgen.sh
+cd $(dirname "$0")/..
+
+# Install binstall
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+
+# Install wasm-bindgen-cli.
+if [ "$(wasm-bindgen --version | cut -d' ' -f2)" != "0.2.86" ]; then
+  echo "Building wasm-bindgen..."
+  cargo binstall wasm-bindgen-cli@0.2.86 --force --no-confirm
+fi
+
