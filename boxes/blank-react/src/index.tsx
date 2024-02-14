382626cddaa175041695e2eb70ad3c350351ffe3
José Pedro Sousa <jose@aztecprotocol.com>
2024-02-14 13:47:12 +0000

    feat: Boxes update (#4498)

---
+++ boxes/blank-react/src/index.tsx
@@ -0,0 +1,14 @@
+import { Home } from "./pages/home";
+import "react-toastify/dist/ReactToastify.css";
+import * as ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+root.render(
+  <>
+    <Home />
+    <ToastContainer />
+  </>,
+);
---
+++ boxes/blank-react/src/pages/home.tsx
@@ -0,0 +1,18 @@
+import { ContractComponent } from "./contract";
+import { useContract } from "../hooks/useContract";
+
+export function Home() {
+  const { contract, deploy, wait } = useContract();
+
+  if (!contract) {
+    return (
+      <form onSubmit={deploy}>
+        <button type="submit" disabled={wait}>
+          Deploy dummy contract
+        </button>
+      </form>
+    );
+  }
+
+  return <ContractComponent contract={contract} />;
+}
