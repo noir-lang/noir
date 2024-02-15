8b68b8f20722890ae0193b8880bfe5de23c5c2dd
Jake Fecher <jfecher11@gmail.com>
2024-02-15 09:45:11 -0600

    Remove println

--- compiler/noirc_evaluator/src/ssa/ssa_gen/mod.rs
+++ compiler/noirc_evaluator/src/ssa/ssa_gen/mod.rs
@@ -480,16 +480,6 @@ impl<'a> FunctionContext<'a> {
         self.builder.set_location(for_expr.end_range_location);
         let end_index = self.codegen_non_tuple_expression(&for_expr.end_range)?;
 
-
-        eprintln!(
-            "Type of start index is {:?} type of iterator is {:?} type of end index is {:?}",
-            self.builder.type_of_value(start_index),
-            index_type,
-            self.builder.type_of_value(end_index)
-        );
-
-
-
         // Set the location of the initial jmp instruction to the start range. This is the location
         // used to issue an error if the start range cannot be determined at compile-time.
         self.builder.set_location(for_expr.start_range_location);
