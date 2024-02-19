acc27b1bd2ec21c7b5c71f02974bd49d29b4caa5
ludamad <adam.domurad@gmail.com>
2024-02-19 13:56:09 +0000

    fix: debug build (#4666)

--- barretenberg/cpp/src/barretenberg/ecc/curves/bn254/fr_straight.bench.cpp
+++ barretenberg/cpp/src/barretenberg/ecc/curves/bn254/fr_straight.bench.cpp
@@ -205,7 +205,7 @@ void sqr(State& state) noexcept
 }
 BENCHMARK(sqr);
 } // namespace
+#endif
 
 // NOLINTNEXTLINE macro invocation triggers style guideline errors from googletest code
 BENCHMARK_MAIN();
-#endif
