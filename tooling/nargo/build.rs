b5e5c30f4db52c79ef556e80660f39db369b1911
Tom French <15848336+TomAFrench@users.noreply.github.com>
2024-02-21 17:39:09 +0000

    chore!: bump msrv to 1.73.0 (#4406)
    # Description
    
    ## Problem\*
    
    Resolves <!-- Link to GitHub Issue -->
    
    ## Summary\*
    
    Bumpalo bumped their MSRV in a [patch/minor
    release](https://github.com/fitzgen/bumpalo/commit/f8597ceb3600807a902fa9692fb43c49e7b63b27)
    and wasmer is using an unlocked dependency so we need to bump to match.
    
    ## Additional Context
    
    
    
    ## Documentation\*
    
    Check one:
    - [x] No documentation needed.
    - [ ] Documentation included in this PR.
    - [ ] **[Exceptional Case]** Documentation to be submitted in a separate
    PR.
    
    # PR Checklist\*
    
    - [x] I have tested the changes locally.
    - [x] I have formatted the changes with [Prettier](https://prettier.io/)
    and/or `cargo fmt` on default settings.

--- tooling/nargo/build.rs
+++ tooling/nargo/build.rs
@@ -2,8 +2,8 @@ use rustc_version::{version, Version};
 
 fn check_rustc_version() {
     assert!(
-        version().unwrap() >= Version::parse("1.71.1").unwrap(),
-        "The minimal supported rustc version is 1.71.1."
+        version().unwrap() >= Version::parse("1.73.0").unwrap(),
+        "The minimal supported rustc version is 1.73.0."
     );
 }
 
