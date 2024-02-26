9df3308665fd19a90d81a10fc1e20baff257ec72
Tom French <tom@tomfren.ch>
2024-02-26 11:46:02 +0000

    fix: fix formatting of `call_data` visibility

--- tooling/nargo_fmt/src/utils.rs
+++ tooling/nargo_fmt/src/utils.rs
@@ -145,7 +145,7 @@ impl HasItem for Param {
         let visibility = match self.visibility {
             Visibility::Public => "pub ",
             Visibility::Private => "",
-            Visibility::DataBus => "call_data",
+            Visibility::DataBus => "call_data ",
         };
 
         if self.pattern.is_synthesized() || self.typ.is_synthesized() {
