b9ba1fed61eeb66315cbc77dc5eb81ee87778ca1
AztecBot <tech@aztecprotocol.com>
2024-02-27 10:57:48 +0000

    chore: Sync to noir-lang/noir
    chore(noir): extend 4681 bitsize refactor (https://github.com/AztecProtocol/aztec-packages/pull/4689)
    feat!: renamings of state var wrappers (https://github.com/AztecProtocol/aztec-packages/pull/4739)
    chore(ci): enforce formatting of noir rust code (https://github.com/AztecProtocol/aztec-packages/pull/4765)
    chore: bootstrap improvements. (https://github.com/AztecProtocol/aztec-packages/pull/4711)
    refactor: using Tuples in `TxEffect`s and renaming note commitments (https://github.com/AztecProtocol/aztec-packages/pull/4717)
    chore: Build nargo against Ubuntu 20 for better compatability (https://github.com/AztecProtocol/aztec-packages/pull/4710)
    fix(dsl): Add full recursive verification test (https://github.com/AztecProtocol/aztec-packages/pull/4658)
    feat!: autogenerate compute_note_hash_and_nullifier (https://github.com/AztecProtocol/aztec-packages/pull/4610)
    chore: add struct for each bigint modulus (https://github.com/AztecProtocol/aztec-packages/pull/4422)

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,5 +15,8 @@ if [ -n "$CMD" ]; then
   fi
 fi
 
+# Attempt to just pull artefacts from CI and exit on success.
+./bootstrap_cache.sh && exit
+
 ./scripts/bootstrap_native.sh
 ./scripts/bootstrap_packages.sh
