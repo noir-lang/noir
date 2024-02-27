8307dadd853d5091841e169c841ab6b09c223efb
José Pedro Sousa <jose@aztecprotocol.com>
2024-02-27 12:05:26 +0000

    chore(docs): getting a bot to comment on docs PRs with docs previews (#4600)
    This PR makes it substantially easy to review and see changes in PRs
    containing docs changes by deploying a preview and commenting on the PR
    with a link to that preview.
    
    There are a few changes worth mentioning:
    - The deploy/non-deploy logic is moved from the CircleCI config to the
    downstream script `deploy_netlify`
    - This script will always deploy in prod when PRs are merged, so no
    change in logic here.
    - But now, the script gets that PR's file changes via the Github API and
    checks if there are changes in docs. If there are, then the deploy runs
    without the `--prod` flag, which will deploy a preview only
    - It then parses the output searching for the Unique URL which is passed
    to the bot comment logic
    - The bot comment logic is changed so it can be reused, by basically
    adding an enum for the different comment markdowns and importing them
    dynamically
    
    This way, the deploy only runs in PRs that have changes in docs,
    commenting on those PRs with a deploy preview. When merged, changes are
    deployed in prod.

--- docs/deploy_netlify.sh
+++ docs/deploy_netlify.sh
@@ -5,5 +5,41 @@ set -eu
 extract_repo docs /usr/src extracted-repo
 cd extracted-repo/src/docs
 npm install netlify-cli -g
-netlify deploy --site aztec-docs-dev
-netlify deploy --site aztec-docs-dev --prod
+
+DEPLOY_OUTPUT=""
+
+# Check if we're on master
+if [ "$1" = "master" ]; then
+    # Deploy to production if the argument is "master"
+    DEPLOY_OUTPUT=$(netlify deploy --site aztec-docs-dev --prod)
+else    
+    # TODO we should prob see if check_rebuild can be used for this
+    PR_URL="$2"
+    API_URL="${PR_URL/github.com/api.github.com/repos}"
+    API_URL="${API_URL/pull/pulls}"
+    API_URL="${API_URL}/files"
+
+    echo "API URL: $API_URL"
+
+    DOCS_CHANGED=$(curl -L \
+        -H "Authorization: Bearer $AZTEC_BOT_COMMENTER_GITHUB_TOKEN" \
+        "${API_URL}" | \
+        jq '[.[] | select(.filename | startswith("docs/"))] | length > 0')
+
+    echo "Docs changed: $DOCS_CHANGED"
+
+    if [ "$DOCS_CHANGED" = "false" ]; then
+        echo "No docs changed, not deploying"
+        exit 0
+    fi
+
+    # Regular deploy if the argument is not "master" and docs changed
+    DEPLOY_OUTPUT=$(netlify deploy --site aztec-docs-dev)
+    UNIQUE_DEPLOY_URL=$(echo "$DEPLOY_OUTPUT" | grep -E "https://.*aztec-docs-dev.netlify.app" | awk '{print $4}')
+    echo "Unique deploy URL: $UNIQUE_DEPLOY_URL"
+
+    extract_repo yarn-project /usr/src project
+    cd project/src/yarn-project/scripts
+
+    UNIQUE_DEPLOY_URL=$UNIQUE_DEPLOY_URL yarn docs-preview-comment
+fi
--- yarn-project/scripts/src/bin/docs-preview.ts
+++ yarn-project/scripts/src/bin/docs-preview.ts
@@ -1,7 +1,7 @@
 import { COMMENT_TYPES } from '../types.js';
 import main from '../utils/comment.js';
 
-void main(COMMENT_TYPES.BENCH).catch(err => {
+void main(COMMENT_TYPES.DOCS).catch(err => {
   // eslint-disable-next-line no-console
   console.error(err.message);
   process.exit(1);
