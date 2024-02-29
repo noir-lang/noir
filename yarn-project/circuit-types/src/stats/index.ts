cf8bd85376169cdeb6fbda40e19ae2601bbb3370
Santiago Palladino <santiago@aztecprotocol.com>
2024-02-29 14:18:07 -0300

    chore: Reduce size for rollup benchmark

--- yarn-project/circuit-types/src/stats/index.ts
+++ yarn-project/circuit-types/src/stats/index.ts
@@ -5,7 +5,7 @@ export * from './benchmarks.js';
 /** Block sizes to use for benchmark tests on multiple block sizes. */
 export const BENCHMARK_BLOCK_SIZES = process.env.BENCHMARK_BLOCK_SIZES
   ? process.env.BENCHMARK_BLOCK_SIZES.split(',').map(Number)
-  : [8, 32, 128];
+  : [8, 32, 64];
 
 /** Block size to use for building chains of multiple blocks. */
 export const BENCHMARK_HISTORY_BLOCK_SIZE = process.env.BENCHMARK_HISTORY_BLOCK_SIZE
