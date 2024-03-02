087efd3f42f15eae40527820ed2b89b5882c35aa
Tom French <tom@tomfren.ch>
2024-03-02 21:44:43 +0000

    feat: add optimization pass to remove witness equality constraints

--- acvm-repo/acvm/src/compiler/optimizers/mod.rs
+++ acvm-repo/acvm/src/compiler/optimizers/mod.rs
@@ -4,13 +4,15 @@ use acir::circuit::{Circuit, Opcode};
 mod general;
 mod redundant_range;
 mod unused_memory;
+mod witness_rebinding;
 
 pub(crate) use general::GeneralOptimizer;
 pub(crate) use redundant_range::RangeOptimizer;
 use tracing::info;
 
+use witness_rebinding::WitnessRebindingOptimizer;
 // use self::constant_backpropagation::ConstantBackpropagationOptimizer;
-use self::unused_memory::UnusedMemoryOptimizer;
+use unused_memory::UnusedMemoryOptimizer;
 
 use super::{transform_assert_messages, AcirTransformationMap};
 
@@ -58,18 +60,17 @@ pub(super) fn optimize_internal(acir: Circuit) -> (Circuit, Vec<usize>) {
     let (acir, acir_opcode_positions) =
         memory_optimizer.remove_unused_memory_initializations(acir_opcode_positions);
 
-    // let (acir, acir_opcode_positions) =
-    // ConstantBackpropagationOptimizer::backpropagate_constants(acir, acir_opcode_positions);
+    let (acir, acir_opcode_positions) =
+        WitnessRebindingOptimizer::rebind_witnesses(acir, acir_opcode_positions);
 
     // Range optimization pass
     let range_optimizer = RangeOptimizer::new(acir);
     let (acir, acir_opcode_positions) =
         range_optimizer.replace_redundant_ranges(acir_opcode_positions);
 
-    // let (acir, acir_opcode_positions) =
-    // ConstantBackpropagationOptimizer::backpropagate_constants(acir, acir_opcode_positions);
+    let (acir, acir_opcode_positions) =
+        WitnessRebindingOptimizer::rebind_witnesses(acir, acir_opcode_positions);
 
     info!("Number of opcodes after: {}", acir.opcodes.len());
-
     (acir, acir_opcode_positions)
 }
