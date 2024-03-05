c4bb9f08834dde75b52a17cd38f1b62f56525da5
AztecBot <tech@aztecprotocol.com>
2024-03-05 17:41:16 +0000

    chore: sync noir repo (#4947)

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -eu
+
+cd $(dirname "$0")
+
+CMD=${1:-}
+
+if [ -n "$CMD" ]; then
+  if [ "$CMD" = "clean" ]; then
+    git clean -fdx
+    exit 0
+  else
+    echo "Unknown command: $CMD"
+    exit 1
+  fi
+fi
+
+# Attempt to just pull artefacts from CI and exit on success.
+[ -n "${USE_CACHE:-}" ] && ./bootstrap_cache.sh && exit
+
+./scripts/bootstrap_native.sh
+./scripts/bootstrap_packages.sh
