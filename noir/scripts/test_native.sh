1be9243f17996429b4282413ef7db45b6229b537
Tom French <15848336+TomAFrench@users.noreply.github.com>
2024-03-07 19:17:07 +0000

    chore: disable `hello_world_example` noir test in aztec-packages CI (#5061)

--- noir/scripts/test_native.sh
+++ noir/scripts/test_native.sh
@@ -10,4 +10,8 @@ export GIT_COMMIT=${COMMIT_HASH:-$(git rev-parse --verify HEAD)}
 
 cargo fmt --all --check
 cargo clippy --workspace --locked --release
-cargo test --workspace --locked --release
+
+./.github/scripts/cargo-binstall-install.sh
+cargo-binstall cargo-nextest --version 0.9.67 -y --secure
+
+cargo nextest run --locked --release -E '!test(hello_world_example)'
