f8778e0d85a4fd9e75f6dc16f51257a956cce345
Jake Fecher <jfecher11@gmail.com>
2024-03-14 13:28:51 -0500

    Finish pass

--- compiler/noirc_evaluator/src/ssa/opt/rc.rs
+++ compiler/noirc_evaluator/src/ssa/opt/rc.rs
@@ -1,6 +1,15 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-use crate::ssa::{ir::{function::Function, instruction::InstructionId, types::Type}, ssa_gen::Ssa};
+use crate::ssa::{
+    ir::{
+        basic_block::BasicBlockId,
+        function::Function,
+        instruction::{Instruction, InstructionId, TerminatorInstruction},
+        types::Type,
+        value::ValueId,
+    },
+    ssa_gen::Ssa,
+};
 
 impl Ssa {
     /// This pass removes `inc_rc` and `dec_rc` instructions
@@ -23,19 +32,26 @@ impl Ssa {
 #[derive(Default)]
 struct Context {
     // All inc_rc instructions encountered without a corresponding dec_rc.
+    // These are only searched for in the first block of a function.
+    //
     // The type of the array being operated on is recorded.
     // If an array_set to that array type is encountered, that is also recorded.
-    inc_rcs: HashMap<Type, InstructionId>,
-
-    dec_rcs: HashMap<Type, InstructionId>,
-
-    // When a dec_rc is encountered, the most recent inc_rc (of a matching array type)
-    // is popped off the inc_rc_stack. If the IncDec object was not possibly mutated,
-    // then the inc_rc and dec_rc instructions are both pushed here to be removed
-    // from the program later.
-    inc_decs_to_remove: Vec<(InstructionId, InstructionId)>,
+    inc_rcs: HashMap<Type, Vec<IncRc>>,
 }
 
+struct IncRc {
+    id: InstructionId,
+    array: ValueId,
+    possibly_mutated: bool,
+}
+
+/// This function is very simplistic for now. It takes advantage of the fact that dec_rc
+/// instructions are currently issued only at the end of a function for parameters and will
+/// only check the first and last block for inc & dec rc instructions to be removed. The rest
+/// of the function is still checked for array_set instructions.
+///
+/// This restriction lets this function largely ignore merging intermediate results from other
+/// blocks and handling loops.
 fn remove_paired_rc(function: &mut Function) {
     // `dec_rc` is only issued for parameters currently so we can speed things
     // up a bit by skipping any functions without them.
@@ -45,15 +61,100 @@ fn remove_paired_rc(function: &mut Function) {
 
     let mut context = Context::default();
 
-    // Iterate through each block in the function to find array_sets first.
-    // We'll combine the results of each block later.
-    for block in function.reachable_blocks() {
-
-    }
+    context.find_rcs_in_entry_block(function);
+    context.scan_for_array_sets(function);
+    let to_remove = context.find_rcs_to_remove(function);
+    remove_instructions(to_remove, function);
 }
 
 fn contains_array_parameter(function: &mut Function) -> bool {
-    function.parameters().iter().any(|parameter| {
-        function.dfg.type_of_value(*parameter).contains_an_array()
-    })
+    let mut parameters = function.parameters().iter();
+    parameters.any(|parameter| function.dfg.type_of_value(*parameter).contains_an_array())
+}
+
+impl Context {
+    fn find_rcs_in_entry_block(&mut self, function: &Function) {
+        let entry = function.entry_block();
+
+        for instruction in function.dfg[entry].instructions() {
+            if let Instruction::IncrementRc { value } = &function.dfg[*instruction] {
+                let typ = function.dfg.type_of_value(*value);
+
+                // We assume arrays aren't mutated until we find an array_set
+                let inc_rc = IncRc { id: *instruction, array: *value, possibly_mutated: false };
+                self.inc_rcs.entry(typ).or_default().push(inc_rc);
+            }
+        }
+    }
+
+    /// Find each array_set instruction in the function and mark any arrays used
+    /// by the inc_rc instructions as possibly mutated if they're the same type.
+    fn scan_for_array_sets(&mut self, function: &Function) {
+        for block in function.reachable_blocks() {
+            for instruction in function.dfg[block].instructions() {
+                if let Instruction::ArraySet { array, .. } = function.dfg[*instruction] {
+                    let typ = function.dfg.type_of_value(array);
+                    if let Some(inc_rcs) = self.inc_rcs.get_mut(&typ) {
+                        for inc_rc in inc_rcs {
+                            inc_rc.possibly_mutated = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /// Find each dec_rc instruction and if the most recent inc_rc instruction for the same value
+    /// is not possibly mutated, then we can remove them both. Returns each such pair.
+    fn find_rcs_to_remove(&mut self, function: &Function) -> HashSet<InstructionId> {
+        let last_block = Self::find_last_block(function);
+        let mut to_remove = HashSet::new();
+
+        for instruction in function.dfg[last_block].instructions() {
+            if let Instruction::DecrementRc { value } = &function.dfg[*instruction] {
+                if let Some(inc_rc) = self.pop_rc_for(*value, function) {
+                    if !inc_rc.possibly_mutated {
+                        to_remove.insert(inc_rc.id);
+                        to_remove.insert(*instruction);
+                    }
+                }
+            }
+        }
+
+        to_remove
+    }
+
+    /// Finds the block of the function with the Return instruction
+    fn find_last_block(function: &Function) -> BasicBlockId {
+        for block in function.reachable_blocks() {
+            if matches!(
+                function.dfg[block].terminator(),
+                Some(TerminatorInstruction::Return { .. })
+            ) {
+                return block;
+            }
+        }
+
+        unreachable!("SSA Function {} has no reachable return instruction!", function.id())
+    }
+
+    /// Finds and pops the IncRc for the given array value if possible.
+    fn pop_rc_for(&mut self, value: ValueId, function: &Function) -> Option<IncRc> {
+        let typ = function.dfg.type_of_value(value);
+
+        let rcs = self.inc_rcs.get_mut(&typ)?;
+        let position = rcs.iter().position(|inc_rc| inc_rc.array == value)?;
+
+        Some(rcs.remove(position))
+    }
+}
+
+fn remove_instructions(to_remove: HashSet<InstructionId>, function: &mut Function) {
+    if !to_remove.is_empty() {
+        for block in function.reachable_blocks() {
+            function.dfg[block]
+                .instructions_mut()
+                .retain(|instruction| !to_remove.contains(instruction))
+        }
+    }
 }
