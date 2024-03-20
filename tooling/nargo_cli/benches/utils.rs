60ef1ae6556f3f0a0929730ea11e2223bd9cfdba
Tom French <tom@tomfren.ch>
2024-03-20 14:41:04 +0000

    chore: fix up benchmarking scripts

--- tooling/nargo_cli/benches/utils.rs
+++ tooling/nargo_cli/benches/utils.rs
@@ -4,11 +4,16 @@ use std::path::PathBuf;
 fn get_selected_tests() -> Vec<PathBuf> {
     let manifest_dir = match std::env::var("CARGO_MANIFEST_DIR") {
         Ok(dir) => PathBuf::from(dir),
-        Err(_) => std::env::current_dir().unwrap().join("crates").join("nargo_cli"),
+        Err(_) => std::env::current_dir().unwrap(),
     };
-    let test_dir = manifest_dir.join("tests").join("execution_success");
+    let test_dir = manifest_dir
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .join("test_programs")
+        .join("execution_success");
 
-    let selected_tests =
-        vec!["8_integration", "sha256_blocks", "struct", "eddsa", "regression", "regression_2099"];
+    let selected_tests = vec!["struct", "eddsa", "regression"];
     selected_tests.into_iter().map(|t| test_dir.join(t)).collect()
 }
