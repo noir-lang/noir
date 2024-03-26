9d2a8c51d5e6950ccedf2ba305c4604d3b80be88
vezenovm <mvezenov@gmail.com>
2024-03-26 15:46:50 +0000

    fix slice capacity tracker

--- compiler/noirc_evaluator/src/ssa/opt/flatten_cfg/capacity_tracker.rs
+++ compiler/noirc_evaluator/src/ssa/opt/flatten_cfg/capacity_tracker.rs
@@ -73,6 +73,7 @@ impl<'a> SliceCapacityTracker<'a> {
                         Intrinsic::SlicePopFront => (Some(1), results.len() - 1),
                         // The slice capacity of these intrinsics is not determined by the arguments of the function.
                         Intrinsic::ToBits(_) | Intrinsic::ToRadix(_) => (None, 1),
+                        Intrinsic::AsSlice => (Some(0), 1),
                         _ => return,
                     };
                     let result_slice = results[result_index];
@@ -90,10 +91,10 @@ impl<'a> SliceCapacityTracker<'a> {
                                     self.compute_slice_capacity(*arg, slice_sizes);
                                 }
                             }
-                            if let Some(contents_capacity) = slice_sizes.get(&slice_contents) {
-                                let new_capacity = *contents_capacity + 1;
-                                slice_sizes.insert(result_slice, new_capacity);
-                            }
+
+                            let contents_capacity = slice_sizes.get(&slice_contents).expect("ICE: Should have a capacity set for the input to a slice intrinsic");
+                            let new_capacity = *contents_capacity + 1;
+                            slice_sizes.insert(result_slice, new_capacity);
                         }
                         Intrinsic::SlicePopBack
                         | Intrinsic::SliceRemove
@@ -102,13 +103,9 @@ impl<'a> SliceCapacityTracker<'a> {
                                 .expect("ICE: Should have an argument index for slice intrinsics");
                             let slice_contents = arguments[argument_index];
 
-                            // We do not decrement the size on intrinsics that could remove values from a slice.
-                            // This is because we could potentially go back to the smaller slice and not fill in dummies.
-                            // This pass should be tracking the potential max that a slice ***could be***
-                            if let Some(contents_capacity) = slice_sizes.get(&slice_contents) {
-                                let new_capacity = *contents_capacity - 1;
-                                slice_sizes.insert(result_slice, new_capacity);
-                            }
+                            let contents_capacity = slice_sizes.get(&slice_contents).expect("ICE: Should have a capacity set for the input to a slice intrinsic");
+                            let new_capacity = *contents_capacity - 1;
+                            slice_sizes.insert(result_slice, new_capacity);
                         }
                         Intrinsic::ToBits(_) => {
                             // Compiler sanity check
@@ -120,6 +117,15 @@ impl<'a> SliceCapacityTracker<'a> {
                             assert!(matches!(self.dfg.type_of_value(result_slice), Type::Slice(_)));
                             slice_sizes
                                 .insert(result_slice, FieldElement::max_num_bytes() as usize);
+                        }
+                        Intrinsic::AsSlice => {
+                            let argument_index = argument_index
+                                .expect("ICE: Should have an argument index for AsSlice builtin");
+                            let array_size = self
+                                .dfg
+                                .try_get_array_length(arguments[argument_index])
+                                .expect("ICE: Should be have an array length for AsSlice input");
+                            slice_sizes.insert(result_slice, array_size);
                         }
                         _ => {}
                     }
