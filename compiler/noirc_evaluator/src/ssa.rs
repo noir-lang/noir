dd47fe03c256eeb8a689642ef83ca1358ecb3755
jfecher <jake@aztecprotocol.com>
2024-03-28 12:39:22 -0500

    Update compiler/noirc_evaluator/src/ssa.rs
    Co-authored-by: Maxim Vezenov <mvezenov@gmail.com>

--- compiler/noirc_evaluator/src/ssa.rs
+++ compiler/noirc_evaluator/src/ssa.rs
@@ -224,7 +224,7 @@ impl SsaBuilder {
         pass: fn(Ssa) -> Result<Ssa, RuntimeError>,
         msg: &str,
     ) -> Result<Self, RuntimeError> {
-        self.ssa = time(msg, self.print_ssa_passes, || pass(self.ssa))?;
+        self.ssa = time(msg, self.print_codegen_timings, || pass(self.ssa))?;
         Ok(self.print(msg))
     }
 
