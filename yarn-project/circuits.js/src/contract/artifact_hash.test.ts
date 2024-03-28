5c5b6270e4cc9c09c7f208f6523227e51d4acc20
esau <152162806+sklppy88@users.noreply.github.com>
2024-03-28 19:17:43 +0100

    refactor: Make get notes return all notes at beginning of array #4991 (#5321)
    Resolves #4991
    
    ---------
    
    Co-authored-by: Miranda Wood <miranda@aztecprotocol.com>
    Co-authored-by: ledwards2225 <98505400+ledwards2225@users.noreply.github.com>
    Co-authored-by: maramihali <mara@aztecprotocol.com>
    Co-authored-by: Santiago Palladino <santiago@aztecprotocol.com>
    Co-authored-by: ludamad <adam.domurad@gmail.com>
    Co-authored-by: AztecBot <tech@aztecprotocol.com>
    Co-authored-by: Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
    Co-authored-by: PhilWindle <60546371+PhilWindle@users.noreply.github.com>
    Co-authored-by: Jan Beneš <janbenes1234@gmail.com>

--- yarn-project/circuits.js/src/contract/artifact_hash.test.ts
+++ yarn-project/circuits.js/src/contract/artifact_hash.test.ts
@@ -5,7 +5,7 @@ describe('ArtifactHash', () => {
   it('calculates the artifact hash', () => {
     const artifact = getBenchmarkContractArtifact();
     expect(computeArtifactHash(artifact).toString()).toMatchInlineSnapshot(
-      `"0x1c4308cf4acda970916ac5dd5ae32106bd782b17d2da4f50090ff5b77032ad02"`,
+      `"0x12ee05255924ea5eed79c7fe41f57a5309e40d6c9861efac8a8986dac6145a63"`,
     );
   });
 });
