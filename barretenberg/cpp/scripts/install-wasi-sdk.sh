bea3fcbde91d08f13cb7c2ceeff8be33b3edcdfd
ludamad <adam.domurad@gmail.com>
2024-03-28 12:15:58 -0400

    fix(ci): turn on earthly for everyone (#5423)
    - Reenable and fix e2e-browser and e2e-docs-examples (which was broken
    in master, fixed by Palla)
    - Re-enstate single-threaded wasi for e2e-browser
    Note there is an intermittent issue with client_ivc_tests in ARM (more
    often than x86, anyway) so disabling for now until thats sorted
    - Reduce number of runners (2 x86, 1 ARM, 1 x86 smaller bench machine)
    because we now use e2e with github runners
    - Turn on earthly for everyone

--- barretenberg/cpp/scripts/install-wasi-sdk.sh
+++ barretenberg/cpp/scripts/install-wasi-sdk.sh
@@ -22,7 +22,6 @@ fi
 # Install the webassembly toolchain.
 mkdir -p src
 cd ./src
-# TODO(https://github.com/AztecProtocol/barretenberg/issues/865): is this needed?
 curl -s -L https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-20/wasi-sdk-20.0-$OS.tar.gz | tar zxf -
 if [ "$OS" == "linux" ]; then
   # Exceptional linux case that provides an llvm build that works on Ubuntu 20.
@@ -35,5 +34,5 @@ else
   # Can remove once future releases are more stable.
   curl -s -L https://wasi-sdk.s3.eu-west-2.amazonaws.com/yamt-wasi-sysroot-20.0.threads.tgz | tar zxf -
 fi
-# TODO(https://github.com/AztecProtocol/barretenberg/issues/906): in the future this should use eartlhy and a 'SAVE ARTIFACT wasi-sdk AS LOCAL wasi-sdk'
+# TODO(https://github.com/AztecProtocol/barretenberg/issues/906): in the future this should use earthly and a 'SAVE ARTIFACT wasi-sdk AS LOCAL wasi-sdk'
 mv wasi-sdk-20.0+threads wasi-sdk
