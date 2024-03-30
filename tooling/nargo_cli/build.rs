60e6f2fed66bb333c86bc4e8915429007b1d55b7
Tom French <tom@tomfren.ch>
2024-03-30 01:44:09 +0000

    chore: remove last traces of nix

--- tooling/nargo_cli/build.rs
+++ tooling/nargo_cli/build.rs
@@ -16,8 +16,7 @@ const GIT_COMMIT: &&str = &"GIT_COMMIT";
 fn main() {
     check_rustc_version();
 
-    // Only use build_data if the environment variable isn't set
-    // The environment variable is always set when working via Nix
+    // Only use build_data if the environment variable isn't set.
     if std::env::var(GIT_COMMIT).is_err() {
         build_data::set_GIT_COMMIT();
         build_data::set_GIT_DIRTY();
