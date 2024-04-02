74c871d8125057ef2eae624593ba0fbca846f1ed
vezenovm <mvezenov@gmail.com>
2024-04-02 18:00:05 +0000

    use core Future

--- acvm-repo/acvm_js/src/execute.rs
+++ acvm-repo/acvm_js/src/execute.rs
@@ -14,6 +14,7 @@ use crate::{
     JsExecutionError, JsWitnessMap, JsWitnessStack,
 };
 use core::pin::Pin;
+use core::future::Future;
 
 #[wasm_bindgen]
 pub struct WasmBlackBoxFunctionSolver(Bn254BlackBoxSolver);
