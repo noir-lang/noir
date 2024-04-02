5190ead97e97b7c12d08f1183845425da07536fd
Michael J. Klein <lambdamichael@gmail.com>
2024-04-02 17:10:45 -0400

    replace is_whitespace with is_code_whitespace (prevent vertical tabs, etc), cleanup, wip implementing use statements, in the middle of ..:{..}-style recursive use statements, enhance whitespace handling in shim from lexer to lalrpop

--- compiler/noirc_frontend/src/lexer/lexer.rs
+++ compiler/noirc_frontend/src/lexer/lexer.rs
@@ -94,7 +94,7 @@ impl<'a> Lexer<'a> {
 
     fn next_token(&mut self) -> SpannedTokenResult {
         match self.next_char() {
-            Some(x) if x.is_whitespace() => {
+            Some(x) if Self::is_code_whitespace(x) => {
                 let spanned = self.eat_whitespace(x);
                 if self.skip_whitespaces {
                     self.next_token()
@@ -560,10 +560,14 @@ impl<'a> Lexer<'a> {
         }
     }
 
+    fn is_code_whitespace(c: char) -> bool {
+        c == '\t' || c == '\n' || c == '\r' || c == ' '
+    }
+
     /// Skips white space. They are not significant in the source language
     fn eat_whitespace(&mut self, initial_char: char) -> SpannedToken {
         let start = self.position;
-        let whitespace = self.eat_while(initial_char.into(), |ch| ch.is_whitespace());
+        let whitespace = self.eat_while(initial_char.into(), Self::is_code_whitespace);
         SpannedToken::new(Token::Whitespace(whitespace), Span::inclusive(start, self.position))
     }
 }
@@ -1153,77 +1157,6 @@ mod tests {
         }
     }
 
-    #[test]
-    fn test_lalrpop() {
-        use lalrpop_util::lalrpop_mod;
-        lalrpop_mod!(pub noir_parser); // synthesized by LALRPOP
-
-        // let input = "<";
-        // let mut test_lexer = Lexer::new(input);
-        // assert_eq!(test_lexer.next(), Some(Ok((0, Token::Less, 1))));
-        // assert_eq!(test_lexer.next(), Some(Ok((0, Token::EOF, 1))));
-        // assert_eq!(test_lexer.next(), None);
-
-        // let input = "! != + ( ) { } [ ] | , ; : :: < <= > >= & - -> . .. % / * = == << >>";
-        // let input = "23314";
-        let input = "true";
-        let mut lexer = Lexer::new(input);
-
-        // let calculated = noir_parser::TermParser::new().parse(&mut errors, lexer);
-
-        let mut errors = Vec::new();
-        let mut test_lexer = Lexer::new(input);
-        assert_eq!(test_lexer.next(), Some(Ok((0, Token::Bool(true), 4))));
-        assert_eq!(test_lexer.next(), Some(Ok((3, Token::EOF, 4))));
-        assert_eq!(test_lexer.next(), None);
-
-
-        // TODO: this is a hack to get the references working
-        // -> this likely means that we'll want to propagate the <'input> lifetime further into Token
-        let lexer_result = lexer.into_iter().collect::<Vec<_>>();
-        let referenced_lexer_result = lexer_result.iter().map(|token_result| {
-            token_result.as_ref().map(|(start, ref token, end)| {
-                (*start, token_to_tok(token), *end)
-            }).map_err(|x| x.clone())
-        });
-
-        let calculated = noir_parser::TermParser::new().parse(input, &mut errors, referenced_lexer_result);
-
-        // assert!(calculated == Ok(Token::Int(23314_i128.into())), "{:?}", calculated);
-        // assert!(calculated.is_ok(), "{:?}", calculated);
-        assert_eq!(calculated, Ok(Tok::Bool(true)), "{:?}", calculated);
-
-
-        // use statement tests
-        let use_statement_inputs = vec![
-            "use dep::std::compat;",
-            "use dep::std::ec::consts::te::baby_jubjub;",
-            "use dep::std::ec::tecurve::affine::Point as TEPoint;",
-            "use dep::std::hash;",
-            "use dep::std::eddsa::{eddsa_to_pub, eddsa_poseidon_verify, eddsa_verify_with_hasher};",
-            "use dep::std::hash::poseidon2::Poseidon2Hasher;",
-            "use dep::std::hash::pedersen::PedersenHasher;",
-        ];
-
-        for use_statement_input in use_statement_inputs {
-            let mut lexer = Lexer::new(use_statement_input);
-            let mut errors = Vec::new();
-
-            // TODO: this is a hack to get the references working
-            // -> this likely means that we'll want to propagate the <'input> lifetime further into Token
-            let lexer_result = lexer.into_iter().collect::<Vec<_>>();
-            let referenced_lexer_result = lexer_result.iter().map(|token_result| {
-                token_result.as_ref().map(|(start, ref token, end)| {
-                    (*start, token_to_tok(token), *end)
-                }).map_err(|x| x.clone())
-            });
-
-            let calculated = noir_parser::PathParser::new().parse(use_statement_input, &mut errors, referenced_lexer_result);
-            assert!(calculated.is_ok(), "{:?}", calculated);
-        }
-
-    }
-
     // returns a vector of:
     //   (expected_token_discriminator, strings_to_lex)
     // expected_token_discriminator matches a given token when
@@ -1332,5 +1265,78 @@ mod tests {
                 }
             }
         }
+    }
+
+    #[test]
+    fn test_lalrpop() {
+        use lalrpop_util::lalrpop_mod;
+        lalrpop_mod!(pub noir_parser); // synthesized by LALRPOP
+
+        // let input = "<";
+        // let mut test_lexer = Lexer::new(input);
+        // assert_eq!(test_lexer.next(), Some(Ok((0, Token::Less, 1))));
+        // assert_eq!(test_lexer.next(), Some(Ok((0, Token::EOF, 1))));
+        // assert_eq!(test_lexer.next(), None);
+
+        // // let input = "! != + ( ) { } [ ] | , ; : :: < <= > >= & - -> . .. % / * = == << >>";
+        // // let input = "23314";
+        // let input = "true";
+        // let lexer = Lexer::new(input);
+        //
+        // // let calculated = noir_parser::TermParser::new().parse(&mut errors, lexer);
+        //
+        // let mut errors = Vec::new();
+        // let mut test_lexer = Lexer::new(input);
+        // assert_eq!(test_lexer.next(), Some(Ok((0, Token::Bool(true), 4))));
+        // assert_eq!(test_lexer.next(), Some(Ok((3, Token::EOF, 4))));
+        // assert_eq!(test_lexer.next(), None);
+        //
+        //
+        // // TODO: this is a hack to get the references working
+        // // -> this likely means that we'll want to propagate the <'input> lifetime further into Token
+        // let lexer_result = lexer.into_iter().collect::<Vec<_>>();
+        // let referenced_lexer_result = lexer_result.iter().map(|token_result| {
+        //     token_result.as_ref().map(|(start, ref token, end)| {
+        //         (*start, token_to_tok(token), *end)
+        //     }).map_err(|x| x.clone())
+        // });
+        //
+        // let calculated = noir_parser::TopLevelStatementParser::new().parse(input, &mut errors, referenced_lexer_result);
+        //
+        // // assert!(calculated == Ok(Token::Int(23314_i128.into())), "{:?}", calculated);
+        // // assert!(calculated.is_ok(), "{:?}", calculated);
+        // assert_eq!(calculated, Ok(Tok::Bool(true)), "{:?}", calculated);
+
+
+        // use statement tests
+        let use_statement_inputs = vec![
+            "use\t\r\n   dep::std::compat;",
+            "use dep::std::compat;",
+            "use dep::std::ec::consts::te::baby_jubjub;",
+            "use dep::std::hash;",
+            "use dep::std::hash::poseidon2::Poseidon2Hasher;",
+            "use dep::std::hash::pedersen::PedersenHasher;",
+            "use dep::std::ec::tecurve::affine::Point as TEPoint;",
+            "use dep::std::eddsa::{eddsa_to_pub, eddsa_poseidon_verify, eddsa_verify_with_hasher};",
+        ];
+
+        for use_statement_input in use_statement_inputs {
+            let mut lexer = Lexer::new(use_statement_input);
+            lexer.skip_whitespaces = false;
+            let mut errors = Vec::new();
+
+            // TODO: this is a hack to get the references working
+            // -> this likely means that we'll want to propagate the <'input> lifetime further into Token
+            let lexer_result = lexer.into_iter().collect::<Vec<_>>();
+            let referenced_lexer_result = lexer_result.iter().map(|token_result| {
+                token_result.as_ref().map(|(start, ref token, end)| {
+                    (*start, token_to_tok(token), *end)
+                }).map_err(|x| x.clone())
+            });
+
+            let calculated = noir_parser::TopLevelStatementParser::new().parse(use_statement_input, &mut errors, referenced_lexer_result);
+            assert!(calculated.is_ok(), "{:?}\n\n{:?}\n\n{:?}", calculated, lexer_result, use_statement_input);
+        }
+
     }
 }
