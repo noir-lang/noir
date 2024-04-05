3e6c4dd4cdfebaca2a9e2f4f4f58d8b2dc0dd42a
Tom French <tom@tomfren.ch>
2024-04-05 21:13:12 +0100

    chore: cut down stuff to just recursion proof

--- compiler/integration-tests/test/browser/recursion.test.ts
+++ compiler/integration-tests/test/browser/recursion.test.ts
@@ -41,62 +41,6 @@ describe('It compiles noir program code, receiving circuit bytes and abi object.
     circuit_main_toml = await new Response(await getFile(`${base_relative_path}/${circuit_main}/Prover.toml`)).text();
   });
 
-  it('Should generate valid inner proof for correct input, then verify proof within a proof', async () => {
-    const main_program = await getCircuit(`${base_relative_path}/${circuit_main}`);
-    const main_inputs: InputMap = TOML.parse(circuit_main_toml) as InputMap;
-
-    const main_backend = new BarretenbergBackend(main_program);
-
-    const { witness: main_witnessUint8Array } = await new Noir(main_program).execute(main_inputs);
-
-    const main_proof = await main_backend.generateProof(main_witnessUint8Array);
-    const main_verification = await main_backend.verifyProof(main_proof);
-
-    logger.debug('main_verification', main_verification);
-
-    expect(main_verification).to.be.true;
-
-    const numPublicInputs = 1;
-    const { proofAsFields, vkAsFields, vkHash } = await main_backend.generateRecursiveProofArtifacts(
-      main_proof,
-      numPublicInputs,
-    );
-
-    const recursion_inputs: InputMap = {
-      verification_key: vkAsFields,
-      proof: proofAsFields,
-      public_inputs: [main_inputs.y as Field],
-      key_hash: vkHash,
-    };
-
-    logger.debug('recursion_inputs', recursion_inputs);
-
-    const recursion_program = await getCircuit(`${base_relative_path}/${circuit_recursion}`);
-
-    const recursion_backend = new BarretenbergBackend(recursion_program);
-
-    const { witness: recursion_witnessUint8Array } = await new Noir(recursion_program).execute(recursion_inputs);
-
-    const recursion_proof = await recursion_backend.generateProof(recursion_witnessUint8Array);
-
-    // Causes an "unreachable" error.
-    // Due to the fact that it's a non-recursive proof?
-    //
-    // const recursion_numPublicInputs = 1;
-    // const { proofAsFields: recursion_proofAsFields } = await recursion_backend.generateRecursiveProofArtifacts(
-    //   recursion_proof,
-    //   recursion_numPublicInputs,
-    // );
-    //
-    // logger.debug('recursion_proofAsFields', recursion_proofAsFields);
-
-    const recursion_verification = await recursion_backend.verifyProof(recursion_proof);
-
-    logger.debug('recursion_verification', recursion_verification);
-
-    expect(recursion_verification).to.be.true;
-  }).timeout(60 * 20e3);
-
   it('Should generate two valid inner proofs for correct input, then verify proofs within a proof', async () => {
     const main_program = await getCircuit(`${base_relative_path}/${circuit_main}`);
     const main_inputs: InputMap = TOML.parse(circuit_main_toml) as InputMap;
@@ -137,6 +81,8 @@ describe('It compiles noir program code, receiving circuit bytes and abi object.
 
     const { witness: recursion_witnessUint8Array } = await new Noir(recursion_program).execute(recursion_inputs);
 
+
+    console.log("got here!")
     const recursion_proof = await recursion_backend.generateProof(recursion_witnessUint8Array);
 
     // Causes an "unreachable" error.
@@ -150,10 +96,10 @@ describe('It compiles noir program code, receiving circuit bytes and abi object.
     //
     // logger.debug('recursion_proofAsFields', recursion_proofAsFields);
 
-    const recursion_verification = await recursion_backend.verifyProof(recursion_proof);
+    // const recursion_verification = await recursion_backend.verifyProof(recursion_proof);
 
-    logger.debug('recursion_verification', recursion_verification);
+    // logger.debug('recursion_verification', recursion_verification);
 
-    expect(recursion_verification).to.be.true;
+    // expect(recursion_verification).to.be.true;
   }).timeout(60 * 20e3);
 });
