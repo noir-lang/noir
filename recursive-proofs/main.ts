5e036fca53175f3128fb7794b99d71ad94c4c0e5
James Zaki <james.zaki@proton.me>
2024-04-05 17:55:27 +0100

    Verify two inner proofs (exception)

--- recursive-proofs/main.ts
+++ recursive-proofs/main.ts
@@ -33,49 +33,39 @@ type FullNoir = {
 }
 
 async function start() {
-  // Generate inner proof artifacts
-  console.log("Creating Noir from circuit...");
   const simple: FullNoir = await fullNoirFromCircuit('not_odd');
+  const outer: FullNoir = await fullNoirFromCircuit('recurse');
 
-  console.log("Executing binary circuit for witness...");
-  const innerInput = { n: 2 };
-  const innerWitness = (await simple.noir.execute(innerInput)).witness;
-  console.log("Generating intermediate proof...");
-  const innerProof: ProofData = await simple.backend.generateProof(innerWitness);
+  // Generate inner proof artifacts
+  console.log("Generating intermediate proof artifacts 1...");
+  const innerWitness1 = (await simple.noir.execute({ n: 2 })).witness;
+  const innerProof1: ProofData = await simple.backend.generateProof(innerWitness1);
+  const artifacts1 = await simple.backend.generateRecursiveProofArtifacts(innerProof1, 1);
 
-  if (false) { // mess up proof
-    console.log("Messing intermediate proof...");
-    innerProof.proof[0] += 1;
-  }
+  console.log("Generating intermediate proof artifacts 2...");
+  const innerWitness2 = (await simple.noir.execute({ n: 4 })).witness;
+  const innerProof2: ProofData = await simple.backend.generateProof(innerWitness2);
+  const artifacts2 = await simple.backend.generateRecursiveProofArtifacts(innerProof2, 1);
 
-  console.log("Generating recursive proof artifacts...");
-  const { proofAsFields, vkAsFields, vkHash } = await simple.backend.generateRecursiveProofArtifacts(innerProof, 1);
-  // console.log({ proofAsFields, vkAsFields, vkHash });
   simple.backend.destroy();
 
   // Generate and verify outer proof
-  console.log("Creating Noir from circuit...");
-  const outer: FullNoir = await fullNoirFromCircuit('recurse');
-  console.log("Executing binary circuit for witness...");
+  console.log("Generating outer proof...");
   const outerInput = {
-    verification_key: vkAsFields,
-    proof: proofAsFields,
-    public_inputs: ["0"],
-    key_hash: vkHash
+    verification_key: artifacts1.vkAsFields,
+    public_inputs: ["1"], // expect output of inner call to be "true"
+    key_hash: artifacts1.vkHash,
+    proof1: artifacts1.proofAsFields,
+    proof2: artifacts2.proofAsFields
   };
   const outerWitness = (await outer.noir.execute(
     outerInput
   )).witness;
-
-  console.log("Generating outer proof...");
   const outerProof: ProofData = await outer.backend.generateProof(outerWitness);
 
   console.log("Verifying outer proof...");
-  console.log(await outer.backend.verifyProof(outerProof));
-
-  // console.log("Executing lib circuit function to verify inner proof");
-  // let res = await main(vkAsFields, proofAsFields, ["7"], vkHash);
-  // console.log(res);
+  const res: boolean = await outer.backend.verifyProof(outerProof);
+  console.log("Verification", res ? "PASSED" : "failed");
 
   outer.backend.destroy();
 }
