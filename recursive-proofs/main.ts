112f83d03f5c379e2cce565c34e8302f7e2d8980
James Zaki <james.zaki@proton.me>
2024-04-12 13:31:04 +0100

    Modify for single nested recursion

--- recursive-proofs/main.ts
+++ recursive-proofs/main.ts
@@ -35,45 +35,68 @@ type FullNoir = {
 }
 
 async function start() {
-  const simple: FullNoir = await fullNoirFromCircuit('not_odd');
-  const outer: FullNoir = await fullNoirFromCircuit('recurse');
+  const leaf: FullNoir = await fullNoirFromCircuit('sum');
 
-  // Generate inner proof artifacts
-  console.log("Generating intermediate proof artifacts 1...");
-  const innerWitness1 = (await simple.noir.execute({ n: 2 })).witness;
-  const innerProof1: ProofData = await simple.backend.generateProof(innerWitness1);
-  const artifacts1 = await simple.backend.generateRecursiveProofArtifacts(innerProof1, 1);
+  const leafParams = { a: 1, b: 3 };
 
-  console.log("Generating intermediate proof artifacts 2...");
-  const innerWitness2 = (await simple.noir.execute({ n: 4 })).witness;
-  const innerProof2: ProofData = await simple.backend.generateProof(innerWitness2);
-  const artifacts2 = await simple.backend.generateRecursiveProofArtifacts(innerProof2, 1);
+  // Generate leaf proof artifacts
+  let { witness, returnValue } = await leaf.noir.execute(leafParams);
+  console.log("leaf: %d + %d = ", ...Object.values(leafParams), Number(returnValue).toString());
+  const innerProof1: ProofData = await leaf.backend.generateProof(witness);
+  console.log("Generating intermediate proof artifacts leaf...");
+  const artifacts1 = await leaf.backend.generateRecursiveProofArtifacts(
+    innerProof1,
+    Object.keys(leafParams).length + 1
+  );
 
-  simple.backend.destroy();
+
+  let pub_inputs: string[] = [
+    ...(Object.values(leafParams).map(a => Number(a).toString())),
+    Number(returnValue).toString()
+  ];
+
+  let recurseLeaf: FullNoir = await fullNoirFromCircuit('recurseLeaf');
+
+  const a = returnValue;
+  const b = 5;
+
+  const nodeParams = {
+    verification_key: artifacts1.vkAsFields,
+    public_inputs: pub_inputs,
+    key_hash: artifacts1.vkHash,
+    proof: artifacts1.proofAsFields,
+    num: 5
+  };
+
+  ({ witness, returnValue } = await recurseLeaf.noir.execute(nodeParams));
+  console.log("recurseLeaf: %d + %d = ", a, b, Number(returnValue).toString());
+  const innerProof2: ProofData = await recurseLeaf.backend.generateProof(witness);
+  console.log("Generating intermediate proof artifacts recurseLeaf...");
+  const artifacts2 = await recurseLeaf.backend.generateRecursiveProofArtifacts(
+    innerProof2,
+    Object.keys(nodeParams).length + 1
+  );
+  console.log("artifacts2 generated.");
 
   // Generate and verify outer proof
-  const outerInput = {
-    verification_key: artifacts1.vkAsFields,
-    public_inputs: ["1"], // expect output of inner call to be "true"
-    key_hash: artifacts1.vkHash,
-    proof1: artifacts1.proofAsFields,
-    proof2: artifacts2.proofAsFields
-  };
-  const outerWitness = (await outer.noir.execute(
-    outerInput
-  )).witness;
-  try {
-    console.log("Generating outer proof...");
-    const outerProof: ProofData = await outer.backend.generateProof(outerWitness);
-    console.log("Verifying outer proof...");
-    const res: boolean = await outer.backend.verifyProof(outerProof);
-    console.log("Verification", res ? "PASSED" : "failed");
-  }
-  catch (e) {
-    console.log(e);
-  }
+  // const outerParams = {
+  //   verification_key: artifacts2.vkAsFields,
+  //   public_inputs: [...(Object.values(nodeParams)), returnValue], // returns proven sum
+  //   key_hash: artifacts2.vkHash,
+  //   proof: artifacts2.proofAsFields
+  // };
 
-  outer.backend.destroy();
+  // const recurseNode: FullNoir = await fullNoirFromCircuit('recurseNode');
+  // ({ witness, returnValue } = await recurseNode.noir.execute(outerParams));
+  // console.log("Generating outer proof...");
+  // const outerProof: ProofData = await recurseNode.backend.generateProof(witness);
+  // console.log("Verifying outer proof...");
+  // const res: boolean = await recurseNode.backend.verifyProof(outerProof);
+  // console.log("Verification", res ? "PASSED" : "failed");
+
+  // recurseNode.backend.destroy();
+  recurseLeaf.backend.destroy();
+  leaf.backend.destroy();
 }
 
 start();
