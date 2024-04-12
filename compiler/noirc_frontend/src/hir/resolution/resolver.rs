6bdc324a2506a3c8cceb0995066cfd8f3b67ac5a
jfecher <jake@aztecprotocol.com>
2024-04-12 13:18:28 -0400

    chore: Add Hir -> Ast conversion (#4788)
    # Description
    
    ## Problem\*
    
    Resolves https://github.com/noir-lang/noir/issues/4796
    
    ## Summary\*
    
     Adds a way to go from the Hir back to the Ast.
    This conversion isn't exact and information is lost currently. Notably
    many spans are lost and some constructs like `Type::TypeVariable` have
    no equivalent in the Ast.
    
    This also isn't expected to be idempotent. Different nodes may be
    desugared, `let` statements may have explicit types added, etc.
    
    ## Additional Context
    
    This is unused currently and can't be triggered from the current
    compiler at all. I'm adding this PR now to split off required changes
    for metaprogramming into smaller chunks.
    
    ## Documentation\*
    
    Check one:
    - [x] No documentation needed.
    - [ ] Documentation included in this PR.
    - [ ] **[For Experimental Features]** Documentation to be submitted in a
    separate PR.
    
    # PR Checklist\*
    
    - [x] I have tested the changes locally.
    - [x] I have formatted the changes with [Prettier](https://prettier.io/)
    and/or `cargo fmt` on default settings.

--- compiler/noirc_frontend/src/hir/resolution/resolver.rs
+++ compiler/noirc_frontend/src/hir/resolution/resolver.rs
@@ -502,6 +502,7 @@ impl<'a> Resolver<'a> {
                 let fields = self.resolve_type_inner(*fields, new_variables);
                 Type::FmtString(Box::new(resolved_size), Box::new(fields))
             }
+            Code => Type::Code,
             Unit => Type::Unit,
             Unspecified => Type::Error,
             Error => Type::Error,
