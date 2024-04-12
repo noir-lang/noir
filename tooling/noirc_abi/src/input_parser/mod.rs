68b8fd5498bf71b56c1f1aad36b7be4369fccb65
Tom French <15848336+TomAFrench@users.noreply.github.com>
2024-04-12 18:24:33 +0100

    Update tooling/noirc_abi/src/input_parser/mod.rs

--- tooling/noirc_abi/src/input_parser/mod.rs
+++ tooling/noirc_abi/src/input_parser/mod.rs
@@ -139,7 +139,7 @@ impl InputValue {
                 if map.len() > fields.len() {
                     let expected_fields: HashSet<String> =
                         fields.iter().map(|(field, _)| field.to_string()).collect();
-                    let extra_field = map.keys().cloned().find(|key| !expected_fields.contains(key)).expect("`map` is larger than type so there must exist on which does not exist in `fields`");
+                    let extra_field = map.keys().cloned().find(|key| !expected_fields.contains(key)).expect("`map` is larger than the expected type's `fields` so it must contain an unexpected field");
                     return Err(InputTypecheckingError::UnexpectedField {
                         path,
                         typ: abi_param.clone(),
