9084c9912c2fb433d629404de1c59a88ec7cf0a4
Stan Manilov <stan.manilov@metacraft-labs.com>
2024-04-16 14:36:10 +0300

    Prepare to support different PLONKY2 target types
    The new possibilities Boolean and Array are not used yet, but more
    support is added for Integer, including propagation of bit size to
    resulting target.
    
    This commit improves error handling.
    
    The new test category noirc_frontend_failure is added for testing input
    programs that are expected to trigger an error in noirc_frontend.

--- compiler/noirc_evaluator/src/ssa/plonky2_gen/mod.rs
+++ compiler/noirc_evaluator/src/ssa/plonky2_gen/mod.rs
@@ -10,7 +10,10 @@ use super::{
 };
 use acvm::FieldElement;
 pub use circuit::Plonky2Circuit;
-use plonky2::{field::types::Field, iop::target::Target, plonk::circuit_data::CircuitConfig};
+use plonky2::{
+    field::types::Field, iop::target::BoolTarget, iop::target::Target,
+    plonk::circuit_data::CircuitConfig,
+};
 use std::collections::HashMap;
 
 use self::config::{P2Builder, P2Config, P2Field};
@@ -24,7 +27,34 @@ use crate::ssa::ir::{
 };
 
 struct P2Value {
-    target: Target,
+    target: P2Target,
+    typ: P2Type,
+}
+
+impl P2Value {
+    fn get_target(&self) -> Result<Target, Plonky2GenError> {
+        Ok(match self.target {
+            P2Target::IntTarget(target) => target,
+            P2Target::BoolTarget(bool_target) => bool_target.target,
+            _ => {
+                return Err(Plonky2GenError::ICE {
+                    message: "get_target called on a non-int, non-bool value".to_owned(),
+                })
+            }
+        })
+    }
+}
+
+enum P2Type {
+    Boolean,
+    Integer(u32),
+    Array,
+}
+
+enum P2Target {
+    IntTarget(Target),
+    BoolTarget(BoolTarget),
+    ArrayTarget(u32),
 }
 
 pub(crate) struct Builder {
@@ -47,11 +77,11 @@ impl Builder {
             ssa.functions.into_values().find(|value| value.name() == "main").unwrap();
         let entry_block_id = main_function.entry_block();
         let entry_block = main_function.dfg[entry_block_id].clone();
-        let parameters = entry_block
-            .parameters()
-            .iter()
-            .map(|value_id| self.add_parameter(&main_function.dfg, *value_id))
-            .collect();
+
+        let mut parameters = Vec::new();
+        for value_id in entry_block.parameters().iter() {
+            parameters.push(self.add_parameter(&main_function.dfg, *value_id)?);
+        }
         for instruction_id in entry_block.instructions() {
             match self.add_instruction(&main_function.dfg, *instruction_id) {
                 Err(error) => return Err(error),
@@ -62,38 +92,57 @@ impl Builder {
         Ok(Plonky2Circuit { data, parameters, parameter_names })
     }
 
-    fn add_parameter(&mut self, dfg: &DataFlowGraph, value_id: ValueId) -> Target {
+    fn add_parameter(
+        &mut self,
+        dfg: &DataFlowGraph,
+        value_id: ValueId,
+    ) -> Result<Target, Plonky2GenError> {
         let value = dfg[value_id].clone();
         let p2value = match value {
             Value::Param { block: _, position: _, typ } => match typ {
                 Type::Numeric(numeric_type) => match numeric_type {
-                    NumericType::Unsigned { .. } => {
-                        P2Value { target: self.builder.add_virtual_target() }
+                    NumericType::Unsigned { bit_size } => P2Value {
+                        target: P2Target::IntTarget(self.builder.add_virtual_target()),
+                        typ: P2Type::Integer(bit_size),
+                    },
+                    _ => {
+                        return Err(Plonky2GenError::UnsupportedFeature {
+                            name: "parameters that are not unsigned integers".to_owned(),
+                        })
                     }
-                    _ => todo!(),
                 },
-                _ => todo!(),
+                _ => {
+                    return Err(Plonky2GenError::UnsupportedFeature {
+                        name: "parameters that are not numeric".to_owned(),
+                    })
+                }
             },
-            _ => todo!(),
+            _ => {
+                return Err(Plonky2GenError::ICE {
+                    message: "add_parameter passed a value that is nto Value::Param".to_owned(),
+                })
+            }
         };
 
-        let result = p2value.target;
+        let result = p2value.get_target();
         self.set(value_id, p2value);
         result
     }
 
     /// Converts from ssa::ir::instruction::BinaryOp to the equivalent P2Builder instruction, when
     /// such conversion is straightforward.
-    fn simple_convert(
+    fn convert_integer_op(
         &mut self,
         lhs: ValueId,
         rhs: ValueId,
         p2builder_op: fn(&mut P2Builder, Target, Target) -> Target,
-    ) -> P2Value {
-        let a = self.get_target(lhs);
-        let b = self.get_target(rhs);
-        let target = p2builder_op(&mut self.builder, a, b);
-        P2Value { target }
+    ) -> Result<P2Value, Plonky2GenError> {
+        let (bit_size_a, target_a) = self.get_integer(lhs)?;
+        let (bit_size_b, target_b) = self.get_integer(rhs)?;
+        assert!(bit_size_a == bit_size_b);
+
+        let target = p2builder_op(&mut self.builder, target_a, target_b);
+        Ok(P2Value { target: P2Target::IntTarget(target), typ: P2Type::Integer(bit_size_a) })
     }
 
     fn add_instruction(
@@ -107,19 +156,19 @@ impl Builder {
             Instruction::Binary(Binary { lhs, rhs, operator }) => {
                 let p2value = match operator {
                     super::ir::instruction::BinaryOp::Mul => {
-                        self.simple_convert(lhs, rhs, P2Builder::mul)
+                        self.convert_integer_op(lhs, rhs, P2Builder::mul)
                     }
 
                     super::ir::instruction::BinaryOp::Div => {
-                        self.simple_convert(lhs, rhs, P2Builder::div)
+                        self.convert_integer_op(lhs, rhs, P2Builder::div)
                     }
 
                     super::ir::instruction::BinaryOp::Add => {
-                        self.simple_convert(lhs, rhs, P2Builder::add)
+                        self.convert_integer_op(lhs, rhs, P2Builder::add)
                     }
 
                     super::ir::instruction::BinaryOp::Sub => {
-                        self.simple_convert(lhs, rhs, P2Builder::sub)
+                        self.convert_integer_op(lhs, rhs, P2Builder::sub)
                     }
 
                     _ => {
@@ -131,17 +180,17 @@ impl Builder {
                 let destinations: Vec<_> =
                     dfg.instruction_results(instruction_id).iter().cloned().collect();
                 assert!(destinations.len() == 1);
-                self.set(destinations[0], p2value);
+                self.set(destinations[0], p2value?);
             }
 
             Instruction::Constrain(lhs, rhs, _) => {
-                let a = self.get_target(lhs);
-                let b = self.get_target(rhs);
+                let a = self.get_target(lhs)?;
+                let b = self.get_target(rhs)?;
                 self.builder.connect(a, b);
             }
 
             Instruction::RangeCheck { value, max_bit_size, assert_message: _ } => {
-                let x = self.get_target(value);
+                let x = self.get_target(value)?;
                 self.builder.range_check(x, usize::try_from(max_bit_size).unwrap());
             }
 
@@ -165,8 +214,30 @@ impl Builder {
         self.translation.get(&value_id).unwrap()
     }
 
-    fn get_target(&mut self, value_id: ValueId) -> Target {
-        self.get(value_id).target
+    fn get_integer(&mut self, value_id: ValueId) -> Result<(u32, Target), Plonky2GenError> {
+        let value = self.get(value_id);
+        let bit_size = match value.typ {
+            P2Type::Integer(bit_size) => bit_size,
+            _ => {
+                return Err(Plonky2GenError::ICE {
+                    message: "lhs argument to convert_integer_op is not integer".to_owned(),
+                })
+            }
+        };
+        let target = match value.target {
+            P2Target::IntTarget(target) => target,
+            _ => {
+                return Err(Plonky2GenError::ICE {
+                    message: "lhs argument to convert_integer_op has non-integer target".to_owned(),
+                })
+            }
+        };
+        Ok((bit_size, target))
+    }
+
+    /// Get the PLONKY2 target of a value, regardless of whether its type is Integer or Boolean.
+    fn get_target(&mut self, value_id: ValueId) -> Result<Target, Plonky2GenError> {
+        self.get(value_id).get_target()
     }
 }
 
