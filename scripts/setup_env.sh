dd3a521b59b950871645306179d23a3f332ef6f3
ludamad <adam.domurad@gmail.com>
2024-04-17 10:13:15 -0400

    chore: workaround earthly flake (#5811)
    Unfortunate but rare issue I saw crop up

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -8,6 +8,6 @@ echo FORCE_COLOR=1 >> $GITHUB_ENV
 echo "Logging in to Docker..."
 echo $1 | docker login -u aztecprotocolci --password-stdin
 
-# Make earthly-cloud and earthly-cloud-bench scripts available
+# Make earthly-ci script available
 echo "PATH=$(dirname $(realpath $0)):$PATH" >> $GITHUB_ENV
 echo "EARTHLY_CONFIG=$(git rev-parse --show-toplevel)/.github/earthly-ci-config.yml" >> $GITHUB_ENV
