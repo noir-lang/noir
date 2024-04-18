2422891fa021cfb4c83b91849ff1f22baa93a4b9
Santiago Palladino <santiago@aztecprotocol.com>
2024-04-18 12:08:44 -0300

    chore: Validate instance deployer address every time we request it (#5848)
    Prevents deep errors in e2e tests where we fail to find a publicly
    deployed contract because the address of the deployer does not match.

--- yarn-project/protocol-contracts/src/instance-deployer/index.ts
+++ yarn-project/protocol-contracts/src/instance-deployer/index.ts
@@ -5,7 +5,13 @@ import { ContractInstanceDeployerArtifact } from './artifact.js';
 
 /** Returns the canonical deployment of the instance deployer contract. */
 export function getCanonicalInstanceDeployer(): ProtocolContract {
-  return getCanonicalProtocolContract(ContractInstanceDeployerArtifact, 1);
+  const contract = getCanonicalProtocolContract(ContractInstanceDeployerArtifact, 1);
+  if (!contract.address.equals(InstanceDeployerAddress)) {
+    throw new Error(
+      `Incorrect address for contract deployer (got ${contract.address.toString()} but expected (${InstanceDeployerAddress.toString()}). Check DEPLOYER_CONTRACT_ADDRESS is set to the correct value in the constants files and run the protocol-contracts package tests.`,
+    );
+  }
+  return contract;
 }
 
 export const InstanceDeployerAddress = AztecAddress.fromBigInt(DEPLOYER_CONTRACT_ADDRESS);
