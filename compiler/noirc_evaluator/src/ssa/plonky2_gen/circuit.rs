7c9b5617fff261cdb257c4a7cbed287cce64a48f
Stan Manilov <stanislav.manilov@gmail.com>
2024-04-19 18:11:43 +0300

    Add ArrayGet
    This required some extension of the type system at the PLONKY2 backend.
    The type system is not final, but it serves our purposes for the time
    being, while not being completely hacky.

--- compiler/noirc_evaluator/src/ssa/plonky2_gen/circuit.rs
+++ compiler/noirc_evaluator/src/ssa/plonky2_gen/circuit.rs
@@ -1,6 +1,8 @@
-use super::config::P2CircuitData;
+use super::config::{P2CircuitData, P2Field};
 use super::noir_to_plonky2_field;
-use noirc_abi::InputMap;
+use crate::errors::Plonky2GenError;
+use acvm::FieldElement;
+use noirc_abi::{input_parser::InputValue, InputMap};
 use plonky2::iop::{
     target::Target,
     witness::{PartialWitness, WitnessWrite},
@@ -41,22 +43,68 @@ impl<'de> Deserialize<'de> for Plonky2Circuit {
 }
 
 impl Plonky2Circuit {
-    pub fn prove(&self, inputs: &InputMap) -> Option<Vec<u8>> {
+    pub fn prove(&self, inputs: &InputMap) -> Result<Vec<u8>, Plonky2GenError> {
         let mut pw = PartialWitness::new();
-        for (target, param_name) in self.parameters.iter().zip(&self.parameter_names) {
+        let mut j = 0;
+        for param_name in self.parameter_names.iter() {
             let value = inputs[param_name].clone();
 
             match value {
-                noirc_abi::input_parser::InputValue::Field(field) => {
-                    let field = noir_to_plonky2_field(field);
-                    pw.set_target(*target, field)
+                InputValue::Field(field) => {
+                    self.set_parameter(&mut j, field, &mut pw);
                 }
-                _ => todo!(),
+
+                InputValue::Vec(input_values) => {
+                    let _ = self.set_array_parameter(&mut j, &input_values, &mut pw)?;
+                }
+                _ => {
+                    let feature_name = format!("parameter {:?}", value);
+                    return Err(Plonky2GenError::UnsupportedFeature { name: feature_name });
+                }
             }
         }
 
-        let proof = self.data.prove(pw).ok()?;
-        let proof_seiralized = serde_json::to_vec(&proof).ok()?;
-        Some(proof_seiralized)
+        let proof = match self.data.prove(pw) {
+            Ok(proof) => proof,
+            Err(error) => {
+                let error_message = format!("Unexpected proof failure: {:?}", error);
+                return Err(Plonky2GenError::ICE { message: error_message });
+            }
+        };
+        let proof_serialized = match serde_json::to_vec(&proof) {
+            Ok(bytes) => bytes,
+            Err(error) => {
+                let error_message = format!("Unexpected serialization error: {:?}", error);
+                return Err(Plonky2GenError::ICE { message: error_message });
+            }
+        };
+        Ok(proof_serialized)
+    }
+
+    fn set_parameter(&self, j: &mut usize, field: FieldElement, pw: &mut PartialWitness<P2Field>) {
+        let target = self.parameters[*j];
+        *j += 1;
+        let field = noir_to_plonky2_field(field);
+        pw.set_target(target, field)
+    }
+
+    fn set_array_parameter(
+        &self,
+        j: &mut usize,
+        input_values: &Vec<noirc_abi::input_parser::InputValue>,
+        pw: &mut PartialWitness<P2Field>,
+    ) -> Result<(), Plonky2GenError> {
+        for input_value in input_values.iter() {
+            match input_value {
+                InputValue::Field(field) => {
+                    self.set_parameter(j, *field, pw);
+                }
+                _ => {
+                    let feature_name = format!("array parameter with non-field elements");
+                    return Err(Plonky2GenError::UnsupportedFeature { name: feature_name });
+                }
+            }
+        }
+        Ok(())
     }
 }
--- tooling/nargo_cli/src/cli/prove_cmd.rs
+++ tooling/nargo_cli/src/cli/prove_cmd.rs
@@ -141,7 +141,13 @@ pub(crate) fn prove_package(
             "prove command has been deprecated without the --use_plonky2_backend_experimental flag";
         return Err(CliError::InvalidProof(message.into()));
     } else {
-        compiled_program.plonky2_circuit.unwrap().prove(&inputs_map).unwrap()
+        match compiled_program.plonky2_circuit.unwrap().prove(&inputs_map) {
+            Ok(proof) => proof,
+            Err(error) => {
+                let error_message = format!("{:?}", error);
+                return Err(CliError::BackendError(BackendError::UnfitBackend(error_message)));
+            }
+        }
     };
 
     if check_proof {
