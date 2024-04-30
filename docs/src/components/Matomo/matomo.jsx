fae9b67ed24f689c27df691426c2215bb7982bbb
José Pedro Sousa <outgoing@zpedro.dev>
2024-04-30 16:12:39 +0100

    adding consent banner and logic

--- docs/src/components/Matomo/matomo.jsx
+++ docs/src/components/Matomo/matomo.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
-function getSiteId(siteConfig) {
-  const env = siteConfig.customFields.MATOMO_ENV;
+function getSiteId(env) {
   if (env == 'dev') {
     return '3';
   } else if (env == 'staging') {
@@ -11,62 +10,109 @@ function getSiteId(siteConfig) {
     return '1';
   }
 }
+function pushInstruction(name, ...args) {
+  return window._paq.push([name, ...args]);
+}
 
 export default function useMatomo() {
   const { siteConfig } = useDocusaurusContext();
-  const [isOptedOut, setIsOptedOut] = useState(false);
+  const [showBanner, setShowBanner] = useState(false);
+  const env = siteConfig.customFields.MATOMO_ENV;
+  const urlBase = 'https://noirlang.matomo.cloud/';
+  const trackerUrl = `${urlBase}matomo.php`;
+  const srcUrl = `${urlBase}matomo.js`;
+
+  window._paq = window._paq || [];
 
   useEffect(() => {
-    window._paq = window._paq || [];
-    console.log('effect');
-    console.log(window);
+    pushInstruction(function () {
+      setShowBanner(!this.hasRememberedConsent());
+    });
 
-    var u = 'https://noirlang.matomo.cloud/';
-    const siteId = getSiteId(siteConfig);
-    // const secureCookie = siteId === '3' ? false : true;
-    // console.log('secureCookie', secureCookie);
-    // window._paq.push(['setSecureCookie', secureCookie]);
-    window._paq.push(['setTrackerUrl', u + 'matomo.php']);
-    window._paq.push(['setSiteId', siteId]);
+    // pushInstruction('requireConsent', true);
+    pushInstruction('setTrackerUrl', trackerUrl);
+    pushInstruction('setSiteId', getSiteId(env));
+    if (env !== 'prod') {
+      pushInstruction('setSecureCookie', false);
+    }
 
-    var d = document,
-      g = d.createElement('script'),
-      s = d.getElementsByTagName('script')[0];
-    g.async = true;
-    g.src = 'https://cdn.matomo.cloud/noirlang.matomo.cloud/matomo.js';
-    s.parentNode.insertBefore(g, s);
+    const doc = document;
+    const scriptElement = doc.createElement('script');
+    const scripts = doc.getElementsByTagName('script')[0];
+
+    scriptElement.type = 'text/javascript';
+    scriptElement.async = true;
+    scriptElement.defer = true;
+    scriptElement.src = srcUrl;
+
+    if (scripts && scripts.parentNode) {
+      scripts.parentNode.insertBefore(scriptElement, scripts);
+    }
   }, []);
 
   useEffect(() => {
-    window._paq = window._paq || [];
-    window._paq.push(['trackPageView']);
+    pushInstruction('trackPageView');
   }, [window.location.href]);
 
-  useEffect(() => {
-    window._paq = window._paq || [];
-    function setOptOutText(element) {
-      console.log(element.checked);
-      window._paq.push([
-        function () {
-          element.checked = !this.isUserOptedOut();
-          document.querySelector('label[for=optout] strong').innerText = this.isUserOptedOut()
-            ? 'You are currently opted out. Click here to opt in.'
-            : 'You are currently opted in. Click here to opt out.';
-        },
-      ]);
-    }
+  const optIn = () => {
+    pushInstruction('rememberConsentGiven');
+    setShowBanner(false);
+  };
 
-    var optOut = document.getElementById('optout');
-    optOut.addEventListener('click', function () {
-      if (this.checked) {
-        window._paq.push(['forgetUserOptOut']);
-      } else {
-        window._paq.push(['optUserOut']);
-      }
-      setOptOutText(optOut);
+  const optOut = () => {
+    pushInstruction('forgetConsentGiven');
+    setShowBanner(false);
+  };
+
+  const debug = () => {
+    pushInstruction(function () {
+      console.log(this.getRememberedConsent());
     });
-    setOptOutText(optOut);
-  }, [isOptedOut]);
+  };
 
-  return <></>;
+  const reset = () => {
+    pushInstruction('forgetConsentGiven');
+    setShowBanner(true);
+  };
+
+  if (!showBanner) {
+    return (
+      <div id="optout-form">
+        <div className="homepage_footer">
+          <p>Debugging analytics</p>
+          <div className="homepage_cta_footer_container">
+            <button className="cta-button button button--secondary button--sm" onClick={debug}>
+              Debug
+            </button>
+            <button className="cta-button button button--secondary button--sm" onClick={reset}>
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div id="optout-form">
+      <div className="homepage_footer">
+        <p>
+          We value your privacy and don't track your browsing by default. If you'd like to help us improve, you can
+          allow cookies for tracking page views, time on site, and other analytics. Find out how we use cookies and how
+          you can change your settings.
+        </p>
+        <div className="homepage_cta_footer_container">
+          <button className="cta-button button button--primary button--sm" onClick={optIn}>
+            I accept cookies
+          </button>
+          <button className="cta-button button button--secondary button--sm" onClick={optOut}>
+            I refuse cookies
+          </button>
+          <button className="cta-button button button--secondary button--sm" onClick={debug}>
+            Debug
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- docs/src/theme/Root.js
+++ docs/src/theme/Root.js
@@ -2,27 +2,11 @@ import React from 'react';
 import useMatomo from '@site/src/components/Matomo/matomo';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import useIsBrowser from '@docusaurus/useIsBrowser';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 function OptOutForm() {
-  useMatomo();
+  const banner = useMatomo();
 
-  return (
-    <div id="optout-form">
-      <p>
-        You may choose not to have a unique web analytics cookie identification number assigned to your computer to
-        avoid the aggregation and analysis of data collected on this website.
-      </p>
-      <p>To make that choice, please click below to receive an opt-out cookie.</p>
-
-      <p>
-        <input type="checkbox" id="optout" />
-        <label htmlFor="optout">
-          <strong></strong>
-        </label>
-      </p>
-    </div>
-  );
+  return <>{banner}</>;
 }
 
 export default function Root({ children }) {
@@ -31,8 +15,8 @@ export default function Root({ children }) {
 
   return (
     <>
-      <BrowserOnly>{() => <OptOutForm />}</BrowserOnly>
       {children}
+      <BrowserOnly>{() => <OptOutForm />}</BrowserOnly>
     </>
   );
 }
