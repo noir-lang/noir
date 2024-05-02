6cc924dc44a36d9ef2aeda05ea69a120898fc272
Charlie Lye <karl.lye@gmail.com>
2024-05-02 13:15:16 +0100

    feat: osxcross (#6099)
    At present we only learn if mac builds fail when we run the bb-publish
    workflow. Ultimately it would be nice to build bb for mac as part of the
    rest of our build workflow. We can do this with cross-compilation from
    linux to mac.
    
    Developers should read and be aware of [Apples
    License](https://www.apple.com/legal/sla/docs/xcode.pdf) terms before
    using this functionality.
    
    - Added support for Apple arm64 and amd64 cross-compilation to
    CmakePresets. `darwin-arm64` and `darwin-amd64`.
    - Updated `serialize.hpp` so we can still build on Apple. (it's a
    kludge, we shouldn't be serializing `size_t`).
    - Updated `build-image` to include osxcross.
    - `osxcross` and `foundry` are both published so they shouldn't need to
    be rebuilt. However this does mean if they need updating we have a
    manual process to follow e.g. `make foundry` on both an x86 and arm
    machine.
    - Add new target to bb Earthfile that can cross-compile `bb`.

--- barretenberg/cpp/src/barretenberg/common/serialize.hpp
+++ barretenberg/cpp/src/barretenberg/common/serialize.hpp
@@ -121,6 +121,20 @@ inline void write(uint8_t*& it, uint64_t value)
     it += 8;
 }
 
+#ifdef __APPLE__
+inline void read(uint8_t const*& it, unsigned long& value)
+{
+    value = ntohll(*reinterpret_cast<unsigned long const*>(it));
+    it += 8;
+}
+
+inline void write(uint8_t*& it, unsigned long value)
+{
+    *reinterpret_cast<unsigned long*>(it) = htonll(value);
+    it += 8;
+}
+#endif
+
 #ifndef __i386__
 inline void read(uint8_t const*& it, uint128_t& value)
 {
