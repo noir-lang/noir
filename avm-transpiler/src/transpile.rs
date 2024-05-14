16ab1f70752a4b167667a338d8c3e215a6554002
Facundo <fcarreiro@users.noreply.github.com>
2024-05-14 19:46:30 +0100

    fix(avm-transpiler): fix cast to u1 (#6402)
    Noir has the concept of `u1` but in the AVM we interpret it as `u8`.
    However, Brillig uses `CAST` to truncate as well. In particular `a % 2`
    compiles to a `CAST<u1>` (kudos to Noir there). We were transpiling that
    to a `CAST<u8>`, via `tag_for_bit_size`.
    
    This PR fixes the problem by doing a proper truncation to 1 bit when
    casting to `u1`. This is done using `toradix`. Thanks @IlyasRidhuan for
    the scratch-space-free solution.
    
    Note: this is inefficient and we should consider introducing `u1` or
    suggesting removal from Brillig/Noir. This might not be the last time we
    are bit by this.

--- avm-transpiler/src/transpile.rs
+++ avm-transpiler/src/transpile.rs
@@ -275,11 +275,7 @@ pub fn brillig_to_avm(
                 source,
                 bit_size,
             } => {
-                avm_instrs.push(generate_cast_instruction(
-                    source.to_usize() as u32,
-                    destination.to_usize() as u32,
-                    tag_from_bit_size(*bit_size),
-                ));
+                handle_cast(&mut avm_instrs, source, destination, *bit_size);
             }
             BrilligOpcode::ForeignCall {
                 function,
@@ -460,6 +456,45 @@ fn handle_external_call(
         ],
         ..Default::default()
     });
+}
+
+fn handle_cast(
+    avm_instrs: &mut Vec<AvmInstruction>,
+    source: &MemoryAddress,
+    destination: &MemoryAddress,
+    bit_size: u32,
+) {
+    let source_offset = source.to_usize() as u32;
+    let dest_offset = destination.to_usize() as u32;
+
+    if bit_size == 1 {
+        assert!(
+            matches!(tag_from_bit_size(bit_size), AvmTypeTag::UINT8),
+            "If u1 doesn't map to u8 anymore, change this code!"
+        );
+        avm_instrs.extend([
+            // We cast to Field to be able to use toradix.
+            generate_cast_instruction(source_offset, dest_offset, AvmTypeTag::FIELD),
+            // Toradix with radix 2 and 1 limb is the same as modulo 2.
+            // We need to insert an instruction explicitly because we want to fine-tune 'indirect'.
+            AvmInstruction {
+                opcode: AvmOpcode::TORADIXLE,
+                indirect: Some(ALL_DIRECT),
+                tag: None,
+                operands: vec![
+                    AvmOperand::U32 { value: dest_offset },
+                    AvmOperand::U32 { value: dest_offset },
+                    AvmOperand::U32 { value: /*radix=*/ 2},
+                    AvmOperand::U32 { value: /*limbs=*/ 1},
+                ],
+            },
+            // Then we cast back to u8 (which is what we use for u1).
+            generate_cast_instruction(dest_offset, dest_offset, AvmTypeTag::UINT8),
+        ]);
+    } else {
+        let tag = tag_from_bit_size(bit_size);
+        avm_instrs.push(generate_cast_instruction(source_offset, dest_offset, tag));
+    }
 }
 
 /// Handle an AVM NOTEHASHEXISTS instruction
@@ -980,9 +1015,9 @@ fn handle_black_box_function(avm_instrs: &mut Vec<AvmInstruction>, operation: &B
             radix,
             output,
         } => {
-            let num_limbs = output.size;
-            let input_offset = input.0;
-            let output_offset = output.pointer.0;
+            let num_limbs = output.size as u32;
+            let input_offset = input.0 as u32;
+            let output_offset = output.pointer.0 as u32;
             assert!(radix <= &256u32, "Radix must be less than or equal to 256");
 
             avm_instrs.push(AvmInstruction {
@@ -991,17 +1026,15 @@ fn handle_black_box_function(avm_instrs: &mut Vec<AvmInstruction>, operation: &B
                 tag: None,
                 operands: vec![
                     AvmOperand::U32 {
-                        value: input_offset as u32,
+                        value: input_offset,
                     },
                     AvmOperand::U32 {
-                        value: output_offset as u32,
+                        value: output_offset,
                     },
                     AvmOperand::U32 { value: *radix },
-                    AvmOperand::U32 {
-                        value: num_limbs as u32,
-                    },
+                    AvmOperand::U32 { value: num_limbs },
                 ],
-            })
+            });
         }
         _ => panic!("Transpiler doesn't know how to process {:?}", operation),
     }
@@ -1224,6 +1257,7 @@ pub fn map_brillig_pcs_to_avm_pcs(brillig_bytecode: &[BrilligOpcode]) -> Vec<usi
     for i in 0..brillig_bytecode.len() - 1 {
         let num_avm_instrs_for_this_brillig_instr = match &brillig_bytecode[i] {
             BrilligOpcode::Const { bit_size: 254, .. } => 2,
+            BrilligOpcode::Cast { bit_size: 1, .. } => 3,
             _ => 1,
         };
         // next Brillig pc will map to an AVM pc offset by the
--- yarn-project/simulator/src/avm/avm_simulator.test.ts
+++ yarn-project/simulator/src/avm/avm_simulator.test.ts
@@ -80,6 +80,17 @@ describe('AVM simulator: transpiled Noir contracts', () => {
     expect(results.output).toEqual([new Fr(3)]);
   });
 
+  it('modulo and u1', async () => {
+    const calldata: Fr[] = [new Fr(2)];
+    const context = initContext({ env: initExecutionEnvironment({ calldata }) });
+
+    const bytecode = getAvmTestContractBytecode('modulo2');
+    const results = await new AvmSimulator(context).executeBytecode(bytecode);
+
+    expect(results.reverted).toBe(false);
+    expect(results.output).toEqual([new Fr(0)]);
+  });
+
   it('Should be recognized as AVM bytecode (magic present)', () => {
     const bytecode = getAvmTestContractBytecode('add_args_return');
     expect(isAvmBytecode(bytecode));
