1fa59637a0829208d382d1dded36df33f4d61582
Tom French <15848336+TomAFrench@users.noreply.github.com>
2024-05-17 00:33:52 +0100

    chore: switch over to constructing gates report in bash (#6491)
    This PR switches us over to using `gates_report.sh` rather than `nargo
    info`

--- noir-projects/gates_report.sh
+++ noir-projects/gates_report.sh
@@ -5,27 +5,23 @@ set -eu
 # The script needs some slight updating as `nargo info` expects a complete JSON object, while this script expects a single object field
 # representing a list of circuit reports for a program. 
 # The ACIR tests in barretenberg also expect every target bytecode to have the name `acir.gz` while this script expects the same name of the package
-echo "Compile noir-protocol-circuits for gates report..."
 cd noir-protocol-circuits
 PROTOCOL_CIRCUITS_DIR=$PWD
-
-# Compile programs into artifacts that the backend expects
-NARGO=${NARGO:-../../noir/noir-repo/target/release/nargo}
-$NARGO compile --only-acir
 
 BB_BIN=${BB_BIN:-../../barretenberg/cpp/build/bin/bb}
 
 echo "{\"programs\": [" > gates_report.json
 
 # Bound for checking where to place last parentheses 
-NUM_ARTIFACTS=$(ls -1q "$PROTOCOL_CIRCUITS_DIR/target"/*.gz | wc -l)
+NUM_ARTIFACTS=$(ls -1q "$PROTOCOL_CIRCUITS_DIR/target"/*.json | wc -l)
 
 ITER="1"
-for pathname in "$PROTOCOL_CIRCUITS_DIR/target"/*.gz; do    
-    ARTIFACT_NAME=$(basename -s .gz "$pathname")
+for pathname in "$PROTOCOL_CIRCUITS_DIR/target"/*.json; do    
+    ARTIFACT_NAME=$(basename -s .json "$pathname")
 
-    echo "{\"package_name\": \"$ARTIFACT_NAME\"," >> gates_report.json
-    $BB_BIN gates -b "./target/$ARTIFACT_NAME.gz" >> gates_report.json
+    GATES_INFO=$($BB_BIN gates -b "./target/$ARTIFACT_NAME.json")
+    MAIN_FUNCTION_INFO=$(echo $GATES_INFO | jq -r '.functions[0] | .name = "main"')
+    echo "{\"package_name\": \"$ARTIFACT_NAME\", \"functions\": [$MAIN_FUNCTION_INFO]" >> gates_report.json
 
     if (($ITER == $NUM_ARTIFACTS)); then
         echo "}" >> gates_report.json
