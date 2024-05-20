2a5d975a09dcdd5adfcb8fc14af711ad3c40c022
just-mitch <68168980+just-mitch@users.noreply.github.com>
2024-05-20 12:25:32 -0400

    chore: anvil kill wrapper now supports mac (#6520)
    Also remove errant workspace settings. Should we remove it from source
    control?

--- yarn-project/end-to-end/scripts/anvil_kill_wrapper.sh
+++ yarn-project/end-to-end/scripts/anvil_kill_wrapper.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 
-# Find the parent of this script.
-PARENT_PID=$(awk '{print $4}' /proc/$$/stat)
+# Function to get the PPID in macOS
+get_ppid_macos() {
+  ps -j $$ | awk 'NR==2 {print $3}'
+}
+
+# Function to get the PPID in Linux
+get_ppid_linux() {
+  awk '{print $4}' /proc/$$/stat
+}
+
+# Function to check if a process is alive in macOS
+is_process_alive_macos() {
+  ps -p $1 > /dev/null 2>&1
+}
+
+# Function to check if a process is alive in Linux
+is_process_alive_linux() {
+  [ -d /proc/$1 ]
+}
+
+
+# Determine the operating system and call the appropriate function
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  PARENT_PID=$(get_ppid_macos)
+  check_process_alive() { is_process_alive_macos $1; }
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  PARENT_PID=$(get_ppid_linux)
+  check_process_alive() { is_process_alive_linux $1; }
+else
+  echo "Unsupported OS"
+  exit 1
+fi
+
+# echo "Parent PID: $PARENT_PID"
 
 # Start anvil in the background.
 anvil $@ &
@@ -14,6 +46,6 @@ cleanup() {
 trap cleanup EXIT
 
 # Continuously check if the parent process is still alive.
-while [ -d /proc/$PARENT_PID ]; do
-    sleep 1
+while check_process_alive $PARENT_PID; do
+  sleep 1
 done
