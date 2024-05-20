21dc72aaf29ada2c1a12682d3763370c76eff524
Santiago Palladino <santiago@aztecprotocol.com>
2024-05-20 18:31:27 -0300

    chore: Copy subset of constants to cpp (#6544)
    Copying all aztec constants to cpp-land caused unnecessary rebuilds of
    barretenberg, since only a handful of constants are actually used. This
    adds a list of what constants are actually used in cpp that are used as
    a filter.

--- yarn-project/circuits.js/src/scripts/constants.in.ts
+++ yarn-project/circuits.js/src/scripts/constants.in.ts
@@ -7,6 +7,22 @@ const TS_CONSTANTS_FILE = '../constants.gen.ts';
 const CPP_AZTEC_CONSTANTS_FILE = '../../../../barretenberg/cpp/src/barretenberg/vm/avm_trace/aztec_constants.hpp';
 const SOLIDITY_CONSTANTS_FILE = '../../../../l1-contracts/src/core/libraries/ConstantsGen.sol';
 
+// Whitelist of constants that will be copied to aztec_constants.hpp.
+// We don't copy everything as just a handful are needed, and updating them breaks the cache and triggers expensive bb builds.
+const CPP_CONSTANTS = [
+  'TOTAL_FEES_LENGTH',
+  'GAS_FEES_LENGTH',
+  'GAS_LENGTH',
+  'CONTENT_COMMITMENT_LENGTH',
+  'GLOBAL_VARIABLES_LENGTH',
+  'APPEND_ONLY_TREE_SNAPSHOT_LENGTH',
+  'PARTIAL_STATE_REFERENCE_LENGTH',
+  'STATE_REFERENCE_LENGTH',
+  'HEADER_LENGTH',
+  'CALL_CONTEXT_LENGTH',
+  'PUBLIC_CONTEXT_INPUTS_LENGTH',
+];
+
 /**
  * Parsed content.
  */
@@ -46,7 +62,7 @@ function processConstantsCpp(constants: { [key: string]: string }): string {
   const code: string[] = [];
   Object.entries(constants).forEach(([key, value]) => {
     // We exclude large numbers
-    if (!(value.startsWith('0x') || value.includes('0_0'))) {
+    if (CPP_CONSTANTS.includes(key) && !(value.startsWith('0x') || value.includes('0_0'))) {
       code.push(`const size_t ${key} = ${value};`);
     }
   });
