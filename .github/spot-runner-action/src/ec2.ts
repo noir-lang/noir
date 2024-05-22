f45eb94248ce0ef38344d1fc7e55998fdf31723c
ludamad <adam.domurad@gmail.com>
2024-05-22 10:34:22 -0400

    chore(ci): fix running out of disk (#6597)

--- .github/spot-runner-action/src/ec2.ts
+++ .github/spot-runner-action/src/ec2.ts
@@ -198,7 +198,7 @@ export class Ec2Instance {
           {
             DeviceName: "/dev/sda1",
             Ebs: {
-              VolumeSize: 32,
+              VolumeSize: 64,
               VolumeType: 'gp3',
               Throughput: 1000,
               Iops: 5000
