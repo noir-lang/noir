5ed3f4970b6f668e399b2947d893475f3df3d51d
Álvaro Rodríguez <sirasistant@gmail.com>
2024-05-24 13:33:24 +0200

    Update compiler/noirc_evaluator/src/ssa/opt/resolve_within_unconstrained.rs
    Co-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>

--- compiler/noirc_evaluator/src/ssa/opt/resolve_within_unconstrained.rs
+++ compiler/noirc_evaluator/src/ssa/opt/resolve_within_unconstrained.rs
@@ -12,7 +12,7 @@ use fxhash::FxHashSet as HashSet;
 
 impl Ssa {
     /// An SSA pass to find any calls to `Intrinsic::WithinUnconstrained` and replacing any uses of the result of the intrinsic
-    /// with the resoled boolean value.
+    /// with the resolved boolean value.
     /// Note that this pass must run after the pass that does runtime separation, since in SSA generation an ACIR function can end up targeting brillig.
     #[tracing::instrument(level = "trace", skip(self))]
     pub(crate) fn resolve_within_unconstrained(mut self) -> Self {
