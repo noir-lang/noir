58a7d1089ab424c3441329a7a723a77548fa6027
Jake Fecher <jake@aztecprotocol.com>
2024-06-04 13:55:21 +0100

    Switch over frontend tests as well

--- compiler/noirc_frontend/src/tests.rs
+++ compiler/noirc_frontend/src/tests.rs
@@ -81,7 +81,7 @@ pub(crate) fn get_program(src: &str) -> (ParsedModule, Context, Vec<(Compilation
             &mut context,
             program.clone().into_sorted(),
             root_file_id,
-            false,
+            true,
             &[], // No macro processors
         ));
     }
