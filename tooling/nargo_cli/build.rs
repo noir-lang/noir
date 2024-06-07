7ebde567a92aa707ee069b77b0a49c710c7a6006
Nikolay Nikolov <nickysn@gmail.com>
2024-06-07 21:36:52 +0300

    feat(plonky2): Added plonky2_verify_success tests

--- tooling/nargo_cli/build.rs
+++ tooling/nargo_cli/build.rs
@@ -42,6 +42,7 @@ fn main() {
     generate_plonky2_prove_failure_tests(&mut test_file, &test_dir);
     // generate_plonky2_prove_unsupported_tests(&mut test_file, &test_dir);
     // generate_plonky2_prove_crash_tests(&mut test_file, &test_dir);
+    generate_plonky2_verify_success_tests(&mut test_file, &test_dir);
 }
 
 /// Some tests are explicitly ignored in brillig due to them failing.
@@ -689,6 +690,50 @@ fn plonky2_prove_crash_{test_name}() {{
     cmd.arg("prove");
 
     cmd.assert().failure().stderr(predicate::str::contains("The application panicked (crashed)."));
+}}
+            "#,
+            test_dir = test_dir.display(),
+        )
+        .expect("Could not write templated test file.");
+    }
+}
+
+/// Tests using the experimental PLONKY2 backend as a proving engine that are expected to succeed.
+fn generate_plonky2_verify_success_tests(test_file: &mut File, test_data_dir: &Path) {
+    let test_sub_dir = "plonky2_verify_success";
+    let test_data_dir = test_data_dir.join(test_sub_dir);
+
+    let test_case_dirs =
+        fs::read_dir(test_data_dir).unwrap().flatten().filter(|c| c.path().is_dir());
+
+    for test_dir in test_case_dirs {
+        let test_name =
+            test_dir.file_name().into_string().expect("Directory can't be converted to string");
+        if test_name.contains('-') {
+            panic!(
+                "Invalid test directory: {test_name}. Cannot include `-`, please convert to `_`"
+            );
+        };
+        let test_dir = &test_dir.path();
+
+        write!(
+            test_file,
+            r#"
+#[test]
+fn plonky2_verify_success_{test_name}() {{
+    let test_program_dir = PathBuf::from("{test_dir}");
+
+    let mut cmd = Command::cargo_bin("nargo").unwrap();
+    cmd.arg("--program-dir").arg(test_program_dir.clone());
+    cmd.arg("prove");
+
+    cmd.assert().success();
+
+    let mut cmd2 = Command::cargo_bin("nargo").unwrap();
+    cmd2.arg("--program-dir").arg(test_program_dir);
+    cmd2.arg("verify");
+
+    cmd2.assert().success();
 }}
             "#,
             test_dir = test_dir.display(),
