2de85dd27bb2bd4c0b911fd6ff61a8c1db56f8e8
Jake Fecher <jfecher11@gmail.com>
2024-06-21 11:23:50 -0500

    Fix InterpreterError formatting

--- compiler/noirc_frontend/src/hir/comptime/errors.rs
+++ compiler/noirc_frontend/src/hir/comptime/errors.rs
@@ -4,6 +4,7 @@ use crate::{
     hir::def_collector::dc_crate::CompilationError, parser::ParserError, token::Tokens, Type,
 };
 use acvm::{acir::AcirField, FieldElement};
+use fm::FileId;
 use iter_extended::vecmap;
 use noirc_errors::{CustomDiagnostic, Location};
 
@@ -12,144 +13,41 @@ use super::value::Value;
 /// The possible errors that can halt the interpreter.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum InterpreterError {
-    ArgumentCountMismatch {
-        expected: usize,
-        actual: usize,
-        location: Location,
-    },
-    TypeMismatch {
-        expected: Type,
-        value: Value,
-        location: Location,
-    },
-    NonComptimeVarReferenced {
-        name: String,
-        location: Location,
-    },
-    IntegerOutOfRangeForType {
-        value: FieldElement,
-        typ: Type,
-        location: Location,
-    },
-    ErrorNodeEncountered {
-        location: Location,
-    },
-    NonFunctionCalled {
-        value: Value,
-        location: Location,
-    },
-    NonBoolUsedInIf {
-        value: Value,
-        location: Location,
-    },
-    NonBoolUsedInConstrain {
-        value: Value,
-        location: Location,
-    },
-    FailingConstraint {
-        message: Option<Value>,
-        location: Location,
-    },
-    NoMethodFound {
-        name: String,
-        typ: Type,
-        location: Location,
-    },
-    NonIntegerUsedInLoop {
-        value: Value,
-        location: Location,
-    },
-    NonPointerDereferenced {
-        value: Value,
-        location: Location,
-    },
-    NonTupleOrStructInMemberAccess {
-        value: Value,
-        location: Location,
-    },
-    NonArrayIndexed {
-        value: Value,
-        location: Location,
-    },
-    NonIntegerUsedAsIndex {
-        value: Value,
-        location: Location,
-    },
-    NonIntegerIntegerLiteral {
-        typ: Type,
-        location: Location,
-    },
-    NonIntegerArrayLength {
-        typ: Type,
-        location: Location,
-    },
-    NonNumericCasted {
-        value: Value,
-        location: Location,
-    },
-    IndexOutOfBounds {
-        index: usize,
-        length: usize,
-        location: Location,
-    },
-    ExpectedStructToHaveField {
-        value: Value,
-        field_name: String,
-        location: Location,
-    },
-    TypeUnsupported {
-        typ: Type,
-        location: Location,
-    },
-    InvalidValueForUnary {
-        value: Value,
-        operator: &'static str,
-        location: Location,
-    },
-    InvalidValuesForBinary {
-        lhs: Value,
-        rhs: Value,
-        operator: &'static str,
-        location: Location,
-    },
-    CastToNonNumericType {
-        typ: Type,
-        location: Location,
-    },
-    QuoteInRuntimeCode {
-        location: Location,
-    },
-    NonStructInConstructor {
-        typ: Type,
-        location: Location,
-    },
-    CannotInlineMacro {
-        value: Value,
-        location: Location,
-    },
-    UnquoteFoundDuringEvaluation {
-        location: Location,
-    },
-    FailedToParseMacro {
-        error: ParserError,
-        tokens: Rc<Tokens>,
-        parse_rule: &'static str,
-        file: fm::FileId,
-    },
-
-    Unimplemented {
-        item: String,
-        location: Location,
-    },
+    ArgumentCountMismatch { expected: usize, actual: usize, location: Location },
+    TypeMismatch { expected: Type, value: Value, location: Location },
+    NonComptimeVarReferenced { name: String, location: Location },
+    IntegerOutOfRangeForType { value: FieldElement, typ: Type, location: Location },
+    ErrorNodeEncountered { location: Location },
+    NonFunctionCalled { value: Value, location: Location },
+    NonBoolUsedInIf { value: Value, location: Location },
+    NonBoolUsedInConstrain { value: Value, location: Location },
+    FailingConstraint { message: Option<Value>, location: Location },
+    NoMethodFound { name: String, typ: Type, location: Location },
+    NonIntegerUsedInLoop { value: Value, location: Location },
+    NonPointerDereferenced { value: Value, location: Location },
+    NonTupleOrStructInMemberAccess { value: Value, location: Location },
+    NonArrayIndexed { value: Value, location: Location },
+    NonIntegerUsedAsIndex { value: Value, location: Location },
+    NonIntegerIntegerLiteral { typ: Type, location: Location },
+    NonIntegerArrayLength { typ: Type, location: Location },
+    NonNumericCasted { value: Value, location: Location },
+    IndexOutOfBounds { index: usize, length: usize, location: Location },
+    ExpectedStructToHaveField { value: Value, field_name: String, location: Location },
+    TypeUnsupported { typ: Type, location: Location },
+    InvalidValueForUnary { value: Value, operator: &'static str, location: Location },
+    InvalidValuesForBinary { lhs: Value, rhs: Value, operator: &'static str, location: Location },
+    CastToNonNumericType { typ: Type, location: Location },
+    QuoteInRuntimeCode { location: Location },
+    NonStructInConstructor { typ: Type, location: Location },
+    CannotInlineMacro { value: Value, location: Location },
+    UnquoteFoundDuringEvaluation { location: Location },
+    FailedToParseMacro { error: ParserError, tokens: Rc<Tokens>, parse_rule: String, file: FileId },
+    Unimplemented { item: String, location: Location },
 
     // Perhaps this should be unreachable! due to type checking also preventing this error?
     // Currently it and the Continue variant are the only interpreter errors without a Location field
-    BreakNotInLoop {
-        location: Location,
-    },
-    ContinueNotInLoop {
-        location: Location,
-    },
+    BreakNotInLoop { location: Location },
+    ContinueNotInLoop { location: Location },
 
     // These cases are not errors, they are just used to prevent us from running more code
     // until the loop can be resumed properly. These cases will never be displayed to users.
