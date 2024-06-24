f2f8ecc833d4725d0829f9c339389c90d1a4fbcd
guipublic <47281315+guipublic@users.noreply.github.com>
2024-06-24 20:37:12 +0200

    chore: add back Pedersen blackbox functions (revert PR 5221) (#5318)
    # Description
    
    ## Problem\*
    
    This PR adds (back) the blackbox functions for Pedersen hash/commitment,
    until we get a similar circuit (in term of number of gates) with the
    Noir implementation.
    
    ## Summary\*
    The Noir version is kept and assert to ouput the same value in a test.
    
    
    ## Additional Context
    This PR reverts the PR #5221
    
    
    ## Documentation\*
    
    Check one:
    - [X] No documentation needed.
    - [ ] Documentation included in this PR.
    - [ ] **[For Experimental Features]** Documentation to be submitted in a
    separate PR.
    
    # PR Checklist\*
    
    - [X] I have tested the changes locally.
    - [X] I have formatted the changes with [Prettier](https://prettier.io/)
    and/or `cargo fmt` on default settings.
    
    ---------
    
    Co-authored-by: Tom French <tom@tomfren.ch>
    Co-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>

--- compiler/noirc_evaluator/src/ssa/acir_gen/acir_ir/generated_acir.rs
+++ compiler/noirc_evaluator/src/ssa/acir_gen/acir_ir/generated_acir.rs
@@ -224,6 +224,16 @@ impl<F: AcirField> GeneratedAcir<F> {
                     output: outputs[0],
                 }
             }
+            BlackBoxFunc::PedersenCommitment => BlackBoxFuncCall::PedersenCommitment {
+                inputs: inputs[0].clone(),
+                outputs: (outputs[0], outputs[1]),
+                domain_separator: constant_inputs[0].to_u128() as u32,
+            },
+            BlackBoxFunc::PedersenHash => BlackBoxFuncCall::PedersenHash {
+                inputs: inputs[0].clone(),
+                output: outputs[0],
+                domain_separator: constant_inputs[0].to_u128() as u32,
+            },
             BlackBoxFunc::EcdsaSecp256k1 => {
                 BlackBoxFuncCall::EcdsaSecp256k1 {
                     // 32 bytes for each public key co-ordinate
@@ -361,8 +371,6 @@ impl<F: AcirField> GeneratedAcir<F> {
                     .expect("Compiler should generate correct size inputs"),
                 outputs: outputs.try_into().expect("Compiler should generate correct size outputs"),
             },
-            BlackBoxFunc::PedersenCommitment => todo!("Deprecated Blackbox"),
-            BlackBoxFunc::PedersenHash => todo!("Deprecated Blackbox"),
         };
 
         self.push_opcode(AcirOpcode::BlackBoxFuncCall(black_box_func_call));
@@ -641,7 +649,9 @@ fn black_box_func_expected_input_size(name: BlackBoxFunc) -> Option<usize> {
         | BlackBoxFunc::Keccak256
         | BlackBoxFunc::SHA256
         | BlackBoxFunc::Blake2s
-        | BlackBoxFunc::Blake3 => None,
+        | BlackBoxFunc::Blake3
+        | BlackBoxFunc::PedersenCommitment
+        | BlackBoxFunc::PedersenHash => None,
 
         BlackBoxFunc::Keccakf1600 => Some(25),
         // The permutation takes a fixed number of inputs, but the inputs length depends on the proving system implementation.
@@ -677,8 +687,6 @@ fn black_box_func_expected_input_size(name: BlackBoxFunc) -> Option<usize> {
 
         // FromLeBytes takes a variable array of bytes as input
         BlackBoxFunc::BigIntFromLeBytes => None,
-        BlackBoxFunc::PedersenCommitment => todo!(),
-        BlackBoxFunc::PedersenHash => todo!(),
     }
 }
 
@@ -701,6 +709,12 @@ fn black_box_expected_output_size(name: BlackBoxFunc) -> Option<usize> {
         BlackBoxFunc::Poseidon2Permutation => None,
 
         BlackBoxFunc::Sha256Compression => Some(8),
+
+        // Pedersen commitment returns a point
+        BlackBoxFunc::PedersenCommitment => Some(2),
+
+        // Pedersen hash returns a field
+        BlackBoxFunc::PedersenHash => Some(1),
 
         // Can only apply a range constraint to one
         // witness at a time.
@@ -730,8 +744,6 @@ fn black_box_expected_output_size(name: BlackBoxFunc) -> Option<usize> {
 
         // AES encryption returns a variable number of outputs
         BlackBoxFunc::AES128Encrypt => None,
-        BlackBoxFunc::PedersenCommitment => todo!(),
-        BlackBoxFunc::PedersenHash => todo!(),
     }
 }
 
