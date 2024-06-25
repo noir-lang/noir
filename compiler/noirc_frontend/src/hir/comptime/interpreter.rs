3271b9b61b87d71bbe899da6987f69b7fe48dd55
Jake Fecher <jfecher11@gmail.com>
2024-06-25 13:18:20 -0500

    Format

--- compiler/noirc_frontend/src/hir/comptime/interpreter.rs
+++ compiler/noirc_frontend/src/hir/comptime/interpreter.rs
@@ -259,11 +259,7 @@ impl<'a> Interpreter<'a> {
     }
 
     /// Define a new variable in the current scope
-    fn define(
-        &mut self,
-        id: DefinitionId,
-        argument: Value,
-    ) {
+    fn define(&mut self, id: DefinitionId, argument: Value) {
         self.current_scope_mut().insert(id, argument);
     }
 
@@ -1186,9 +1182,7 @@ impl<'a> Interpreter<'a> {
 
     fn store_lvalue(&mut self, lvalue: HirLValue, rhs: Value) -> IResult<()> {
         match lvalue {
-            HirLValue::Ident(ident, typ) => {
-                self.mutate(ident.id, rhs, ident.location)
-            }
+            HirLValue::Ident(ident, typ) => self.mutate(ident.id, rhs, ident.location),
             HirLValue::Dereference { lvalue, element_type: _, location } => {
                 match self.evaluate_lvalue(&lvalue)? {
                     Value::Pointer(value) => {
