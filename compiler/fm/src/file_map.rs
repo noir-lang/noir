239ecc1c127199439f6c57f2986dfe7fc561604c
Michael J Klein <michaeljklein@users.noreply.github.com>
2024-06-27 10:55:10 -0400

    Update compiler/fm/src/file_map.rs
    Co-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>

--- compiler/fm/src/file_map.rs
+++ compiler/fm/src/file_map.rs
@@ -33,8 +33,6 @@ impl From<&PathBuf> for PathString {
 #[derive(Debug, Clone)]
 pub struct FileMap {
     files: SimpleFiles<PathString, String>,
-    // TODO remove before PR
-    pub name_to_id: HashMap<PathString, FileId>,
 }
 
 // XXX: Note that we derive Default here due to ModuleOrigin requiring us to set a FileId
