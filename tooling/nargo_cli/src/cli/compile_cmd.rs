5ba8d43ee466512edb5b3ec2f940849476635e05
Maxim Vezenov <mvezenov@gmail.com>
2024-06-28 15:34:58 +0000

    initial testing program for expr width

--- tooling/nargo_cli/src/cli/compile_cmd.rs
+++ tooling/nargo_cli/src/cli/compile_cmd.rs
@@ -190,7 +190,10 @@ fn compile_programs(
                 compile_options,
                 load_cached_program(package),
             )?;
+            println!("{}", program.program.functions[0].opcodes.len());
             let program = nargo::ops::transform_program(program, compile_options.expression_width);
+            println!("Optimized ACIR for main:");
+            println!("{}", program.program.functions[0].opcodes.len());
             save_program_to_file(
                 &program.clone().into(),
                 &package.name,
