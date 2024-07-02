9a2202d23de3700fff1cc758ac408acdfc7c304a
Tom French <tom@tomfren.ch>
2024-07-02 14:44:26 +0100

    chore: remove unnecessary `before` block

--- examples/recursion_js/test/recursive.test.ts
+++ examples/recursion_js/test/recursive.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { join, resolve } from 'path';
 
-import { CompiledCircuit, InputMap, InputValue, ProofData } from '@noir-lang/types';
+import { CompiledCircuit, ProofData } from '@noir-lang/types';
 import { Noir } from '@noir-lang/noir_js';
 import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
 import { compile, createFileManager } from '@noir-lang/noir_wasm';
@@ -49,26 +49,30 @@ describe('can verify recursive proofs', () => {
   let recurseLeaf: FullNoir;
   let recurseNode: FullNoir;
 
-  let witness: Uint8Array;
-  let returnValue: InputValue;
-
-  let outerParams: InputMap;
-
   before(async () => {
     // Create Noir objects for each circuit
     leaf = await fullNoirFromCircuit('sum'); // a + b = c
     recurseLeaf = await fullNoirFromCircuit('recurseLeaf'); // verify l1 + l2 = n1, then sum n1 + n2
     recurseNode = await fullNoirFromCircuit('recurseNode'); // verify n1 + n2 = root1
+  });
 
+  after(async () => {
+    // Cleanup
+    recurseNode.backend.destroy();
+    recurseLeaf.backend.destroy();
+    leaf.backend.destroy();
+  });
+
+  it('does recursive proof', async () => {
     // Generate leaf proof artifacts (S1, addition of 1 and 3)
 
     // Leaf params of left branch
     const leafParams = { a: 1, b: 3 };
     let numPubInputs = 2;
 
-    ({ witness, returnValue } = await leaf.noir.execute(leafParams));
-    console.log('leaf: %d + %d = ', ...Object.values(leafParams), Number(returnValue).toString());
-    const innerProof1: ProofData = await leaf.backend.generateProof(witness);
+    const leafExecution = await leaf.noir.execute(leafParams);
+    console.log('leaf: %d + %d = ', ...Object.values(leafParams), Number(leafExecution.returnValue).toString());
+    const innerProof1: ProofData = await leaf.backend.generateProof(leafExecution.witness);
     console.log('\n\ninnerProof1.publicInputs\n', innerProof1.publicInputs);
     console.log('Generating intermediate proof artifacts for leaf calculation...');
     const artifacts1 = await leaf.backend.generateRecursiveProofArtifacts(
@@ -78,10 +82,10 @@ describe('can verify recursive proofs', () => {
 
     const pub_inputs: string[] = [
       ...Object.values(leafParams).map((n) => Number(n).toString()),
-      Number(returnValue).toString(),
+      Number(leafExecution.returnValue).toString(),
     ];
 
-    const a = returnValue;
+    const a = leafExecution.returnValue;
     const b = 5; // Sum of leaf branches beneath right node
 
     // Generate node proof artifacts (S2: verify 1+3=4 proof, add 5)
@@ -94,9 +98,9 @@ describe('can verify recursive proofs', () => {
     };
     numPubInputs = pub_inputs.length;
 
-    ({ witness, returnValue } = await recurseLeaf.noir.execute(nodeParams));
-    console.log('recurseLeaf: %d + %d = ', a, b, Number(returnValue).toString());
-    const innerProof2: ProofData = await recurseLeaf.backend.generateProof(witness);
+    const nodeExecution = await recurseLeaf.noir.execute(nodeParams);
+    console.log('recurseLeaf: %d + %d = ', a, b, Number(nodeExecution.returnValue).toString());
+    const innerProof2: ProofData = await recurseLeaf.backend.generateProof(nodeExecution.witness);
     console.log('Generating intermediate proof artifacts recurseLeaf...');
     const artifacts2 = await recurseLeaf.backend.generateRecursiveProofArtifacts(
       innerProof2,
@@ -106,26 +110,16 @@ describe('can verify recursive proofs', () => {
     console.log('artifacts2.proof length = ', artifacts2.proofAsFields.length);
 
     // Generate outer proof artifacts (S3: verify 4+5=9)
-    outerParams = {
+    const outerParams = {
       verification_key: artifacts2.vkAsFields,
       public_inputs: innerProof2.publicInputs, // 20 = 4 public inputs + 16 aggregation bytes,
       key_hash: artifacts2.vkHash,
       proof: artifacts2.proofAsFields, // 93 = 109 - 16 aggregation bytes in public inputs
     };
-  });
-
-  after(async () => {
-    // Cleanup
-    recurseNode.backend.destroy();
-    recurseLeaf.backend.destroy();
-    leaf.backend.destroy();
-  });
-
-  it('does recursive proof', async () => {
     console.log('Executing...');
-    ({ witness, returnValue } = await recurseNode.noir.execute(outerParams));
+    const rootExecution = await recurseNode.noir.execute(outerParams);
     console.log('Generating outer proof...');
-    const outerProof: ProofData = await recurseNode.backend.generateProof(witness);
+    const outerProof: ProofData = await recurseNode.backend.generateProof(rootExecution.witness);
     console.log('Verifying outer proof...');
     const resNode: boolean = await recurseNode.backend.verifyProof(outerProof);
     console.log('Verification', resNode ? 'PASSED' : 'failed');
