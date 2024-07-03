c60ad3635445479235ab34deed0cd6c66539ddba
Rumata888 <isennovskiy@gmail.com>
2024-07-03 16:22:39 +0000

    comments

--- compiler/noirc_evaluator/src/ssa/opt/independent_subgraph_detection.rs
+++ compiler/noirc_evaluator/src/ssa/opt/independent_subgraph_detection.rs
@@ -1,3 +1,6 @@
+//! This module defines an SSA pass that detects if the final function has any subgraphs independent from inputs and outputs.
+//! If this is the case, then part of the final circuit can be completely replaced by any other passing circuit, since there are no constraints ensuring connections.
+//! So the compiler informs the developer of this as a bug
 use im::HashMap;
 
 use crate::errors::{InternalBug, SsaReport};
@@ -10,6 +13,7 @@ use crate::ssa::ssa_gen::Ssa;
 use std::collections::{BTreeMap, HashSet};
 
 impl Ssa {
+    /// Go through each top-level non-brillig function and detect if it has independent subgraphs
     #[tracing::instrument(level = "trace", skip(self))]
     pub(crate) fn detect_independent_subgraphs(mut self) -> Ssa {
         for function in self.functions.values() {
@@ -23,18 +27,12 @@ impl Ssa {
         }
         self
     }
+
+    /// Detect independent subgraphs (not connected to function inputs or outputs) and return a vector of bug reports if some are found
     fn detect_independent_subgraphs_within_function(&self, function: &Function) -> Vec<SsaReport> {
         let mut context = Context::default();
-        let function_parameters = function.parameters();
-        let mut nonconstant_parameters_and_return_values: HashSet<ValueId> = function_parameters
-            .iter()
-            .filter(|&x| match function.dfg[*x] {
-                Value::NumericConstant { .. } => false,
-                _ => true,
-            })
-            .copied()
-            .collect();
-        nonconstant_parameters_and_return_values.extend(function.returns());
+
+        // Go through each block in the function and create a list of sets of ValueIds connected by instructions
         context.block_queue.push(function.entry_block());
         while let Some(block) = context.block_queue.pop() {
             if context.visited_blocks.contains(&block) {
@@ -43,35 +41,67 @@ impl Ssa {
             context.visited_blocks.insert(block);
             context.connect_value_ids_in_block(function, block, &self.functions);
         }
+
+        // Merge ValueIds into sets, where each original small set of ValueIds is merged with another set if they intersect
         context.merge_sets();
+
+        let function_parameters = function.parameters();
+        let variable_parameters_and_return_values: HashSet<ValueId> = function_parameters
+            .iter()
+            .chain(function.returns())
+            .filter(|&x| match function.dfg[*x] {
+                Value::NumericConstant { .. } => false, // Constant values don't connect elements and can be reused in different subgraphs, so we need to avoid them
+                _ => true,
+            })
+            .copied()
+            .collect();
+
         let mut connected_sets_indices: HashSet<usize> = HashSet::new();
-        for parameter_or_return_value in nonconstant_parameters_and_return_values.iter() {
+
+        // Go through each parameter and each set and check if the set contains the parameter
+        // If it's the case, then that set doesn't present an issue
+        for parameter_or_return_value in variable_parameters_and_return_values.iter() {
             for (set_index, final_set) in context.value_sets.iter().enumerate() {
                 if final_set.contains(parameter_or_return_value) {
                     connected_sets_indices.insert(set_index);
                 }
             }
         }
+
+        // All the other sets of variables are independent
         let disconnected_sets_indices: Vec<usize> =
             HashSet::from_iter(0..(context.value_sets.len()))
                 .difference(&connected_sets_indices)
                 .copied()
                 .collect();
+
         let all_brillig_generated_values: HashSet<ValueId> =
             context.brillig_return_to_argument_map.keys().copied().collect();
+
+        // Go through each disconnected set
         for set_index in disconnected_sets_indices.iter() {
             let current_set = &context.value_sets[*set_index];
+
+            // Find brillig-generated values in the set
             let intersection: Vec<ValueId> =
                 all_brillig_generated_values.intersection(current_set).cloned().collect();
-            if intersection.len() == 0 {
-                panic!("Somehow produced a disconnected subgraph without any brillig. How is that possible?")
+            if intersection.is_empty() {
+                // This is probably a test and the values are optimized in a weird way
+                continue;
             }
+
+            // Go through all brillig outputs in the set
             for brillig_output_in_set in intersection.iter() {
+                // Get the inputs that correspond to the output
                 let inputs: HashSet<ValueId> = HashSet::from_iter(
                     context.brillig_return_to_argument_map[brillig_output_in_set].iter().copied(),
                 );
+
+                // Check if any of them are not in the set
                 let unused_inputs: Vec<ValueId> = inputs.difference(current_set).copied().collect();
-                if unused_inputs.len() != 0 {
+
+                // There is a value not in the set, which means that the inputs/outputs of this call have not been properly constrained
+                if !unused_inputs.is_empty() {
                     context.warnings.push(SsaReport::Bug(InternalBug::IndependentSubgraph {
                         call_stack: function.dfg.get_call_stack(
                             context.brillig_return_to_instruction_id_map[brillig_output_in_set],
@@ -95,6 +125,7 @@ struct Context {
 }
 
 impl Context {
+    /// Go through each instruction in the block and add a set of ValueIds connected through that instruction
     fn connect_value_ids_in_block(
         &mut self,
         function: &Function,
@@ -105,6 +136,7 @@ impl Context {
 
         for instruction in instructions.iter() {
             let results = function.dfg.instruction_results(*instruction);
+            // For most instructions we just connect inputs and outputs
             match &function.dfg[*instruction] {
                 Instruction::Binary(binary) => {
                     let mut value_ids = vec![binary.lhs, binary.rhs];
@@ -161,6 +193,8 @@ impl Context {
                         },
                         Value::Function(callee) => match all_functions[&callee].runtime() {
                             RuntimeType::Brillig => {
+                                // For calls to brillig functions we memorize the mapping of results to argument ValueId's and InstructionId's
+                                // The latter are needed to produce the callstack later
                                 for result in results.iter() {
                                     self.brillig_return_to_argument_map
                                         .insert(*result, argument_ids.clone());
@@ -189,6 +223,7 @@ impl Context {
         self.block_queue.extend(function.dfg[block].successors());
     }
 
+    /// Add a set of ValueIds to the vector of connected values while ignoring constants
     fn connect_values(&mut self, function: &Function, values: &[ValueId]) {
         self.value_sets.push(HashSet::from_iter(
             values
@@ -201,27 +236,41 @@ impl Context {
         ));
     }
 
+    /// Merge all small sets into larger ones based on whether the sets intersect or not
+    ///
+    /// If two small sets have a common ValueId, we merge them into one
     fn merge_sets(&mut self) {
         let mut new_set_id: usize = 0;
         let mut updated_sets: HashMap<usize, HashSet<ValueId>> = HashMap::new();
         let mut value_dictionary: HashMap<ValueId, usize> = HashMap::new();
-        let mut parse_value_set: HashSet<ValueId> = HashSet::new();
+        let mut parsed_value_set: HashSet<ValueId> = HashSet::new();
+
+        // Go through each set
         for set in self.value_sets.iter() {
+            // Check if the set has any of the ValueIds we've encountered at previous iterations
             let intersection: HashSet<ValueId> =
-                set.intersection(&parse_value_set).cloned().collect();
-            parse_value_set.extend(set.iter());
+                set.intersection(&parsed_value_set).cloned().collect();
+            parsed_value_set.extend(set.iter());
+
+            // If there is no intersection, add the new set to updated sets
             if intersection.is_empty() {
                 updated_sets.insert(new_set_id, set.clone());
+
+                // Add each entry to a dictionary for quick lookups of which ValueId is in which updated set
                 for entry in set.iter() {
                     value_dictionary.insert(*entry, new_set_id);
                 }
                 new_set_id += 1;
                 continue;
             }
+
+            // If there is an intersection, we have to join the sets
             let mut joining_sets_ids: HashSet<usize> =
                 intersection.iter().map(|x| value_dictionary[x]).collect();
             let mut largest_set_size = usize::MIN;
             let mut largest_set_index = usize::MAX;
+
+            // We need to find the largest set to move as few elements as possible
             for set_id in joining_sets_ids.iter() {
                 if updated_sets[set_id].len() > largest_set_size {
                     (largest_set_index, largest_set_size) = (*set_id, updated_sets[set_id].len());
@@ -231,13 +280,19 @@ impl Context {
 
             let mut largest_set =
                 updated_sets.extract(&largest_set_index).expect("Set should be in the hashmap").0;
+
+            // For each of other sets that need to be joined
             for set_id in joining_sets_ids.iter() {
+                // Map each element to the largest set and insert it
                 for element in updated_sets[set_id].iter() {
                     value_dictionary[element] = largest_set_index;
                     largest_set.insert(*element);
                 }
+                // Remove the old set
                 updated_sets.remove(set_id);
             }
+
+            // Join the new set with the largest sets
             for element in set.iter() {
                 value_dictionary.insert(*element, largest_set_index);
                 largest_set.insert(*element);
@@ -255,7 +310,15 @@ mod test {
     };
 
     #[test]
+    /// Test that a connected function raises no warnings
     fn test_simple_connected_function() {
+        // fn main {
+        //   b0(v0: Field, v1: Field):
+        //      v2 = add v0, 1
+        //      v3 = mul v1, 2
+        //      v4 = eq v2, v3
+        //      return v2
+        // }
         let main_id = Id::test_new(0);
         let mut builder = FunctionBuilder::new("main".into(), main_id);
         let v0 = builder.add_parameter(Type::field());
@@ -275,7 +338,22 @@ mod test {
     }
 
     #[test]
+    /// Test where the results of a call to a brillig function are not connected to main function inputs or outputs
+    /// This should be detected.
     fn test_simple_function_with_disconnected_part() {
+        //  unconstrained fn br(v0: Field, v1: Field){
+        //      v2 = add v0, v1
+        //      return v2
+        //  }
+        //
+        //  fn main {
+        //   b0(v0: Field, v1: Field):
+        //      v2 = add v0, 1
+        //      v3 = mul v1, 2
+        //      v4 = call br(v2, v3)
+        //      v5 = add v4, 2
+        //      return
+        // }
         let main_id = Id::test_new(0);
         let mut builder = FunctionBuilder::new("main".into(), main_id);
         let v0 = builder.add_parameter(Type::field());
