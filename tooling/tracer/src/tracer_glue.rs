ed050b241a958c9dcf4666e9142cfe7e598bb53c
Stan Manilov <stanislav.manilov@gmail.com>
2024-07-10 19:07:49 +0300

    feat(tracer): Register variables in trace
    This commit is a first part of a two-part change; the foundation is laid
    here to register variables during execution (debugging) of a Noir
    program. The second part change will convert all types, not just Field,
    as can be seen in the code from this first part.
    
    The tests deleted in this commit will be restored then.

--- tooling/tracer/src/tracer_glue.rs
+++ tooling/tracer/src/tracer_glue.rs
@@ -1,6 +1,9 @@
-use crate::{SourceLocation, StackFrame};
+use crate::{stack_frame::Variable, SourceLocation, StackFrame};
 
-use runtime_tracing::{Line, Tracer};
+use acvm::acir::AcirField; // necessary, for `to_i128` to work
+use acvm::FieldElement;
+use noirc_printable_type::{PrintableType, PrintableValue};
+use runtime_tracing::{Line, Tracer, ValueRecord};
 use std::path::PathBuf;
 
 /// Registers a tracing step to the given `location` in the given `tracer`.
@@ -9,6 +12,47 @@ pub(crate) fn register_step(tracer: &mut Tracer, location: &SourceLocation) {
     let path = &PathBuf::from(filepath.to_string());
     let line = Line(*line_number as i64);
     tracer.register_step(path, line);
+}
+
+/// Registers all variables in the given frame for the last registered step. Each time a new step is
+/// registered, all of its variables need to be registered too. If no variables are registered for a
+/// step, the frontend will not carry over the variables registered for the previous step.
+pub(crate) fn register_variables(tracer: &mut Tracer, frame: &StackFrame) {
+    let mut variables = frame.variables.clone();
+    variables.sort();
+    for variable in &variables {
+        register_variable(tracer, variable);
+    }
+}
+
+/// Registers a variable for the last registered step.
+///
+/// See `register_variables`.
+fn register_variable(tracer: &mut Tracer, variable: &Variable) {
+    let value_record = register_value(tracer, &variable.value, &variable.typ);
+    tracer.register_variable_with_full_value(&variable.name, value_record);
+}
+
+/// Registers a value of a given type. Registers the type, if it's the first time it occurs.
+fn register_value(
+    tracer: &mut Tracer,
+    value: &PrintableValue<FieldElement>,
+    typ: &PrintableType,
+) -> ValueRecord {
+    if let PrintableType::Field = typ {
+        if let PrintableValue::Field(field_value) = value {
+            let type_id = tracer.ensure_type_id(runtime_tracing::TypeKind::Int, "Field");
+            ValueRecord::Int { i: field_value.to_i128() as i64, type_id }
+        } else {
+            // Note(stanm): panic here, because this means the compiler frontend is broken, which
+            // is not the responsibility of this module. Should not be reachable in integration
+            // tests (but reachable in unit tests).
+            panic!("type-value mismatch: type: {:?}; value: {:?}", typ, value)
+        }
+    } else {
+        // TODO(stanm): cover all types and remove `todo!`.
+        todo!("not implemented yet: type that is not Field: {:?}", typ)
+    }
 }
 
 /// Registers a call to the given `frame` at the given `location` in the given `tracer`.
