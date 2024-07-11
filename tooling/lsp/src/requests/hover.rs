bd7672a8360ce54e4b2010ea4e22bf4cf135943b
Ary Borenszweig <asterite@gmail.com>
2024-07-11 11:10:26 -0300

    Include crate name

--- tooling/lsp/src/requests/hover.rs
+++ tooling/lsp/src/requests/hover.rs
@@ -4,6 +4,7 @@ use async_lsp::ResponseError;
 use lsp_types::{Hover, HoverContents, HoverParams, MarkupContent, MarkupKind};
 use noirc_frontend::{
     ast::Visibility,
+    graph::CrateId,
     hir::def_map::ModuleId,
     hir_def::stmt::HirPattern,
     macros_api::{NodeInterner, StructId},
@@ -15,7 +16,7 @@ use noirc_frontend::{
 
 use crate::LspState;
 
-use super::{process_request, to_lsp_location};
+use super::{process_request, to_lsp_location, ProcessRequestCallbackArgs};
 
 pub(crate) fn on_hover_request(
     state: &mut LspState,
@@ -29,7 +30,7 @@ pub(crate) fn on_hover_request(
                 range: lsp_location.map(|location| location.range),
                 contents: HoverContents::Markup(MarkupContent {
                     kind: MarkupKind::Markdown,
-                    value: format_reference(reference, args.interner),
+                    value: format_reference(reference, &args),
                 }),
             }
         })
@@ -38,30 +39,28 @@ pub(crate) fn on_hover_request(
     future::ready(result)
 }
 
-fn format_reference(reference: ReferenceId, interner: &NodeInterner) -> String {
+fn format_reference(reference: ReferenceId, args: &ProcessRequestCallbackArgs) -> String {
     match reference {
-        ReferenceId::Module(id) => format_module(id, interner),
-        ReferenceId::Struct(id) => format_struct(id, interner),
-        ReferenceId::StructMember(id, field_index) => {
-            format_struct_member(id, field_index, interner)
-        }
-        ReferenceId::Trait(id) => format_trait(id, interner),
-        ReferenceId::Global(id) => format_global(id, interner),
-        ReferenceId::Function(id) => format_function(id, interner),
-        ReferenceId::Alias(id) => format_alias(id, interner),
-        ReferenceId::Local(id) => format_local(id, &interner),
+        ReferenceId::Module(id) => format_module(id, args),
+        ReferenceId::Struct(id) => format_struct(id, args),
+        ReferenceId::StructMember(id, field_index) => format_struct_member(id, field_index, args),
+        ReferenceId::Trait(id) => format_trait(id, args),
+        ReferenceId::Global(id) => format_global(id, args),
+        ReferenceId::Function(id) => format_function(id, args),
+        ReferenceId::Alias(id) => format_alias(id, args),
+        ReferenceId::Local(id) => format_local(id, &args),
         ReferenceId::Reference(location, _) => {
-            format_reference(interner.find_referenced(location).unwrap(), interner)
+            format_reference(args.interner.find_referenced(location).unwrap(), args)
         }
     }
 }
-fn format_module(id: ModuleId, interner: &NodeInterner) -> String {
-    let module_attributes = interner.module_attributes(&id);
+fn format_module(id: ModuleId, args: &ProcessRequestCallbackArgs) -> String {
+    let module_attributes = args.interner.module_attributes(&id);
 
     let mut string = String::new();
     if format_parent_module_from_module_id(
         &ModuleId { krate: id.krate, local_id: module_attributes.parent },
-        interner,
+        args,
         &mut string,
     ) {
         string.push_str("\n");
@@ -72,12 +71,12 @@ fn format_module(id: ModuleId, interner: &NodeInterner) -> String {
     string
 }
 
-fn format_struct(id: StructId, interner: &NodeInterner) -> String {
-    let struct_type = interner.get_struct(id);
+fn format_struct(id: StructId, args: &ProcessRequestCallbackArgs) -> String {
+    let struct_type = args.interner.get_struct(id);
     let struct_type = struct_type.borrow();
 
     let mut string = String::new();
-    if format_parent_module(ReferenceId::Struct(id), interner, &mut string) {
+    if format_parent_module(ReferenceId::Struct(id), args, &mut string) {
         string.push_str("\n");
     }
     string.push_str("    ");
@@ -96,13 +95,17 @@ fn format_struct(id: StructId, interner: &NodeInterner) -> String {
     string
 }
 
-fn format_struct_member(id: StructId, field_index: usize, interner: &NodeInterner) -> String {
-    let struct_type = interner.get_struct(id);
+fn format_struct_member(
+    id: StructId,
+    field_index: usize,
+    args: &ProcessRequestCallbackArgs,
+) -> String {
+    let struct_type = args.interner.get_struct(id);
     let struct_type = struct_type.borrow();
     let (field_name, field_type) = struct_type.field_at(field_index);
 
     let mut string = String::new();
-    if format_parent_module(ReferenceId::Struct(id), interner, &mut string) {
+    if format_parent_module(ReferenceId::Struct(id), args, &mut string) {
         string.push_str("::");
     }
     string.push_str(&struct_type.name.0.contents);
@@ -114,11 +117,11 @@ fn format_struct_member(id: StructId, field_index: usize, interner: &NodeInterne
     string
 }
 
-fn format_trait(id: TraitId, interner: &NodeInterner) -> String {
-    let a_trait = interner.get_trait(id);
+fn format_trait(id: TraitId, args: &ProcessRequestCallbackArgs) -> String {
+    let a_trait = args.interner.get_trait(id);
 
     let mut string = String::new();
-    if format_parent_module(ReferenceId::Trait(id), interner, &mut string) {
+    if format_parent_module(ReferenceId::Trait(id), args, &mut string) {
         string.push_str("\n");
     }
     string.push_str("    ");
@@ -128,13 +131,13 @@ fn format_trait(id: TraitId, interner: &NodeInterner) -> String {
     string
 }
 
-fn format_global(id: GlobalId, interner: &NodeInterner) -> String {
-    let global_info = interner.get_global(id);
+fn format_global(id: GlobalId, args: &ProcessRequestCallbackArgs) -> String {
+    let global_info = args.interner.get_global(id);
     let definition_id = global_info.definition_id;
-    let typ = interner.definition_type(definition_id);
+    let typ = args.interner.definition_type(definition_id);
 
     let mut string = String::new();
-    if format_parent_module(ReferenceId::Global(id), interner, &mut string) {
+    if format_parent_module(ReferenceId::Global(id), args, &mut string) {
         string.push_str("\n");
     }
     string.push_str("    ");
@@ -145,15 +148,15 @@ fn format_global(id: GlobalId, interner: &NodeInterner) -> String {
     string
 }
 
-fn format_function(id: FuncId, interner: &NodeInterner) -> String {
-    let func_meta = interner.function_meta(&id);
-    let func_name_definition_id = interner.definition(func_meta.name.id);
+fn format_function(id: FuncId, args: &ProcessRequestCallbackArgs) -> String {
+    let func_meta = args.interner.function_meta(&id);
+    let func_name_definition_id = args.interner.definition(func_meta.name.id);
 
     let mut string = String::new();
     let formatted_parent_module =
-        format_parent_module(ReferenceId::Function(id), interner, &mut string);
+        format_parent_module(ReferenceId::Function(id), args, &mut string);
     let formatted_parent_struct = if let Some(struct_id) = func_meta.struct_id {
-        let struct_type = interner.get_struct(struct_id);
+        let struct_type = args.interner.get_struct(struct_id);
         let struct_type = struct_type.borrow();
         if formatted_parent_module {
             string.push_str("::");
@@ -173,7 +176,7 @@ fn format_function(id: FuncId, interner: &NodeInterner) -> String {
     string.push('(');
     let parameters = &func_meta.parameters;
     for (index, (pattern, typ, visibility)) in parameters.iter().enumerate() {
-        format_pattern(pattern, interner, &mut string);
+        format_pattern(pattern, args.interner, &mut string);
         string.push_str(": ");
         if matches!(visibility, Visibility::Public) {
             string.push_str("pub ");
@@ -198,12 +201,12 @@ fn format_function(id: FuncId, interner: &NodeInterner) -> String {
     string
 }
 
-fn format_alias(id: TypeAliasId, interner: &NodeInterner) -> String {
-    let type_alias = interner.get_type_alias(id);
+fn format_alias(id: TypeAliasId, args: &ProcessRequestCallbackArgs) -> String {
+    let type_alias = args.interner.get_type_alias(id);
     let type_alias = type_alias.borrow();
 
     let mut string = String::new();
-    format_parent_module(ReferenceId::Alias(id), interner, &mut string);
+    format_parent_module(ReferenceId::Alias(id), args, &mut string);
     string.push_str("\n");
     string.push_str("    ");
     string.push_str("type ");
@@ -213,12 +216,12 @@ fn format_alias(id: TypeAliasId, interner: &NodeInterner) -> String {
     string
 }
 
-fn format_local(id: DefinitionId, interner: &NodeInterner) -> String {
-    let definition_info = interner.definition(id);
+fn format_local(id: DefinitionId, args: &ProcessRequestCallbackArgs) -> String {
+    let definition_info = args.interner.definition(id);
     let DefinitionKind::Local(expr_id) = definition_info.kind else {
         panic!("Expected a local reference to reference a local definition")
     };
-    let typ = interner.definition_type(id);
+    let typ = args.interner.definition_type(id);
 
     let mut string = String::new();
     string.push_str("    ");
@@ -271,30 +274,52 @@ fn format_pattern(pattern: &HirPattern, interner: &NodeInterner, string: &mut St
 
 fn format_parent_module(
     referenced: ReferenceId,
-    interner: &NodeInterner,
+    args: &ProcessRequestCallbackArgs,
     string: &mut String,
 ) -> bool {
-    let Some(module) = interner.reference_module(referenced) else {
+    let Some(module) = args.interner.reference_module(referenced) else {
         return false;
     };
 
-    return format_parent_module_from_module_id(&module, interner, string);
+    return format_parent_module_from_module_id(&module, args, string);
 }
 
 fn format_parent_module_from_module_id(
     module: &ModuleId,
-    interner: &NodeInterner,
+    args: &ProcessRequestCallbackArgs,
     string: &mut String,
 ) -> bool {
-    let Some(module_attributes) = interner.try_module_attributes(module) else {
-        return false;
+    let crate_id = module.krate;
+    let crate_name = match crate_id {
+        CrateId::Root(_) => Some(args.root_crate_name.clone()),
+        CrateId::Crate(_) => args
+            .root_crate_dependnencies
+            .iter()
+            .find(|dep| dep.crate_id == crate_id)
+            .map(|dep| format!("{}", dep.name)),
+        CrateId::Stdlib(_) => Some("std".to_string()),
+        CrateId::Dummy => None,
     };
 
-    string.push_str("    ");
+    let wrote_crate = if let Some(crate_name) = crate_name {
+        string.push_str("    ");
+        string.push_str(&crate_name);
+        true
+    } else {
+        false
+    };
+
+    let Some(module_attributes) = args.interner.try_module_attributes(module) else {
+        return wrote_crate;
+    };
+
+    if wrote_crate {
+        string.push_str("::");
+    }
 
     let mut segments = Vec::new();
     let mut current_attributes = module_attributes;
-    while let Some(parent_attributes) = interner.try_module_attributes(&ModuleId {
+    while let Some(parent_attributes) = args.interner.try_module_attributes(&ModuleId {
         krate: module.krate,
         local_id: current_attributes.parent,
     }) {
