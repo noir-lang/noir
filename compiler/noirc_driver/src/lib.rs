6fe8275e6968951d8870295f06c256649670d590
Maxim Vezenov <mvezenov@gmail.com>
2024-07-12 22:00:25 +0000

    DEFAULT_EXPRESSION_WIDTH const

--- compiler/noirc_driver/src/lib.rs
+++ compiler/noirc_driver/src/lib.rs
@@ -50,15 +50,9 @@ pub const NOIRC_VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const NOIR_ARTIFACT_VERSION_STRING: &str =
     concat!(env!("CARGO_PKG_VERSION"), "+", env!("GIT_COMMIT"));
 
-/// Default expression width used for Noir compilation.
-/// The ACVM native type `ExpressionWidth` has its own default which should always be unbounded,
-/// while we can sometimes expect the compilation target width to change.
-/// Thus, we set it separately here rather than trying to alter the default derivation of the type.
-pub const DEFAULT_EXPRESSION_WIDTH: ExpressionWidth = ExpressionWidth::Bounded { width: 4 };
-
 #[derive(Args, Clone, Debug, Default)]
 pub struct CompileOptions {
-    /// Override the expression width requested by the backend.
+    /// Specify the backend expression width that should be targeted
     #[arg(long, value_parser = parse_expression_width)]
     pub expression_width: Option<ExpressionWidth>,
 
