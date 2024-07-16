9d8705a6399a7c2d27b665ff65311e2b3a66eb81
guipublic <guipublic@gmail.com>
2024-07-16 09:38:35 +0000

    constant folding for keccak256

--- compiler/noirc_evaluator/src/ssa/acir_gen/acir_ir/generated_acir.rs
+++ compiler/noirc_evaluator/src/ssa/acir_gen/acir_ir/generated_acir.rs
@@ -293,26 +293,7 @@ impl<F: AcirField> GeneratedAcir<F> {
                 input2: Box::new([inputs[3][0], inputs[4][0], inputs[5][0]]),
                 outputs: (outputs[0], outputs[1], outputs[2]),
             },
-            BlackBoxFunc::Keccak256 => {
-                let var_message_size = match inputs.to_vec().pop() {
-                    Some(var_message_size) => var_message_size[0],
-                    None => {
-                        return Err(InternalError::MissingArg {
-                            name: "".to_string(),
-                            arg: "message_size".to_string(),
-                            call_stack: self.call_stack.clone(),
-                        });
-                    }
-                };
-
-                BlackBoxFuncCall::Keccak256 {
-                    inputs: inputs[0].clone(),
-                    var_message_size,
-                    outputs: outputs
-                        .try_into()
-                        .expect("Compiler should generate correct size outputs"),
-                }
-            }
+            BlackBoxFunc::Keccak256 =>  unreachable!("unexpected BlackBox {}", func_name.to_string()),
             BlackBoxFunc::Keccakf1600 => BlackBoxFuncCall::Keccakf1600 {
                 inputs: inputs[0]
                     .clone()
