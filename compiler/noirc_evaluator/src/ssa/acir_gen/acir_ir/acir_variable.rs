96534247b8b2513d4a27d7413c43b09bea64dc1e
Maxim Vezenov <mvezenov@gmail.com>
2024-07-18 14:25:36 +0000

    more granular add_var check

--- compiler/noirc_evaluator/src/ssa/acir_gen/acir_ir/acir_variable.rs
+++ compiler/noirc_evaluator/src/ssa/acir_gen/acir_ir/acir_variable.rs
@@ -24,6 +24,7 @@ use acvm::{
 use fxhash::FxHashMap as HashMap;
 use iter_extended::{try_vecmap, vecmap};
 use num_bigint::BigUint;
+use std::cmp::Ordering;
 use std::{borrow::Cow, hash::Hash};
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
@@ -591,7 +592,7 @@ impl<F: AcirField> AcirContext<F> {
         let lhs_data = self.vars[&lhs].clone();
         let rhs_data = self.vars[&rhs].clone();
 
-        let result = match (lhs_data.clone(), rhs_data.clone()) {
+        let result = match (lhs_data, rhs_data) {
             // (x * 1) == (1 * x) == x
             (AcirVarData::Const(constant), _) if constant.is_one() => rhs,
             (_, AcirVarData::Const(constant)) if constant.is_one() => lhs,
@@ -679,22 +680,49 @@ impl<F: AcirField> AcirContext<F> {
         let lhs_expr = self.var_to_expression(lhs)?;
         let rhs_expr = self.var_to_expression(rhs)?;
 
-        let mut sum_expr = &lhs_expr + &rhs_expr;
-        let sum_expr_fits = fits_in_one_identity(&sum_expr, self.expression_width);
+        let sum_expr = &lhs_expr + &rhs_expr;
+        if fits_in_one_identity(&sum_expr, self.expression_width) {
+            let sum_var = self.add_data(AcirVarData::from(sum_expr));
 
-        if !sum_expr_fits {
-            let lhs_witness_var = self.get_or_create_witness_var(lhs)?;
-            let lhs_witness_expr = self.var_to_expression(lhs_witness_var)?;
-            let rhs_witness_var = self.get_or_create_witness_var(rhs)?;
-            let rhs_witness_expr = self.var_to_expression(rhs_witness_var)?;
-
-            let new_sum_expr = &lhs_witness_expr + &rhs_witness_expr;
-            let sum_var = self.add_data(AcirVarData::from(new_sum_expr.clone()));
-
-            // Create new witness for the sum expression
-            let sum_witness_var = self.get_or_create_witness_var(sum_var)?;
-            sum_expr = self.var_to_expression(sum_witness_var)?;
+            return Ok(sum_var);
         }
+
+        let sum_expr = match lhs_expr.width().cmp(&rhs_expr.width()) {
+            Ordering::Greater => {
+                let lhs_witness_var = self.get_or_create_witness_var(lhs)?;
+                let lhs_witness_expr = self.var_to_expression(lhs_witness_var)?;
+
+                let new_sum_expr = &lhs_witness_expr + &rhs_expr;
+                if fits_in_one_identity(&new_sum_expr, self.expression_width) {
+                    new_sum_expr
+                } else {
+                    let rhs_witness_var = self.get_or_create_witness_var(rhs)?;
+                    let rhs_witness_expr = self.var_to_expression(rhs_witness_var)?;
+                    &lhs_expr + &rhs_witness_expr
+                }
+            }
+            Ordering::Less => {
+                let rhs_witness_var = self.get_or_create_witness_var(rhs)?;
+                let rhs_witness_expr = self.var_to_expression(rhs_witness_var)?;
+
+                let new_sum_expr = &lhs_expr + &rhs_witness_expr;
+                if fits_in_one_identity(&new_sum_expr, self.expression_width) {
+                    new_sum_expr
+                } else {
+                    let rhs_witness_var = self.get_or_create_witness_var(rhs)?;
+                    let rhs_witness_expr = self.var_to_expression(rhs_witness_var)?;
+                    &lhs_expr + &rhs_witness_expr
+                }
+            }
+            Ordering::Equal => {
+                let lhs_witness_var = self.get_or_create_witness_var(lhs)?;
+                let lhs_witness_expr = self.var_to_expression(lhs_witness_var)?;
+                let rhs_witness_var = self.get_or_create_witness_var(rhs)?;
+                let rhs_witness_expr = self.var_to_expression(rhs_witness_var)?;
+
+                &lhs_witness_expr + &rhs_witness_expr
+            }
+        };
 
         let sum_var = self.add_data(AcirVarData::from(sum_expr));
 
@@ -2018,16 +2046,16 @@ impl<F: AcirField> From<Expression<F>> for AcirVarData<F> {
 
 /// Checks if this expression can fit into one arithmetic identity
 fn fits_in_one_identity<F: AcirField>(expr: &Expression<F>, width: ExpressionWidth) -> bool {
-    // A Polynomial with more than one mul term cannot fit into one opcode
-    if expr.mul_terms.len() > 1 {
-        return false;
-    };
-
     let width = match &width {
         ExpressionWidth::Unbounded => {
             return true;
         }
         ExpressionWidth::Bounded { width } => *width,
+    };
+
+    // A Polynomial with more than one mul term cannot fit into one opcode
+    if expr.mul_terms.len() > 1 {
+        return false;
     };
 
     expr.width() <= width
