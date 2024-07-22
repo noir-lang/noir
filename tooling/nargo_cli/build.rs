517168d946c8e44b8b8c76cbac09774ffcaf4d5e
Stan Manilov <stanislav.manilov@gmail.com>
2024-07-22 13:42:26 +0300

    feat(tracer): Add additional artifacts in trace tests

--- tooling/nargo_cli/build.rs
+++ tooling/nargo_cli/build.rs
@@ -594,41 +594,69 @@ fn generate_plonky2_trace_tests(test_file: &mut File, test_data_dir: &Path) {
 fn plonky2_trace_{test_name}() {{
     use tempfile::tempdir;
 
-    let test_program_dir = PathBuf::from("{test_dir}");
+    let test_program_dir_path = PathBuf::from("{test_dir}");
 
     let temp_dir = tempdir().unwrap();
 
     let mut cmd = Command::cargo_bin("nargo").unwrap();
-    cmd.arg("--program-dir").arg(test_program_dir.to_str().unwrap());
+    cmd.arg("--program-dir").arg(test_program_dir_path.to_str().unwrap());
     cmd.arg("trace").arg("--trace-dir").arg(temp_dir.path());
-
 
     let trace_file_path = temp_dir.path().join("trace.json");
     let file_written_message = format!("Saved trace to {{:?}}", trace_file_path);
 
     cmd.assert().success().stdout(predicate::str::contains(file_written_message));
 
-    let expected_trace_path = test_program_dir.join("expected_trace.json");
+    let expected_trace_path = test_program_dir_path.join("expected_trace.json");
     let expected_trace = fs::read_to_string(expected_trace_path).unwrap();
     let mut expected_json: Value = serde_json::from_str(&expected_trace).unwrap();
 
     let actual_trace = fs::read_to_string(trace_file_path).unwrap();
     let mut actual_json: Value = serde_json::from_str(&actual_trace).unwrap();
 
-    // Rewrite all paths to avoid comparing them.
+    // Ignore paths in test, because they need to be absolute and supporting them would make the
+    // test too complicated.
     for trace_item in expected_json.as_array_mut().unwrap() {{
         if let Some(path) = trace_item.get_mut("Path") {{
-            *path = json!("it-does-not-matter");
+            *path = json!("ignored-in-test");
         }}
     }}
 
     for trace_item in actual_json.as_array_mut().unwrap() {{
         if let Some(path) = trace_item.get_mut("Path") {{
-            *path = json!("it-does-not-matter");
+            *path = json!("ignored-in-test");
         }}
     }}
 
     assert_eq!(expected_json, actual_json);
+
+    let expected_metadata_path = test_program_dir_path.join("expected_metadata.json");
+    let expected_metadata = fs::read_to_string(expected_metadata_path).unwrap();
+    let mut expected_metadata_json: Value = serde_json::from_str(&expected_metadata).unwrap();
+    if let Some(path) = expected_metadata_json.get_mut("workdir") {{
+        *path = json!("ignored-in-test");
+    }}
+
+    let actual_metadata_path = temp_dir.path().join("trace_metadata.json");
+    let actual_metadata = fs::read_to_string(actual_metadata_path).unwrap();
+    let mut actual_metadata_json: Value = serde_json::from_str(&actual_metadata).unwrap();
+    if let Some(path) = actual_metadata_json.get_mut("workdir") {{
+        *path = json!("ignored-in-test");
+    }}
+
+    assert_eq!(expected_metadata_json, actual_metadata_json);
+
+    let expected_paths_file_path = test_program_dir_path.join("expected_paths.json");
+    let expected_paths = fs::read_to_string(expected_paths_file_path).unwrap();
+    let expected_paths_json: Value = serde_json::from_str(&expected_paths).unwrap();
+    let num_expected_paths = expected_paths_json.as_array().unwrap().len();
+
+    let actual_paths_file_path = temp_dir.path().join("trace_paths.json");
+    let actual_paths = fs::read_to_string(actual_paths_file_path).unwrap();
+    let actual_paths_json: Value = serde_json::from_str(&actual_paths).unwrap();
+    let num_actual_paths = actual_paths_json.as_array().unwrap().len();
+
+    assert_eq!(num_expected_paths, num_actual_paths);
 }}
 "#,
             test_dir = test_dir.display(),
