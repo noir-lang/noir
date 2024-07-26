9b3ac3530afe01a111e0fdff56be4dfbf506203e
Ary Borenszweig <asterite@gmail.com>
2024-07-26 18:52:39 -0300

    Another use of turbofish_span

--- compiler/noirc_frontend/src/elaborator/types.rs
+++ compiler/noirc_frontend/src/elaborator/types.rs
@@ -1623,8 +1623,7 @@ impl<'context> Elaborator<'context> {
             }
 
             if segment.generics.is_some() {
-                // From "foo::<T>", create a span for just "::<T>"
-                let span = Span::from(segment.ident.span().end()..segment.span.end());
+                let span = segment.turbofish_span();
                 self.push_err(TypeCheckError::UnsupportedTurbofishUsage { span });
             }
         }
