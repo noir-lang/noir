89e8fb0eec69fcc9f0dd52ea72178fb7b074fb3a
Maxim Vezenov <mvezenov@gmail.com>
2024-08-08 17:20:00 +0000

    one more

--- acvm-repo/acvm/src/pwg/mod.rs
+++ acvm-repo/acvm/src/pwg/mod.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 use acir::{
     brillig::ForeignCallResult,
     circuit::{
-        brillig::BrilligBytecode,
+        brillig::{BrilligBytecode, BrilligFunctionId},
         opcodes::{BlockId, ConstantOrWitnessEnum, FunctionInput},
         AssertionPayload, ErrorSelector, ExpressionOrMemory, Opcode, OpcodeLocation,
         RawAssertionPayload, ResolvedAssertionPayload, STRING_ERROR_SELECTOR,
