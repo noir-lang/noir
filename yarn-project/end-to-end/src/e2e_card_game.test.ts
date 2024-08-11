7d26fc6a61c4bcae3e283a2a8178082cc68e7007
Jan Beneš <janbenes1234@gmail.com>
2024-08-11 15:56:08 +0200

    refactor: card contract cleanup (#7874)
    Removing redundant code from card contract. Originally discussed
    [here](https://github.com/AztecProtocol/aztec-packages/pull/7834#discussion_r1709216578).

--- yarn-project/end-to-end/src/e2e_card_game.test.ts
+++ yarn-project/end-to-end/src/e2e_card_game.test.ts
@@ -183,7 +183,7 @@ describe('e2e_card_game', () => {
           .join_game(GAME_ID, [cardToField(firstPlayerCollection[0]), cardToField(firstPlayerCollection[1])])
           .send()
           .wait(),
-      ).rejects.toThrow(`Card not found`);
+      ).rejects.toThrow(`Not all cards were removed`);
 
       const collection = await contract.methods.view_collection_cards(firstPlayer, 0).simulate({ from: firstPlayer });
       expect(boundedVecToArray(collection)).toHaveLength(1);
