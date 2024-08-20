19bd8a9e678b2c3671a49a20090885f1d5d8048c
Cat McGee <helloworld@mcgee.cat>
2024-08-20 20:10:51 +0900

    feat(docs): Cookbook integration (#8083)
    Closes https://github.com/AztecProtocol/dev-rel/issues/372

--- docs/src/theme/Root.js
+++ docs/src/theme/Root.js
@@ -2,12 +2,16 @@ import React from 'react';
 import useMatomo from '@site/src/components/Matomo/matomo';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import useIsBrowser from '@docusaurus/useIsBrowser';
+import AskCookbook from '@cookbookdev/docsbot/react';
 
 function OptOutForm() {
   const banner = useMatomo();
 
   return <>{banner}</>;
 }
+
+/** It's a public API key, so it's safe to expose it here. */
+const COOKBOOK_PUBLIC_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NmIyOWRmZDM3ZWIwYzRiMTVlZGYzMDAiLCJpYXQiOjE3MjI5ODE4ODUsImV4cCI6MjAzODU1Nzg4NX0.19rsZIFNmgVEbpmDjEJ8oPnL7uHYePytf-Ex1pjm2_8';
 
 export default function Root({ children }) {
   const useIsBrowserValue = useIsBrowser();
@@ -17,6 +21,7 @@ export default function Root({ children }) {
     <>
       {children}
       <BrowserOnly>{() => <OptOutForm />}</BrowserOnly>
+      <BrowserOnly>{() => <AskCookbook apiKey={COOKBOOK_PUBLIC_API_KEY} />}</BrowserOnly>
     </>
   );
 }
