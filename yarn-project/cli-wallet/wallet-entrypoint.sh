ff94aa20a0bc799710e4515bf4e7496c6bda8be7
Gregorio Juliana <gregojquiros@gmail.com>
2024-08-22 16:13:10 +0200

    feat: removed socat, fixed aztec-run (#8145)
    Removes socat as dependency for aztec bins, only complaining in case the
    user is using a mac and wants to access the SSH functionality of the
    wallet.

--- yarn-project/cli-wallet/wallet-entrypoint.sh
+++ yarn-project/cli-wallet/wallet-entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-
-cleanup() {
-    kill -9 $SOCAT_PID
-    rm -rf $SOCKET
-}
-
 if [[ -n "${SSH_AUTH_SOCK_SOCAT_PORT:-}" ]]; then
+    cleanup() {
+        kill -9 $SOCAT_PID
+        rm -rf $SOCKET
+    }
+
     SOCKET="$HOME/.aztec/aztec-wallet-$RANDOM.sock"
     socat UNIX-LISTEN:$SOCKET,fork TCP:host.docker.internal:${SSH_AUTH_SOCK_SOCAT_PORT} &
     SOCAT_PID=$!
     trap cleanup EXIT SIGKILL SIGTERM
+    export SSH_AUTH_SOCK="${SOCKET:-}"
 fi
 
-SSH_AUTH_SOCK="${SOCKET:-}" node --no-warnings /usr/src/yarn-project/cli-wallet/dest/bin/index.js $@
+node --no-warnings /usr/src/yarn-project/cli-wallet/dest/bin/index.js $@
