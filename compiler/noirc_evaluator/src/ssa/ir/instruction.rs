9d8f2bd759837d7f1f78c1b56b8e30de35c80867
guipublic <47281315+guipublic@users.noreply.github.com>
2024-08-22 17:29:23 +0200

    fix: do not use predicate for index in array operation, when the index is safe (#5779)
    # Description
    
    ## Problem\*
    
    Resolves #5761
    
    ## Summary\*
    
    For array operation, we keep the index when it is safe, instead of using
    the predicate.
    
    
    ## Additional Context
    
    
    
    ## Documentation\*
    
    Check one:
    - [X] No documentation needed.
    - [ ] Documentation included in this PR.
    - [ ] **[For Experimental Features]** Documentation to be submitted in a
    separate PR.
    
    # PR Checklist\*
    
    - [X] I have tested the changes locally.
    - [X] I have formatted the changes with [Prettier](https://prettier.io/)
    and/or `cargo fmt` on default settings.

--- compiler/noirc_evaluator/src/ssa/ir/instruction.rs
+++ compiler/noirc_evaluator/src/ssa/ir/instruction.rs
@@ -396,13 +396,9 @@ impl Instruction {
                 true
             }
 
-            // `ArrayGet`s which read from "known good" indices from an array don't need a predicate.
             Instruction::ArrayGet { array, index } => {
-                #[allow(clippy::match_like_matches_macro)]
-                match (dfg.type_of_value(*array), dfg.get_numeric_constant(*index)) {
-                    (Type::Array(_, len), Some(index)) if index.to_u128() < (len as u128) => false,
-                    _ => true,
-                }
+                // `ArrayGet`s which read from "known good" indices from an array should not need a predicate.
+                !dfg.is_safe_index(*index, *array)
             }
 
             Instruction::EnableSideEffects { .. } | Instruction::ArraySet { .. } => true,
