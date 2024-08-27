eba57b42aafc2b4b5fe0ebc9a8edd22a9fdbe71b
Santiago Palladino <santiago@aztecprotocol.com>
2024-08-27 12:52:03 -0300

    chore: Configure interval mining for anvil (#8211)
    For all setups that are not e2e tests, sets up anvil to use 12-second
    block times interval mining, as opposed to automine. This will make
    setup a bit slower unfortunately, but will reduce divergence with the
    work on block building.

--- iac/mainnet-fork/scripts/run_nginx_anvil.sh
+++ iac/mainnet-fork/scripts/run_nginx_anvil.sh
@@ -22,7 +22,7 @@ echo "result: ${MNEMONIC_STRIPPED:0:10}..."
 mkdir -p /data
 
 # Run anvil silently
-.foundry/bin/anvil --silent --host $HOST -p $PORT -m "$MNEMONIC_STRIPPED" -f=https://mainnet.infura.io/v3/$INFURA_API_KEY --chain-id=$L1_CHAIN_ID --fork-block-number=15918000 --block-base-fee-per-gas=10 -s=$SNAPSHOT_FREQUENCY --state=./data/state --balance=1000000000000000000 >/dev/null &
+.foundry/bin/anvil --silent --block-time 12 --host $HOST -p $PORT -m "$MNEMONIC_STRIPPED" -f=https://mainnet.infura.io/v3/$INFURA_API_KEY --chain-id=$L1_CHAIN_ID --fork-block-number=15918000 --block-base-fee-per-gas=10 -s=$SNAPSHOT_FREQUENCY --state=./data/state --balance=1000000000000000000 >/dev/null &
 
 echo "Waiting for ethereum host at $ETHEREUM_HOST..."
 while ! curl -s $ETHEREUM_HOST >/dev/null; do sleep 1; done
