b0e5393add1b35d6c8812ad10541ca2acc3b0f64
Ary Borenszweig <asterite@gmail.com>
2024-08-28 15:06:37 -0300

    Only show at most three backtrace frames

--- compiler/noirc_frontend/src/hir/comptime/errors.rs
+++ compiler/noirc_frontend/src/hir/comptime/errors.rs
@@ -361,9 +361,12 @@ impl<'a> From<&'a InterpreterError> for CustomDiagnostic {
                 };
                 let mut diagnostic =
                     CustomDiagnostic::simple_error(primary, secondary, location.span);
-                for frame in call_stack {
+
+                // Only take at most 3 frames starting from the top of the stack to avoid producing too much output
+                for frame in call_stack.iter().rev().take(3) {
                     diagnostic.add_secondary_with_file("".to_string(), frame.span, frame.file);
                 }
+
                 diagnostic
             }
             InterpreterError::NoMethodFound { name, typ, location } => {
