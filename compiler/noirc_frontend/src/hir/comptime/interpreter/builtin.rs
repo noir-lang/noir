e29d4b3646f0527fc01bc4584ee33616db922c72
Ary Borenszweig <asterite@gmail.com>
2024-08-30 17:36:44 -0300

    feat: add `Type::as_string` (#5871)
    # Description
    
    ## Problem
    
    Part of #5668
    
    ## Summary
    
    
    ## Additional Context
    
    
    
    ## Documentation
    
    Check one:
    - [ ] No documentation needed.
    - [x] Documentation included in this PR.
    - [ ] **[For Experimental Features]** Documentation to be submitted in a
    separate PR.
    
    # PR Checklist
    
    - [x] I have tested the changes locally.
    - [x] I have formatted the changes with [Prettier](https://prettier.io/)
    and/or `cargo fmt` on default settings.
    
    ---------
    
    Co-authored-by: jfecher <jake@aztecprotocol.com>

--- compiler/noirc_frontend/src/hir/comptime/interpreter/builtin.rs
+++ compiler/noirc_frontend/src/hir/comptime/interpreter/builtin.rs
@@ -144,6 +144,7 @@ impl<'local, 'context> Interpreter<'local, 'context> {
             "type_as_constant" => type_as_constant(arguments, return_type, location),
             "type_as_integer" => type_as_integer(arguments, return_type, location),
             "type_as_slice" => type_as_slice(arguments, return_type, location),
+            "type_as_str" => type_as_str(arguments, return_type, location),
             "type_as_struct" => type_as_struct(arguments, return_type, location),
             "type_as_tuple" => type_as_tuple(arguments, return_type, location),
             "type_eq" => type_eq(arguments, location),
@@ -537,6 +538,21 @@ fn type_as_slice(
     type_as(arguments, return_type, location, |typ| {
         if let Type::Slice(slice_type) = typ {
             Some(Value::Type(*slice_type))
+        } else {
+            None
+        }
+    })
+}
+
+// fn as_str(self) -> Option<Type>
+fn type_as_str(
+    arguments: Vec<(Value, Location)>,
+    return_type: Type,
+    location: Location,
+) -> IResult<Value> {
+    type_as(arguments, return_type, location, |typ| {
+        if let Type::String(n) = typ {
+            Some(Value::Type(*n))
         } else {
             None
         }
