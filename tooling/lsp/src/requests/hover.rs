8f031c60c31e3b29b779c72213f2750105833ea4
Tom French <tom@tomfren.ch>
2024-08-30 15:06:30 +0100

    .

--- tooling/lsp/src/requests/hover.rs
+++ tooling/lsp/src/requests/hover.rs
@@ -334,33 +334,27 @@ fn format_parent_module_from_module_id(
         segments.push(&module_attributes.name);
 
         let mut current_attributes = module_attributes;
-        while let Some(parent_local_id) = current_attributes.parent {
-            let parent_module_id = ModuleId { krate: module.krate, local_id: parent_local_id };
-            if let Some(parent_attributes) = args.interner.try_module_attributes(&parent_module_id)
-            {
-                segments.push(&parent_attributes.name);
-                current_attributes = parent_attributes;
-            } else {
-                // We cannot find a parent module.
+        loop {
+            let Some(parent_local_id) = current_attributes.parent else {
                 break;
-            }
+            };
+
+            let Some(parent_attributes) = args.interner.try_module_attributes(&ModuleId {
+                krate: module.krate,
+                local_id: parent_local_id,
+            }) else {
+                break;
+            };
+
+            segments.push(&parent_attributes.name);
+            current_attributes = parent_attributes;
         }
     }
 
-    let crate_id = module.krate;
-    let crate_name = match crate_id {
-        CrateId::Root(_) => Some(args.crate_name.clone()),
-        CrateId::Crate(_) => args
-            .dependencies
-            .iter()
-            .find(|dep| dep.crate_id == crate_id)
-            .map(|dep| format!("{}", dep.name)),
-        CrateId::Stdlib(_) => Some("std".to_string()),
-        CrateId::Dummy => unreachable!("ICE: A dummy CrateId should not be accessible"),
-    };
-
-    if let Some(crate_name) = &crate_name {
-        segments.push(crate_name);
+    // We don't record module attriubtes for the root module,
+    // so we handle that case separately
+    if let CrateId::Root(_) = module.krate {
+        segments.push(&args.crate_name);
     };
 
     if segments.is_empty() {
