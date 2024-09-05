0b73e07b0eab0a96378321177d7a9035bbf9257a
jfecher <jake@aztecprotocol.com>
2024-09-05 11:24:51 -0500

    Update compiler/noirc_frontend/src/hir/comptime/interpreter/builtin.rs

--- compiler/noirc_frontend/src/hir/comptime/interpreter/builtin.rs
+++ compiler/noirc_frontend/src/hir/comptime/interpreter/builtin.rs
@@ -1666,28 +1666,6 @@ fn fmtstr_quoted_contents(
     Ok(Value::Quoted(Rc::new(tokens)))
 }
 
-// fn quoted(self) -> Quoted
-fn fmtstr_quoted(
-    interner: &NodeInterner,
-    arguments: Vec<(Value, Location)>,
-    location: Location,
-) -> IResult<Value> {
-    let self_argument = check_one_argument(arguments, location)?;
-    let (string, _) = get_format_string(interner, self_argument)?;
-    let (tokens, _) = Lexer::lex(&string);
-    let str = tokens
-        .0
-        .into_iter()
-        .filter(|token| !matches!(token.clone().into_token(), Token::EOF))
-        .map(|token| token.into_token().display(interner).to_string())
-        .collect::<Vec<_>>()
-        .join("");
-
-    let token = Token::Str(str);
-
-    Ok(Value::Quoted(Rc::new(vec![token])))
-}
-
 // fn body(self) -> Expr
 fn function_def_body(
     interner: &NodeInterner,
