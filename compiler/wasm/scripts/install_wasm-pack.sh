125c8134c876dad9795816b984213f5ca9285a05
Tom French <tom@tomfren.ch>
2024-09-11 13:31:17 +0100

    .

--- compiler/wasm/scripts/install_wasm-pack.sh
+++ compiler/wasm/scripts/install_wasm-pack.sh
@@ -3,7 +3,7 @@ set -eu
 
 cd $(dirname "$0")/..
 
-../../../.github/scripts/cargo-binstall-install.sh
+../../.github/scripts/cargo-binstall-install.sh
 
 # Install wasm-pack 
 cargo-binstall wasm-pack@0.12.1 -y --force
