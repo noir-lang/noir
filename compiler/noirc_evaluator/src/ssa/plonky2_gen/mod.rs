b14b863145711839dde6b38cb493e62a9d42a627
Nikolay Nikolov <nickysn@gmail.com>
2024-09-17 14:43:41 +0300

    refactor(plonky2): removed unreachable code. No functional changes.

--- compiler/noirc_evaluator/src/ssa/plonky2_gen/mod.rs
+++ compiler/noirc_evaluator/src/ssa/plonky2_gen/mod.rs
@@ -218,10 +218,6 @@ impl P2Type {
                     }
                 }
                 NumericType::Signed { bit_size } => P2Type::Integer(bit_size, true),
-                _ => {
-                    let feature_name = format!("the {numeric_type} type");
-                    return Err(Plonky2GenError::UnsupportedFeature { name: feature_name });
-                }
             },
             Type::Array(composite_type, array_size) => {
                 P2Type::Array(
