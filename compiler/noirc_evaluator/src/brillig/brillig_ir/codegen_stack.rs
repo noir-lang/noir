bffe5a3bb6c8caf957ee38cbd7f10cdff5ee1253
sirasistant <sirasistant@gmail.com>
2024-09-18 18:57:35 +0000

    feat: Implement solver for mov_registers_to_registers

--- compiler/noirc_evaluator/src/brillig/brillig_ir/codegen_stack.rs
+++ compiler/noirc_evaluator/src/brillig/brillig_ir/codegen_stack.rs
@@ -1,4 +1,7 @@
+mod mov_registers_solver;
+
 use acvm::{acir::brillig::MemoryAddress, AcirField};
+use mov_registers_solver::{is_loop, MoveRegistersSolver};
 
 use super::{debug_show::DebugToString, registers::RegisterAllocator, BrilligContext};
 
@@ -10,17 +13,29 @@ impl<F: AcirField + DebugToString, Registers: RegisterAllocator> BrilligContext<
         sources: Vec<MemoryAddress>,
         destinations: Vec<MemoryAddress>,
     ) {
-        let new_sources: Vec<_> = sources
-            .iter()
-            .map(|source| {
-                let new_source = self.allocate_register();
-                self.mov_instruction(new_source, *source);
-                new_source
-            })
-            .collect();
-        for (new_source, destination) in new_sources.iter().zip(destinations.iter()) {
-            self.mov_instruction(*destination, *new_source);
-            self.deallocate_register(*new_source);
+        let chains =
+            MoveRegistersSolver::sources_destinations_to_move_chains(sources, destinations);
+        for mut chain in chains {
+            assert!(chain.len() > 0, "Empty chain found");
+
+            // If the chain is a loop, we need a temporary register to break the loop
+            if is_loop(&chain) {
+                let temp_register = self.allocate_register();
+                // Backup the first destination
+                self.mov_instruction(temp_register, chain[0].1);
+                // Do all operations but the last one
+                let last = chain.pop().unwrap();
+                for (source, destination) in chain {
+                    self.mov_instruction(destination, source);
+                }
+                // Move the backup to the last destination
+                self.mov_instruction(last.1, temp_register);
+                self.deallocate_register(temp_register);
+            } else {
+                for (source, destination) in chain {
+                    self.mov_instruction(destination, source);
+                }
+            }
         }
     }
 }
