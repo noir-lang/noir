86164d5cf1696ce209fc87136f62c07b5d94aa96
Maxim Vezenov <mvezenov@gmail.com>
2024-09-20 17:07:51 +0000

    cleanup old comment

--- compiler/noirc_evaluator/src/ssa.rs
+++ compiler/noirc_evaluator/src/ssa.rs
@@ -96,7 +96,6 @@ pub(crate) fn optimize_into_acir(
     .run_pass(Ssa::inline_functions, "After Inlining:")
     // Run mem2reg with the CFG separated into blocks
     .run_pass(Ssa::mem2reg, "After Mem2Reg:")
-    // .run_pass(Ssa::remove_paired_rc, "After Removing Paired rc_inc & rc_decs:")
     .run_pass(Ssa::as_slice_optimization, "After `as_slice` optimization")
     .try_run_pass(
         Ssa::evaluate_static_assert_and_assert_constant,
