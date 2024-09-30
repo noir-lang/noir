07925664131fec7958f14d39195ff584985708e3
Michael J Klein <michaeljklein@users.noreply.github.com>
2024-09-30 13:49:27 -0400

    Update compiler/noirc_frontend/src/hir/def_collector/errors.rs
    Co-authored-by: Maxim Vezenov <mvezenov@gmail.com>

--- compiler/noirc_frontend/src/hir/def_collector/errors.rs
+++ compiler/noirc_frontend/src/hir/def_collector/errors.rs
@@ -106,7 +106,7 @@ impl<'a> From<&'a UnsupportedNumericGenericType> for Diagnostic {
         let typ = &error.typ;
 
         Diagnostic::simple_error(
-            format!("{name} has a type of {typ}. The only supported types of numeric generics are integers, fields, and booleans."),
+            format!("{name} has a type of {typ}. The only supported numeric generic types are `u1`, `u8`, `u16`, and `u32`."),
             "Unsupported numeric generic type".to_string(),
             error.ident.0.span(),
         )
