18f1e2e19cfb19ee6b45a8d9370d90d469174118
sirasistant <sirasistant@gmail.com>
2024-10-04 11:41:43 +0000

    fix panic with databus in composite types

--- compiler/noirc_evaluator/src/ssa/function_builder/data_bus.rs
+++ compiler/noirc_evaluator/src/ssa/function_builder/data_bus.rs
@@ -121,16 +121,17 @@ impl FunctionBuilder {
                 databus.index += 1;
             }
             Type::Array(typ, len) => {
-                assert!(typ.len() == 1, "unsupported composite type");
                 databus.map.insert(value, databus.index);
                 for i in 0..len {
-                    // load each element of the array
-                    let index = self
-                        .current_function
-                        .dfg
-                        .make_constant(FieldElement::from(i as i128), Type::length_type());
-                    let element = self.insert_array_get(value, index, typ[0].clone());
-                    self.add_to_data_bus(element, databus);
+                    for subitem_typ in typ.iter() {
+                        // load each element of the array
+                        let index = self
+                            .current_function
+                            .dfg
+                            .make_constant(FieldElement::from(i as i128), Type::length_type());
+                        let element = self.insert_array_get(value, index, subitem_typ.clone());
+                        self.add_to_data_bus(element, databus);
+                    }
                 }
             }
             Type::Reference(_) => {
