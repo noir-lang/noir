4737766e4eb5a7dac57eb02c77d2d279c92bedd6
Akosh Farkash <aakoshh@gmail.com>
2024-10-07 12:21:48 +0100

    Proptest Keccak256

--- tooling/nargo_cli/tests/stdlib-props.rs
+++ tooling/nargo_cli/tests/stdlib-props.rs
@@ -8,28 +8,37 @@ use nargo::{
 use noirc_abi::input_parser::InputValue;
 use noirc_driver::{
     compile_main, file_manager_with_stdlib, prepare_crate, CompilationResult, CompileOptions,
-    CompiledProgram,
+    CompiledProgram, CrateId,
 };
 use noirc_frontend::hir::Context;
 use proptest::prelude::*;
+use sha3::Digest;
 
 /// Inputs and expected output of a snippet encoded in ABI format.
 #[derive(Debug)]
 struct SnippetInputOutput {
+    description: String,
     inputs: BTreeMap<String, InputValue>,
     expected_output: InputValue,
 }
 impl SnippetInputOutput {
     fn new(inputs: Vec<(&str, InputValue)>, output: InputValue) -> Self {
         Self {
+            description: "".to_string(),
             inputs: inputs.into_iter().map(|(k, v)| (k.to_string(), v)).collect(),
             expected_output: output,
         }
     }
+
+    /// Attach some description to hint at the scenario we are testing.
+    fn with_description(mut self, description: String) -> Self {
+        self.description = description;
+        self
+    }
 }
 
-/// Compile the main function in a code snippet.
-fn compile_snippet(source: String) -> CompilationResult<CompiledProgram> {
+/// Prepare a code snippet.
+fn prepare_snippet(source: String) -> (Context<'static, 'static>, CrateId) {
     let root = Path::new("");
     let file_name = Path::new("main.nr");
     let mut file_manager = file_manager_with_stdlib(root);
@@ -41,17 +50,41 @@ fn compile_snippet(source: String) -> CompilationResult<CompiledProgram> {
     let mut context = Context::new(file_manager, parsed_files);
     let root_crate_id = prepare_crate(&mut context, file_name);
 
-    compile_main(&mut context, root_crate_id, &CompileOptions::default(), None)
+    (context, root_crate_id)
 }
 
-fn test_snippet(source: String, strategy: BoxedStrategy<SnippetInputOutput>) {
-    let (program, _) = compile_snippet(source).expect("failed to compile");
+/// Compile the main function in a code snippet.
+///
+/// Use `force_brillig` to test it as an unconstrainted function without having to change the code.
+/// This is useful for methods that use the `runtime::is_uncontrained()` method to change their behaviour.
+fn prepare_and_compile_snippet(
+    source: String,
+    force_brillig: bool,
+) -> CompilationResult<CompiledProgram> {
+    let (mut context, root_crate_id) = prepare_snippet(source);
+    let options = CompileOptions { force_brillig, ..Default::default() };
+    compile_main(&mut context, root_crate_id, &options, None)
+}
+
+/// Compile a snippet and run property tests against it by generating random input/output pairs
+/// according to the strategy, executing the snippet with the input, and asserting that the
+/// output it returns is the one we expect.
+fn run_snippet_proptest(
+    source: String,
+    force_brillig: bool,
+    strategy: BoxedStrategy<SnippetInputOutput>,
+) {
+    let program = match prepare_and_compile_snippet(source.clone(), force_brillig) {
+        Ok((program, _)) => program,
+        Err(e) => panic!("failed to compile program:\n{source}\n{e:?}"),
+    };
 
     let blackbox_solver = bn254_blackbox_solver::Bn254BlackBoxSolver;
     let foreign_call_executor =
         RefCell::new(DefaultForeignCallExecutor::new(false, None, None, None));
 
     // Generate multiple input/output
+    // TODO: Execute with the interpreter as well.
     proptest!(|(io in strategy)| {
         let initial_witness = program.abi.encode(&io.inputs, None).expect("failed to encode");
         let mut foreign_call_executor = foreign_call_executor.borrow_mut();
@@ -70,22 +103,13 @@ fn test_snippet(source: String, strategy: BoxedStrategy<SnippetInputOutput>) {
         let (_, return_value) = program.abi.decode(main_witness).expect("failed to decode");
         let return_value = return_value.expect("should decode a return value");
 
-        prop_assert_eq!(return_value, io.expected_output)
+        prop_assert_eq!(return_value, io.expected_output, "{}", io.description);
     });
 }
 
+/// This is just a simple test to check that property testing works.
 #[test]
-fn works() {
-    // TODO:
-    // - define source to test, with a main function for entry
-    // - create dummy create with the source code as in `prepare_source`
-    // - compile using `create_main`
-    // - put things into a RefCell so they can be reused
-    // - define proptest! that encodes input and decodes output witness
-    // - execute_program
-    // - execute with interpreter
-    // - execute as unconstrained because hashing functions detect it
-
+fn test_basic() {
     let program = "fn main(init: u32) -> pub u32 {
         let mut x = init;
         for i in 0 .. 6 {
@@ -104,5 +128,59 @@ fn works() {
         })
         .boxed();
 
-    test_snippet(program.to_string(), strategy);
+    run_snippet_proptest(program.to_string(), false, strategy);
+}
+
+// TODO: - Sha256, Keccak256, Sha512, Schnorr, Poseidon2 and Poseidon
+
+#[test]
+fn test_keccak256() {
+    fn hash(input: &[u8]) -> [u8; 32] {
+        sha3::Keccak256::digest(input).try_into().expect("result is 256 bits")
+    }
+
+    // Keccak256 runs differently depending on whether it's unconstrained or not.
+    for force_brillig in [false, true] {
+        // XXX: Currently it fails with inputs >= 135 bytes
+        for max_len in [0usize, 10, 100, 134] {
+            // The maximum length is used to pick the generic version of the method.
+            let program = format!(
+                "fn main(input: [u8; {max_len}], message_size: u32) -> pub [u8; 32] {{
+                    std::hash::keccak256(input, message_size)
+                }}"
+            );
+
+            // The actual input length can be up to the maximum.
+            let strategy = (0..=max_len)
+                .prop_flat_map(|len| prop::collection::vec(any::<u8>(), len))
+                .prop_map(move |mut msg| {
+                    // The output is the hash of the data as it is.
+                    let output = hash(&msg);
+
+                    // The input has to be padded to the maximum length.
+                    let msg_size = msg.len();
+                    msg.resize(max_len, 0u8);
+
+                    SnippetInputOutput::new(
+                        vec![
+                            ("input", bytes_input(&msg)),
+                            ("message_size", InputValue::Field(FieldElement::from(msg_size))),
+                        ],
+                        bytes_input(&output),
+                    )
+                    .with_description(format!(
+                        "force_brillig = {force_brillig}, max_len = {max_len}"
+                    ))
+                })
+                .boxed();
+
+            run_snippet_proptest(program.to_string(), force_brillig, strategy);
+        }
+    }
+}
+
+fn bytes_input(bytes: &[u8]) -> InputValue {
+    InputValue::Vec(
+        bytes.iter().map(|b| InputValue::Field(FieldElement::from(*b as u32))).collect(),
+    )
 }
