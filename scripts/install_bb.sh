e25d605e789a9d26551e868eb9239072d1755edc
TomAFrench <tom@tomfren.ch>
2024-10-10 10:37:04 +0000

    chore(ci): pin commit to install bbup from

--- scripts/install_bb.sh
+++ scripts/install_bb.sh
@@ -5,7 +5,7 @@ VERSION="0.56.0"
 BBUP_PATH=~/.bb/bbup
 
 if ! [ -f $BBUP_PATH ]; then 
-    curl -L https://raw.githubusercontent.com/AztecProtocol/aztec-packages/master/barretenberg/cpp/installation/install | bash
+    curl -L https://raw.githubusercontent.com/AztecProtocol/aztec-packages/09c3b288eb96d2fe37e7bb6dedb45f9c9f4a4044/barretenberg/cpp/installation/install | bash
 fi
 
 $BBUP_PATH -v $VERSION
