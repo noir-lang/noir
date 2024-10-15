00780d4cbd9e6538ea67f576eeb3337cf25d0dce
Ary Borenszweig <asterite@gmail.com>
2024-10-15 13:27:44 -0300

    A few simple statements

--- tooling/nargo_fmt/src/formatter/statement.rs
+++ tooling/nargo_fmt/src/formatter/statement.rs
@@ -1,4 +1,7 @@
-use noirc_frontend::ast::{Statement, StatementKind};
+use noirc_frontend::{
+    ast::{Statement, StatementKind},
+    token::Keyword,
+};
 
 use super::{chunks::Chunks, Formatter};
 
@@ -10,21 +13,24 @@ impl<'a> Formatter<'a> {
             StatementKind::Let(_let_statement) => todo!("Format let statement"),
             StatementKind::Constrain(_constrain_statement) => todo!("Format constrain statement"),
             StatementKind::Expression(expression) => {
-                chunks.text(self.chunk(|formatter| {
-                    formatter.write_indentation();
-                }));
                 self.format_expression(expression, &mut chunks);
             }
             StatementKind::Assign(_assign_statement) => todo!("Format assign statement"),
             StatementKind::For(_for_loop_statement) => todo!("Format for loop statement"),
-            StatementKind::Break => todo!("Format break statement"),
-            StatementKind::Continue => todo!("Format continue statement"),
+            StatementKind::Break => {
+                chunks.text(self.chunk(|formatter| {
+                    formatter.write_keyword(Keyword::Break);
+                    formatter.write_semicolon();
+                }));
+            }
+            StatementKind::Continue => {
+                chunks.text(self.chunk(|formatter| {
+                    formatter.write_keyword(Keyword::Continue);
+                    formatter.write_semicolon();
+                }));
+            }
             StatementKind::Comptime(_statement) => todo!("Format comptime statement"),
             StatementKind::Semi(expression) => {
-                chunks.text(self.chunk(|formatter| {
-                    formatter.write_indentation();
-                }));
-
                 self.format_expression(expression, &mut chunks);
 
                 chunks.text(self.chunk(|formatter| {
@@ -36,5 +42,50 @@ impl<'a> Formatter<'a> {
                 unreachable!("Should not be present in the AST")
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::assert_format;
+
+    #[test]
+    fn format_expression_statement() {
+        let src = " fn foo() { 1 } ";
+        let expected = "fn foo() {
+    1
+}
+";
+        assert_format(src, expected);
+    }
+
+    #[test]
+    fn format_semi_statement() {
+        let src = " fn foo() { 1 ; } ";
+        let expected = "fn foo() {
+    1;
+}
+";
+        assert_format(src, expected);
+    }
+
+    #[test]
+    fn format_break_statement() {
+        let src = " fn foo() { break  ; } ";
+        let expected = "fn foo() {
+    break;
+}
+";
+        assert_format(src, expected);
+    }
+
+    #[test]
+    fn format_continue_statement() {
+        let src = " fn foo() { continue  ; } ";
+        let expected = "fn foo() {
+    continue;
+}
+";
+        assert_format(src, expected);
     }
 }
