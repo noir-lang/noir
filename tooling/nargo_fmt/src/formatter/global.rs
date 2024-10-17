b62feb78f5232f12d7cd2706718483e685205179
Ary Borenszweig <asterite@gmail.com>
2024-10-17 12:55:42 -0300

    comptime mut globals

--- tooling/nargo_fmt/src/formatter/global.rs
+++ tooling/nargo_fmt/src/formatter/global.rs
@@ -1,5 +1,5 @@
 use noirc_frontend::{
-    ast::{ItemVisibility, LetStatement},
+    ast::{ItemVisibility, LetStatement, Pattern},
     token::Keyword,
 };
 
@@ -15,9 +15,37 @@ impl<'a> Formatter<'a> {
         chunks.text(self.chunk(|formatter| {
             formatter.format_item_visibility(visibility);
         }));
+
+        if let_statement.comptime {
+            chunks.text(self.chunk(|formatter| {
+                formatter.write_keyword(Keyword::Comptime);
+                formatter.write_space();
+            }));
+        }
+
+        let pattern = let_statement.pattern;
+        let pattern = match pattern {
+            Pattern::Identifier(..) => pattern,
+            Pattern::Mutable(pattern, _span, _) => {
+                // `mut global x` is represented in the AST with a mutable pattern.
+                // A mutable pattern would be `mut x` but here we have `mut global x`,
+                // so in that case we write `mut` now, then the pattern we'll write doesn't
+                // have the `mut` part.
+                chunks.text(self.chunk(|formatter| {
+                    formatter.write_keyword(Keyword::Mut);
+                    formatter.write_space();
+                }));
+
+                *pattern
+            }
+            Pattern::Tuple(..) | Pattern::Struct(..) | Pattern::Interned(..) => {
+                unreachable!("Global pattern cannot be a tuple, struct or interned")
+            }
+        };
+
         chunks.group(self.format_let_or_global(
             Keyword::Global,
-            let_statement.pattern,
+            pattern,
             let_statement.r#type,
             Some(let_statement.expression),
         ));
@@ -44,6 +72,20 @@ mod tests {
     fn format_global_with_type() {
         let src = " pub  global  x  :  Field  =  1  ; ";
         let expected = "pub global x: Field = 1;\n";
+        assert_format(src, expected);
+    }
+
+    #[test]
+    fn format_comptime_global() {
+        let src = " pub  comptime  global  x  :  Field  =  1  ; ";
+        let expected = "pub comptime global x: Field = 1;\n";
+        assert_format(src, expected);
+    }
+
+    #[test]
+    fn format_comptime_mut_global() {
+        let src = " pub  comptime  mut  global  x  :  Field  =  1  ; ";
+        let expected = "pub comptime mut global x: Field = 1;\n";
         assert_format(src, expected);
     }
 }
