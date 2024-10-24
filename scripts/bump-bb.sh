08013cf5d7731aabb81f6fd833c350c941294565
Tom French <tom@tomfren.ch>
2024-10-24 20:33:07 +0100

    .

--- scripts/bump-bb.sh
+++ scripts/bump-bb.sh
@@ -5,7 +5,7 @@ BB_VERSION=$1
 sed -i.bak "s/^VERSION=.*/VERSION=\"$BB_VERSION\"/" ./scripts/install_bb.sh  && rm ./scripts/install_bb.sh.bak
 
 tmp=$(mktemp)
-INTEGRATION_TESTS_PACKAGE_JSON=./compiler/integration_tests/package.json
+INTEGRATION_TESTS_PACKAGE_JSON=./compiler/integration-tests/package.json
 jq --arg v $BB_VERSION '.dependencies."@aztec/bb.js" = $v' $INTEGRATION_TESTS_PACKAGE_JSON > $tmp && mv $tmp $INTEGRATION_TESTS_PACKAGE_JSON
 
 YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install
