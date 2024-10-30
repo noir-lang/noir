713ff2250345ef5d9c33715bcea967beba382c93
Michael J Klein <michaeljklein@users.noreply.github.com>
2024-10-30 07:01:08 -0400

    chore: add regression tests for #6314 (#6381)
    Co-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>
    Co-authored-by: Tom French <tom@tomfren.ch>

--- compiler/noirc_frontend/src/tests/traits.rs
+++ compiler/noirc_frontend/src/tests/traits.rs
@@ -263,6 +263,64 @@ fn errors_if_impl_trait_constraint_is_not_satisfied() {
 }
 
 #[test]
+// Regression test for https://github.com/noir-lang/noir/issues/6314
+// Baz inherits from a single trait: Foo
+fn regression_6314_single_inheritance() {
+    let src = r#"
+        trait Foo {
+            fn foo(self) -> Self;
+        }
+        
+        trait Baz: Foo {}
+        
+        impl<T> Baz for T where T: Foo {}
+        
+        fn main() { }
+    "#;
+    assert_no_errors(src);
+}
+
+#[test]
+// Regression test for https://github.com/noir-lang/noir/issues/6314
+// Baz inherits from two traits: Foo and Bar
+fn regression_6314_double_inheritance() {
+    let src = r#"
+        trait Foo {
+            fn foo(self) -> Self;
+        }
+       
+        trait Bar {
+            fn bar(self) -> Self;
+        }
+       
+        trait Baz: Foo + Bar {}
+       
+        impl<T> Baz for T where T: Foo + Bar {}
+       
+        fn baz<T>(x: T) -> T where T: Baz {
+            x.foo().bar()
+        }
+       
+        impl Foo for Field {
+            fn foo(self) -> Self {
+                self + 1
+            }
+        }
+       
+        impl Bar for Field {
+            fn bar(self) -> Self {
+                self + 2
+            }
+        }
+       
+        fn main() {
+            assert(0.foo().bar() == baz(0));
+        }"#;
+
+    assert_no_errors(src);
+}
+
+#[test]
 fn removes_assumed_parent_traits_after_function_ends() {
     let src = r#"
     trait Foo {}
