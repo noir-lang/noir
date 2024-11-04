2b4fda7834f6694a0b31cc8464df1e64cdba84fc
Nikolay Nikolov <nickysn@gmail.com>
2024-11-04 20:22:23 +0200

    test: added also a llvm-cov report command to the 3_cov.sh script

--- utils/test-coverage/3_cov.sh
+++ utils/test-coverage/3_cov.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
+# shows detailes line by line coverage in the source code (very long output):
 llvm-cov show -Xdemangler=rustfilt target/debug/nargo -instr-profile=noir_tests.profdata -show-line-counts-or-regions -show-instantiations
+
+# shows a summary of the coverage in a given cargo package (compiler/noirc_frontend in this example):
+llvm-cov report --use-color --instr-profile=./noir_tests.profdata --object target/debug/nargo --sources `find compiler/noirc_frontend -name "*.rs"`
