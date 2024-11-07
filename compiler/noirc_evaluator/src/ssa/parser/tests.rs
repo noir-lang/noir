c2f91645e21ffe24bd1dc8c4062bedd7a9171095
Ary Borenszweig <asterite@gmail.com>
2024-11-07 16:14:32 -0300

    Also test brillig function

--- compiler/noirc_evaluator/src/ssa/parser/tests.rs
+++ compiler/noirc_evaluator/src/ssa/parser/tests.rs
@@ -8,9 +8,20 @@ fn assert_ssa_roundtrip(src: &str) {
 }
 
 #[test]
-fn test_empty_function() {
+fn test_empty_acir_function() {
     let src = "
 acir(inline) fn main f0 {
+  b0():
+    return
+}
+";
+    assert_ssa_roundtrip(src);
+}
+
+#[test]
+fn test_empty_brillig_function() {
+    let src = "
+brillig(inline) fn main f0 {
   b0():
     return
 }
