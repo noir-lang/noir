53207c49434e6ef09675c69b51c2a165fd0c8784
Jake Fecher <jfecher11@gmail.com>
2024-11-12 11:52:57 -0600

    Fix missed resolve in DIE pass

--- compiler/noirc_evaluator/src/ssa/opt/die.rs
+++ compiler/noirc_evaluator/src/ssa/opt/die.rs
@@ -194,7 +194,8 @@ impl Context {
 
     /// Inspects a value and marks all instruction results as used.
     fn mark_used_instruction_results(&mut self, dfg: &DataFlowGraph, value_id: ValueId) {
-        if matches!(&dfg[dfg.resolve(value_id)], Value::Instruction { .. } | Value::Param { .. }) {
+        let value_id = dfg.resolve(value_id);
+        if matches!(&dfg[value_id], Value::Instruction { .. } | Value::Param { .. }) {
             self.used_values.insert(value_id);
         }
     }
