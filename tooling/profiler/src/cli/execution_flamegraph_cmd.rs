8265cfe82bc47192cf4d5ce2ca73769d211aad2e
Maxim Vezenov <mvezenov@gmail.com>
2024-11-17 15:30:02 +0000

    cleanup

--- tooling/profiler/src/cli/execution_flamegraph_cmd.rs
+++ tooling/profiler/src/cli/execution_flamegraph_cmd.rs
@@ -45,12 +45,6 @@ fn run_with_generator(
     let program =
         read_program_from_file(artifact_path).context("Error reading program from file")?;
 
-    assert_eq!(
-        program.bytecode.unconstrained_functions.len(),
-        1,
-        "This command should only be run with a program force compiled to Brillig"
-    );
-
     let (inputs_map, _) = read_inputs_from_file(prover_toml_path, Format::Toml, &program.abi)?;
 
     let initial_witness = program.abi.encode(&inputs_map, None)?;
