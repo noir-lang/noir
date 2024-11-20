7677f6625780fd76d0b74724a2d88d00dbd590b9
Maxim Vezenov <mvezenov@gmail.com>
2024-11-20 11:11:12 -0500

    Update compiler/noirc_evaluator/src/ssa/opt/unrolling.rs

--- compiler/noirc_evaluator/src/ssa/opt/unrolling.rs
+++ compiler/noirc_evaluator/src/ssa/opt/unrolling.rs
@@ -82,7 +82,6 @@ impl Function {
     }
 }
 
-#[derive(Debug, Clone)]
 pub(super) struct Loop {
     /// The header block of a loop is the block which dominates all the
     /// other blocks in the loop.
