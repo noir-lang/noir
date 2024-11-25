eb51fad6a2db883132fa9c8943ab5164f15cae39
Tom French <15848336+TomAFrench@users.noreply.github.com>
2024-11-25 23:29:32 +0000

    Update compiler/noirc_evaluator/src/ssa/opt/flatten_cfg.rs

--- compiler/noirc_evaluator/src/ssa/opt/flatten_cfg.rs
+++ compiler/noirc_evaluator/src/ssa/opt/flatten_cfg.rs
@@ -1490,8 +1490,8 @@ mod test {
         let separate_ssa = separate_ssa.flatten_cfg();
 
         // This program is much the same as the above except that `b3` has been inlined into `b2`.
-        // This means that when we try to find the join-point of one of these branches we end up inside one of those
-        // branches.
+        // `b2` is both the `then` block and the block in which both of the branches merge together again,
+        // this means that we end up inlining the merge block before we've finished inlining the `else` block.
         let merged_return_block_src = "
         acir(inline) fn main f0 {
           b0(v0: u1):
