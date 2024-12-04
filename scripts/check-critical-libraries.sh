91c5432c0973c6b1cfc79777f5a8ca078c94cf04
Tom French <tom@tomfren.ch>
2024-12-04 17:13:51 +0000

    chore: add script to check for critical library

---
+++ scripts/check-critical-libraries.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+# Run relative to repo root
+cd $(dirname "$0")/../
+
+CRITICAL_LIBRARIES=$(grep -v "^#\|^$" ./CRITICAL_NOIR_LIBRARIES)
+readarray -t REPOS_TO_CHECK < <(echo "$CRITICAL_LIBRARIES")
+
+getLatestReleaseTagForRepo() {
+    REPO_NAME=$1
+    TAG=$(gh release list -R $REPO_NAME --json 'tagName,isLatest' -q '.[] | select(.isLatest == true).tagName')
+    if [[ -z $TAG ]]; then
+        echo "$REPO_NAME has no valid release" >&2
+        exit 1
+    fi
+    echo $TAG
+}
+
+for REPO in ${REPOS_TO_CHECK[@]}; do
+    echo $REPO   
+    TMP_DIR=$(mktemp -d)
+    
+    TAG=$(getLatestReleaseTagForRepo $REPO)
+    git clone $REPO -c advice.detachedHead=false --depth 1 --branch $TAG $TMP_DIR
+    
+    nargo test --program-dir $TMP_DIR
+
+    rm -rf $TMP_DIR
+done
