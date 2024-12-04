3c5a92cb0f097f3c698f058d2d9804b6f0f7b1bb
Ratmir Karabut <rkarabut@users.noreply.github.com>
2024-12-04 03:00:12 +0300

    Update compiler/noirc_evaluator/src/errors.rs
    Co-authored-by: Michael J Klein <michaeljklein@users.noreply.github.com>

--- compiler/noirc_evaluator/src/errors.rs
+++ compiler/noirc_evaluator/src/errors.rs
@@ -96,7 +96,7 @@ impl From<SsaReport> for FileDiagnostic {
                         ("There is no path from the output of this brillig call to either return values or inputs of the circuit, which creates an independent subgraph. This is quite likely a soundness vulnerability".to_string(), call_stack)
                     }
                     InternalBug::UncheckedBrilligCall { call_stack } => {
-                        ("There is no assert checking this brillig call's inputs against its return values in some way. This should be done to prevent potential soundness vulnerabilities".to_string(), call_stack)
+                        ("There is no constraint between this brillig call's inputs and its return values. This should be done to prevent potential soundness vulnerabilities".to_string(), call_stack)
                     }
                     InternalBug::AssertFailed { call_stack } => ("As a result, the compiled circuit is ensured to fail. Other assertions may also fail during execution".to_string(), call_stack)
                 };
