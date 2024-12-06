b43e0d4536205cedb7a06f882b6feb12b79dc2c1
guipublic <guipublic@gmail.com>
2024-12-06 15:55:34 +0000

    add unit test

--- compiler/noirc_evaluator/src/ssa/ir/instruction/call/blackbox.rs
+++ compiler/noirc_evaluator/src/ssa/ir/instruction/call/blackbox.rs
@@ -337,3 +337,93 @@ pub(super) fn simplify_signature(
         _ => SimplifyResult::None,
     }
 }
+
+#[cfg(feature = "bn254")]
+#[cfg(test)]
+mod test {
+    use crate::ssa::opt::assert_normalized_ssa_equals;
+    use crate::ssa::Ssa;
+
+    #[cfg(feature = "bn254")]
+    #[test]
+    fn full_constant_folding() {
+        let src = r#"
+            acir(inline) fn main f0 {
+            b0():
+                v0 = make_array [Field 2, Field 3, Field 5, Field 5] : [Field; 4]
+                v1 = make_array [Field 1, Field 17631683881184975370165255887551781615748388533673675138860, Field 0, Field 1, Field 17631683881184975370165255887551781615748388533673675138860, Field 0] : [Field; 6]
+                v2 = call multi_scalar_mul (v1, v0) -> [Field; 3]
+                return v2
+            }"#;
+        let ssa = Ssa::from_str(src).unwrap();
+
+        let expected_src = r#"
+            acir(inline) fn main f0 {
+            b0():
+                v3 = make_array [Field 2, Field 3, Field 5, Field 5] : [Field; 4]
+                v7 = make_array [Field 1, Field 17631683881184975370165255887551781615748388533673675138860, Field 0, Field 1, Field 17631683881184975370165255887551781615748388533673675138860, Field 0] : [Field; 6]
+                v10 = make_array [Field 1478523918288173385110236399861791147958001875200066088686689589556927843200, Field 700144278551281040379388961242974992655630750193306467120985766322057145630, Field 0] : [Field; 3]
+                return v10
+            }
+            "#;
+        assert_normalized_ssa_equals(ssa, expected_src);
+    }
+
+    #[cfg(feature = "bn254")]
+    #[test]
+    fn simplify_zero() {
+        let src = r#"
+            acir(inline) fn main f0 {
+            b0(v0: Field, v1: Field):
+                v2 = make_array [v0, Field 0, Field 0, Field 0, v0, Field 0] : [Field; 6]
+                v3 = make_array [
+                Field 0, Field 0, Field 1, v0, v1, Field 0, Field 1, v0, Field 0] : [Field; 9]
+                v4 = call multi_scalar_mul (v3, v2) -> [Field; 3]
+
+                return v4
+            
+            }"#;
+        let ssa = Ssa::from_str(src).unwrap();
+        //First point is zero, second scalar is zero, so we should be left with the scalar mul of the last point.
+        let expected_src = r#"
+            acir(inline) fn main f0 {
+            b0(v0: Field, v1: Field):
+                v3 = make_array [v0, Field 0, Field 0, Field 0, v0, Field 0] : [Field; 6]
+                v5 = make_array [Field 0, Field 0, Field 1, v0, v1, Field 0, Field 1, v0, Field 0] : [Field; 9]
+                v6 = make_array [v0, Field 0] : [Field; 2]
+                v7 = make_array [Field 1, v0, Field 0] : [Field; 3]
+                v9 = call multi_scalar_mul(v7, v6) -> [Field; 3]
+                return v9
+            }
+            "#;
+        assert_normalized_ssa_equals(ssa, expected_src);
+    }
+
+    #[cfg(feature = "bn254")]
+    #[test]
+    fn partial_constant_folding() {
+        let src = r#"
+            acir(inline) fn main f0 {
+            b0(v0: Field, v1: Field):
+                v2 = make_array [Field 1, Field 0, v0, Field 0, Field 2, Field 0] : [Field; 6]
+                v3 = make_array [
+                Field 1, Field 17631683881184975370165255887551781615748388533673675138860, Field 0, v0, v1, Field 0, Field 1, Field 17631683881184975370165255887551781615748388533673675138860, Field 0] : [Field; 9]
+                v4 = call multi_scalar_mul (v3, v2) -> [Field; 3]
+                return v4
+            }"#;
+        let ssa = Ssa::from_str(src).unwrap();
+        //First and last scalar/point are constant, so we should be left with the msm of the middle point and the folded constant point
+        let expected_src = r#"
+            acir(inline) fn main f0 {
+            b0(v0: Field, v1: Field):
+                v5 = make_array [Field 1, Field 0, v0, Field 0, Field 2, Field 0] : [Field; 6]
+                v7 = make_array [Field 1, Field 17631683881184975370165255887551781615748388533673675138860, Field 0, v0, v1, Field 0, Field 1, Field 17631683881184975370165255887551781615748388533673675138860, Field 0] : [Field; 9]
+                v8 = make_array [v0, Field 0, Field 1, Field 0] : [Field; 4]
+                v12 = make_array [v0, v1, Field 0, Field -3227352362257037263902424173275354266044964400219754872043023745437788450996, Field 8902249110305491597038405103722863701255802573786510474664632793109847672620, u1 0] : [Field; 6]
+                v14 = call multi_scalar_mul(v12, v8) -> [Field; 3]
+                return v14
+            }
+            "#;
+        assert_normalized_ssa_equals(ssa, expected_src);
+    }
+}
