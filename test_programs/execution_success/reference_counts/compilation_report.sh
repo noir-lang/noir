167f2e766a067dbc90722d1bf370e0541e5174be
Maxim Vezenov <mvezenov@gmail.com>
2024-12-10 05:18:25 +0000

    discern on workspaces

---
+++ test_programs/execution_success/reference_counts/compilation_report.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+set -e
+
+current_dir=$(pwd)
+base_path="$current_dir/execution_success"
+
+# Tests to be profiled for compilation report
+tests_to_profile=("sha256_regression" "regression_4709" "ram_blowup_regression")
+
+# If there is an argument that means we want to re-use an already existing compilation report
+# rather than generating a new one.
+# When reusing a report, the script can only profile one additional test at the moment.
+if [ "$#" -eq 0 ]; then
+  echo "{\"compilation_reports\": [ " > $current_dir/compilation_report.json
+
+else 
+  # Delete last two lines so that we can re-use the previous report 
+  sed -i '$d' $current_dir/compilation_report.json | sed -i '$d' $current_dir/compilation_report.json
+
+  echo "}, " >> $current_dir/compilation_report.json
+
+  # The additional report is expected to be in the current directory
+  base_path="$current_dir"
+  tests_to_profile=(".")
+fi
+
+ITER="1"
+NUM_ARTIFACTS=${#tests_to_profile[@]}
+
+for dir in ${tests_to_profile[@]}; do 
+    if [[ " ${excluded_dirs[@]} " =~ " ${dir} " ]]; then
+      continue
+    fi
+
+    if [[ ${CI-false} = "true" ]] && [[ " ${ci_excluded_dirs[@]} " =~ " ${dir} " ]]; then
+      continue
+    fi
+
+    cd $base_path/$dir
+
+    echo $base_path/$dir
+    echo $(ls ./)
+
+    cat Nargo.toml
+
+    NAME_LINE=$(grep -oE 'name\s*=\s*"([^"]+)"' Nargo.toml)
+    echo $NAME_LINE
+
+    PACKAGE_NAME=$(grep -oE 'name\s*=\s*"([^"]+)"' $base_path/$dir/Nargo.toml | sed 's/name\s*=\s*"//;s/"//')
+    
+    echo $PACKAGE_NAME
+
+    head -n 1 Nargo.toml | grep -q "[workspace]"
+    if [ $? -eq 0 ] && [ "$#" -eq 0 ]; then
+        PACKAGE_NAME=$(basename $current_dir)
+    else 
+        PACKAGE_NAME=$dir
+    fi
+
+    echo $PACKAGE_NAME
+
+    COMPILE_TIME=$((time nargo compile --force) 2>&1 | grep real | grep -oE '[0-9]+m[0-9]+.[0-9]+s')
+    echo -e " {\n    \"artifact_name\":\"$PACKAGE_NAME\",\n    \"time\":\"$COMPILE_TIME\"" >> $current_dir/compilation_report.json
+
+    if (($ITER == $NUM_ARTIFACTS)); then
+        echo "}" >> $current_dir/compilation_report.json
+    else 
+        echo "}, " >> $current_dir/compilation_report.json
+    fi
+
+    ITER=$(( $ITER + 1 ))
+done
+
+echo "]}" >> $current_dir/compilation_report.json
+
