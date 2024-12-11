5a4aa5fae3cb855369f8b6edc4aebc8e9d4d597b
Maxim Vezenov <mvezenov@gmail.com>
2024-12-11 04:46:35 +0000

    move compilation report merging to script and cleanup

--- .github/scripts/merge-bench-reports.sh
+++ .github/scripts/merge-bench-reports.sh
@@ -6,16 +6,13 @@ echo "Merging reports"
 REPORT_NAME=$1
 NAME_PLURAL=""$REPORT_NAME"s"
 
-echo $NAME_PLURAL
-
 combined_reports="[]"
 
 # Iterate over each report and merge them
 for report in ./reports/*; do
-  echo $report
   # The report is saved under ./memory_report_{ matrix_report }/memory_report_{ matrix_report }.json
   FILE_PATH=$(echo $(ls $report)) 
-  echo $FILE_PATH
+
   # Extract the 'memory_reports' array from each report and merge it
   combined_reports=$(jq '[."'"$NAME_PLURAL"'"[]] + '"$combined_reports" <<< "$(cat "$report/$FILE_PATH")")
 done
