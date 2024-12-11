a2d031fa4807391948518d6bec8b51e6d4ee25e1
Maxim Vezenov <mvezenov@gmail.com>
2024-12-11 09:35:20 -0500

    chore(ci): Handle external libraries in compilation timing report (#6750)
    Co-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>

--- test_programs/compilation_report.sh
+++ test_programs/compilation_report.sh
@@ -3,11 +3,17 @@ set -e
 
 current_dir=$(pwd)
 base_path="$current_dir/execution_success"
-test_dirs=$(ls $base_path)
 
 # Tests to be profiled for compilation report
 tests_to_profile=("sha256_regression" "regression_4709" "ram_blowup_regression")
+
 echo "{\"compilation_reports\": [ " > $current_dir/compilation_report.json
+
+# If there is an argument that means we want to generate a report for only the current directory
+if [ "$#" -ne 0 ]; then
+  base_path="$current_dir"
+  tests_to_profile=(".")
+fi
 
 ITER="1"
 NUM_ARTIFACTS=${#tests_to_profile[@]}
@@ -23,9 +29,14 @@ for dir in ${tests_to_profile[@]}; do
 
     cd $base_path/$dir
 
-    COMPILE_TIME=$((time nargo compile --force) 2>&1 | grep real | grep -oE '[0-9]+m[0-9]+.[0-9]+s')
-    echo -e " {\n    \"artifact_name\":\"$dir\",\n    \"time\":\"$COMPILE_TIME\"\n" >> $current_dir/compilation_report.json
+    PACKAGE_NAME=$dir
+    if [ "$#" -ne 0 ]; then
+      PACKAGE_NAME=$(basename $current_dir)
+    fi
 
+    COMPILE_TIME=$((time nargo compile --force --silence-warnings) 2>&1 | grep real | grep -oE '[0-9]+m[0-9]+.[0-9]+s')
+    echo -e " {\n    \"artifact_name\":\"$PACKAGE_NAME\",\n    \"time\":\"$COMPILE_TIME\"" >> $current_dir/compilation_report.json
+    
     if (($ITER == $NUM_ARTIFACTS)); then
         echo "}" >> $current_dir/compilation_report.json
     else 
@@ -36,4 +47,3 @@ for dir in ${tests_to_profile[@]}; do
 done
 
 echo "]}" >> $current_dir/compilation_report.json
-
