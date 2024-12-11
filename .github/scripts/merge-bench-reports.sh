aced17d312215fa36b9358fd19f376977f010e13
Maxim Vezenov <mvezenov@gmail.com>
2024-12-11 04:52:22 +0000

    fix comments

--- .github/scripts/merge-bench-reports.sh
+++ .github/scripts/merge-bench-reports.sh
@@ -10,16 +10,16 @@ combined_reports="[]"
 
 # Iterate over each report and merge them
 for report in ./reports/*; do
-  # The report is saved under ./memory_report_{ matrix_report }/memory_report_{ matrix_report }.json
+  # The report is saved under ./$REPORT_NAME_{ matrix_report }/$REPORT_NAME_{ matrix_report }.json
   FILE_PATH=$(echo $(ls $report)) 
 
-  # Extract the 'memory_reports' array from each report and merge it
+  # Extract the $NAME_PLURAL array from each report and merge it
   combined_reports=$(jq '[."'"$NAME_PLURAL"'"[]] + '"$combined_reports" <<< "$(cat "$report/$FILE_PATH")")
 done
 
 combined_reports=$(jq '[."'$NAME_PLURAL'"[]] + '"$combined_reports" <<< "$(cat ./$REPORT_NAME.json)")
 
-# Wrap the merged memory reports into a new object as to keep the 'memory_reports' key
+# Wrap the merged memory reports into a new object as to keep the $NAME_PLURAL key
 final_report="{\"$NAME_PLURAL\": $combined_reports}"
 
 echo "$final_report" > $REPORT_NAME.json
