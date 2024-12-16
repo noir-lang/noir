fe2e626cd3300f26a7f5fc0ac025123186bd2c88
Ary Borenszweig <asterite@gmail.com>
2024-12-16 13:31:55 -0300

    Reuse function and better output

--- .github/scripts/check_test_results.sh
+++ .github/scripts/check_test_results.sh
@@ -6,22 +6,27 @@ set -eu
 # If any of the files doesn't exist or is empty, the script will consider that the test suite
 # couldn't be compiled.
 
+function process_json_lines() {
+  cat $1 | jq -c 'select(.type == "test" and .event != "started") | {suite: .suite, name: .name, status: .event}' | jq -s -c 'sort_by(.suite, .name) | .[]' > $1.jq
+}
+
 if [ -s $1 ] && [ -s $2 ]; then
   # Both files exist, let's compare them
-  cat $1 | jq -c 'select(.type == "test" and .event != "started") | {suite: .suite, name: .name, status: .event}' | jq -s -c 'sort_by(.suite, .name) | .[]' > $1.jq
-  cat $2 | jq -c 'select(.type == "test" and .event != "started") | {suite: .suite, name: .name, status: .event}' | jq -s -c 'sort_by(.suite, .name) | .[]' > $2.jq
-
+  $(process_json_lines $1)
+  $(process_json_lines $2)
   diff $1.jq $2.jq
 elif [ -s $1 ]; then
   # Only the expected file exists, which means the actual test couldn't be compiled.
-  echo "Error: external library tests couldn't be compiled. You could rename $1 to $1.does_not_compile if it's expected that the external library can't be compiled."
+  echo "Error: external library tests couldn't be compiled."
+  echo "You could rename '$1' to '$1.does_not_compile' if it's expected that the external library can't be compiled."
   exit -1
 elif [ -s $2 ]; then
   # Only the actual file exists, which means we are expecting the external library
   # not to compile but it did.
-  echo "Error: expected external library not to compile, but it did. Please run its tests with 'nargo test --format json' and save that output to $1"
-  echo "Got this output:"
-  cat $2
+  echo "Error: expected external library not to compile, but it did."
+  echo "You could create '$1' with these contents:"
+  $(process_json_lines $2)
+  cat $2.jq
   exit -1
 else
   # Both files don't exists, which means we are expecting the external library not
