9778b24a3d0a23963cdf896e1ca44f139376931a
Tom French <15848336+TomAFrench@users.noreply.github.com>
2024-12-17 13:37:35 +0000

    Update merge-gates-reports.sh

--- .github/scripts/merge-gates-reports.sh
+++ .github/scripts/merge-gates-reports.sh
@@ -8,7 +8,7 @@ for report in ./reports/*; do
   # The report is saved under ./$REPORT_NAME_{ matrix_report }/$REPORT_NAME_{ matrix_report }.json
   FILE_PATH=$(echo $(ls $report)) 
 
-  # Extract the $NAME_PLURAL array from each report and merge it
+  # Extract the programs array from each report and merge it
   combined_reports=$(jq --argjson COMBINED_REPORTS "$combined_reports" '.programs += $COMBINED_REPORTS.programs' <<< "$(cat "$report/$FILE_PATH")")
 done
 
