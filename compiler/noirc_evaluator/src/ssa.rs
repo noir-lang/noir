6858e93ec011d2e8675b70cc78f217ddfecacf45
Tom French <tom@tomfren.ch>
2025-01-09 11:47:25 +0000

    feat: simplify CFG after `resolve_is_unconstrained` SSA pass

--- compiler/noirc_evaluator/src/ssa.rs
+++ compiler/noirc_evaluator/src/ssa.rs
@@ -154,10 +154,11 @@ fn optimize_all(builder: SsaBuilder, options: &SsaEvaluatorOptions) -> Result<Ss
         .run_pass(Ssa::defunctionalize, "Defunctionalization")
         .run_pass(Ssa::remove_paired_rc, "Removing Paired rc_inc & rc_decs")
         .run_pass(Ssa::resolve_is_unconstrained, "Resolving IsUnconstrained")
+        .run_pass(Ssa::simplify_cfg, "Simplifying (1st)")
         .run_pass(|ssa| ssa.inline_functions(options.inliner_aggressiveness), "Inlining (1st)")
         // Run mem2reg with the CFG separated into blocks
         .run_pass(Ssa::mem2reg, "Mem2Reg (1st)")
-        .run_pass(Ssa::simplify_cfg, "Simplifying (1st)")
+        .run_pass(Ssa::simplify_cfg, "Simplifying (2nd)")
         .run_pass(Ssa::as_slice_optimization, "`as_slice` optimization")
         .run_pass(Ssa::remove_unreachable_functions, "Removing Unreachable Functions")
         .try_run_pass(
