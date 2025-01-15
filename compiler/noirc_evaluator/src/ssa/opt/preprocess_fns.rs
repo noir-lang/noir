6943765f15e31583b325b22df5a6020736bf2994
Akosh Farkash <aakoshh@gmail.com>
2025-01-15 13:49:58 +0000

    Simplify a CFG and run DIE as well

--- compiler/noirc_evaluator/src/ssa/opt/preprocess_fns.rs
+++ compiler/noirc_evaluator/src/ssa/opt/preprocess_fns.rs
@@ -9,14 +9,8 @@ impl Ssa {
         aggressiveness: i64,
         max_bytecode_increase_percent: Option<i32>,
     ) -> Ssa {
-        // Ok(self
-        //     .inline_functions_limited(aggressiveness)
-        //     .as_slice_optimization()
-        //     .try_unroll_loops_iteratively(max_bytecode_increase_percent, true)?
-        //     .mem2reg())
-
         // TODO: Ideally we would go bottom-up, starting with the "leaf" functions, so we inline already optimized code into
-        // the ones that call them, but for now just see what happens if we pre-process the "serialize" function.
+        // the ones that call them, but for now just see what happens if we pre-process the "serialize" function instances.
         let to_preprocess = self
             .functions
             .iter()
@@ -35,6 +29,10 @@ impl Ssa {
             let _ = function.try_unroll_loops_iteratively(max_bytecode_increase_percent, true);
             // Reduce the number of redundant stores/loads after unrolling
             function.mem2reg();
+            // Try to reduce the number of blocks.
+            function.simplify_function();
+            // Remove leftover instructions.
+            function.dead_instruction_elimination(true);
             // Put it back into the SSA, so the next functions can pick it up.
             self.functions.insert(id, function);
         }
