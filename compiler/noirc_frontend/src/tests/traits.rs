aa7fea8ea81897ecbbe601655000eca60222a251
Michael Klein <lambdamichael@gmail.com>
2025-01-16 11:13:22 -0500

    move compile-success test to a frontend test

--- compiler/noirc_frontend/src/tests/traits.rs
+++ compiler/noirc_frontend/src/tests/traits.rs
@@ -1236,3 +1236,47 @@ fn warns_if_trait_is_not_in_scope_for_generic_function_call_and_there_is_only_on
     assert_eq!(ident.to_string(), "foo");
     assert_eq!(trait_name, "private_mod::Foo");
 }
+
+#[test]
+fn regression_6530() {
+    let src = r#"
+    pub trait From<T> {
+        fn from(input: T) -> Self;
+    }
+    
+    pub trait Into<T> {
+        fn into(self) -> T;
+    }
+    
+    impl<T, U> Into<T> for U
+    where
+        T: From<U>,
+    {
+        fn into(self) -> T {
+            T::from(self)
+        }
+    }
+    
+    struct Foo {
+        inner: Field,
+    }
+    
+    impl Into<Field> for Foo {
+        fn into(self) -> Field {
+            self.inner
+        }
+    }
+    
+    fn main() {
+        let foo = Foo { inner: 0 };
+    
+        // This works:
+        let _: Field = Into::<Field>::into(foo);
+    
+        // This was failing with 'No matching impl':
+        let _: Field = foo.into();
+    }
+    "#;
+    let errors = get_program_errors(src);
+    assert_eq!(errors.len(), 0);
+}
