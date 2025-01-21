9118df1c7d584736e2d60e378edde6a80c093d23
github-action-benchmark <github@users.noreply.github.com>
2025-01-21 15:20:58 +0000

    add Execution Memory (customSmallerIsBetter) benchmark result for 521f5ce5721181e0b686887bdb47d3be1a8501a8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737472853983,
+  "lastUpdate": 1737472858173,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -6971,6 +6971,95 @@ window.BENCHMARK_DATA = {
           {
             "name": "workspace",
             "value": 123.62,
+            "unit": "MB"
+          },
+          {
+            "name": "regression_4709",
+            "value": 315.98,
+            "unit": "MB"
+          },
+          {
+            "name": "ram_blowup_regression",
+            "value": 512.6,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-root",
+            "value": 498.92,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-merge",
+            "value": 473.61,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-root",
+            "value": 1230,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-merge",
+            "value": 498.93,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-base-public",
+            "value": 730.78,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-base-private",
+            "value": 591.18,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-tail",
+            "value": 180.99,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-reset",
+            "value": 245.6,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-inner",
+            "value": 207.99,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "15848336+TomAFrench@users.noreply.github.com",
+            "name": "Tom French",
+            "username": "TomAFrench"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "521f5ce5721181e0b686887bdb47d3be1a8501a8",
+          "message": "fix: avoid creating unnecessary memory blocks (#7114)",
+          "timestamp": "2025-01-21T15:01:38Z",
+          "tree_id": "d89ea45c4fc69fa98cf4ddc246ba24a5ee3013dd",
+          "url": "https://github.com/noir-lang/noir/commit/521f5ce5721181e0b686887bdb47d3be1a8501a8"
+        },
+        "date": 1737472856985,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "keccak256",
+            "value": 74.69,
+            "unit": "MB"
+          },
+          {
+            "name": "workspace",
+            "value": 123.84,
             "unit": "MB"
           },
           {
