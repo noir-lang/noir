d3aa2ae2eeb5749ca2807e9e1de1c0a9cd09ee90
github-action-benchmark <github@users.noreply.github.com>
2025-01-23 20:11:04 +0000

    add Compilation Memory (customSmallerIsBetter) benchmark result for c17e228f98e5714fb3a2b56ff4c0a2aa469e77c4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737663063028,
+  "lastUpdate": 1737663064952,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -4026,6 +4026,105 @@ window.BENCHMARK_DATA = {
           {
             "name": "private-kernel-inner",
             "value": 270.11,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "asterite@gmail.com",
+            "name": "Ary Borenszweig",
+            "username": "asterite"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "c17e228f98e5714fb3a2b56ff4c0a2aa469e77c4",
+          "message": "feat: inline simple functions (#7160)\n\nCo-authored-by: Maxim Vezenov <mvezenov@gmail.com>",
+          "timestamp": "2025-01-23T19:45:12Z",
+          "tree_id": "17abbd06d6427b547d80629e8f1031c81a3d5f12",
+          "url": "https://github.com/noir-lang/noir/commit/c17e228f98e5714fb3a2b56ff4c0a2aa469e77c4"
+        },
+        "date": 1737663064049,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "keccak256",
+            "value": 77.72,
+            "unit": "MB"
+          },
+          {
+            "name": "workspace",
+            "value": 124.12,
+            "unit": "MB"
+          },
+          {
+            "name": "regression_4709",
+            "value": 424.87,
+            "unit": "MB"
+          },
+          {
+            "name": "ram_blowup_regression",
+            "value": 1480,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-root",
+            "value": 613.98,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-merge",
+            "value": 496.57,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-root-single-tx",
+            "value": 5760,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-root-empty",
+            "value": 500.59,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-root",
+            "value": 5770,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-merge",
+            "value": 613.98,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-base-public",
+            "value": 2160,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-base-private",
+            "value": 1130,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-tail",
+            "value": 199.58,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-reset",
+            "value": 540.8,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-inner",
+            "value": 270.19,
             "unit": "MB"
           }
         ]
