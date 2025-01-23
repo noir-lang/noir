edde1a2657f65e27a0ca70b18b79af5fdcf94b50
Ary Borenszweig <asterite@gmail.com>
2025-01-23 10:34:41 -0300

    Try to first see how it changes without the "called only once" condition

--- compiler/noirc_evaluator/src/ssa/opt/inlining.rs
+++ compiler/noirc_evaluator/src/ssa/opt/inlining.rs
@@ -91,17 +91,9 @@ impl Ssa {
     }
 
     pub(crate) fn inline_simple_functions(mut self: Ssa) -> Ssa {
-        let callers = compute_callers(&self);
-        let times_called = compute_times_called(&callers);
-
         let should_inline_call = |function: &Function| {
             let entry_block_id = function.entry_block();
             let entry_block = &function.dfg[entry_block_id];
-
-            // If a function is only called once, inline it
-            if times_called.get(&function.id()) == Some(&1) {
-                return true;
-            }
 
             // Only inline functions with a single block
             if entry_block.successors().next().is_some() {
