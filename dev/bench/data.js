c24343ff9dde0d79373d42d0b7158c23ea87a3a9
github-action-benchmark <github@users.noreply.github.com>
2025-01-23 21:08:36 +0000

    add Execution Memory (customSmallerIsBetter) benchmark result for ff55a77b809cf40f747d79048639a8c544d733b5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737666508731,
+  "lastUpdate": 1737666516945,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -14281,6 +14281,95 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/noir-lang/noir/commit/c17e228f98e5714fb3a2b56ff4c0a2aa469e77c4"
         },
         "date": 1737663062109,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "keccak256",
+            "value": 74.87,
+            "unit": "MB"
+          },
+          {
+            "name": "workspace",
+            "value": 124.11,
+            "unit": "MB"
+          },
+          {
+            "name": "regression_4709",
+            "value": 316.15,
+            "unit": "MB"
+          },
+          {
+            "name": "ram_blowup_regression",
+            "value": 512.7,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-root",
+            "value": 516.95,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-merge",
+            "value": 491.62,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-root",
+            "value": 1250,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-merge",
+            "value": 516.96,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-base-public",
+            "value": 748.3,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-base-private",
+            "value": 609.72,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-tail",
+            "value": 181.66,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-reset",
+            "value": 246.27,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-inner",
+            "value": 209.75,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jc@joshcrites.com",
+            "name": "josh crites",
+            "username": "critesjosh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ff55a77b809cf40f747d79048639a8c544d733b5",
+          "message": "fix(docs): Update broken links to EC lib (#7141)",
+          "timestamp": "2025-01-23T20:45:01Z",
+          "tree_id": "b95dcc33b2e44b72acceac9d0dfb66d41c4c86eb",
+          "url": "https://github.com/noir-lang/noir/commit/ff55a77b809cf40f747d79048639a8c544d733b5"
+        },
+        "date": 1737666516223,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
