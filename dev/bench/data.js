4594b8fef4b2cf966dbba1f44ff27820400f9521
github-action-benchmark <github@users.noreply.github.com>
2025-01-31 16:29:21 +0000

    add Compilation Time (customSmallerIsBetter) benchmark result for 0c6c6371c6d0a19be3f4347678fab52f6c91f79a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738340956303,
+  "lastUpdate": 1738340961669,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -5038,100 +5038,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "15848336+TomAFrench@users.noreply.github.com",
-            "name": "Tom French",
-            "username": "TomAFrench"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "521f5ce5721181e0b686887bdb47d3be1a8501a8",
-          "message": "fix: avoid creating unnecessary memory blocks (#7114)",
-          "timestamp": "2025-01-21T15:01:38Z",
-          "tree_id": "d89ea45c4fc69fa98cf4ddc246ba24a5ee3013dd",
-          "url": "https://github.com/noir-lang/noir/commit/521f5ce5721181e0b686887bdb47d3be1a8501a8"
-        },
-        "date": 1737472676079,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "sha256_regression",
-            "value": 0.977,
-            "unit": "s"
-          },
-          {
-            "name": "regression_4709",
-            "value": 0.806,
-            "unit": "s"
-          },
-          {
-            "name": "ram_blowup_regression",
-            "value": 19.6,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-root",
-            "value": 3.61,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-merge",
-            "value": 2.102,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-block-root-single-tx",
-            "value": 85,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-block-root-empty",
-            "value": 2.052,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-block-root",
-            "value": 85.4,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-block-merge",
-            "value": 3.534,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-base-public",
-            "value": 29.74,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-base-private",
-            "value": 10.36,
-            "unit": "s"
-          },
-          {
-            "name": "private-kernel-tail",
-            "value": 0.967,
-            "unit": "s"
-          },
-          {
-            "name": "private-kernel-reset",
-            "value": 6.838,
-            "unit": "s"
-          },
-          {
-            "name": "private-kernel-inner",
-            "value": 2.166,
-            "unit": "s"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "asterite@gmail.com",
             "name": "Ary Borenszweig",
             "username": "asterite"
@@ -9811,6 +9717,105 @@ window.BENCHMARK_DATA = {
           {
             "name": "private-kernel-inner",
             "value": 1.954,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jake@aztecprotocol.com",
+            "name": "jfecher",
+            "username": "jfecher"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "0c6c6371c6d0a19be3f4347678fab52f6c91f79a",
+          "message": "feat(experimental): Implement enum tag constants (#7183)\n\nCo-authored-by: Ary Borenszweig <asterite@gmail.com>",
+          "timestamp": "2025-01-31T16:07:47Z",
+          "tree_id": "01981f622b8d6472081c9cd3aa47b379af2402ef",
+          "url": "https://github.com/noir-lang/noir/commit/0c6c6371c6d0a19be3f4347678fab52f6c91f79a"
+        },
+        "date": 1738340960814,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sha256_regression",
+            "value": 1.04,
+            "unit": "s"
+          },
+          {
+            "name": "regression_4709",
+            "value": 0.837,
+            "unit": "s"
+          },
+          {
+            "name": "ram_blowup_regression",
+            "value": 20.1,
+            "unit": "s"
+          },
+          {
+            "name": "global_var_regression_entry_points",
+            "value": 0.576,
+            "unit": "s"
+          },
+          {
+            "name": "rollup-root",
+            "value": 3.616,
+            "unit": "s"
+          },
+          {
+            "name": "rollup-merge",
+            "value": 2.038,
+            "unit": "s"
+          },
+          {
+            "name": "rollup-block-root-single-tx",
+            "value": 68,
+            "unit": "s"
+          },
+          {
+            "name": "rollup-block-root-empty",
+            "value": 2.142,
+            "unit": "s"
+          },
+          {
+            "name": "rollup-block-root",
+            "value": 72.5,
+            "unit": "s"
+          },
+          {
+            "name": "rollup-block-merge",
+            "value": 3.57,
+            "unit": "s"
+          },
+          {
+            "name": "rollup-base-public",
+            "value": 25.3,
+            "unit": "s"
+          },
+          {
+            "name": "rollup-base-private",
+            "value": 10.286,
+            "unit": "s"
+          },
+          {
+            "name": "private-kernel-tail",
+            "value": 0.978,
+            "unit": "s"
+          },
+          {
+            "name": "private-kernel-reset",
+            "value": 6.486,
+            "unit": "s"
+          },
+          {
+            "name": "private-kernel-inner",
+            "value": 2.006,
             "unit": "s"
           }
         ]
