1ae035fd0719b63f632e1225d9f240ef074b3705
Maxim Vezenov <mvezenov@gmail.com>
2025-02-03 07:41:37 -0500

    fix(ssa): Only attempt to inline constant Brillig calls for entry points (#7260)

--- compiler/noirc_evaluator/src/brillig/brillig_gen.rs
+++ compiler/noirc_evaluator/src/brillig/brillig_gen.rs
@@ -58,7 +58,11 @@ pub(crate) fn gen_brillig_for(
     brillig: &Brillig,
 ) -> Result<GeneratedBrillig<FieldElement>, InternalError> {
     // Create the entry point artifact
-    let globals_memory_size = brillig.globals_memory_size.get(&func.id()).copied().unwrap_or(0);
+    let globals_memory_size = brillig
+        .globals_memory_size
+        .get(&func.id())
+        .copied()
+        .expect("Should have the globals memory size specified for an entry point");
     let mut entry_point = BrilligContext::new_entry_point_artifact(
         arguments,
         FunctionContext::return_values(func),
