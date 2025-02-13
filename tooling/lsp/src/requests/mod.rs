81b86e2a9bfe991bc0385118094656648a125587
Ary Borenszweig <asterite@gmail.com>
2025-02-13 10:07:41 -0300

    fix: let LSP read `noirfmt.toml` for formatting files (#7355)

--- tooling/lsp/src/requests/mod.rs
+++ tooling/lsp/src/requests/mod.rs
@@ -1,5 +1,5 @@
 use std::collections::BTreeMap;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::{collections::HashMap, future::Future};
 
 use crate::{insert_all_files_for_workspace_into_file_manager, parse_diff, PackageCacheData};
@@ -301,6 +301,10 @@ fn on_formatting_inner(
     state: &LspState,
     params: lsp_types::DocumentFormattingParams,
 ) -> Result<Option<Vec<lsp_types::TextEdit>>, ResponseError> {
+    // The file_path might be Err/None if the action runs against an unsaved file
+    let file_path = params.text_document.uri.to_file_path().ok();
+    let directory_path = file_path.as_ref().and_then(|path| path.parent());
+
     let path = params.text_document.uri.to_string();
 
     if let Some(source) = state.input_files.get(&path) {
@@ -310,7 +314,8 @@ fn on_formatting_inner(
             return Ok(None);
         }
 
-        let new_text = nargo_fmt::format(source, module, &Config::default());
+        let config = read_format_config(directory_path);
+        let new_text = nargo_fmt::format(source, module, &config);
 
         let start_position = Position { line: 0, character: 0 };
         let end_position = Position {
@@ -324,6 +329,19 @@ fn on_formatting_inner(
         }]))
     } else {
         Ok(None)
+    }
+}
+
+fn read_format_config(file_path: Option<&Path>) -> Config {
+    match file_path {
+        Some(file_path) => match Config::read(file_path) {
+            Ok(config) => config,
+            Err(err) => {
+                eprintln!("{}", err);
+                Config::default()
+            }
+        },
+        None => Config::default(),
     }
 }
 
