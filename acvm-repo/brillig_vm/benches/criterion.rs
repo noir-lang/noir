ffb37867c9d00a7da74bed10a1497d331e9de269
Tom French <tom@tomfren.ch>
2025-02-14 18:57:13 +0000

    chore: add benchmark for field byte decomposition

--- acvm-repo/brillig_vm/benches/criterion.rs
+++ acvm-repo/brillig_vm/benches/criterion.rs
@@ -1,61 +1,71 @@
-use acir::{brillig, AcirField, FieldElement};
+use acir::{
+    brillig::{self, MemoryAddress},
+    AcirField, FieldElement,
+};
+use brillig_vm::VMStatus;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 use pprof::criterion::{Output, PProfProfiler};
+use rand::Rng;
 
 use std::time::Duration;
 
-fn field_addition(c: &mut Criterion) {
+fn byte_decomposition(c: &mut Criterion) {
     let mut bytecode = vec![
+        // Radix
         brillig::Opcode::Const {
-            destination: brillig::MemoryAddress::Direct(0),
-            bit_size: brillig::BitSize::Field,
+            destination: MemoryAddress::Direct(2),
+            bit_size: brillig::BitSize::Integer(brillig::IntegerBitSize::U32),
+            value: FieldElement::from(2u128),
+        },
+        // Output pointer
+        brillig::Opcode::Const {
+            destination: MemoryAddress::Direct(3),
+            bit_size: brillig::BitSize::Integer(brillig::IntegerBitSize::U32),
+            value: FieldElement::from(5u128),
+        },
+        // num_limbs
+        brillig::Opcode::Const {
+            destination: MemoryAddress::Direct(4),
+            bit_size: brillig::BitSize::Integer(brillig::IntegerBitSize::U32),
+            value: FieldElement::from(32u128),
+        },
+        // output_bits
+        brillig::Opcode::Const {
+            destination: MemoryAddress::Direct(5),
+            bit_size: brillig::BitSize::Integer(brillig::IntegerBitSize::U1),
+            value: FieldElement::from(false),
+        },
+        // calldata offset
+        brillig::Opcode::Const {
+            destination: MemoryAddress::Direct(0),
+            bit_size: brillig::BitSize::Integer(brillig::IntegerBitSize::U32),
+            value: FieldElement::from(0u128),
+        },
+        // calldata size
+        brillig::Opcode::Const {
+            destination: MemoryAddress::Direct(1),
+            bit_size: brillig::BitSize::Integer(brillig::IntegerBitSize::U32),
             value: FieldElement::from(1u128),
         },
-        brillig::Opcode::Const {
-            destination: brillig::MemoryAddress::Direct(1),
-            bit_size: brillig::BitSize::Field,
-            value: FieldElement::from(2u128),
+        brillig::Opcode::CalldataCopy {
+            destination_address: MemoryAddress::Direct(0),
+            size_address: MemoryAddress::Direct(1),
+            offset_address: MemoryAddress::Direct(0),
         },
     ];
 
-    for _ in 0..100 {
-        bytecode.push(brillig::Opcode::BinaryFieldOp {
-            destination: brillig::MemoryAddress::Direct(0),
-            op: brillig::BinaryFieldOp::Add,
-            lhs: brillig::MemoryAddress::Direct(0),
-            rhs: brillig::MemoryAddress::Direct(1),
-        });
+    for _ in 0..1 {
+        bytecode.push(brillig::Opcode::BlackBox(brillig::BlackBoxOp::ToRadix {
+            input: MemoryAddress::Direct(0),
+            radix: MemoryAddress::Direct(2),
+            output_pointer: MemoryAddress::Direct(3),
+            num_limbs: MemoryAddress::Direct(4),
+            output_bits: MemoryAddress::Direct(5),
+        }));
     }
 
-    bench_bytecode(c, "field_addition", &bytecode);
-}
-
-fn byte_addition(c: &mut Criterion) {
-    let mut bytecode = vec![
-        brillig::Opcode::Const {
-            destination: brillig::MemoryAddress::Direct(0),
-            bit_size: brillig::BitSize::Field,
-            value: FieldElement::from(1u128),
-        },
-        brillig::Opcode::Const {
-            destination: brillig::MemoryAddress::Direct(1),
-            bit_size: brillig::BitSize::Field,
-            value: FieldElement::from(2u128),
-        },
-    ];
-
-    for _ in 0..100 {
-        bytecode.push(brillig::Opcode::BinaryIntOp {
-            destination: brillig::MemoryAddress::Direct(2),
-            op: brillig::BinaryIntOp::Add,
-            bit_size: brillig::IntegerBitSize::U8,
-            lhs: brillig::MemoryAddress::Direct(0),
-            rhs: brillig::MemoryAddress::Direct(1),
-        });
-    }
-
-    bench_bytecode(c, "byte_addition", &bytecode);
+    bench_bytecode(c, "byte_decomposition", &bytecode);
 }
 
 fn bench_bytecode<F: AcirField>(
@@ -66,16 +76,25 @@ fn bench_bytecode<F: AcirField>(
     c.bench_function(benchmark_name, |b| {
         b.iter_batched(
             || {
-                let vm = brillig_vm::VM::new(
-                    Vec::new(),
+                let input = rand::thread_rng().gen::<u128>();
+                let input_field = F::from(input);
+                let mut vm = brillig_vm::VM::new(
+                    vec![input_field],
                     bytecode,
-                    Vec::new(),
                     &acvm_blackbox_solver::StubbedBlackBoxSolver(false),
                     false,
                 );
+
+                // Process up to the last opcode which we want to benchmark
+                for _ in 0..bytecode.len() - 1 {
+                    vm.process_opcode();
+                }
                 vm
             },
-            |mut vm| vm.process_opcodes(),
+            |mut vm| {
+                let status = vm.process_opcodes();
+                assert!(matches!(status, VMStatus::Finished { .. }))
+            },
             criterion::BatchSize::SmallInput,
         );
     });
@@ -84,6 +103,6 @@ fn bench_bytecode<F: AcirField>(
 criterion_group! {
     name = execution_benches;
     config = Criterion::default().sample_size(20).measurement_time(Duration::from_secs(20)).with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
-    targets = field_addition, byte_addition
+    targets = byte_decomposition
 }
 criterion_main!(execution_benches);
