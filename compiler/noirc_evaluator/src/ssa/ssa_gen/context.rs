ec7194bb0706ae252ab4d7a4fd741d84dcb1ab48
Ary Borenszweig <asterite@gmail.com>
2025-02-17 15:38:13 -0300

    Remove extra comments

--- compiler/noirc_evaluator/src/ssa/ssa_gen/context.rs
+++ compiler/noirc_evaluator/src/ssa/ssa_gen/context.rs
@@ -309,7 +309,6 @@ impl<'a> FunctionContext<'a> {
             match numeric_type {
                 NumericType::NativeField => -value,
                 NumericType::Signed { bit_size } | NumericType::Unsigned { bit_size } => {
-                    // The math here is a bit convoluted to avoid overflowing for u128
                     let base = if bit_size == 128 { u128::MAX } else { 2_u128.pow(bit_size) - 1 };
                     FieldElement::from(base) - value
                 }
