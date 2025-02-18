18e17e79ae58e2a5b97d1d184ce1c621c3acc8c5
Ary Borenszweig <asterite@gmail.com>
2025-02-18 07:53:11 -0300

    Add a few more tests

--- tooling/lsp/src/requests/hover/from_visitor.rs
+++ tooling/lsp/src/requests/hover/from_visitor.rs
@@ -4,7 +4,7 @@ use acvm::FieldElement;
 use fm::{FileId, FileMap};
 use lsp_types::{Hover, HoverContents, MarkupContent, MarkupKind, Position};
 use noirc_errors::{Location, Span};
-use noirc_frontend::{ast::Visitor, node_interner::NodeInterner, parse_program};
+use noirc_frontend::{ast::Visitor, node_interner::NodeInterner, parse_program, Type};
 use num_bigint::BigInt;
 
 use crate::{
@@ -63,16 +63,56 @@ impl<'a> Visitor for HoverFinder<'a> {
             return;
         };
 
-        let value_base_10 = value.to_string();
-
-        // For simplicity we parse the value as a BigInt to convert it to hex
-        // because `FieldElement::to_hex` will include many leading zeros.
-        let value_big_int = BigInt::from_str(&value_base_10).unwrap();
-        let negative = if negative { "-" } else { "" };
-
-        let value =
-            format!("    {typ}\n---\nvalue of literal: `{negative}{value_base_10} ({negative}0x{value_big_int:02x})`");
+        let value = format_integer(typ, value, negative);
         let contents = HoverContents::Markup(MarkupContent { kind: MarkupKind::Markdown, value });
         self.hover = Some(Hover { contents, range });
+    }
+}
+
+fn format_integer(typ: Type, value: FieldElement, negative: bool) -> String {
+    let value_base_10 = value.to_string();
+
+    // For simplicity we parse the value as a BigInt to convert it to hex
+    // because `FieldElement::to_hex` will include many leading zeros.
+    let value_big_int = BigInt::from_str(&value_base_10).unwrap();
+    let negative = if negative { "-" } else { "" };
+
+    format!("    {typ}\n---\nvalue of literal: `{negative}{value_base_10} ({negative}0x{value_big_int:02x})`")
+}
+
+#[cfg(test)]
+mod tests {
+    use noirc_frontend::{
+        ast::{IntegerBitSize, Signedness},
+        Type,
+    };
+
+    use super::format_integer;
+
+    #[test]
+    fn format_integer_zero() {
+        let typ = Type::FieldElement;
+        let value = 0_u128.into();
+        let negative = false;
+        let expected = "    Field\n---\nvalue of literal: `0 (0x00)`";
+        assert_eq!(format_integer(typ, value, negative), expected);
+    }
+
+    #[test]
+    fn format_positive_integer() {
+        let typ = Type::Integer(Signedness::Unsigned, IntegerBitSize::ThirtyTwo);
+        let value = 123456_u128.into();
+        let negative = false;
+        let expected = "    u32\n---\nvalue of literal: `123456 (0x1e240)`";
+        assert_eq!(format_integer(typ, value, negative), expected);
+    }
+
+    #[test]
+    fn format_negative_integer() {
+        let typ = Type::Integer(Signedness::Signed, IntegerBitSize::SixtyFour);
+        let value = 987654_u128.into();
+        let negative = true;
+        let expected = "    i64\n---\nvalue of literal: `-987654 (-0xf1206)`";
+        assert_eq!(format_integer(typ, value, negative), expected);
     }
 }
