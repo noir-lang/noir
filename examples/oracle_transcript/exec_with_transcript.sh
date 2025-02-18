0afd653742e6d8b61c269ee1f483dd2685261928
Akosh Farkash <aakoshh@gmail.com>
2025-02-18 14:08:51 +0000

    Add example and capture oracle transcript

--- examples/oracle_transcript/exec_with_transcript.sh
+++ examples/oracle_transcript/exec_with_transcript.sh
@@ -3,4 +3,14 @@ set -eu
 
 dir=$(dirname $0)
 
-nargo --program-dir $dir test
+# Execute the test to capture oracle calls.
+NARGO_TEST_FOREIGN_CALL_LOG=$dir/Oracle.test.jsonl \
+    nargo --program-dir $dir test
+
+# Get rid of the mock setup calls
+cat $dir/Oracle.test.jsonl \
+    | jq --slurp -r -c '.[] | select(.call.function | contains("mock") | not)' \
+    > $dir/Oracle.jsonl
+
+# TODO: Execute `main` with the Prover.toml and Oracle.jsonl files.
+nargo execute --skip-underconstrained-check
