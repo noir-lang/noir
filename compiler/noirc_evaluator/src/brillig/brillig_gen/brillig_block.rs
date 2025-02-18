cf0ee3da9457e97bb96ff53112ff16a137407000
Maxim Vezenov <mvezenov@gmail.com>
2025-02-18 23:15:43 +0000

    cargo fmt

--- compiler/noirc_evaluator/src/brillig/brillig_gen/brillig_block.rs
+++ compiler/noirc_evaluator/src/brillig/brillig_gen/brillig_block.rs
@@ -768,7 +768,9 @@ impl<'block, Registers: RegisterAllocator> BrilligBlock<'block, Registers> {
                 let index_variable = self.convert_ssa_single_addr_value(*index, dfg);
 
                 // Slice access checks are generated separately against the slice's dynamic length field.
-                if !dfg.is_safe_index(*index, *array) && matches!(dfg.type_of_value(*array), Type::Array(..))  {
+                if !dfg.is_safe_index(*index, *array)
+                    && matches!(dfg.type_of_value(*array), Type::Array(..))
+                {
                     self.validate_array_index(array_variable, index_variable);
                 }
 
@@ -797,7 +799,9 @@ impl<'block, Registers: RegisterAllocator> BrilligBlock<'block, Registers> {
                 );
 
                 // Slice access checks are generated separately against the slice's dynamic length field.
-                if !dfg.is_safe_index(*index, *array) && matches!(dfg.type_of_value(*array), Type::Array(..)) {
+                if !dfg.is_safe_index(*index, *array)
+                    && matches!(dfg.type_of_value(*array), Type::Array(..))
+                {
                     self.validate_array_index(source_variable, index_register);
                 }
 
