4a2286094841d3ffa8862028744ea73f4e4c4d27
Maxim Vezenov <mvezenov@gmail.com>
2025-02-18 22:57:34 +0000

    restore tests for debugging

--- compiler/noirc_evaluator/src/brillig/brillig_gen/brillig_block.rs
+++ compiler/noirc_evaluator/src/brillig/brillig_gen/brillig_block.rs
@@ -767,7 +767,8 @@ impl<'block, Registers: RegisterAllocator> BrilligBlock<'block, Registers> {
 
                 let index_variable = self.convert_ssa_single_addr_value(*index, dfg);
 
-                if !dfg.is_safe_index(*index, *array) {
+                // Slice validation should be generated separately against its dynamic lengthl
+                if !dfg.is_safe_brillig_index(*index, *array) && matches!(dfg.type_of_value(*array), Type::Array(..)) {
                     self.validate_array_index(array_variable, index_variable);
                 }
 
--- compiler/noirc_evaluator/src/ssa/opt/brillig_array_gets.rs
+++ compiler/noirc_evaluator/src/ssa/opt/brillig_array_gets.rs
@@ -1,13 +1,19 @@
-use fxhash::FxHashMap;
+//! In the Brillig runtime arrays are represented as [RC, ...items],
+//! Certain operations such as array gets only utilize the items pointer.
+//! Without handling the items pointer offset in SSA, it is left to Brillig generation
+//! to offset the array pointer.
+//! Slices are represented in a similar manner, except the items pointer starts at three rather than one.
+//!
+//! For array operations with constant indices adding an instruction to offset the pointer
+//! is unnecessary as we already know the index. This pass looks for such array operations
+//! with constant indices and replaces their index with the appropriate offset.
+//!
+use fxhash::FxHashMap as HashMap;
 
 use crate::{
     brillig::brillig_ir::BRILLIG_MEMORY_ADDRESSING_BIT_SIZE,
     ssa::{
-        ir::{
-            function::Function,
-            instruction::{BinaryOp, Instruction},
-            types::NumericType,
-        },
+        ir::{function::Function, instruction::Instruction, types::{NumericType, Type}},
         Ssa,
     },
 };
@@ -15,7 +21,9 @@ use crate::{
 impl Ssa {
     #[tracing::instrument(level = "trace", skip(self))]
     pub(crate) fn brillig_array_gets(mut self) -> Ssa {
-        for function in self.functions.values_mut() {
+        let brillig_functions =
+            self.functions.values_mut().filter(|function| function.runtime().is_brillig());
+        for function in brillig_functions {
             function.brillig_array_gets();
         }
 
@@ -27,7 +35,7 @@ impl Function {
     pub(super) fn brillig_array_gets(&mut self) {
         let reachable_blocks = self.reachable_blocks();
 
-        let mut instructions_to_update = FxHashMap::default();
+        let mut instructions_to_update = HashMap::default();
         for block_id in reachable_blocks.into_iter() {
             for instruction_id in self.dfg[block_id].instructions().to_vec() {
                 match self.dfg[instruction_id] {
@@ -49,8 +57,14 @@ impl Function {
                 Instruction::ArrayGet { array, index } => {
                     let index_constant =
                         self.dfg.get_numeric_constant(index).expect("ICE: Expected constant index");
+                    let offset = if matches!(self.dfg.type_of_value(array), Type::Array(..)) {
+                        1u128
+                    } else {
+                        3u128
+                        // continue;
+                    };
                     let index = self.dfg.make_constant(
-                        index_constant + 1u128.into(),
+                        index_constant + offset.into(),
                         NumericType::unsigned(BRILLIG_MEMORY_ADDRESSING_BIT_SIZE),
                     );
                     Instruction::ArrayGet { array, index }
@@ -61,5 +75,66 @@ impl Function {
             };
             self.dfg[instruction_id] = new_instruction;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ssa::opt::assert_normalized_ssa_equals;
+
+    use super::Ssa;
+
+    #[test]
+    fn offset_array_get_constant_index() {
+        let src = "
+        brillig(inline) fn main f0 {
+          b0(v0: [Field; 3]):
+            v2 = array_get v0, index u32 0 -> Field
+            return v2
+        }
+        ";
+
+        let ssa = Ssa::from_str(src).unwrap();
+        let ssa = ssa.brillig_array_gets();
+
+        let expected = "
+        brillig(inline) fn main f0 {
+          b0(v0: [Field; 3]):
+            v2 = array_get v0, index u32 1 -> Field
+            return v2
+        }
+        ";
+
+        assert_normalized_ssa_equals(ssa, expected);
+    }
+
+    #[test]
+    fn do_not_offset_dynamic_array_get() {
+        let src = "
+        brillig(inline) fn main f0 {
+          b0(v0: [Field; 3], v1: u32):
+            v2 = array_get v0, v1 -> Field
+            return v2
+        }
+        ";
+
+        let ssa = Ssa::from_str(src).unwrap();
+        let ssa = ssa.brillig_array_gets();
+        assert_normalized_ssa_equals(ssa, src);
+    }
+
+    #[test]
+    fn do_not_offset_array_get_in_acir() {
+        let src = "
+        acir(inline) fn main f0 {
+          b0(v0: [Field; 3]):
+            v2 = array_get v0, index u32 0 -> Field
+            return v2
+        }
+        ";
+
+        let ssa = Ssa::from_str(src).unwrap();
+        let ssa = ssa.brillig_array_gets();
+        assert_normalized_ssa_equals(ssa, src);
     }
 }
