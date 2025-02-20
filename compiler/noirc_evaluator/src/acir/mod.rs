e6cc0cb8709ca35c1d1d5d5ab0a621d1abf5514e
Tom French <tom@tomfren.ch>
2025-02-20 14:29:45 +0000

    fix: prevent incorrect ACIRgen caused by noop truncations

--- compiler/noirc_evaluator/src/acir/mod.rs
+++ compiler/noirc_evaluator/src/acir/mod.rs
@@ -2094,6 +2094,8 @@ impl<'a> Context<'a> {
         max_bit_size: u32,
         dfg: &DataFlowGraph,
     ) -> Result<AcirVar, RuntimeError> {
+        assert_ne!(bit_size, max_bit_size, "Attempted to generate a noop truncation");
+
         let mut var = self.convert_numeric_value(value_id, dfg)?;
         match &dfg[value_id] {
             Value::Instruction { instruction, .. } => {
