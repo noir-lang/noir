095c4ea26311ee93fc94b2fcc8f9d663bf52031d
Stan Manilov <stan.manilov@metacraft-labs.com>
2025-02-20 10:40:40 +0200

    fix(build): cargo test succeeds again
    This was a slight regression due to
    https://github.com/blocksense-network/noir/pull/194.
    
    (cherry picked from commit 1b02f5b9415dc190727b989b0a0cbc6d1bdc5952)

--- tooling/nargo_cli/build.rs
+++ tooling/nargo_cli/build.rs
@@ -605,8 +605,9 @@ fn trace_{test_name}() {{
     cmd.arg("--program-dir").arg(test_program_dir_path.to_str().unwrap());
     cmd.arg("trace").arg("--trace-dir").arg(temp_dir.path());
 
+    let trace_dir_path = temp_dir.path().as_os_str().to_str().unwrap();
     let trace_file_path = temp_dir.path().join("trace.json");
-    let file_written_message = format!("Saved trace to {{:?}}", trace_file_path);
+    let file_written_message = format!("Saved trace to {{}}", trace_dir_path);
 
     cmd.assert().success().stdout(predicate::str::contains(file_written_message));
 
