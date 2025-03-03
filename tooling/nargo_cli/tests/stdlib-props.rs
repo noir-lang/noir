ef05f26e39a591ffb9609ac6a9bef043923c449d
Tom French <tom@tomfren.ch>
2025-03-03 19:48:58 +0000

    ackually it's spelt pedantic

--- tooling/nargo_cli/tests/stdlib-props.rs
+++ tooling/nargo_cli/tests/stdlib-props.rs
@@ -78,8 +78,8 @@ fn run_snippet_proptest(
         Err(e) => panic!("failed to compile program; brillig = {force_brillig}:\n{source}\n{e:?}"),
     };
 
-    let pedandic_solving = true;
-    let blackbox_solver = bn254_blackbox_solver::Bn254BlackBoxSolver(pedandic_solving);
+    let pedantic_solving = true;
+    let blackbox_solver = bn254_blackbox_solver::Bn254BlackBoxSolver(pedantic_solving);
     let foreign_call_executor = RefCell::new(DefaultForeignCallBuilder::default().build());
 
     // Generate multiple input/output
