d5597e27f1bd901ffba7dfa2d4e3962e706fca6c
Akosh Farkash <aakoshh@gmail.com>
2025-03-06 09:53:36 +0000

    Rename noir-execute to noir-executor

--- tooling/artifact_cli/src/bin/executor.rs
+++ tooling/artifact_cli/src/bin/executor.rs
@@ -12,22 +12,26 @@ const PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
 static VERSION_STRING: &str = formatcp!("version = {}\n", PKG_VERSION,);
 
 #[derive(Parser, Debug)]
-#[command(name="noir-execute", author, version=VERSION_STRING, about, long_about = None)]
+#[command(name="noir-executor", author, version=VERSION_STRING, about, long_about = None)]
 struct ExecutorCli {
-    #[command(flatten)]
-    command: execute_cmd::ExecuteCommand,
+    #[command(subcommand)]
+    command: ExecutorCommand,
 }
 
 #[non_exhaustive]
 #[derive(Subcommand, Clone, Debug)]
-enum ArtifactCommand {
+enum ExecutorCommand {
     Execute(execute_cmd::ExecuteCommand),
+    // TODO: Add other commands related to executing recordings from TypeScript; see https://github.com/AztecProtocol/aztec-packages/pull/12148
+    // For example to decode an input map and render it as an ABI encoded prover file for integration tests.
 }
 
 pub fn start_cli() -> eyre::Result<()> {
     let ExecutorCli { command } = ExecutorCli::parse();
 
-    execute_cmd::run(command)?;
+    match command {
+        ExecutorCommand::Execute(cmd) => execute_cmd::run(cmd)?,
+    }
 
     Ok(())
 }
@@ -41,7 +45,7 @@ fn main() {
         .init();
 
     if let Err(e) = start_cli() {
-        eprintln!("{e:?}");
+        eprintln!("{e:#}");
         std::process::exit(1);
     }
 }
