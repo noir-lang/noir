0a5ef9cea7450674c24c934bd76daa8401e42acc
github-action-benchmark <github@users.noreply.github.com>
2025-03-06 18:13:38 +0000

    add Execution Memory (customSmallerIsBetter) benchmark result for 04f968a46fbc02eb6fe4bb725f30827a6e9b6a2d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741284811060,
+  "lastUpdate": 1741284818940,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -11517,70 +11517,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "15848336+TomAFrench@users.noreply.github.com",
-            "name": "Tom French",
-            "username": "TomAFrench"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "6f7884838fc0fdab2a1864f0d701bccf79d3e359",
-          "message": "feat: optimize out range checks on limiting cases (#7510)",
-          "timestamp": "2025-02-25T12:20:40Z",
-          "tree_id": "819db7b017ea76b944f2bc134a0932870baa6f18",
-          "url": "https://github.com/noir-lang/noir/commit/6f7884838fc0fdab2a1864f0d701bccf79d3e359"
-        },
-        "date": 1740487444877,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "private-kernel-inner",
-            "value": 225.1,
-            "unit": "MB"
-          },
-          {
-            "name": "private-kernel-reset",
-            "value": 259.7,
-            "unit": "MB"
-          },
-          {
-            "name": "private-kernel-tail",
-            "value": 195.11,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-base-private",
-            "value": 441.61,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-base-public",
-            "value": 382.78,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-block-root",
-            "value": 1420,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-merge",
-            "value": 321.71,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-root",
-            "value": 329.09,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "asterite@gmail.com",
             "name": "Ary Borenszweig",
             "username": "asterite"
@@ -14670,6 +14606,70 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/noir-lang/noir/commit/1a1eb6f12f595d7b0f4a003351208a914ab5f385"
         },
         "date": 1741284525772,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "private-kernel-inner",
+            "value": 239.68,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-reset",
+            "value": 274.06,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-tail",
+            "value": 213.29,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-base-private",
+            "value": 507.47,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-base-public",
+            "value": 416.26,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-root",
+            "value": 1420,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-merge",
+            "value": 290.26,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-root",
+            "value": 296.73,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cryptostiv7@gmail.com",
+            "name": "crStiv",
+            "username": "crStiv"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "04f968a46fbc02eb6fe4bb725f30827a6e9b6a2d",
+          "message": "chore: add underscore parameter documentation (#7562)\n\nCo-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>",
+          "timestamp": "2025-03-06T17:47:11Z",
+          "tree_id": "1af45b97deee8e915a258710c46bb71fb08d0bb9",
+          "url": "https://github.com/noir-lang/noir/commit/04f968a46fbc02eb6fe4bb725f30827a6e9b6a2d"
+        },
+        "date": 1741284816571,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
