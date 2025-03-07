883204a5c3661898ad4fdf00fd6c71a831f7c48f
github-action-benchmark <github@users.noreply.github.com>
2025-03-07 16:45:38 +0000

    add Compilation Memory (customSmallerIsBetter) benchmark result for ba5beb68945d5f0e4455cdb79513aad081ace318

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,82 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741365725576,
+  "lastUpdate": 1741365938878,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
-      {
-        "commit": {
-          "author": {
-            "email": "jake@aztecprotocol.com",
-            "name": "jfecher",
-            "username": "jfecher"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
-          "id": "d118e170a33d1307ccc1240374e22d86180216ed",
-          "message": "fix(experimental): Replace most remaining match panics with errors (#7536)",
-          "timestamp": "2025-02-26T19:15:13Z",
-          "tree_id": "ac85cb7caa824af84aad35d93628c347dfcf5aae",
-          "url": "https://github.com/noir-lang/noir/commit/d118e170a33d1307ccc1240374e22d86180216ed"
-        },
-        "date": 1740598755103,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "private-kernel-inner",
-            "value": 306.51,
-            "unit": "MB"
-          },
-          {
-            "name": "private-kernel-reset",
-            "value": 616.21,
-            "unit": "MB"
-          },
-          {
-            "name": "private-kernel-tail",
-            "value": 230.08,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-base-private",
-            "value": 979.83,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-base-public",
-            "value": 856.97,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-block-root-empty",
-            "value": 367.03,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-block-root-single-tx",
-            "value": 7870,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-block-root",
-            "value": 7870,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-merge",
-            "value": 365.44,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-root",
-            "value": 412.95,
-            "unit": "MB"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -3699,6 +3625,80 @@ window.BENCHMARK_DATA = {
           {
             "name": "rollup-root",
             "value": 349.4,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jake@aztecprotocol.com",
+            "name": "jfecher",
+            "username": "jfecher"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ba5beb68945d5f0e4455cdb79513aad081ace318",
+          "message": "fix: Error on infinitely recursive types (#7579)",
+          "timestamp": "2025-03-07T16:21:07Z",
+          "tree_id": "52f7b7d8a9707cb2159befecdea22fb71b459230",
+          "url": "https://github.com/noir-lang/noir/commit/ba5beb68945d5f0e4455cdb79513aad081ace318"
+        },
+        "date": 1741365936411,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "private-kernel-inner",
+            "value": 299.95,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-reset",
+            "value": 609.91,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-tail",
+            "value": 226.6,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-base-private",
+            "value": 1250,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-base-public",
+            "value": 1330,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-root-empty",
+            "value": 303.43,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-root-single-tx",
+            "value": 7860,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-root",
+            "value": 7870,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-merge",
+            "value": 301.86,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-root",
+            "value": 349.42,
             "unit": "MB"
           }
         ]
