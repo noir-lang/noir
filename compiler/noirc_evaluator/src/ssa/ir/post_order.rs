63556cc4975e925e8b7724552c3aff0daa6b113b
Maxim Vezenov <mvezenov@gmail.com>
2025-03-10 14:40:39 +0000

    improve comment

--- compiler/noirc_evaluator/src/ssa/ir/post_order.rs
+++ compiler/noirc_evaluator/src/ssa/ir/post_order.rs
@@ -63,7 +63,7 @@ impl PostOrder {
                         // stack, we push the item that's due for a visit first to the top.
                         for successor_id in cfg.successors(block_id) {
                             if !visited.contains(&successor_id) {
-                                // This not visited check would also be cover by the next
+                                // This not visited check would also be covered by the next
                                 // iteration, but checking here too saves an iteration per successor.
                                 stack.push((Visit::First, successor_id));
                             }
