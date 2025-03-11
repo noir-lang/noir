c53bb93e9868f3651ddb9af916b4065fe7479362
Maxim Vezenov <mvezenov@gmail.com>
2025-03-11 18:57:55 +0000

    still block control dep checks on acir and add comments

--- compiler/noirc_evaluator/src/ssa/opt/loop_invariant.rs
+++ compiler/noirc_evaluator/src/ssa/opt/loop_invariant.rs
@@ -150,7 +150,14 @@ impl<'f> LoopInvariantContext<'f> {
         self.set_values_defined_in_loop(loop_);
 
         for block in loop_.blocks.iter() {
-            self.is_control_dependent_post_pre_header(loop_, *block);
+            // The CFG will ultimately be entirely flattened in ACIR.
+            // We can leave hoisting of instructions based upon predicates as part
+            // of constant folding once we we have a flat CFG.
+            // Thus, we forego checking control dependence on ACIR as to
+            // avoid greater compilation cost.
+            if self.inserter.function.runtime().is_brillig() {
+                self.is_control_dependent_post_pre_header(loop_, *block);
+            }
 
             for instruction_id in self.inserter.function.dfg[*block].take_instructions() {
                 self.transform_to_unchecked_from_loop_bounds(instruction_id);
