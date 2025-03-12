97838fc1afa74a208c6517ef6ea6910f99c70848
Maxim Vezenov <mvezenov@gmail.com>
2025-03-12 18:19:35 +0000

    just check dom frontiers and not control dep blocks map

--- compiler/noirc_evaluator/src/ssa/opt/loop_invariant.rs
+++ compiler/noirc_evaluator/src/ssa/opt/loop_invariant.rs
@@ -220,7 +220,8 @@ impl<'f> LoopInvariantContext<'f> {
         // the current block and the loop header
         for predecessor in all_predecessors {
             // if self.control_dependent_blocks.contains(&predecessor)
-            // || self.is_control_dependent(predecessor, block)
+            //     || self.is_control_dependent(predecessor, block)
+            // {
             if self.is_control_dependent(predecessor, block) {
                 self.current_block_control_dependent = true;
                 self.control_dependent_blocks.insert(predecessor);
