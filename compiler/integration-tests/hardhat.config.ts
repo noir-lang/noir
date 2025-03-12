97bf59ae5efc4ac50de55c27961e7b5f089d0122
Tom French <tom@tomfren.ch>
2025-03-12 15:26:32 +0000

    .

--- compiler/integration-tests/hardhat.config.ts
+++ compiler/integration-tests/hardhat.config.ts
@@ -4,7 +4,7 @@ import '@nomicfoundation/hardhat-ethers';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.19',
+    version: '0.8.21',
     settings: {
       optimizer: {
         enabled: true,
