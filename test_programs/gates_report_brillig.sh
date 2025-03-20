009d0c640fc02fdbc0fc3dd404f393a40b118811
Maxim Vezenov <mvezenov@gmail.com>
2025-03-20 15:36:06 +0000

    try to simplify once more with set -e and set -o pipefail

--- test_programs/gates_report_brillig.sh
+++ test_programs/gates_report_brillig.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+set -o pipefail
 
 # These tests are incompatible with artifact size reporting
 excluded_dirs=(
--- test_programs/gates_report_brillig_execution.sh
+++ test_programs/gates_report_brillig_execution.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+set -o pipefail
 
 # These tests are incompatible with execution trace reporting
 excluded_dirs=(
