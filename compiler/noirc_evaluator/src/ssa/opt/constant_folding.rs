1e33f1f2f77607ad5781da48f3c9bb6a8b0339c5
Ary Borenszweig <asterite@gmail.com>
2025-03-27 11:22:30 -0300

    `requires_acir_gen_predicate` for ArraySet is true

--- compiler/noirc_evaluator/src/ssa/opt/constant_folding.rs
+++ compiler/noirc_evaluator/src/ssa/opt/constant_folding.rs
@@ -443,9 +443,7 @@ impl<'brillig> Context<'brillig> {
         // Thus, even if the index is dynamic (meaning the array get would have side effects),
         // we can simplify the operation when we take into account the predicate.
         if let Instruction::ArraySet { index, value, .. } = &instruction {
-            let use_predicate =
-                self.use_constraint_info && instruction.requires_acir_gen_predicate(&function.dfg);
-            let predicate = use_predicate.then_some(side_effects_enabled_var);
+            let predicate = self.use_constraint_info.then_some(side_effects_enabled_var);
 
             let array_get = Instruction::ArrayGet { array: instruction_results[0], index: *index };
 
