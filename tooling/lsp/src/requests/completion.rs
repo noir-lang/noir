75a6dce5c9d649c7b52289f66c3d4a570c628397
Ary Borenszweig <asterite@gmail.com>
2025-04-03 13:10:30 -0300

    Visit unresolved generics

--- tooling/lsp/src/requests/completion.rs
+++ tooling/lsp/src/requests/completion.rs
@@ -1259,6 +1259,10 @@ impl Visitor for NodeFinder<'_> {
             attribute.accept(AttributeTarget::Function, self);
         }
 
+        for generic in &noir_function.def.generics {
+            generic.accept(self);
+        }
+
         let old_type_parameters = self.type_parameters.clone();
         self.collect_type_parameters_in_generics(&noir_function.def.generics);
 
@@ -1300,6 +1304,10 @@ impl Visitor for NodeFinder<'_> {
     }
 
     fn visit_noir_trait_impl(&mut self, noir_trait_impl: &NoirTraitImpl, _: Span) -> bool {
+        for generic in &noir_trait_impl.impl_generics {
+            generic.accept(self);
+        }
+
         let UnresolvedTypeData::Named(trait_name, _, _) = &noir_trait_impl.r#trait.typ else {
             return false;
         };
@@ -1338,6 +1346,10 @@ impl Visitor for NodeFinder<'_> {
     }
 
     fn visit_type_impl(&mut self, type_impl: &TypeImpl, _: Span) -> bool {
+        for generic in &type_impl.generics {
+            generic.accept(self);
+        }
+
         type_impl.object_type.accept(self);
 
         self.type_parameters.clear();
