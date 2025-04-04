5419b6b743bc0e7b3470be228b0d3ec0987cb188
Tom French <tom@tomfren.ch>
2025-04-04 11:45:13 +0100

    chore: create new crate for testing `test_programs`

---
+++ test_programs/src/lib.rs
@@ -0,0 +1,203 @@
+#[cfg(test)]
+mod tests {
+    // Some of these imports are consumed by the injected tests
+    use assert_cmd::prelude::*;
+    use predicates::prelude::*;
+
+    use std::path::{Path, PathBuf};
+    use std::process::{Command, Output};
+
+    // Utilities to keep the test matrix labels more intuitive.
+    #[derive(Debug, Clone, Copy)]
+    struct ForceBrillig(pub bool);
+    #[derive(Debug, Clone, Copy)]
+    struct Inliner(pub i64);
+
+    fn setup_nargo(
+        test_program_dir: &Path,
+        test_command: &str,
+        force_brillig: ForceBrillig,
+        inliner_aggressiveness: Inliner,
+    ) -> Command {
+        let mut nargo = Command::cargo_bin("nargo").unwrap();
+        nargo.arg("--program-dir").arg(test_program_dir);
+        nargo.arg(test_command).arg("--force");
+        nargo.arg("--inliner-aggressiveness").arg(inliner_aggressiveness.0.to_string());
+        // Check whether the test case is non-deterministic
+        nargo.arg("--check-non-determinism");
+        // Allow more bytecode in exchange to catch illegal states.
+        nargo.arg("--enable-brillig-debug-assertions");
+
+        // Enable enums and ownership as unstable features
+        nargo.arg("-Zenums");
+        nargo.arg("-Zownership");
+
+        if force_brillig.0 {
+            {
+                nargo.arg("--force-brillig");
+
+                // Set the maximum increase so that part of the optimization is exercised (it might fail).
+                nargo.arg("--max-bytecode-increase-percent");
+                nargo.arg("50");
+            }
+        }
+
+        nargo
+    }
+
+    fn snapshot_output(output: Output, test_program_dir: &Path) {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let stdout = remove_noise_lines(stdout.to_string());
+        let stdout = delete_test_program_dir_occurrences(stdout, test_program_dir);
+
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        let stderr = remove_noise_lines(stderr.to_string());
+        let stderr = delete_test_program_dir_occurrences(stderr, test_program_dir);
+
+        let test_name = test_program_dir.file_name().unwrap().to_string_lossy().to_string();
+        insta::with_settings!(
+            {
+                snapshot_path => test_program_dir,
+                prepend_module_to_snapshot => false,
+            },
+            {
+                insta::assert_snapshot!(
+                    test_name,
+                    &format!(
+                        "success: {:?}\nexit_code: {}\n----- stdout -----\n{}\n----- stderr -----\n{}",
+                        output.status.success(),
+                        output.status.code().unwrap_or(!0),
+                        stdout,
+                        stderr
+                    ),
+                )
+            }
+        );
+    }
+
+    fn remove_noise_lines(string: String) -> String {
+        string
+            .lines()
+            .filter(|line| {
+                !line.contains("Witness saved to")
+                    && !line.contains("Circuit witness successfully solved")
+                    && !line.contains("Waiting for lock")
+            })
+            .collect::<Vec<&str>>()
+            .join("\n")
+    }
+
+    fn delete_test_program_dir_occurrences(string: String, test_program_dir: &Path) -> String {
+        let test_program_base_dir = test_program_dir.parent().unwrap().parent().unwrap();
+        let mut test_program_base_dir = test_program_base_dir.to_string_lossy().to_string();
+        if !test_program_base_dir.ends_with('/') {
+            test_program_base_dir.push('/');
+        }
+
+        let mut test_program_dir = test_program_dir.to_string_lossy().to_string();
+        if !test_program_dir.ends_with('/') {
+            test_program_dir.push('/');
+        }
+
+        // We replace both test_program_dir (test_programs/compile_failure/foo) and its ancestor
+        // (test_programs) because in some cases some programs refer to other programs in `test_programs`.
+        string
+            .lines()
+            .map(|line| {
+                line.replace(&test_program_dir, "").replace(&test_program_base_dir, "../../")
+            })
+            .collect::<Vec<String>>()
+            .join("\n")
+    }
+
+    fn execution_success(mut nargo: Command, test_program_dir: PathBuf, check_stdout: bool) {
+        nargo.assert().success();
+
+        if check_stdout {
+            let output = nargo.output().unwrap();
+            snapshot_output(output, &test_program_dir);
+        }
+    }
+
+    fn execution_failure(mut nargo: Command) {
+        nargo
+            .assert()
+            .failure()
+            .stderr(predicate::str::contains("The application panicked (crashed).").not());
+    }
+
+    fn noir_test_success(mut nargo: Command, test_program_dir: PathBuf) {
+        nargo.assert().success();
+        let output = nargo.output().unwrap();
+        snapshot_output(output, &test_program_dir);
+    }
+
+    fn noir_test_failure(mut nargo: Command, test_program_dir: PathBuf) {
+        nargo
+            .assert()
+            .failure()
+            .stderr(predicate::str::contains("The application panicked (crashed).").not());
+        let output = nargo.output().unwrap();
+        snapshot_output(output, &test_program_dir);
+    }
+
+    fn compile_success_empty(mut nargo: Command, no_warnings: bool) {
+        nargo.arg("--json");
+        let output = nargo.output().expect("Failed to execute command");
+
+        if !output.status.success() {
+            {
+                panic!(
+                    "`nargo info` failed with: {}",
+                    String::from_utf8(output.stderr).unwrap_or_default()
+                );
+            }
+        }
+
+        if no_warnings {
+            nargo.assert().success().stderr(predicate::str::contains("warning:").not());
+        }
+
+        // `compile_success_empty` tests should be able to compile down to an empty circuit.
+        let json: serde_json::Value = serde_json::from_slice(&output.stdout).unwrap_or_else(|e| {
+            {
+                panic!(
+                    "JSON was not well-formatted {:?}\n\n{:?}",
+                    e,
+                    std::str::from_utf8(&output.stdout)
+                )
+            }
+        });
+        let num_opcodes = &json["programs"][0]["functions"][0]["opcodes"];
+        assert_eq!(
+            num_opcodes.as_u64().expect("number of opcodes should fit in a u64"),
+            0,
+            "expected the number of opcodes to be 0"
+        );
+    }
+
+    fn compile_success_contract(mut nargo: Command) {
+        nargo.assert().success().stderr(predicate::str::contains("warning:").not());
+    }
+
+    fn compile_success_no_bug(mut nargo: Command) {
+        nargo.assert().success().stderr(predicate::str::contains("bug:").not());
+    }
+
+    fn compile_success_with_bug(mut nargo: Command) {
+        nargo.assert().success().stderr(predicate::str::contains("bug:"));
+    }
+
+    fn compile_failure(mut nargo: Command, test_program_dir: PathBuf) {
+        nargo
+            .assert()
+            .failure()
+            .stderr(predicate::str::contains("The application panicked (crashed).").not());
+
+        let output = nargo.output().unwrap();
+        snapshot_output(output, &test_program_dir);
+    }
+
+    // include tests generated by `build.rs`
+    include!(concat!(env!("OUT_DIR"), "/execute.rs"));
+}
