5eab7995ba4ac2ed13a9d3c29a183db3a90f10c1
Akosh Farkash <aakoshh@gmail.com>
2025-04-04 12:02:07 +0100

    Redistribute frequencies

--- tooling/ast_fuzzer/src/program/freq.rs
+++ tooling/ast_fuzzer/src/program/freq.rs
@@ -30,25 +30,87 @@ impl Index<&str> for Freqs {
 /// Help with cumulative frequency distributions.
 pub(crate) struct Freq {
     freqs: Freqs,
-    acc: usize,
     x: usize,
+    accumulated: usize,
+    disabled: usize,
 }
 
 impl Freq {
     pub fn new(u: &mut Unstructured, freqs: &Freqs) -> arbitrary::Result<Self> {
-        Ok(Self { freqs: freqs.clone(), acc: 0, x: u.choose_index(freqs.total())? })
+        let x = u.choose_index(freqs.total())?;
+        Ok(Self { freqs: freqs.clone(), x, accumulated: 0, disabled: 0 })
     }
 
     /// Check if a key is enabled, based on the already checked cumulative values.
     pub fn enabled(&mut self, key: &str) -> bool {
-        self.acc += self.freqs[key];
-        self.x < self.acc
+        self.accumulated += self.freqs[key];
+        self.passed()
     }
 
-    /// Like `enabled`, but if `cond` is `false` it does not increase the cumulative value,
-    /// so as not to distort the next call, ie. if we have have 5% then another 5%,
-    /// if the first one is disabled, the second doesn't become 10%.
+    /// Like `enabled`, but if `cond` is `false` it redistributes the probability
+    /// to the remaining keys, as if the current one never existed.
     pub fn enabled_if(&mut self, key: &str, cond: bool) -> bool {
-        cond && self.enabled(key)
+        if cond {
+            self.enabled(key)
+        } else {
+            self.disabled += self.freqs[key];
+            false
+        }
+    }
+
+    /// Check if the accumulated weights have passed the random variable `x`,
+    /// adjusted with the total disabled weight.
+    fn passed(&self) -> bool {
+        if self.freqs.total == 0 {
+            return false;
+        }
+        let adj_total = self.freqs.total - self.disabled;
+        let adj_x = self.x * adj_total / self.freqs.total;
+        self.accumulated > adj_x
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Freq, Freqs};
+
+    /// Create a test distribution with a given `x in 0..100`.
+    fn make_test_freq(x: usize) -> Freq {
+        assert!(x < 100, "scale goes to 100");
+        let freqs = Freqs::new(&[("foo", 40), ("bar", 30), ("baz", 20), ("qux", 10)]);
+        Freq { freqs, accumulated: 0, disabled: 0, x }
+    }
+
+    /// Test that it enables baz, because the cumulative freq including baz is 90.
+    #[test]
+    fn test_freq_enabled() {
+        let mut freq = make_test_freq(80);
+        assert!(!freq.enabled("foo"));
+        assert!(!freq.enabled("bar"));
+        assert!(freq.enabled("baz"));
+    }
+
+    /// Test that changing the order results in a different label being enabled,
+    /// because the cross the threshold at a different call.
+    #[test]
+    fn test_freq_enabled_order() {
+        let mut freq = make_test_freq(80);
+        assert!(!freq.enabled("baz"));
+        assert!(!freq.enabled("foo"));
+        assert!(freq.enabled("bar"));
+    }
+
+    /// Test that some labels being disabled redistribute the probability
+    /// on the remaining ones. In this example we choose x=25 out of 100,
+    /// which is the 25th percentile, but disable the first 70 weights,
+    /// which means if we want `x` to be the 25th percentile of the
+    /// remaining 30 we adjust it down to 7, which enabled baz instead
+    /// of qux.
+    #[test]
+    fn test_freq_redistribution() {
+        let mut freq = make_test_freq(25);
+        assert!(!freq.enabled_if("foo", false));
+        assert!(!freq.enabled_if("bar", false));
+        assert!(freq.enabled("baz"));
     }
 }
