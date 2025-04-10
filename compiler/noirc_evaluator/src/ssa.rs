07e1cbb4683ba73b1b9822224cb70d3b8a34a72a
Maxim Vezenov <mvezenov@gmail.com>
2025-04-10 19:14:38 +0000

    unroll and mem2reg once more

--- compiler/noirc_evaluator/src/ssa.rs
+++ compiler/noirc_evaluator/src/ssa.rs
@@ -216,10 +216,15 @@ fn optimize_all(builder: SsaBuilder, options: &SsaEvaluatorOptions) -> Result<Ss
         .run_pass(Ssa::flatten_basic_conditionals, "Simplify conditionals for unconstrained")
         .run_pass(Ssa::remove_enable_side_effects, "EnableSideEffectsIf removal")
         .run_pass(Ssa::fold_constants_using_constraints, "Constraint Folding")
+        .try_run_pass(
+            |ssa| ssa.unroll_loops_iteratively(options.max_bytecode_increase_percent),
+            "Unrolling (2nd)",
+        )?
         .run_pass(Ssa::make_constrain_not_equal_instructions, "Adding constrain not equal")
         .run_pass(Ssa::check_u128_mul_overflow, "Check u128 mul overflow")
         .run_pass(Ssa::dead_instruction_elimination, "Dead Instruction Elimination (1st)")
         .run_pass(Ssa::simplify_cfg, "Simplifying (3rd):")
+        .run_pass(Ssa::mem2reg, "Mem2Reg (5th)")
         .run_pass(Ssa::array_set_optimization, "Array Set Optimizations")
         // The Brillig globals pass expected that we have the used globals map set for each function.
         // The used globals map is determined during DIE, so we should duplicate entry points before a DIE pass run.
