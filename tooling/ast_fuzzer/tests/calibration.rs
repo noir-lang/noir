0b174879ed9834ff334888aa6c0a664a6e7b4281
Akosh Farkash <aakoshh@gmail.com>
2025-04-14 15:43:24 +0100

    Print expr and stmt separately

--- tooling/ast_fuzzer/tests/calibration.rs
+++ tooling/ast_fuzzer/tests/calibration.rs
@@ -12,54 +12,30 @@ use std::collections::BTreeMap;
 
 use arbtest::arbtest;
 use noir_ast_fuzzer::{Config, arb_program, visit_expr};
-use noirc_frontend::monomorphization::ast::Expression;
+use noirc_frontend::monomorphization::ast::{Expression, Type};
 
 #[test]
 fn arb_program_freqs_in_expected_range() {
-    // Counting labels separately for ACIR and Brillig.
-    let mut counts: BTreeMap<bool, BTreeMap<&str, usize>> = Default::default();
+    // Counting labels separately for ACIR and Brillig, and then whether it's an expression or a statement.
+    let mut counts: BTreeMap<bool, BTreeMap<&str, BTreeMap<&str, usize>>> = Default::default();
     let mut program_count = 0;
 
     arbtest(|u| {
         let program = arb_program(u, Config::default())?;
         for func in program.functions {
-            let counts = counts.entry(func.unconstrained).or_default();
-
-            let mut obs = |key| {
-                let cnt = counts.entry(key).or_default();
-                *cnt = *cnt + 1;
-            };
-
             // Visit the
             visit_expr(&func.body, &mut |expr| {
-                let key = match expr {
-                    Expression::Literal(_) => "literal",
-                    Expression::Block(_) => "block",
-                    Expression::Unary(_) => "unary",
-                    Expression::Binary(_) => "binary",
-                    Expression::For(_) => "for",
-                    Expression::Loop(_) => "loop",
-                    Expression::While(_) => "while",
-                    Expression::If(_) => "if",
-                    Expression::Match(_) => "match",
-                    Expression::Call(_) => "call",
-                    Expression::Let(_) => "let",
-                    Expression::Constrain(_, _, _) => "constrain",
-                    Expression::Assign(_) => "assign",
-                    Expression::Drop(_) => "drop",
-                    Expression::Break => "break",
-                    Expression::Continue => "continue",
-                    Expression::Ident(_)
-                    | Expression::Cast(_)
-                    | Expression::Tuple(_)
-                    | Expression::ExtractTupleField(_, _)
-                    | Expression::Index(_)
-                    | Expression::Semi(_)
-                    | Expression::Clone(_) => {
-                        return true;
-                    }
+                let Some((group, key)) = classify(expr) else {
+                    return true;
                 };
-                obs(key);
+                let count = counts
+                    .entry(func.unconstrained)
+                    .or_default()
+                    .entry(group)
+                    .or_default()
+                    .entry(key)
+                    .or_default();
+                *count = *count + 1;
                 true
             });
         }
@@ -71,12 +47,54 @@ fn arb_program_freqs_in_expected_range() {
     .size_max(1 << 20);
 
     println!("Generated {program_count} programs.");
-    for (c, counts) in counts {
-        println!("{} frequencies:", if c { "Brillig" } else { "ACIR" });
-        for (key, count) in counts {
-            println!("\t{key}: {count}");
+    for (unconstrained, counts) in counts {
+        println!("{} frequencies:", if unconstrained { "Brillig" } else { "ACIR" });
+        for (group, counts) in counts {
+            let total = counts.values().sum::<usize>();
+            println!("\t{group} (total {total}):");
+            for (key, count) in counts {
+                println!(
+                    "\t\t{key}:{} {count}\t({}/100)",
+                    std::iter::repeat_n(" ", 15 - key.len()).collect::<String>(),
+                    count * 100 / total
+                );
+            }
         }
     }
 
     // TODO: Assert relative frequencies
+}
+
+/// Classify the expression into "expr" or "stmt" for frequency settings.
+fn classify(expr: &Expression) -> Option<(&'static str, &'static str)> {
+    let cat = match expr {
+        Expression::Ident(_)
+        | Expression::Cast(_)
+        | Expression::Tuple(_)
+        | Expression::ExtractTupleField(_, _)
+        | Expression::Index(_)
+        | Expression::Semi(_)
+        | Expression::Clone(_) => {
+            return None;
+        }
+        Expression::Literal(_) => ("expr", "literal"),
+        Expression::Block(xs) => {
+            (xs.last().and_then(classify).map(|(c, _)| c).unwrap_or("stmt"), "block")
+        }
+        Expression::Unary(_) => ("expr", "unary"),
+        Expression::Binary(_) => ("expr", "binary"),
+        Expression::For(_) => ("stmt", "for"),
+        Expression::Loop(_) => ("stmt", "loop"),
+        Expression::While(_) => ("stmt", "while"),
+        Expression::If(x) => (if x.typ == Type::Unit { "stmt" } else { "expr" }, "if"),
+        Expression::Match(_) => todo!("match"),
+        Expression::Call(x) => (if x.return_type == Type::Unit { "stmt" } else { "expr" }, "call"),
+        Expression::Let(_) => ("stmt", "let"),
+        Expression::Constrain(_, _, _) => ("stmt", "constrain"),
+        Expression::Assign(_) => ("stmt", "assign"),
+        Expression::Drop(_) => ("stmt", "drop"),
+        Expression::Break => ("stmt", "break"),
+        Expression::Continue => ("stmt", "continue"),
+    };
+    Some(cat)
 }
