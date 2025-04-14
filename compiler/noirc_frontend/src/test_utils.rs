969c44c6ccb38a737d720555331ec5196bc464b7
Maxim Vezenov <mvezenov@gmail.com>
2025-04-14 20:38:03 +0000

    update error to warn cases list

--- compiler/noirc_frontend/src/test_utils.rs
+++ compiler/noirc_frontend/src/test_utils.rs
@@ -171,7 +171,6 @@ fn emit_compile_test(test_path: &str, src: &str, mut expect: Expect) {
     let error_to_warn_cases = [
         "cast_256_to_u8_size_checks",
         "enums_errors_on_unspecified_unstable_enum",
-        "immutable_references_without_ownership_feature",
         "imports_warns_on_use_of_private_exported_item",
         "metaprogramming_does_not_fail_to_parse_macro_on_parser_warning",
         "resolve_unused_var",
