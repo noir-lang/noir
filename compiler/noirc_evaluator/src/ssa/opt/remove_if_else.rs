6dbd7994ab4cff9763a2d0a985055802f9a03321
Ary Borenszweig <asterite@gmail.com>
2025-04-16 09:54:45 -0300

    Use `Function::mutate` in `remove_if_else`

--- compiler/noirc_evaluator/src/ssa/opt/remove_if_else.rs
+++ compiler/noirc_evaluator/src/ssa/opt/remove_if_else.rs
@@ -6,7 +6,7 @@ use fxhash::FxHashMap as HashMap;
 use crate::ssa::ir::function::RuntimeType;
 use crate::ssa::ir::instruction::Hint;
 use crate::ssa::ir::types::NumericType;
-use crate::ssa::ir::value::{ValueId, ValueMapping};
+use crate::ssa::ir::value::ValueId;
 use crate::ssa::{
     Ssa,
     ir::{
@@ -63,31 +63,26 @@ impl Context {
         // Make sure this optimization runs when there's only one block
         assert_eq!(function.dfg[block].successors().count(), 0);
 
-        let instructions = function.dfg[block].take_instructions();
         let one = FieldElement::one();
         let mut current_conditional = function.dfg.make_constant(one, NumericType::bool());
-        let mut values_to_replace = ValueMapping::default();
 
-        for instruction in instructions {
-            // Before we process instructions, replace any values we previously determined we need to replace
-            if !values_to_replace.is_empty() {
-                let instruction = &mut function.dfg[instruction];
-                instruction.replace_values(&values_to_replace);
-            }
+        function.mutate(|context| {
+            let instruction_id = context.instruction_id;
+            let instruction = context.instruction();
 
-            match &function.dfg[instruction] {
+            match instruction {
                 Instruction::IfElse { then_condition, then_value, else_condition, else_value } => {
                     let then_condition = *then_condition;
                     let else_condition = *else_condition;
                     let then_value = *then_value;
                     let else_value = *else_value;
 
-                    let typ = function.dfg.type_of_value(then_value);
+                    let typ = context.dfg.type_of_value(then_value);
                     assert!(!matches!(typ, Type::Numeric(_)));
 
-                    let call_stack = function.dfg.get_instruction_call_stack_id(instruction);
+                    let call_stack = context.dfg.get_instruction_call_stack_id(instruction_id);
                     let mut value_merger = ValueMerger::new(
-                        &mut function.dfg,
+                        &mut context.dfg,
                         block,
                         &mut self.slice_sizes,
                         &mut self.array_set_conditionals,
@@ -102,8 +97,8 @@ impl Context {
                         else_value,
                     );
 
-                    let _typ = function.dfg.type_of_value(value);
-                    let results = function.dfg.instruction_results(instruction);
+                    let _typ = context.dfg.type_of_value(value);
+                    let results = context.dfg.instruction_results(instruction_id);
                     let result = results[0];
                     // let result = match typ {
                     //     Type::Array(..) => results[0],
@@ -111,54 +106,47 @@ impl Context {
                     //     other => unreachable!("IfElse instructions should only have arrays or slices at this point. Found {other:?}"),
                     // };
 
-                    values_to_replace.insert(result, value);
+                    context.remove_current_instruction();
+                    context.replace_value(result, value);
                     self.array_set_conditionals.insert(value, current_conditional);
                 }
                 Instruction::Call { func, arguments } => {
-                    if let Value::Intrinsic(intrinsic) = function.dfg[*func] {
-                        let results = function.dfg.instruction_results(instruction);
+                    if let Value::Intrinsic(intrinsic) = context.dfg[*func] {
+                        let results = context.dfg.instruction_results(instruction_id);
 
-                        match slice_capacity_change(&function.dfg, intrinsic, arguments, results) {
+                        match slice_capacity_change(&context.dfg, intrinsic, arguments, results) {
                             SizeChange::None => (),
                             SizeChange::SetTo(value, new_capacity) => {
                                 self.slice_sizes.insert(value, new_capacity);
                             }
                             SizeChange::Inc { old, new } => {
-                                let old_capacity = self.get_or_find_capacity(&function.dfg, old);
+                                let old_capacity = self.get_or_find_capacity(&context.dfg, old);
                                 self.slice_sizes.insert(new, old_capacity + 1);
                             }
                             SizeChange::Dec { old, new } => {
-                                let old_capacity = self.get_or_find_capacity(&function.dfg, old);
+                                let old_capacity = self.get_or_find_capacity(&context.dfg, old);
                                 // We use a saturating sub here as calling `pop_front` or `pop_back` on a zero-length slice
                                 // would otherwise underflow.
                                 self.slice_sizes.insert(new, old_capacity.saturating_sub(1));
                             }
                         }
                     }
-                    function.dfg[block].instructions_mut().push(instruction);
                 }
                 Instruction::ArraySet { array, .. } => {
-                    let results = function.dfg.instruction_results(instruction);
+                    let results = context.dfg.instruction_results(instruction_id);
                     let result = if results.len() == 2 { results[1] } else { results[0] };
 
                     self.array_set_conditionals.insert(result, current_conditional);
 
-                    let old_capacity = self.get_or_find_capacity(&function.dfg, *array);
+                    let old_capacity = self.get_or_find_capacity(&context.dfg, *array);
                     self.slice_sizes.insert(result, old_capacity);
-                    function.dfg[block].instructions_mut().push(instruction);
                 }
                 Instruction::EnableSideEffectsIf { condition } => {
                     current_conditional = *condition;
-                    function.dfg[block].instructions_mut().push(instruction);
                 }
-                _ => {
-                    function.dfg[block].instructions_mut().push(instruction);
-                }
+                _ => (),
             }
-        }
-
-        function.dfg.replace_values_in_block_terminator(block, &values_to_replace);
-        function.dfg.data_bus.replace_values(&values_to_replace);
+        });
     }
 
     fn get_or_find_capacity(&mut self, dfg: &DataFlowGraph, value: ValueId) -> u32 {
