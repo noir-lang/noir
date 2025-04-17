0e939d13a6ecdb342726740675ab214b73d0ad77
TomAFrench <tom@tomfren.ch>
2025-04-17 23:58:44 +0000

    .

--- tooling/nargo_cli/tests/stdlib-props.rs
+++ tooling/nargo_cli/tests/stdlib-props.rs
@@ -96,41 +96,6 @@ fn fuzz_basic() {
     run_snippet_proptest(program.to_string(), false, strategy);
 }
 
-#[test]
-fn fuzz_poseidon2_equivalence() {
-    use bn254_blackbox_solver::poseidon_hash;
-
-    // Test empty, small, then around the RATE value, then bigger inputs.
-    for max_len in [0, 1, 3, 4, 100] {
-        let source = format!(
-            "fn main(input: [Field; {max_len}], message_size: u32) -> pub Field {{
-                std::hash::poseidon2::Poseidon2::hash(input, message_size)
-            }}"
-        );
-
-        let strategy = (0..=max_len)
-            .prop_flat_map(field_vec_strategy)
-            .prop_map(move |mut msg| {
-                let output = poseidon_hash(&msg, msg.len() < max_len).expect("failed to hash");
-
-                // The input has to be padded to the maximum length.
-                let msg_size = msg.len();
-                msg.resize(max_len, FieldElement::from(0u64));
-
-                let inputs = vec![
-                    ("input", InputValue::Vec(vecmap(msg, InputValue::Field))),
-                    ("message_size", InputValue::Field(FieldElement::from(msg_size))),
-                ];
-
-                SnippetInputOutput::new(inputs, InputValue::Field(output))
-                    .with_description(format!("max_len = {max_len}"))
-            })
-            .boxed();
-
-        run_snippet_proptest(source.clone(), false, strategy);
-    }
-}
-
 fn field_vec_strategy(len: usize) -> impl Strategy<Value = Vec<FieldElement>> {
     // Generate Field elements from random 32 byte vectors.
     let field = prop::collection::vec(any::<u8>(), 32)
