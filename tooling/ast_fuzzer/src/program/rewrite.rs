ec2759c77fff52ab4f990f5a078f948425569b3f
Akosh Farkash <aakoshh@gmail.com>
2025-04-22 17:44:49 +0100

    Recursion: WIP

--- tooling/ast_fuzzer/src/program/rewrite.rs
+++ tooling/ast_fuzzer/src/program/rewrite.rs
@@ -1,9 +1,12 @@
-use std::collections::HashSet;
+use std::collections::{BTreeMap, HashSet};
 
 use arbitrary::Unstructured;
+use im::HashMap;
 use noirc_frontend::{
     ast::BinaryOpKind,
-    monomorphization::ast::{Definition, Expression, Function, IdentId, LocalId, Program, Type},
+    monomorphization::ast::{
+        Call, Definition, Expression, FuncId, Function, IdentId, LocalId, Program, Type,
+    },
     shared::Visibility,
 };
 
@@ -12,27 +15,59 @@ use super::{
     visitor::{visit_expr, visit_expr_mut},
 };
 
-/// Find recursive functions and add a `ctx_depth` parameter to them.
-pub(crate) fn add_recursion_depth(
+/// Find recursive functions and add a `ctx_limit: &mut u32` parameter to them,
+/// which we use to limit the number of recursive calls. This is complicated by
+/// the fact that we cannot pass mutable references from ACIR to Brillig. To
+/// overcome that, we create a proxy function for unconstrained functions that
+/// take `mut ctx_limit: u32` instead, and pass it on as a mutable ref.
+pub(crate) fn add_recursion_limit(
     ctx: &mut Context,
     u: &mut Unstructured,
 ) -> arbitrary::Result<()> {
     // Collect recursive functions, ie. the ones which call other functions.
+    // Remember if they are unconstrained; those need proxies as well.
     let recursive_functions = ctx
         .functions
         .iter()
-        .filter_map(|(id, func)| expr::has_call(&func.body).then_some(*id))
-        .collect::<HashSet<_>>();
+        .filter_map(|(id, func)| expr::has_call(&func.body).then_some((*id, func.unconstrained)))
+        .collect::<BTreeMap<_, _>>();
+
+    // Create proxies for unconstrained recursive functions.
+    // let mut proxy_functions = HashMap::new();
+    // let mut next_func_id = FuncId(ctx.functions.len() as u32);
+
+    // for (id, func) in &ctx.functions {
+    //     if expr::has_call(&func.body) {
+    //         recursive_functions.insert(id);
+    //         if func.unconstrained && *id != Program::main_id() {
+    //             let mut proxy = func.clone();
+    //             proxy.id = next_func_id;
+    //             proxy.name =format!("{}_proxy", proxy.name);
+    //             proxy.body = Expression::Call(Call {})
+
+    //             next_func_id = FuncId(next_func_id.0 + 1);
+    //             proxy_functions.insert(id, )
+    //         }
+    //     }
+    // }
+
+    // Create proxy functions for the unconstrained recursive functions.
+    // We could check whether they are called from ACIR, but that would require further traversals.
 
     for (func_id, func) in
-        ctx.functions.iter_mut().filter(|(id, _)| recursive_functions.contains(id))
+        ctx.functions.iter_mut().filter(|(id, _)| recursive_functions.contains_key(id))
     {
         let is_main = *func_id == Program::main_id();
+
         // We'll need a new ID for variables or parameters. We could speed this up by
         // 1) caching this value in a "function meta" construct, or
-        // 2) using `u32::MAX`, but we wouldn't be able to add caching to `Program`,
-        // so eventually we'll need to look at the values to do random mutations.
+        // 2) using `u32::MAX`, but then we would be in a worse situation next time
+        // 3) draw values from `Context` instead of `FunctionContext`, which breaks continuity, but saves an extra traversal.
+        // We wouldn't be able to add caching to `Program` without changing it, so eventually we'll need to look at the values
+        // to do random mutations, or we have to pass back some meta along with `Program` and look it up there. For now we
+        // traverse the AST to figure out what the next ID to use is.
         let (next_local_id, next_ident_id) = next_local_and_ident_id(func);
+
         let depth_id = LocalId(next_local_id);
         let depth_name = "ctx_depth".to_string();
         let depth_ident_id = IdentId(next_ident_id);
