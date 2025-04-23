1286104786c825d3bd8f2db1fe40494657b89b5d
github-action-benchmark <github@users.noreply.github.com>
2025-04-23 20:39:33 +0000

    add ACVM Benchmarks (cargo) benchmark result for 3e3e30297d1319e785f4bc04f8d807d4301d2fa1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745439709647,
+  "lastUpdate": 1745440772537,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -17971,42 +17971,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "15848336+TomAFrench@users.noreply.github.com",
-            "name": "Tom French",
-            "username": "TomAFrench"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
-          "id": "c1106e9a1aa7684a1da4a9a4f947d26b404dce7f",
-          "message": "chore: bump bignum timeout (#8080)",
-          "timestamp": "2025-04-15T11:18:49Z",
-          "tree_id": "38946e94af03b3d432f1a0be81618e036b3bc2a7",
-          "url": "https://github.com/noir-lang/noir/commit/c1106e9a1aa7684a1da4a9a4f947d26b404dce7f"
-        },
-        "date": 1744717105046,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 266820,
-            "range": "± 1663",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 233984,
-            "range": "± 2027",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "asterite@gmail.com",
             "name": "Ary Borenszweig",
             "username": "asterite"
@@ -19938,6 +19902,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 3226030,
             "range": "± 8811",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "isennovskiy@gmail.com",
+            "name": "Innokentii Sennovskii",
+            "username": "Rumata888"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3e3e30297d1319e785f4bc04f8d807d4301d2fa1",
+          "message": "fix: Detect ABI change of fuzzing harnesses (#8193)",
+          "timestamp": "2025-04-23T20:20:27Z",
+          "tree_id": "b5521c1df7fcd37bcc2ee7e24d0805f329c608bc",
+          "url": "https://github.com/noir-lang/noir/commit/3e3e30297d1319e785f4bc04f8d807d4301d2fa1"
+        },
+        "date": 1745440766463,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 270451,
+            "range": "± 511",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 239621,
+            "range": "± 1203",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 3234976,
+            "range": "± 12087",
             "unit": "ns/iter"
           }
         ]
