e23dd87d8958e3aebca89f63c6a17e6306278b81
Akosh Farkash <aakoshh@gmail.com>
2025-04-25 14:18:06 +0100

    Set -len_control to ramp it up faster

--- tooling/ast_fuzzer/fuzz/fuzz_targets/init_vs_final.rs
+++ tooling/ast_fuzzer/fuzz/fuzz_targets/init_vs_final.rs
@@ -3,7 +3,7 @@
 //! and the fully optimized version.
 //!
 //! ```text
-//! cargo +nightly fuzz run init_vs_final
+//! cargo +nightly fuzz run init_vs_final -- -runs=10000 -max_len=1048576 -len_control=0
 //! ```
 #![no_main]
 
