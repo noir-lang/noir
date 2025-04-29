0a46490e24642dc4975c8b7aee0fbfbc0e1477b6
rustdoc <github@users.noreply.github.com>
2025-04-29 14:16:44 +0000

    update rustdoc

--- docs/trait.impl/arbitrary/trait.Arbitrary.js
+++ docs/trait.impl/arbitrary/trait.Arbitrary.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["noir_ssa_fuzzer",[["impl&lt;'arbitrary&gt; Arbitrary&lt;'arbitrary&gt; for <a class=\"enum\" href=\"noir_ssa_fuzzer/typed_value/enum.ValueType.html\" title=\"enum noir_ssa_fuzzer::typed_value::ValueType\">ValueType</a>"]]]]);
+    var implementors = Object.fromEntries([["noir_ast_fuzzer",[["impl Arbitrary&lt;'_&gt; for <a class=\"struct\" href=\"noir_ast_fuzzer/compare/struct.CompareOptions.html\" title=\"struct noir_ast_fuzzer::compare::CompareOptions\">CompareOptions</a>"]]],["noir_ssa_fuzzer",[["impl&lt;'arbitrary&gt; Arbitrary&lt;'arbitrary&gt; for <a class=\"enum\" href=\"noir_ssa_fuzzer/typed_value/enum.ValueType.html\" title=\"enum noir_ssa_fuzzer::typed_value::ValueType\">ValueType</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[224]}
+//{"start":57,"fragment_lengths":[211,225]}
