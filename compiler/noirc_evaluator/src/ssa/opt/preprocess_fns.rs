96d4a73443c82fa4493d29c5a450a457d954025c
Maxim Vezenov <mvezenov@gmail.com>
2025-05-01 21:08:13 +0000

    actually call pre flat die in preprocess fns

--- compiler/noirc_evaluator/src/ssa/opt/preprocess_fns.rs
+++ compiler/noirc_evaluator/src/ssa/opt/preprocess_fns.rs
@@ -67,7 +67,7 @@ impl Ssa {
         // Remove any functions that have been inlined into others already.
         let ssa = self.remove_unreachable_functions();
         // Remove leftover instructions.
-        ssa.dead_instruction_elimination()
+        ssa.dead_instruction_elimination_pre_flattening()
     }
 }
 
