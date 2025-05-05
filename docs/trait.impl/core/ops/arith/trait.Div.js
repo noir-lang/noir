ca89a676dd87ab79d0e2d84c68ac9f3b20817ef5
rustdoc <github@users.noreply.github.com>
2025-05-05 19:15:34 +0000

    update rustdoc

--- docs/trait.impl/core/ops/arith/trait.Div.js
+++ docs/trait.impl/core/ops/arith/trait.Div.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["acir_field",[["impl&lt;F: PrimeField&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/ops/arith/trait.Div.html\" title=\"trait core::ops::arith::Div\">Div</a> for <a class=\"struct\" href=\"acir_field/struct.GenericFieldElement.html\" title=\"struct acir_field::GenericFieldElement\">FieldElement</a>&lt;F&gt;"]]]]);
+    var implementors = Object.fromEntries([["acir_field",[["impl&lt;F: PrimeField&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/ops/arith/trait.Div.html\" title=\"trait core::ops::arith::Div\">Div</a> for <a class=\"struct\" href=\"acir_field/struct.GenericFieldElement.html\" title=\"struct acir_field::GenericFieldElement\">FieldElement</a>&lt;F&gt;"]]],["noirc_frontend",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/ops/arith/trait.Div.html\" title=\"trait core::ops::arith::Div\">Div</a> for <a class=\"struct\" href=\"noirc_frontend/signed_field/struct.SignedField.html\" title=\"struct noirc_frontend::signed_field::SignedField\">SignedField</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[334]}
+//{"start":57,"fragment_lengths":[334,327]}
