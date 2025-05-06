a1de81fab3f91c02f0254c5a0b9d3c02906b9733
Maxim Vezenov <mvezenov@gmail.com>
2025-05-06 15:05:12 +0000

    fix tests

--- compiler/noirc_evaluator/src/ssa/opt/pure.rs
+++ compiler/noirc_evaluator/src/ssa/opt/pure.rs
@@ -333,9 +333,8 @@ mod test {
             acir(inline) fn pure_basic f6 {
               b0():
                 v0 = make_array [Field 0, Field 1] : [Field; 2]
-                v1 = array_get v0, index u32 1 -> Field
-                v2 = allocate -> &mut Field
-                store Field 0 at v2
+                v1 = allocate -> &mut Field
+                store Field 0 at v1
                 return
             }
 
@@ -408,9 +407,8 @@ mod test {
         acir(inline) pure fn pure_basic f6 {
           b0():
             v2 = make_array [Field 0, Field 1] : [Field; 2]
-            v4 = array_get v2, index u32 1 -> Field
-            v5 = allocate -> &mut Field
-            store Field 0 at v5
+            v3 = allocate -> &mut Field
+            store Field 0 at v3
             return
         }
         acir(inline) pure fn pure_recursive f7 {
