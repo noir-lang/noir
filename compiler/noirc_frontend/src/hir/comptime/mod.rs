9e83c5ff5c767c3425a46b7eff4ccc20262ec763
jfecher <jfecher11@gmail.com>
2025-05-08 10:32:31 -0500

    Update compiler/noirc_frontend/src/hir/comptime/mod.rs

--- compiler/noirc_frontend/src/hir/comptime/mod.rs
+++ compiler/noirc_frontend/src/hir/comptime/mod.rs
@@ -1,7 +1,8 @@
 //! The comptime interpreter is used to execute Noir code which is marked to
 //! be executed at compilation time. The main uses for this are:
-//!     - Metaprogramming such as deriving trait implementations.
-//!     - Evaluation of globals.
+//!     - Metaprogramming such as deriving trait implementations
+//!     - Interpreting comptime blocks, attribute functions, and macros
+//!     - Evaluation of globals
 //!
 //! This interpreter is run on the HIR while the program is being resolved and type-checked, and before it is monomorphized. Code written to be run at comptime may
 //! then require additional type hints compared to equivalent non-comptime Noir code.
