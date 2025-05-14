ae8e5263b87aaf8249a7a672ceef23553a374966
Akosh Farkash <aakoshh@gmail.com>
2025-05-14 11:38:41 +0100

    Fix can_be_main to be recursive

--- tooling/ast_fuzzer/src/program/mod.rs
+++ tooling/ast_fuzzer/src/program/mod.rs
@@ -425,6 +425,9 @@ impl Context {
                 break;
             }
         }
+        if is_main {
+            println!("TYPE FOR MAIN: {typ}");
+        }
         self.types.insert(typ.clone());
 
         Ok(typ)
