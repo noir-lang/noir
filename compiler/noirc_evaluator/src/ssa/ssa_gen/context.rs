ad375c21f560cb35ef099174bab3fcf59ca971ac
Ary Borenszweig <asterite@gmail.com>
2025-05-15 10:24:46 -0300

    fix: prevent overflow when casting negative integer to u128

--- compiler/noirc_evaluator/src/ssa/ssa_gen/context.rs
+++ compiler/noirc_evaluator/src/ssa/ssa_gen/context.rs
@@ -665,10 +665,19 @@ impl<'a> FunctionContext<'a> {
                         // value_sign is 1 if the value is positive, 0 otherwise
                         let value_sign =
                             self.builder.insert_binary(value_as_unsigned, BinaryOp::Lt, half_width);
-
+                        let max_for_incoming_type_size = if *incoming_type_size == 128 {
+                            u128::MAX
+                        } else {
+                            2_u128.pow(*incoming_type_size) - 1
+                        };
+                        let max_for_target_type_size = if target_type_size == 128 {
+                            u128::MAX
+                        } else {
+                            2_u128.pow(target_type_size) - 1
+                        };
                         let patch = self.builder.numeric_constant(
                             FieldElement::from(
-                                2_u128.pow(target_type_size) - 2_u128.pow(*incoming_type_size),
+                                max_for_target_type_size - max_for_incoming_type_size,
                             ),
                             NumericType::unsigned(target_type_size),
                         );
