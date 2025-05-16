e2b63d8900b2e0190597d3697d63ed7ff2f0774e
rustdoc <github@users.noreply.github.com>
2025-05-16 15:58:00 +0000

    update rustdoc

---
+++ docs/trait.impl/num_enum/trait.TryFromPrimitive.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["acir",[["impl TryFromPrimitive for <a class=\"enum\" href=\"acir/serialization/enum.Format.html\" title=\"enum acir::serialization::Format\">Format</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[157]}
