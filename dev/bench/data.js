684637c31aed3541a67cae641eb3d85d99801050
github-action-benchmark <github@users.noreply.github.com>
2025-05-20 13:30:45 +0000

    add ACVM Benchmarks (cargo) benchmark result for 4c6426eaa11cbc0a050616f59090652242290dc2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747740680319,
+  "lastUpdate": 1747747845442,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -18461,48 +18461,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": false,
-          "id": "27941c993754c784de9e6a0bec5b3ef991c8a730",
-          "message": "fix: incorrect parameter visibility location in monomorphized AST pri… (#8453)",
-          "timestamp": "2025-05-13T13:02:58Z",
-          "tree_id": "8657549ecb4c6b15398ad9aa31270e4879d338f7",
-          "url": "https://github.com/noir-lang/noir/commit/27941c993754c784de9e6a0bec5b3ef991c8a730"
-        },
-        "date": 1747142669977,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 269209,
-            "range": "± 723",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 240189,
-            "range": "± 2810",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 3234896,
-            "range": "± 6246",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "asterite@gmail.com",
-            "name": "Ary Borenszweig",
-            "username": "asterite"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
           "id": "28fa47e092b388fa8f56f1fad247591affa7d3e8",
           "message": "chore: rename regression test (#8483)",
           "timestamp": "2025-05-13T14:04:32Z",
@@ -20544,6 +20502,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 3564415,
             "range": "± 10521",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "asterite@gmail.com",
+            "name": "Ary Borenszweig",
+            "username": "asterite"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "4c6426eaa11cbc0a050616f59090652242290dc2",
+          "message": "chore: use insta for execution_success stdout (#8576)",
+          "timestamp": "2025-05-20T13:08:13Z",
+          "tree_id": "c2187a46ffc07c152010ce4754278dd5e5a5a55f",
+          "url": "https://github.com/noir-lang/noir/commit/4c6426eaa11cbc0a050616f59090652242290dc2"
+        },
+        "date": 1747747835583,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 255633,
+            "range": "± 1816",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 225467,
+            "range": "± 2315",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 3210218,
+            "range": "± 2072",
             "unit": "ns/iter"
           }
         ]
