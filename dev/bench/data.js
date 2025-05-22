6e89b6a08d31564805a507fd33a15662ccda729e
github-action-benchmark <github@users.noreply.github.com>
2025-05-22 11:56:16 +0000

    add ACVM Benchmarks (cargo) benchmark result for 79fb4823b47d3ec06d8d26b1bbf03c52fedcd2fa

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747914867883,
+  "lastUpdate": 1747914976518,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -17190,48 +17190,6 @@ window.BENCHMARK_DATA = {
             "name": "GitHub",
             "username": "web-flow"
           },
-          "distinct": true,
-          "id": "8cf48fc155fc4d7db6125a50545a45d461fb898a",
-          "message": "fix: parenthesized pattern, and correct 1-element tuple printing (#8482)",
-          "timestamp": "2025-05-14T12:25:14Z",
-          "tree_id": "f03e0e4edd7ca9fe2d3f714dcb6d55a1865bec3a",
-          "url": "https://github.com/noir-lang/noir/commit/8cf48fc155fc4d7db6125a50545a45d461fb898a"
-        },
-        "date": 1747226703347,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 281385,
-            "range": "± 542",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 251431,
-            "range": "± 9913",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 3603747,
-            "range": "± 5528",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "asterite@gmail.com",
-            "name": "Ary Borenszweig",
-            "username": "asterite"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
           "distinct": false,
           "id": "6f544928d9a9cac1ffd42082625fbea263964ff5",
           "message": "feat: primitive types are no longer keywords (#8470)\n\nCo-authored-by: AztecBot <tech@aztecprotocol.com>\nCo-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>",
@@ -19274,6 +19232,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 3210970,
             "range": "± 14557",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "84741533+defkit@users.noreply.github.com",
+            "name": "defkit",
+            "username": "defkit"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "79fb4823b47d3ec06d8d26b1bbf03c52fedcd2fa",
+          "message": "feat(ssa): ssa execution (#8135)\n\nCo-authored-by: defkit <84741533+jewelofchaos9@users.noreply.github.com>\nCo-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>",
+          "timestamp": "2025-05-22T11:34:47Z",
+          "tree_id": "b3069a037481210865ee71d3ef6e33e906adcf87",
+          "url": "https://github.com/noir-lang/noir/commit/79fb4823b47d3ec06d8d26b1bbf03c52fedcd2fa"
+        },
+        "date": 1747914966333,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 255025,
+            "range": "± 550",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 224362,
+            "range": "± 1198",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 3208807,
+            "range": "± 1359",
             "unit": "ns/iter"
           }
         ]
