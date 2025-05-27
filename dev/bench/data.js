4184c9a6202003f705a897bff3c61caa0e233121
github-action-benchmark <github@users.noreply.github.com>
2025-05-27 11:51:29 +0000

    add ACVM Benchmarks (cargo) benchmark result for 2a79d90851faea8c38dc50d994b2c2c7ba8201ff

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748345039155,
+  "lastUpdate": 1748346689201,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -15210,48 +15210,6 @@ window.BENCHMARK_DATA = {
             "name": "GitHub",
             "username": "web-flow"
           },
-          "distinct": false,
-          "id": "f0de22e22f28ebb45831c4972f77514235e3a0c9",
-          "message": "fix(frontend): Override to allow empty array input (#8568)",
-          "timestamp": "2025-05-19T15:16:46Z",
-          "tree_id": "8e3d77bdd97b1d482313da42324a96e8a7f802ad",
-          "url": "https://github.com/noir-lang/noir/commit/f0de22e22f28ebb45831c4972f77514235e3a0c9"
-        },
-        "date": 1747669021990,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 264770,
-            "range": "± 2605",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 232482,
-            "range": "± 7908",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 3579742,
-            "range": "± 4220",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "mvezenov@gmail.com",
-            "name": "Maxim Vezenov",
-            "username": "vezenovm"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
           "distinct": true,
           "id": "1feac18e7d54673f78d0d2d32c6c09d470a3841c",
           "message": "chore(ssa): Add weighted edges to call graph, move callers and callees methods to call graph (#8513)",
@@ -17294,6 +17252,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 3214547,
             "range": "± 29011",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "15848336+TomAFrench@users.noreply.github.com",
+            "name": "Tom French",
+            "username": "TomAFrench"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2a79d90851faea8c38dc50d994b2c2c7ba8201ff",
+          "message": "feat!: prevent compiling blake3 hashes which barretenberg cannot prove (#8690)",
+          "timestamp": "2025-05-27T11:14:28Z",
+          "tree_id": "c30b61196176132e139a47c23266da24c61fcd78",
+          "url": "https://github.com/noir-lang/noir/commit/2a79d90851faea8c38dc50d994b2c2c7ba8201ff"
+        },
+        "date": 1748346678366,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 254235,
+            "range": "± 1077",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 223381,
+            "range": "± 4195",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 3585666,
+            "range": "± 5523",
             "unit": "ns/iter"
           }
         ]
