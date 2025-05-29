69a662bd8992c1111d5eb6ee1a6c81202620dcb0
Ratmir Karabut <rkarabut@gmail.com>
2025-05-29 12:31:32 +0300

    Fix merge

--- tooling/ast_fuzzer/fuzz/src/targets/comptime_vs_brillig.rs
+++ tooling/ast_fuzzer/fuzz/src/targets/comptime_vs_brillig.rs
@@ -21,8 +21,6 @@ pub fn fuzz(u: &mut Unstructured) -> eyre::Result<()> {
         avoid_err_by_zero: true,
         // and it gets old to have to edit u128 to fit into u32 for the frontend to parse
         avoid_large_int_literals: true,
-        // Avoid break/continue (until #8382 is merged)
-        avoid_loop_control: true,
         // Has to only use expressions valid in comptime
         comptime_friendly: true,
         // Force brillig
