f2021b897aeb5a1d3446bd1e70e54039ab59c00b
github-action-benchmark <github@users.noreply.github.com>
2025-05-30 19:01:53 +0000

    add Compilation Memory (customSmallerIsBetter) benchmark result for d6e4ce9cb2e74c4239843ff57c22b56fac4530fe

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748631609267,
+  "lastUpdate": 1748631712814,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
-      {
-        "commit": {
-          "author": {
-            "email": "mvezenov@gmail.com",
-            "name": "Maxim Vezenov",
-            "username": "vezenovm"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
-          "id": "0031c313ed9358d3589a98ae812330fa114286ab",
-          "message": "chore(ci): Update expected failures for noir_rsa  (#8608)",
-          "timestamp": "2025-05-21T19:56:00Z",
-          "tree_id": "b42ccf9ed162a9debee64640b157056cca6b0c9d",
-          "url": "https://github.com/noir-lang/noir/commit/0031c313ed9358d3589a98ae812330fa114286ab"
-        },
-        "date": 1747858684929,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "semaphore_depth_10",
-            "value": 128.35,
-            "unit": "MB"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -3244,6 +3215,85 @@ window.BENCHMARK_DATA = {
           {
             "name": "rollup-root",
             "value": 391.89,
+            "unit": "MB"
+          },
+          {
+            "name": "semaphore_depth_10",
+            "value": 106.4,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "asterite@gmail.com",
+            "name": "Ary Borenszweig",
+            "username": "asterite"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d6e4ce9cb2e74c4239843ff57c22b56fac4530fe",
+          "message": "fix: cast signed to u1 follow-up (#8730)",
+          "timestamp": "2025-05-30T18:30:18Z",
+          "tree_id": "01b2a3f7dbed49b2238996373b8199f8a74b98bd",
+          "url": "https://github.com/noir-lang/noir/commit/d6e4ce9cb2e74c4239843ff57c22b56fac4530fe"
+        },
+        "date": 1748631702416,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "private-kernel-inner",
+            "value": 292.39,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-reset",
+            "value": 533.03,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-tail",
+            "value": 192.3,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-base-private",
+            "value": 1390,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-base-public",
+            "value": 1550,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-root-empty",
+            "value": 353.42,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-root-single-tx",
+            "value": 7160,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-root",
+            "value": 7170,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-merge",
+            "value": 337.62,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-root",
+            "value": 391.91,
             "unit": "MB"
           },
           {
