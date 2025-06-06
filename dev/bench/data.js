ed2edd52e97c6df2d0272d462b20daac108309a0
github-action-benchmark <github@users.noreply.github.com>
2025-06-06 17:15:48 +0000

    add ACVM Benchmarks (cargo) benchmark result for 11d3d339a6a0e495dbcf023a0f881dcec2737bd4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749228986689,
+  "lastUpdate": 1749230147565,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -18471,48 +18471,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "aakoshh@gmail.com",
-            "name": "Akosh Farkash",
-            "username": "aakoshh"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "f8f230d229115fd8cf3278098929f4d89c1900b7",
-          "message": "chore(fuzz): Fuzz the SSA parser (#8647)",
-          "timestamp": "2025-05-28T16:22:22Z",
-          "tree_id": "dc2f8b351155173f3821cf2a3bd220f94e116b71",
-          "url": "https://github.com/noir-lang/noir/commit/f8f230d229115fd8cf3278098929f4d89c1900b7"
-        },
-        "date": 1748450595765,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 267304,
-            "range": "± 303",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 233441,
-            "range": "± 1310",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 3235034,
-            "range": "± 17260",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "asterite@gmail.com",
             "name": "Ary Borenszweig",
             "username": "asterite"
@@ -20564,6 +20522,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 2780062,
             "range": "± 1825",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "asterite@gmail.com",
+            "name": "Ary Borenszweig",
+            "username": "asterite"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "11d3d339a6a0e495dbcf023a0f881dcec2737bd4",
+          "message": "fix: bind self generic type in trait calls via a concrete type (#8825)",
+          "timestamp": "2025-06-06T16:42:02Z",
+          "tree_id": "cac03c3bc6906e84ec0b7a87c4b1425e61fb718f",
+          "url": "https://github.com/noir-lang/noir/commit/11d3d339a6a0e495dbcf023a0f881dcec2737bd4"
+        },
+        "date": 1749230136927,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 252902,
+            "range": "± 594",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 222483,
+            "range": "± 5304",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 2816285,
+            "range": "± 2843",
             "unit": "ns/iter"
           }
         ]
