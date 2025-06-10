99dc150e55b785373bddbefd67ae978d8314a299
Akosh Farkash <aakoshh@gmail.com>
2025-06-10 18:39:30 +0100

    Use proptest to run tests determinitically

--- tooling/ast_fuzzer/fuzz/src/targets/mod.rs
+++ tooling/ast_fuzzer/fuzz/src/targets/mod.rs
@@ -6,10 +6,12 @@ pub mod pass_vs_prev;
 
 #[cfg(test)]
 mod tests {
-    use std::time::Duration;
+
+    use std::time::{Duration, Instant};
 
     use arbitrary::Unstructured;
     use color_eyre::eyre;
+    use proptest::prelude::*;
 
     pub fn seed_from_env() -> Option<u64> {
         let Ok(seed) = std::env::var("NOIR_ARBTEST_SEED") else { return None };
@@ -42,18 +44,55 @@ mod tests {
     pub fn fuzz_with_arbtest(f: impl Fn(&mut Unstructured) -> eyre::Result<()>) {
         let _ = env_logger::try_init();
 
-        let mut prop = arbtest::arbtest(|u| {
+        if let Some(seed) = seed_from_env() {
+            run_reproduce(f, seed);
+        } else {
+            run_deterministic(f);
+        }
+    }
+
+    /// Reproduce the result of a single seed.
+    fn run_reproduce(f: impl Fn(&mut Unstructured) -> eyre::Result<()>, seed: u64) {
+        arbtest::arbtest(|u| {
             f(u).unwrap();
             Ok(())
         })
-        .budget(Duration::from_secs(20))
-        .size_min(1 << 12)
-        .size_max(1 << 20);
+        .seed(seed)
+        .run();
+    }
 
-        if let Some(seed) = seed_from_env() {
-            prop = prop.seed(seed);
-        }
+    /// Run multiple tests with a deterministic RNG.
+    fn run_deterministic(f: impl Fn(&mut Unstructured) -> eyre::Result<()>) {
+        // Comptime tests run slower than others.
+        let start = Instant::now();
+        let timeout = Duration::from_secs(20);
 
-        prop.run();
+        let config = proptest::test_runner::Config {
+            cases: 1000,
+            failure_persistence: None,
+            ..Default::default()
+        };
+        let rng = proptest::test_runner::TestRng::deterministic_rng(config.rng_algorithm);
+        let mut runner = proptest::test_runner::TestRunner::new_with_rng(config, rng);
+
+        runner
+            .run(&seed_strategy(), |seed| {
+                if start.elapsed() < timeout {
+                    run_reproduce(&f, seed);
+                }
+                Ok(())
+            })
+            .unwrap();
+    }
+
+    /// Generate seeds for `arbtest` where the top 32 bits are random and the lower 32 bits represent the input size.
+    fn seed_strategy() -> proptest::strategy::BoxedStrategy<u64> {
+        let min_size: u32 = 1 << 12;
+        let max_size: u32 = 1 << 20;
+        (min_size..max_size)
+            .prop_flat_map(move |size| {
+                any::<u64>().prop_map(move |raw| (size as u64) | (raw << u32::BITS))
+            })
+            .boxed()
     }
 }
