1b07e11fded019709c55f7fa871fba9ec1577439
guipublic <guipublic@gmail.com>
2025-06-12 09:28:18 +0000

    code review: add a unit test

--- compiler/noirc_evaluator/src/ssa/opt/loop_invariant.rs
+++ compiler/noirc_evaluator/src/ssa/opt/loop_invariant.rs
@@ -989,7 +989,6 @@ fn can_be_hoisted(
         }
 
         // A cast may have dependence on a range-check, which may not be hoisted, so we cannot always hoist a cast.
-        // Since at the end a cast is a no-op, not hoisting a cast should not have much impact.
         Cast(_, _) | Constrain(..) | ConstrainNotEqual(..) | RangeCheck { .. } => {
             hoist_with_predicate
         }
@@ -2580,5 +2579,114 @@ mod control_dependence {
         assert_eq!(lhs_id.to_u32(), 8);
 
         assert_normalized_ssa_equals(ssa, src);
+    }
+
+    #[test]
+    fn do_not_hoist_control_dependent_cast() {
+        // We want to check the case that a cast under a predicate in a loop is not hoisted
+        //
+        // This is the SSA for the following program:
+        // ```noir
+        // fn main(a: bool, c: i8) -> pub i16 {
+        //     for _ in 0..1 {
+        //         if a {
+        //             let _ = c * 127;
+        //         };
+        //     }
+        //     3
+        // }
+        // ```
+        // Although `c*127` is loop invariant, the overflow checks of the multiplication must not be hoisted from the conditional `if a {..}`
+        // They are code-gen as:
+        //    `range_check v23 to 8 bits`
+        //    `v24 = cast v23 as u8`
+
+        let src = r"
+        acir(inline) impure fn main f0 {
+  b0(v0: u1, v1: i8):
+    jmp b1(u32 0)
+  b1(v2: u32):
+    v4 = eq v2, u32 0
+    jmpif v4 then: b2, else: b3
+  b2():
+    jmpif v0 then: b4, else: b5
+  b3():
+    return i16 3
+  b4():
+    v7 = mul v1, i8 127
+    v8 = cast v7 as u16
+    v9 = truncate v8 to 8 bits, max_bit_size: 16
+    v10 = cast v1 as u8
+    v12 = lt v10, u8 128
+    v13 = not v12
+    v14 = cast v1 as Field
+    v15 = cast v12 as Field
+    v16 = mul v15, v14
+    v18 = sub Field 256, v14
+    v19 = cast v13 as Field
+    v20 = mul v19, v18
+    v21 = add v16, v20
+    v23 = mul v21, Field 127
+    range_check v23 to 8 bits
+    v24 = cast v23 as u8
+    v25 = not v12
+    v26 = cast v25 as u8
+    v27 = unchecked_add u8 128, v26
+    v28 = lt v24, v27
+    constrain v28 == u1 1
+    v30 = cast v9 as i8
+    jmp b5()
+  b5():
+    v33 = unchecked_add v2, u32 1
+    jmp b1(v33)
+}
+    ";
+
+        let ssa = Ssa::from_str(src).unwrap();
+        let ssa = ssa.loop_invariant_code_motion();
+
+        // We expect `v24 = cast v23 as u8` not to be hoisted and be kept in block `b4`.
+        // If we were to hoist that cast to the outer loop's header, we would get potentially
+        // an unsafe cast. It must stay just after the range-check.
+        assert_ssa_snapshot!(ssa, @r"
+acir(inline) impure fn main f0 {
+  b0(v0: u1, v1: i8):
+    v4 = mul v1, i8 127
+    jmp b1(u32 0)
+  b1(v2: u32):
+    v6 = eq v2, u32 0
+    jmpif v6 then: b2, else: b3
+  b2():
+    jmpif v0 then: b4, else: b5
+  b3():
+    return i16 3
+  b4():
+    v8 = cast v4 as u16
+    v9 = truncate v8 to 8 bits, max_bit_size: 16
+    v10 = cast v1 as u8
+    v12 = lt v10, u8 128
+    v13 = not v12
+    v14 = cast v1 as Field
+    v15 = cast v12 as Field
+    v16 = mul v15, v14
+    v18 = sub Field 256, v14
+    v19 = cast v13 as Field
+    v20 = mul v19, v18
+    v21 = add v16, v20
+    v23 = mul v21, Field 127
+    range_check v23 to 8 bits
+    v24 = cast v23 as u8
+    v25 = not v12
+    v26 = cast v25 as u8
+    v27 = unchecked_add u8 128, v26
+    v28 = lt v24, v27
+    constrain v28 == u1 1
+    v30 = cast v9 as i8
+    jmp b5()
+  b5():
+    v32 = unchecked_add v2, u32 1
+    jmp b1(v32)
+}
+        ");
     }
 }
