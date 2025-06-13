29adaaafcbc90b1b1f17d66865bfff966471910f
Akosh Farkash <aakoshh@gmail.com>
2025-06-13 15:12:54 +0100

    Track which variables are derived form dynamic input

--- tooling/ast_fuzzer/src/lib.rs
+++ tooling/ast_fuzzer/src/lib.rs
@@ -83,7 +83,6 @@ impl Default for Config {
             ("call", 15),
         ]);
         let stmt_freqs_acir = Freqs::new(&[
-            ("drop", 0), // The `ownership` module says it will insert `Drop` and `Clone`.
             ("assign", 30),
             ("if", 10),
             ("for", 22),
@@ -92,7 +91,6 @@ impl Default for Config {
             ("constrain", 5),
         ]);
         let stmt_freqs_brillig = Freqs::new(&[
-            ("drop", 0),
             ("break", 20),
             ("continue", 20),
             ("assign", 30),
