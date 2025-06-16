c91a5b659730cf70f243375db22c91c3e9cd5419
Oana Barbu <oana@ata.network>
2025-06-17 00:41:59 +0200

    remove acvm

--- acvm-repo/bn254_blackbox_solver/src/generator/mod.rs
+++
@@ -1,8 +0,0 @@
-//! This module is adapted from the [Barustenberg][barustenberg] Rust implementation of the Barretenberg library.
-//!
-//! Code is used under the MIT license
-//!
-//! [barustenberg]: https://github.com/laudiacay/barustenberg/blob/df6bc6f095fe7f288bf6a12e7317fd8eb33d68ae/
-
-pub(crate) mod generators;
-mod hash_to_curve;
