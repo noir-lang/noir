1cc4aa2d10c8e52db51c37cf489d7b95e6ba4873
jewelofchaos9 <jewelofchaos9@gmail.com>
2025-06-18 13:22:05 +0000

    init

--- compiler/noirc_evaluator/src/ssa/opt/check_u128_mul_overflow.rs
+++ compiler/noirc_evaluator/src/ssa/opt/check_u128_mul_overflow.rs
@@ -89,7 +89,7 @@ fn check_u128_mul_overflow(
     }
 
     let u128 = NumericType::unsigned(128);
-    let two_pow_64 = dfg.make_constant(two_pow_64.into(), u128);
+    let two_pow_64_plus_1 = dfg.make_constant((two_pow_64 + 1).into(), u128);
 
     let res = if lhs_value.is_some() && rhs_value.is_some() {
         // If both values are known at compile time, at this point we know it overflows
@@ -97,30 +97,51 @@ fn check_u128_mul_overflow(
     } else if lhs_value.is_some() {
         // If only the left-hand side is known we just need to check that the right-hand side
         // isn't greater than 2^64
-        let instruction =
-            Instruction::Binary(Binary { lhs: rhs, rhs: two_pow_64, operator: BinaryOp::Div });
-        dfg.insert_instruction_and_results(instruction, block, None, call_stack).first()
+        let instruction = Instruction::Binary(Binary {
+            lhs: rhs,
+            rhs: two_pow_64_plus_1,
+            operator: BinaryOp::Lt,
+        });
+        let rhs_less_than_two_pow_64_plus_1 =
+            dfg.insert_instruction_and_results(instruction, block, None, call_stack).first();
+        let not = Instruction::Not(rhs_less_than_two_pow_64_plus_1);
+        dfg.insert_instruction_and_results(not, block, None, call_stack).first()
     } else if rhs_value.is_some() {
         // Same goes for the other side
         let instruction =
-            Instruction::Binary(Binary { lhs, rhs: two_pow_64, operator: BinaryOp::Div });
-        dfg.insert_instruction_and_results(instruction, block, None, call_stack).first()
+            Instruction::Binary(Binary { lhs, rhs: two_pow_64_plus_1, operator: BinaryOp::Lt });
+        let lhs_less_than_two_pow_64_plus_1 =
+            dfg.insert_instruction_and_results(instruction, block, None, call_stack).first();
+        let not = Instruction::Not(lhs_less_than_two_pow_64_plus_1);
+        dfg.insert_instruction_and_results(not, block, None, call_stack).first()
     } else {
         // Check both sides
         let instruction =
-            Instruction::Binary(Binary { lhs, rhs: two_pow_64, operator: BinaryOp::Div });
-        let divided_lhs =
+            Instruction::Binary(Binary { lhs, rhs: two_pow_64_plus_1, operator: BinaryOp::Lt });
+        let lhs_less_than_two_pow_64_plus_1 =
             dfg.insert_instruction_and_results(instruction, block, None, call_stack).first();
+        let not = Instruction::Not(lhs_less_than_two_pow_64_plus_1);
+        let lhs_greater_than_two_pow_64 =
+            dfg.insert_instruction_and_results(not, block, None, call_stack).first();
 
-        let instruction =
-            Instruction::Binary(Binary { lhs: rhs, rhs: two_pow_64, operator: BinaryOp::Div });
-        let divided_rhs =
+        let instruction = Instruction::Binary(Binary {
+            lhs: rhs,
+            rhs: two_pow_64_plus_1,
+            operator: BinaryOp::Lt,
+        });
+        let rhs_less_than_two_pow_64_plus_1 =
             dfg.insert_instruction_and_results(instruction, block, None, call_stack).first();
+        let not = Instruction::Not(rhs_less_than_two_pow_64_plus_1);
+        let rhs_greater_than_two_pow_64 =
+            dfg.insert_instruction_and_results(not, block, None, call_stack).first();
 
         // Unchecked as operands are restricted to be less than 2^64 so multiplying them cannot overflow.
         let mul = BinaryOp::Mul { unchecked: true };
-        let instruction =
-            Instruction::Binary(Binary { lhs: divided_lhs, rhs: divided_rhs, operator: mul });
+        let instruction = Instruction::Binary(Binary {
+            lhs: lhs_greater_than_two_pow_64,
+            rhs: rhs_greater_than_two_pow_64,
+            operator: mul,
+        });
         dfg.insert_instruction_and_results(instruction, block, None, call_stack).first()
     };
 
@@ -186,7 +207,8 @@ mod tests {
         acir(inline) fn main f0 {
           b0(v0: u128):
             v2 = mul v0, u128 18446744073709551617
-            v4 = div v0, u128 18446744073709551616
+            v3 = lt v0, u128 18446744073709551617
+            v4 = not v3
             constrain v4 == u128 0, "attempt to multiply with overflow"
             return
         }
@@ -209,7 +231,8 @@ mod tests {
         acir(inline) fn main f0 {
           b0(v0: u128):
             v2 = mul u128 18446744073709551617, v0
-            v4 = div v0, u128 18446744073709551616
+            v3 = lt v0, u128 18446744073709551617
+            v4 = not v3
             constrain v4 == u128 0, "attempt to multiply with overflow"
             return
         }
@@ -232,10 +255,12 @@ mod tests {
         acir(inline) fn main f0 {
           b0(v0: u128, v1: u128):
             v2 = mul v0, v1
-            v4 = div v0, u128 18446744073709551616
-            v5 = div v1, u128 18446744073709551616
-            v6 = unchecked_mul v4, v5
-            constrain v6 == u128 0, "attempt to multiply with overflow"
+            v4 = lt v0, u128 18446744073709551617
+            v5 = not v4
+            v6 = lt v1, u128 18446744073709551617
+            v7 = not v6
+            v8 = unchecked_mul v5, v7
+            constrain v8 == u128 0, "attempt to multiply with overflow"
             return
         }
         "#);
