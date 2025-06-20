07d4e11c07bbaf3e66edc06061c9b272545a48b4
github-action-benchmark <github@users.noreply.github.com>
2025-06-20 15:47:03 +0000

    add ACVM Benchmarks (cargo) benchmark result for 5845dc5aaf039d71d45700bb6784b4df7da03bae

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750431389993,
+  "lastUpdate": 1750434423164,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -18606,48 +18606,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "47281315+guipublic@users.noreply.github.com",
-            "name": "guipublic",
-            "username": "guipublic"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
-          "id": "98d19fb4d61fd3b4f6420862af2d5f7af8a30f0f",
-          "message": "fix: do not hoist control dependent cast (#8886)",
-          "timestamp": "2025-06-12T13:33:21Z",
-          "tree_id": "10dcd7ce24c9840b64f859dae449b9a419f3c837",
-          "url": "https://github.com/noir-lang/noir/commit/98d19fb4d61fd3b4f6420862af2d5f7af8a30f0f"
-        },
-        "date": 1749736865097,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 249601,
-            "range": "± 350",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 221233,
-            "range": "± 5564",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 2785241,
-            "range": "± 3351",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "15848336+TomAFrench@users.noreply.github.com",
             "name": "Tom French",
             "username": "TomAFrench"
@@ -20699,6 +20657,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 2788895,
             "range": "± 8654",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aakoshh@gmail.com",
+            "name": "Akosh Farkash",
+            "username": "aakoshh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5845dc5aaf039d71d45700bb6784b4df7da03bae",
+          "message": "fix(LICM): Consider negative loop bounds before hoisting `div <const> <induction-var>` (#8986)",
+          "timestamp": "2025-06-20T15:23:43Z",
+          "tree_id": "76e81fdb0c31a247d609f46e04fe61f68e70a2a9",
+          "url": "https://github.com/noir-lang/noir/commit/5845dc5aaf039d71d45700bb6784b4df7da03bae"
+        },
+        "date": 1750434410415,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 255947,
+            "range": "± 1103",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 224694,
+            "range": "± 2439",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 2787518,
+            "range": "± 21554",
             "unit": "ns/iter"
           }
         ]
