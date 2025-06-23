216941fed312411d4a1a978811888113bda09f67
Ary Borenszweig <asterite@gmail.com>
2025-06-23 12:47:47 -0300

    Remove unreachable instructions in the first SSA pass

--- compiler/noirc_evaluator/src/ssa.rs
+++ compiler/noirc_evaluator/src/ssa.rs
@@ -138,6 +138,7 @@ pub struct ArtifactsAndWarnings(pub Artifacts, pub Vec<SsaReport>);
 /// something we take can advantage of in the [secondary_passes].
 pub fn primary_passes(options: &SsaEvaluatorOptions) -> Vec<SsaPass> {
     vec![
+        SsaPass::new(Ssa::remove_unreachable_instructions, "Remove Unreachable Instructions"),
         SsaPass::new(Ssa::remove_unreachable_functions, "Removing Unreachable Functions"),
         SsaPass::new(Ssa::defunctionalize, "Defunctionalization"),
         SsaPass::new(Ssa::inline_simple_functions, "Inlining simple functions"),
