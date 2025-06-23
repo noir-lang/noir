5e4df45c72af8445fa60877fe2e82083b5428d39
github-action-benchmark <github@users.noreply.github.com>
2025-06-23 15:12:58 +0000

    add ACVM Benchmarks (cargo) benchmark result for 01b9e1665c04e97ce9cb5e320adaa2b0c7dc60cd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750683753023,
+  "lastUpdate": 1750691578361,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -18606,48 +18606,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "asterite@gmail.com",
-            "name": "Ary Borenszweig",
-            "username": "asterite"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "53bd661af5be4bdf683225f2ec6cb8798a722415",
-          "message": "fix: catch unbound type variables during frontend compilation (#8686)\n\nCo-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>",
-          "timestamp": "2025-06-12T17:09:08Z",
-          "tree_id": "dd2138b62a7330cc9ef2d984e857851d86bf57c2",
-          "url": "https://github.com/noir-lang/noir/commit/53bd661af5be4bdf683225f2ec6cb8798a722415"
-        },
-        "date": 1749749572904,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 250682,
-            "range": "± 684",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 222796,
-            "range": "± 1437",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 2784621,
-            "range": "± 50705",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "aakoshh@gmail.com",
             "name": "Akosh Farkash",
             "username": "aakoshh"
@@ -20699,6 +20657,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 2786647,
             "range": "± 6268",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aakoshh@gmail.com",
+            "name": "Akosh Farkash",
+            "username": "aakoshh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "01b9e1665c04e97ce9cb5e320adaa2b0c7dc60cd",
+          "message": "fix(fuzz): Use indirection for taking `&mut` over an array element (#8992)",
+          "timestamp": "2025-06-23T14:49:30Z",
+          "tree_id": "7fbf067ac53681d08507bf8b91852c9a732afdb8",
+          "url": "https://github.com/noir-lang/noir/commit/01b9e1665c04e97ce9cb5e320adaa2b0c7dc60cd"
+        },
+        "date": 1750691564740,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 263684,
+            "range": "± 262",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 230242,
+            "range": "± 7080",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 2812795,
+            "range": "± 7917",
             "unit": "ns/iter"
           }
         ]
