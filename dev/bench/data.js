8977d0dc41c20ac7f4826cf329ba89e6e4a0ff5e
github-action-benchmark <github@users.noreply.github.com>
2025-06-23 13:02:29 +0000

    add Execution Memory (customSmallerIsBetter) benchmark result for 309d44549e225be52ce070802875650300619298

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750683271652,
+  "lastUpdate": 1750683748689,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -11362,75 +11362,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "15848336+TomAFrench@users.noreply.github.com",
-            "name": "Tom French",
-            "username": "TomAFrench"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "945ea6e11d5ca89cfb62bc8e7dc5dda16fefd512",
-          "message": "fix: preserve functions which are used in `array_set` instructions (#8891)",
-          "timestamp": "2025-06-12T15:30:56Z",
-          "tree_id": "8cf216331194f398ce0c56f4f79905d8f238cae2",
-          "url": "https://github.com/noir-lang/noir/commit/945ea6e11d5ca89cfb62bc8e7dc5dda16fefd512"
-        },
-        "date": 1749744727400,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "private-kernel-inner",
-            "value": 202.15,
-            "unit": "MB"
-          },
-          {
-            "name": "private-kernel-reset",
-            "value": 225.87,
-            "unit": "MB"
-          },
-          {
-            "name": "private-kernel-tail",
-            "value": 177.41,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-base-private",
-            "value": 489.94,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-base-public",
-            "value": 423.25,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-block-root",
-            "value": 1400,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-merge",
-            "value": 312.07,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-root",
-            "value": 317.76,
-            "unit": "MB"
-          },
-          {
-            "name": "semaphore_depth_10",
-            "value": 71.02,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "asterite@gmail.com",
             "name": "Ary Borenszweig",
             "username": "asterite"
@@ -14760,6 +14691,75 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/noir-lang/noir/commit/4e8654353b0f10270a32fe9c6cc7dda1c85234f0"
         },
         "date": 1750675273468,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "private-kernel-inner",
+            "value": 202.36,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-reset",
+            "value": 226.11,
+            "unit": "MB"
+          },
+          {
+            "name": "private-kernel-tail",
+            "value": 177.65,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-base-private",
+            "value": 507.43,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-base-public",
+            "value": 440.62,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-block-root",
+            "value": 1510,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-merge",
+            "value": 329.71,
+            "unit": "MB"
+          },
+          {
+            "name": "rollup-root",
+            "value": 331.81,
+            "unit": "MB"
+          },
+          {
+            "name": "semaphore_depth_10",
+            "value": 71.02,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "15848336+TomAFrench@users.noreply.github.com",
+            "name": "Tom French",
+            "username": "TomAFrench"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "309d44549e225be52ce070802875650300619298",
+          "message": "chore: remove unreachable functions after inlining simple functions (#8987)",
+          "timestamp": "2025-06-23T12:23:36Z",
+          "tree_id": "3d3c7fdb771610f16588a9db5b4ccd316972d204",
+          "url": "https://github.com/noir-lang/noir/commit/309d44549e225be52ce070802875650300619298"
+        },
+        "date": 1750683736303,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
