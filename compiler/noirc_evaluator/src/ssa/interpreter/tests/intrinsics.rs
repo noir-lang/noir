9f131846ac214eede68a0767f2c7ba5f959a7b4f
Akosh Farkash <aakoshh@gmail.com>
2025-06-26 14:56:44 +0100

    Add unit test for print

--- compiler/noirc_evaluator/src/ssa/interpreter/tests/intrinsics.rs
+++ compiler/noirc_evaluator/src/ssa/interpreter/tests/intrinsics.rs
@@ -1,5 +1,5 @@
 use crate::ssa::interpreter::{
-    tests::expect_value,
+    tests::{expect_printed_output, expect_value},
     value::{NumericValue, Value},
 };
 
@@ -50,4 +50,61 @@ fn as_witness() {
     ",
     );
     assert_eq!(value, Value::Numeric(NumericValue::Field(1_u128.into())));
+}
+
+#[test]
+fn print() {
+    let src = r#"
+        acir(inline) fn main f0 {
+          b0():
+            call f1(u8 123)
+            v4 = make_array [Field 1, Field 2] : [Field; 2]
+            call f2(i8 0, u1 1, v4)
+            v16 = make_array b"hello {a} == {a}"
+            call f3(v16, Field 2, i8 0, u1 1, v4, i8 0, u1 1, v4)
+            return
+        }
+        acir(inline) fn println f1 {
+          b0(v0: u8):
+            call f6(u1 1, v0)
+            return
+        }
+        acir(inline) fn println f2 {
+          b0(v0: i8, v1: u1, v2: [Field; 2]):
+            call f5(u1 1, v0, v1, v2)
+            return
+        }
+        acir(inline) fn println f3 {
+          b0(v0: [u8; 16], v1: Field, v2: i8, v3: u1, v4: [Field; 2], v5: i8, v6: u1, v7: [Field; 2]):
+            call f4(u1 1, v0, v1, v2, v3, v4, v5, v6, v7)
+            return
+        }
+        brillig(inline) fn print_unconstrained f4 {
+          b0(v0: u1, v1: [u8; 16], v2: Field, v3: i8, v4: u1, v5: [Field; 2], v6: i8, v7: u1, v8: [Field; 2]):
+            v37 = make_array b"{\"kind\":\"tuple\",\"types\":[{\"kind\":\"signedinteger\",\"width\":8},{\"kind\":\"boolean\"},{\"kind\":\"array\",\"length\":2,\"type\":{\"kind\":\"field\"}}]}"
+            v38 = make_array b"{\"kind\":\"tuple\",\"types\":[{\"kind\":\"signedinteger\",\"width\":8},{\"kind\":\"boolean\"},{\"kind\":\"array\",\"length\":2,\"type\":{\"kind\":\"field\"}}]}"
+            call print(v0, v1, v2, v3, v4, v5, v6, v7, v8, v37, v38, u1 1)
+            return
+        }
+        brillig(inline) fn print_unconstrained f5 {
+          b0(v0: u1, v1: i8, v2: u1, v3: [Field; 2]):
+            v32 = make_array b"{\"kind\":\"tuple\",\"types\":[{\"kind\":\"signedinteger\",\"width\":8},{\"kind\":\"boolean\"},{\"kind\":\"array\",\"length\":2,\"type\":{\"kind\":\"field\"}}]}"
+            call print(v0, v1, v2, v3, v32, u1 0)
+            return
+        }
+        brillig(inline) fn print_unconstrained f6 {
+          b0(v0: u1, v1: u8):
+            v20 = make_array b"{\"kind\":\"unsignedinteger\",\"width\":8}"
+            call print(v0, v1, v20, u1 0)
+            return
+        }
+    "#;
+
+    let printed_output = expect_printed_output(src);
+
+    insta::assert_snapshot!(printed_output, @"
+    123
+    (0, true, [0x01, 0x02])
+    hello (0, true, [0x01, 0x02]) == (0, true, [0x01, 0x02])
+    ")
 }
