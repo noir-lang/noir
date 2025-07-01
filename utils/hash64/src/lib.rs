f5edbf4ca323f0c17c8e4334706e3bc2889b620e
Tom French <15848336+TomAFrench@users.noreply.github.com>
2025-07-01 15:07:00 +0000

    chore: use `BuildHasher` in `hash64` crate

--- utils/hash64/src/lib.rs
+++ utils/hash64/src/lib.rs
@@ -1,7 +1,5 @@
 /// Utility to return the 64-bit hash of a value using rustc-hash.
 pub fn hash64<T: std::hash::Hash + ?Sized>(v: &T) -> u64 {
-    use std::hash::Hasher;
-    let mut state = rustc_hash::FxHasher::default();
-    v.hash(&mut state);
-    state.finish()
+    use std::hash::BuildHasher;
+    rustc_hash::FxBuildHasher.hash_one(v)
 }
