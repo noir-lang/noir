f3f9cbc201b462222a6e35fe740ac59a4728004c
github-action-benchmark <github@users.noreply.github.com>
2025-07-01 18:04:13 +0000

    add ACVM Benchmarks (cargo) benchmark result for be74580c5621683d4239ac76886d9ccb6ea50ca9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751390600839,
+  "lastUpdate": 1751393053235,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -18651,48 +18651,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": false,
-          "id": "06c73d7c6390fb5030353f5b82089b84cf415d75",
-          "message": "fix: better package name validation and error messages for nargo new/init (#8978)",
-          "timestamp": "2025-06-20T14:12:49Z",
-          "tree_id": "6296b347540474fddb5a097e89b7406f322a5a7d",
-          "url": "https://github.com/noir-lang/noir/commit/06c73d7c6390fb5030353f5b82089b84cf415d75"
-        },
-        "date": 1750430182752,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 255334,
-            "range": "± 1284",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 222081,
-            "range": "± 2352",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 2786586,
-            "range": "± 3698",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "asterite@gmail.com",
-            "name": "Ary Borenszweig",
-            "username": "asterite"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
           "id": "256c67899ade021c0140a204dd00801eaebda055",
           "message": "fix: check \"negate with overflow\" in comptime code + allow u1 to be used in comptime code (#8969)\n\nCo-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>",
           "timestamp": "2025-06-20T14:13:38Z",
@@ -20734,6 +20692,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 2777802,
             "range": "± 6641",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "asterite@gmail.com",
+            "name": "Ary Borenszweig",
+            "username": "asterite"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "be74580c5621683d4239ac76886d9ccb6ea50ca9",
+          "message": "fix: revert #9044 (#9080)",
+          "timestamp": "2025-07-01T17:38:43Z",
+          "tree_id": "c75d661f00cb22c6b87241917d90194eb5344c6e",
+          "url": "https://github.com/noir-lang/noir/commit/be74580c5621683d4239ac76886d9ccb6ea50ca9"
+        },
+        "date": 1751393040059,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 253141,
+            "range": "± 1261",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 223332,
+            "range": "± 5168",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 2779023,
+            "range": "± 1267",
             "unit": "ns/iter"
           }
         ]
