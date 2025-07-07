e03f3984e3e76739158621b21a702b703869c06f
guipublic <47281315+guipublic@users.noreply.github.com>
2025-07-07 16:36:47 +0200

    chore!: disallow casting signed integers to field (#8818)

--- compiler/noirc_frontend/src/tests.rs
+++ compiler/noirc_frontend/src/tests.rs
@@ -2040,6 +2040,30 @@ fn cast_negative_one_to_u8_size_checks() {
 
 #[named]
 #[test]
+fn cast_signed_i8_to_field_must_error() {
+    let src = r#"
+        fn main() {
+            assert((-1 as i8) as Field != 0);
+                   ^^^^^^^^^^^^^^^^^^^ Only unsigned integer types may be casted to Field
+        }
+    "#;
+    check_errors(src, Some(&format!("{}_1", function_path!())));
+}
+
+#[named]
+#[test]
+fn cast_signed_i32_to_field_must_error() {
+    let src = r#"
+        fn main(x: i32) {
+            assert(x as Field != 0);
+                   ^^^^^^^^^^ Only unsigned integer types may be casted to Field
+        }
+    "#;
+    check_errors(src, Some(&format!("{}_1", function_path!())));
+}
+
+#[named]
+#[test]
 fn constant_used_with_numeric_generic() {
     let src = r#"
     struct ValueNote {
