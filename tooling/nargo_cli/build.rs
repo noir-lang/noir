4e690a1e3881c74f7b027abdbc0f4be36a66f3ca
Jake Fecher <jfecher11@gmail.com>
2025-07-08 14:00:36 -0500

    Add tests to nargo expand's ignore list

--- tooling/nargo_cli/build.rs
+++ tooling/nargo_cli/build.rs
@@ -178,7 +178,7 @@ const TESTS_WITHOUT_STDOUT_CHECK: [&str; 0] = [];
 /// These tests are ignored because of existing bugs in `nargo expand`.
 /// As the bugs are fixed these tests should be removed from this list.
 /// (some are ignored on purpose for the same reason as `IGNORED_NARGO_EXPAND_EXECUTION_TESTS`)
-const IGNORED_NARGO_EXPAND_COMPILE_SUCCESS_EMPTY_TESTS: [&str; 14] = [
+const IGNORED_NARGO_EXPAND_COMPILE_SUCCESS_EMPTY_TESTS: [&str; 15] = [
     // bug
     "associated_type_bounds",
     // bug
@@ -208,11 +208,12 @@ const IGNORED_NARGO_EXPAND_COMPILE_SUCCESS_EMPTY_TESTS: [&str; 14] = [
     "nested_trait_associated_type_regression_8252",
     // bug: the `<T as Trait>::N` syntax is dropped and becomes just `N`.
     "associated_constants_in_as_trait_expr",
+    "trait_as_type_associated_type_not_lost",
 ];
 
 /// These tests are ignored because of existing bugs in `nargo expand`.
 /// As the bugs are fixed these tests should be removed from this list.
-const IGNORED_NARGO_EXPAND_COMPILE_SUCCESS_NO_BUG_TESTS: [&str; 19] = [
+const IGNORED_NARGO_EXPAND_COMPILE_SUCCESS_NO_BUG_TESTS: [&str; 21] = [
     "noirc_frontend_tests_arithmetic_generics_checked_casts_do_not_prevent_canonicalization",
     "noirc_frontend_tests_check_trait_as_type_as_fn_parameter",
     "noirc_frontend_tests_check_trait_as_type_as_two_fn_parameters",
@@ -232,6 +233,8 @@ const IGNORED_NARGO_EXPAND_COMPILE_SUCCESS_NO_BUG_TESTS: [&str; 19] = [
     "noirc_frontend_tests_u32_globals_as_sizes_in_types",
     "noirc_frontend_tests_unused_items_considers_struct_as_constructed_if_trait_method_is_called",
     "noirc_frontend_tests_traits_associated_constant_of_generic_type_used_in_expression",
+    "noirc_frontend_tests_traits_as_trait_path_called_multiple_times_for_different_t_1",
+    "noirc_frontend_tests_traits_as_trait_path_called_multiple_times_for_different_t_2",
 ];
 
 const IGNORED_NARGO_EXPAND_COMPILE_SUCCESS_WITH_BUG_TESTS: [&str; 1] =
