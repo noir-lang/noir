f96c43ce2eda60c5794c8e68738a0126809fba12
guipublic <guipublic@gmail.com>
2025-07-17 17:29:03 +0200

    fix build

--- acvm-repo/acvm/src/compiler/mod.rs
+++ acvm-repo/acvm/src/compiler/mod.rs
@@ -92,7 +92,7 @@ fn transform_assert_messages<F: Clone>(
 pub fn compile<F: AcirField>(
     acir: Circuit<F>,
     expression_width: ExpressionWidth,
-    //TEMP experimental_optimization: bool,
+    _experimental_optimization: bool,
 ) -> (Circuit<F>, AcirTransformationMap) {
     let acir_opcode_positions = (0..acir.opcodes.len()).collect::<Vec<_>>();
 
