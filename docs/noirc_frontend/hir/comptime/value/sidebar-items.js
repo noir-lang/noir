806f27b8aa301467c34ae891231f0d521730f361
rustdoc <github@users.noreply.github.com>
2025-07-21 16:21:08 +0000

    update rustdoc

--- docs/noirc_frontend/hir/comptime/value/sidebar-items.js
+++ docs/noirc_frontend/hir/comptime/value/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["ExprValue","TypedExpr","Value"],"fn":["parse_tokens","unwrap_rc"],"struct":["Closure"]};
+window.SIDEBAR_ITEMS = {"enum":["ExprValue","TypedExpr","Value"],"fn":["parse_tokens","unwrap_rc"],"struct":["Closure"],"type":["StructFields"]};
