a3610a9a9923cb5d3e6b03dfcda35818df406b50
Ary Borenszweig <asterite@gmail.com>
2025-07-21 09:16:35 -0300

    Some more nargo expand tests now pass

--- tooling/nargo_cli/build.rs
+++ tooling/nargo_cli/build.rs
@@ -178,7 +178,7 @@ const TESTS_WITHOUT_STDOUT_CHECK: [&str; 0] = [];
 /// These tests are ignored because of existing bugs in `nargo expand`.
 /// As the bugs are fixed these tests should be removed from this list.
 /// (some are ignored on purpose for the same reason as `IGNORED_NARGO_EXPAND_EXECUTION_TESTS`)
-const IGNORED_NARGO_EXPAND_COMPILE_SUCCESS_EMPTY_TESTS: [&str; 14] = [
+const IGNORED_NARGO_EXPAND_COMPILE_SUCCESS_EMPTY_TESTS: [&str; 10] = [
     // bug
     "associated_type_bounds",
     // bug
@@ -188,14 +188,6 @@ const IGNORED_NARGO_EXPAND_COMPILE_SUCCESS_EMPTY_TESTS: [&str; 14] = [
     // this one works, but copying its `Nargo.toml` file to somewhere else doesn't work
     // because it references another project by a relative path
     "reexports",
-    // bug
-    "serialize_1",
-    // bug
-    "serialize_2",
-    // bug
-    "serialize_3",
-    // bug
-    "serialize_4",
     // bug
     "trait_function_calls",
     // bug
@@ -223,8 +215,6 @@ const IGNORED_NARGO_EXPAND_COMPILE_SUCCESS_NO_BUG_TESTS: [&str; 10] = [
     "noirc_frontend_tests_traits_accesses_associated_type_inside_trait_impl_using_self",
     "noirc_frontend_tests_traits_accesses_associated_type_inside_trait_using_self",
     "noirc_frontend_tests_u32_globals_as_sizes_in_types",
-    // "noirc_frontend_tests_traits_as_trait_path_called_multiple_times_for_different_t_1",
-    // "noirc_frontend_tests_traits_as_trait_path_called_multiple_times_for_different_t_2",
 ];
 
 const IGNORED_NARGO_EXPAND_COMPILE_SUCCESS_WITH_BUG_TESTS: [&str; 1] =
