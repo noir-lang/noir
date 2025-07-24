ca5b2938a9960026c544c34d248d02b7a8a3f158
github-action-benchmark <github@users.noreply.github.com>
2025-07-24 17:43:34 +0000

    add ACVM Benchmarks (cargo) benchmark result for 637e0601aa5dc0baa29934783e6ff7fdf8e24133

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753372168741,
+  "lastUpdate": 1753379014053,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -16616,48 +16616,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "15848336+TomAFrench@users.noreply.github.com",
-            "name": "Tom French",
-            "username": "TomAFrench"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "e7a98f2ffb2521b834747bf9a40dc7be1f813bcd",
-          "message": "feat: optimize array reads from length 1 arrays (#9203)",
-          "timestamp": "2025-07-15T14:18:40Z",
-          "tree_id": "0d69c80bad46e1a72d1274b50199552d589c9e97",
-          "url": "https://github.com/noir-lang/noir/commit/e7a98f2ffb2521b834747bf9a40dc7be1f813bcd"
-        },
-        "date": 1752590664719,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 249529,
-            "range": "± 267",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 222426,
-            "range": "± 3155",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 2778625,
-            "range": "± 1893",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "aakoshh@gmail.com",
             "name": "Akosh Farkash",
             "username": "aakoshh"
@@ -18709,6 +18667,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 2812121,
             "range": "± 1716",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aakoshh@gmail.com",
+            "name": "Akosh Farkash",
+            "username": "aakoshh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "637e0601aa5dc0baa29934783e6ff7fdf8e24133",
+          "message": "fix(mem2reg): Register aliases when the `IfElse` result in a reference (#9305)",
+          "timestamp": "2025-07-24T17:17:46Z",
+          "tree_id": "c1ebaee6a39867b1fe52c5d065533bd2bcdeb906",
+          "url": "https://github.com/noir-lang/noir/commit/637e0601aa5dc0baa29934783e6ff7fdf8e24133"
+        },
+        "date": 1753378998090,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 247126,
+            "range": "± 527",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 217529,
+            "range": "± 910",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 2787308,
+            "range": "± 1433",
             "unit": "ns/iter"
           }
         ]
