fc13b248727aa36a0e58e29c1905d8ad7d8b8686
Maxim Vezenov <mvezenov@gmail.com>
2025-07-24 17:28:36 +0000

    initial acir parser, working arithmetic expressions and most black box funcs

---
+++ acvm-repo/acir/src/parser/tests.rs
@@ -0,0 +1,380 @@
+use crate::circuit::Circuit;
+
+/// Trims leading whitespace from each line of the input string
+#[cfg(test)]
+pub(crate) fn trim_leading_whitespace_from_lines(src: &str) -> String {
+    let mut lines = src.trim_end().lines();
+    let mut first_line = lines.next().unwrap();
+    while first_line.is_empty() {
+        first_line = lines.next().unwrap();
+    }
+    let first_line_original_length = first_line.len();
+    let mut result = first_line.trim_start().to_string();
+    let first_line_trimmed_length = result.len();
+
+    // Try to see how many spaces we chopped off the first line
+    let difference = first_line_original_length - first_line_trimmed_length;
+    for line in lines {
+        result.push('\n');
+        // Try to remove just `difference` spaces to preserve indents
+        if line.len() - line.trim_start().len() >= difference {
+            result.push_str(&line.chars().skip(difference).collect::<String>());
+        } else {
+            result.push_str(line.trim_start());
+        }
+    }
+    result
+}
+
+fn assert_circuit_roundtrip(src: &str) {
+    let circuit = Circuit::from_str(src).unwrap();
+    let circuit = circuit.to_string();
+    let circuit = trim_leading_whitespace_from_lines(&circuit);
+    let src = trim_leading_whitespace_from_lines(src);
+    if circuit != src {
+        println!("Expected:\n~~~\n{src}\n~~~\nGot:\n~~~\n{circuit}\n~~~");
+        similar_asserts::assert_eq!(circuit, src);
+    }
+}
+
+#[test]
+fn current_witness() {
+    let src = "
+    current witness index : _1
+    private parameters indices : []
+    public parameters indices : []
+    return value indices : []
+    ";
+    assert_circuit_roundtrip(src);
+}
+
+#[test]
+fn private_parameters() {
+    let src = "
+    current witness index : _4
+    private parameters indices : [_0, _1, _2, _3, _4]
+    public parameters indices : []
+    return value indices : []
+    ";
+    assert_circuit_roundtrip(src);
+}
+
+#[test]
+fn public_parameters() {
+    let src = "
+    current witness index : _9
+    private parameters indices : [_0, _1, _2, _3, _4]
+    public parameters indices : [_5, _6, _7, _8, _9]
+    return value indices : []
+    ";
+    assert_circuit_roundtrip(src);
+}
+
+#[test]
+fn return_values() {
+    let src = "
+    current witness index : _12
+    private parameters indices : [_0, _1, _2, _3, _4]
+    public parameters indices : [_5, _6, _7, _8, _9]
+    return value indices : [_10, _11, _12]
+    ";
+    assert_circuit_roundtrip(src);
+}
+
+#[test]
+fn assert_zero_opcodes() {
+    let src = "
+    current witness index : _9
+    private parameters indices : [_0, _1, _2, _3, _4]
+    public parameters indices : [_5, _6, _7, _8, _9]
+    return value indices : []
+    EXPR [ (1, _0) (-1, _5) 0 ]
+    EXPR [ (1, _1) (-1, _6) 0 ]
+    EXPR [ (1, _2) (-1, _7) 0 ]
+    EXPR [ (1, _3) (-1, _8) 0 ]
+    EXPR [ (1, _4) (-1, _9) 0 ]    
+    ";
+    assert_circuit_roundtrip(src);
+}
+
+#[test]
+fn assert_zero_with_mul_terms() {
+    let src = "
+    current witness index : _6
+    private parameters indices : [_0, _1, _2]
+    public parameters indices : []
+    return value indices : []
+    EXPR [ (1, _0, _1) (-1, _3) 0 ]
+    EXPR [ (1, _3, _3) (-1, _4) 0 ]
+    EXPR [ (1, _4, _4) (-1, _5) 0 ]
+    EXPR [ (1, _5, _5) (-1, _6) 0 ]
+    EXPR [ (-1, _2) (1, _6) 0 ]
+    ";
+    assert_circuit_roundtrip(src);
+}
+
+#[test]
+fn range_and_xor() {
+    let src = "
+    current witness index : _2
+    private parameters indices : [_0]
+    public parameters indices : [_1]
+    return value indices : []
+    BLACKBOX::RANGE [(_0, 32)] []
+    BLACKBOX::RANGE [(_1, 32)] []
+    BLACKBOX::XOR [(_0, 32), (_1, 32)] [_2]
+    EXPR [ (1, _2) -15 ]
+    ";
+    assert_circuit_roundtrip(src);
+}
+
+#[test]
+fn aes128_encrypt() {
+    // This ACIR represents an accurately constrained aes128 encryption in ACIR
+    let src = "
+    current witness index : _75
+    private parameters indices : [_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43]
+    public parameters indices : [_44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59]
+    return value indices : []
+    BLACKBOX::RANGE [(_0, 8)] []
+    BLACKBOX::RANGE [(_1, 8)] []
+    BLACKBOX::RANGE [(_2, 8)] []
+    BLACKBOX::RANGE [(_3, 8)] []
+    BLACKBOX::RANGE [(_4, 8)] []
+    BLACKBOX::RANGE [(_5, 8)] []
+    BLACKBOX::RANGE [(_6, 8)] []
+    BLACKBOX::RANGE [(_7, 8)] []
+    BLACKBOX::RANGE [(_8, 8)] []
+    BLACKBOX::RANGE [(_9, 8)] []
+    BLACKBOX::RANGE [(_10, 8)] []
+    BLACKBOX::RANGE [(_11, 8)] []
+    BLACKBOX::RANGE [(_12, 8)] []
+    BLACKBOX::RANGE [(_13, 8)] []
+    BLACKBOX::RANGE [(_14, 8)] []
+    BLACKBOX::RANGE [(_15, 8)] []
+    BLACKBOX::RANGE [(_16, 8)] []
+    BLACKBOX::RANGE [(_17, 8)] []
+    BLACKBOX::RANGE [(_18, 8)] []
+    BLACKBOX::RANGE [(_19, 8)] []
+    BLACKBOX::RANGE [(_20, 8)] []
+    BLACKBOX::RANGE [(_21, 8)] []
+    BLACKBOX::RANGE [(_22, 8)] []
+    BLACKBOX::RANGE [(_23, 8)] []
+    BLACKBOX::RANGE [(_24, 8)] []
+    BLACKBOX::RANGE [(_25, 8)] []
+    BLACKBOX::RANGE [(_26, 8)] []
+    BLACKBOX::RANGE [(_27, 8)] []
+    BLACKBOX::RANGE [(_28, 8)] []
+    BLACKBOX::RANGE [(_29, 8)] []
+    BLACKBOX::RANGE [(_30, 8)] []
+    BLACKBOX::RANGE [(_31, 8)] []
+    BLACKBOX::RANGE [(_32, 8)] []
+    BLACKBOX::RANGE [(_33, 8)] []
+    BLACKBOX::RANGE [(_34, 8)] []
+    BLACKBOX::RANGE [(_35, 8)] []
+    BLACKBOX::RANGE [(_36, 8)] []
+    BLACKBOX::RANGE [(_37, 8)] []
+    BLACKBOX::RANGE [(_38, 8)] []
+    BLACKBOX::RANGE [(_39, 8)] []
+    BLACKBOX::RANGE [(_40, 8)] []
+    BLACKBOX::RANGE [(_41, 8)] []
+    BLACKBOX::RANGE [(_42, 8)] []
+    BLACKBOX::RANGE [(_43, 8)] []
+    BLACKBOX::RANGE [(_44, 8)] []
+    BLACKBOX::RANGE [(_45, 8)] []
+    BLACKBOX::RANGE [(_46, 8)] []
+    BLACKBOX::RANGE [(_47, 8)] []
+    BLACKBOX::RANGE [(_48, 8)] []
+    BLACKBOX::RANGE [(_49, 8)] []
+    BLACKBOX::RANGE [(_50, 8)] []
+    BLACKBOX::RANGE [(_51, 8)] []
+    BLACKBOX::RANGE [(_52, 8)] []
+    BLACKBOX::RANGE [(_53, 8)] []
+    BLACKBOX::RANGE [(_54, 8)] []
+    BLACKBOX::RANGE [(_55, 8)] []
+    BLACKBOX::RANGE [(_56, 8)] []
+    BLACKBOX::RANGE [(_57, 8)] []
+    BLACKBOX::RANGE [(_58, 8)] []
+    BLACKBOX::RANGE [(_59, 8)] []
+    BLACKBOX::AES128_ENCRYPT [(_12, 8), (_13, 8), (_14, 8), (_15, 8), (_16, 8), (_17, 8), (_18, 8), (_19, 8), (_20, 8), (_21, 8), (_22, 8), (_23, 8), (_24, 8), (_25, 8), (_26, 8), (_27, 8), (_28, 8), (_29, 8), (_30, 8), (_31, 8), (_32, 8), (_33, 8), (_34, 8), (_35, 8), (_36, 8), (_37, 8), (_38, 8), (_39, 8), (_40, 8), (_41, 8), (_42, 8), (_43, 8)] [_60, _61, _62, _63, _64, _65, _66, _67, _68, _69, _70, _71, _72, _73, _74, _75]
+    EXPR [ (-1, _44) (1, _60) 0 ]
+    EXPR [ (-1, _45) (1, _61) 0 ]
+    EXPR [ (-1, _46) (1, _62) 0 ]
+    EXPR [ (-1, _47) (1, _63) 0 ]
+    EXPR [ (-1, _48) (1, _64) 0 ]
+    EXPR [ (-1, _49) (1, _65) 0 ]
+    EXPR [ (-1, _50) (1, _66) 0 ]
+    EXPR [ (-1, _51) (1, _67) 0 ]
+    EXPR [ (-1, _52) (1, _68) 0 ]
+    EXPR [ (-1, _53) (1, _69) 0 ]
+    EXPR [ (-1, _54) (1, _70) 0 ]
+    EXPR [ (-1, _55) (1, _71) 0 ]
+    EXPR [ (-1, _56) (1, _72) 0 ]
+    EXPR [ (-1, _57) (1, _73) 0 ]
+    EXPR [ (-1, _58) (1, _74) 0 ]
+    EXPR [ (-1, _59) (1, _75) 0 ]
+    ";
+    assert_circuit_roundtrip(src);
+}
+
+#[test]
+fn blake2s() {
+    let src = "
+    current witness index : _68
+    private parameters indices : [_0, _1, _2, _3, _4]
+    public parameters indices : [_5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36]
+    return value indices : []
+    BLACKBOX::BLAKE2S [(_0, 8), (_1, 8), (_2, 8), (_3, 8), (_4, 8)] [_37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64, _65, _66, _67, _68]
+    ";
+    assert_circuit_roundtrip(src);
+}
+
+#[test]
+fn blake3() {
+    let src = "
+    current witness index : _37
+    private parameters indices : [_0, _1, _2, _3, _4]
+    public parameters indices : []
+    return value indices : []
+    BLACKBOX::BLAKE3 [(_0, 8), (_1, 8), (_2, 8), (_3, 8), (_4, 8)] [_5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36]
+    ";
+    assert_circuit_roundtrip(src);
+}
+
+#[test]
+fn ecdsa_secp256k1() {
+    let input_witnesses: Vec<String> = (0..160).map(|i| format!("(_{}, 8)", i)).collect();
+    let inputs_str = input_witnesses.join(", ");
+
+    let src = format!(
+        "
+    current witness index : _160
+    private parameters indices : []
+    public parameters indices : []
+    return value indices : []
+    BLACKBOX::ECDSA_SECP256K1 [{inputs_str}] [_160]
+    "
+    );
+    assert_circuit_roundtrip(&src);
+}
+
+#[test]
+#[should_panic]
+fn ecdsa_secp256k1_missing_inputs() {
+    let input_witnesses: Vec<String> = (0..100).map(|i| format!("(_{}, 8)", i)).collect();
+    let inputs_str = input_witnesses.join(", ");
+
+    let src = format!(
+        "
+    current witness index : _100
+    private parameters indices : []
+    public parameters indices : []
+    return value indices : []
+    BLACKBOX::ECDSA_SECP256K1 [{inputs_str}] [_100]
+    "
+    );
+    assert_circuit_roundtrip(&src);
+}
+
+#[test]
+fn ecdsa_secp256r1() {
+    let input_witnesses: Vec<String> = (0..160).map(|i| format!("(_{}, 8)", i)).collect();
+    let inputs_str = input_witnesses.join(", ");
+
+    let src = format!(
+        "
+    current witness index : _160
+    private parameters indices : []
+    public parameters indices : []
+    return value indices : []
+    BLACKBOX::ECDSA_SECP256R1 [{inputs_str}] [_160]
+    "
+    );
+    assert_circuit_roundtrip(&src);
+}
+
+#[test]
+#[should_panic]
+fn ecdsa_secp256r1_missing_inputs() {
+    let input_witnesses: Vec<String> = (0..100).map(|i| format!("(_{}, 8)", i)).collect();
+    let inputs_str = input_witnesses.join(", ");
+
+    let src = format!(
+        "
+    current witness index : _100
+    private parameters indices : []
+    public parameters indices : []
+    return value indices : []
+    BLACKBOX::ECDSA_SECP256R1 [{inputs_str}] [_100]
+    "
+    );
+    assert_circuit_roundtrip(&src);
+}
+
+#[test]
+fn keccakf1600() {
+    let src = "
+    current witness index : _50
+    private parameters indices : []
+    public parameters indices : []
+    return value indices : []
+    BLACKBOX::KECCAKF1600 [
+        (_0, 64), (_1, 64), (_2, 64), (_3, 64), (_4, 64), 
+        (_5, 64), (_6, 64), (_7, 64), (_8, 64), (_9, 64),
+        (_10, 64), (_11, 64), (_12, 64), (_13, 64), (_14, 64),
+        (_15, 64), (_16, 64), (_17, 64), (_18, 64), (_19, 64),
+        (_20, 64), (_21, 64), (_22, 64), (_23, 64), (_24, 64)
+    ] [
+        _25, _26, _27, _28, _29,
+        _30, _31, _32, _33, _34,
+        _35, _36, _37, _38, _39,
+        _40, _41, _42, _43, _44,
+        _45, _46, _47, _48, _49
+    ]
+    ";
+    assert_circuit_roundtrip(src);
+}
+
+#[test]
+fn embedded_curve_add() {
+    let src = "
+    current witness index : _9
+    private parameters indices : []
+    public parameters indices : []
+    return value indices : []
+    BLACKBOX::EMBEDDED_CURVE_ADD [(_0, 255), (_1, 255), (_2, 1), (_3, 255), (_4, 255), (_5, 1)] [_6, _7, _8]
+    ";
+    assert_circuit_roundtrip(src);
+}
+
+#[test]
+fn poseidon2_permutation() {
+    let src = "
+    current witness index : _5
+    private parameters indices : []
+    public parameters indices : []
+    return value indices : []
+    BLACKBOX::POSEIDON2_PERMUTATION [(_0, 255), (_1, 255), (_2, 255)] [_3, _4, _5]
+    ";
+    assert_circuit_roundtrip(src);
+}
+
+#[test]
+fn sha256_compression() {
+    let input_witnesses: Vec<String> = (0..24).map(|i| format!("(_{}, 8)", i)).collect();
+    let inputs_str = input_witnesses.join(", ");
+
+    let output_witnesses: Vec<String> = (24..32).map(|i| format!("_{}", i)).collect();
+    let outputs_str = output_witnesses.join(", ");
+
+    let src = format!(
+        "
+    current witness index : _31
+    private parameters indices : []
+    public parameters indices : []
+    return value indices : []
+    BLACKBOX::SHA256_COMPRESSION [{inputs_str}] [{outputs_str}]
+    "
+    );
+    assert_circuit_roundtrip(&src);
+}
---
+++ acvm-repo/acir/src/parser/token.rs
@@ -0,0 +1,146 @@
+use acir_field::FieldElement;
+use noirc_errors::{Position, Span, Spanned};
+
+#[derive(Debug)]
+pub(crate) struct SpannedToken(Spanned<Token>);
+
+impl SpannedToken {
+    pub(crate) fn new(token: Token, span: Span) -> SpannedToken {
+        SpannedToken(Spanned::from(span, token))
+    }
+
+    pub(crate) fn span(&self) -> Span {
+        self.0.span()
+    }
+
+    pub(crate) fn token(&self) -> &Token {
+        &self.0.contents
+    }
+
+    pub(crate) fn into_token(self) -> Token {
+        self.0.contents
+    }
+}
+
+/// Token types used in the ACIR text format.
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) enum Token {
+    /// Identifier such as `current`, `EXPR`, etc.
+    Ident(String),
+    /// Reserved identifiers such as `EXPR`.
+    /// All words in ACIR's human readable are expected to be keywords
+    Keyword(Keyword),
+    /// Witness index, like `_42`
+    Witness(u32),
+    /// Integer value represented using the underlying native field element
+    Int(FieldElement),
+    /// :
+    Colon,
+    /// ;
+    Semicolon,
+    /// ,
+    Comma,
+    /// [
+    LeftBracket,
+    /// ]
+    RightBracket,
+    /// (
+    LeftParen,
+    /// )
+    RightParen,
+    Eof,
+}
+
+impl Token {
+    pub(super) fn into_single_span(self, position: Position) -> SpannedToken {
+        self.into_span(position, position)
+    }
+
+    pub(super) fn into_span(self, start: Position, end: Position) -> SpannedToken {
+        SpannedToken(Spanned::from_position(start, end, self))
+    }
+}
+
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Token::Ident(ident) => write!(f, "{ident}"),
+            Token::Keyword(keyword) => write!(f, "{keyword}"),
+            Token::Witness(index) => write!(f, "_{index}"),
+            Token::Int(int) => write!(f, "{int}"),
+            Token::Colon => write!(f, ":"),
+            Token::Semicolon => write!(f, ";"),
+            Token::Comma => write!(f, ","),
+            Token::LeftBracket => write!(f, "["),
+            Token::RightBracket => write!(f, "]"),
+            Token::LeftParen => write!(f, "("),
+            Token::RightParen => write!(f, ")"),
+            Token::Eof => write!(f, "(end of stream)"),
+        }
+    }
+}
+
+/// ACIR human readable text format keywords
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub(crate) enum Keyword {
+    /// current
+    Current,
+    /// witness
+    Witness,
+    /// index
+    Index,
+    /// private
+    Private,
+    /// parameters
+    Parameters,
+    /// indices
+    Indices,
+    /// public
+    Public,
+    /// return
+    Return,
+    /// value
+    Value,
+    /// EXPR
+    Expression,
+    /// BLACKBOX
+    BlackBoxFuncCall,
+}
+
+impl Keyword {
+    pub(super) fn lookup_keyword(word: &str) -> Option<Token> {
+        let keyword = match word {
+            "current" => Keyword::Current,
+            "witness" => Keyword::Witness,
+            "index" => Keyword::Index,
+            "private" => Keyword::Private,
+            "parameters" => Keyword::Parameters,
+            "indices" => Keyword::Indices,
+            "public" => Keyword::Public,
+            "return" => Keyword::Return,
+            "value" => Keyword::Value,
+            "EXPR" => Keyword::Expression,
+            "BLACKBOX" => Keyword::BlackBoxFuncCall,
+            _ => return None,
+        };
+        Some(Token::Keyword(keyword))
+    }
+}
+
+impl std::fmt::Display for Keyword {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Keyword::Current => write!(f, "current"),
+            Keyword::Witness => write!(f, "witness"),
+            Keyword::Index => write!(f, "index"),
+            Keyword::Private => write!(f, "private"),
+            Keyword::Parameters => write!(f, "parameters"),
+            Keyword::Indices => write!(f, "indices"),
+            Keyword::Public => write!(f, "public"),
+            Keyword::Return => write!(f, "return"),
+            Keyword::Value => write!(f, "value"),
+            Keyword::Expression => write!(f, "EXPR"),
+            Keyword::BlackBoxFuncCall => write!(f, "BLACKBOX"),
+        }
+    }
+}
