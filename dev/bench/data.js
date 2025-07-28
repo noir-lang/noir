4c0508daa38dcb10863bf08bef9e9688ba295eb1
github-action-benchmark <github@users.noreply.github.com>
2025-07-28 20:32:59 +0000

    add Execution Memory (customSmallerIsBetter) benchmark result for c0b92e68472ec05654cdcc1c1ad18e98ec651c4d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753734746813,
+  "lastUpdate": 1753734779119,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -7552,80 +7552,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": false,
-          "id": "8797651e4dcfe77cd46353188b0f60c2a1dce8b2",
-          "message": "fix(ssa): Change constraint message to \"multiply\" (#9230)",
-          "timestamp": "2025-07-17T14:05:56Z",
-          "tree_id": "b1161946aac795b821aa6c8ba9f4abaa663fd833",
-          "url": "https://github.com/noir-lang/noir/commit/8797651e4dcfe77cd46353188b0f60c2a1dce8b2"
-        },
-        "date": 1752764129055,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "private-kernel-inner",
-            "value": 201.91,
-            "unit": "MB"
-          },
-          {
-            "name": "private-kernel-reset",
-            "value": 237.94,
-            "unit": "MB"
-          },
-          {
-            "name": "private-kernel-tail",
-            "value": 191.14,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-base-private",
-            "value": 502.2,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-base-public",
-            "value": 435.06,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-block-root",
-            "value": 1510,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-merge",
-            "value": 324.61,
-            "unit": "MB"
-          },
-          {
-            "name": "rollup-root",
-            "value": 327.25,
-            "unit": "MB"
-          },
-          {
-            "name": "semaphore_depth_10",
-            "value": 69.27,
-            "unit": "MB"
-          },
-          {
-            "name": "sha512_100_bytes",
-            "value": 55.51,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "aakoshh@gmail.com",
-            "name": "Akosh Farkash",
-            "username": "aakoshh"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
           "id": "c7ce62a51d82b26d1daaadf6bd32fcb6e8711e3e",
           "message": "fix(fuzz): Use scoping for variable dynamism (#9233)",
           "timestamp": "2025-07-17T14:52:04Z",
@@ -9945,6 +9871,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/noir-lang/noir/commit/4f733adf44501d52eea35d03c9761b27e295af0f"
         },
         "date": 1753729941058,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "semaphore_depth_10",
+            "value": 69.46,
+            "unit": "MB"
+          },
+          {
+            "name": "sha512_100_bytes",
+            "value": 54.95,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "asterite@gmail.com",
+            "name": "Ary Borenszweig",
+            "username": "asterite"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "c0b92e68472ec05654cdcc1c1ad18e98ec651c4d",
+          "message": "fix: do u128 operations with u128, not i128 (#9345)",
+          "timestamp": "2025-07-28T20:04:11Z",
+          "tree_id": "6ddedec0ced5601d9c0e133ceea96884a1efb9fe",
+          "url": "https://github.com/noir-lang/noir/commit/c0b92e68472ec05654cdcc1c1ad18e98ec651c4d"
+        },
+        "date": 1753734762307,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
