b9dfdd3849fb650871d16bf67b2d430ab42c4e53
github-action-benchmark <github@users.noreply.github.com>
2025-07-29 16:40:23 +0000

    add Compilation Time (customSmallerIsBetter) benchmark result for 8349a40a27c7f10d51b8b8cb05e7ddf3d78f713f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753807221497,
+  "lastUpdate": 1753807223920,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -2468,90 +2468,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": false,
-          "id": "c30f8ef3c39ec98adee177e43b17fd99d7a10852",
-          "message": "fix: bind self when type-checking AsTraitPath (#9236)",
-          "timestamp": "2025-07-17T17:29:18Z",
-          "tree_id": "13df6d7ce0806d6916f7e6ae71c706ca83905b27",
-          "url": "https://github.com/noir-lang/noir/commit/c30f8ef3c39ec98adee177e43b17fd99d7a10852"
-        },
-        "date": 1752775259021,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "private-kernel-inner",
-            "value": 2.178,
-            "unit": "s"
-          },
-          {
-            "name": "private-kernel-reset",
-            "value": 8.202,
-            "unit": "s"
-          },
-          {
-            "name": "private-kernel-tail",
-            "value": 1.616,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-base-private",
-            "value": 17.62,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-base-public",
-            "value": 14.66,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-block-root-empty",
-            "value": 21.44,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-block-root-single-tx",
-            "value": 208,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-block-root",
-            "value": 213,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-merge",
-            "value": 1.714,
-            "unit": "s"
-          },
-          {
-            "name": "rollup-root",
-            "value": 1.834,
-            "unit": "s"
-          },
-          {
-            "name": "semaphore-depth-10",
-            "value": 0.838,
-            "unit": "s"
-          },
-          {
-            "name": "sha512-100-bytes",
-            "value": 1.995,
-            "unit": "s"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "asterite@gmail.com",
-            "name": "Ary Borenszweig",
-            "username": "asterite"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
           "id": "1719bc3a7977248e792816df3a3e907f2d006a95",
           "message": "fix: suggest traits via visible reexports if they are not directly visible (#9242)",
           "timestamp": "2025-07-17T20:15:42Z",
@@ -4901,6 +4817,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "sha512-100-bytes",
             "value": 1.546,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "asterite@gmail.com",
+            "name": "Ary Borenszweig",
+            "username": "asterite"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8349a40a27c7f10d51b8b8cb05e7ddf3d78f713f",
+          "message": "chore: remove unused `compile_workspace` (#9353)",
+          "timestamp": "2025-07-29T16:09:51Z",
+          "tree_id": "5acbfcd1233504ff9421ea789931c8eb22ba124c",
+          "url": "https://github.com/noir-lang/noir/commit/8349a40a27c7f10d51b8b8cb05e7ddf3d78f713f"
+        },
+        "date": 1753807205476,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "semaphore-depth-10",
+            "value": 0.773,
+            "unit": "s"
+          },
+          {
+            "name": "sha512-100-bytes",
+            "value": 1.815,
             "unit": "s"
           }
         ]
