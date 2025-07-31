d00409f94633ffa2c54b7a90cec12edce39d49c2
github-action-benchmark <github@users.noreply.github.com>
2025-07-31 20:04:38 +0000

    add Execution Memory (customSmallerIsBetter) benchmark result for cb6b7e34ece13f2b99a0dc3d845b3254f8c20c5f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753992275684,
+  "lastUpdate": 1753992278388,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -5112,40 +5112,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "jfecher11@gmail.com",
-            "name": "jfecher",
-            "username": "jfecher"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "3c18e331de7b0bdec60b94f625158e453de622cb",
-          "message": "fix: Use `u32` in `RepeatedValue` example in generics docs (#9273)",
-          "timestamp": "2025-07-22T14:22:48+01:00",
-          "tree_id": "87c5995062efc83749a3d31da4a29e8b44d2d54e",
-          "url": "https://github.com/noir-lang/noir/commit/3c18e331de7b0bdec60b94f625158e453de622cb"
-        },
-        "date": 1753190903226,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "semaphore_depth_10",
-            "value": 69.3,
-            "unit": "MB"
-          },
-          {
-            "name": "sha512_100_bytes",
-            "value": 55.56,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "72797635+Savio-Sou@users.noreply.github.com",
             "name": "Savio",
             "username": "Savio-Sou"
@@ -6795,6 +6761,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/noir-lang/noir/commit/941611c3c9ddc6e7d1bc54f61491febc55ef1158"
         },
         "date": 1753991329847,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "semaphore_depth_10",
+            "value": 69.46,
+            "unit": "MB"
+          },
+          {
+            "name": "sha512_100_bytes",
+            "value": 54.95,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "asterite@gmail.com",
+            "name": "Ary Borenszweig",
+            "username": "asterite"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cb6b7e34ece13f2b99a0dc3d845b3254f8c20c5f",
+          "message": "fix: trait where clause check fixes (#9369)",
+          "timestamp": "2025-07-31T19:34:34Z",
+          "tree_id": "80c68aa050a218b871760a39df49d70392b68b2d",
+          "url": "https://github.com/noir-lang/noir/commit/cb6b7e34ece13f2b99a0dc3d845b3254f8c20c5f"
+        },
+        "date": 1753992260496,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
