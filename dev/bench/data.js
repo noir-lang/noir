728662d2eb53ddba3d3d8a41de87845589335e9d
github-action-benchmark <github@users.noreply.github.com>
2025-07-31 20:01:57 +0000

    add ACVM Benchmarks (cargo) benchmark result for cb6b7e34ece13f2b99a0dc3d845b3254f8c20c5f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753991719111,
+  "lastUpdate": 1753992117071,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -10916,48 +10916,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "jfecher11@gmail.com",
-            "name": "jfecher",
-            "username": "jfecher"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "3c18e331de7b0bdec60b94f625158e453de622cb",
-          "message": "fix: Use `u32` in `RepeatedValue` example in generics docs (#9273)",
-          "timestamp": "2025-07-22T14:22:48+01:00",
-          "tree_id": "87c5995062efc83749a3d31da4a29e8b44d2d54e",
-          "url": "https://github.com/noir-lang/noir/commit/3c18e331de7b0bdec60b94f625158e453de622cb"
-        },
-        "date": 1753190720812,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 248164,
-            "range": "± 1038",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 218901,
-            "range": "± 2759",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 2776199,
-            "range": "± 21816",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "72797635+Savio-Sou@users.noreply.github.com",
             "name": "Savio",
             "username": "Savio-Sou"
@@ -13009,6 +12967,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 2811780,
             "range": "± 7195",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "asterite@gmail.com",
+            "name": "Ary Borenszweig",
+            "username": "asterite"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cb6b7e34ece13f2b99a0dc3d845b3254f8c20c5f",
+          "message": "fix: trait where clause check fixes (#9369)",
+          "timestamp": "2025-07-31T19:34:34Z",
+          "tree_id": "80c68aa050a218b871760a39df49d70392b68b2d",
+          "url": "https://github.com/noir-lang/noir/commit/cb6b7e34ece13f2b99a0dc3d845b3254f8c20c5f"
+        },
+        "date": 1753992100573,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 249234,
+            "range": "± 223",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 220852,
+            "range": "± 3342",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 2787290,
+            "range": "± 3541",
             "unit": "ns/iter"
           }
         ]
