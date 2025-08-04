aa43676252ea358ebfba99d37e591d72d48986e1
github-action-benchmark <github@users.noreply.github.com>
2025-08-04 16:32:40 +0000

    add ACVM Benchmarks (cargo) benchmark result for b61f5eb6f371ee8e51c2ac42de38ed821b084b1b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754325153390,
+  "lastUpdate": 1754325160780,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -12046,48 +12046,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "ee66c987f6392c05e6b46cdd9560d38251b0fb24",
-          "message": "fix: disallow `_` in signatures and struct members (#9301)",
-          "timestamp": "2025-07-23T19:00:39Z",
-          "tree_id": "9c8bf2cc249556381101bfaecbd7d0613af6a6c1",
-          "url": "https://github.com/noir-lang/noir/commit/ee66c987f6392c05e6b46cdd9560d38251b0fb24"
-        },
-        "date": 1753298814879,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 250152,
-            "range": "± 385",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 219955,
-            "range": "± 7612",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 2779156,
-            "range": "± 5137",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "asterite@gmail.com",
-            "name": "Ary Borenszweig",
-            "username": "asterite"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "6a2a28fe2b426570bedad9916129f0147e8604dd",
           "message": "chore: add regression tests for #6383 (#9302)",
           "timestamp": "2025-07-23T22:28:37Z",
@@ -14129,6 +14087,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 2802517,
             "range": "± 1351",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aakoshh@gmail.com",
+            "name": "Akosh Farkash",
+            "username": "aakoshh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "b61f5eb6f371ee8e51c2ac42de38ed821b084b1b",
+          "message": "fix(ssa_gen): Generate code for index before the collection (#9332)\n\nCo-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>",
+          "timestamp": "2025-08-04T16:07:14Z",
+          "tree_id": "38b72e1d92c627a959392253cb4a652fc48df69b",
+          "url": "https://github.com/noir-lang/noir/commit/b61f5eb6f371ee8e51c2ac42de38ed821b084b1b"
+        },
+        "date": 1754325143224,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 249785,
+            "range": "± 692",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 220800,
+            "range": "± 3244",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 2782033,
+            "range": "± 1314",
             "unit": "ns/iter"
           }
         ]
