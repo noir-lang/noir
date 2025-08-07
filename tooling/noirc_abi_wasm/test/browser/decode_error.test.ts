433086e901bd137da3758a2613ba6d4fa6b6f5f5
Tom French <15848336+TomAFrench@users.noreply.github.com>
2025-08-07 13:44:41 +0000

    .

--- tooling/noirc_abi_wasm/test/browser/decode_error.test.ts
+++ tooling/noirc_abi_wasm/test/browser/decode_error.test.ts
@@ -10,7 +10,7 @@ it('Recovers custom field errors', async () => {
 
   const payload: RawAssertionPayload = {
     selector: FAKE_FIELD_SELECTOR,
-    data: ['0x000001'],
+    data: [Uint8Array.from(Buffer.from('0x0000000000000000000000000000000000000000000000000000000000000001', 'hex'))],
   };
 
   const decoded = abiDecodeError(abi, payload);
@@ -22,7 +22,10 @@ it('Recovers custom tuple errors', async () => {
 
   const payload: RawAssertionPayload = {
     selector: FAKE_TUPLE_SELECTOR,
-    data: ['0x000001', '0x000002'],
+    data: [
+      Uint8Array.from(Buffer.from('0x0000000000000000000000000000000000000000000000000000000000000001', 'hex')),
+      Uint8Array.from(Buffer.from('0x0000000000000000000000000000000000000000000000000000000000000002', 'hex')),
+    ],
   };
 
   const decoded = abiDecodeError(abi, payload);
@@ -33,11 +36,13 @@ it('Recovers custom fmt string errors', async () => {
   const { FAKE_FMT_STRING_SELECTOR, abi, SAMPLE_FMT_STRING } = await import('../shared/decode_error');
 
   // FmtStrings contain the string serialized to fields
-  const data = [...SAMPLE_FMT_STRING].map((c) => `0x${c.charCodeAt(0).toString(16)}`);
+  const data = [...SAMPLE_FMT_STRING].map((c) =>
+    Uint8Array.from(Buffer.from(`0x${c.charCodeAt(0).toString(16)}`, 'hex')),
+  );
   // Then they contain the length of the values to replace
-  data.push('0x01');
+  data.push(Uint8Array.from(Buffer.from('0x0000000000000000000000000000000000000000000000000000000000000001', 'hex')));
   // And then the value to replace
-  data.push('0x07');
+  data.push(Uint8Array.from(Buffer.from('0x0000000000000000000000000000000000000000000000000000000000000007', 'hex')));
 
   const payload: RawAssertionPayload = {
     selector: FAKE_FMT_STRING_SELECTOR,
@@ -53,7 +58,10 @@ it('Recovers struct errors', async () => {
 
   const payload: RawAssertionPayload = {
     selector: FAKE_STRUCT_SELECTOR,
-    data: ['0x01', '0x02'],
+    data: [
+      Uint8Array.from(Buffer.from('0x0000000000000000000000000000000000000000000000000000000000000001', 'hex')),
+      Uint8Array.from(Buffer.from('0x0000000000000000000000000000000000000000000000000000000000000002', 'hex')),
+    ],
   };
 
   const decoded = abiDecodeError(abi, payload);
