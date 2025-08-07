aa5392e5d382ac46ad935982e1d23f690c25951f
rustdoc <github@users.noreply.github.com>
2025-08-07 20:14:26 +0000

    update rustdoc

--- docs/noirc_frontend/ownership/sidebar-items.js
+++ docs/noirc_frontend/ownership/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["clone_expr","contains_array_or_str_type","unwrap_tuple_type"],"mod":["last_uses"],"struct":["Context"]};
+window.SIDEBAR_ITEMS = {"fn":["clone_expr","contains_array_or_str_type","contains_index","unwrap_tuple_type"],"mod":["last_uses"],"struct":["Context"]};
