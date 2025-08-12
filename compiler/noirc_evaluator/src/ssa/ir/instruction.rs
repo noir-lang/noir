149a370036905ec72f0ffcf819cd3bddaa10484b
Tom French <15848336+TomAFrench@users.noreply.github.com>
2025-08-12 15:59:22 +0000

    fix: mark bitshifts as no longer licm-able

--- compiler/noirc_evaluator/src/ssa/ir/instruction.rs
+++ compiler/noirc_evaluator/src/ssa/ir/instruction.rs
@@ -757,7 +757,7 @@ impl Binary {
                 // for unsigned types (here we assume the type of binary.lhs is the same)
                 dfg.type_of_value(self.rhs).is_unsigned()
             }
-            BinaryOp::Div | BinaryOp::Mod => true,
+            BinaryOp::Div | BinaryOp::Mod | BinaryOp::Shl | BinaryOp::Shr => true,
             BinaryOp::Add { unchecked: true }
             | BinaryOp::Sub { unchecked: true }
             | BinaryOp::Mul { unchecked: true }
@@ -765,9 +765,7 @@ impl Binary {
             | BinaryOp::Lt
             | BinaryOp::And
             | BinaryOp::Or
-            | BinaryOp::Xor
-            | BinaryOp::Shl
-            | BinaryOp::Shr => false,
+            | BinaryOp::Xor => false,
         }
     }
 }
