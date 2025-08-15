f40c17080f9035a5d0e991adec3b94c6b278f125
github-action-benchmark <github@users.noreply.github.com>
2025-08-15 11:36:32 +0000

    add Opcode count (customSmallerIsBetter) benchmark result for bae92367d46588ef018977d3ecd9def4e653fa48

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755257790883,
+  "lastUpdate": 1755257792545,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -23010,40 +23010,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "asterite@gmail.com",
-            "name": "Ary Borenszweig",
-            "username": "asterite"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
-          "id": "5d6dd7e10dce9c47fbb1c3da822d40198bf3f118",
-          "message": "fix: perserve purities after SSA normalization (#9355)\n\nCo-authored-by: jfecher <jfecher11@gmail.com>",
-          "timestamp": "2025-07-30T14:58:35Z",
-          "tree_id": "1371f92c367de81aeca39c1ec8cc3631efc40f1c",
-          "url": "https://github.com/noir-lang/noir/commit/5d6dd7e10dce9c47fbb1c3da822d40198bf3f118"
-        },
-        "date": 1753889158929,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "semaphore-depth-10",
-            "value": 5763,
-            "unit": "opcodes"
-          },
-          {
-            "name": "sha512-100-bytes",
-            "value": 13173,
-            "unit": "opcodes"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "jfecher11@gmail.com",
             "name": "jfecher",
             "username": "jfecher"
@@ -26493,6 +26459,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/noir-lang/noir/commit/3052aa0e75c4eacf173adc272c4a087bb184be30"
         },
         "date": 1755199926724,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "semaphore-depth-10",
+            "value": 5763,
+            "unit": "opcodes"
+          },
+          {
+            "name": "sha512-100-bytes",
+            "value": 13173,
+            "unit": "opcodes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "15848336+TomAFrench@users.noreply.github.com",
+            "name": "Tom French",
+            "username": "TomAFrench"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bae92367d46588ef018977d3ecd9def4e653fa48",
+          "message": "chore: add missing cases of snapshot tests in `remove_bit_shifts` (#9505)",
+          "timestamp": "2025-08-15T12:31:23+01:00",
+          "tree_id": "e4e645f64721a60574d8909ec8f245f9ebc5f204",
+          "url": "https://github.com/noir-lang/noir/commit/bae92367d46588ef018977d3ecd9def4e653fa48"
+        },
+        "date": 1755257771984,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
