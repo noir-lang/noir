c5cb09ee89fc079a6db77fb88a6425038a00f1fb
jewelofchaos9 <jewelofchaos9@gmail.com>
2025-08-18 11:24:26 +0000

    init

--- tooling/ssa_fuzzer/fuzzer/src/mutations/basic_types/vec.rs
+++ tooling/ssa_fuzzer/fuzzer/src/mutations/basic_types/vec.rs
@@ -8,34 +8,18 @@
 //! 6. Push a default element to the end of the vector
 
 use crate::mutations::configuration::{
-    SIZE_OF_LARGE_ARBITRARY_BUFFER, SIZE_OF_SMALL_ARBITRARY_BUFFER, VecMutationConfig,
-    VecMutationOptions,
+    SIZE_OF_LARGE_ARBITRARY_BUFFER, VecMutationConfig, VecMutationOptions,
 };
 use libfuzzer_sys::arbitrary::{Arbitrary, Unstructured};
 use rand::{Rng, rngs::StdRng};
 
-trait MutateVec<T>
-where
-    T: for<'a> Arbitrary<'a> + Default,
-{
-    fn mutate(
-        rng: &mut StdRng,
-        vec: &mut Vec<T>,
-        mutate_element_function: impl Fn(&mut T, &mut StdRng),
-    );
-}
-
 /// Mutate the entire vector, replacing it with a random vector
 struct RandomMutation;
-impl<T> MutateVec<T> for RandomMutation
-where
-    T: for<'a> Arbitrary<'a> + Default,
-{
-    fn mutate(
-        rng: &mut StdRng,
-        vec: &mut Vec<T>,
-        _mutate_element_function: impl Fn(&mut T, &mut StdRng),
-    ) {
+impl RandomMutation {
+    fn mutate<T>(rng: &mut StdRng, vec: &mut Vec<T>)
+    where
+        T: for<'a> Arbitrary<'a>,
+    {
         let mut bytes = [0u8; SIZE_OF_LARGE_ARBITRARY_BUFFER];
         rng.fill(&mut bytes);
         *vec = Unstructured::new(&bytes).arbitrary().unwrap();
@@ -44,36 +28,28 @@ where
 
 /// Insert a random element at a random index
 struct RandomInsertion;
-impl<T> MutateVec<T> for RandomInsertion
-where
-    T: for<'a> Arbitrary<'a> + Default,
-{
-    fn mutate(
+impl RandomInsertion {
+    fn mutate<T>(
         rng: &mut StdRng,
         vec: &mut Vec<T>,
-        _mutate_element_function: impl Fn(&mut T, &mut StdRng),
-    ) {
-        let mut bytes = [0u8; SIZE_OF_SMALL_ARBITRARY_BUFFER];
-        rng.fill(&mut bytes);
-        let element = Unstructured::new(&bytes).arbitrary().unwrap();
+        generate_random_element_function: impl Fn(&mut StdRng) -> T,
+    ) where
+        T: for<'a> Arbitrary<'a>,
+    {
+        let element = generate_random_element_function(rng);
         if !vec.is_empty() {
             let index = rng.gen_range(0..vec.len());
             vec.insert(index, element);
+        } else {
+            vec.push(element);
         }
     }
 }
 
 /// Delete a random element at a random index
 struct RandomDeletion;
-impl<T> MutateVec<T> for RandomDeletion
-where
-    T: for<'a> Arbitrary<'a> + Default,
-{
-    fn mutate(
-        rng: &mut StdRng,
-        vec: &mut Vec<T>,
-        _mutate_element_function: impl Fn(&mut T, &mut StdRng),
-    ) {
+impl RandomDeletion {
+    fn mutate<T>(rng: &mut StdRng, vec: &mut Vec<T>) {
         if !vec.is_empty() {
             let index = rng.gen_range(0..vec.len());
             vec.remove(index);
@@ -83,15 +59,8 @@ where
 
 /// Swap two random elements at random indices
 struct RandomSwap;
-impl<T> MutateVec<T> for RandomSwap
-where
-    T: for<'a> Arbitrary<'a> + Default,
-{
-    fn mutate(
-        rng: &mut StdRng,
-        vec: &mut Vec<T>,
-        _mutate_element_function: impl Fn(&mut T, &mut StdRng),
-    ) {
+impl RandomSwap {
+    fn mutate<T>(rng: &mut StdRng, vec: &mut Vec<T>) {
         if !vec.is_empty() {
             let index1 = rng.gen_range(0..vec.len());
             let index2 = rng.gen_range(0..vec.len());
@@ -102,11 +71,8 @@ where
 
 /// Mutate a random element at a random index
 struct RandomElementMutation;
-impl<T> MutateVec<T> for RandomElementMutation
-where
-    T: for<'a> Arbitrary<'a> + Default,
-{
-    fn mutate(
+impl RandomElementMutation {
+    fn mutate<T>(
         rng: &mut StdRng,
         vec: &mut Vec<T>,
         mutate_element_function: impl Fn(&mut T, &mut StdRng),
@@ -120,15 +86,11 @@ where
 
 /// Push a default element to the end of the vector
 struct PushDefaultElement;
-impl<T> MutateVec<T> for PushDefaultElement
-where
-    T: for<'a> Arbitrary<'a> + Default,
-{
-    fn mutate(
-        _rng: &mut StdRng,
-        vec: &mut Vec<T>,
-        _mutate_element_function: impl Fn(&mut T, &mut StdRng),
-    ) {
+impl PushDefaultElement {
+    fn mutate<T>(vec: &mut Vec<T>)
+    where
+        T: Default,
+    {
         vec.push(T::default());
     }
 }
@@ -137,20 +99,21 @@ pub(crate) fn mutate_vec<T>(
     vec: &mut Vec<T>,
     rng: &mut StdRng,
     mutate_element_function: impl Fn(&mut T, &mut StdRng),
+    generate_random_element_function: impl Fn(&mut StdRng) -> T,
     config: VecMutationConfig,
 ) where
     T: for<'a> Arbitrary<'a> + Default,
 {
     match config.select(rng) {
-        VecMutationOptions::Random => RandomMutation::mutate(rng, vec, mutate_element_function),
-        VecMutationOptions::Insertion => RandomInsertion::mutate(rng, vec, mutate_element_function),
-        VecMutationOptions::Deletion => RandomDeletion::mutate(rng, vec, mutate_element_function),
-        VecMutationOptions::Swap => RandomSwap::mutate(rng, vec, mutate_element_function),
+        VecMutationOptions::Random => RandomMutation::mutate(rng, vec),
+        VecMutationOptions::Insertion => {
+            RandomInsertion::mutate(rng, vec, generate_random_element_function)
+        }
+        VecMutationOptions::Deletion => RandomDeletion::mutate(rng, vec),
+        VecMutationOptions::Swap => RandomSwap::mutate(rng, vec),
         VecMutationOptions::ElementMutation => {
             RandomElementMutation::mutate(rng, vec, mutate_element_function);
         }
-        VecMutationOptions::PushDefault => {
-            PushDefaultElement::mutate(rng, vec, mutate_element_function);
-        }
+        VecMutationOptions::PushDefault => PushDefaultElement::mutate(vec),
     }
 }
