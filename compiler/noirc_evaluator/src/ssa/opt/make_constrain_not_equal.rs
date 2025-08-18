1739d6aca203b23d8afc53cf75923fe2e0a14ce3
Ary Borenszweig <asterite@gmail.com>
2025-08-18 10:15:17 -0300

    Add a test for the brillig case

--- compiler/noirc_evaluator/src/ssa/opt/make_constrain_not_equal.rs
+++ compiler/noirc_evaluator/src/ssa/opt/make_constrain_not_equal.rs
@@ -97,10 +97,13 @@ impl Function {
 
 #[cfg(test)]
 mod tests {
-    use crate::{assert_ssa_snapshot, ssa::ssa_gen::Ssa};
+    use crate::{
+        assert_ssa_snapshot,
+        ssa::{opt::assert_normalized_ssa_equals, ssa_gen::Ssa},
+    };
 
     #[test]
-    fn test_make_constrain_not_equals() {
+    fn replaces_constrain_with_constrain_not_equal_in_acir() {
         let src = "
         acir(inline) fn main f1 {
           b0(v0: Field, v1: Field):
@@ -119,5 +122,22 @@ mod tests {
             return
         }
         ");
+    }
+
+    #[test]
+    fn does_not_replace_constrain_with_constrain_not_equal_in_brillig() {
+        let src = "
+        brillig(inline) fn main f1 {
+          b0(v0: Field, v1: Field):
+            v2 = eq v0, v1
+            constrain v2 == u1 0
+            return
+        }
+        ";
+        let ssa = Ssa::from_str(src).unwrap();
+        let ssa = ssa.make_constrain_not_equal();
+
+        // Expect no changes
+        assert_normalized_ssa_equals(ssa, src);
     }
 }
