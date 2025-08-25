990d54621ef46a0a868c94682f93dc71ad4a2cba
github-action-benchmark <github@users.noreply.github.com>
2025-08-25 18:28:52 +0000

    add ACVM Benchmarks (cargo) benchmark result for f435c938ca1e1a2ce4449a484cc6d3dae270b3dd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756145872122,
+  "lastUpdate": 1756146531589,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -20171,48 +20171,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "163862677+noirwhal@users.noreply.github.com",
-            "name": "noirwhal",
-            "username": "noirwhal"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "7bda5023c198f20e7f328c51baf0b5125b851b9b",
-          "message": "chore: bump external pinned commits (#9530)\n\nCo-authored-by: noirwhal <tomfrench@aztecprotocol.com>",
-          "timestamp": "2025-08-18T09:55:42Z",
-          "tree_id": "40945480bf4d362e5b05d9dc1c3b84d6c77bf30d",
-          "url": "https://github.com/noir-lang/noir/commit/7bda5023c198f20e7f328c51baf0b5125b851b9b"
-        },
-        "date": 1755512814333,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 247555,
-            "range": "± 1490",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 219113,
-            "range": "± 6855",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 2812380,
-            "range": "± 1904",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "84741533+defkit@users.noreply.github.com",
             "name": "defkit",
             "username": "defkit"
@@ -22264,6 +22222,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 2779793,
             "range": "± 1502",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mvezenov@gmail.com",
+            "name": "Maxim Vezenov",
+            "username": "vezenovm"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f435c938ca1e1a2ce4449a484cc6d3dae270b3dd",
+          "message": "fix(inlining): Do not inline globals and lower them during ACIR gen (#9626)",
+          "timestamp": "2025-08-25T18:01:47Z",
+          "tree_id": "2dbb2effc17825d83f37510e5fe162ad42bae891",
+          "url": "https://github.com/noir-lang/noir/commit/f435c938ca1e1a2ce4449a484cc6d3dae270b3dd"
+        },
+        "date": 1756146512014,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 272532,
+            "range": "± 1994",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 236773,
+            "range": "± 2397",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 2913617,
+            "range": "± 15914",
             "unit": "ns/iter"
           }
         ]
