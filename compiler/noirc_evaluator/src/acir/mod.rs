f435c938ca1e1a2ce4449a484cc6d3dae270b3dd
Maxim Vezenov <mvezenov@gmail.com>
2025-08-25 14:01:47 -0400

    fix(inlining): Do not inline globals and lower them during ACIR gen (#9626)

--- compiler/noirc_evaluator/src/acir/mod.rs
+++ compiler/noirc_evaluator/src/acir/mod.rs
@@ -6,9 +6,9 @@
 //! ACIR generation is performed by calling the [Ssa::into_acir] method, providing any necessary brillig bytecode.
 //! The compiled program will be returned as an [`Artifacts`] type.
 
-use fxhash::FxHashMap as HashMap;
+use fxhash::{FxHashMap as HashMap, FxHashSet as HashSet};
 use noirc_errors::call_stack::CallStack;
-use std::collections::{BTreeMap, HashSet};
+use std::collections::BTreeMap;
 use types::{AcirDynamicArray, AcirValue};
 
 use acvm::acir::{
@@ -85,6 +85,12 @@ struct SharedContext<F: AcirField> {
     /// Keeps track of Brillig std lib calls per function that need to still be resolved
     /// with the correct function pointer from the `brillig_stdlib_func_pointer` map.
     brillig_stdlib_calls_to_resolve: HashMap<FunctionId, Vec<(OpcodeLocation, BrilligFunctionId)>>,
+
+    /// `used_globals` needs to be built from a function call graph.
+    ///
+    /// Maps an ACIR function to the globals used in that function.
+    /// This includes all globals used in functions called internally.
+    used_globals: HashMap<FunctionId, HashSet<ValueId>>,
 }
 
 impl<F: AcirField> SharedContext<F> {
@@ -239,7 +245,7 @@ impl<'a> Context<'a> {
             ssa_values: HashMap::default(),
             current_side_effects_enabled_var,
             acir_context,
-            initialized_arrays: HashSet::new(),
+            initialized_arrays: HashSet::default(),
             memory_blocks: HashMap::default(),
             internal_memory_blocks: HashMap::default(),
             internal_mem_block_lengths: HashMap::default(),
@@ -315,8 +321,37 @@ impl<'a> Context<'a> {
             expr.to_witness().expect("return vars should be witnesses")
         });
 
-        self.data_bus = dfg.data_bus.to_owned();
         let mut warnings = Vec::new();
+
+        let used_globals =
+            self.shared_context.used_globals.remove(&main_func.id()).unwrap_or_default();
+
+        let globals_dfg = (*main_func.dfg.globals).clone();
+        let globals_dfg = DataFlowGraph::from(globals_dfg);
+        for (id, value) in globals_dfg.values_iter() {
+            if !used_globals.contains(&id) {
+                continue;
+            }
+            match value {
+                Value::NumericConstant { .. } => {
+                    self.convert_value(id, dfg);
+                }
+                Value::Instruction { instruction, .. } => {
+                    warnings.extend(self.convert_ssa_instruction(
+                        *instruction,
+                        &globals_dfg,
+                        ssa,
+                    )?);
+                }
+                _ => {
+                    panic!(
+                        "Expected either an instruction or a numeric constant for a global value"
+                    )
+                }
+            }
+        }
+
+        self.data_bus = dfg.data_bus.to_owned();
         for instruction_id in entry_block.instructions() {
             warnings.extend(self.convert_ssa_instruction(*instruction_id, dfg, ssa)?);
         }
