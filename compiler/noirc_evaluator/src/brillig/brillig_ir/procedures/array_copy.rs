7a0a16f0b94d3ab3681dfe7d3f9cf912df2e7b48
Maxim Vezenov <mvezenov@gmail.com>
2025-08-25 15:09:44 +0000

    cleanyp

--- compiler/noirc_evaluator/src/brillig/brillig_ir/procedures/array_copy.rs
+++ compiler/noirc_evaluator/src/brillig/brillig_ir/procedures/array_copy.rs
@@ -153,8 +153,7 @@ impl<F: AcirField + DebugToString, Registers: RegisterAllocator> BrilligContext<
         });
 
         let newline = ValueOrArray::MemoryAddress(ReservedRegisters::usize_one());
-        let message_with_func_name =
-            format!("{ARRAY_COPY_COUNTER_MESSAGE} {}: {{}}", &self.name(),);
+        let message_with_func_name = format!("{ARRAY_COPY_COUNTER_MESSAGE} {}: {{}}", &self.name());
         let message = literal_string_to_value(&message_with_func_name, self);
         let item_count = ValueOrArray::MemoryAddress(ReservedRegisters::usize_one());
         let value_to_print = ValueOrArray::MemoryAddress(array_copy_counter.extract_register());
