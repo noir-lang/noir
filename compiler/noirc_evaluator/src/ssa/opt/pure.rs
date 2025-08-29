55df6bc5ea2fc06a8d56babc63e14f5f71468fc4
Maxim Vezenov <mvezenov@gmail.com>
2025-08-29 14:22:47 +0000

    insert in release

--- compiler/noirc_evaluator/src/ssa/opt/pure.rs
+++ compiler/noirc_evaluator/src/ssa/opt/pure.rs
@@ -246,10 +246,8 @@ fn analyze_call_graph(
             // Each function belongs to exactly one SCC by definition of SCCs.
             // Therefore inserting into func_to_scc here is safe, and there will
             // be no overwrites.
-            debug_assert!(
-                func_to_scc.insert(func, i).is_none(),
-                "Function appears in multiple SCCs"
-            );
+            let inserted = func_to_scc.insert(func, i);
+            debug_assert!(inserted.is_none(), "Function appears in multiple SCCs");
         }
     }
 
