ef4454f9434ffc69d93eb060bfc8e89abe8de5f3
Tom French <15848336+TomAFrench@users.noreply.github.com>
2025-09-01 18:27:53 +0000

    .

--- compiler/noirc_frontend/src/test_utils.rs
+++ compiler/noirc_frontend/src/test_utils.rs
@@ -57,9 +57,7 @@ pub(crate) fn remove_experimental_warnings(errors: &mut Vec<CompilationError>) {
     });
 }
 
-pub(crate) fn get_program<'a, 'b>(
-    src: &'a str,
-) -> (ParsedModule, Context<'a, 'b>, Vec<CompilationError>) {
+pub(crate) fn get_program(src: &str) -> (ParsedModule, Context, Vec<CompilationError>) {
     let allow_parser_errors = false;
     get_program_with_options(src, allow_parser_errors, FrontendOptions::test_default())
 }
