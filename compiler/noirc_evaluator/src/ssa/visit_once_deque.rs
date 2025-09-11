20702563d4c2e0c2a4ce04d9845218a9dd735c4c
Tom French <15848336+TomAFrench@users.noreply.github.com>
2025-09-11 12:33:50 +0000

    chore: add quick sanity test

--- compiler/noirc_evaluator/src/ssa/visit_once_deque.rs
+++ compiler/noirc_evaluator/src/ssa/visit_once_deque.rs
@@ -28,3 +28,26 @@ impl VisitOnceDeque {
         if self.visited_blocks.insert(item) { Some(item) } else { self.pop_back() }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::ssa::{ir::basic_block::BasicBlockId, visit_once_deque::VisitOnceDeque};
+
+    #[test]
+    fn does_not_return_duplicates() {
+        let mut deque = VisitOnceDeque::default();
+        deque.extend([
+            BasicBlockId::test_new(0),
+            BasicBlockId::test_new(1),
+            BasicBlockId::test_new(2),
+            BasicBlockId::test_new(0),
+            BasicBlockId::test_new(1),
+        ]);
+
+        assert_eq!(deque.pop_front(), Some(BasicBlockId::test_new(0)));
+        assert_eq!(deque.pop_back(), Some(BasicBlockId::test_new(1)));
+        assert_eq!(deque.pop_front(), Some(BasicBlockId::test_new(2)));
+        assert_eq!(deque.pop_front(), None);
+        assert_eq!(deque.pop_back(), None);
+    }
+}
