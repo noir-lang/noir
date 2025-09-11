7495637fb630103ea04df26ff05086dba202275f
Ary Borenszweig <asterite@gmail.com>
2025-09-11 14:28:58 -0300

    Move comments collector to separate file

--- tooling/lsp/src/requests/folding_range.rs
+++ tooling/lsp/src/requests/folding_range.rs
@@ -2,21 +2,15 @@ use std::future;
 
 use async_lsp::{
     ResponseError,
-    lsp_types::{
-        self, FoldingRange, FoldingRangeKind, FoldingRangeParams, Position,
-        TextDocumentPositionParams,
-    },
-};
-use fm::{FileId, FileMap};
-use noirc_frontend::{
-    lexer::Lexer,
-    token::{DocStyle, Token},
+    lsp_types::{FoldingRange, FoldingRangeParams, Position, TextDocumentPositionParams},
 };
 
 use crate::{
     LspState,
-    requests::{process_request, to_lsp_location},
+    requests::{folding_range::comments_collector::FoldingRangeCommentsCollector, process_request},
 };
+
+mod comments_collector;
 
 pub(crate) fn on_folding_range_request(
     state: &mut LspState,
@@ -31,111 +25,14 @@ pub(crate) fn on_folding_range_request(
         let file_id = args.location.file;
         let file = args.files.get_file(file_id).unwrap();
         let source = file.source();
+
         let comments_collector = FoldingRangeCommentsCollector::new(file_id, args.files);
         let comment_ranges = comments_collector.collect(source);
+
         Some(comment_ranges)
     });
 
     future::ready(result)
-}
-
-struct FoldingRangeCommentsCollector<'files> {
-    file_id: FileId,
-    files: &'files FileMap,
-    ranges: Vec<FoldingRange>,
-    current_line_comment_group: Option<LineCommentGroup>,
-}
-
-struct LineCommentGroup {
-    start: lsp_types::Location,
-    end: lsp_types::Location,
-    doc_style: Option<DocStyle>,
-}
-
-impl<'files> FoldingRangeCommentsCollector<'files> {
-    fn new(file_id: FileId, files: &'files FileMap) -> Self {
-        Self { file_id, files, ranges: Vec::new(), current_line_comment_group: None }
-    }
-
-    fn collect(mut self, source: &str) -> Vec<FoldingRange> {
-        let lexer = Lexer::new(source, self.file_id).skip_comments(false);
-
-        for token in lexer {
-            let Ok(token) = token else {
-                continue;
-            };
-
-            let location = token.location();
-
-            match token.into_token() {
-                Token::BlockComment(..) => {
-                    self.push_current_line_comment_group();
-
-                    let Some(location) = to_lsp_location(self.files, self.file_id, location.span)
-                    else {
-                        continue;
-                    };
-
-                    // Block comments are never grouped with other comments
-                    self.ranges.push(FoldingRange {
-                        start_line: location.range.start.line,
-                        start_character: None,
-                        end_line: location.range.end.line,
-                        end_character: None,
-                        kind: Some(FoldingRangeKind::Comment),
-                        collapsed_text: None,
-                    });
-                }
-                Token::LineComment(_, doc_style) => {
-                    let Some(location) = to_lsp_location(self.files, self.file_id, location.span)
-                    else {
-                        continue;
-                    };
-
-                    if let Some(group) = &mut self.current_line_comment_group {
-                        // Keep grouping while the line comment style is the same and they are consecutive lines
-                        if group.doc_style == doc_style
-                            && location.range.end.line - group.end.range.end.line <= 1
-                        {
-                            group.end = location;
-                            continue;
-                        }
-
-                        // A new group starts, so push the current one
-                        Self::push_line_comment_group(group, &mut self.ranges);
-                    }
-
-                    let start = location.clone();
-                    self.current_line_comment_group =
-                        Some(LineCommentGroup { start, end: location, doc_style });
-                }
-                _ => {
-                    self.push_current_line_comment_group();
-                }
-            }
-        }
-
-        self.push_current_line_comment_group();
-
-        self.ranges
-    }
-
-    fn push_current_line_comment_group(&mut self) {
-        if let Some(group) = self.current_line_comment_group.take() {
-            Self::push_line_comment_group(&group, &mut self.ranges);
-        }
-    }
-
-    fn push_line_comment_group(group: &LineCommentGroup, ranges: &mut Vec<FoldingRange>) {
-        ranges.push(FoldingRange {
-            start_line: group.start.range.start.line,
-            start_character: None,
-            end_line: group.end.range.end.line,
-            end_character: None,
-            kind: Some(FoldingRangeKind::Comment),
-            collapsed_text: None,
-        });
-    }
 }
 
 #[cfg(test)]
@@ -144,8 +41,8 @@ mod tests {
 
     use super::*;
     use async_lsp::lsp_types::{
-        DidOpenTextDocumentParams, PartialResultParams, TextDocumentIdentifier, TextDocumentItem,
-        WorkDoneProgressParams,
+        DidOpenTextDocumentParams, FoldingRangeKind, PartialResultParams, TextDocumentIdentifier,
+        TextDocumentItem, WorkDoneProgressParams,
     };
     use tokio::test;
 
@@ -245,6 +142,30 @@ mod tests {
         let range = &ranges[1];
         assert_eq!(range.start_line, 3);
         assert_eq!(range.end_line, 5);
+        assert_eq!(range.kind, Some(FoldingRangeKind::Comment));
+    }
+
+    #[test]
+    async fn test_series_of_mod() {
+        let src = "
+        mod one;
+        mod two;
+
+        mod three;
+        mod four;
+        mod five;
+        ";
+        let ranges = get_folding_ranges(src).await;
+        assert_eq!(ranges.len(), 2);
+
+        let range = &ranges[0];
+        assert_eq!(range.start_line, 1);
+        assert_eq!(range.end_line, 2);
+        assert_eq!(range.kind, Some(FoldingRangeKind::Comment));
+
+        let range = &ranges[1];
+        assert_eq!(range.start_line, 4);
+        assert_eq!(range.end_line, 6);
         assert_eq!(range.kind, Some(FoldingRangeKind::Comment));
     }
 }
