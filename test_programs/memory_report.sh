7bde491bb3d57a8448354caa5ca6ca6c34d885b6
Tom French <15848336+TomAFrench@users.noreply.github.com>
2025-09-16 18:22:56 +0000

    .

--- test_programs/memory_report.sh
+++ test_programs/memory_report.sh
@@ -40,7 +40,7 @@ for test_path in ${tests_to_profile[@]}; do
             test_name=$(basename $test_path)
         fi
 
-        if [ -n "$test_name" ]; then
+        if [ -z "$test_name" ]; then
             echo "test name is empty"
             exit 1
         fi
