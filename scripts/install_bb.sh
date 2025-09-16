cf9553aa83285fdcf95efc8f26b8f4d67656405a
Tom French <15848336+TomAFrench@users.noreply.github.com>
2025-09-16 18:03:40 +0000

    .

--- scripts/install_bb.sh
+++ scripts/install_bb.sh
@@ -5,7 +5,7 @@ VERSION="v3.0.0-nightly.20250916"
 BBUP_PATH=~/.bb/bbup
 
 if ! [ -f $BBUP_PATH ]; then 
-    curl -L https://bbup.aztec.network | bash
+    curl -L https://raw.githubusercontent.com/AztecProtocol/aztec-packages/refs/heads/next/barretenberg/bbup/install | bash
 fi
 
 $BBUP_PATH -v $VERSION
