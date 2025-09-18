70b11106f9be7a50eea653b518dd5946b7c4ddc2
Maxim Vezenov <mvezenov@gmail.com>
2025-09-18 21:41:42 +0000

    intercept buffer/index.js again

--- compiler/integration-tests/web-test-runner.config.mjs
+++ compiler/integration-tests/web-test-runner.config.mjs
@@ -36,6 +36,9 @@ export default {
       if (ctx.url.includes('pino/browser.js')) {
         ctx.url = '/compiler/integration-tests/test/mocks/pino.js';
       }
+      if (ctx.url.includes('buffer/index.js')) {
+        ctx.url = '/compiler/integration-tests/test/mocks/buffer.js';
+      }
       await next();
     },
   ],
