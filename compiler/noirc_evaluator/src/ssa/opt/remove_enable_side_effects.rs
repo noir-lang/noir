3175621a8a5c191ecacb442236d1e1e5f0afe618
Akosh Farkash <aakoshh@gmail.com>
2025-09-19 17:59:31 +0100

    Fix more unit tests

--- compiler/noirc_evaluator/src/ssa/opt/remove_enable_side_effects.rs
+++ compiler/noirc_evaluator/src/ssa/opt/remove_enable_side_effects.rs
@@ -292,10 +292,10 @@ mod test {
     fn remove_enable_side_effects_for_slice_push_front() {
         let src = "
         acir(inline) predicate_pure fn main f0 {
-          b0(v0: [u32; 3], v1: u32):
+          b0(v0: [u32; 3], v1: u1, v2: u32):
             v3 = array_get v0, index u32 0 -> u32
             v7 = make_array [Field 1, Field 2, Field 3] : [Field]
-            v9 = array_set v7, index v0, value Field 4
+            v9 = array_set v7, index v2, value Field 4
 
             // this instruction should be removed
             enable_side_effects v1
@@ -308,11 +308,11 @@ mod test {
         let ssa = ssa.remove_enable_side_effects();
         assert_ssa_snapshot!(ssa, @r"
         acir(inline) predicate_pure fn main f0 {
-          b0(v0: u32, v1: u1):
-            v3 = array_get v0, index u32 0 -> u32
-            v7 = make_array [Field 1, Field 2, Field 3] : [Field]
-            v9 = array_set v7, index v0, value Field 4
-            v13, v14 = call slice_push_front(u32 3, v9, Field 5) -> (u32, [Field])
+          b0(v0: [u32; 3], v1: u1, v2: u32):
+            v4 = array_get v0, index u32 0 -> u32
+            v8 = make_array [Field 1, Field 2, Field 3] : [Field]
+            v10 = array_set v8, index v2, value Field 4
+            v14, v15 = call slice_push_front(u32 3, v10, Field 5) -> (u32, [Field])
             return
         }
         ");
@@ -354,10 +354,10 @@ mod test {
     fn keep_enable_side_effects_for_slice_insert() {
         let src = "
         acir(inline) predicate_pure fn main f0 {
-          b0(v0: [u32; 3], v1: u1):
+          b0(v0: [u32; 3], v1: u1, v2: u32):
             v3 = array_get v0, index u32 0 -> u32
             v7 = make_array [Field 1, Field 2, Field 3] : [Field]
-            v9 = array_set v7, index v0, value Field 4
+            v9 = array_set v7, index v2, value Field 4
             enable_side_effects v1
             v13, v14 = call slice_insert(u32 3, v9, u32 1, Field 5) -> (u32, [Field])
             return
@@ -370,10 +370,10 @@ mod test {
     fn keep_enable_side_effects_for_slice_remove() {
         let src = "
         acir(inline) predicate_pure fn main f0 {
-          b0(v0: [u32; 3], v1: u32):
+          b0(v0: [u32; 3], v1: u1, v2: u32):
             v3 = array_get v0, index u32 0 -> u32
             v7 = make_array [Field 1, Field 2, Field 3] : [Field]
-            v9 = array_set v7, index v0, value Field 4
+            v9 = array_set v7, index v2, value Field 4
             enable_side_effects v1
             v13, v14, v15 = call slice_remove(u32 3, v9, u32 1) -> (u32, [Field], Field)
             return
