912953ece1603bb8afabefe217723c899272ac9f
Ary Borenszweig <asterite@gmail.com>
2025-09-22 15:14:10 -0300

    Use `results()`

--- compiler/noirc_evaluator/src/ssa/opt/constant_folding/mod.rs
+++ compiler/noirc_evaluator/src/ssa/opt/constant_folding/mod.rs
@@ -23,7 +23,7 @@ use crate::ssa::{
     interpreter::{Interpreter, InterpreterOptions},
     ir::{
         basic_block::BasicBlockId,
-        dfg::{DataFlowGraph, InsertInstructionResult},
+        dfg::DataFlowGraph,
         dom::DominatorTree,
         function::{Function, FunctionId},
         instruction::{ArrayOffset, Instruction, InstructionId},
@@ -350,18 +350,14 @@ impl Context {
             .then(|| vecmap(old_results, |result| dfg.type_of_value(*result)));
 
         let call_stack = dfg.get_instruction_call_stack_id(id);
-        let new_results = match dfg.insert_instruction_and_results_if_simplified(
+        let results = dfg.insert_instruction_and_results_if_simplified(
             instruction,
             block,
             ctrl_typevars,
             call_stack,
             Some(id),
-        ) {
-            InsertInstructionResult::SimplifiedTo(new_result) => vec![new_result],
-            InsertInstructionResult::SimplifiedToMultiple(new_results) => new_results,
-            InsertInstructionResult::Results(_, new_results) => new_results.to_vec(),
-            InsertInstructionResult::InstructionRemoved => vec![],
-        };
+        );
+        let new_results = results.results().to_vec();
         // Optimizations while inserting the instruction should not change the number of results.
         assert_eq!(old_results.len(), new_results.len());
 
