59665565e695954eb48de06e5026277da183c3b5
Maxim Vezenov <mvezenov@gmail.com>
2025-09-26 17:18:09 +0000

    remove debug stuff

--- compiler/noirc_evaluator/src/acir/tests/mod.rs
+++ compiler/noirc_evaluator/src/acir/tests/mod.rs
@@ -35,9 +35,7 @@ fn ssa_to_acir_program(src: &str) -> Program<FieldElement> {
 }
 
 fn ssa_to_acir_program_with_debug_info(src: &str) -> (Program<FieldElement>, Vec<DebugInfo>) {
-    let mut ssa = Ssa::from_str(src).unwrap();
-    ssa.normalize_ids();
-    println!("{}", ssa.print_with(None));
+    let ssa = Ssa::from_str(src).unwrap();
     let arg_size_and_visibilities = ssa
         .functions
         .iter()
