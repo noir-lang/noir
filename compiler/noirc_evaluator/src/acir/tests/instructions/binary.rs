83d77047665cd9ac4d1c2359e732567986c04fdd
Ary Borenszweig <asterite@gmail.com>
2025-09-29 14:46:23 -0300

    Add some more, some with predicates

--- compiler/noirc_evaluator/src/acir/tests/instructions/binary.rs
+++ compiler/noirc_evaluator/src/acir/tests/instructions/binary.rs
@@ -162,7 +162,7 @@ fn unchecked_add_u8() {
 }
 
 #[test]
-fn checked_add_u8() {
+fn checked_add_u8_no_predicate() {
     let src = "
     acir(inline) fn main f0 {
       b0(v0: u8, v1: u8):
@@ -185,10 +185,65 @@ fn checked_add_u8() {
     ");
 }
 
-// No tests for sub/mul since they'll be similar to the ones for add
+#[test]
+fn checked_add_u8_with_predicate() {
+    let src = "
+    acir(inline) fn main f0 {
+      b0(v0: u8, v1: u8, v2: u1):
+        enable_side_effects v2
+        v3 = add v0, v1
+        return v3
+    }
+    ";
+    let program = ssa_to_acir_program(src);
+
+    // Note how every operand in the addition (w0, w1) is multiplied by the predicate (w2)
+    assert_circuit_snapshot!(program, @r"
+    func 0
+    current witness: w4
+    private parameters: [w0, w1, w2]
+    public parameters: []
+    return values: [w3]
+    BLACKBOX::RANGE [w0]:8 bits []
+    BLACKBOX::RANGE [w1]:8 bits []
+    BLACKBOX::RANGE [w2]:1 bits []
+    EXPR w4 = w0*w2 + w1*w2
+    BLACKBOX::RANGE [w4]:8 bits []
+    EXPR w4 = w3
+    ");
+}
 
 #[test]
-fn div_u8() {
+fn checked_mul_u8_with_predicate() {
+    let src = "
+    acir(inline) fn main f0 {
+      b0(v0: u8, v1: u8, v2: u1):
+        enable_side_effects v2
+        v3 = mul v0, v1
+        return v3
+    }
+    ";
+    let program = ssa_to_acir_program(src);
+
+    // w0 is multiplied by w1, then the reslt (w4) is multiplied by the predicate (w2)
+    assert_circuit_snapshot!(program, @r"
+    func 0
+    current witness: w5
+    private parameters: [w0, w1, w2]
+    public parameters: []
+    return values: [w3]
+    BLACKBOX::RANGE [w0]:8 bits []
+    BLACKBOX::RANGE [w1]:8 bits []
+    BLACKBOX::RANGE [w2]:1 bits []
+    EXPR w4 = w0*w1
+    EXPR w5 = w2*w4
+    BLACKBOX::RANGE [w5]:8 bits []
+    EXPR w5 = w3
+    ");
+}
+
+#[test]
+fn div_u8_no_predicate() {
     let src = "
     acir(inline) fn main f0 {
       b0(v0: u8, v1: u8):
@@ -221,6 +276,117 @@ fn div_u8() {
     BLACKBOX::RANGE [w6]:8 bits []
     EXPR w5 = -w1*w4 + w0
     EXPR w4 = w2
+
+    unconstrained func 0: directive_invert
+    0: @21 = const u32 1
+    1: @20 = const u32 0
+    2: @0 = calldata copy [@20; @21]
+    3: @2 = const field 0
+    4: @3 = field eq @0, @2
+    5: jump if @3 to 8
+    6: @1 = const field 1
+    7: @0 = field field_div @1, @0
+    8: stop &[@20; @21]
+    unconstrained func 1: directive_integer_quotient
+    0: @10 = const u32 2
+    1: @11 = const u32 0
+    2: @0 = calldata copy [@11; @10]
+    3: @2 = field int_div @0, @1
+    4: @1 = field mul @2, @1
+    5: @1 = field sub @0, @1
+    6: @0 = @2
+    7: stop &[@11; @10]
+    ");
+}
+
+#[test]
+fn div_u8_with_predicate() {
+    let src = "
+    acir(inline) fn main f0 {
+      b0(v0: u8, v1: u8, v2: u1):
+        enable_side_effects v2
+        v3 = div v0, v1
+        return v3
+    }
+    ";
+    let program = ssa_to_acir_program(src);
+
+    // TODO: explain this result
+    assert_circuit_snapshot!(program, @r"
+    func 0
+    current witness: w9
+    private parameters: [w0, w1, w2]
+    public parameters: []
+    return values: [w3]
+    BLACKBOX::RANGE [w0]:8 bits []
+    BLACKBOX::RANGE [w1]:8 bits []
+    BLACKBOX::RANGE [w2]:1 bits []
+    BRILLIG CALL func 0: inputs: [w1], outputs: [w4]
+    EXPR w5 = -w1*w4 + 1
+    EXPR 0 = w1*w5
+    EXPR 0 = w2*w5
+    BRILLIG CALL func 1: PREDICATE: w2
+    inputs: [w0, w1], outputs: [w6, w7]
+    BLACKBOX::RANGE [w6]:8 bits []
+    BLACKBOX::RANGE [w7]:8 bits []
+    EXPR w8 = w1 - w2 - w7
+    BLACKBOX::RANGE [w8]:8 bits []
+    EXPR w9 = w1*w6 + w7
+    EXPR 0 = w0*w2 - w2*w9
+    EXPR w6 = w3
+
+    unconstrained func 0: directive_invert
+    0: @21 = const u32 1
+    1: @20 = const u32 0
+    2: @0 = calldata copy [@20; @21]
+    3: @2 = const field 0
+    4: @3 = field eq @0, @2
+    5: jump if @3 to 8
+    6: @1 = const field 1
+    7: @0 = field field_div @1, @0
+    8: stop &[@20; @21]
+    unconstrained func 1: directive_integer_quotient
+    0: @10 = const u32 2
+    1: @11 = const u32 0
+    2: @0 = calldata copy [@11; @10]
+    3: @2 = field int_div @0, @1
+    4: @1 = field mul @2, @1
+    5: @1 = field sub @0, @1
+    6: @0 = @2
+    7: stop &[@11; @10]
+    ");
+}
+
+#[test]
+fn mod_u8_no_predicate() {
+    let src = "
+    acir(inline) fn main f0 {
+      b0(v0: u8, v1: u8):
+        v2 = mod v0, v1
+        return v2
+    }
+    ";
+    let program = ssa_to_acir_program(src);
+
+    // This is similar to div_u8_no_predicate except that the return witness is constrained
+    // against w5 instead of w4.
+    assert_circuit_snapshot!(program, @r"
+    func 0
+    current witness: w6
+    private parameters: [w0, w1]
+    public parameters: []
+    return values: [w2]
+    BLACKBOX::RANGE [w0]:8 bits []
+    BLACKBOX::RANGE [w1]:8 bits []
+    BRILLIG CALL func 0: inputs: [w1], outputs: [w3]
+    EXPR 0 = w1*w3 - 1
+    BRILLIG CALL func 1: inputs: [w0, w1], outputs: [w4, w5]
+    BLACKBOX::RANGE [w4]:8 bits []
+    BLACKBOX::RANGE [w5]:8 bits []
+    EXPR w6 = w1 - w5 - 1
+    BLACKBOX::RANGE [w6]:8 bits []
+    EXPR w5 = -w1*w4 + w0
+    EXPR w5 = w2
 
     unconstrained func 0: directive_invert
     0: @21 = const u32 1
@@ -291,5 +457,79 @@ fn lt_u8() {
     5: @1 = field sub @0, @1
     6: @0 = @2
     7: stop &[@11; @10]
+    ");
+}
+
+#[test]
+fn and_u8() {
+    let src = "
+    acir(inline) fn main f0 {
+      b0(v0: u8, v1: u8):
+        v2 = and v0, v1
+        return v2
+    }
+    ";
+    let program = ssa_to_acir_program(src);
+
+    assert_circuit_snapshot!(program, @r"
+    func 0
+    current witness: w3
+    private parameters: [w0, w1]
+    public parameters: []
+    return values: [w2]
+    BLACKBOX::RANGE [w0]:8 bits []
+    BLACKBOX::RANGE [w1]:8 bits []
+    BLACKBOX::AND [w0, w1]:8 bits [w3]
+    EXPR w3 = w2
+    ");
+}
+
+#[test]
+fn or_u8() {
+    let src = "
+    acir(inline) fn main f0 {
+      b0(v0: u8, v1: u8):
+        v2 = or v0, v1
+        return v2
+    }
+    ";
+    let program = ssa_to_acir_program(src);
+
+    assert_circuit_snapshot!(program, @r"
+    func 0
+    current witness: w5
+    private parameters: [w0, w1]
+    public parameters: []
+    return values: [w2]
+    BLACKBOX::RANGE [w0]:8 bits []
+    BLACKBOX::RANGE [w1]:8 bits []
+    EXPR w3 = -w0 + 255
+    EXPR w4 = -w1 + 255
+    BLACKBOX::AND [w3, w4]:8 bits [w5]
+    EXPR w5 = -w2 + 255
+    ");
+}
+
+#[test]
+fn xor_u8() {
+    let src = "
+    acir(inline) fn main f0 {
+      b0(v0: u8, v1: u8):
+        v2 = xor v0, v1
+        return v2
+    }
+    ";
+    let program = ssa_to_acir_program(src);
+
+    assert_circuit_snapshot!(program, @r"
+    func 0
+    current witness: w3
+    private parameters: [w0, w1]
+    public parameters: []
+    return values: [w2]
+    BLACKBOX::RANGE [w0]:8 bits []
+    BLACKBOX::RANGE [w1]:8 bits []
+    BLACKBOX::XOR [w0, w1]:8 bits [w3]
+    EXPR w3 = w2
     ");
 }
