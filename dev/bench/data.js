1c30664e2192ac55553d47718737adfb2d938198
github-action-benchmark <github@users.noreply.github.com>
2025-10-01 19:03:15 +0000

    add ACVM Benchmarks (cargo) benchmark result for 79ef33bd7b9325ea91ec174e53562cb13874c4a1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759344988525,
+  "lastUpdate": 1759345395117,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -22816,48 +22816,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "asterite@gmail.com",
-            "name": "Ary Borenszweig",
-            "username": "asterite"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
-          "id": "4696f026afa515404770b21b705be24c807702cb",
-          "message": "chore: minor constant_folding refactors (#9954)\n\nCo-authored-by: Maxim Vezenov <mvezenov@gmail.com>",
-          "timestamp": "2025-09-22T19:22:55Z",
-          "tree_id": "a5f7525025dc0c3d0c1519420c163f16330f8213",
-          "url": "https://github.com/noir-lang/noir/commit/4696f026afa515404770b21b705be24c807702cb"
-        },
-        "date": 1758570371790,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 252242,
-            "range": "± 786",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 225267,
-            "range": "± 11932",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 2783607,
-            "range": "± 7196",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "aakoshh@gmail.com",
             "name": "Akosh Farkash",
             "username": "aakoshh"
@@ -24909,6 +24867,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 2781879,
             "range": "± 1746",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mvezenov@gmail.com",
+            "name": "Maxim Vezenov",
+            "username": "vezenovm"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "79ef33bd7b9325ea91ec174e53562cb13874c4a1",
+          "message": "fix(acir): Extend slice on dynamic insertion and compilation panic when flattening (#10051)",
+          "timestamp": "2025-10-01T18:37:19Z",
+          "tree_id": "ac06b7bf110f7bb375cb48cac1e0f5f4827c08a3",
+          "url": "https://github.com/noir-lang/noir/commit/79ef33bd7b9325ea91ec174e53562cb13874c4a1"
+        },
+        "date": 1759345373904,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 253018,
+            "range": "± 913",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 224953,
+            "range": "± 525",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 2784715,
+            "range": "± 28462",
             "unit": "ns/iter"
           }
         ]
