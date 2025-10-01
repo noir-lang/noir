b6cd3506c0783f59cf41f572296540d78201ccbd
Ary Borenszweig <asterite@gmail.com>
2025-10-01 12:14:32 -0300

    Snapshots

--- compiler/noirc_evaluator/src/acir/tests/intrinsics.rs
+++ compiler/noirc_evaluator/src/acir/tests/intrinsics.rs
@@ -24,24 +24,23 @@ fn slice_push_back() {
     // Note that w9 is now at the end of memory block 3 and that w1 has been asserted to equal 3
     assert_circuit_snapshot!(program, @r"
     func 0
-    current witness: w10
     private parameters: [w0, w1]
     public parameters: []
     return values: []
-    EXPR w2 = 2
-    EXPR w3 = 3
-    INIT id: 1, len: 2, witnesses: [w2, w3]
-    EXPR w4 = 4
-    MEM id: 1, write: w4 at: w0
-    EXPR w5 = 0
-    MEM id: 1, read at: w5, value: w6
-    EXPR w7 = 1
-    MEM id: 1, read at: w7, value: w8
-    EXPR w9 = 10
-    EXPR w1 = 3
-    INIT id: 3, len: 3, witnesses: [w6, w8, w9]
-    EXPR w10 = 20
-    MEM id: 3, write: w10 at: w0
+    ASSERT w2 = 2
+    ASSERT w3 = 3
+    INIT b1 = [w2, w3]
+    ASSERT w4 = 4
+    WRITE b1[w0] = w4
+    ASSERT w5 = 0
+    READ w6 = b1[w5]
+    ASSERT w7 = 1
+    READ w8 = b1[w7]
+    ASSERT w9 = 10
+    ASSERT w1 = 3
+    INIT b3 = [w6, w8, w9]
+    ASSERT w10 = 20
+    WRITE b3[w0] = w10
     ");
 }
 
@@ -65,24 +64,23 @@ fn slice_push_front() {
     // Note that w9 is now in the front of memory block 3 and that w1 has been asserted to equal 3
     assert_circuit_snapshot!(program, @r"
     func 0
-    current witness: w10
     private parameters: [w0, w1]
     public parameters: []
     return values: []
-    EXPR w2 = 2
-    EXPR w3 = 3
-    INIT id: 1, len: 2, witnesses: [w2, w3]
-    EXPR w4 = 4
-    MEM id: 1, write: w4 at: w0
-    EXPR w5 = 0
-    MEM id: 1, read at: w5, value: w6
-    EXPR w7 = 1
-    MEM id: 1, read at: w7, value: w8
-    EXPR w9 = 10
-    EXPR w1 = 3
-    INIT id: 3, len: 3, witnesses: [w9, w6, w8]
-    EXPR w10 = 20
-    MEM id: 3, write: w10 at: w0
+    ASSERT w2 = 2
+    ASSERT w3 = 3
+    INIT b1 = [w2, w3]
+    ASSERT w4 = 4
+    WRITE b1[w0] = w4
+    ASSERT w5 = 0
+    READ w6 = b1[w5]
+    ASSERT w7 = 1
+    READ w8 = b1[w7]
+    ASSERT w9 = 10
+    ASSERT w1 = 3
+    INIT b3 = [w9, w6, w8]
+    ASSERT w10 = 20
+    WRITE b3[w0] = w10
     ");
 }
 
@@ -108,25 +106,24 @@ fn slice_pop_back() {
     // In practice, when writing to the slice we would assert that the index is less than w1
     assert_circuit_snapshot!(program, @r"
     func 0
-    current witness: w10
     private parameters: [w0, w1]
     public parameters: []
     return values: []
-    EXPR w2 = 2
-    EXPR w3 = 3
-    INIT id: 1, len: 2, witnesses: [w2, w3]
-    EXPR w4 = 4
-    MEM id: 1, write: w4 at: w0
-    EXPR w5 = 1
-    MEM id: 1, read at: w5, value: w6
-    EXPR w7 = 0
-    MEM id: 1, read at: w7, value: w8
-    MEM id: 1, read at: w5, value: w9
-    EXPR w1 = 1
-    EXPR w6 = 3
-    INIT id: 3, len: 2, witnesses: [w8, w9]
-    EXPR w10 = 20
-    MEM id: 3, write: w10 at: w0
+    ASSERT w2 = 2
+    ASSERT w3 = 3
+    INIT b1 = [w2, w3]
+    ASSERT w4 = 4
+    WRITE b1[w0] = w4
+    ASSERT w5 = 1
+    READ w6 = b1[w5]
+    ASSERT w7 = 0
+    READ w8 = b1[w7]
+    READ w9 = b1[w5]
+    ASSERT w1 = 1
+    ASSERT w6 = 3
+    INIT b3 = [w8, w9]
+    ASSERT w10 = 20
+    WRITE b3[w0] = w10
     ");
 }
 
@@ -150,25 +147,24 @@ fn slice_pop_front() {
     // In practice, when writing to the slice we would assert that the index is less than w1
     assert_circuit_snapshot!(program, @r"
     func 0
-    current witness: w10
     private parameters: [w0, w1]
     public parameters: []
     return values: []
-    EXPR w2 = 2
-    EXPR w3 = 3
-    INIT id: 1, len: 2, witnesses: [w2, w3]
-    EXPR w4 = 4
-    MEM id: 1, write: w4 at: w0
-    EXPR w5 = 0
-    MEM id: 1, read at: w5, value: w6
-    EXPR w7 = 1
-    MEM id: 1, read at: w7, value: w8
-    MEM id: 1, read at: w5, value: w9
-    EXPR w9 = 2
-    EXPR w1 = 1
-    INIT id: 3, len: 1, witnesses: [w8]
-    EXPR w10 = 20
-    MEM id: 3, write: w10 at: w0
+    ASSERT w2 = 2
+    ASSERT w3 = 3
+    INIT b1 = [w2, w3]
+    ASSERT w4 = 4
+    WRITE b1[w0] = w4
+    ASSERT w5 = 0
+    READ w6 = b1[w5]
+    ASSERT w7 = 1
+    READ w8 = b1[w7]
+    READ w9 = b1[w5]
+    ASSERT w9 = 2
+    ASSERT w1 = 1
+    INIT b3 = [w8]
+    ASSERT w10 = 20
+    WRITE b3[w0] = w10
     ");
 }
 
@@ -196,66 +192,65 @@ fn slice_insert() {
     // The Brillig calls are to our stdlib quotient directive
     assert_circuit_snapshot!(program, @r"
     func 0
-    current witness: w34
     private parameters: [w0, w1]
     public parameters: []
     return values: []
-    BLACKBOX::RANGE [w1]:32 bits []
-    EXPR w2 = 2
-    EXPR w3 = 3
-    EXPR w4 = 5
-    INIT id: 1, len: 3, witnesses: [w2, w3, w4]
-    EXPR w5 = 4
-    MEM id: 1, write: w5 at: w0
-    EXPR w6 = 0
-    INIT id: 2, len: 3, witnesses: [w6, w6, w6]
-    BRILLIG CALL func 0: inputs: [-w1 + 18446744073709551616, 18446744073709551616], outputs: [w7, w8]
-    BLACKBOX::RANGE [w7]:1 bits []
-    BLACKBOX::RANGE [w8]:64 bits []
-    EXPR w8 = -w1 - 18446744073709551616*w7 + 18446744073709551616
-    BRILLIG CALL func 0: inputs: [-w1 + 18446744073709551615, 18446744073709551616], outputs: [w9, w10]
-    BLACKBOX::RANGE [w9]:1 bits []
-    BLACKBOX::RANGE [w10]:64 bits []
-    EXPR w10 = -w1 - 18446744073709551616*w9 + 18446744073709551615
-    MEM id: 1, read at: w6, value: w11
-    EXPR w12 = w7*w9 - w7 + 1
-    EXPR w13 = -10*w7*w9 + w11*w12 + 10*w7
-    MEM id: 2, write: w13 at: w6
-    BRILLIG CALL func 0: inputs: [-w1 + 18446744073709551617, 18446744073709551616], outputs: [w14, w15]
-    BLACKBOX::RANGE [w14]:1 bits []
-    BLACKBOX::RANGE [w15]:64 bits []
-    EXPR w15 = -w1 - 18446744073709551616*w14 + 18446744073709551617
-    BRILLIG CALL func 0: inputs: [-w1 + 18446744073709551616, 18446744073709551616], outputs: [w16, w17]
-    BLACKBOX::RANGE [w16]:1 bits []
-    BLACKBOX::RANGE [w17]:64 bits []
-    EXPR w17 = -w1 - 18446744073709551616*w16 + 18446744073709551616
-    EXPR w18 = -w14 + 1
-    MEM id: 1, read at: w18, value: w19
-    EXPR w20 = w14*w16 - w14 + 1
-    EXPR w21 = 1
-    EXPR w22 = -10*w14*w16 + w19*w20 + 10*w14
-    MEM id: 2, write: w22 at: w21
-    BRILLIG CALL func 0: inputs: [-w1 + 18446744073709551618, 18446744073709551616], outputs: [w23, w24]
-    BLACKBOX::RANGE [w23]:1 bits []
-    BLACKBOX::RANGE [w24]:64 bits []
-    EXPR w24 = -w1 - 18446744073709551616*w23 + 18446744073709551618
-    BRILLIG CALL func 0: inputs: [-w1 + 18446744073709551617, 18446744073709551616], outputs: [w25, w26]
-    BLACKBOX::RANGE [w25]:1 bits []
-    BLACKBOX::RANGE [w26]:64 bits []
-    EXPR w26 = -w1 - 18446744073709551616*w25 + 18446744073709551617
-    EXPR w27 = -w23 + 2
-    MEM id: 1, read at: w27, value: w28
-    EXPR w29 = w23*w25 - w23 + 1
-    EXPR w30 = -10*w23*w25 + w28*w29 + 10*w23
-    MEM id: 2, write: w30 at: w2
-    EXPR w1 = 4
-    MEM id: 2, read at: w6, value: w31
-    MEM id: 2, read at: w21, value: w32
-    MEM id: 2, read at: w2, value: w33
-    INIT id: 3, len: 3, witnesses: [w31, w32, w33]
-    EXPR w34 = 20
-    MEM id: 3, write: w34 at: w0
-    
+    BLACKBOX::RANGE input: w1, bits: 32
+    ASSERT w2 = 2
+    ASSERT w3 = 3
+    ASSERT w4 = 5
+    INIT b1 = [w2, w3, w4]
+    ASSERT w5 = 4
+    WRITE b1[w0] = w5
+    ASSERT w6 = 0
+    INIT b2 = [w6, w6, w6]
+    BRILLIG CALL func: 0, inputs: [-w1 + 18446744073709551616, 18446744073709551616], outputs: [w7, w8]
+    BLACKBOX::RANGE input: w7, bits: 1
+    BLACKBOX::RANGE input: w8, bits: 64
+    ASSERT w8 = -w1 - 18446744073709551616*w7 + 18446744073709551616
+    BRILLIG CALL func: 0, inputs: [-w1 + 18446744073709551615, 18446744073709551616], outputs: [w9, w10]
+    BLACKBOX::RANGE input: w9, bits: 1
+    BLACKBOX::RANGE input: w10, bits: 64
+    ASSERT w10 = -w1 - 18446744073709551616*w9 + 18446744073709551615
+    READ w11 = b1[w6]
+    ASSERT w12 = w7*w9 - w7 + 1
+    ASSERT w13 = -10*w7*w9 + w11*w12 + 10*w7
+    WRITE b2[w6] = w13
+    BRILLIG CALL func: 0, inputs: [-w1 + 18446744073709551617, 18446744073709551616], outputs: [w14, w15]
+    BLACKBOX::RANGE input: w14, bits: 1
+    BLACKBOX::RANGE input: w15, bits: 64
+    ASSERT w15 = -w1 - 18446744073709551616*w14 + 18446744073709551617
+    BRILLIG CALL func: 0, inputs: [-w1 + 18446744073709551616, 18446744073709551616], outputs: [w16, w17]
+    BLACKBOX::RANGE input: w16, bits: 1
+    BLACKBOX::RANGE input: w17, bits: 64
+    ASSERT w17 = -w1 - 18446744073709551616*w16 + 18446744073709551616
+    ASSERT w18 = -w14 + 1
+    READ w19 = b1[w18]
+    ASSERT w20 = w14*w16 - w14 + 1
+    ASSERT w21 = 1
+    ASSERT w22 = -10*w14*w16 + w19*w20 + 10*w14
+    WRITE b2[w21] = w22
+    BRILLIG CALL func: 0, inputs: [-w1 + 18446744073709551618, 18446744073709551616], outputs: [w23, w24]
+    BLACKBOX::RANGE input: w23, bits: 1
+    BLACKBOX::RANGE input: w24, bits: 64
+    ASSERT w24 = -w1 - 18446744073709551616*w23 + 18446744073709551618
+    BRILLIG CALL func: 0, inputs: [-w1 + 18446744073709551617, 18446744073709551616], outputs: [w25, w26]
+    BLACKBOX::RANGE input: w25, bits: 1
+    BLACKBOX::RANGE input: w26, bits: 64
+    ASSERT w26 = -w1 - 18446744073709551616*w25 + 18446744073709551617
+    ASSERT w27 = -w23 + 2
+    READ w28 = b1[w27]
+    ASSERT w29 = w23*w25 - w23 + 1
+    ASSERT w30 = -10*w23*w25 + w28*w29 + 10*w23
+    WRITE b2[w2] = w30
+    ASSERT w1 = 4
+    READ w31 = b2[w6]
+    READ w32 = b2[w21]
+    READ w33 = b2[w2]
+    INIT b3 = [w31, w32, w33]
+    ASSERT w34 = 20
+    WRITE b3[w0] = w34
+
     unconstrained func 0
     0: @10 = const u32 2
     1: @11 = const u32 0
@@ -292,46 +287,45 @@ fn slice_remove() {
     // The Brillig calls are to our stdlib quotient directive
     assert_circuit_snapshot!(program, @r"
     func 0
-    current witness: w24
     private parameters: [w0, w1, w2]
     public parameters: []
     return values: []
-    EXPR w3 = 2
-    EXPR w4 = 3
-    EXPR w5 = 5
-    INIT id: 1, len: 3, witnesses: [w3, w4, w5]
-    EXPR w6 = 4
-    MEM id: 1, write: w6 at: w0
-    EXPR w7 = 0
-    MEM id: 1, read at: w7, value: w8
-    EXPR w9 = 1
-    MEM id: 1, read at: w9, value: w10
-    MEM id: 1, read at: w3, value: w11
-    MEM id: 1, read at: w1, value: w12
-    INIT id: 2, len: 3, witnesses: [w8, w10, w11]
-    MEM id: 1, read at: w9, value: w13
-    BRILLIG CALL func 0: inputs: [-w1 + 18446744073709551616, 18446744073709551616], outputs: [w14, w15]
-    BLACKBOX::RANGE [w14]:1 bits []
-    BLACKBOX::RANGE [w15]:64 bits []
-    EXPR w15 = -w1 - 18446744073709551616*w14 + 18446744073709551616
-    EXPR w16 = w13*w14 - w8*w14 + w8
-    MEM id: 2, write: w16 at: w7
-    MEM id: 1, read at: w3, value: w17
-    BRILLIG CALL func 0: inputs: [-w1 + 18446744073709551617, 18446744073709551616], outputs: [w18, w19]
-    BLACKBOX::RANGE [w18]:1 bits []
-    BLACKBOX::RANGE [w19]:64 bits []
-    EXPR w19 = -w1 - 18446744073709551616*w18 + 18446744073709551617
-    EXPR w20 = w17*w18 - w10*w18 + w10
-    MEM id: 2, write: w20 at: w9
-    EXPR w1 = 2
-    EXPR w12 = w2
-    MEM id: 2, read at: w7, value: w21
-    MEM id: 2, read at: w9, value: w22
-    MEM id: 2, read at: w3, value: w23
-    INIT id: 3, len: 3, witnesses: [w21, w22, w23]
-    EXPR w24 = 20
-    MEM id: 3, write: w24 at: w0
-    
+    ASSERT w3 = 2
+    ASSERT w4 = 3
+    ASSERT w5 = 5
+    INIT b1 = [w3, w4, w5]
+    ASSERT w6 = 4
+    WRITE b1[w0] = w6
+    ASSERT w7 = 0
+    READ w8 = b1[w7]
+    ASSERT w9 = 1
+    READ w10 = b1[w9]
+    READ w11 = b1[w3]
+    READ w12 = b1[w1]
+    INIT b2 = [w8, w10, w11]
+    READ w13 = b1[w9]
+    BRILLIG CALL func: 0, inputs: [-w1 + 18446744073709551616, 18446744073709551616], outputs: [w14, w15]
+    BLACKBOX::RANGE input: w14, bits: 1
+    BLACKBOX::RANGE input: w15, bits: 64
+    ASSERT w15 = -w1 - 18446744073709551616*w14 + 18446744073709551616
+    ASSERT w16 = w13*w14 - w8*w14 + w8
+    WRITE b2[w7] = w16
+    READ w17 = b1[w3]
+    BRILLIG CALL func: 0, inputs: [-w1 + 18446744073709551617, 18446744073709551616], outputs: [w18, w19]
+    BLACKBOX::RANGE input: w18, bits: 1
+    BLACKBOX::RANGE input: w19, bits: 64
+    ASSERT w19 = -w1 - 18446744073709551616*w18 + 18446744073709551617
+    ASSERT w20 = w17*w18 - w10*w18 + w10
+    WRITE b2[w9] = w20
+    ASSERT w1 = 2
+    ASSERT w12 = w2
+    READ w21 = b2[w7]
+    READ w22 = b2[w9]
+    READ w23 = b2[w3]
+    INIT b3 = [w21, w22, w23]
+    ASSERT w24 = 20
+    WRITE b3[w0] = w24
+
     unconstrained func 0
     0: @10 = const u32 2
     1: @11 = const u32 0
