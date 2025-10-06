d8c7570f0f5b2fa28c3120c4e5dec8238d7ca89f
Tom French <15848336+TomAFrench@users.noreply.github.com>
2025-10-06 14:52:45 +0000

    .

--- compiler/noirc_evaluator/src/ssa/mod.rs
+++ compiler/noirc_evaluator/src/ssa/mod.rs
@@ -235,7 +235,7 @@ pub fn primary_passes(options: &SsaEvaluatorOptions) -> Vec<SsaPass<'_>> {
         SsaPass::new(Ssa::remove_truncate_after_range_check, "Removing Truncate after RangeCheck"),
         SsaPass::new(Ssa::checked_to_unchecked, "Checked to unchecked"),
         SsaPass::new(
-            Ssa::fold_constants_using_constraints(options.constant_folding_max_iter),
+            |ssa| ssa.fold_constants_using_constraints(options.constant_folding_max_iter),
             "Constant Folding using constraints",
         ),
         SsaPass::new(
