d08265b45d2910d064a0f7118842e38f90823036
Maxim Vezenov <mvezenov@gmail.com>
2025-10-08 17:26:15 +0000

    clippy

--- compiler/noirc_evaluator/src/brillig/brillig_ir/artifact.rs
+++ compiler/noirc_evaluator/src/brillig/brillig_ir/artifact.rs
@@ -335,9 +335,4 @@ impl<F: Clone + std::fmt::Debug> BrilligArtifact<F> {
     pub(crate) fn set_call_stack(&mut self, call_stack: CallStackId) {
         self.call_stack_id = call_stack;
     }
-
-    #[cfg(test)]
-    pub(crate) fn take_labels(&mut self) -> HashMap<Label, usize> {
-        std::mem::take(&mut self.labels)
-    }
 }
