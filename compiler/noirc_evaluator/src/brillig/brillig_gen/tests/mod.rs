b5a5ebb1e0af9bb67d8f3a1e6721895c6bafaedc
Maxim Vezenov <mvezenov@gmail.com>
2025-10-10 15:08:48 -0400

    chore(brillig_gen): Brillig artifact display for unit testing  (#10165)

--- compiler/noirc_evaluator/src/brillig/brillig_gen/tests/mod.rs
+++ compiler/noirc_evaluator/src/brillig/brillig_gen/tests/mod.rs
@@ -1,13 +1,31 @@
-// The tests in the following files are simple tests checking the result of generating brillig bytecode
-// from a simple brillig function in SSA form. The brillig function is doing one elementary operation to
-// show how it is generated on the Brillig bytecode level.
-// Brillig-gen is adding overhead to the elementary operation for hanling the function call.
-// See the first test in binary_test.rs for a breakdown of the overhead.
-#[cfg(test)]
-mod binary_test;
-#[cfg(test)]
-mod black_box_test;
-#[cfg(test)]
-mod call_test;
-#[cfg(test)]
-mod memory_test;
+//! The tests in the following files are simple tests checking the result of generating brillig bytecode
+//! from a simple brillig function in SSA form. The brillig function is doing one elementary operation to
+//! show how it is generated on the Brillig bytecode level.
+//!
+//! Every unit test will be checking against against an individual Brillig artifact
+//! that has not yet undergone linking. This means any calls, to both external functions or procedures,
+//! and jumps are expected to be unresolved. Thus any call/jump is expected to still have a label of `0`.
+
+use crate::{
+    brillig::{Brillig, BrilligOptions},
+    ssa::ssa_gen::Ssa,
+};
+
+mod binary;
+mod black_box;
+mod call;
+mod memory;
+
+fn ssa_to_brillig_artifacts(src: &str) -> Brillig {
+    let ssa = Ssa::from_str(src).unwrap();
+    ssa.to_brillig(&BrilligOptions::default())
+}
+
+#[macro_export]
+macro_rules! assert_artifact_snapshot {
+    ($artifact:expr, $($arg:tt)*) => {
+        #[allow(unused_mut)]
+        let artifact_string = $artifact.to_string();
+        insta::assert_snapshot!(artifact_string, $($arg)*)
+    };
+}
