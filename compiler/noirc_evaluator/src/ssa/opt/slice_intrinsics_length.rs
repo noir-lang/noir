bcbcad41c01ced3da610a11520bbb8e5d881afdb
Ary Borenszweig <asterite@gmail.com>
2025-10-27 14:15:49 -0300

    Optimize slice_pop_back length

--- compiler/noirc_evaluator/src/ssa/opt/slice_intrinsics_length.rs
+++ compiler/noirc_evaluator/src/ssa/opt/slice_intrinsics_length.rs
@@ -84,10 +84,13 @@ impl Function {
                 } else {
                     None
                 }
-            } else if slice_remove.is_some_and(|op| target_func == &op) {
+            } else if slice_remove.is_some_and(|op| target_func == &op)
+                || slice_pop_back.is_some_and(|op| target_func == &op)
+            {
                 if let Some(length) = context.dfg.get_numeric_constant(arguments[0]) {
                     if !length.is_zero() {
-                        // For `slice_remove(length, ...)` we can replace the resulting length with length - 1
+                        // For `slice_remove(length, ...)` we can replace the resulting length with length - 1.
+                        // Same goes for `slice_pop_back`.
                         let length = length - FieldElement::one();
                         let new_slice_length = context.dfg.instruction_results(instruction_id)[0];
                         Some((new_slice_length, length))
@@ -191,7 +194,7 @@ mod test {
         acir(inline) fn main f0 {
           b0():
             v0 = make_array [Field 2, Field 3] : [Field]
-            v1, v2 = call slice_remove(u32 2, v0, u32 1) -> (u32, [Field])
+            v1, v2, v3 = call slice_remove(u32 2, v0, u32 1) -> (u32, [Field], Field)
             return v1
         }
         ";
@@ -203,7 +206,7 @@ mod test {
         acir(inline) fn main f0 {
           b0():
             v2 = make_array [Field 2, Field 3] : [Field]
-            v6, v7 = call slice_remove(u32 2, v2, u32 1) -> (u32, [Field])
+            v6, v7, v8 = call slice_remove(u32 2, v2, u32 1) -> (u32, [Field], Field)
             return u32 1
         }
         ");
@@ -253,6 +256,30 @@ mod test {
             v2 = make_array [Field 2, Field 3] : [Field]
             v6, v7 = call slice_push_back(u32 2, v2, Field 4) -> (u32, [Field])
             return u32 3
+        }
+        ");
+    }
+
+    #[test]
+    fn slice_pop_back_optimization() {
+        let src = "
+        acir(inline) fn main f0 {
+          b0():
+            v0 = make_array [Field 2, Field 3] : [Field]
+            v1, v2, v3 = call slice_pop_back(u32 2, v0) -> (u32, [Field], Field)
+            return v1
+        }
+        ";
+        let ssa = Ssa::from_str(src).unwrap();
+
+        // Here `v1` was replaced with 1 because we know the new length is 2 - 1
+        let ssa = ssa.slice_instrinsics_length_optimization();
+        assert_ssa_snapshot!(ssa, @r"
+        acir(inline) fn main f0 {
+          b0():
+            v2 = make_array [Field 2, Field 3] : [Field]
+            v5, v6, v7 = call slice_pop_back(u32 2, v2) -> (u32, [Field], Field)
+            return u32 1
         }
         ");
     }
