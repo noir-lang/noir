ee748dca34b1c6412b7cb5a2b9e49faef2a08f3f
Maxim Vezenov <mvezenov@gmail.com>
2025-10-29 19:45:05 +0000

    unit tests for item generation

--- compiler/noirc_frontend/src/tests/metaprogramming.rs
+++ compiler/noirc_frontend/src/tests/metaprogramming.rs
@@ -1,9 +1,13 @@
-use crate::hir::{
-    comptime::ComptimeError,
-    def_collector::{
-        dc_crate::CompilationError,
-        errors::{DefCollectorErrorKind, DuplicateType},
+use crate::{
+    elaborator::UnstableFeature,
+    hir::{
+        comptime::ComptimeError,
+        def_collector::{
+            dc_crate::CompilationError,
+            errors::{DefCollectorErrorKind, DuplicateType},
+        },
     },
+    tests::check_errors_using_features,
 };
 
 use crate::tests::{
@@ -181,6 +185,117 @@ fn generate_function_with_macros() {
         y + 2_i32
     }
     ");
+}
+
+#[test]
+fn generate_function_with_macros_on_trait() {
+    let src = "
+    #[foo]
+    trait MyTrait {}
+
+    comptime fn foo(_f: TraitDefinition) -> Quoted {
+        quote {
+            pub fn bar(x: i32) -> i32  {  
+                let y = x + 1;
+                y + 2
+            }
+        }
+    }
+
+    struct Foo {}
+    impl MyTrait for Foo {}
+    fn main() {
+        let _ = Foo {};
+    }
+    ";
+
+    let expanded = assert_no_errors_and_to_string(src);
+    insta::assert_snapshot!(expanded, @r"
+    trait MyTrait {
+    
+    }
+    
+    comptime fn foo(_f: TraitDefinition) -> Quoted {
+        quote {
+            pub fn bar(x: i32) -> i32 {
+                let y = x + 1;
+                y + 2
+            }
+        }
+    }
+    
+    pub fn bar(x: i32) -> i32 {
+        let y: i32 = x + 1_i32;
+        y + 2_i32
+    }
+    
+    struct Foo {
+    }
+    
+    impl MyTrait for Foo {
+    
+    }
+    
+    fn main() {
+        let _: Foo = Foo { };
+    }
+    ");
+}
+
+#[test]
+fn do_not_generate_function_with_macros_on_trait_impl() {
+    let src = "
+    trait MyTrait {}
+
+    struct Foo {}
+
+    #[foo]
+    impl MyTrait for Foo {}
+
+    comptime fn foo(_f: TraitImpl) -> Quoted {
+                ^^^ unused function foo
+                ~~~ unused function
+        quote {
+            pub fn bar(x: i32) -> i32  {  
+                let y = x + 1;
+                y + 2
+            }
+        }
+    }
+
+    fn main() {
+        let _ = Foo {};
+    }
+    ";
+    check_errors(src);
+}
+
+/// Enum attributes are not run at compile-time.
+#[test]
+fn do_not_generate_function_with_macros_on_enum() {
+    let src = "
+    #[foo]
+    enum MyEnum {
+        Foo(u32),
+    }
+
+    comptime fn foo(_f: TypeDefinition) -> Quoted {
+                ^^^ unused function foo
+                ~~~ unused function
+        quote {
+            pub fn bar(x: i32) -> i32  {  
+                let y = x + 1;
+                y + 2
+            }
+        }
+    }
+
+    fn main() {
+        let _ = MyEnum::Foo;
+    }
+    ";
+    let features = vec![UnstableFeature::Enums];
+    check_errors_using_features(src, &features);
 }
 
 #[test]
@@ -382,6 +497,102 @@ fn error_if_attribute_not_in_scope() {
         #[not_in_scope]
         ^^^^^^^^^^^^^^^ Attribute function `not_in_scope` is not in scope
         fn main() {}
+    "#;
+    check_errors(src);
+}
+
+#[test]
+fn cannot_generate_module_declarations() {
+    let src = r#"
+        #[bad_attr]
+        ~~~~~~~~~~~ While running this function attribute
+        fn main() {}
+        
+        comptime fn bad_attr(_: FunctionDefinition) -> Quoted {
+            quote { mod new_module; }
+                    ^^^^^^^^^^^^^^^ Unsupported statement type to unquote
+                    ~~~~~~~~~~~~~~~ Only functions, structs, globals, and impls can be unquoted here
+        }
+    "#;
+    check_errors(src);
+}
+
+#[test]
+fn cannot_generate_imports() {
+    let src = r#"
+        #[bad_attr]
+        ~~~~~~~~~~~ While running this function attribute
+        fn main() {}
+
+        comptime fn bad_attr(_: FunctionDefinition) -> Quoted {
+            quote { use std::hash; }
+                    ^^^^^^^^^^^^^^ Unsupported statement type to unquote
+                    ~~~~~~~~~~~~~~ Only functions, structs, globals, and impls can be unquoted here
+        }
+    "#;
+    check_errors(src);
+}
+
+#[test]
+fn cannot_generate_traits() {
+    let src = r#"
+        #[bad_attr]
+        ~~~~~~~~~~~ While running this function attribute
+        fn main() {}
+
+        comptime fn bad_attr(_: FunctionDefinition) -> Quoted {
+            quote { trait MyTrait {} }
+                    ^^^^^^^^^^^^^^^^ Unsupported statement type to unquote
+                    ~~~~~~~~~~~~~~~~ Only functions, structs, globals, and impls can be unquoted here
+        }
+    "#;
+    check_errors(src);
+}
+
+#[test]
+fn cannot_generate_type_aliases() {
+    let src = r#"
+        #[bad_attr]
+        ~~~~~~~~~~~ While running this function attribute
+        fn main() {}
+
+        comptime fn bad_attr(_: FunctionDefinition) -> Quoted {
+            quote { type MyType = Field; }
+                    ^^^^^^^^^^^^^^^^^^^^ Unsupported statement type to unquote
+                    ~~~~~~~~~~~~~~~~~~~~ Only functions, structs, globals, and impls can be unquoted here
+        }
+    "#;
+    check_errors(src);
+}
+
+#[test]
+fn cannot_generate_submodules() {
+    let src = r#"
+        #[bad_attr]
+        ~~~~~~~~~~~ While running this function attribute
+        fn main() {}
+
+        comptime fn bad_attr(_: FunctionDefinition) -> Quoted {
+            quote { mod inner { fn foo() {} } }
+                    ^^^^^^^^^^^^^^^^^^^^^^^^^ Unsupported statement type to unquote
+                    ~~~~~~~~~~~~~~~~~~~~~~~~~ Only functions, structs, globals, and impls can be unquoted here
+        }
+    "#;
+    check_errors(src);
+}
+
+#[test]
+fn cannot_generate_inner_attributes() {
+    let src = r#"
+        #[bad_attr]
+        ~~~~~~~~~~~ While running this function attribute
+        fn main() {}
+
+        comptime fn bad_attr(_: FunctionDefinition) -> Quoted {
+            quote { #![inner_attr] }
+                    ^^^^^^^^^^^^^^ Unsupported statement type to unquote
+                    ~~~~~~~~~~~~~~ Only functions, structs, globals, and impls can be unquoted here
+        }
     "#;
     check_errors(src);
 }
