a9c7459095122ce7c9d9f723a805bb728bac3513
github-action-benchmark <github@users.noreply.github.com>
2025-10-30 15:11:36 +0000

    add ACVM Benchmarks (cargo) benchmark result for 9808525b4f420397ea30ba91293ffb6539668e3f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761769830797,
+  "lastUpdate": 1761837095882,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -23076,48 +23076,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "4ba5c86fcaee4675c75a2801e3df9c835cadc364",
-          "message": "chore(ACIR): no need to return types in `flatten` (#10210)",
-          "timestamp": "2025-10-16T14:22:40Z",
-          "tree_id": "36e32f58f879203d35e2b66f24ffffc7b170e17a",
-          "url": "https://github.com/noir-lang/noir/commit/4ba5c86fcaee4675c75a2801e3df9c835cadc364"
-        },
-        "date": 1760626017807,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 262858,
-            "range": "± 605",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 229968,
-            "range": "± 5634",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 2785376,
-            "range": "± 7846",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "asterite@gmail.com",
-            "name": "Ary Borenszweig",
-            "username": "asterite"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "9d70064a07a388dd62fcb513ca5f262029889bfe",
           "message": "chore(ACIR): display/parse memory arrays as b0, b1, etc. (#10211)",
           "timestamp": "2025-10-16T16:20:13Z",
@@ -25159,6 +25117,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 2795128,
             "range": "± 7751",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jfecher11@gmail.com",
+            "name": "jfecher",
+            "username": "jfecher"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9808525b4f420397ea30ba91293ffb6539668e3f",
+          "message": "chore: Fix typo in defunctionalize docs (#10321)",
+          "timestamp": "2025-10-30T14:39:52Z",
+          "tree_id": "75b50b8aa7da14c3d61a30d8391319757a58ada0",
+          "url": "https://github.com/noir-lang/noir/commit/9808525b4f420397ea30ba91293ffb6539668e3f"
+        },
+        "date": 1761837068763,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 263360,
+            "range": "± 2266",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 232649,
+            "range": "± 5970",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 2797384,
+            "range": "± 16695",
             "unit": "ns/iter"
           }
         ]
