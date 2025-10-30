d813c425dc99f887ab69eff0f4cf3f1f62cea82e
guipublic <guipublic@gmail.com>
2025-10-30 12:56:02 +0100

    refactor codegen_control_flow

--- compiler/noirc_evaluator/src/brillig/brillig_ir/procedures/revert_with_string.rs
+++ compiler/noirc_evaluator/src/brillig/brillig_ir/procedures/revert_with_string.rs
@@ -1,10 +1,13 @@
-use acvm::AcirField;
+use acvm::{AcirField, acir::brillig::HeapVector};
 
 use super::ProcedureId;
-use crate::brillig::brillig_ir::{
-    BrilligContext,
-    debug_show::DebugToString,
-    registers::{RegisterAllocator, ScratchSpace},
+use crate::{
+    brillig::brillig_ir::{
+        BrilligContext, ReservedRegisters,
+        debug_show::DebugToString,
+        registers::{RegisterAllocator, ScratchSpace},
+    },
+    ssa::ir::instruction::ErrorType,
 };
 
 impl<F: AcirField + DebugToString, Registers: RegisterAllocator> BrilligContext<F, Registers> {
@@ -19,5 +22,19 @@ pub(super) fn compile_revert_with_string_procedure<F: AcirField + DebugToString>
     brillig_context: &mut BrilligContext<F, ScratchSpace>,
     revert_string: String,
 ) {
-    brillig_context.revert_with_string(revert_string);
+    // Compute the error selector and register the error type
+    let error_type = ErrorType::String(revert_string);
+    let error_selector = error_type.selector();
+    brillig_context.obj.error_types.insert(error_selector, error_type);
+
+    // Write the selector to the free memory pointer and trap with it as revert data
+    brillig_context.indirect_const_instruction(
+        ReservedRegisters::free_memory_pointer(),
+        64,
+        u128::from(error_selector.as_u64()).into(),
+    );
+    brillig_context.trap_instruction(HeapVector {
+        pointer: ReservedRegisters::free_memory_pointer(),
+        size: ReservedRegisters::usize_one(),
+    });
 }
