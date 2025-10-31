49c9b6a543c492490df9ebfaa481feccab80118f
Ary Borenszweig <asterite@gmail.com>
2025-10-31 10:44:57 -0300

    Extract trait_impls

--- tooling/nargo_doc/src/html.rs
+++ tooling/nargo_doc/src/html.rs
@@ -11,6 +11,7 @@ use crate::{
         has_path::HasPath,
         id_to_path::compute_id_to_path,
         markdown_utils::{fix_markdown, markdown_summary},
+        trait_impls::gather_all_trait_impls,
     },
     items::{
         Crate, Function, Generic, Global, HasNameAndComments, Impl, Item, Module, Struct,
@@ -23,6 +24,7 @@ mod has_class;
 mod has_path;
 mod id_to_path;
 mod markdown_utils;
+mod trait_impls;
 
 /// Returns a list of (path, contents) representing the HTML files for the given crates.
 /// The paths are relative paths that can be joined to a base directory.
@@ -1135,39 +1137,6 @@ fn type_to_string(typ: &Type) -> String {
             format!("{}{}{}", type_to_string(lhs), operator, type_to_string(rhs))
         }
         Type::TraitAsType { trait_name, .. } => format!("impl-{trait_name}"),
-    }
-}
-
-fn gather_all_trait_impls(workspace: &Workspace) -> HashMap<TypeId, HashSet<TraitImpl>> {
-    let mut trait_impls = HashMap::new();
-
-    for krate in &workspace.crates {
-        for item in &krate.root_module.items {
-            gather_trait_impls_in_item(item, &mut trait_impls);
-        }
-    }
-
-    trait_impls
-}
-
-fn gather_trait_impls_in_item(item: &Item, trait_impls: &mut HashMap<TypeId, HashSet<TraitImpl>>) {
-    match item {
-        Item::Module(module) => {
-            for item in &module.items {
-                gather_trait_impls_in_item(item, trait_impls);
-            }
-        }
-        Item::Struct(struct_) => {
-            for impl_ in &struct_.trait_impls {
-                trait_impls.entry(impl_.trait_id).or_default().insert(impl_.clone());
-            }
-        }
-        Item::Trait(trait_) => {
-            for impl_ in &trait_.trait_impls {
-                trait_impls.entry(impl_.trait_id).or_default().insert(impl_.clone());
-            }
-        }
-        Item::TypeAlias(_) | Item::Function(_) | Item::Global(_) => {}
     }
 }
 
---
+++ tooling/nargo_doc/src/html/trait_impls.rs
@@ -0,0 +1,36 @@
+use std::collections::{HashMap, HashSet};
+
+use crate::items::{Item, TraitImpl, TypeId, Workspace};
+
+pub(super) fn gather_all_trait_impls(workspace: &Workspace) -> HashMap<TypeId, HashSet<TraitImpl>> {
+    let mut trait_impls = HashMap::new();
+
+    for krate in &workspace.crates {
+        for item in &krate.root_module.items {
+            gather_trait_impls_in_item(item, &mut trait_impls);
+        }
+    }
+
+    trait_impls
+}
+
+fn gather_trait_impls_in_item(item: &Item, trait_impls: &mut HashMap<TypeId, HashSet<TraitImpl>>) {
+    match item {
+        Item::Module(module) => {
+            for item in &module.items {
+                gather_trait_impls_in_item(item, trait_impls);
+            }
+        }
+        Item::Struct(struct_) => {
+            for impl_ in &struct_.trait_impls {
+                trait_impls.entry(impl_.trait_id).or_default().insert(impl_.clone());
+            }
+        }
+        Item::Trait(trait_) => {
+            for impl_ in &trait_.trait_impls {
+                trait_impls.entry(impl_.trait_id).or_default().insert(impl_.clone());
+            }
+        }
+        Item::TypeAlias(_) | Item::Function(_) | Item::Global(_) => {}
+    }
+}
