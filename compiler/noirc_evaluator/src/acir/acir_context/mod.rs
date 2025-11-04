8ae82489e3d7e30c1e1577d572b9d60dce1c1d00
Tom French <15848336+TomAFrench@users.noreply.github.com>
2025-11-04 11:40:03 +0000

    chore: reject initializing zero-length memory

--- compiler/noirc_evaluator/src/acir/acir_context/mod.rs
+++ compiler/noirc_evaluator/src/acir/acir_context/mod.rs
@@ -1389,11 +1389,7 @@ impl<F: AcirField> AcirContext<F> {
 
     /// Insert the MemoryInit for the Return Data array, using the provided witnesses
     pub(crate) fn initialize_return_data(&mut self, block_id: BlockId, init: Vec<Witness>) {
-        self.acir_ir.push_opcode(Opcode::MemoryInit {
-            block_id,
-            init,
-            block_type: BlockType::ReturnData,
-        });
+        self.acir_ir.initialize_memory(block_id, init, BlockType::ReturnData);
     }
 
     /// Initializes an array in memory with the given values `optional_values`.
@@ -1414,15 +1410,16 @@ impl<F: AcirField> AcirContext<F> {
             Some(value) => {
                 let mut values = Vec::new();
                 self.initialize_array_inner(&mut values, value)?;
+                debug_assert_eq!(
+                    values.len(),
+                    len,
+                    "Initialized array length does not match the expected length"
+                );
                 values
             }
         };
 
-        self.acir_ir.push_opcode(Opcode::MemoryInit {
-            block_id,
-            init: initialized_values,
-            block_type: databus,
-        });
+        self.acir_ir.initialize_memory(block_id, initialized_values, databus);
 
         Ok(())
     }
