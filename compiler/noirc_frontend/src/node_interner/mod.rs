2f0b65d4f8444449a3b77389917301f243ce73b2
Akosh Farkash <aakoshh@gmail.com>
2025-11-05 14:29:15 +0000

    Move into separate pusher module.

--- compiler/noirc_frontend/src/node_interner/mod.rs
+++ compiler/noirc_frontend/src/node_interner/mod.rs
@@ -47,6 +47,8 @@ mod methods;
 mod reexports;
 mod trait_impl;
 
+pub mod pusher;
+
 pub use dependency::DependencyId;
 use globals::GlobalInfo;
 pub use globals::{GlobalId, GlobalValue};
@@ -1491,112 +1493,5 @@ fn get_type_method_key(typ: &Type) -> Option<TypeMethodKey> {
         | Type::InfixExpr(..)
         | Type::CheckedCast { .. }
         | Type::TraitAsType(..) => None,
-    }
-}
-
-pub mod pusher {
-    use std::{marker::PhantomData, ops::Deref};
-
-    use noirc_errors::Location;
-
-    use crate::{
-        Type,
-        node_interner::{ExprId, NodeInterner, StmtId},
-    };
-
-    pub struct HasNothing;
-    pub struct HasLocation;
-
-    pub struct PushedExpr<S = HasNothing> {
-        id: ExprId,
-        has_location: bool,
-        has_type: bool,
-        status: PhantomData<S>,
-    }
-
-    impl PushedExpr<HasNothing> {
-        /// Create a new pusher that will need the location and the type pushed.
-        pub fn new(id: ExprId) -> Self {
-            Self { id, has_location: false, has_type: false, status: PhantomData }
-        }
-
-        /// Push the location first, then the type.
-        pub fn push_location(
-            self,
-            interner: &mut NodeInterner,
-            location: Location,
-        ) -> PushedExpr<HasLocation> {
-            let id = self.id;
-            interner.push_expr_location(id, location);
-            std::mem::forget(self);
-            PushedExpr { id, has_location: true, has_type: false, status: PhantomData }
-        }
-    }
-
-    impl PushedExpr<HasLocation> {
-        /// Push the type after the location, returning the ID as there are no more missing pieces.
-        pub fn push_type(mut self, interner: &mut NodeInterner, typ: Type) -> ExprId {
-            interner.push_expr_type(self.id, typ);
-            self.has_type = true;
-            self.id
-        }
-    }
-
-    /// Give convenient access to the ID in case it is needed to make the next piece available.
-    impl<S> Deref for PushedExpr<S> {
-        type Target = ExprId;
-
-        fn deref(&self) -> &Self::Target {
-            &self.id
-        }
-    }
-
-    /// Panic if we dropped the pusher without having pushed both location and type.
-    impl<S> Drop for PushedExpr<S> {
-        fn drop(&mut self) {
-            if !self.has_location {
-                panic!("location hasn't been pushed for {:?}", self.id);
-            }
-            if !self.has_type {
-                panic!("type hasn't been pushed for {:?}", self.id);
-            }
-        }
-    }
-
-    pub struct PushedStmt<S = HasNothing> {
-        id: StmtId,
-        has_location: bool,
-        status: PhantomData<S>,
-    }
-
-    impl PushedStmt<HasNothing> {
-        /// Create a new pusher that will need the location pushed.
-        pub fn new(id: StmtId) -> Self {
-            Self { id, has_location: false, status: PhantomData }
-        }
-
-        /// Push the location first, returning the ID as there are no other missing pieces.
-        pub fn push_location(mut self, interner: &mut NodeInterner, location: Location) -> StmtId {
-            interner.push_stmt_location(self.id, location);
-            self.has_location = true;
-            self.id
-        }
-    }
-
-    impl<S> Deref for PushedStmt<S> {
-        type Target = StmtId;
-
-        fn deref(&self) -> &Self::Target {
-            &self.id
-        }
-    }
-
-    /// Panic if we dropped the pusher without having pushed the location.
-    impl<S> Drop for PushedStmt<S> {
-        fn drop(&mut self) {
-            if !self.has_location {
-                panic!("location hasn't been pushed for {:?}", self.id);
-            }
-        }
     }
 }
