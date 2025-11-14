7f0d491bcc3c77145aabe4d71641561caf534f34
Maxim Vezenov <mvezenov@gmail.com>
2025-11-14 14:36:34 -0300

    a few more comments

--- compiler/noirc_evaluator/src/acir/tests/arrays.rs
+++ compiler/noirc_evaluator/src/acir/tests/arrays.rs
@@ -360,6 +360,10 @@ fn non_homogenous_array_dynamic_access() {
   "#;
 
   let program = ssa_to_acir_program(src);
+
+  // b0 is our actual array input while b1 is our element type sizes array.
+  // You can see that in `w44 = b1[w28]` we use the supplied witness index to read the flattened index from b1.
+  // `w44` is then used to read from the b0 array.
   assert_circuit_snapshot!(program, @r"
   func 0
   private parameters: [w0, w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13, w14, w15, w16, w17, w18, w19, w20, w21, w22, w23, w24, w25, w26, w27, w28]
