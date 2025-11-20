691028ac178bc06f9d4ec2e42cb5e2f03a159b6d
Ary Borenszweig <asterite@gmail.com>
2025-11-20 10:16:46 -0300

    feat(doc): colorize code blocks

--- tooling/nargo_doc/src/html.rs
+++ tooling/nargo_doc/src/html.rs
@@ -8,6 +8,7 @@ use noirc_frontend::ast::ItemVisibility;
 use crate::{
     html::{
         all_items::AllItems,
+        colorize::colorize_code_blocks,
         has_class::HasClass,
         has_uri::HasUri,
         id_to_info::{ItemInfo, compute_id_to_info},
@@ -23,6 +24,7 @@ use crate::{
 };
 
 mod all_items;
+mod colorize;
 mod has_class;
 mod has_uri;
 mod id_to_info;
@@ -374,10 +376,11 @@ impl HTMLCreator {
                 self.output.push_str("<div class=\"item-description\">");
                 if let Some((comments, links)) = item.comments() {
                     let comments = self.process_comments_links(links, comments.clone());
+                    let comments = colorize_code_blocks(comments);
 
                     let summary = markdown_summary(&comments);
 
-                    let markdown = markdown::to_html(&summary);
+                    let markdown = markdown_utils::to_html(&summary);
                     let markdown = markdown.trim_start_matches("<p>");
                     let summary = markdown.trim().trim_end_matches("</p>").trim();
 
@@ -1385,8 +1388,9 @@ impl HTMLCreator {
 
         let comments = fix_markdown(comments, current_heading_level);
         let comments = self.process_comments_links(links, comments);
+        let comments = colorize_code_blocks(comments);
 
-        let html = markdown::to_html(&comments);
+        let html = markdown_utils::to_html(&comments);
         self.output.push_str("<div class=\"comments\">\n");
         self.output.push_str(&html);
         self.output.push_str("</div>\n");
@@ -1905,6 +1909,6 @@ fn is_self_param(param: &FunctionParam, self_type: Option<&Type>) -> bool {
     }
 }
 
-fn escape_html(input: &str) -> String {
+pub(super) fn escape_html(input: &str) -> String {
     input.replace('<', "&lt;").replace('>', "&gt;")
 }
---
+++ tooling/nargo_doc/src/html/colorize.rs
@@ -0,0 +1,170 @@
+use noirc_frontend::{
+    lexer::Lexer,
+    token::{FmtStrFragment, LocatedToken, Token},
+};
+
+use crate::html::escape_html;
+
+pub(super) fn colorize_code_blocks(comments: String) -> String {
+    if !comments.contains("```") {
+        return comments;
+    }
+
+    let mut in_code_block = false; // Are we inside a code block?
+    let mut highlighting = false; // Do we need to highlight code?
+    let mut result = String::new();
+
+    let lines = comments.lines().collect::<Vec<_>>();
+    for (index, line) in lines.iter().enumerate() {
+        let trimmed_line = line.trim();
+        if trimmed_line.starts_with("```") {
+            in_code_block = !in_code_block;
+        }
+
+        // We assume code blocks without a name are for Noir.
+        // We also colorize Rust because the two langauges are very similar, and because some
+        // of our stdlib docs use Rust code blocks.
+        if in_code_block
+            && (trimmed_line == "```" || trimmed_line == "```noir" || trimmed_line == "```rust")
+        {
+            result.push_str("<pre><code>");
+            highlighting = true;
+        } else if !in_code_block && trimmed_line == "```" {
+            result.push_str("</pre></code>");
+            highlighting = false;
+        } else {
+            if highlighting {
+                result.push_str(&colorize_code_line(line));
+            } else {
+                result.push_str(line);
+            }
+            if index != lines.len() - 1 {
+                result.push('\n');
+            }
+        }
+    }
+
+    if in_code_block && highlighting {
+        result.push_str("</pre></code>");
+    }
+
+    result
+}
+
+fn colorize_code_line(line: &str) -> String {
+    let mut result = String::new();
+    let lexer = Lexer::new_with_dummy_file(line).skip_comments(false).skip_whitespaces(false);
+
+    for token in lexer {
+        let Ok(token) = token else {
+            // If lexing fails, give up and return the original line
+            return line.to_string();
+        };
+        if matches!(token.token(), Token::EOF) {
+            break;
+        }
+
+        colorize_token(&token, line, &mut result);
+    }
+
+    result
+}
+
+fn colorize_token(token: &LocatedToken, line: &str, result: &mut String) {
+    let span = token.span();
+    let token = token.token();
+    let token_str = &line[span.start() as usize..span.end() as usize];
+    let class = match token {
+        Token::Int(..) => Some("number"),
+        Token::Bool(_) => Some("bool-var"),
+        Token::Str(_) | Token::RawStr(_, _) => Some("string"),
+        Token::FmtStr(fragments, _) => {
+            result.push_str("<span class=\"string\">f\"</span>");
+            for fragment in fragments {
+                match fragment {
+                    FmtStrFragment::String(string) => {
+                        result.push_str(&format!(
+                            "<span class=\"string\">{}</span>",
+                            escape_html(string)
+                        ));
+                    }
+                    FmtStrFragment::Interpolation(string, _) => {
+                        result.push_str("<span class=\"interpolation\">{");
+                        result.push_str(string);
+                        result.push_str("}</span>");
+                    }
+                }
+            }
+            result.push_str("<span class=\"string\">\"</span>");
+            return;
+        }
+        Token::Keyword(_) => Some("kw"),
+        Token::AttributeStart { .. } => None,
+        Token::LineComment(_, doc_style) | Token::BlockComment(_, doc_style) => match doc_style {
+            Some(..) => Some("doccomment"),
+            None => Some("comment"),
+        },
+        Token::Quote(tokens) => {
+            result.push_str("<span class=\"kw\">quote</span> {");
+            for token in &tokens.0 {
+                colorize_token(token, line, result);
+            }
+            result.push('}');
+            return;
+        }
+        Token::QuotedType(_)
+        | Token::Ident(_)
+        | Token::InternedExpr(..)
+        | Token::InternedStatement(..)
+        | Token::InternedLValue(..)
+        | Token::InternedUnresolvedTypeData(..)
+        | Token::InternedPattern(..)
+        | Token::InternedCrate(..)
+        | Token::Less
+        | Token::LessEqual
+        | Token::Greater
+        | Token::GreaterEqual
+        | Token::Equal
+        | Token::NotEqual
+        | Token::Plus
+        | Token::Minus
+        | Token::Star
+        | Token::Slash
+        | Token::Percent
+        | Token::Ampersand
+        | Token::SliceStart
+        | Token::Caret
+        | Token::ShiftLeft
+        | Token::ShiftRight
+        | Token::Dot
+        | Token::DoubleDot
+        | Token::DoubleDotEqual
+        | Token::LeftParen
+        | Token::RightParen
+        | Token::LeftBrace
+        | Token::RightBrace
+        | Token::LeftBracket
+        | Token::RightBracket
+        | Token::Arrow
+        | Token::FatArrow
+        | Token::Pipe
+        | Token::Pound
+        | Token::Comma
+        | Token::Colon
+        | Token::DoubleColon
+        | Token::Semicolon
+        | Token::Bang
+        | Token::Assign
+        | Token::DollarSign
+        | Token::LogicalAnd
+        | Token::EOF
+        | Token::Whitespace(_)
+        | Token::UnquoteMarker(_)
+        | Token::Invalid(_) => None,
+    };
+    if let Some(class) = class {
+        result.push_str(&format!("<span class=\"{class}\">{}</span>", escape_html(token_str)));
+    } else {
+        result.push_str(&escape_html(token_str));
+    }
+}
