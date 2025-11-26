e4313717a56a6e929bfc6f9f7fe31de7a9487cbb
github-action-benchmark <github@users.noreply.github.com>
2025-11-26 13:12:15 +0000

    add ACVM Benchmarks (cargo) benchmark result for 3efa5e68408eb93bae710d954d1ca9e1193c4190

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764161161679,
+  "lastUpdate": 1764162735563,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -23181,48 +23181,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": false,
-          "id": "251d991e582df6cb22aff5d178a04a65dd1f4a6f",
-          "message": "fix: error if `Quoted::as_module` finds private module (#10511)",
-          "timestamp": "2025-11-18T17:40:25Z",
-          "tree_id": "c2de5b745de5708b4dc4f34f80d67fd6d4827deb",
-          "url": "https://github.com/noir-lang/noir/commit/251d991e582df6cb22aff5d178a04a65dd1f4a6f"
-        },
-        "date": 1763489028005,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 263566,
-            "range": "± 706",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 235017,
-            "range": "± 6583",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 2796088,
-            "range": "± 69035",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "asterite@gmail.com",
-            "name": "Ary Borenszweig",
-            "username": "asterite"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
           "id": "789455f6bf5a35d21a6398ad85026b05b5779862",
           "message": "fix: evaluate repeated array expr once (#10514)",
           "timestamp": "2025-11-18T17:55:11Z",
@@ -25264,6 +25222,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 2791987,
             "range": "± 4217",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "15848336+TomAFrench@users.noreply.github.com",
+            "name": "Tom French",
+            "username": "TomAFrench"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3efa5e68408eb93bae710d954d1ca9e1193c4190",
+          "message": "chore: group some related dependencies in dependabot and bump them (#10630)",
+          "timestamp": "2025-11-26T13:09:13Z",
+          "tree_id": "07277c49d49f5b75ffc53219e4a3e7744c94c54b",
+          "url": "https://github.com/noir-lang/noir/commit/3efa5e68408eb93bae710d954d1ca9e1193c4190"
+        },
+        "date": 1764162707327,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 257536,
+            "range": "± 481",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 230212,
+            "range": "± 1943",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 2796477,
+            "range": "± 1731",
             "unit": "ns/iter"
           }
         ]
