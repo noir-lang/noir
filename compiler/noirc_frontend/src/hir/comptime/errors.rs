a5edaa61161198bd1f0f66952c671bc879366dfd
jfecher <jfecher11@gmail.com>
2025-12-02 14:26:44 -0600

    fix: Error on duplicate field in set_fields (#10726)

--- compiler/noirc_frontend/src/hir/comptime/errors.rs
+++ compiler/noirc_frontend/src/hir/comptime/errors.rs
@@ -3,7 +3,7 @@ use std::rc::Rc;
 
 use crate::{
     Type,
-    ast::TraitBound,
+    ast::{Ident, TraitBound},
     hir::{
         def_collector::dc_crate::CompilationError,
         type_check::{NoMatchingImplFoundError, TypeCheckError},
@@ -267,6 +267,11 @@ pub enum InterpreterError {
     LoopHaltedForUiResponsiveness {
         location: Location,
     },
+    DuplicateStructFieldInSetFields {
+        name: Ident,
+        index: usize,
+        previous_index: usize,
+    },
     CheckedTransmuteFailed {
         actual: Type,
         expected: Type,
@@ -354,6 +359,7 @@ impl InterpreterError {
             | InterpreterError::CheckedTransmuteFailed { location, .. } => *location,
             InterpreterError::FailedToParseMacro { error, .. } => error.location(),
             InterpreterError::NoMatchingImplFound { error } => error.location,
+            InterpreterError::DuplicateStructFieldInSetFields { name, .. } => name.location(),
             InterpreterError::Break | InterpreterError::Continue => {
                 panic!("Tried to get the location of Break/Continue error!")
             }
@@ -747,6 +753,15 @@ impl<'a> From<&'a InterpreterError> for CustomDiagnostic {
                 let secondary =
                     "This error doesn't happen in normal executions of `nargo`".to_string();
                 CustomDiagnostic::simple_warning(msg, secondary, *location)
+            }
+            InterpreterError::DuplicateStructFieldInSetFields { name, index, previous_index } => {
+                let msg = "Duplicate field name in call to `set_fields`".to_string();
+                let secondary = format!(
+                    "`{name}` first used as field {} then again as field {}",
+                    previous_index + 1,
+                    index + 1
+                );
+                CustomDiagnostic::simple_error(msg, secondary, name.location())
             }
             InterpreterError::CheckedTransmuteFailed { actual, expected, location } => {
                 let msg = format!("Checked transmute failed: `{actual:?}` != `{expected:?}`");
