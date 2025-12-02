919cbce9620d3e7bff55f1497de537350333c794
Akosh Farkash <aakoshh@gmail.com>
2025-12-02 17:35:36 +0000

    chore(audit): `path_resolution`  (#10717)

--- compiler/noirc_frontend/src/hir/def_map/namespace.rs
+++ compiler/noirc_frontend/src/hir/def_map/namespace.rs
@@ -1,7 +1,9 @@
 use super::ModuleDefId;
 use crate::ast::ItemVisibility;
 
-// This works exactly the same as in r-a, just simplified
+/// Result of looking up a name in type and value definitions in scope.
+///
+/// This works exactly the same as in r-a, just simplified.
 #[derive(Debug, PartialEq, Eq, Copy, Clone, Default)]
 pub struct PerNs {
     pub types: Option<(ModuleDefId, ItemVisibility, bool)>,
@@ -9,18 +11,22 @@ pub struct PerNs {
 }
 
 impl PerNs {
+    /// Creates a [PerNs] with a public [ModuleDefId] in `types`, and no `values`.
     pub fn types(t: ModuleDefId) -> PerNs {
         PerNs { types: Some((t, ItemVisibility::Public, false)), values: None }
     }
 
+    /// Iterate the [ModuleDefId]s in both `types` and `values`.
     pub fn iter_defs(self) -> impl Iterator<Item = ModuleDefId> {
-        self.types.map(|it| it.0).into_iter().chain(self.values.map(|it| it.0))
+        self.iter_items().map(|it| it.0)
     }
 
+    /// Iterate the results in both `types` and `values`.
     pub fn iter_items(self) -> impl Iterator<Item = (ModuleDefId, ItemVisibility, bool)> {
         self.types.into_iter().chain(self.values)
     }
 
+    /// Returns `true` if both `types` and `values` are empty.
     pub fn is_none(&self) -> bool {
         self.types.is_none() && self.values.is_none()
     }
