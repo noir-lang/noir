20788206afbf058fd96ff69375e8e4af76d83686
Tom French <15848336+TomAFrench@users.noreply.github.com>
2025-12-03 15:23:21 +0000

    feat: always check bitsize of logical operation inputs

--- acvm-repo/acvm/src/pwg/blackbox/logic.rs
+++ acvm-repo/acvm/src/pwg/blackbox/logic.rs
@@ -15,17 +15,10 @@ pub(super) fn and<F: AcirField>(
     rhs: &FunctionInput<F>,
     num_bits: u32,
     output: &Witness,
-    pedantic_solving: bool,
 ) -> Result<(), OpcodeResolutionError<F>> {
-    solve_logic_opcode(
-        initial_witness,
-        lhs,
-        rhs,
-        num_bits,
-        *output,
-        pedantic_solving,
-        |left, right| bit_and(left, right, num_bits),
-    )
+    solve_logic_opcode(initial_witness, lhs, rhs, num_bits, *output, |left, right| {
+        bit_and(left, right, num_bits)
+    })
 }
 
 /// Solves a [`BlackBoxFunc::XOR`][acir::circuit::black_box_functions::BlackBoxFunc::XOR] opcode and inserts
@@ -36,17 +29,10 @@ pub(super) fn xor<F: AcirField>(
     rhs: &FunctionInput<F>,
     num_bits: u32,
     output: &Witness,
-    pedantic_solving: bool,
 ) -> Result<(), OpcodeResolutionError<F>> {
-    solve_logic_opcode(
-        initial_witness,
-        lhs,
-        rhs,
-        num_bits,
-        *output,
-        pedantic_solving,
-        |left, right| bit_xor(left, right, num_bits),
-    )
+    solve_logic_opcode(initial_witness, lhs, rhs, num_bits, *output, |left, right| {
+        bit_xor(left, right, num_bits)
+    })
 }
 
 /// Derives the rest of the witness based on the initial low level variables
@@ -56,19 +42,14 @@ fn solve_logic_opcode<F: AcirField>(
     b: &FunctionInput<F>,
     num_bits: u32,
     result: Witness,
-    pedantic_solving: bool,
     logic_op: impl Fn(F, F) -> F,
 ) -> Result<(), OpcodeResolutionError<F>> {
     let w_l_value = input_to_value(initial_witness, *a)?;
     let w_r_value = input_to_value(initial_witness, *b)?;
     let assignment = logic_op(w_l_value, w_r_value);
-    // TODO(https://github.com/noir-lang/noir/issues/5985): re-enable these by
-    // default once we figure out how to combine these with existing
-    // noirc_frontend/noirc_evaluator overflow error messages
-    if pedantic_solving {
-        check_bit_size(w_l_value, num_bits)?;
-        check_bit_size(w_r_value, num_bits)?;
-    }
+    check_bit_size(w_l_value, num_bits)?;
+    check_bit_size(w_r_value, num_bits)?;
+
     insert_value(&result, assignment, initial_witness)
 }
 
@@ -76,49 +57,103 @@ fn solve_logic_opcode<F: AcirField>(
 mod tests {
     use crate::pwg::blackbox::{and, xor};
     use acir::{
-        FieldElement,
+        FieldElement, InvalidInputBitSize,
         circuit::opcodes::FunctionInput,
         native_types::{Witness, WitnessMap},
     };
     use std::collections::BTreeMap;
 
-    #[test]
-    fn test_and() {
-        let lhs = FunctionInput::Witness(Witness(1));
-        let rhs = FunctionInput::Witness(Witness(2));
+    mod and {
+        use super::*;
 
-        let mut initial_witness = WitnessMap::from(BTreeMap::from_iter([
-            (Witness(1), FieldElement::from(5u128)),
-            (Witness(2), FieldElement::from(8u128)),
-        ]));
-        and(&mut initial_witness, &lhs, &rhs, 8, &Witness(3), false).unwrap();
-        assert_eq!(initial_witness[&Witness(3)], FieldElement::from(0u128));
+        #[test]
+        fn smoke_test() {
+            let lhs = FunctionInput::Witness(Witness(1));
+            let rhs = FunctionInput::Witness(Witness(2));
 
-        let mut initial_witness = WitnessMap::from(BTreeMap::from_iter([
-            (Witness(1), FieldElement::from(26u128)),
-            (Witness(2), FieldElement::from(34u128)),
-        ]));
-        and(&mut initial_witness, &lhs, &rhs, 8, &Witness(3), false).unwrap();
-        assert_eq!(initial_witness[&Witness(3)], FieldElement::from(2u128));
+            let mut initial_witness = WitnessMap::from(BTreeMap::from_iter([
+                (Witness(1), FieldElement::from(5u128)),
+                (Witness(2), FieldElement::from(8u128)),
+            ]));
+            and(&mut initial_witness, &lhs, &rhs, 8, &Witness(3)).unwrap();
+            assert_eq!(initial_witness[&Witness(3)], FieldElement::from(0u128));
+
+            let mut initial_witness = WitnessMap::from(BTreeMap::from_iter([
+                (Witness(1), FieldElement::from(26u128)),
+                (Witness(2), FieldElement::from(34u128)),
+            ]));
+            and(&mut initial_witness, &lhs, &rhs, 8, &Witness(3)).unwrap();
+            assert_eq!(initial_witness[&Witness(3)], FieldElement::from(2u128));
+        }
+
+         #[test]
+        fn errors_if_input_is_too_large() {
+            let lhs = FunctionInput::Witness(Witness(1));
+            let rhs = FunctionInput::Witness(Witness(2));
+
+            let mut initial_witness = WitnessMap::from(BTreeMap::from_iter([
+                (Witness(1), FieldElement::from(5u128)),
+                (Witness(2), FieldElement::from(256u128)),
+            ]));
+            let result = and(&mut initial_witness, &lhs, &rhs, 8, &Witness(3));
+            assert_eq!(
+                result,
+                Err(crate::pwg::OpcodeResolutionError::InvalidInputBitSize {
+                    opcode_location: crate::pwg::ErrorLocation::Unresolved,
+                    invalid_input_bit_size: InvalidInputBitSize {
+                        value: "256".to_string(),
+                        value_num_bits: 9,
+                        max_bits: 8
+                    },
+                })
+            )
+        }
     }
 
-    #[test]
-    fn test_xor() {
-        let lhs = FunctionInput::Witness(Witness(1));
-        let rhs = FunctionInput::Witness(Witness(2));
+    mod xor {
+        use super::*;
 
-        let mut initial_witness = WitnessMap::from(BTreeMap::from_iter([
-            (Witness(1), FieldElement::from(5u128)),
-            (Witness(2), FieldElement::from(8u128)),
-        ]));
-        xor(&mut initial_witness, &lhs, &rhs, 8, &Witness(3), false).unwrap();
-        assert_eq!(initial_witness[&Witness(3)], FieldElement::from(13u128));
+        #[test]
+        fn test_xor() {
+            let lhs = FunctionInput::Witness(Witness(1));
+            let rhs = FunctionInput::Witness(Witness(2));
 
-        let mut initial_witness = WitnessMap::from(BTreeMap::from_iter([
-            (Witness(1), FieldElement::from(26u128)),
-            (Witness(2), FieldElement::from(34u128)),
-        ]));
-        xor(&mut initial_witness, &lhs, &rhs, 8, &Witness(3), false).unwrap();
-        assert_eq!(initial_witness[&Witness(3)], FieldElement::from(56u128));
+            let mut initial_witness = WitnessMap::from(BTreeMap::from_iter([
+                (Witness(1), FieldElement::from(5u128)),
+                (Witness(2), FieldElement::from(8u128)),
+            ]));
+            xor(&mut initial_witness, &lhs, &rhs, 8, &Witness(3)).unwrap();
+            assert_eq!(initial_witness[&Witness(3)], FieldElement::from(13u128));
+
+            let mut initial_witness = WitnessMap::from(BTreeMap::from_iter([
+                (Witness(1), FieldElement::from(26u128)),
+                (Witness(2), FieldElement::from(34u128)),
+            ]));
+            xor(&mut initial_witness, &lhs, &rhs, 8, &Witness(3)).unwrap();
+            assert_eq!(initial_witness[&Witness(3)], FieldElement::from(56u128));
+        }
+
+        #[test]
+        fn errors_if_input_is_too_large() {
+            let lhs = FunctionInput::Witness(Witness(1));
+            let rhs = FunctionInput::Witness(Witness(2));
+
+            let mut initial_witness = WitnessMap::from(BTreeMap::from_iter([
+                (Witness(1), FieldElement::from(5u128)),
+                (Witness(2), FieldElement::from(256u128)),
+            ]));
+            let result = xor(&mut initial_witness, &lhs, &rhs, 8, &Witness(3));
+            assert_eq!(
+                result,
+                Err(crate::pwg::OpcodeResolutionError::InvalidInputBitSize {
+                    opcode_location: crate::pwg::ErrorLocation::Unresolved,
+                    invalid_input_bit_size: InvalidInputBitSize {
+                        value: "256".to_string(),
+                        value_num_bits: 9,
+                        max_bits: 8
+                    },
+                })
+            )
+        }
     }
 }
