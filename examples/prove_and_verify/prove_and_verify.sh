3e3fac07f0250f88abb0ca3b68ccd95d2b33193c
Akosh Farkash <aakoshh@gmail.com>
2025-12-09 20:31:12 +0000

    First write the VK, then prove

--- examples/prove_and_verify/prove_and_verify.sh
+++ examples/prove_and_verify/prove_and_verify.sh
@@ -3,12 +3,8 @@ set -eu
 
 BACKEND=${BACKEND:-bb}
 
-nargo execute --pedantic-solving witness
+nargo execute --force --pedantic-solving witness
 
-# TODO: `bb` should create `proofs` directory if it doesn't exist.
-mkdir -p proofs
-$BACKEND prove -b ./target/hello_world.json -w ./target/witness.gz -o ./proofs
-
-# TODO: backend should automatically generate vk if necessary.
 $BACKEND write_vk -b ./target/hello_world.json -o ./target
+$BACKEND prove -b ./target/hello_world.json -w ./target/witness.gz -o ./proofs
 $BACKEND verify -k ./target/vk -p ./proofs/proof -i ./proofs/public_inputs
