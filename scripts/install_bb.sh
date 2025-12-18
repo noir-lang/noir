f45aacad8e7deb7864ced2f1aa8f2a06452b6fb4
Akosh Farkash <aakoshh@gmail.com>
2025-12-18 09:14:37 +0000

    Update to bb version Adam pointed at, which still supports all kinds of msgpack

--- scripts/install_bb.sh
+++ scripts/install_bb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-VERSION="3.0.0-nightly.20251104"
+VERSION="v3.0.0-nightly.20251216"
 
 BBUP_PATH=~/.bb/bbup
 
-if ! [ -f $BBUP_PATH ]; then 
+if ! [ -f $BBUP_PATH ]; then
     curl -L https://raw.githubusercontent.com/AztecProtocol/aztec-packages/refs/heads/next/barretenberg/bbup/install | bash
 fi
 
