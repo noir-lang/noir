44512b29b76a007b0e484b8461f66cb9e039db06
Tom French <15848336+TomAFrench@users.noreply.github.com>
2026-01-02 15:12:58 +0000

    chore: remove fold-fibonacci integration test

--- compiler/integration-tests/test/node/prove_and_verify.test.ts
+++ compiler/integration-tests/test/node/prove_and_verify.test.ts
@@ -1,12 +1,10 @@
 import { expect } from 'chai';
 import assert_lt_json from '../../circuits/assert_lt/target/assert_lt.json' assert { type: 'json' };
-import fold_fibonacci_json from '../../circuits/fold_fibonacci/target/fold_fibonacci.json' assert { type: 'json' };
 import { Noir } from '@noir-lang/noir_js';
 import { UltraHonkVerifierBackend, UltraHonkBackend } from '@aztec/bb.js';
 import { CompiledCircuit } from '@noir-lang/types';
 
 const assert_lt_program = assert_lt_json as CompiledCircuit;
-const fold_fibonacci_program = fold_fibonacci_json as CompiledCircuit;
 
 const honkBackend = new UltraHonkBackend(assert_lt_program.bytecode);
 
@@ -70,25 +68,4 @@ it('end-to-end proving and verification with different instances', async () => {
   const verifier = new UltraHonkBackend(assert_lt_program.bytecode);
   const proof_is_valid = await verifier.verifyProof(proof);
   expect(proof_is_valid).to.be.true;
-});
-
-it('end-to-end proof creation and verification for multiple ACIR circuits', async () => {
-  // Noir.Js part
-  const inputs = {
-    x: '10',
-  };
-
-  const program = new Noir(fold_fibonacci_program);
-
-  const { witness } = await program.execute(inputs);
-
-  // bb.js part
-  //
-  // Proof creation
-  const honkBackend = new UltraHonkBackend(fold_fibonacci_program.bytecode);
-  const proof = await honkBackend.generateProof(witness);
-
-  // Proof verification
-  const isValid = await honkBackend.verifyProof(proof);
-  expect(isValid).to.be.true;
 });
