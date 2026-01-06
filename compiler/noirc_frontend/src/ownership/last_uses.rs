5f4e5ddb1010beac98ecced0bff56e46ffc517b5
Maxim Vezenov <mvezenov@gmail.com>
2026-01-06 16:06:58 -0500

    clone array last used in while condition

--- compiler/noirc_frontend/src/ownership/last_uses.rs
+++ compiler/noirc_frontend/src/ownership/last_uses.rs
@@ -352,8 +352,11 @@ impl LastUseContext {
     }
 
     fn track_variables_in_while(&mut self, while_expr: &ast::While) {
+        self.push_loop_scope();
+        // The condition is evaluate on every iteration of the loop and thus must be included in the loop scope.
         self.track_variables_in_expression(&while_expr.condition);
-        self.track_variables_in_loop(&while_expr.body);
+        self.track_variables_in_expression(&while_expr.body);
+        self.pop_loop_scope();
     }
 
     fn track_variables_in_loop(&mut self, loop_body: &Expression) {
