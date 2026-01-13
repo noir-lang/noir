5269c0bb863d5d40ba146e5834390f4f1d7ddec7
jfecher <jfecher11@gmail.com>
2026-01-13 15:46:31 -0500

    Fix stack overflow on some Type methods

--- compiler/noirc_frontend/src/hir_def/types/validity.rs
+++ compiler/noirc_frontend/src/hir_def/types/validity.rs
@@ -1,6 +1,6 @@
 use noirc_errors::Location;
 
-use crate::{NamedGeneric, Type, TypeBinding, ast::Ident};
+use crate::{ARBITRARY_RECURSION_LIMIT, NamedGeneric, Type, TypeBinding, ast::Ident};
 
 /// An type incorrectly used as a program input.
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -25,94 +25,99 @@ impl Type {
     ///
     /// Returns `None` if this type and its nested types are all valid program inputs.
     pub(crate) fn program_input_validity(&self, allow_empty_arrays: bool) -> Option<InvalidType> {
-        match self {
-            // Type::Error is allowed as usual since it indicates an error was already issued and
-            // we don't need to issue further errors about this likely unresolved type
-            // TypeVariable and Generic are allowed here too as they can only result from
-            // generics being declared on the function itself, but we produce a different error in that case.
-            Type::FieldElement
-            | Type::Integer(_, _)
-            | Type::Bool
-            | Type::Constant(_, _)
-            | Type::TypeVariable(_)
-            | Type::NamedGeneric(_)
-            | Type::Error => None,
-
-            Type::Unit
-            | Type::FmtString(_, _)
-            | Type::Function(_, _, _, _)
-            | Type::Reference(..)
-            | Type::Forall(_, _)
-            | Type::Quoted(_)
-            | Type::Vector(_)
-            | Type::TraitAsType(..) => Some(InvalidType::Primitive(self.clone())),
-
-            Type::CheckedCast { to, .. } => to.program_input_validity(allow_empty_arrays),
-
-            Type::Alias(alias, generics) => {
-                let alias = alias.borrow();
-                if let Some(invalid_type) =
-                    alias.get_type(generics).program_input_validity(allow_empty_arrays)
-                {
-                    let alias_name = alias.name.clone();
-                    Some(InvalidType::Alias { alias_name, invalid_type: Box::new(invalid_type) })
-                } else {
-                    None
-                }
+        fn helper(this: &Type, allow_empty_arrays: bool, mut i: u32) -> Option<InvalidType> {
+            if i == ARBITRARY_RECURSION_LIMIT {
+                return None;
             }
+            i += 1;
+            let recur = |typ| helper(typ, allow_empty_arrays, i);
 
-            Type::Array(length, element) => {
-                if !length_is_valid_for_entry_point(length, allow_empty_arrays) {
-                    Some(InvalidType::Primitive(self.clone()))
-                } else {
-                    length
-                        .program_input_validity(allow_empty_arrays)
-                        .or_else(|| element.program_input_validity(allow_empty_arrays))
-                }
-            }
-            Type::String(length) => {
-                if !length_is_valid_for_entry_point(length, allow_empty_arrays) {
-                    Some(InvalidType::EmptyString(self.clone()))
-                } else {
-                    length.program_input_validity(allow_empty_arrays)
-                }
-            }
-            Type::Tuple(elements) => {
-                for element in elements {
-                    if let Some(invalid_type) = element.program_input_validity(allow_empty_arrays) {
-                        return Some(invalid_type);
+            match this {
+                // Type::Error is allowed as usual since it indicates an error was already issued and
+                // we don't need to issue further errors about this likely unresolved type
+                // TypeVariable and Generic are allowed here too as they can only result from
+                // generics being declared on the function itself, but we produce a different error in that case.
+                Type::FieldElement
+                | Type::Integer(_, _)
+                | Type::Bool
+                | Type::Constant(_, _)
+                | Type::TypeVariable(_)
+                | Type::NamedGeneric(_)
+                | Type::Error => None,
+
+                Type::Unit
+                | Type::FmtString(_, _)
+                | Type::Function(_, _, _, _)
+                | Type::Reference(..)
+                | Type::Forall(_, _)
+                | Type::Quoted(_)
+                | Type::Vector(_)
+                | Type::TraitAsType(..) => Some(InvalidType::Primitive(this.clone())),
+
+                Type::CheckedCast { to, .. } => recur(to),
+
+                Type::Alias(alias, generics) => {
+                    let alias = alias.borrow();
+                    if let Some(invalid_type) = recur(&alias.get_type(generics)) {
+                        let alias_name = alias.name.clone();
+                        Some(InvalidType::Alias {
+                            alias_name,
+                            invalid_type: Box::new(invalid_type),
+                        })
+                    } else {
+                        None
                     }
                 }
-                None
-            }
-            Type::DataType(definition, generics) => {
-                let definition = definition.borrow();
 
-                if let Some(fields) = definition.get_fields(generics) {
-                    for (field_name, field, _) in fields {
-                        if let Some(invalid_type) = field.program_input_validity(allow_empty_arrays)
-                        {
-                            let struct_name = definition.name.clone();
-                            let mut fields_raw = definition.fields_raw().unwrap().iter();
-                            let field = fields_raw.find(|field| field.name.as_str() == field_name);
-                            return Some(InvalidType::StructField {
-                                struct_name,
-                                field_name: field.unwrap().name.clone(),
-                                invalid_type: Box::new(invalid_type),
-                            });
+                Type::Array(length, element) => {
+                    if !length_is_valid_for_entry_point(length, allow_empty_arrays) {
+                        Some(InvalidType::Primitive(this.clone()))
+                    } else {
+                        recur(length).or_else(|| recur(element))
+                    }
+                }
+                Type::String(length) => {
+                    if !length_is_valid_for_entry_point(length, allow_empty_arrays) {
+                        Some(InvalidType::EmptyString(this.clone()))
+                    } else {
+                        recur(length)
+                    }
+                }
+                Type::Tuple(elements) => {
+                    for element in elements {
+                        if let Some(invalid_type) = recur(element) {
+                            return Some(invalid_type);
                         }
                     }
                     None
-                } else {
-                    // Arbitrarily disallow enums from program input, though we may support them later
-                    Some(InvalidType::Enum(self.clone()))
                 }
-            }
+                Type::DataType(definition, generics) => {
+                    let definition = definition.borrow();
 
-            Type::InfixExpr(lhs, _, rhs, _) => lhs
-                .program_input_validity(allow_empty_arrays)
-                .or_else(|| rhs.program_input_validity(allow_empty_arrays)),
+                    if let Some(fields) = definition.get_fields(generics) {
+                        for (field_name, field, _) in fields {
+                            if let Some(invalid_type) = helper(&field, allow_empty_arrays, i) {
+                                let struct_name = definition.name.clone();
+                                let mut fields_raw = definition.fields_raw().unwrap().iter();
+                                let field =
+                                    fields_raw.find(|field| field.name.as_str() == field_name);
+                                return Some(InvalidType::StructField {
+                                    struct_name,
+                                    field_name: field.unwrap().name.clone(),
+                                    invalid_type: Box::new(invalid_type),
+                                });
+                            }
+                        }
+                        None
+                    } else {
+                        // Arbitrarily disallow enums from program input, though we may support them later
+                        Some(InvalidType::Enum(this.clone()))
+                    }
+                }
+                Type::InfixExpr(lhs, _, rhs, _) => recur(lhs).or_else(|| recur(rhs)),
+            }
         }
+        helper(self, allow_empty_arrays, 0)
     }
 
     /// Returns this type, or a nested one, if this type can be used as a parameter to an ACIR
