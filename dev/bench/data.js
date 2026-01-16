a3664c64bec871de01a131a5b22f92874ae97700
github-action-benchmark <github@users.noreply.github.com>
2026-01-16 16:16:12 +0000

    add ACVM Benchmarks (cargo) benchmark result for f5f9af5b33d596565c74152161a7a8c916cd54fd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768580051954,
+  "lastUpdate": 1768580172009,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -23131,48 +23131,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "asterite@gmail.com",
-            "name": "Ary Borenszweig",
-            "username": "asterite"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "bb95e527023095696a9795e53c1ccc1ccc64d61c",
-          "message": "fix: safe cast shift rhs to u32 in comptime interpreter (#11151)",
-          "timestamp": "2026-01-09T18:14:22Z",
-          "tree_id": "1aa13bc9169163ba77be262011b78fc042c08556",
-          "url": "https://github.com/noir-lang/noir/commit/bb95e527023095696a9795e53c1ccc1ccc64d61c"
-        },
-        "date": 1767983777772,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 254098,
-            "range": "± 818",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 224199,
-            "range": "± 1905",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 2793842,
-            "range": "± 1415",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "47281315+guipublic@users.noreply.github.com",
             "name": "guipublic",
             "username": "guipublic"
@@ -25224,6 +25182,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 2783821,
             "range": "± 2057",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "asterite@gmail.com",
+            "name": "Ary Borenszweig",
+            "username": "asterite"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f5f9af5b33d596565c74152161a7a8c916cd54fd",
+          "message": "fix(parser): error on unexpected type expression in type alias (#11190)",
+          "timestamp": "2026-01-16T15:54:24Z",
+          "tree_id": "411d61cc33fe2f359ef7d024fdd95aea69b03215",
+          "url": "https://github.com/noir-lang/noir/commit/f5f9af5b33d596565c74152161a7a8c916cd54fd"
+        },
+        "date": 1768580135637,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 256671,
+            "range": "± 428",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 227614,
+            "range": "± 5503",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 2788845,
+            "range": "± 3257",
             "unit": "ns/iter"
           }
         ]
