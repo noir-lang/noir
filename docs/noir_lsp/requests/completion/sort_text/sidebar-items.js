cab4e9b39089ace1e1b7ad62556b365bf954f245
rustdoc <github@users.noreply.github.com>
2026-01-27 21:16:50 +0000

    update rustdoc

--- docs/noir_lsp/requests/completion/sort_text/sidebar-items.js
+++ docs/noir_lsp/requests/completion/sort_text/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["auto_import_sort_text","crate_or_module_sort_text","default_sort_text","new_sort_text","operator_sort_text","self_mismatch_sort_text","underscore_sort_text"]};
+window.SIDEBAR_ITEMS = {"fn":["auto_import_sort_text","crate_or_module_sort_text","default_sort_text","local_variable_sort_text","new_sort_text","operator_sort_text","self_mismatch_sort_text","underscore_sort_text"]};
