98d512a34ad0e273fefa9369fe5f30c77d3ad8a6
rustdoc <github@users.noreply.github.com>
2026-02-04 13:19:18 +0000

    update rustdoc

--- docs/trait.impl/core/borrow/trait.Borrow.js
+++ docs/trait.impl/core/borrow/trait.Borrow.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["noirc_span",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/borrow/trait.Borrow.html\" title=\"trait core::borrow::Borrow\">Borrow</a>&lt;T&gt; for <a class=\"struct\" href=\"noirc_span/struct.Spanned.html\" title=\"struct noirc_span::Spanned\">Spanned</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["noirc_span",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/borrow/trait.Borrow.html\" title=\"trait core::borrow::Borrow\">Borrow</a>&lt;T&gt; for <a class=\"struct\" href=\"noirc_span/struct.Spanned.html\" title=\"struct noirc_span::Spanned\">Spanned</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
