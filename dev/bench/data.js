f5302656ca86602c967945f54686988e47ac158e
github-action-benchmark <github@users.noreply.github.com>
2026-02-19 16:11:09 +0000

    add ACVM Benchmarks (cargo) benchmark result for 12ecac9917f95d6896cfa79fcaad98cef3487f0f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771516389684,
+  "lastUpdate": 1771517469121,
   "repoUrl": "https://github.com/noir-lang/noir",
   "entries": {
     "Compilation Memory": [
@@ -23140,48 +23140,6 @@ window.BENCHMARK_DATA = {
             "name": "GitHub",
             "username": "web-flow"
           },
-          "distinct": true,
-          "id": "be5bff831712b08c1c027924c0797bf9b93731f2",
-          "message": "fix: lookup method in trait constraints for structs (#11533)",
-          "timestamp": "2026-02-10T19:19:40Z",
-          "tree_id": "28379a46f033202da058123a8ac770aebf02f417",
-          "url": "https://github.com/noir-lang/noir/commit/be5bff831712b08c1c027924c0797bf9b93731f2"
-        },
-        "date": 1770752500196,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "purely_sequential_opcodes",
-            "value": 254345,
-            "range": "± 1057",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_opcodes",
-            "value": 221139,
-            "range": "± 4193",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "perfectly_parallel_batch_inversion_opcodes",
-            "value": 2732380,
-            "range": "± 8609",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "asterite@gmail.com",
-            "name": "Ary Borenszweig",
-            "username": "asterite"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
           "distinct": false,
           "id": "60dabc3fb366b11303fc7533f089130121b2359e",
           "message": "fix: utf-8 length for fmtstr (#11510)\n\nCo-authored-by: Akosh Farkash <aakoshh@gmail.com>",
@@ -25224,6 +25182,48 @@ window.BENCHMARK_DATA = {
             "name": "perfectly_parallel_batch_inversion_opcodes",
             "value": 2724466,
             "range": "± 8156",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "asterite@gmail.com",
+            "name": "Ary Borenszweig",
+            "username": "asterite"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "12ecac9917f95d6896cfa79fcaad98cef3487f0f",
+          "message": "feat: improve error location of necessary/unnecessary visibility (#11460)",
+          "timestamp": "2026-02-19T15:47:43Z",
+          "tree_id": "d3a319fcec1c3ac9cb6095d5a9a4fe22b406e3d6",
+          "url": "https://github.com/noir-lang/noir/commit/12ecac9917f95d6896cfa79fcaad98cef3487f0f"
+        },
+        "date": 1771517427407,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "purely_sequential_opcodes",
+            "value": 250875,
+            "range": "± 472",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_opcodes",
+            "value": 221863,
+            "range": "± 4536",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perfectly_parallel_batch_inversion_opcodes",
+            "value": 2724016,
+            "range": "± 1475",
             "unit": "ns/iter"
           }
         ]
