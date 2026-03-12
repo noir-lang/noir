62f0c5888199df6fa29b707eacb69590f217eb9a
guipublic <guipublic@gmail.com>
2026-03-12 18:51:50 +0100

    clippy

--- compiler/noirc_evaluator/src/ssa/opt/mod.rs
+++ compiler/noirc_evaluator/src/ssa/opt/mod.rs
@@ -50,7 +50,6 @@ pub(crate) use array_get::{
     ArrayGetOptimizationResult, ArrayGetOptimizationSideEffects,
     try_optimize_array_get_from_previous_instructions,
 };
-pub(crate) use checked_to_unchecked::is_unsigned_sub_safe;
 pub use constant_folding::DEFAULT_MAX_ITER as CONSTANT_FOLDING_MAX_ITER;
 pub use inlining::MAX_SIMPLE_FUNCTION_WEIGHT as INLINING_MAX_INSTRUCTIONS;
 pub use unrolling::{FORCE_UNROLL_THRESHOLD, MAX_UNROLL_ITERATIONS};
