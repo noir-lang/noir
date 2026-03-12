1195d6d17d4e6484f1f53954aa16b5165762cd6c
sarkoxedaf <sarkoxed@sarkoxed.me>
2026-03-12 09:30:16 +0000

    resolve clippy + make dec_rc public

--- tooling/ssa_fuzzer/src/builder.rs
+++ tooling/ssa_fuzzer/src/builder.rs
@@ -725,10 +725,7 @@ impl FuzzerBuilder {
         value: TypedValue,
         safe_index: bool,
     ) -> TypedValue {
-        let (array_type, array_length) = match array.type_of_variable.clone() {
-            Type::Array(array_type, array_length) => (array_type, array_length),
-            _ => unreachable!("Array type expected"),
-        };
+        let Type::Array(array_type, array_length) = array.type_of_variable.clone() else { unreachable!("Array type expected") };
 
         assert!(index.type_of_variable == Type::Numeric(NumericType::U32));
         let index = if safe_index {
