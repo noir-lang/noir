4ef4c1125048def66b9f941c49e280a25d4b9e0a
rustdoc <github@users.noreply.github.com>
2026-04-09 20:37:34 +0000

    update rustdoc

--- docs/noir_lsp/types/notification/sidebar-items.js
+++ docs/noir_lsp/types/notification/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["DidChangeConfiguration","DidChangeTextDocument","DidCloseTextDocument","DidOpenTextDocument","DidSaveTextDocument","Exit","Initialized"],"struct":["NargoUpdateTests"]};
+window.SIDEBAR_ITEMS = {"enum":["DidChangeConfiguration","DidChangeTextDocument","DidChangeWatchedFiles","DidCloseTextDocument","DidOpenTextDocument","DidSaveTextDocument","Exit","Initialized"],"struct":["NargoUpdateTests"]};
