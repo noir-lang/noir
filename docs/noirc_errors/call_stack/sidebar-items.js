95f9d9196a5aa4ea911124f724a8310207cef8f7
rustdoc <github@users.noreply.github.com>
2026-04-17 19:46:18 +0000

    update rustdoc

--- docs/noirc_errors/call_stack/sidebar-items.js
+++ docs/noirc_errors/call_stack/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["CallStackHelper","CallStackId","LocationNode"],"type":["CallStack"]};
+window.SIDEBAR_ITEMS = {"struct":["CallStack","CallStackHelper","CallStackId","LocationNode"]};
