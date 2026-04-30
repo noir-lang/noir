30e963c48e9260204bb23f370671a0513d5d4f9f
Akosh Farkash <aakoshh@gmail.com>
2026-04-30 18:08:43 +0100

    Remove restriction on radix in comptime code

--- compiler/noirc_frontend/src/hir/comptime/interpreter/builtin.rs
+++ compiler/noirc_frontend/src/hir/comptime/interpreter/builtin.rs
@@ -953,9 +953,6 @@ fn to_le_radix(
 
 fn compute_to_radix_le(field: FieldElement, radix: u32) -> Vec<u8> {
     assert_ne!(radix, 0, "ICE: Radix must be greater than 0");
-    let bit_size = u32::BITS - (radix - 1).leading_zeros();
-    let radix_big = BigUint::from(radix);
-    assert_eq!(BigUint::from(2u128).pow(bit_size), radix_big, "ICE: Radix must be a power of 2");
     let big_integer = BigUint::from_bytes_be(&field.to_be_bytes());
 
     // Decompose the integer into its radix digits in little endian form.
