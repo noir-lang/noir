8ccdd2ebe6c3cf09d39b1728d9ab19fe783a9a64
rustdoc <github@users.noreply.github.com>
2026-06-11 16:57:29 +0000

    update rustdoc

--- docs/trait.impl/serde_core/ser/trait.SerializeTuple.js
+++ docs/trait.impl/serde_core/ser/trait.SerializeTuple.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["msgpack_tagged",[["impl&lt;'ser, 'a, W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.SerializeTuple.html\" title=\"trait serde_core::ser::SerializeTuple\">SerializeTuple</a> for <a class=\"struct\" href=\"msgpack_tagged/serializer/struct.TaggedSerializeViaParent.html\" title=\"struct msgpack_tagged::serializer::TaggedSerializeViaParent\">TaggedSerializeViaParent</a>&lt;'ser, 'a, W&gt;"]]]]);
+    var implementors = Object.fromEntries([["msgpack_tagged",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.SerializeTuple.html\" title=\"trait serde_core::ser::SerializeTuple\">SerializeTuple</a> for <a class=\"struct\" href=\"msgpack_tagged/serializer/struct.TaggedSerializeViaParent.html\" title=\"struct msgpack_tagged::serializer::TaggedSerializeViaParent\">TaggedSerializeViaParent</a>&lt;'_, '_, W&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[560]}
+//{"start":57,"fragment_lengths":[548]}
--- docs/trait.impl/serde_core/ser/trait.SerializeTupleVariant.js
+++ docs/trait.impl/serde_core/ser/trait.SerializeTupleVariant.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["msgpack_tagged",[["impl&lt;'ser, 'a, W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.SerializeTupleVariant.html\" title=\"trait serde_core::ser::SerializeTupleVariant\">SerializeTupleVariant</a> for <a class=\"struct\" href=\"msgpack_tagged/serializer/struct.TaggedSerializeProduct.html\" title=\"struct msgpack_tagged::serializer::TaggedSerializeProduct\">TaggedSerializeProduct</a>&lt;'ser, 'a, W&gt;"]]]]);
+    var implementors = Object.fromEntries([["msgpack_tagged",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.SerializeTupleVariant.html\" title=\"trait serde_core::ser::SerializeTupleVariant\">SerializeTupleVariant</a> for <a class=\"struct\" href=\"msgpack_tagged/serializer/struct.TaggedSerializeProduct.html\" title=\"struct msgpack_tagged::serializer::TaggedSerializeProduct\">TaggedSerializeProduct</a>&lt;'_, '_, W&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[575]}
+//{"start":57,"fragment_lengths":[563]}
