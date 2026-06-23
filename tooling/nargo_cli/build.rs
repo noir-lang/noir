965f47119a582c8942ba0de31621906ef7843a5a
AztecBot <tech@aztec-labs.com>
2026-06-23 14:07:21 +0000

    chore: remove leftover execution_panic test machinery

--- tooling/nargo_cli/build.rs
+++ tooling/nargo_cli/build.rs
@@ -31,7 +31,6 @@ fn main() -> Result<(), String> {
 
     generate_execution_success_tests(&mut test_file, &test_dir);
     generate_execution_failure_tests(&mut test_file, &test_dir);
-    generate_execution_panic_tests(&mut test_file, &test_dir);
     generate_noir_test_success_tests(&mut test_file, &test_dir);
     generate_noir_test_failure_tests(&mut test_file, &test_dir);
     generate_compile_success_empty_tests(&mut test_file, &test_dir);
@@ -537,32 +536,6 @@ fn generate_execution_failure_tests(test_file: &mut File, test_data_dir: &Path) 
                 vary_brillig: !IGNORED_BRILLIG_TESTS.contains(&test_name.as_str()),
                 ..Default::default()
             },
-        );
-    }
-    writeln!(test_file, "}}").unwrap();
-}
-
-fn generate_execution_panic_tests(test_file: &mut File, test_data_dir: &Path) {
-    let test_type = "execution_panic";
-    let test_cases = read_test_cases(test_data_dir, test_type);
-
-    writeln!(
-        test_file,
-        "mod {test_type} {{
-        use super::*;
-    "
-    )
-    .unwrap();
-    for (test_name, test_dir) in test_cases {
-        let test_dir = test_dir.display();
-
-        generate_test_cases(
-            test_file,
-            &test_name,
-            &test_dir,
-            "execute",
-            "execution_panic(nargo);",
-            &MatrixConfig::default(),
         );
     }
     writeln!(test_file, "}}").unwrap();
