14857f0c82fa541a61e5b12147bb008b890b843a
rustdoc <github@users.noreply.github.com>
2026-07-13 14:08:09 +0000

    update rustdoc

--- docs/noirc_evaluator/ssa/validation/flatten_post_check/sidebar-items.js
+++ docs/noirc_evaluator/ssa/validation/flatten_post_check/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["escape_error","is_one","is_predicate","is_predicate_guard","strip_casts","verify_function","verify_side_effect_predicates"]};
+window.SIDEBAR_ITEMS = {"fn":["escape_error","is_div_or_mod_by_nonzero_constant","is_one","is_predicate","is_predicate_guard","strip_casts","verify_function","verify_side_effect_predicates"]};
