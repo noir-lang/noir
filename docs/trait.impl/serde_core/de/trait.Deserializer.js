d309347ad1e414073da2dc249ed0eb44c7b5d50a
rustdoc <github@users.noreply.github.com>
2026-07-21 08:42:47 +0000

    update rustdoc

--- docs/trait.impl/serde_core/de/trait.Deserializer.js
+++ docs/trait.impl/serde_core/de/trait.Deserializer.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["msgpack_tagged",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.Deserializer.html\" title=\"trait serde_core::de::Deserializer\">Deserializer</a>&lt;'de&gt; for &amp;mut <a class=\"struct\" href=\"msgpack_tagged/deserializer/struct.Deserializer.html\" title=\"struct msgpack_tagged::deserializer::Deserializer\">Deserializer</a>&lt;'_, 'de&gt;"]]]]);
+    var implementors = Object.fromEntries([["msgpack_tagged",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.229/serde_core/de/trait.Deserializer.html\" title=\"trait serde_core::de::Deserializer\">Deserializer</a>&lt;'de&gt; for &amp;mut <a class=\"struct\" href=\"msgpack_tagged/deserializer/struct.Deserializer.html\" title=\"struct msgpack_tagged::deserializer::Deserializer\">Deserializer</a>&lt;'_, 'de&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- docs/trait.impl/serde_core/ser/trait.SerializeTupleStruct.js
+++ docs/trait.impl/serde_core/ser/trait.SerializeTupleStruct.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["msgpack_tagged",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.SerializeTupleStruct.html\" title=\"trait serde_core::ser::SerializeTupleStruct\">SerializeTupleStruct</a> for <a class=\"struct\" href=\"msgpack_tagged/serializer/struct.TaggedSerializeProduct.html\" title=\"struct msgpack_tagged::serializer::TaggedSerializeProduct\">TaggedSerializeProduct</a>&lt;'_, '_, W&gt;"]]]]);
+    var implementors = Object.fromEntries([["msgpack_tagged",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.229/serde_core/ser/trait.SerializeTupleStruct.html\" title=\"trait serde_core::ser::SerializeTupleStruct\">SerializeTupleStruct</a> for <a class=\"struct\" href=\"msgpack_tagged/serializer/struct.TaggedSerializeProduct.html\" title=\"struct msgpack_tagged::serializer::TaggedSerializeProduct\">TaggedSerializeProduct</a>&lt;'_, '_, W&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
