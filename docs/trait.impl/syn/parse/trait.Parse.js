2c6338d0146886cb811f12e4e7743c0fbb07f89b
rustdoc <github@users.noreply.github.com>
2026-07-22 11:28:45 +0000

    update rustdoc

--- docs/trait.impl/syn/parse/trait.Parse.js
+++ docs/trait.impl/syn/parse/trait.Parse.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["msgpack_tagged_derive",[["impl <a class=\"trait\" href=\"https://docs.rs/syn/2.0.118/syn/parse/trait.Parse.html\" title=\"trait syn::parse::Parse\">Parse</a> for <a class=\"struct\" href=\"msgpack_tagged_derive/struct.TagArgs.html\" title=\"struct msgpack_tagged_derive::TagArgs\">TagArgs</a>"]]]]);
+    var implementors = Object.fromEntries([["msgpack_tagged_derive",[["impl <a class=\"trait\" href=\"https://docs.rs/syn/3.0.2/syn/parse/trait.Parse.html\" title=\"trait syn::parse::Parse\">Parse</a> for <a class=\"struct\" href=\"msgpack_tagged_derive/struct.TagArgs.html\" title=\"struct msgpack_tagged_derive::TagArgs\">TagArgs</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[298]}
+//{"start":57,"fragment_lengths":[296]}
