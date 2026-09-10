f0b30d5ebeec4b68d35b862060e2b7c01bd5272f
rustdoc <github@users.noreply.github.com>
2026-09-10 10:50:44 +0000

    update rustdoc

--- docs/noirc_evaluator/ssa/ir/dom/sidebar-items.js
+++ docs/noirc_evaluator/ssa/ir/dom/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["DominatorTree","DominatorTreeNode"]};
+window.SIDEBAR_ITEMS = {"enum":["DominanceQueries"],"struct":["DfsInterval","DominatorTree","DominatorTreeNode"]};
